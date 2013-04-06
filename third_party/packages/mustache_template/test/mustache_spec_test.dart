450291f979a52a8bf657a00f49314eb4b181a6c2
Greg Lowe <greg@vis.net.nz>
2013-04-06 22:22:41 +1300

    Update spec test

--- third_party/packages/mustache_template/test/mustache_spec_test.dart
+++ third_party/packages/mustache_template/test/mustache_spec_test.dart
@@ -4,8 +4,25 @@ import 'package:mustache/mustache.dart' as mustache;
 import 'dart:json' as json;
 
 var verbose = false;
+var testName = null;
 
 main() {
+	var args = new Options().arguments;
+	if (!args.isEmpty) {
+		for (var arg in args) {
+			if (arg == '--verbose' || arg == '-v') {
+				verbose = true;
+			} else if (arg.startsWith('-')) {
+				print('Unknown argument: $arg');
+				print('Usage: dart test/mustache_spec_test.dart [--verbose] [test name]');
+				return;
+			} else {
+				testName = arg;
+				verbose = true;
+			}
+		}
+	}
+
 	var specs = new Directory('test/spec')
 		.listSync()
 		.where((f) => f is File && f.path.endsWith('.json'));
@@ -27,6 +44,9 @@ main() {
 }
 
 runTest(String name, String desc, Map data, String template, String expected) {
+	if (testName != null && name != testName)
+		return;
+
 	var output;
 	var exception;
 	try {
@@ -38,10 +58,14 @@ runTest(String name, String desc, Map data, String template, String expected) {
 	var result = passed ? 'Pass' : 'Fail';
 	print('    $result  $name');
 	if (verbose && !passed) {
+		print(desc);
 		print('        Template: $template');
 		print('        Data:     ${json.stringify(data)}');
 		print('        Expected: $expected');
+		print('        Output:   $output');
 		if (exception != null) print('        Exception: $exception'); // TODO stack trace.
+		print('');
+		print('');
 	}
 }
 
