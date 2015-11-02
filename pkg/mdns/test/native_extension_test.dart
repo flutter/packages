a429c947f7d15f72aeaaa523ec8c4e41b023eff2
Søren Gjesse <sgjesse@google.com>
2015-11-02 09:06:19 +0100

    Extend the mDNS package with a native extension used on Mac OS
    On Mac OS the mDNSResponder daemon has opened port 5353
    exclusively. Therefore it is not possible to speak the mDNS protocol
    from Dart using datagram sockets.
    
    The native extension uses the Mac OS DNS Service Discovery C API.
    
    Updated the copyright notice to use Fletch instead of Dart.
    
    BUG= https://github.com/dart-lang/fletch/issues/225
    R=karlklose@google.com, ricow@google.com
    
    Review URL: https://codereview.chromium.org/1407123010 .

---
+++ pkg/mdns/test/native_extension_test.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2015, the Fletch project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE.md file.
+
+import 'dart:async';
+
+import 'package:expect/expect.dart';
+import 'package:mdns/src/native_extension_client.dart';
+
+Future<Null> main() async {
+  String message1 = 'Hello, world!';
+  String result1 = await nativeExtensionEchoTest(message1);
+  Expect.equals(message1, result1);
+
+  List<int> message2 = [1, 2];
+  List<int> result2 = await nativeExtensionEchoTest(message2);
+  Expect.listEquals(message2, result2);
+}
