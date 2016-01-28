afd0ffadc4c4411668f31c00810960758a49b6a0
Michael Thomsen <mit@google.com>
2016-01-28 10:57:01 +0100

    Update project authors statements to Dartino
    Updated using:
    $ find . -type f -exec sed -i 's/Fletch project authors/Dartino project authors/g' {} \;
    
    All tests pass.
    
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org/1649703002 .

--- pkg/mdns/test/lookup_resolver_test.dart
+++ pkg/mdns/test/lookup_resolver_test.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, the Fletch project authors. Please see the AUTHORS file
+// Copyright (c) 2015, the Dartino project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE.md file.
 
