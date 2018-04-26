86502830bfb195d608cf7113cb3844f76d03f3e8
Yegor <yjbanov@google.com>
2018-04-26 10:22:15 -0700

    bump version to 2.0.1 (#16)

--- lib/src/version.dart
+++ lib/src/version.dart
@@ -9,7 +9,7 @@
 library version;
 
 /// The SDK version reported to Sentry.io in the submitted events.
-const String sdkVersion = '2.0.0';
+const String sdkVersion = '2.0.1';
 
 /// The SDK name reported to Sentry.io in the submitted events.
 const String sdkName = 'dart';
