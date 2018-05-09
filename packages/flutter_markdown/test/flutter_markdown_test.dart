d3c46bb7f51b8cc34a8f361cbfc063f72e3f1ec0
Seth Westphal <westy92@gmail.com>
2018-05-09 15:03:59 -0500

    Improve unit tests.

--- packages/flutter_markdown/test/flutter_markdown_test.dart
+++ packages/flutter_markdown/test/flutter_markdown_test.dart
@@ -337,13 +337,21 @@ void main() {
     }
   });
 
-  testWidgets('HTML entities', (WidgetTester tester) async {
-    final String mdLine = 'Line 1 <\n\nc < > c c\n\n< Line & © 2';
-    await tester.pumpWidget(_boilerplate(new MarkdownBody(data: mdLine)));
+  group('Parser does not convert', () {
+    testWidgets('& to &amp; when parsing', (WidgetTester tester) async {
+      await tester.pumpWidget(_boilerplate(const Markdown(data: '&')));
+      _expectTextStrings(tester.allWidgets, <String>['&']);
+    });
 
-    final Iterable<Widget> widgets = tester.allWidgets;
-    _expectTextStrings(
-        widgets, <String>['Line 1 <', 'c < > c c', '< Line & © 2']);
+    testWidgets('< to &lt; when parsing', (WidgetTester tester) async {
+      await tester.pumpWidget(_boilerplate(const Markdown(data: '<')));
+      _expectTextStrings(tester.allWidgets, <String>['<']);
+    });
+
+    testWidgets('existing HTML entities when parsing', (WidgetTester tester) async {
+      await tester.pumpWidget(_boilerplate(const Markdown(data: '&amp; &copy;')));
+      _expectTextStrings(tester.allWidgets, <String>['&amp; &copy;']);
+    });
   });
 
   testWidgets('Changing config - data', (WidgetTester tester) async {
