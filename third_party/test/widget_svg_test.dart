4516446dfbf45e953eaf315e6a616346734179c7
Dan Field <dfield@gmail.com>
2018-05-26 23:29:06 -0400

    pref const

--- third_party/test/widget_svg_test.dart
+++ third_party/test/widget_svg_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mockito/mockito.dart';
 
 void main() {
-  final String svgStr = '''<?xml version="1.0" standalone="no"?>
+  const String svgStr = '''<?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
   "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg viewBox="0 0 1000 300"
