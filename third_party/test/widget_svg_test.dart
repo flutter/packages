7109f7f3dc68f860882c81f263513f0838fdb95c
Dan Field <dfield@gmail.com>
2018-06-14 09:50:12 -0400

    improve tests, improve coverage

--- third_party/test/widget_svg_test.dart
+++ third_party/test/widget_svg_test.dart
@@ -9,41 +9,86 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mockito/mockito.dart';
 
+Future<Null> _checkWidgetAndGolden(Key key, String filename) async {
+  final Finder widgetFinder = find.byKey(key);
+  expect(widgetFinder, findsOneWidget);
+
+  await expectLater(widgetFinder, matchesGoldenFile('golden_widget/$filename'));
+}
+
 void main() {
-  const String svgStr = '''<?xml version="1.0" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
-  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg viewBox="0 0 1000 300"
-     xmlns="http://www.w3.org/2000/svg" version="1.1">
-  <desc>Example text01 - 'Hello, out there' in blue</desc>
-
-  <text x="250" y="150" 
-        font-family="Verdana" font-size="55" stroke="red" fill="blue" >
-    Hello, out there
-  </text>
-
-  <!-- Show outline of canvas using 'rect' element -->
-  <rect x="1" y="1" width="998" height="298"
-        fill="none" stroke="blue" stroke-width="2" />
+  const String svgStr =
+      '''<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 166 202">
+    <defs>
+        <linearGradient id="triangleGradient">
+            <stop offset="20%" stop-color="#000000" stop-opacity=".55" />
+            <stop offset="85%" stop-color="#616161" stop-opacity=".01" />
+        </linearGradient>
+        <linearGradient id="rectangleGradient" x1="0%" x2="0%" y1="0%" y2="100%">
+            <stop offset="20%" stop-color="#000000" stop-opacity=".15" />
+            <stop offset="85%" stop-color="#616161" stop-opacity=".01" />
+        </linearGradient>
+    </defs>
+    <path fill="#42A5F5" fill-opacity=".8" d="M37.7 128.9 9.8 101 100.4 10.4 156.2 10.4"/>
+    <path fill="#42A5F5" fill-opacity=".8" d="M156.2 94 100.4 94 79.5 114.9 107.4 142.8"/>
+    <path fill="#0D47A1" d="M79.5 170.7 100.4 191.6 156.2 191.6 156.2 191.6 107.4 142.8"/>
+    <g transform="matrix(0.7071, -0.7071, 0.7071, 0.7071, -77.667, 98.057)">
+        <rect width="39.4" height="39.4" x="59.8" y="123.1" fill="#42A5F5" />
+        <rect width="39.4" height="5.5" x="59.8" y="162.5" fill="url(#rectangleGradient)" />
+    </g>
+    <path d="M79.5 170.7 120.9 156.4 107.4 142.8" fill="url(#triangleGradient)" />
 </svg>''';
   final Uint8List svg = utf8.encode(svgStr);
 
   testWidgets('SvgPicture.string', (WidgetTester tester) async {
     final GlobalKey key = new GlobalKey();
-    await tester.pumpWidget(new SvgPicture.string(
-      svgStr,
-      key: key,
-    ));
-    expect(find.byKey(key), findsOneWidget);
+    await tester.pumpWidget(new RepaintBoundary(
+        key: key,
+        child: new SvgPicture.string(
+          svgStr,
+          // key: key,
+          width: 100.0,
+          height: 100.0,
+        )));
+
+    await tester.pumpAndSettle();
+    await _checkWidgetAndGolden(key, 'flutter_logo.string.png');
+  });
+
+  testWidgets('SvgPicture.string rtl', (WidgetTester tester) async {
+    final GlobalKey key = new GlobalKey();
+    await tester.pumpWidget(
+      new RepaintBoundary(
+        key: key,
+        child: new Directionality(
+          textDirection: TextDirection.rtl,
+          child: new SvgPicture.string(
+            svgStr,
+            matchTextDirection: true,
+            width: 100.0,
+            height: 100.0,
+          ),
+        ),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+    await _checkWidgetAndGolden(key, 'flutter_logo.string.rtl.png');
   });
 
   testWidgets('SvgPicture.memory', (WidgetTester tester) async {
     final GlobalKey key = new GlobalKey();
-    await tester.pumpWidget(new SvgPicture.memory(
-      svg,
-      key: key,
-    ));
-    expect(find.byKey(key), findsOneWidget);
+    await tester.pumpWidget(
+      new RepaintBoundary(
+        key: key,
+        child: new SvgPicture.memory(
+          svg,
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+
+    await _checkWidgetAndGolden(key, 'flutter_logo.memory.png');
   });
 
   testWidgets('SvgPicture.asset', (WidgetTester tester) async {
@@ -52,12 +97,17 @@ void main() {
         (_) => new Future<ByteData>.value(new ByteData.view(svg.buffer)));
 
     final GlobalKey key = new GlobalKey();
-    await tester.pumpWidget(new SvgPicture.asset(
-      'test.svg',
-      key: key,
-      bundle: mockAsset,
-    ));
-    expect(find.byKey(key), findsOneWidget);
+    await tester.pumpWidget(
+      new RepaintBoundary(
+        key: key,
+        child: new SvgPicture.asset(
+          'test.svg',
+          bundle: mockAsset,
+        ),
+      ),
+    );
+    await tester.pumpAndSettle();
+    await _checkWidgetAndGolden(key, 'flutter_logo.asset.png');
   });
 
   final MockHttpClient mockHttpClient = new MockHttpClient();
@@ -94,11 +144,16 @@ void main() {
     HttpOverrides.runZoned(() async {
       when(mockResponse.statusCode).thenReturn(200);
       final GlobalKey key = new GlobalKey();
-      await tester.pumpWidget(new SvgPicture.network(
-        'test.svg',
-        key: key,
-      ));
-      expect(find.byKey(key), findsOneWidget);
+      await tester.pumpWidget(
+        new RepaintBoundary(
+          key: key,
+          child: new SvgPicture.network(
+            'test.svg',
+          ),
+        ),
+      );
+      await tester.pumpAndSettle();
+      await _checkWidgetAndGolden(key, 'flutter_logo.network.png');
     }, createHttpClient: (SecurityContext c) => mockHttpClient);
   });
 
