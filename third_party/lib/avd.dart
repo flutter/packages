0b95960bcbb03fa2e1054159cff3060279df91ea
Dan Field <dfield@gmail.com>
2018-06-20 21:37:58 -0400

    fix analysis; remove dead code

--- third_party/lib/avd.dart
+++ third_party/lib/avd.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 import 'dart:typed_data';
 import 'dart:ui' show Picture;
 
-import 'package:flutter/services.dart' show rootBundle, AssetBundle;
+import 'package:flutter/services.dart' show AssetBundle;
 import 'package:flutter/widgets.dart';
 import 'package:xml/xml.dart' hide parse;
 import 'package:xml/xml.dart' as xml show parse;
@@ -168,40 +167,3 @@ DrawableRoot fromAvdString(String rawSvg, Size size) {
       viewBox, children, new DrawableDefinitionServer(), null);
 }
 
-/// Creates a [DrawableRoot] from a bundled asset.
-Future<DrawableRoot> loadAsset(String assetName, Size size,
-    {AssetBundle bundle, String package}) async {
-  bundle ??= rootBundle;
-  final String rawSvg = await bundle.loadString(
-    package == null ? assetName : 'packages/$package/$assetName',
-  );
-  return fromAvdString(rawSvg, size);
-}
-
-final HttpClient _httpClient = new HttpClient();
-
-/// Creates a [DrawableRoot] from a network asset with an HTTP get request.
-Future<DrawableRoot> loadNetworkAsset(String url, Size size) async {
-  final Uri uri = Uri.base.resolve(url);
-  final HttpClientRequest request = await _httpClient.getUrl(uri);
-  final HttpClientResponse response = await request.close();
-  if (response.statusCode != HttpStatus.OK)
-    throw new HttpException('Could not get network SVG asset', uri: uri);
-  final String rawSvg = await _consolidateHttpClientResponse(response);
-  return fromAvdString(rawSvg, size);
-}
-
-Future<String> _consolidateHttpClientResponse(
-    HttpClientResponse response) async {
-  final Completer<String> completer = new Completer<String>.sync();
-  final StringBuffer buffer = new StringBuffer();
-
-  response.transform(utf8.decoder).listen((String chunk) {
-    buffer.write(chunk);
-  }, onDone: () {
-    // There's a bug right now where sometimes GZIP encoded payloads aren't coming all the way through..
-    completer.complete(buffer.toString());
-  }, onError: completer.completeError, cancelOnError: true);
-
-  return completer.future;
-}
