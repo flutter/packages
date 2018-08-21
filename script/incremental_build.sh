ab93a7c7533a08fb1ff669089aa0bb66dc429af4
Greg Spencer <gspencer@google.com>
2018-08-20 20:42:28 -0700

    Rename PLUGIN_SHARDING to BUILD_SHARDING

--- script/incremental_build.sh
+++ script/incremental_build.sh
@@ -15,15 +15,15 @@ fi
 BRANCH_NAME="${BRANCH_NAME:-"$(git rev-parse --abbrev-ref HEAD)"}"
 if [[ "${BRANCH_NAME}" == "master" ]]; then
   echo "Running for all packages"
-  (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" $PLUGIN_SHARDING)
+  (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" $BUILD_SHARDING)
 else
   # Sets CHANGED_PACKAGES
   check_changed_packages
 
   if [[ "$CHANGED_PACKAGES" == "" ]]; then
     echo "Running for all packages"
-    (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" $PLUGIN_SHARDING)
+    (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" $BUILD_SHARDING)
   else
-    (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" --plugins="$CHANGED_PACKAGES" $PLUGIN_SHARDING)
+    (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" --plugins="$CHANGED_PACKAGES" $BUILD_SHARDING)
   fi
 fi
