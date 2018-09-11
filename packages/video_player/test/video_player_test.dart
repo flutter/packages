flutter/plugins

commit a9f7657c63542cfa4b83d0f740b5d13b18cc23ae

Rebuild VideoPlayer once VideoPlayerController finishes initialization. (#767)
* Rebuild VideoPlayer once VideoPlayerController finishes initialization.

Otherwise if VideoPlayer is built shortly after the controller starts initialization and doesn't yet have _textureId filled the widgets renders as an empty Container and never rebuilds to show the video later.

* Queue VideoPlayer events in the EventSink.

This ensure that initialization events that are generated in `onPlayerStateChanged` before the Dart side has chance to subscribe to the channel are queued and eventually delivered.

Fixes https://github.com/flutter/flutter/issues/21483.

packages/video_player/lib/video_player.dart
@@ -420,16 +420,39 @@ class _VideoAppLifeCycleObserver extends WidgetsBindingObserver {
 }
 
 /// Displays the video controlled by [controller].
-class VideoPlayer extends StatelessWidget {
+class VideoPlayer extends StatefulWidget {
   final VideoPlayerController controller;
 
   VideoPlayer(this.controller);
 
   @override
+  _VideoPlayerState createState() => new _VideoPlayerState();
+}
+
+class _VideoPlayerState extends State<VideoPlayer> {
+  int textureId;
+
+  @override
+  void initState() {
+    super.initState();
+    textureId = widget.controller._textureId;
+    // Need to listen for initialization events since the actual texture ID
+    // becomes available after asynchronous initialization finishes.
+    widget.controller.addListener(() {
+      final int newTextureId = widget.controller._textureId;
+      if (newTextureId != textureId) {
+        setState(() {
+          textureId = newTextureId;
+        });
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return controller._textureId == null
+    return textureId == null
         ? new Container()
-        : new Texture(textureId: controller._textureId);
+        : new Texture(textureId: textureId);
   }
 }
 

packages/video_player/pubspec.yaml
@@ -2,7 +2,7 @@ name: video_player
 description: Flutter plugin for displaying inline video with other Flutter
   widgets on Android and iOS.
 author: Flutter Team <flutter-dev@googlegroups.com>
-version: 0.6.4
+version: 0.6.5
 homepage: https://github.com/flutter/plugins/tree/master/packages/video_player
 
 flutter:

packages/video_player/android/src/main/java/io/flutter/plugins/videoplayer/QueuingEventSink.java
@@ -1,0 +1,81 @@
+package io.flutter.plugins.videoplayer;
+
+import io.flutter.plugin.common.EventChannel;
+import java.util.ArrayList;
+
+/**
+ * And implementation of {@link EventChannel.EventSink} which can wrap an underlying sink.
+ *
+ * <p>It delivers messages immediately when downstream is available, but it queues messages before
+ * the delegate event sink is set with setDelegate.
+ *
+ * <p>This class is not thread-safe. All calls must be done on the same thread or synchronized
+ * externally.
+ */
+final class QueuingEventSink implements EventChannel.EventSink {
+  private EventChannel.EventSink delegate;
+  private ArrayList<Object> eventQueue = new ArrayList<>();
+  private boolean done = false;
+
+  public void setDelegate(EventChannel.EventSink delegate) {
+    this.delegate = delegate;
+    maybeFlush();
+  }
+
+  @Override
+  public void endOfStream() {
+    enqueue(new EndOfStreamEvent());
+    maybeFlush();
+    done = true;
+  }
+
+  @Override
+  public void error(String code, String message, Object details) {
+    enqueue(new ErrorEvent(code, message, details));
+    maybeFlush();
+  }
+
+  @Override
+  public void success(Object event) {
+    enqueue(event);
+    maybeFlush();
+  }
+
+  private void enqueue(Object event) {
+    if (done) {
+      return;
+    }
+    eventQueue.add(event);
+  }
+
+  private void maybeFlush() {
+    if (delegate == null) {
+      return;
+    }
+    for (Object event : eventQueue) {
+      if (event instanceof EndOfStreamEvent) {
+        delegate.endOfStream();
+      } else if (event instanceof ErrorEvent) {
+        ErrorEvent errorEvent = (ErrorEvent) event;
+        delegate.error(errorEvent.code, errorEvent.message, errorEvent.details);
+      } else {
+        delegate.success(event);
+      }
+    }
+    eventQueue.clear();
+  }
+
+  private static class EndOfStreamEvent {}
+
+  private static class ErrorEvent {
+    String code;
+    String message;
+    Object details;
+
+    ErrorEvent(String code, String message, Object details) {
+      this.code = code;
+      this.message = message;
+      this.details = details;
+    }
+  }
+}

packages/video_player/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayerPlugin.java
@@ -59,7 +59,7 @@ public class VideoPlayerPlugin implements MethodCallHandler {
 
     private final TextureRegistry.SurfaceTextureEntry textureEntry;
 
-    private EventChannel.EventSink eventSink;
+    private QueuingEventSink eventSink = new QueuingEventSink();
 
     private final EventChannel eventChannel;
 
@@ -128,12 +128,12 @@ public class VideoPlayerPlugin implements MethodCallHandler {
           new EventChannel.StreamHandler() {
             @Override
             public void onListen(Object o, EventChannel.EventSink sink) {
-              eventSink = sink;
+              eventSink.setDelegate(sink);
             }
 
             @Override
             public void onCancel(Object o) {
-              eventSink = null;
+              eventSink.setDelegate(null);
             }
           });
 
@@ -148,14 +148,12 @@ public class VideoPlayerPlugin implements MethodCallHandler {
             public void onPlayerStateChanged(final boolean playWhenReady, final int playbackState) {
               super.onPlayerStateChanged(playWhenReady, playbackState);
               if (playbackState == Player.STATE_BUFFERING) {
-                if (eventSink != null) {
-                  Map<String, Object> event = new HashMap<>();
-                  event.put("event", "bufferingUpdate");
-                  List<Integer> range = Arrays.asList(0, exoPlayer.getBufferedPercentage());
-                  // iOS supports a list of buffered ranges, so here is a list with a single range.
-                  event.put("values", Collections.singletonList(range));
-                  eventSink.success(event);
-                }
+                Map<String, Object> event = new HashMap<>();
+                event.put("event", "bufferingUpdate");
+                List<Integer> range = Arrays.asList(0, exoPlayer.getBufferedPercentage());
+                // iOS supports a list of buffered ranges, so here is a list with a single range.
+                event.put("values", Collections.singletonList(range));
+                eventSink.success(event);
               } else if (playbackState == Player.STATE_READY && !isInitialized) {
                 isInitialized = true;
                 sendInitialized();
@@ -212,7 +210,7 @@ public class VideoPlayerPlugin implements MethodCallHandler {
     }
 
     private void sendInitialized() {
-      if (isInitialized && eventSink != null) {
+      if (isInitialized) {
         Map<String, Object> event = new HashMap<>();
         event.put("event", "initialized");
         event.put("duration", exoPlayer.getDuration());

flutter/packages

commit 18932005667ec7788ff9d26c9220c8ec757984e4

Update VideoPlayer widget when the attached controller changes (#769)
* Update textureId in VideoPlayer widget when the attached controller changes.

This fixes a bug introduced in https://github.com/flutter/plugins/pull/767: if a State is attached to a new widget it never switched to the new controller.

Also this change makes sure to remove listeners during deinitialization.

* Add tests for conditions identified in https://github.com/flutter/flutter/issues/21483.

This is a breaking change since it reduces the public interface of VideoPlayerController to make it easier to fake it in the test (and hide unnecessary implementation details).

packages/video_player/lib/video_player.dart
@@ -148,9 +148,9 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   /// is constructed with.
   final DataSourceType dataSourceType;
 
-  String package;
-  Timer timer;
-  bool isDisposed = false;
+  final String package;
+  Timer _timer;
+  bool _isDisposed = false;
   Completer<void> _creatingCompleter;
   StreamSubscription<dynamic> _eventSubscription;
   _VideoAppLifeCycleObserver _lifeCycleObserver;
@@ -171,6 +171,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   /// null.
   VideoPlayerController.network(this.dataSource)
       : dataSourceType = DataSourceType.network,
+        package = null,
         super(new VideoPlayerValue(duration: null));
 
   /// Constructs a [VideoPlayerController] playing a video from a file.
@@ -180,7 +181,11 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   VideoPlayerController.file(File file)
       : dataSource = 'file://${file.path}',
         dataSourceType = DataSourceType.file,
+        package = null,
         super(new VideoPlayerValue(duration: null));
+
+  // Visible for testing.
+  int get textureId => _textureId;
 
   Future<void> initialize() async {
     _lifeCycleObserver = new _VideoAppLifeCycleObserver(this);
@@ -231,7 +236,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
           break;
         case 'completed':
           value = value.copyWith(isPlaying: false);
-          timer?.cancel();
+          _timer?.cancel();
           break;
         case 'bufferingUpdate':
           final List<dynamic> values = map['values'];
@@ -251,7 +256,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
     void errorListener(Object obj) {
       final PlatformException e = obj;
       value = new VideoPlayerValue.erroneous(e.message);
-      timer?.cancel();
+      _timer?.cancel();
     }
 
     _eventSubscription = _eventChannelFor(_textureId)
@@ -268,9 +273,9 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   Future<void> dispose() async {
     if (_creatingCompleter != null) {
       await _creatingCompleter.future;
-      if (!isDisposed) {
-        isDisposed = true;
-        timer?.cancel();
+      if (!_isDisposed) {
+        _isDisposed = true;
+        _timer?.cancel();
         await _eventSubscription?.cancel();
         await _channel.invokeMethod(
           'dispose',
@@ -279,7 +284,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
       }
       _lifeCycleObserver.dispose();
     }
-    isDisposed = true;
+    _isDisposed = true;
     super.dispose();
   }
 
@@ -299,7 +304,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   }
 
   Future<void> _applyLooping() async {
-    if (!value.initialized || isDisposed) {
+    if (!value.initialized || _isDisposed) {
       return;
     }
     _channel.invokeMethod(
@@ -309,7 +314,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   }
 
   Future<void> _applyPlayPause() async {
-    if (!value.initialized || isDisposed) {
+    if (!value.initialized || _isDisposed) {
       return;
     }
     if (value.isPlaying) {
@@ -317,21 +322,21 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
         'play',
         <String, dynamic>{'textureId': _textureId},
       );
-      timer = new Timer.periodic(
+      _timer = new Timer.periodic(
         const Duration(milliseconds: 500),
         (Timer timer) async {
-          if (isDisposed) {
+          if (_isDisposed) {
             return;
           }
           final Duration newPosition = await position;
-          if (isDisposed) {
+          if (_isDisposed) {
             return;
           }
           value = value.copyWith(position: newPosition);
         },
       );
     } else {
-      timer?.cancel();
+      _timer?.cancel();
       await _channel.invokeMethod(
         'pause',
         <String, dynamic>{'textureId': _textureId},
@@ -340,7 +345,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   }
 
   Future<void> _applyVolume() async {
-    if (!value.initialized || isDisposed) {
+    if (!value.initialized || _isDisposed) {
       return;
     }
     await _channel.invokeMethod(
@@ -351,7 +356,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
 
   /// The position in the current video.
   Future<Duration> get position async {
-    if (isDisposed) {
+    if (_isDisposed) {
       return null;
     }
     return new Duration(
@@ -363,7 +368,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
   }
 
   Future<void> seekTo(Duration moment) async {
-    if (isDisposed) {
+    if (_isDisposed) {
       return;
     }
     if (moment > value.duration) {
@@ -430,29 +435,48 @@ class VideoPlayer extends StatefulWidget {
 }
 
 class _VideoPlayerState extends State<VideoPlayer> {
-  int textureId;
+  VoidCallback _listener;
+  int _textureId;
+
+  _VideoPlayerState() {
+    _listener = () {
+      final int newTextureId = widget.controller.textureId;
+      if (newTextureId != _textureId) {
+        setState(() {
+          _textureId = newTextureId;
+        });
+      }
+    };
+  }
 
   @override
   void initState() {
     super.initState();
-    textureId = widget.controller._textureId;
+    _textureId = widget.controller.textureId;
     // Need to listen for initialization events since the actual texture ID
     // becomes available after asynchronous initialization finishes.
-    widget.controller.addListener(() {
-      final int newTextureId = widget.controller._textureId;
-      if (newTextureId != textureId) {
-        setState(() {
-          textureId = newTextureId;
-        });
-      }
-    });
+    widget.controller.addListener(_listener);
+  }
+
+  @override
+  void didUpdateWidget(VideoPlayer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    oldWidget.controller.removeListener(_listener);
+    _textureId = widget.controller.textureId;
+    widget.controller.addListener(_listener);
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    widget.controller.removeListener(_listener);
   }
 
   @override
   Widget build(BuildContext context) {
-    return textureId == null
+    return _textureId == null
         ? new Container()
-        : new Texture(textureId: textureId);
+        : new Texture(textureId: _textureId);
   }
 }
 

packages/video_player/pubspec.yaml
@@ -2,7 +2,7 @@ name: video_player
 description: Flutter plugin for displaying inline video with other Flutter
   widgets on Android and iOS.
 author: Flutter Team <flutter-dev@googlegroups.com>
-version: 0.6.5
+version: 0.7.0
 homepage: https://github.com/flutter/plugins/tree/master/packages/video_player
 
 flutter:
@@ -14,6 +14,10 @@ flutter:
 dependencies:
   meta: "^1.0.5"
   flutter:
+    sdk: flutter
+
+dev_dependencies:
+  flutter_test:
     sdk: flutter
 
 environment:

packages/video_player/test/video_player_test.dart
@@ -1,0 +1,76 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class FakeController extends ValueNotifier<VideoPlayerValue>
+    implements VideoPlayerController {
+  FakeController() : super(new VideoPlayerValue(duration: null));
+
+  @override
+  Future<void> dispose() async {
+    super.dispose();
+  }
+
+  @override
+  int textureId;
+
+  @override
+  String get dataSource => '';
+  @override
+  DataSourceType get dataSourceType => DataSourceType.file;
+  @override
+  String get package => null;
+  @override
+  Future<Duration> get position async => value.position;
+
+  @override
+  Future<void> seekTo(Duration moment) async {}
+  @override
+  Future<void> setVolume(double volume) async {}
+  @override
+  Future<void> initialize() async {}
+  @override
+  Future<void> pause() async {}
+  @override
+  Future<void> play() async {}
+  @override
+  Future<void> setLooping(bool looping) async {}
+}
+
+void main() {
+  testWidgets('update texture', (WidgetTester tester) async {
+    final FakeController controller = FakeController();
+    await tester.pumpWidget(VideoPlayer(controller));
+    expect(find.byType(Texture), findsNothing);
+
+    controller.textureId = 123;
+    controller.value = controller.value.copyWith(
+      duration: new Duration(milliseconds: 100),
+    );
+
+    await tester.pump();
+    expect(find.byType(Texture), findsOneWidget);
+  });
+
+  testWidgets('update controller', (WidgetTester tester) async {
+    final FakeController controller1 = FakeController();
+    controller1.textureId = 101;
+    await tester.pumpWidget(VideoPlayer(controller1));
+    expect(
+        find.byWidgetPredicate(
+          (Widget widget) => widget is Texture && widget.textureId == 101,
+        ),
+        findsOneWidget);
+
+    final FakeController controller2 = FakeController();
+    controller2.textureId = 102;
+    await tester.pumpWidget(VideoPlayer(controller2));
+    expect(
+        find.byWidgetPredicate(
+          (Widget widget) => widget is Texture && widget.textureId == 102,
+        ),
+        findsOneWidget);
+  });
+}