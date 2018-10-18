cf98f1f6ec305923635c39b5f012b07bd77299d4
Dan Field <dnfield@google.com>
2018-10-18 14:01:06 -0700

    update docs, add example

--- packages/multicast_dns/tool/packet_gen.dart
+++ packages/multicast_dns/tool/packet_gen.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2015, the Dartino project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // Support code to generate the hex-lists in test/decode_test.dart from
 // a hex-stream.
 void formatHexStream(String hexStream) {
