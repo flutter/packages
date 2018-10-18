1ae7747cc9330371de7ffdc6b2ef58855864d9cb
Dan Field <dnfield@google.com>
2018-10-18 14:41:03 -0700

    remove lint ignore, update CI

--- packages/multicast_dns/lib/mdns_client.dart
+++ packages/multicast_dns/lib/mdns_client.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: undefined_named_parameter
 // Copyright (c) 2015, the Dartino project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
