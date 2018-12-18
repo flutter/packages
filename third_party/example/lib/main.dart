4b8577ab4cad8653dcb2db5b261e735fd035efc2
Dan Field <dnfield@google.com>
2018-12-18 09:46:04 -0800

    revert unintentional change

--- third_party/example/lib/main.dart
+++ third_party/example/lib/main.dart
@@ -89,35 +89,6 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     _dimension = 250.0;
 
-  _painters.add(SvgPicture.string('''<svg xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 99.75 100.347">
-    <defs>
-        <style>.a{fill:#fff;}.b{fill:url(#c);}.c{fill:#512da8;}.d{fill:#362c66;}.e{fill:url(#d);}.f{fill:#007066;}.g{filter:url(#a);}</style>
-        <linearGradient id="c" x1="0.935" y1="0.877" x2="0.077" y2="0.206" gradientUnits="objectBoundingBox">
-            <stop offset="0" stop-color="#fff"/>
-            <stop offset="0.515" stop-color="#e6e6e6"/>
-            <stop offset="1" stop-color="#f5f5f5"/>
-        </linearGradient>
-        <linearGradient id="d" y1="0.5" x2="1" y2="0.5" gradientUnits="objectBoundingBox">
-            <stop offset="0" stop-color="#35bdb2"/>
-            <stop offset="1" stop-color="#008d7f"/>
-        </linearGradient>
-    </defs>
-    <g transform="translate(-13 -8)">
-        <g class="g" transform="matrix(1, 0, 0, 1, 13, 8)">
-            <rect class="a" width="81.75" height="82.347" rx="19" transform="translate(9 6)"/>
-        </g>
-        <path class="b" d="M25.911,53.347,0,26.709,19.1,27.9,35.206,39.145V20.169h5.848L54.778,32.461,57.524,0,70.054,12.531v9.815a31,31,0,0,1-31,31Z" transform="translate(33.695 43)"/>
-        <g transform="translate(33.338 27.128)">
-            <g transform="translate(0 0)">
-                <path class="c" d="M5.119,45.433v16.12H18.759a2.542,2.542,0,0,1,1.609.571h0l11.939,9.69V45.579L18.6,34.052,5.119,45.433ZM0,64.1V44.251H.006a2.548,2.548,0,0,1,.9-1.95L16.945,28.767l0,.005a2.548,2.548,0,0,1,3.285-.005L36.317,42.294a2.556,2.556,0,0,1,1.11,2.11V77.177h-.008a2.552,2.552,0,0,1-4.161,1.98l-15.407-12.5H2.784c-.074.006-.149.011-.225.011A2.56,2.56,0,0,1,0,64.1Z" transform="translate(0 -22.785)"/>
-                <path class="d" d="M130.086,69.7l-2.009-1.69,3.2-3.969,4.154,3.494Z" transform="translate(-103.599 -51.801)"/>
-                <path class="e" d="M112.144,17.264V33.385h6.734v5.1h-9.069c-.074.007-.149.01-.225.01a2.56,2.56,0,0,1-2.56-2.56V16.083h.006a2.548,2.548,0,0,1,.9-1.95L123.97.6l0,0a2.548,2.548,0,0,1,3.285,0l16.082,13.527a2.556,2.556,0,0,1,1.11,2.11V49.008h-.008a2.552,2.552,0,0,1-4.161,1.98l-15.406-12.5H124v-5.1h1.786a2.54,2.54,0,0,1,1.609.571h0l11.939,9.69V17.411l-13.7-11.528L112.144,17.264Z" transform="translate(-86.571 -0.001)"/>
-                <path class="f" d="M195.833,174.678h.993l-.675,5.1h-.318Z" transform="translate(-158.406 -141.294)"/>
-            </g>
-        </g>
-    </g>
-</svg>'''));
     for (String assetName in assetNames) {
       _painters.add(
         SvgPicture.asset(assetName),
