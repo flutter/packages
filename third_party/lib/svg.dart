1feea2487a3389af2daa9207d7e807b8f4f6d1db
Nicolai Henriksen <nicolai@procrasty.com>
2019-02-09 15:11:30 +0100

    Removed unused import.

--- third_party/lib/svg.dart
+++ third_party/lib/svg.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
-import 'dart:ui' show Picture, window;
+import 'dart:ui' show Picture;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart' show AssetBundle;
