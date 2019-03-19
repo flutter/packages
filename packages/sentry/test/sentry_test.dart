24394856336f7197a5ea16ae257946931f692657
Lejard Hadrien <hadrien.lejard@gmail.com>
2019-03-19 12:32:12 +0100

    sort directives

--- packages/sentry/test/sentry_test.dart
+++ packages/sentry/test/sentry_test.dart
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-import 'dart:async';
 
 import 'package:http/http.dart';
 import 'package:sentry/sentry.dart';
