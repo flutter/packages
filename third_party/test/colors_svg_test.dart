a6d1347d355e291a4b4b62308eb2fbf40e3c3503
christianalfoni <christianalfoni@gmail.com>
2019-09-02 07:58:19 +0200

    more tests

--- third_party/test/colors_svg_test.dart
+++ third_party/test/colors_svg_test.dart
@@ -23,8 +23,10 @@ void main() {
     expect(parseColor('hsl(0,0%,100%)'), const Color(0xFFFFFFFF));
     expect(parseColor('hsl(136,47%,79%)'), const Color(0xFFB0E3BE));
     expect(parseColor('hsl(136,80%,9%)'), const Color(0xFF05290E));
-    expect(parseColor('hsl(135,100%,50%)'), const Color(0xFF00FF40));
-    expect(parseColor('hsl(136,54%,43%)'), const Color(0xFF32A952));
+    expect(parseColor('hsl(17,55%,29%)'), const Color(0xFF733821));
+    expect(parseColor('hsl(78,55%,29%)'), const Color(0xFF5A7321));
+    expect(parseColor('hsl(192,55%,29%)'), const Color(0xFF216273));
+    expect(parseColor('hsl(297,55%,29%)'), const Color(0xFF6F2173));
     expect(parseColor('hsla(0,0%,100%, 0.0)'), const Color(0x00FFFFFF));
     expect(() => parseColor('invalid name'), throwsStateError);
   });
