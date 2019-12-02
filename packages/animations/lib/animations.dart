492ff8f58b2d1f479fee670edb7af50555fa8b46
Michael Goderbauer <goderbauer@google.com>
2019-12-02 15:48:00 -0800

    add export

--- packages/animations/lib/animations.dart
+++ packages/animations/lib/animations.dart
@@ -2,4 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export 'src/fade_through_transition.dart';
 export 'src/open_container.dart';
