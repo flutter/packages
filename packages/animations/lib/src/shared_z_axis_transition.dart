869133d3da5ff134f88f305f9e050af6678fd358
Shi Hao Hong <shihaohong94@gmail.com>
2019-12-30 14:40:40 -0800

    Add API documentation

--- packages/animations/lib/src/shared_z_axis_transition.dart
+++ packages/animations/lib/src/shared_z_axis_transition.dart
@@ -9,10 +9,61 @@ import 'package:flutter/widgets.dart';
 
 import 'utils/curves.dart';
 
-/// TODO: documentation
-class SharedZAxisPageTransitionBuilder extends PageTransitionsBuilder {
-  /// Construct a [SharedZAxisPageTransitionBuilder].
-  const SharedZAxisPageTransitionBuilder();
+/// Used by [PageTransitionsTheme] to define a page route transition animation
+/// in which outgoing and incoming elements share a scale transition.
+///
+/// The shared axis pattern provides the transition animation between UI elements
+/// that have a spatial or navigational relationship. For example,
+/// transitioning from one page of a sign up page to the next one.
+///
+///
+/// The following example shows how the SharedZAxisPageTransitionsBuilder can
+/// be used in a [PageTransitionsTheme] to change the default transitions
+/// of [MaterialPageRoute]s.
+///
+/// ```dart
+/// MaterialApp(
+///   theme: ThemeData(
+///     pageTransitionsTheme: PageTransitionsTheme(
+///       builders: {
+///         TargetPlatform.android: SharedZAxisPageTransitionsBuilder(),
+///         TargetPlatform.iOS: SharedZAxisPageTransitionsBuilder(),
+///       },
+///     ),
+///   ),
+///   routes: {
+///     '/': (BuildContext context) {
+///       return Container(
+///         color: Colors.red,
+///         child: Center(
+///           child: MaterialButton(
+///             child: Text('Push route'),
+///             onPressed: () {
+///               Navigator.of(context).pushNamed('/a');
+///             },
+///           ),
+///         ),
+///       );
+///     },
+///     '/a' : (BuildContext context) {
+///       return Container(
+///         color: Colors.blue,
+///         child: Center(
+///           child: MaterialButton(
+///             child: Text('Pop route'),
+///             onPressed: () {
+///               Navigator.of(context).pop();
+///             },
+///           ),
+///         ),
+///       );
+///     },
+///   },
+/// );
+/// ```
+class SharedZAxisPageTransitionsBuilder extends PageTransitionsBuilder {
+  /// Construct a [SharedZAxisPageTransitionsBuilder].
+  const SharedZAxisPageTransitionsBuilder();
 
   @override
   Widget buildTransitions<T>(
@@ -22,7 +73,7 @@ class SharedZAxisPageTransitionBuilder extends PageTransitionsBuilder {
     Animation<double> secondaryAnimation,
     Widget child,
   ) {
-    return _SharedZAxisPageTransition(
+    return SharedZAxisPageTransition(
       animation: animation,
       secondaryAnimation: secondaryAnimation,
       child: child,
@@ -30,24 +81,119 @@ class SharedZAxisPageTransitionBuilder extends PageTransitionsBuilder {
   }
 }
 
-class _SharedZAxisPageTransition extends StatefulWidget {
-  const _SharedZAxisPageTransition({
+/// Defines a transition in which outgoing and incoming elements share a scale
+/// transition.
+///
+/// The shared axis pattern provides the transition animation between UI elements
+/// that have a spatial or navigational relationship. For example,
+/// transitioning from one page of a sign up page to the next one.
+///
+/// Consider using [SharedZAxisPageTransition] within a
+/// [PageTransitionsTheme] if you want to apply this kind of transition to
+/// [MaterialPageRoute] transitions within a Navigator (see
+/// [SharedZAxisPageTransitionsBuilder] for some example code).
+///
+/// This transition can also be used directly in a
+/// [PageTransitionSwitcher.transitionBuilder] to transition
+/// from one widget to another as seen in the following example:
+///
+/// ```dart
+/// int _selectedIndex = 0;
+///
+/// final List<Color> _colors = [Colors.white, Colors.red, Colors.yellow];
+///
+/// @override
+/// Widget build(BuildContext context) {
+///   return Scaffold(
+///     appBar: AppBar(
+///       title: const Text('Page Transition Example'),
+///     ),
+///     body: PageTransitionSwitcher(
+///       reverse: true,
+///       transitionBuilder: (
+///         Widget child,
+///         Animation<double> primaryAnimation,
+///         Animation<double> secondaryAnimation,
+///       ) {
+///         return SharedZAxisPageTransition(
+///           animation: primaryAnimation,
+///           secondaryAnimation: secondaryAnimation,
+///           child: child,
+///         );
+///       },
+///       child: Container(
+///         key: ValueKey<int>(_selectedIndex),
+///         color: _colors[_selectedIndex],
+///         child: Center(
+///           child: FlutterLogo(size: 300),
+///         )
+///       ),
+///     ),
+///     bottomNavigationBar: BottomNavigationBar(
+///       items: const <BottomNavigationBarItem>[
+///         BottomNavigationBarItem(
+///           icon: Icon(Icons.home),
+///           title: Text('White'),
+///         ),
+///         BottomNavigationBarItem(
+///           icon: Icon(Icons.business),
+///           title: Text('Red'),
+///         ),
+///         BottomNavigationBarItem(
+///           icon: Icon(Icons.school),
+///           title: Text('Yellow'),
+///         ),
+///       ],
+///       currentIndex: _selectedIndex,
+///       onTap: (int index) {
+///         setState(() {
+///           _selectedIndex = index;
+///         });
+///       },
+///     ),
+///   );
+/// }
+/// ```
+class SharedZAxisPageTransition extends StatefulWidget {
+  /// Creates a [SharedZAxisPageTransition].
+  ///
+  /// The [animation] and [secondaryAnimation] argument are required and must
+  /// not be null.
+  const SharedZAxisPageTransition({
     Key key,
-    this.animation,
-    this.secondaryAnimation,
+    @required this.animation,
+    @required this.secondaryAnimation,
     this.child,
   }) : super(key: key);
 
+  /// The animation that drives the [child]'s entrance and exit.
+  ///
+  /// See also:
+  ///
+  ///  * [TransitionRoute.animate], which is the value given to this property
+  ///    when it is used as a page transition.
   final Animation<double> animation;
+
+  /// The animation that transitions [child] when new content is pushed on top
+  /// of it.
+  ///
+  /// See also:
+  ///
+  ///  * [TransitionRoute.secondaryAnimation], which is the value given to this
+  ///    property when the it is used as a page transition.
   final Animation<double> secondaryAnimation;
+
+  /// The widget below this widget in the tree.
+  ///
+  /// This widget will transition in and out as driven by [animation] and
+  /// [secondaryAnimation].
   final Widget child;
 
   @override
-  __SharedZAxisPageTransitionState createState() => __SharedZAxisPageTransitionState();
+  _SharedZAxisPageTransitionState createState() => _SharedZAxisPageTransitionState();
 }
 
-class __SharedZAxisPageTransitionState extends State<_SharedZAxisPageTransition> {
-
+class _SharedZAxisPageTransitionState extends State<SharedZAxisPageTransition> {
   @override
   Widget build(BuildContext context) {
     // Scale Transitions
