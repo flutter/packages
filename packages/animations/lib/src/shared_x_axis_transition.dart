e26da20de7f7ff8c475f0ab9ebb0204fad2b8dfc
Shi Hao Hong <shihaohong94@gmail.com>
2020-01-02 10:58:43 -0800

    Implement shared x axis transition

---
+++ packages/animations/lib/src/shared_x_axis_transition.dart
@@ -0,0 +1,399 @@
+// Copyright 2019 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/animation.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'utils/curves.dart';
+
+/// Used by [PageTransitionsTheme] to define a page route transition animation
+/// in which outgoing and incoming elements share a horizontal fade transition.
+///
+/// The shared axis pattern provides the transition animation between UI elements
+/// that have a spatial or navigational relationship. For example,
+/// transitioning from one page of a sign up page to the next one.
+///
+///
+/// The following example shows how the SharedXAxisPageTransitionsBuilder can
+/// be used in a [PageTransitionsTheme] to change the default transitions
+/// of [MaterialPageRoute]s.
+///
+/// ```dart
+/// MaterialApp(
+///   theme: ThemeData(
+///     pageTransitionsTheme: PageTransitionsTheme(
+///       builders: {
+///         TargetPlatform.android: SharedXAxisPageTransitionsBuilder(),
+///         TargetPlatform.iOS: SharedXAxisPageTransitionsBuilder(),
+///       },
+///     ),
+///   ),
+///   routes: {
+///     '/': (BuildContext context) {
+///       return Container(
+///         color: Colors.red,
+///         child: Center(
+///           child: MaterialButton(
+///             child: Text('Push route'),
+///             onPressed: () {
+///               Navigator.of(context).pushNamed('/a');
+///             },
+///           ),
+///         ),
+///       );
+///     },
+///     '/a' : (BuildContext context) {
+///       return Container(
+///         color: Colors.blue,
+///         child: Center(
+///           child: MaterialButton(
+///             child: Text('Pop route'),
+///             onPressed: () {
+///               Navigator.of(context).pop();
+///             },
+///           ),
+///         ),
+///       );
+///     },
+///   },
+/// );
+/// ```
+class SharedXAxisPageTransitionsBuilder extends PageTransitionsBuilder {
+  /// Construct a [SharedXAxisPageTransitionsBuilder].
+  const SharedXAxisPageTransitionsBuilder();
+
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T> route,
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return SharedXAxisTransition(
+      animation: animation,
+      secondaryAnimation: secondaryAnimation,
+      child: child,
+    );
+  }
+}
+
+/// Defines a transition in which outgoing and incoming elements share a horizontal
+/// transition.
+///
+/// The shared axis pattern provides the transition animation between UI elements
+/// that have a spatial or navigational relationship. For example,
+/// transitioning from one page of a sign up page to the next one.
+///
+/// Consider using [SharedXAxisTransition] within a
+/// [PageTransitionsTheme] if you want to apply this kind of transition to
+/// [MaterialPageRoute] transitions within a Navigator (see
+/// [SharedXAxisPageTransitionsBuilder] for example code).
+///
+/// This transition can also be used directly in a
+/// [PageTransitionSwitcher.transitionBuilder] to transition
+/// from one widget to another as seen in the following example:
+/// ```dart
+/// int _selectedIndex = 0;
+///
+/// final List<Color> _colors = [Colors.white, Colors.red, Colors.yellow];
+///
+/// @override
+/// Widget build(BuildContext context) {
+///   return Scaffold(
+///     appBar: AppBar(
+///       title: const Text('Page Transition Example'),
+///     ),
+///     body: PageTransitionSwitcher(
+///       // reverse: true, // uncomment to see transition in reverse
+///       transitionBuilder: (
+///         Widget child,
+///         Animation<double> primaryAnimation,
+///         Animation<double> secondaryAnimation,
+///       ) {
+///         return SharedXAxisTransition(
+///           animation: primaryAnimation,
+///           secondaryAnimation: secondaryAnimation,
+///           child: child,
+///         );
+///       },
+///       child: Container(
+///         key: ValueKey<int>(_selectedIndex),
+///         color: _colors[_selectedIndex],
+///         child: Center(
+///           child: FlutterLogo(size: 300),
+///         )
+///       ),
+///     ),
+///     bottomNavigationBar: BottomNavigationBar(
+///       items: const <BottomNavigationBarItem>[
+///         BottomNavigationBarItem(
+///           icon: Icon(Icons.home),
+///           title: Text('White'),
+///         ),
+///         BottomNavigationBarItem(
+///           icon: Icon(Icons.business),
+///           title: Text('Red'),
+///         ),
+///         BottomNavigationBarItem(
+///           icon: Icon(Icons.school),
+///           title: Text('Yellow'),
+///         ),
+///       ],
+///       currentIndex: _selectedIndex,
+///       onTap: (int index) {
+///         setState(() {
+///           _selectedIndex = index;
+///         });
+///       },
+///     ),
+///   );
+/// }
+/// ```
+class SharedXAxisTransition extends StatefulWidget {
+  /// Creates a [SharedXAxisTransition].
+  ///
+  /// The [animation] and [secondaryAnimation] argument are required and must
+  /// not be null.
+  const SharedXAxisTransition({
+    Key key,
+    @required this.animation,
+    @required this.secondaryAnimation,
+    this.child,
+  }) : super(key: key);
+
+  /// The animation that drives the [child]'s entrance and exit.
+  ///
+  /// See also:
+  ///
+  ///  * [TransitionRoute.animate], which is the value given to this property
+  ///    when it is used as a page transition.
+  final Animation<double> animation;
+
+  /// The animation that transitions [child] when new content is pushed on top
+  /// of it.
+  ///
+  /// See also:
+  ///
+  ///  * [TransitionRoute.secondaryAnimation], which is the value given to this
+  ///    property when the it is used as a page transition.
+  final Animation<double> secondaryAnimation;
+
+  /// The widget below this widget in the tree.
+  ///
+  /// This widget will transition in and out as driven by [animation] and
+  /// [secondaryAnimation].
+  final Widget child;
+
+  @override
+  _SharedXAxisTransitionState createState() => _SharedXAxisTransitionState();
+}
+
+class _SharedXAxisTransitionState extends State<SharedXAxisTransition> {
+  AnimationStatus _effectiveAnimationStatus;
+  AnimationStatus _effectiveSecondaryAnimationStatus;
+
+  @override
+  void initState() {
+    super.initState();
+    _effectiveAnimationStatus = widget.animation.status;
+    _effectiveSecondaryAnimationStatus = widget.secondaryAnimation.status;
+    widget.animation.addStatusListener(_animationListener);
+    widget.secondaryAnimation.addStatusListener(_secondaryAnimationListener);
+  }
+
+  void _animationListener(AnimationStatus animationStatus) {
+    _effectiveAnimationStatus = _calculateEffectiveAnimationStatus(
+      lastEffective: _effectiveAnimationStatus,
+      current: animationStatus,
+    );
+  }
+
+  void _secondaryAnimationListener(AnimationStatus animationStatus) {
+    _effectiveSecondaryAnimationStatus = _calculateEffectiveAnimationStatus(
+      lastEffective: _effectiveSecondaryAnimationStatus,
+      current: animationStatus,
+    );
+  }
+
+  // When a transition is interrupted midway we just want to play the ongoing
+  // animation in reverse. Switching to the actual reverse transition would
+  // yield a disjoint experience since the forward and reverse transitions are
+  // very different.
+  AnimationStatus _calculateEffectiveAnimationStatus({
+    @required AnimationStatus lastEffective,
+    @required AnimationStatus current,
+  }) {
+    assert(current != null);
+    assert(lastEffective != null);
+    switch (current) {
+      case AnimationStatus.dismissed:
+      case AnimationStatus.completed:
+        return current;
+      case AnimationStatus.forward:
+        switch (lastEffective) {
+          case AnimationStatus.dismissed:
+          case AnimationStatus.completed:
+          case AnimationStatus.forward:
+            return current;
+          case AnimationStatus.reverse:
+            return lastEffective;
+        }
+        break;
+      case AnimationStatus.reverse:
+        switch (lastEffective) {
+          case AnimationStatus.dismissed:
+          case AnimationStatus.completed:
+          case AnimationStatus.reverse:
+            return current;
+          case AnimationStatus.forward:
+            return lastEffective;
+        }
+        break;
+    }
+    return null; // unreachable
+  }
+
+  @override
+  void didUpdateWidget(SharedXAxisTransition oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.animation != widget.animation) {
+      oldWidget.animation.removeStatusListener(_animationListener);
+      widget.animation.addStatusListener(_animationListener);
+      _animationListener(widget.animation.status);
+    }
+    if (oldWidget.secondaryAnimation != widget.secondaryAnimation) {
+      oldWidget.secondaryAnimation
+          .removeStatusListener(_secondaryAnimationListener);
+      widget.secondaryAnimation.addStatusListener(_secondaryAnimationListener);
+      _secondaryAnimationListener(widget.secondaryAnimation.status);
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.animation.removeStatusListener(_animationListener);
+    widget.secondaryAnimation.removeStatusListener(_secondaryAnimationListener);
+    super.dispose();
+  }
+
+  static final Tween<double> _flippedTween = Tween<double>(
+    begin: 1.0,
+    end: 0.0,
+  );
+  static Animation<double> _flip(Animation<double> animation) {
+    return _flippedTween.animate(animation);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: widget.animation,
+      builder: (BuildContext context, Widget child) {
+        assert(_effectiveAnimationStatus != null);
+        switch (_effectiveAnimationStatus) {
+          case AnimationStatus.forward:
+            return  _EnterTransition(
+              animation: widget.animation,
+              child: child,
+            );
+          case AnimationStatus.dismissed:
+          case AnimationStatus.reverse:
+          case AnimationStatus.completed:
+            return _ExitTransition(
+              animation: _flip(widget.animation),
+              child: child,
+            );
+        }
+        return null; // unreachable
+      },
+      child: AnimatedBuilder(
+        animation: widget.secondaryAnimation,
+        builder: (BuildContext context, Widget child) {
+          assert(_effectiveSecondaryAnimationStatus != null);
+          switch (_effectiveSecondaryAnimationStatus) {
+            case AnimationStatus.forward:
+              return _ExitTransition(
+                animation: widget.secondaryAnimation,
+                child: child,
+              );
+            case AnimationStatus.dismissed:
+            case AnimationStatus.reverse:
+            case AnimationStatus.completed:
+              return _EnterTransition(
+                animation: _flip(widget.secondaryAnimation),
+                child: child,
+              );
+          }
+          return null; // unreachable
+        },
+        child: widget.child,
+      ),
+    );
+  }
+}
+
+class _EnterTransition extends StatelessWidget {
+  const _EnterTransition({
+    this.animation,
+    this.child,
+  });
+
+  final Animation<double> animation;
+  final Widget child;
+
+  static Animatable<double> fadeInTransition = CurveTween(curve: decelerateEasing)
+    .chain(CurveTween(curve: const Interval(0.3, 1.0)));
+
+  static Animatable<Offset> slideInTransition = Tween<Offset>(
+    begin: const Offset(30, 0.0),
+    end: Offset.zero,
+  ).chain(CurveTween(curve: standardEasing));
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: fadeInTransition.animate(animation),
+      child: Transform.translate(
+        offset: slideInTransition.evaluate(animation),
+        child: child,
+      ),
+    );
+  }
+}
+
+class _ExitTransition extends StatelessWidget {
+  const _ExitTransition({
+    this.animation,
+    this.child,
+  });
+
+  final Animation<double> animation;
+  final Widget child;
+
+  static Animatable<double> fadeOutTransition = FlippedCurveTween(curve: accelerateEasing)
+    .chain(CurveTween(curve: const Interval(0.0, 0.3)));
+
+  static Animatable<Offset> slideOutTransition = Tween<Offset>(
+    begin: Offset.zero,
+    end: const Offset(30, 0.0),
+  ).chain(CurveTween(curve: standardEasing));
+
+  @override
+  Widget build(BuildContext context) {
+    print(slideOutTransition.animate(animation).value);
+    return FadeTransition(
+      opacity: fadeOutTransition.animate(animation),
+      child: Container(
+        color: Theme.of(context).canvasColor,
+        child: Transform.translate(
+          offset: slideOutTransition.evaluate(animation),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
