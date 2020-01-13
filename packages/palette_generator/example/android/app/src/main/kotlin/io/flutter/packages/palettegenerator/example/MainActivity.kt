023a64c1a53433eae0af16e223f639d1c981b1ea
Greg Spencer <gspencergoog@users.noreply.github.com>
2020-01-13 14:10:04 -0800

    Update build files for example, add macos as target (#59)
    Updates the palette_generator's example to AndroidX, and adds a macos target.

---
+++ packages/palette_generator/example/android/app/src/main/kotlin/io/flutter/packages/palettegenerator/example/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.flutter.packages.palettegenerator.imagecolors
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
