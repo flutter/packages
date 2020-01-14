33a2f35242e38f585fb8a5ad56d3fe2454cab270
Shi Hao Hong <shihaohong@google.com>
2020-01-13 17:33:58 -0800

    Add fade portion of forwards transition test

--- packages/animations/test/fade_transition_test.dart
+++ packages/animations/test/fade_transition_test.dart
@@ -41,6 +41,49 @@ void main() {
   );
 
   // runs forward
+  testWidgets(
+    'FadeTransitionConfiguration runs forward',
+    (WidgetTester tester) async {
+      final GlobalKey key = GlobalKey();
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Builder(builder: (BuildContext context) {
+              return Center(
+                child: RaisedButton(
+                  onPressed: () {
+                    showModal(
+                      context: context,
+                      configuration: FadeTransitionConfiguration(),
+                      builder: (BuildContext context) {
+                        return _FlutterLogoModal(key: key);
+                      },
+                    );
+                  },
+                  child: Icon(Icons.add),
+                ),
+              );
+            }),
+          ),
+        ),
+      );
+      await tester.tap(find.byType(RaisedButton));
+      await tester.pump();
+      // Opacity duration: First 30ms linear transition at the
+      // start
+      double topFadeTransitionOpacity = _getOpacity(key, tester);
+      expect(topFadeTransitionOpacity, 0.0);
+
+      await tester.pump(const Duration(milliseconds: 23));
+      topFadeTransitionOpacity = _getOpacity(key, tester);
+      expect(topFadeTransitionOpacity, closeTo(0.5, 0.05));
+
+      await tester.pump(const Duration(milliseconds: 45));
+      expect(find.byType(_FlutterLogoModal), findsOneWidget);
+      topFadeTransitionOpacity = _getOpacity(key, tester);
+      expect(topFadeTransitionOpacity, 1.0);
+    },
+  );
 
   // runs backwards
 
@@ -49,8 +92,21 @@ void main() {
   // state is not lost when transitioning
 }
 
+double _getOpacity(GlobalKey key, WidgetTester tester) {
+  final Finder finder = find.ancestor(
+    of: find.byKey(key),
+    matching: find.byType(FadeTransition),
+  );
+  return tester.widgetList(finder).fold<double>(1.0, (double a, Widget widget) {
+    final FadeTransition transition = widget as FadeTransition;
+    return a * transition.opacity.value;
+  });
+}
+
 class _FlutterLogoModal extends StatelessWidget {
-  const _FlutterLogoModal();
+  const _FlutterLogoModal({
+    Key key,
+  })  : super(key: key);
 
   @override
   Widget build(BuildContext context) {
