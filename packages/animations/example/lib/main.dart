901e554e995ebfc479f47c9e81559cc84fd7442a
Shi Hao Hong <shihaohong94@gmail.com>
2020-01-16 18:25:32 -0800

    Fix import problem

--- packages/animations/example/lib/main.dart
+++ packages/animations/example/lib/main.dart
@@ -1,10 +1,9 @@
 // Copyright 2019 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-import 'package:animations_example/container_transition.dart';
 import 'package:flutter/material.dart';
 
+import 'container_transition.dart';
 import 'fade_through_transition.dart';
 import 'shared_axis_transition.dart';
 
