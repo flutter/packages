c21cd8a6e8695caa6289949449865c24a2e5d07c
Michael Goderbauer <goderbauer@google.com>
2020-02-03 13:40:39 -0800

    Add scripts for flutter/tests (#80)

---
+++ customer_testing.bat
@@ -0,0 +1,12 @@
+REM This file is used by
+REM https://github.com/flutter/tests/tree/master/registry/flutter_packages.test
+REM to run the tests of certain packages in this repository as a presubmit
+REM for the flutter/flutter repository.
+REM Changes to this file (and any tests in this repository) are only honored
+REM after the commit hash in the "flutter_packages.test" mentioned above has
+REM been updated.
+REM Remember to also update the Posix version (customer_testing.sh) when
+REM changing this file.
+
+cd packages/animations
+flutter test
---
+++ customer_testing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This file is used by
+# https://github.com/flutter/tests/tree/master/registry/flutter_packages.test
+# to run the tests of certain packages in this repository as a presubmit
+# for the flutter/flutter repository.
+# Changes to this file (and any tests in this repository) are only honored
+# after the commit hash in the "flutter_packages.test" mentioned above has been
+# updated.
+# Remember to also update the Windows version (customer_testing.bat) when
+# changing this file.
+
+set -e
+
+cd packages/animations
+flutter test
