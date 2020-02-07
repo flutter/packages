8b041f01c9fed1279a27955b48555803e1118c74
Aaron Clarke <aaclarke@google.com>
2020-02-07 14:54:54 -0800

    removed stray import

--- packages/pigeon/test/dart_generator_test.dart
+++ packages/pigeon/test/dart_generator_test.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:mirrors';
-
 import 'package:test/test.dart';
 import 'package:pigeon/dart_generator.dart';
 import 'package:pigeon/ast.dart';
