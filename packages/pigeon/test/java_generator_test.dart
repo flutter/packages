19346b10578d88af47ad3e2065300ace4632502b
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2020-02-20 16:31:59 -0800

    [pigeon] added support for builtin java datatypes (#100)

--- packages/pigeon/test/java_generator_test.dart
+++ packages/pigeon/test/java_generator_test.dart
@@ -25,7 +25,7 @@ void main() {
     final String code = sink.toString();
     expect(code, contains('public class Messages'));
     expect(code, contains('public static class Foobar'));
-    expect(code, contains('private int field1;'));
+    expect(code, contains('private Long field1;'));
   });
 
   test('package', () {
@@ -69,5 +69,34 @@ void main() {
     final String code = sink.toString();
     expect(code, contains('public interface Api'));
     expect(code, matches('Output.*doSomething.*Input'));
+  });
+
+  test('all the simple datatypes header', () {
+    final Root root = Root(apis: <Api>[], classes: <Class>[
+      Class(name: 'Foobar', fields: <Field>[
+        Field(name: 'aBool', dataType: 'bool'),
+        Field(name: 'aInt', dataType: 'int'),
+        Field(name: 'aDouble', dataType: 'double'),
+        Field(name: 'aString', dataType: 'String'),
+        Field(name: 'aUint8List', dataType: 'Uint8List'),
+        Field(name: 'aInt32List', dataType: 'Int32List'),
+        Field(name: 'aInt64List', dataType: 'Int64List'),
+        Field(name: 'aFloat64List', dataType: 'Float64List'),
+      ]),
+    ]);
+
+    final StringBuffer sink = StringBuffer();
+    final JavaOptions javaOptions = JavaOptions();
+    javaOptions.className = 'Messages';
+    generateJava(javaOptions, root, sink);
+    final String code = sink.toString();
+    expect(code, contains('private Boolean aBool;'));
+    expect(code, contains('private Long aInt;'));
+    expect(code, contains('private Double aDouble;'));
+    expect(code, contains('private String aString;'));
+    expect(code, contains('private byte[] aUint8List;'));
+    expect(code, contains('private int[] aInt32List;'));
+    expect(code, contains('private long[] aInt64List;'));
+    expect(code, contains('private double[] aFloat64List;'));
   });
 }
