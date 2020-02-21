df038f2e34b1c1b2815a185de21ae660b81edc09
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2020-02-21 13:54:27 -0800

    added ios unit tests and fixed bug with null (#111)

---
+++ packages/pigeon/platform_tests/ios_unit_tests/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+/// An empty app.
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- packages/pigeon/run_tests.sh
+++ packages/pigeon/run_tests.sh
@@ -53,6 +53,23 @@ test_pigeon_android ./pigeons/message.dart
 test_pigeon_ios ./pigeons/message.dart
 test_pigeon_ios ./pigeons/host2flutter.dart
 
+pub run pigeon \
+  --input pigeons/message.dart \
+  --dart_out /dev/null \
+  --objc_header_out platform_tests/ios_unit_tests/ios/Runner/messages.h \
+  --objc_source_out platform_tests/ios_unit_tests/ios/Runner/messages.m
+clang-format -i platform_tests/ios_unit_tests/ios/Runner/messages.h
+clang-format -i platform_tests/ios_unit_tests/ios/Runner/messages.m
+pushd $PWD
+cd platform_tests/ios_unit_tests/ios/
+ xcodebuild \
+    -workspace Runner.xcworkspace \
+    -scheme RunnerTests \
+    -sdk iphonesimulator \
+    -destination 'platform=iOS Simulator,name=iPhone 8' \
+    test | xcpretty
+popd
+
 # e2e tests are not checked in until some issues can be worked out with e2e.
 if [ -e "e2e_tests" ]; then
   DARTLE_H="e2e_tests/test_objc/ios/Runner/dartle.h"
