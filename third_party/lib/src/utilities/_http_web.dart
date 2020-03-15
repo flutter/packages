06f089ddb11586fdfc207d277dd68deea46eb0da
Dan Field <dnfield@google.com>
2020-03-15 07:31:51 -0700

    ..

--- third_party/lib/src/utilities/_http_web.dart
+++ third_party/lib/src/utilities/_http_web.dart
@@ -5,10 +5,9 @@ import 'dart:typed_data';
 
 /// Fetches an HTTP resource from the specified [url] using the specified [headers].
 Future<Uint8List> httpGet(String url, {Map<String, String> headers}) async {
-  // final HttpRequest request = await HttpRequest.request(
-  final String request = await HttpRequest.requestCrossOrigin(
+  final HttpRequest request = await HttpRequest.request(
     url,
-    // requestHeaders: headers,
+    requestHeaders: headers,
   );
-  return Uint8List.fromList(utf8.encode(request));
+  return Uint8List.fromList(utf8.encode(request.responseText));
 }
