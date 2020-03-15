60086359e4a8161c1b9933774e37893745fc4dce
Dan Field <dnfield@google.com>
2020-03-15 07:30:17 -0700

    http changes

---
+++ third_party/lib/src/utilities/_http_web.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:html';
+import 'dart:typed_data';
+
+/// Fetches an HTTP resource from the specified [url] using the specified [headers].
+Future<Uint8List> httpGet(String url, {Map<String, String> headers}) async {
+  // final HttpRequest request = await HttpRequest.request(
+  final String request = await HttpRequest.requestCrossOrigin(
+    url,
+    // requestHeaders: headers,
+  );
+  return Uint8List.fromList(utf8.encode(request));
+}
