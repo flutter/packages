a6eb711a9a996e5905dfe4aa37651888916f91ae
Stuart Morgan <stuartmorgan@google.com>
2020-03-16 16:43:41 -0700

    Create flutter_template_images package
    Contains images used in 'flutter create' templates, to avoid needing to
    check them into flutter/flutter where they will permanently inflate the
    repo size.
    
    Currently only contains the Windows app icon, since existing template
    icons are already part of the Flutter repository. Future template
    images, including changes to the existing images, should live here
    instead.

---
+++ packages/flutter_template_images/CHANGELOG.md
@@ -0,0 +1,3 @@
+## [0.0.1] - 2020-03-16
+
+* Windows app template icon.
---
+++ packages/flutter_template_images/lib/flutter_template_images.dart
@@ -0,0 +1,3 @@
+library flutter_template_images;
+
+// There is no source for this package, since it only exists to hold images.
