0ad54d2d6fa3731fe558ac9104af378a0d3c6ac3
Aaron Clarke <aaclarke@google.com>
2020-04-22 13:40:33 -0700

    formatter again

--- packages/pigeon/e2e_tests/test_objc/ios/Runner/MyApi.h
+++ packages/pigeon/e2e_tests/test_objc/ios/Runner/MyApi.h
@@ -4,7 +4,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /// Implementation of the Pigeon generated interface Api.
-@interface MyApi : NSObject <ACApi>
+@interface MyApi : NSObject<ACApi>
 @end
 
 NS_ASSUME_NONNULL_END
