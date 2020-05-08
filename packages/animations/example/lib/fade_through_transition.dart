d6e2114a93593f5b3bf74506929155086c1464f8
Shi Hao Hong <shihaohong94@gmail.com>
2020-05-08 10:05:54 -0700

    Remove all TODOs upon new stable release

--- packages/animations/example/lib/fade_through_transition.dart
+++ packages/animations/example/lib/fade_through_transition.dart
@@ -93,10 +93,7 @@ class _ExampleCard extends StatelessWidget {
                     children: <Widget>[
                       Text(
                         '123 photos',
-                        // TODO(shihaohong): Remove this once Flutter stable adopts the modern
-                        // Material text style nomenclature.
-                        // ignore: deprecated_member_use
-                        style: Theme.of(context).textTheme.body2,
+                        style: Theme.of(context).textTheme.bodyText1,
                       ),
                       Text(
                         '123 photos',
