329008610834a275a332db9473ad0c2e10d732df
Michael Goderbauer <goderbauer@google.com>
2020-05-20 10:33:58 -0700

    review comments

--- packages/animations/lib/src/dual_transition_builder.dart
+++ packages/animations/lib/src/dual_transition_builder.dart
@@ -18,15 +18,19 @@ typedef TransitionBuilder = Widget Function(
   Widget child,
 );
 
-/// A animated builder that animates its [child] with different transitions
-/// based on the [AnimationStatus] of the provided [animation].
+/// A transition builder that animates its [child] based on the
+/// [AnimationStatus] of the provided [animation].
+///
+/// This widget can be used to specify different enter and exit transitions for
+/// a [child].
 ///
 /// While the [animation] runs forward, the [child] is animated according to
 /// [forwardBuilder] and while the [animation] is running in reverse, it is
 /// animated according to [reverseBuilder].
 ///
-/// This widget can be used to specify different enter and exit transitions for
-/// a [child].
+/// Using this builder allows the widget tree to maintain its shape by nesting
+/// the enter and exit transitions. This ensures that no state information of
+/// any descendant widget is lost when the transition starts or completes.
 class DualTransitionBuilder extends StatefulWidget {
   /// Creates a [DualTransitionBuilder].
   ///
@@ -50,15 +54,14 @@ class DualTransitionBuilder extends StatefulWidget {
   /// to [reverseBuilder].
   final Animation<double> animation;
 
-  /// A builder for a transition that wraps [child].
+  /// A builder for the transition that makes [child] appear on screen.
+  ///
+  /// The [child] should be fully visible when the provided `animation` reaches
+  /// 1.0.
   ///
   /// The `animation` provided to this builder is running forward from 0.0 to
   /// 1.0 when [animation] runs _forward_. When [animation] runs in reverse,
   /// the given animation is set to [kAlwaysCompleteAnimation].
-  ///
-  /// This builder should be used to create the animation that makes the [child]
-  /// appear on screen. The [child] should be fully visible when the `animation`
-  /// reaches 1.0.
   ///
   /// The builder will be called whenever the [Animation.value] of the given
   /// `animation` changes.
@@ -69,15 +72,14 @@ class DualTransitionBuilder extends StatefulWidget {
   ///   disappear from the screen.
   final TransitionBuilder forwardBuilder;
 
-  /// A builder for a transition that wraps [child].
+  /// A builder for a transition that makes [child] disappear from the screen..
+  ///
+  /// The [child] should be fully invisible when the provided `animation`
+  /// reaches 1.0.
   ///
   /// The `animation` provided to this builder is running forward from 0.0 to
   /// 1.0 when [animation] runs in _reverse_. When [animation] runs forward,
   /// the given animation is set to [kAlwaysDismissedAnimation].
-  ///
-  /// This builder should be used to create the animation that makes the [child]
-  /// disappear from the screen. The [child] should be fully invisible when the
-  /// `animation` reaches 1.0.
   ///
   /// The builder will be called whenever the [Animation.value] of the given
   /// `animation` changes.
@@ -88,9 +90,9 @@ class DualTransitionBuilder extends StatefulWidget {
   ///    appear on screen.
   final TransitionBuilder reverseBuilder;
 
-  /// The widget below this widget in the tree.
+  /// The widget below this [DualTransitionBuilder] in the tree.
   ///
-  /// This widget will be wrapped by the transitions built by
+  /// This child widget will be wrapped by the transitions built by
   /// [forwardBuilder] and [reverseBuilder].
   final Widget child;
 
