fa82a6a39214632140879650fa6674df9f3ab90d
Michael Goderbauer <goderbauer@google.com>
2020-07-27 21:58:44 -0700

    ++

--- packages/animations/lib/src/shared_axis_transition.dart
+++ packages/animations/lib/src/shared_axis_transition.dart
@@ -6,8 +6,8 @@ import 'package:flutter/animation.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart'
     hide
-        decelerateEasing,
-        standardEasing,
+        decelerateEasing, // ignore: undefined_hidden_name
+        standardEasing, // ignore: undefined_hidden_name
         accelerateEasing; // ignore: undefined_hidden_name
 // TODO(goderbauer): Remove implementation import when material properly exports the file.
 import 'package:/flutter/src/material/curves.dart'; // ignore: implementation_imports
