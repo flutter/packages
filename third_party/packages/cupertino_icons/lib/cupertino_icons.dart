8e57c42f320e54b84fc239fb70b78c0a0ac62fb5
Xiao Yu <xiao@xster.net>
2020-09-02 12:48:49 -0700

    rename placeholder dart file to conform to pana lint

---
+++ third_party/packages/cupertino_icons/lib/cupertino_icons.dart
@@ -0,0 +1,4 @@
+// File to create a lib/ directory for a well-formed .packages file and to
+// conform to pub analyzer.
+//
+// This is an asset package. No dart import needed.
--- third_party/packages/cupertino_icons/lib/placeholder.dart
+++
@@ -1,2 +0,0 @@
-// File to create a lib/ directory for a well-formed .packages file and to
-// conform to pub analyzer.
