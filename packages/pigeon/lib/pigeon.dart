a2a56eaa6f8b93a3e8fed994328c2902b723a93c
Aaron Clarke <aaclarke@google.com>
2020-09-03 14:18:26 -0700

    [pigeon] made the pigeon files support array datatypes more completely

--- packages/pigeon/lib/pigeon.dart
+++ packages/pigeon/lib/pigeon.dart
@@ -1,1 +1,2 @@
+export 'dart:typed_data' show Uint8List, Int32List, Int64List, Float64List;
 export 'pigeon_lib.dart';
---
+++ packages/pigeon/pigeons/all_datatypes.dart
@@ -0,0 +1,28 @@
+// Copyright 2020 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:pigeon/pigeon.dart';
+
+class Everything {
+  bool aBool;
+  int anInt;
+  double aDouble;
+  String aString;
+  Uint8List aByteArray;
+  Int32List a4ByteArray;
+  Int64List a8ByteArray;
+  Float64List aFloatArray;
+  List aList;
+  Map aMap;
+}
+
+@HostApi()
+abstract class HostEverything {
+  Everything giveMeEverything();
+}
+
+@FlutterApi()
+abstract class FlutterEverything {
+  Everything giveMeEverything();
+}
