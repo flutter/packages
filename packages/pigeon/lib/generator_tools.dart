b56a9005b6451a2a745f20f95943ff881b5628b2
creativecreatorormaybenot <creativecreatorormaybenot@gmail.com>
2020-10-06 16:29:07 +0000

    [pigeon] Fix on Windows (#216)

--- packages/pigeon/lib/generator_tools.dart
+++ packages/pigeon/lib/generator_tools.dart
@@ -8,7 +8,7 @@ import 'dart:mirrors';
 import 'ast.dart';
 
 /// The current version of pigeon.
-const String pigeonVersion = '0.1.9';
+const String pigeonVersion = '0.1.10';
 
 /// Read all the content from [stdin] to a String.
 String readStdin() {
