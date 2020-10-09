f07e43136c9d8d0b9e83dc3d1712ebeaf575b54d
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2020-10-09 13:02:43 -0700

    [pigeon] updated example page (#221)
    * pigeon - updated the examples tab
    
    * fixed version number

--- packages/pigeon/lib/generator_tools.dart
+++ packages/pigeon/lib/generator_tools.dart
@@ -8,7 +8,7 @@ import 'dart:mirrors';
 import 'ast.dart';
 
 /// The current version of pigeon.
-const String pigeonVersion = '0.1.10';
+const String pigeonVersion = '0.1.10+1';
 
 /// Read all the content from [stdin] to a String.
 String readStdin() {
