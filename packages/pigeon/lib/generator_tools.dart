4797a0de84cc608091a21877c5f44abec0297d13
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2020-11-30 10:21:27 -0800

    [pigeon] Fixed running with Dart versions with NNBD enabled (moved import). (#251)

--- packages/pigeon/lib/generator_tools.dart
+++ packages/pigeon/lib/generator_tools.dart
@@ -8,7 +8,7 @@ import 'dart:mirrors';
 import 'ast.dart';
 
 /// The current version of pigeon.
-const String pigeonVersion = '0.1.15';
+const String pigeonVersion = '0.1.16';
 
 /// Read all the content from [stdin] to a String.
 String readStdin() {
