d787605baa60127833f7c6ff240209864e0cc9d6
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2020-12-10 15:14:11 -0800

    updated version to 0.1.17 (#255)

--- packages/pigeon/lib/generator_tools.dart
+++ packages/pigeon/lib/generator_tools.dart
@@ -8,7 +8,7 @@ import 'dart:mirrors';
 import 'ast.dart';
 
 /// The current version of pigeon.
-const String pigeonVersion = '0.1.16';
+const String pigeonVersion = '0.1.17';
 
 /// Read all the content from [stdin] to a String.
 String readStdin() {
