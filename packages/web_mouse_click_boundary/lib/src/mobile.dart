33024f8703e8eb13223d125eb607f8d1ab2ea40f
David Iglesias Teixeira <ditman@gmail.com>
2020-12-10 17:10:27 -0800

    Remove 'clickable' attribute.

--- packages/web_mouse_click_boundary/lib/src/mobile.dart
+++ packages/web_mouse_click_boundary/lib/src/mobile.dart
@@ -8,15 +8,11 @@ import 'package:flutter/widgets.dart';
 /// A Widget that prevents clicks from being swallowed by HtmlViewElements.
 class MouseClickBoundary extends StatelessWidget {
   /// Create a `MouseClickBoundary` around a `child`.
-  const MouseClickBoundary({@required this.child, this.clickable = false, this.debug = false, Key key}) : super(key: key);
+  const MouseClickBoundary({@required this.child, this.debug = false, Key key}) : super(key: key);
 
   /// The Widget that is being wrapped by this MouseClickBoundary.
   /// It needs to be properly sized (like a Button).
   final Widget child;
-
-  /// The clickability status of the wrapped child. 
-  /// This is needed in Web to render the correct mouse cursor on wrapped children.
-  final bool clickable;
 
   /// Render the view with a semi-transparent red background, for debug purposes.
   /// This is useful when rendering this as a "layout" widget, like the root element
--- packages/web_mouse_click_boundary/lib/src/web.dart
+++ packages/web_mouse_click_boundary/lib/src/web.dart
@@ -9,25 +9,19 @@ import 'package:flutter/widgets.dart';
 
 import 'shim/dart_ui.dart' as ui;
 
-const String _static = '__somethingRandom__';
-const String _clickable = '__somethingClickable__';
+const String _viewType = '__mouseClickBoundaryViewType__';
 const String _debug = 'debug__';
 
-String _getViewType({bool clickable = false, bool debug = false}) {
-  String viewType = clickable ? _clickable : _static;
-  viewType += debug ? _debug : '';
-  return viewType;
+String _getViewType({bool debug = false}) {
+  return debug ? _viewType + _debug : _viewType;
 }
 
-void _registerWrapper({bool clickable = false, bool debug = false}) {
-  final String viewType = _getViewType(clickable: clickable, debug: debug);
+void _registerWrapper({bool debug = false}) {
+  final String viewType = _getViewType(debug: debug);
 
   ui.platformViewRegistry.registerViewFactory(viewType,
       (int viewId) {
     final html.Element wrapper = html.DivElement();
-    if (clickable) {
-      wrapper.style.cursor = 'pointer';
-    }
     if (debug) {
       wrapper.style.backgroundColor = 'rgba(255, 0, 0, .5)';
     }
@@ -40,7 +34,7 @@ void _registerWrapper({bool clickable = false, bool debug = false}) {
 class MouseClickBoundary extends StatelessWidget {
   /// Creates a MouseClickBoundary for the web.
   /// If the underlying viewFactories are not registered yet, it registers them.
-  MouseClickBoundary({@required this.child, this.clickable = false, this.debug = false, Key key}) : super(key: key) {
+  MouseClickBoundary({@required this.child, this.debug = false, Key key}) : super(key: key) {
     if (!_registered) {
       _register();
     }
@@ -49,10 +43,6 @@ class MouseClickBoundary extends StatelessWidget {
   /// The Widget that is being wrapped by this MouseClickBoundary.
   /// It needs to be properly sized (like a Button).
   final Widget child;
-
-  /// The clickability status of the wrapped child. 
-  /// This is needed in Web to render the correct mouse cursor on wrapped children.
-  final bool clickable;
 
   /// Render the view with a semi-transparent red background, for debug purposes.
   /// This is useful when rendering this as a "layout" widget, like the root child
@@ -68,15 +58,13 @@ class MouseClickBoundary extends StatelessWidget {
 
     _registerWrapper();
     _registerWrapper(debug: true);
-    _registerWrapper(clickable: true);
-    _registerWrapper(clickable: true, debug: true);
 
     _registered = true;
   }
 
   @override
   Widget build(BuildContext context) {
-    final String viewType = _getViewType(clickable: clickable, debug: debug);
+    final String viewType = _getViewType(debug: debug);
     return Stack(
       children: <Widget>[
         Positioned.fill(
