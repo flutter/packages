34ad3522b1ebb823aeded8869350f12a56e32b04
Emmanuel Garcia <egarciad@google.com>
2020-12-11 15:29:50 -0800

    Version

--- packages/pigeon/lib/generator_tools.dart
+++ packages/pigeon/lib/generator_tools.dart
@@ -8,7 +8,7 @@ import 'dart:mirrors';
 import 'ast.dart';
 
 /// The current version of pigeon.
-const String pigeonVersion = '0.1.17';
+const String pigeonVersion = '0.1.18';
 
 /// Read all the content from [stdin] to a String.
 String readStdin() {
