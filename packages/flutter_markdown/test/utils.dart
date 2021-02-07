dd342f4418099bbd6fa31bcd6f2f4110a587b6f5
Michael Jordan <mjordan@quiverware.com>
2021-02-06 17:33:29 -0700

    Fix image test mock for null safety (#328)

--- packages/flutter_markdown/test/utils.dart
+++ packages/flutter_markdown/test/utils.dart
@@ -2,15 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-import 'dart:io' as io;
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 
 final TextTheme textTheme =
     Typography.material2018(platform: TargetPlatform.android)
@@ -166,119 +162,3 @@ Widget boilerplate(Widget child) {
     child: child,
   );
 }
-
-class MockHttpClient extends Mock implements io.HttpClient {
-  @override
-  Future<io.HttpClientRequest> getUrl(Uri? url) =>
-      super.noSuchMethod(Invocation.method(#getUrl, [url]));
-}
-
-class MockHttpClientRequest extends Mock implements io.HttpClientRequest {}
-
-class MockHttpClientResponse extends Mock implements io.HttpClientResponse {}
-
-class MockHttpHeaders extends Mock implements io.HttpHeaders {}
-
-class TestHttpOverrides extends io.HttpOverrides {
-  io.HttpClient createHttpClient(io.SecurityContext? context) {
-    return createMockImageHttpClient(context);
-  }
-}
-
-// Returns a mock HTTP client that responds with an image to all requests.
-MockHttpClient createMockImageHttpClient(io.SecurityContext? _) {
-  final MockHttpClient client = MockHttpClient();
-  final MockHttpClientRequest request = MockHttpClientRequest();
-  final MockHttpClientResponse response = MockHttpClientResponse();
-  final MockHttpHeaders headers = MockHttpHeaders();
-
-  when(client.getUrl(any))
-      .thenAnswer((_) => Future<io.HttpClientRequest>.value(request));
-  when(request.headers).thenReturn(headers);
-  when(request.close())
-      .thenAnswer((_) => Future<io.HttpClientResponse>.value(response));
-  when(response.contentLength).thenReturn(_transparentImage.length);
-  when(response.statusCode).thenReturn(io.HttpStatus.ok);
-  when(response.compressionState)
-      .thenReturn(io.HttpClientResponseCompressionState.notCompressed);
-  when(response.listen(any)).thenAnswer((Invocation invocation) {
-    final void Function(List<int>)? onData = invocation.positionalArguments[0];
-    final void Function()? onDone = invocation.namedArguments[#onDone];
-    final void Function(Object, [StackTrace?])? onError =
-        invocation.namedArguments[#onError];
-    final bool? cancelOnError = invocation.namedArguments[#cancelOnError];
-
-    return Stream<List<int>>.fromIterable(<List<int>>[_transparentImage])
-        .listen(onData,
-            onDone: onDone, onError: onError, cancelOnError: cancelOnError);
-  });
-
-  return client;
-}
-
-const List<int> _transparentImage = const <int>[
-  0x89,
-  0x50,
-  0x4E,
-  0x47,
-  0x0D,
-  0x0A,
-  0x1A,
-  0x0A,
-  0x00,
-  0x00,
-  0x00,
-  0x0D,
-  0x49,
-  0x48,
-  0x44,
-  0x52,
-  0x00,
-  0x00,
-  0x00,
-  0x01,
-  0x00,
-  0x00,
-  0x00,
-  0x01,
-  0x08,
-  0x06,
-  0x00,
-  0x00,
-  0x00,
-  0x1F,
-  0x15,
-  0xC4,
-  0x89,
-  0x00,
-  0x00,
-  0x00,
-  0x0A,
-  0x49,
-  0x44,
-  0x41,
-  0x54,
-  0x78,
-  0x9C,
-  0x63,
-  0x00,
-  0x01,
-  0x00,
-  0x00,
-  0x05,
-  0x00,
-  0x01,
-  0x0D,
-  0x0A,
-  0x2D,
-  0xB4,
-  0x00,
-  0x00,
-  0x00,
-  0x00,
-  0x49,
-  0x45,
-  0x4E,
-  0x44,
-  0xAE,
-];
