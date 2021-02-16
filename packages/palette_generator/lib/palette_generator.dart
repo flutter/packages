bbb054711afe3e3011d8ef4b0414bc5becdd8521
Matej Zidek <zidek.matt@gmail.com>
2021-02-16 19:26:20 +0100

    review: make histogram non-nullable

--- packages/palette_generator/lib/palette_generator.dart
+++ packages/palette_generator/lib/palette_generator.dart
@@ -1020,16 +1020,16 @@ class _ColorCount {
 }
 
 class _ColorHistogram {
-  final Map<int, Map<int, Map<int, _ColorCount?>>> _hist =
-      <int, Map<int, Map<int, _ColorCount?>>>{};
+  final Map<int, Map<int, Map<int, _ColorCount>>> _hist =
+      <int, Map<int, Map<int, _ColorCount>>>{};
   final DoubleLinkedQueue<Color> _keys = DoubleLinkedQueue<Color>();
 
   _ColorCount? operator [](Color color) {
-    final Map<int, Map<int, _ColorCount?>>? redMap = _hist[color.red];
+    final Map<int, Map<int, _ColorCount>>? redMap = _hist[color.red];
     if (redMap == null) {
       return null;
     }
-    final Map<int, _ColorCount?>? blueMap = redMap[color.blue];
+    final Map<int, _ColorCount>? blueMap = redMap[color.blue];
     if (blueMap == null) {
       return null;
     }
@@ -1043,15 +1043,15 @@ class _ColorHistogram {
 
     bool newColor = false;
 
-    Map<int, Map<int, _ColorCount?>>? redMap = _hist[red];
+    Map<int, Map<int, _ColorCount>>? redMap = _hist[red];
     if (redMap == null) {
-      _hist[red] = redMap = <int, Map<int, _ColorCount?>>{};
+      _hist[red] = redMap = <int, Map<int, _ColorCount>>{};
       newColor = true;
     }
 
-    Map<int, _ColorCount?>? blueMap = redMap[blue];
+    Map<int, _ColorCount>? blueMap = redMap[blue];
     if (blueMap == null) {
-      redMap[blue] = blueMap = <int, _ColorCount?>{};
+      redMap[blue] = blueMap = <int, _ColorCount>{};
       newColor = true;
     }
 
@@ -1068,7 +1068,10 @@ class _ColorHistogram {
   void removeWhere(bool predicate(Color key)) {
     for (Color key in _keys) {
       if (predicate(key)) {
-        _hist[key.red]?[key.blue]?[key.green] = null;
+        final _ColorCount? colorCount = _hist[key.red]?[key.blue]?[key.green];
+        if (colorCount != null) {
+          _hist.remove(colorCount);
+        }
       }
     }
     _keys.removeWhere((Color color) => predicate(color));
