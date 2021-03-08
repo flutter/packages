8de66459b7c25153dc15bed92510988daf10fb4d
David Iglesias Teixeira <ditman@gmail.com>
2021-03-08 11:11:36 -0800

    Import from googleapis_auth.dart

--- packages/extension_google_sign_in_as_googleapis_auth/test/extension_google_sign_in_as_googleapis_auth_test.dart
+++ packages/extension_google_sign_in_as_googleapis_auth/test/extension_google_sign_in_as_googleapis_auth_test.dart
@@ -5,7 +5,7 @@
 // https://developers.google.com/open-source/licenses/bsd
 
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:googleapis_auth/auth.dart' as gapis;
+import 'package:googleapis_auth/googleapis_auth.dart' as gapis;
 import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sign_in_as_googleapis_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 
