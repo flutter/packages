344f575216cacfcf09d477cf2a32fbe1fac574d3
Kevin Moore <kevmoo@users.noreply.github.com>
2021-03-15 13:57:57 -0700

    [extension_google_sign_in_as_googleapis_auth] Update example... (#311)
    * [extension_google_sign_in_as_googleapis_auth] Update example to use scope from the People API
    
    Make GoogleSignIn instance final
    To use scope from the People API
    Remove x bit from readme and pubspec.yaml
    Add link to root repo README
    
    * Update version in pubspec, and CHANGELOG.

--- packages/extension_google_sign_in_as_googleapis_auth/example/lib/main.dart
+++ packages/extension_google_sign_in_as_googleapis_auth/example/lib/main.dart
@@ -10,11 +10,8 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sign_in_as_googleapis_auth.dart';
 import 'package:googleapis/people/v1.dart';
 
-GoogleSignIn _googleSignIn = GoogleSignIn(
-  scopes: <String>[
-    'email',
-    'https://www.googleapis.com/auth/contacts.readonly',
-  ],
+final GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: <String>[PeopleServiceApi.contactsReadonlyScope],
 );
 
 void main() {
