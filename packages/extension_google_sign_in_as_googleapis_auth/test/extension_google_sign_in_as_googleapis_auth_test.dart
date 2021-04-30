d2df797902d2752c332b051539107e8e3c2233fd
Stuart Morgan <stuartmorgan@google.com>
2021-04-30 16:13:32 -0400

    Standardize copyright year as 2013

--- packages/extension_google_sign_in_as_googleapis_auth/test/extension_google_sign_in_as_googleapis_auth_test.dart
+++ packages/extension_google_sign_in_as_googleapis_auth/test/extension_google_sign_in_as_googleapis_auth_test.dart
@@ -1,4 +1,4 @@
-// Copyright 2020 The Flutter Authors
+// Copyright 2013 The Flutter Authors
 //
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file or at
--- packages/multicast_dns/example/main.dart
+++ packages/multicast_dns/example/main.dart
@@ -1,4 +1,4 @@
-// Copyright 2018 The Flutter Authors. Please see the AUTHORS file
+// Copyright 2013 The Flutter Authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
