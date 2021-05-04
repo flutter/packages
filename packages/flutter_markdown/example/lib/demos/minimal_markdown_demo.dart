b583e6aefefbf575a84ed63140a42a1dfefbf471
Stuart Morgan <stuartmorgan@google.com>
2021-05-04 10:25:11 -0400

    Add more missing files from flutter_markdown

--- packages/flutter_markdown/example/lib/demos/minimal_markdown_demo.dart
+++ packages/flutter_markdown/example/lib/demos/minimal_markdown_demo.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import '../shared/markdown_demo_widget.dart';
