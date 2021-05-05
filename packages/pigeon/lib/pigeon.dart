c5e4b74691cca32a5a133857407f9555c5e1242e
stuartmorgan <stuartmorgan@google.com>
2021-05-05 16:38:47 -0400

    Add missing license blocks to files (#340)
    Add license blocks to the tops of files that were missing them.
    
    Preparation for enabling license block validation in CI.

--- packages/pigeon/lib/pigeon.dart
+++ packages/pigeon/lib/pigeon.dart
@@ -1,2 +1,6 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 export 'dart:typed_data' show Uint8List, Int32List, Int64List, Float64List;
 export 'pigeon_lib.dart';
