4804697bf29c99a686fdb1a6704f6b4faa96daf8
stuartmorgan <stuartmorgan@google.com>
2021-05-10 13:48:16 -0400

    Update repo tooling (#344)
    Updates to better align with the current flutter/plugins tooling:
    - Pulls the latest flutter_plugin_tools
    - Enables the new version-check functionality
    - Enables the new license-check functionality
    - Switches over to the new publish check functionality
    - Moves format checking to a bot that does several of the very fast
      checks, leaving tests as their own bot
    - Switches incremental_build.sh to use the new --run-on-changed-packages
      functionality instead of the bash check_changed_packages
    - Moves check_changed_packages into local_tests.sh to minimize the
      chances of future new code trying to use it, as it is now legacy.
    - Renames incremental_build.sh to tool_runner.sh, to better match its
      current behavior/use

--- script/tool_runner.sh
+++ script/tool_runner.sh
@@ -7,8 +7,6 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 REPO_DIR="$(dirname "$SCRIPT_DIR")"
 
-source "$SCRIPT_DIR/common.sh"
-
 # Set some default actions if run without arguments.
 ACTIONS=("$@")
 if [[ "${#ACTIONS[@]}" == 0 ]]; then
@@ -20,13 +18,5 @@ if [[ "${BRANCH_NAME}" == "master" ]]; then
   echo "Running for all packages"
   (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" $BUILD_SHARDING)
 else
-  # Sets CHANGED_PACKAGES
-  check_changed_packages
-
-  if [[ "$CHANGED_PACKAGES" == "" ]]; then
-    echo "Running for all packages"
-    (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" $BUILD_SHARDING)
-  else
-    (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" --plugins="$CHANGED_PACKAGES" $BUILD_SHARDING)
-  fi
+  (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" --run-on-changed-packages $BUILD_SHARDING)
 fi
