ad64d762dfe19caaa5b84ad9ffb3342ffa525ed5
garyqian <garyq@google.com>
2021-05-13 19:09:36 -0700

    Android unittest

--- packages/pigeon/test/objc_generator_test.dart
+++ packages/pigeon/test/objc_generator_test.dart
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:test/test.dart';
 import 'package:pigeon/objc_generator.dart';
 import 'package:pigeon/ast.dart';
+import 'package:test/test.dart';
 
 void main() {
   test('gen one class header', () {
--- packages/pigeon/test/pigeon_lib_test.dart
+++ packages/pigeon/test/pigeon_lib_test.dart
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:test/test.dart';
 import 'package:pigeon/pigeon_lib.dart';
 import 'package:pigeon/ast.dart';
+import 'package:test/test.dart';
 
 class Input1 {
   String? input;
