f23e909f4eaf4c1822774c178988b0d1a4350c7d
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2021-06-14 10:28:19 -0700

    [pigeon] replaced all_datatype compilation tests with platform unit tests (#373)

---
+++ packages/pigeon/platform_tests/ios_unit_tests/ios/RunnerTests/AllDatatypesTest.m
@@ -0,0 +1,75 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <Flutter/Flutter.h>
+#import <XCTest/XCTest.h>
+#import "EchoMessenger.h"
+#import "all_datatypes.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////
+@interface AllDatatypesTest : XCTestCase
+@end
+
+///////////////////////////////////////////////////////////////////////////////////////////
+@implementation AllDatatypesTest
+
+- (void)testAllNull {
+  Everything* everything = [[Everything alloc] init];
+  EchoBinaryMessenger* binaryMessenger = [[EchoBinaryMessenger alloc] init];
+  FlutterEverything* api = [[FlutterEverything alloc] initWithBinaryMessenger:binaryMessenger];
+  XCTestExpectation* expectation = [self expectationWithDescription:@"callback"];
+  [api echo:everything
+      completion:^(Everything* _Nonnull result, NSError* _Nullable error) {
+        XCTAssertNil(result.aBool);
+        XCTAssertNil(result.anInt);
+        XCTAssertNil(result.aDouble);
+        XCTAssertNil(result.aString);
+        XCTAssertNil(result.aByteArray);
+        XCTAssertNil(result.a4ByteArray);
+        XCTAssertNil(result.a8ByteArray);
+        XCTAssertNil(result.aFloatArray);
+        XCTAssertNil(result.aList);
+        XCTAssertNil(result.aMap);
+        [expectation fulfill];
+      }];
+  [self waitForExpectations:@[ expectation ] timeout:1.0];
+}
+
+- (void)testAllEquals {
+  Everything* everything = [[Everything alloc] init];
+  everything.aBool = @NO;
+  everything.anInt = @(1);
+  everything.aDouble = @(2.0);
+  everything.aString = @"123";
+  everything.aByteArray = [FlutterStandardTypedData
+      typedDataWithBytes:[@"1234" dataUsingEncoding:NSUTF8StringEncoding]];
+  everything.a4ByteArray = [FlutterStandardTypedData
+      typedDataWithInt32:[@"1234" dataUsingEncoding:NSUTF8StringEncoding]];
+  everything.a8ByteArray = [FlutterStandardTypedData
+      typedDataWithInt64:[@"12345678" dataUsingEncoding:NSUTF8StringEncoding]];
+  everything.aFloatArray = [FlutterStandardTypedData
+      typedDataWithFloat64:[@"12345678" dataUsingEncoding:NSUTF8StringEncoding]];
+  everything.aList = @[ @(1), @(2) ];
+  everything.aMap = @{ @"hello" : @(1234) };
+  EchoBinaryMessenger* binaryMessenger = [[EchoBinaryMessenger alloc] init];
+  FlutterEverything* api = [[FlutterEverything alloc] initWithBinaryMessenger:binaryMessenger];
+  XCTestExpectation* expectation = [self expectationWithDescription:@"callback"];
+  [api echo:everything
+      completion:^(Everything* _Nonnull result, NSError* _Nullable error) {
+        XCTAssertEqual(result.aBool, everything.aBool);
+        XCTAssertEqual(result.anInt, everything.anInt);
+        XCTAssertEqual(result.aDouble, everything.aDouble);
+        XCTAssertEqualObjects(result.aString, everything.aString);
+        XCTAssertEqualObjects(result.aByteArray.data, everything.aByteArray.data);
+        XCTAssertEqualObjects(result.a4ByteArray.data, everything.a4ByteArray.data);
+        XCTAssertEqualObjects(result.a8ByteArray.data, everything.a8ByteArray.data);
+        XCTAssertEqualObjects(result.aFloatArray.data, everything.aFloatArray.data);
+        XCTAssertEqualObjects(result.aList, everything.aList);
+        XCTAssertEqualObjects(result.aMap, everything.aMap);
+        [expectation fulfill];
+      }];
+  [self waitForExpectations:@[ expectation ] timeout:1.0];
+}
+
+@end
