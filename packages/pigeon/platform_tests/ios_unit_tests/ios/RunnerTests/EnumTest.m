311d10b3ee304884f81e7444ff206939d0a64dde
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2021-06-14 13:03:58 -0700

    [Pigeon] enum ios unittest (#376)

---
+++ packages/pigeon/platform_tests/ios_unit_tests/ios/RunnerTests/EnumTest.m
@@ -0,0 +1,31 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <Flutter/Flutter.h>
+#import <XCTest/XCTest.h>
+#import "EchoMessenger.h"
+#import "enum.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////
+@interface EnumTest : XCTestCase
+@end
+
+///////////////////////////////////////////////////////////////////////////////////////////
+@implementation EnumTest
+
+- (void)testEcho {
+  ACData* data = [[ACData alloc] init];
+  data.state = ACStateError;
+  EchoBinaryMessenger* binaryMessenger = [[EchoBinaryMessenger alloc] init];
+  ACEnumApi2Flutter* api = [[ACEnumApi2Flutter alloc] initWithBinaryMessenger:binaryMessenger];
+  XCTestExpectation* expectation = [self expectationWithDescription:@"callback"];
+  [api echo:data
+      completion:^(ACData* _Nonnull result, NSError* _Nullable error) {
+        XCTAssertEqual(data.state, result.state);
+        [expectation fulfill];
+      }];
+  [self waitForExpectations:@[ expectation ] timeout:1.0];
+}
+
+@end
