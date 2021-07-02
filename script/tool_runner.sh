8d38599f912441cc42c61751a0130b25e44cabc8
stuartmorgan <stuartmorgan@google.com>
2021-07-02 10:47:27 -0700

    Replace 'pub' with 'dart pub' in CI scripts (#401)
    Bare use of 'pub' is deprecated.
    
    Also updates the pinned repo tool version, since some uses of 'pub' have
    been fixed there.

--- script/tool_runner.sh
+++ script/tool_runner.sh
@@ -16,7 +16,7 @@ fi
 BRANCH_NAME="${BRANCH_NAME:-"$(git rev-parse --abbrev-ref HEAD)"}"
 if [[ "${BRANCH_NAME}" == "master" ]]; then
   echo "Running for all packages"
-  (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" $BUILD_SHARDING)
+  (cd "$REPO_DIR" && dart pub global run flutter_plugin_tools "${ACTIONS[@]}" $BUILD_SHARDING)
 else
-  (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" --run-on-changed-packages $BUILD_SHARDING)
+  (cd "$REPO_DIR" && dart pub global run flutter_plugin_tools "${ACTIONS[@]}" --run-on-changed-packages $BUILD_SHARDING)
 fi
