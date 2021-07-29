e36e256ea093323018c26394b8e1d95955cc9794
gaaclarke <30870216+gaaclarke@users.noreply.github.com>
2021-07-29 14:43:25 -0700

    [pointer_interceptor] suppressed new linter warnings (#421)

--- packages/pointer_interceptor/lib/src/shim/dart_ui_fake.dart
+++ packages/pointer_interceptor/lib/src/shim/dart_ui_fake.dart
@@ -5,6 +5,7 @@
 // Fake interface for the logic that this package needs from (web-only) dart:ui.
 // This is conditionally exported so the analyzer sees these methods as available.
 
+// ignore: avoid_classes_with_only_static_members
 /// Shim for web_ui engine.PlatformViewRegistry
 /// https://github.com/flutter/engine/blob/master/lib/web_ui/lib/ui.dart#L62
 // ignore: camel_case_types
@@ -15,6 +16,7 @@ class platformViewRegistry {
       String viewTypeId, dynamic Function(int viewId) viewFactory) {}
 }
 
+// ignore: avoid_classes_with_only_static_members
 /// Shim for web_ui engine.AssetManager.
 /// https://github.com/flutter/engine/blob/master/lib/web_ui/lib/src/engine/assets.dart#L12
 // ignore: camel_case_types
