3be04908193e2afb4bb10e0bef0da7565d3170b0
David Iglesias Teixeira <ditman@gmail.com>
2021-07-30 16:04:49 -0700

    ignore_for_file: avoid_classes_with_only_static_members

--- packages/dart_ui_web_shim/lib/src/dart_ui_fake.dart
+++ packages/dart_ui_web_shim/lib/src/dart_ui_fake.dart
@@ -5,9 +5,11 @@
 // Fake interface for the logic that this package needs from (web-only) dart:ui.
 // This is conditionally exported so the analyzer sees these methods as available.
 
+// ignore_for_file: avoid_classes_with_only_static_members
+// ignore_for_file: camel_case_types
+
 /// Shim for web_ui engine.PlatformViewRegistry
 /// https://github.com/flutter/engine/blob/master/lib/web_ui/lib/ui.dart#L62
-// ignore: camel_case_types, avoid_classes_with_only_static_members
 class platformViewRegistry {
   /// Shim for registerViewFactory
   /// https://github.com/flutter/engine/blob/master/lib/web_ui/lib/ui.dart#L72
@@ -20,7 +22,6 @@ class platformViewRegistry {
 
 /// Shim for web_ui engine.AssetManager.
 /// https://github.com/flutter/engine/blob/master/lib/web_ui/lib/src/engine/assets.dart#L12
-// ignore: camel_case_types, avoid_classes_with_only_static_members
 class webOnlyAssetManager {
   /// Shim for getAssetUrl.
   /// https://github.com/flutter/engine/blob/master/lib/web_ui/lib/src/engine/assets.dart#L45
