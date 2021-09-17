6796dff123249f0f8e9fd095151ae5b2b1af83c1
Aaron Clarke <aaclarke@google.com>
2021-09-17 10:57:59 -0700

    [pigeon] fixed bug when using integer primitives on java

--- packages/pigeon/lib/java_generator.dart
+++ packages/pigeon/lib/java_generator.dart
@@ -171,7 +171,13 @@ static MessageCodec<Object> getCodec() {
                   indent.writeln(
                       'ArrayList<Object> args = (ArrayList<Object>)message;');
                   enumerate(method.arguments, (int index, NamedType arg) {
-                    final String argType = _javaTypeForDartType(arg.type);
+                    // The StandardMessageCodec can give us [Integer, Long] for
+                    // a Dart 'int'.  To keep things simple we just use 64bit
+                    // longs in Pigeon with Java.
+                    final bool isInt = arg.type.baseName == 'int';
+                    final String argType =
+                        isInt ? 'Number' : _javaTypeForDartType(arg.type);
+                    final String argCast = isInt ? '.longValue()' : '';
                     final String argName = _getSafeArgumentName(index, arg);
                     indent.writeln(
                         '$argType $argName = ($argType)args.get($index);');
@@ -180,7 +186,7 @@ static MessageCodec<Object> getCodec() {
                       indent.writeln(
                           'throw new NullPointerException("$argName unexpectedly null.");');
                     });
-                    methodArgument.add(argName);
+                    methodArgument.add('$argName$argCast');
                   });
                 }
                 if (method.isAsynchronous) {
