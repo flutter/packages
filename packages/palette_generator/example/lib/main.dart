8111c4be77c7c4d72f0d7954310ddf91e2367613
David Iglesias Teixeira <ditman@gmail.com>
2021-09-22 18:42:23 -0700

    Fix unnecessary_import lint in palette_generator

--- packages/palette_generator/example/lib/main.dart
+++ packages/palette_generator/example/lib/main.dart
@@ -6,7 +6,6 @@ import 'dart:async';
 import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 import 'package:palette_generator/palette_generator.dart';
 
