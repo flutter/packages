c444f12b65f2649de1e76a227bbabcebc55f28a4
Dan Field <dnfield@google.com>
2021-10-08 16:15:54 -0700

    Update flutter_svg.dart

--- third_party/lib/flutter_svg.dart
+++ third_party/lib/flutter_svg.dart
@@ -1,4 +1,6 @@
 export 'src/picture_provider.dart';
 export 'src/picture_stream.dart';
+export 'src/svg/default_theme.dart';
+export 'src/svg/theme.dart';
 export 'src/vector_drawable.dart';
 export 'svg.dart';
