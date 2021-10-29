718e27cf08be0e6ab0c4335145ad7137a208d274
Jonas Uekötter <dev@uekoetter.dev>
2021-10-29 20:22:56 +0200

    add license

--- packages/cross_file/test/image_data.dart
+++ packages/cross_file/test/image_data.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /// A 50x50 blue square png.
 const List<int> kBlueSquarePng = <int>[
   0x89,
