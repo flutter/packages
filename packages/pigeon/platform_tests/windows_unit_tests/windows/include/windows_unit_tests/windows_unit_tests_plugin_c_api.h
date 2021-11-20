88302c519e696ca6dcfaa47aebedf746e2612210
Stuart Morgan <stuartmorgan@google.com>
2021-11-19 20:25:55 -0500

    Missing licenses in template code

--- packages/pigeon/platform_tests/windows_unit_tests/windows/include/windows_unit_tests/windows_unit_tests_plugin_c_api.h
+++ packages/pigeon/platform_tests/windows_unit_tests/windows/include/windows_unit_tests/windows_unit_tests_plugin_c_api.h
@@ -1,3 +1,6 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 #ifndef FLUTTER_PLUGIN_WINDOWS_UNIT_TESTS_PLUGIN_C_API_H_
 #define FLUTTER_PLUGIN_WINDOWS_UNIT_TESTS_PLUGIN_C_API_H_
 
