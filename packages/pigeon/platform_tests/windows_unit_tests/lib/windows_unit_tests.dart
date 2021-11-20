5419a71d9d008a9617efb5501ada65a1fc276cf4
Stuart Morgan <stuartmorgan@google.com>
2021-11-19 22:02:43 -0500

    Template cleanup

--- packages/pigeon/platform_tests/windows_unit_tests/lib/windows_unit_tests.dart
+++ packages/pigeon/platform_tests/windows_unit_tests/lib/windows_unit_tests.dart
@@ -2,10 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: public_member_api_docs
+
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+// ignore: avoid_classes_with_only_static_members
 class WindowsUnitTests {
   static const MethodChannel _channel = MethodChannel('windows_unit_tests');
 
