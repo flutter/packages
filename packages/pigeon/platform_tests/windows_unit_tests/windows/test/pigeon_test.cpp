15c595c0fffa5558402fd77f0163d34a6ce8b84a
stuartmorgan <stuartmorgan@google.com>
2021-11-19 23:28:00 -0500

    [pigeon] Create the skeleton for Windows unit tests (#516)
    Creates a Windows plugin, with unit tests configured using the model that has been used for flutter/plugins Windows unit tests. This was created by:
    - Running `flutter create --platforms=windows -t plugin`.
    - Replacing the method channel with a placeholder.
    - Adding the CMake logic to build unit tests, along with a placeholder unit test.
    
    This is intended to be used as the foundation for testing Pigeon-generated Windows platform channels, similar to the existing native Pigeon tests for iOS and Android.
    
    Part of https://github.com/flutter/flutter/issues/91061

---
+++ packages/pigeon/platform_tests/windows_unit_tests/windows/test/pigeon_test.cpp
@@ -0,0 +1,53 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include <flutter/method_call.h>
+#include <flutter/method_result_functions.h>
+#include <flutter/standard_method_codec.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <string>
+
+#include "windows_unit_tests_plugin.h"
+
+namespace windows_unit_tests {
+namespace test {
+
+namespace {
+
+using ::testing::DoAll;
+using ::testing::Pointee;
+using ::testing::Return;
+using ::testing::SetArgPointee;
+using flutter::EncodableMap;
+using flutter::EncodableValue;
+
+class MockMethodResult : public flutter::MethodResult<> {
+ public:
+  MOCK_METHOD(void, SuccessInternal, (const EncodableValue* result),
+              (override));
+  MOCK_METHOD(void, ErrorInternal,
+              (const std::string& error_code, const std::string& error_message,
+               const EncodableValue* details),
+              (override));
+  MOCK_METHOD(void, NotImplementedInternal, (), (override));
+};
+
+}  // namespace
+
+TEST(PigeonTests, Placeholder) {
+  std::unique_ptr<MockMethodResult> result =
+      std::make_unique<MockMethodResult>();
+
+  // Expect a success response.
+  EXPECT_CALL(*result, SuccessInternal(Pointee(EncodableValue(true))));
+
+  WindowsUnitTestsPlugin plugin;
+  plugin.HandleMethodCall(flutter::MethodCall<>("placeholder", nullptr),
+                          std::move(result));
+}
+
+}  // namespace test
+}  // namespace windows_unit_tests
---
+++ packages/pigeon/platform_tests/windows_unit_tests/windows/windows_unit_tests_plugin.cpp
@@ -0,0 +1,46 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include "windows_unit_tests_plugin.h"
+
+#include <flutter/method_channel.h>
+#include <flutter/plugin_registrar_windows.h>
+#include <flutter/standard_method_codec.h>
+
+#include <memory>
+
+namespace windows_unit_tests {
+
+// static
+void WindowsUnitTestsPlugin::RegisterWithRegistrar(
+    flutter::PluginRegistrarWindows *registrar) {
+  auto channel =
+      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+          registrar->messenger(), "windows_unit_tests",
+          &flutter::StandardMethodCodec::GetInstance());
+
+  auto plugin = std::make_unique<WindowsUnitTestsPlugin>();
+
+  channel->SetMethodCallHandler([plugin_pointer = plugin.get()](
+      const auto &call, auto result) {
+    plugin_pointer->HandleMethodCall(call, std::move(result));
+  });
+
+  registrar->AddPlugin(std::move(plugin));
+}
+
+WindowsUnitTestsPlugin::WindowsUnitTestsPlugin() {}
+
+WindowsUnitTestsPlugin::~WindowsUnitTestsPlugin() {}
+
+void WindowsUnitTestsPlugin::HandleMethodCall(
+    const flutter::MethodCall<flutter::EncodableValue> &method_call,
+    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
+  if (method_call.method_name().compare("placeholder") == 0) {
+    result->Success(flutter::EncodableValue(true));
+  } else {
+    result->NotImplemented();
+  }
+}
+
+}  // namespace windows_unit_tests
