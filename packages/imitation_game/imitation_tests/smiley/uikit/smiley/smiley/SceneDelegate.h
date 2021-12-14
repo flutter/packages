7d47716409226933000ba3bd3dc091eaefea9b83
Stuart Morgan <stuartmorgan@google.com>
2021-12-14 16:26:37 -0500

    Reformat for clang-format update

--- packages/imitation_game/imitation_tests/smiley/uikit/smiley/smiley/SceneDelegate.h
+++ packages/imitation_game/imitation_tests/smiley/uikit/smiley/smiley/SceneDelegate.h
@@ -4,7 +4,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SceneDelegate : UIResponder<UIWindowSceneDelegate>
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
 
 @property(strong, nonatomic) UIWindow* window;
 
