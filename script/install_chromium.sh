e3be16281d090aef59acff086ec8f48edd0e4a00
David Iglesias Teixeira <ditman@gmail.com>
2022-02-15 19:07:08 -0800

    Be a smart-ass and attempt to run tests both in old and new Chromium.

--- script/install_chromium.sh
+++ script/install_chromium.sh
@@ -8,7 +8,11 @@ set -x
 # The build of Chromium used to test web functionality.
 #
 # Chromium builds can be located here: https://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?prefix=Linux_x64/
-CHROMIUM_BUILD=768968
+# 
+# Set CHROMIUM_BUILD env-var to let the script know what to download.
+
+: "${CHROMIUM_BUILD:=950363}" # Default value for the CHROMIUM_BUILD env.
+echo "Downloading CHROMIUM_BUILD=${CHROMIUM_BUILD}"
 
 mkdir .chromium
 wget "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F${CHROMIUM_BUILD}%2Fchrome-linux.zip?alt=media" -O .chromium/chromium.zip
