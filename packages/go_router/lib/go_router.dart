f955c7ec3e5c56146c493713f8c494e0a2debcde
Chun-Heng Tai <chtai@google.com>
2022-02-17 15:25:47 -0800

    license header

--- packages/go_router/lib/go_router.dart
+++ packages/go_router/lib/go_router.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /// A declarative router for Flutter based on Navigation 2 supporting
 /// deep linking, data-driven routes and more
 library go_router;
