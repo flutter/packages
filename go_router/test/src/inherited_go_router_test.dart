4c5d57b0c511af626bc915d5bba53cd008163035
Ascênio Sanderson <ascenio@hotmail.com>
2022-02-18 23:25:55 -0300

    test: adds `InheritedGoRouter` tests

---
+++ go_router/test/src/inherited_go_router_test.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:go_router/go_router.dart';
+import 'package:go_router/src/inherited_go_router.dart';
+
+void main() {
+  test('does not update on changes', () {
+    final oldGoRouter = GoRouter(routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const Page1(),
+      ),
+    ]);
+    final newGoRouter = GoRouter(routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const Page2(),
+      ),
+    ]);
+    final oldInheritedGoRouter = InheritedGoRouter(
+      goRouter: oldGoRouter,
+      child: Container(),
+    );
+    final newInheritedGoRouter = InheritedGoRouter(
+      goRouter: newGoRouter,
+      child: Container(),
+    );
+    final shouldNotify = newInheritedGoRouter.updateShouldNotify(
+      oldInheritedGoRouter,
+    );
+    expect(shouldNotify, false);
+  });
+
+  test('adds `goRouter` as a diagnostics property', () {
+    final goRouter = GoRouter(
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const Page1(),
+        ),
+      ],
+    );
+    final inheritedGoRouter = InheritedGoRouter(
+      goRouter: goRouter,
+      child: Container(),
+    );
+    final properties = DiagnosticPropertiesBuilder();
+    inheritedGoRouter.debugFillProperties(properties);
+    expect(properties.properties.length, 1);
+    expect(properties.properties.first, isA<DiagnosticsProperty<GoRouter>>());
+    expect(properties.properties.first.value, goRouter);
+  });
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container();
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container();
+}
