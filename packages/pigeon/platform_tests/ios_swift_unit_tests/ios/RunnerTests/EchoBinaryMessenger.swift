4fbadee251b0b8e2c671881ff134f99500e59282
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2022-03-01 15:57:54 -0300

    Fix license blocks

--- packages/pigeon/platform_tests/ios_swift_unit_tests/ios/RunnerTests/EchoBinaryMessenger.swift
+++ packages/pigeon/platform_tests/ios_swift_unit_tests/ios/RunnerTests/EchoBinaryMessenger.swift
@@ -1,10 +1,6 @@
-//
-//  EchoBinaryMessenger.swift
-//  RunnerTests
-//
-//  Created by Ailton Vieira on 28/02/22.
-//  Copyright © 2022 The Flutter Authors. All rights reserved.
-//
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import Foundation
 import Flutter
--- packages/pigeon/platform_tests/ios_swift_unit_tests/ios/RunnerTests/Utils.swift
+++ packages/pigeon/platform_tests/ios_swift_unit_tests/ios/RunnerTests/Utils.swift
@@ -1,10 +1,6 @@
-//
-//  Utils.swift
-//  RunnerTests
-//
-//  Created by Ailton Vieira on 01/03/22.
-//  Copyright © 2022 The Flutter Authors. All rights reserved.
-//
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import Foundation
 
