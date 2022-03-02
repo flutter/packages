801a235f854a1ec087faa7b2db7d96b8a02a04df
Dan Field <dnfield@google.com>
2022-03-01 22:13:20 -0800

    Add paint, vertices, indexed vertices, and tests

---
+++ packages/vector_graphics_compiler/test/vertices_test.dart
@@ -0,0 +1,46 @@
+import 'dart:typed_data';
+
+import 'package:vector_graphics_compiler/vector_graphics_compiler.dart';
+
+import 'package:test/test.dart';
+
+void main() {
+  test('Vertices.fromFloat32List', () {
+    final Vertices vertices = Vertices.fromFloat32List(Float32List.fromList(
+      <double>[1, 2, 3, 4, 5, 6],
+    ));
+
+    expect(
+      vertices.vertexPoints,
+      const <Point>[Point(1, 2), Point(3, 4), Point(5, 6)],
+    );
+
+    expect(
+      () => Vertices.fromFloat32List(Float32List.fromList([1])),
+      throwsA(isA<ArgumentError>()),
+    );
+  });
+
+  test('IndexedVertices - creates valid index', () {
+    final Vertices vertices = Vertices.fromFloat32List(Float32List.fromList(
+      <double>[1, 1, 2, 2, 3, 3, 1, 1, 4, 4, 2, 2, 3, 3, 5, 5, 4, 4],
+    ));
+
+    final IndexedVertices indexedVertices = vertices.createIndex();
+    expect(indexedVertices.vertices.length, 10);
+    expect(indexedVertices.indices!.length, 9);
+    expect(indexedVertices.vertices, [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]);
+    expect(indexedVertices.indices, [0, 1, 2, 0, 3, 1, 2, 4, 3]);
+  });
+
+  test('IndexedVertices - does not index if index is larger', () {
+    final Float32List original = Float32List.fromList(
+      <double>[1, 1, 2, 2, 3, 3, 1, 2, 4, 4, 2, 3, 3, 4, 5, 5, 4, 5],
+    );
+    final Vertices vertices = Vertices.fromFloat32List(original);
+
+    final IndexedVertices indexedVertices = vertices.createIndex();
+    expect(indexedVertices.vertices, original);
+    expect(indexedVertices.indices, null);
+  });
+}
