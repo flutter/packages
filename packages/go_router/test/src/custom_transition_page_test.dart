f87f52e5355c1587be1f52901591f133a5ed06b0
Ascênio Sanderson <ascenio@hotmail.com>
2022-03-06 23:25:46 -0300

    refactor: tests `NoTransitionPage` based on widget's position

--- packages/go_router/test/src/custom_transition_page_test.dart
+++ packages/go_router/test/src/custom_transition_page_test.dart
@@ -6,8 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
 
-import '../go_router_test.dart';
-
 void main() {
   testWidgets('CustomTransitionPage builds its child using transitionsBuilder',
       (WidgetTester tester) async {
@@ -34,21 +32,55 @@ void main() {
     expect(find.byWidget(child), findsOneWidget);
   });
 
-  test('NoTransitionPage does not apply any transition', () {
-    const HomeScreen homeScreen = HomeScreen();
-    const NoTransitionPage<void> page =
-        NoTransitionPage<void>(child: homeScreen);
-    const AlwaysStoppedAnimation<double> primaryAnimation =
-        AlwaysStoppedAnimation<double>(0);
-    const AlwaysStoppedAnimation<double> secondaryAnimation =
-        AlwaysStoppedAnimation<double>(1);
-    final Widget widget = page.transitionsBuilder(
-      DummyBuildContext(),
-      primaryAnimation,
-      secondaryAnimation,
-      homeScreen,
+  testWidgets('NoTransitionPage does not apply any transition',
+      (WidgetTester tester) async {
+    final ValueNotifier<bool> showHomeValueNotifier =
+        ValueNotifier<bool>(false);
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ValueListenableBuilder<bool>(
+          valueListenable: showHomeValueNotifier,
+          builder: (_, bool showHome, __) {
+            return Navigator(
+              pages: <Page<void>>[
+                const NoTransitionPage<void>(
+                  child: LoginScreen(),
+                ),
+                if (showHome) const NoTransitionPage<void>(child: HomeScreen()),
+              ],
+              onPopPage: (Route<dynamic> route, dynamic result) {
+                return route.didPop(result);
+              },
+            );
+          },
+        ),
+      ),
     );
-    expect(widget, homeScreen);
+
+    final Finder homeScreenFinder = find.byType(HomeScreen);
+
+    showHomeValueNotifier.value = true;
+    await tester.pump();
+    final Offset homeScreenPositionInTheMiddleOfAddition =
+        tester.getTopLeft(homeScreenFinder);
+    await tester.pumpAndSettle();
+    final Offset homeScreenPositionAfterAddition =
+        tester.getTopLeft(homeScreenFinder);
+
+    showHomeValueNotifier.value = false;
+    await tester.pump();
+    final Offset homeScreenPositionInTheMiddleOfRemoval =
+        tester.getTopLeft(homeScreenFinder);
+    await tester.pumpAndSettle();
+
+    expect(
+      homeScreenPositionInTheMiddleOfAddition,
+      homeScreenPositionAfterAddition,
+    );
+    expect(
+      homeScreenPositionAfterAddition,
+      homeScreenPositionInTheMiddleOfRemoval,
+    );
   });
 }
 
@@ -56,5 +88,24 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('HomeScreen'),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('LoginScreen'),
+      ),
+    );
+  }
 }
