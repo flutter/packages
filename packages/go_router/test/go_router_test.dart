f59732ecdc13b77635263941951fac1cb9b9a7bd
David Iglesias <ditman@gmail.com>
2022-03-15 02:55:23 -0700

    [go_router] Fix API of test mock. (#1177)

--- packages/go_router/test/go_router_test.dart
+++ packages/go_router/test/go_router_test.dart
@@ -1650,6 +1650,13 @@ class DummyBuildContext implements BuildContext {
     throw UnimplementedError();
   }
 
+  // @override
+  // TODO(dit): Remove ignore below when flutter 2.11.0-0.0.pre.724 becomes stable
+  // ignore:annotate_overrides
+  void dispatchNotification(Notification notification) {
+    throw UnimplementedError();
+  }
+
   @override
   T? findAncestorRenderObjectOfType<T extends RenderObject>() {
     throw UnimplementedError();
