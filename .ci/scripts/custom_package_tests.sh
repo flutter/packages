49b9bcba24a8dd402f716c00e84c6fb0ea1ffd06
Stuart Morgan <stuartmorgan@google.com>
2022-03-18 12:03:26 -0400

    [ci] Switch local_tests to the new repo tool command
    Eliminates local_tests.sh in favor of the new `custom-test` command in
    `flutter_plugin_tools`.
    
    Related changes:
    - Renames the test step to match the command name, and be clearer;
      "local" was an ambiguous name.
    - Enables the tests on Linux, since generally we want to run tests
      across platforms.
    
    Fixes https://github.com/flutter/flutter/issues/85543

--- .ci/scripts/custom_package_tests.sh
+++ .ci/scripts/custom_package_tests.sh
@@ -3,4 +3,5 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-./script/local_tests.sh
+dart pub global run flutter_plugin_tools custom-test \
+   --packages-for-branch --log-timing
