cb22b39eab4939d455555cbee1276f1ff2e16729
stuartmorgan <stuartmorgan@google.com>
2022-04-19 12:51:40 -0400

    [pigeon] Enable NNBD in tool (#1532)
    Opts Pigeon into running in NNBD mode. All of the code had already been converted.
    
    Also updates a legacy Dart command (dart pub run) in the test script to the modern version.

--- packages/pigeon/bin/run_tests.dart
+++ packages/pigeon/bin/run_tests.dart
@@ -209,7 +209,6 @@ Future<int> _runPigeon(
     bool streamOutput = true}) async {
   const bool hasDart = false;
   final List<String> args = <String>[
-    'pub',
     'run',
     'pigeon',
     '--input',
--- packages/pigeon/lib/generator_tools.dart
+++ packages/pigeon/lib/generator_tools.dart
@@ -8,7 +8,7 @@ import 'dart:mirrors';
 import 'ast.dart';
 
 /// The current version of pigeon. This must match the version in pubspec.yaml.
-const String pigeonVersion = '3.0.0';
+const String pigeonVersion = '3.0.1';
 
 /// Read all the content from [stdin] to a String.
 String readStdin() {
