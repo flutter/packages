fc0c33710c9f6be5bcf8066c80b47777d8133ea5
chunhtai <47866232+chunhtai@users.noreply.github.com>
2022-05-17 15:02:06 -0700

    [ci] Skip tests that aren't suppose to run on win (#1942)

--- .ci/scripts/dart_unit_tests_win32.sh
+++ .ci/scripts/dart_unit_tests_win32.sh
@@ -3,5 +3,5 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-dart pub global run flutter_plugin_tools test --packages-for-branch \
-  --log-timing
+dart pub global run flutter_plugin_tools test --exclude=script/configs/windows_unit_tests_exceptions.yaml \
+  --packages-for-branch --log-timing
