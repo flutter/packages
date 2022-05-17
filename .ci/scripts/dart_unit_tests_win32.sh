dd8ea5f259903580db2db51f42fd4f9b004c8dab
Chun-Heng Tai <chtai@google.com>
2022-05-17 10:29:03 -0700

    add config and TODO links

--- .ci/scripts/dart_unit_tests_win32.sh
+++ .ci/scripts/dart_unit_tests_win32.sh
@@ -3,7 +3,5 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Exclude flutter_image because its tests need a test server, so are run via custom_package_tests.
-# Exclude xgd_directories because it is a Linux only package.
-dart pub global run flutter_plugin_tools test --exclude=flutter_image,fuchsia_ctl,xgd_directories \
+dart pub global run flutter_plugin_tools test --exclude=../../script/configs/windows_unit_tests_exceptions.yaml \
   --packages-for-branch --log-timing
