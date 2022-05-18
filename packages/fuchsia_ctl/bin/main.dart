6b7bd1d4334938a3b1110d82f8446f35fb917859
ramanjitsingh-hub <63701517+ramanjitsingh-hub@users.noreply.github.com>
2022-05-18 16:59:08 +0530

    Update main.dart

--- packages/fuchsia_ctl/bin/main.dart
+++ packages/fuchsia_ctl/bin/main.dart
@@ -8,7 +8,6 @@ import 'package:args/args.dart';
 import 'package:file/file.dart';
 import 'package:file/local.dart';
 import 'package:fuchsia_ctl/fuchsia_ctl.dart';
-import 'package:meta/meta.dart';
 import 'package:path/path.dart' as path;
 import 'package:retry/retry.dart';
 import 'package:uuid/uuid.dart';
