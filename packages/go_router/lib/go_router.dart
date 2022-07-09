25577e2b0a7ac1437c5e3a452b508fccdeb2af2a
Nazareno Cavazzon <49735945+NazarenoCavazzon@users.noreply.github.com>
2022-07-09 16:48:29 -0300

    Update go_router.dart

--- packages/go_router/lib/go_router.dart
+++ packages/go_router/lib/go_router.dart
@@ -70,6 +70,21 @@ extension GoRouterHelper on BuildContext {
         extra: extra,
       );
 
+  /// Navigate to a named route onto the page stack.
+  Future<T> pushNamedAsync<T extends Object?>(
+    String name, {
+    Map<String, String> params = const <String, String>{},
+    Map<String, String> queryParams = const <String, String>{},
+    Object? extra,
+  }) async {
+    return GoRouter.of(this).pushNamedAsync<T>(
+      name,
+      params: params,
+      queryParams: queryParams,
+      extra: extra,
+    );
+  }
+
   /// Returns `true` if there is more than 1 page on the stack.
   bool canPop() => GoRouter.of(this).canPop();
 
