9d708e5c13a1f514a6563ba6d46aacc364341698
John Ryan <ryjohn@google.com>
2022-07-26 11:56:29 -0700

    Change typed_routing.dart back to route_data.dart (#2385)
    * Change typed_routing.dart back to route_data.dart
    
    * sort imports
    
    * Use flutter_plugin_tools 0.8.9
    
    * Bump go_router version to 4.2.2
    
    * Update CHANGELOG
    
    * Update CHANGELOG.md

--- packages/go_router/lib/go_router.dart
+++ packages/go_router/lib/go_router.dart
@@ -12,6 +12,6 @@ export 'src/misc/inherited_router.dart';
 export 'src/misc/refresh_stream.dart';
 export 'src/pages/custom_transition_page.dart';
 export 'src/platform.dart' show UrlPathStrategy;
+export 'src/route_data.dart' show GoRouteData, TypedGoRoute;
 export 'src/router.dart';
-export 'src/typed_routing.dart' show GoRouteData, TypedGoRoute;
 export 'src/typedefs.dart' show GoRouterPageBuilder, GoRouterRedirect;
