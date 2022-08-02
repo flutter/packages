202e56cfe986d9addd267047134a2864e435153b
Youssef Attia <yattia@google.com>
2022-08-01 17:08:40 -0700

    example update

--- packages/adaptive_scaffold/example/android/app/src/main/kotlin/dev/flutter/packages/animations/example/MainActivity.kt
+++ packages/adaptive_scaffold/example/android/app/src/main/kotlin/dev/flutter/packages/animations/example/MainActivity.kt
@@ -1,13 +1,10 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+package dev.flutter.packages.animations.example
 
-import 'package:flutter/material.dart';
+import io.flutter.embedding.android.FlutterActivity
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Container(),
-    ),
-  );
+class MainActivity: FlutterActivity() {
+
 }
