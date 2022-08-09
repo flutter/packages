88a3eaea5f2948e161270dc95d75a7fc06b1ec4c
Youssef Attia <yattia@google.com>
2022-08-09 09:33:12 -0700

    main tests

---
+++ packages/adaptive_scaffold/example/test/main_test.dart
@@ -0,0 +1,50 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:adaptive_scaffold_example/adaptive_scaffold_demo.dart'
+    as example;
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final Finder body = find.byKey(const Key('body'));
+  final Finder sBody = find.byKey(const Key('sBody'));
+  final Finder bnav = find.byKey(const Key('bottomNavigation'));
+  final Finder pnav = find.byKey(const Key('primaryNavigation'));
+  final Finder pnav1 = find.byKey(const Key('primaryNavigation1'));
+  testWidgets('dislays correct item of config based on screen width',
+      (WidgetTester tester) async {
+    Future<void> updateScreen(double width) async {
+      await tester.binding.setSurfaceSize(Size(width, 800));
+      await tester.pumpWidget(
+        MaterialApp(
+          home: MediaQuery(
+              data: MediaQueryData(size: Size(width, 800)),
+              child: const example.MyHomePage()),
+        ),
+      );
+      await tester.pumpAndSettle();
+    }
+
+    await updateScreen(300);
+    expect(sBody, findsNothing);
+    expect(bnav, findsOneWidget);
+    expect(body, findsNothing);
+    expect(pnav, findsNothing);
+    expect(pnav1, findsNothing);
+
+    await updateScreen(800);
+    expect(body, findsOneWidget);
+    expect(body, findsOneWidget);
+    expect(bnav, findsNothing);
+    expect(sBody, findsOneWidget);
+    expect(pnav, findsOneWidget);
+    expect(pnav1, findsNothing);
+
+    await updateScreen(1100);
+    expect(body, findsOneWidget);
+    expect(pnav, findsNothing);
+    expect(pnav1, findsOneWidget);
+  });
+}
