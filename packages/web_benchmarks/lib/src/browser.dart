f634ba5526d1ccab26805ad5a0057ea949984108
Aman Verma <verma1090aman@gmail.com>
2022-08-15 19:20:14 +0530

    Revert "update packages and where method"
    This reverts commit 490ca5f7b38201c4495b11209aa2fcb544d4f1c7.

--- packages/web_benchmarks/lib/src/browser.dart
+++ packages/web_benchmarks/lib/src/browser.dart
@@ -237,11 +237,14 @@ String _findSystemChromeExecutable() {
     return '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
   } else if (io.Platform.isWindows) {
     const String kWindowsExecutable = r'Google\Chrome\Application\chrome.exe';
-    final List<String> kWindowsPrefixes = <String?>[
-      io.Platform.environment['LOCALAPPDATA'],
-      io.Platform.environment['PROGRAMFILES'],
-      io.Platform.environment['PROGRAMFILES(X86)'],
-    ].whereType<String>().toList();
+    final List<String> kWindowsPrefixes = <String>[
+      for (String? item in <String?>[
+        io.Platform.environment['LOCALAPPDATA'],
+        io.Platform.environment['PROGRAMFILES'],
+        io.Platform.environment['PROGRAMFILES(X86)'],
+      ])
+        if (item != null) item
+    ];
 
     final String windowsPrefix = kWindowsPrefixes.firstWhere((String prefix) {
       final String expectedPath = path.join(prefix, kWindowsExecutable);
