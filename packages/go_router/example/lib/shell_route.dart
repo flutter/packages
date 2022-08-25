c794b87eb708d6612b70f83a7e7aa3b98b1c194b
John Ryan <ryjohn@google.com>
2022-08-25 13:44:45 -0700

    add explanation to shell route example

--- packages/go_router/example/lib/shell_route.dart
+++ packages/go_router/example/lib/shell_route.dart
@@ -10,6 +10,15 @@ final GlobalKey<NavigatorState> _rootNavigatorKey =
 final GlobalKey<NavigatorState> _shellNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'shell');
 
+// This scenario demonstrates how to set up nested navigation using ShellRoute,
+// which is a pattern where an additional Navigator is placed in the widget tree
+// to be used instead of the root navigator. This allows deep-links to display
+// pages along with other UI components such as a BottomNavigationBar.
+//
+// This example demonstrates how to display a route within a ShellRoute and also
+// push a screen using a different navigator (such as the root Navigator) by
+// providing a `parentNavigatorKey`.
+
 void main() {
   runApp(ShellRouteExampleApp());
 }
