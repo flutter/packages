34cebc57b353162f380dd32830fd1da27961af49
Luis López <davidlopezm@google.com>
2022-09-01 10:40:46 -0500

    Added example

---
+++ packages/dynamic_layouts/example/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter Demo"),
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
