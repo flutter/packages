274742754333d99531a61fce3af3e50677327c66
Santiago Pedroza <pedrozas@google.com>
2022-09-01 11:33:14 -0500

    Added license headers

--- packages/dynamic_layouts/example/ios/Runner/AppDelegate.swift
+++ packages/dynamic_layouts/example/ios/Runner/AppDelegate.swift
@@ -1,4 +1,6 @@
-import UIKit
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.import UIKit
 import Flutter
 
 @UIApplicationMain
