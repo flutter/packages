2c205d23a5c6f0e72338a864d19212660fafa7bd
Luis López <davidlopezm@google.com>
2022-09-01 11:42:04 -0500

    Formatting

--- packages/dynamic_layouts/example/lib/main.dart
+++ packages/dynamic_layouts/example/lib/main.dart
@@ -5,20 +5,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const _MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _MyApp extends StatelessWidget {
+  const _MyApp();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Dynamic Layouts Example"),
+          title: const Text('Dynamic Layouts Example'),
         ),
-        body: const Text("Hello!"),
+        body: const Text('Hello!'),
       ),
     );
   }
--- packages/dynamic_layouts/lib/dynamic_layouts.dart
+++ packages/dynamic_layouts/lib/dynamic_layouts.dart
@@ -7,5 +7,3 @@ export 'src/dynamic_grid.dart';
 export 'src/render_dynamic_grid.dart';
 export 'src/staggered_layout.dart';
 export 'src/wrap_layout.dart';
-export 'src/staggered_layout.dart';
-export 'src/wrap_layout.dart';
