c693a5a6aee75ce99d54685908f1b8dd331c9c12
Santiago Pedroza <pedrozas@google.com>
2022-09-02 16:51:48 -0500

    Removed template comments and nits

--- packages/dynamic_layouts/example/lib/main.dart
+++ packages/dynamic_layouts/example/lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
   /// Main example constructor.
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
