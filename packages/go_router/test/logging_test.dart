4bfdd077a9e8dad34407e8834df1f8c8418fa51b
John Ryan <ryjohn@google.com>
2022-09-08 15:24:14 -0700

    [go_router] Don't clear listeners when logging is disabled (#2533)

--- packages/go_router/test/logging_test.dart
+++ packages/go_router/test/logging_test.dart
@@ -7,21 +7,11 @@ import 'package:go_router/src/logging.dart';
 import 'package:logging/logging.dart';
 
 void main() {
-  // Reset the logger before each test.
-  setUp(() {
-    setLogging();
-  });
-  test('setLogging enables log messages on the logger', () {
-    log.onRecord.listen(expectAsync1<void, LogRecord>((LogRecord r) {}));
-
+  test('setLogging does not clear listeners', () {
+    log.onRecord
+        .listen(expectAsync1<void, LogRecord>((LogRecord r) {}, count: 2));
     setLogging(enabled: true);
     log.info('message');
-  });
-
-  test('setLogging disables log messages on the logger', () {
-    log.onRecord
-        .listen(expectAsync1<void, LogRecord>((LogRecord r) {}, count: 0));
-
     setLogging();
     log.info('message');
   });
