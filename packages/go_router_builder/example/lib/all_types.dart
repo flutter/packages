531e5edd74db13598d757c23119999ba77ae1a07
Mirko Mucaria <skogsfrae@gmail.com>
2022-09-10 15:42:41 +0200

    all types example app refactory

--- packages/go_router_builder/example/lib/all_types.dart
+++ packages/go_router_builder/example/lib/all_types.dart
@@ -11,108 +11,334 @@ import 'shared/data.dart';
 
 part 'all_types.g.dart';
 
-@TypedGoRoute<AllTypesRoute>(
-  path: '/:requiredBigIntField/:requiredBoolField/:requiredDateTimeField'
-      '/:requiredDoubleField/:requiredEnumField/:requiredEnhancedEnumField/:requiredIntField'
-      '/:requiredNumField/:requiredStringField/:requiredUriField',
-)
+@TypedGoRoute<AllTypesBaseRoute>(path: '/', routes: [
+  TypedGoRoute<BigIntRoute>(path: 'big-int-route/:requiredBigIntField'),
+  TypedGoRoute<BoolRoute>(path: 'bool-route/:requiredBoolField'),
+  TypedGoRoute<DateTimeRoute>(path: 'date-time-route/:requiredDateTimeField'),
+  TypedGoRoute<DoubleRoute>(path: 'double-route/:requiredDoubleField'),
+  TypedGoRoute<IntRoute>(path: 'int-route/:requiredIntField'),
+  TypedGoRoute<NumRoute>(path: 'num-route/:requiredNumField'),
+  TypedGoRoute<DoubleRoute>(path: 'double-route/:requiredDoubleField'),
+  TypedGoRoute<EnumRoute>(path: 'enum-route/:requiredEnumFieldField'),
+  TypedGoRoute<EnhancedEnumRoute>(path: 'enhanced-enum-route/:requiredEnumFieldField'),
+  TypedGoRoute<StringRoute>(path: 'string-route/:requiredStringFieldField'),
+  TypedGoRoute<UriRoute>(path: 'uri-route/:requiredUriFieldField'),
+])
 @immutable
-class AllTypesRoute extends GoRouteData {
-  const AllTypesRoute({
+class AllTypesBaseRoute extends GoRouteData {
+  const AllTypesBaseRoute();
+
+  @override
+  Widget build(BuildContext context) => BasePage<void>(
+    dataTitle: 'Root',
+    param: null,
+  );
+}
+
+class BigIntRoute extends GoRouteData {
+  final BigInt requiredBigIntField;
+  final BigInt? bigIntField;
+
+  BigIntRoute({
     required this.requiredBigIntField,
-    required this.requiredBoolField,
-    required this.requiredDateTimeField,
-    required this.requiredDoubleField,
-    required this.requiredEnumField,
-    required this.requiredIntField,
-    required this.requiredNumField,
-    required this.requiredEnhancedEnumField,
-    required this.requiredStringField,
-    required this.requiredUriField,
     this.bigIntField,
-    this.boolField,
-    this.dateTimeField,
-    this.doubleField,
-    this.enumField,
-    this.enhancedEnumField,
-    this.intField,
-    this.numField,
-    this.stringField,
-    this.uriField,
   });
 
-  final BigInt requiredBigIntField;
-  final bool requiredBoolField;
-  final DateTime requiredDateTimeField;
-  final double requiredDoubleField;
-  final PersonDetails requiredEnumField;
-  final SportDetails requiredEnhancedEnumField;
-  final int requiredIntField;
-  final num requiredNumField;
-  final String requiredStringField;
-  final Uri requiredUriField;
+  @override
+  Widget build(BuildContext context) => BasePage<BigInt>(
+    dataTitle: 'BigIntRoute',
+    param: requiredBigIntField,
+    queryParam: bigIntField,
+  );
 
-  final BigInt? bigIntField;
+  Widget drawerTile(BuildContext context) => ListTile(
+        title: Text('BigIntRoute'),
+        onTap: () => go(context),
+        selected: GoRouter.of(context).location == location,
+      );
+}
+
+class BoolRoute extends GoRouteData {
+  final bool requiredBoolField;
   final bool? boolField;
+
+  BoolRoute({
+    required this.requiredBoolField,
+    this.boolField,
+  });
+
+  @override
+  Widget build(BuildContext context) => BasePage<bool>(
+    dataTitle: 'BoolRoute',
+    param: requiredBoolField,
+    queryParam: boolField,
+  );
+
+  Widget drawerTile(BuildContext context) => ListTile(
+        title: Text('BoolRoute'),
+        onTap: () => go(context),
+        selected: GoRouter.of(context).location == location,
+      );
+}
+
+class DateTimeRoute extends GoRouteData {
+  final DateTime requiredDateTimeField;
   final DateTime? dateTimeField;
+
+  DateTimeRoute({
+    required this.requiredDateTimeField,
+    this.dateTimeField,
+  });
+
+  @override
+  Widget build(BuildContext context) => BasePage<DateTime>(
+    dataTitle: 'DateTimeRoute',
+    param: requiredDateTimeField,
+    queryParam: dateTimeField,
+  );
+
+  Widget drawerTile(BuildContext context) => ListTile(
+        title: Text('DateTimeRoute'),
+        onTap: () => go(context),
+        selected: GoRouter.of(context).location == location,
+      );
+}
+
+class DoubleRoute extends GoRouteData {
+  final double requiredDoubleField;
   final double? doubleField;
-  final PersonDetails? enumField;
-  final SportDetails? enhancedEnumField;
+
+  DoubleRoute({
+    required this.requiredDoubleField,
+    this.doubleField,
+  });
+
+  @override
+  Widget build(BuildContext context) => BasePage<double>(
+    dataTitle: 'DoubleRoute',
+    param: requiredDoubleField,
+    queryParam: doubleField,
+  );
+
+  Widget drawerTile(BuildContext context) => ListTile(
+        title: Text('DoubleRoute'),
+        onTap: () => go(context),
+        selected: GoRouter.of(context).location == location,
+      );
+}
+
+class IntRoute extends GoRouteData {
+  final int requiredIntField;
   final int? intField;
+
+  IntRoute({
+    required this.requiredIntField,
+    this.intField,
+  });
+
+  @override
+  Widget build(BuildContext context) => BasePage<int>(
+    dataTitle: 'IntRoute',
+    param: requiredIntField,
+    queryParam: intField,
+  );
+
+  Widget drawerTile(BuildContext context) => ListTile(
+        title: Text('IntRoute'),
+        onTap: () => go(context),
+        selected: GoRouter.of(context).location == location,
+      );
+}
+
+class NumRoute extends GoRouteData {
+  final num requiredNumField;
   final num? numField;
-  final String? stringField;
-  final Uri? uriField;
+
+  NumRoute({
+    required this.requiredNumField,
+    this.numField,
+  });
+
+  @override
+  Widget build(BuildContext context) => BasePage<num>(
+    dataTitle: 'NumRoute',
+    param: requiredNumField,
+    queryParam: numField,
+  );
+
+  Widget drawerTile(BuildContext context) => ListTile(
+        title: Text('NumRoute'),
+        onTap: () => go(context),
+        selected: GoRouter.of(context).location == location,
+      );
+}
+
+class EnumRoute extends GoRouteData {
+  final PersonDetails requiredEnumFieldField;
+  final PersonDetails? enumFieldField;
+
+  EnumRoute({
+    required this.requiredEnumFieldField,
+    this.enumFieldField,
+  });
+
+  @override
+  Widget build(BuildContext context) => BasePage<PersonDetails>(
+    dataTitle: 'EnumRoute',
+    param: requiredEnumFieldField,
+    queryParam: enumFieldField,
+  );
+
+  Widget drawerTile(BuildContext context) => ListTile(
+        title: Text('EnumRoute'),
+        onTap: () => go(context),
+        selected: GoRouter.of(context).location == location,
+      );
+}
+
+class EnhancedEnumRoute extends GoRouteData {
+  final SportDetails requiredEnumFieldField;
+  final SportDetails? enumFieldField;
+
+  EnhancedEnumRoute({
+    required this.requiredEnumFieldField,
+    this.enumFieldField,
+  });
+
+  @override
+  Widget build(BuildContext context) => BasePage<SportDetails>(
+    dataTitle: 'EnhancedEnumRoute',
+    param: requiredEnumFieldField,
+    queryParam: enumFieldField,
+  );
+
+  Widget drawerTile(BuildContext context) => ListTile(
+        title: Text('EnhancedEnumRoute'),
+        onTap: () => go(context),
+        selected: GoRouter.of(context).location == location,
+      );
+}
+
+class StringRoute extends GoRouteData {
+  final String requiredStringFieldField;
+  final String? stringFieldField;
+
+  StringRoute({
+    required this.requiredStringFieldField,
+    this.stringFieldField,
+  });
+
+  @override
+  Widget build(BuildContext context) => BasePage<String>(
+    dataTitle: 'StringRoute',
+    param: requiredStringFieldField,
+    queryParam: stringFieldField,
+  );
+
+  Widget drawerTile(BuildContext context) => ListTile(
+        title: Text('StringRoute'),
+        onTap: () => go(context),
+        selected: GoRouter.of(context).location == location,
+      );
+}
+
+class UriRoute extends GoRouteData {
+  final Uri requiredUriFieldField;
+  final Uri? uriFieldField;
+
+  UriRoute({
+    required this.requiredUriFieldField,
+    this.uriFieldField,
+  });
+
+  @override
+  Widget build(BuildContext context) => BasePage<Uri>(
+    dataTitle: 'UriRoute',
+    param: requiredUriFieldField,
+    queryParam: uriFieldField,
+  );
+
+  Widget drawerTile(BuildContext context) => ListTile(
+        title: Text('UriRoute'),
+        onTap: () => go(context),
+        selected: GoRouter.of(context).location == location,
+      );
+}
+
+class BasePage<T> extends StatelessWidget {
+  final String dataTitle;
+  final T param;
+  final T? queryParam;
+
+  BasePage({
+    required this.dataTitle,
+    required this.param,
+    this.queryParam,
+  });
 
   @override
   Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text('Go router typed routes'),
+    ),
+        drawer: Drawer(
+            child: ListView(
+          children: [
+            BigIntRoute(
+              requiredBigIntField: BigInt.two,
+              bigIntField: BigInt.zero,
+            ).drawerTile(context),
+            BoolRoute(
+              requiredBoolField: true,
+              boolField: false,
+            ).drawerTile(context),
+            DateTimeRoute(
+              requiredDateTimeField: DateTime(1970, 1, 1),
+              dateTimeField: DateTime(0),
+            ).drawerTile(context),
+            DoubleRoute(
+              requiredDoubleField: 3.14,
+              doubleField: -3.14,
+            ).drawerTile(context),
+            IntRoute(
+              requiredIntField: 42,
+              intField: -42,
+            ).drawerTile(context),
+            NumRoute(
+              requiredNumField: 2.71828,
+              numField: -2.71828,
+            ).drawerTile(context),
+            StringRoute(
+              requiredStringFieldField:r'$!/#bob%%20',
+              stringFieldField: r'$!/#bob%%20',
+            ).drawerTile(context),
+            EnumRoute(
+              requiredEnumFieldField: PersonDetails.favoriteSport,
+              enumFieldField: PersonDetails.favoriteFood,
+            ).drawerTile(context),
+            EnhancedEnumRoute(
+              requiredEnumFieldField: SportDetails.football,
+              enumFieldField: SportDetails.volleyball,
+            ).drawerTile(context),
+            UriRoute(
+              requiredUriFieldField: Uri.parse('https://dart.dev'),
+              uriFieldField: Uri.parse('https://dart.dev'),
+            ).drawerTile(context),
+          ],
+        )),
         body: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              const Text('built!'),
-              SelectableText(location),
+              const Text('Built!'),
+              Text(dataTitle),
+              Text('Param: $param'),
+              Text('Query param: $queryParam'),
+              SelectableText(GoRouter.of(context).location),
             ],
           ),
         ),
       );
-
-  @override
-  int get hashCode => Object.hashAll(_items);
-
-  @override
-  bool operator ==(Object other) {
-    if (other is AllTypesRoute) {
-      final List<Object?> mine = _items;
-      final List<Object?> theirs = other._items;
-      for (int i = 0; i < mine.length; i++) {
-        if (mine[i] != theirs[i]) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
-  List<Object?> get _items => <Object?>[
-        requiredBigIntField,
-        requiredBoolField,
-        requiredDateTimeField,
-        requiredDoubleField,
-        requiredEnumField,
-        requiredIntField,
-        requiredNumField,
-        requiredStringField,
-        requiredUriField,
-        bigIntField,
-        boolField,
-        dateTimeField,
-        doubleField,
-        enumField,
-        intField,
-        numField,
-        stringField,
-        uriField,
-      ];
 }
 
 void main() => runApp(AllTypesApp());
@@ -130,60 +356,6 @@ class AllTypesApp extends StatelessWidget {
   late final GoRouter _router = GoRouter(
     debugLogDiagnostics: true,
     routes: $appRoutes,
-    initialLocation: AllTypesRoute(
-      requiredBigIntField: BigInt.two,
-      requiredBoolField: true,
-      requiredDateTimeField: DateTime.now(),
-      requiredDoubleField: 3.14,
-      requiredEnumField: PersonDetails.favoriteSport,
-      requiredEnhancedEnumField: SportDetails.football,
-      requiredIntField: -42,
-      requiredNumField: 3.15,
-      requiredStringField: r'$!/#bob%%20',
-      requiredUriField: Uri.parse('https://dart.dev'),
-      bigIntField: BigInt.zero,
-      boolField: false,
-      dateTimeField: DateTime(0),
-      doubleField: 3.14,
-      enumField: PersonDetails.favoriteSport,
-      enhancedEnumField: SportDetails.volleyball,
-      intField: -42,
-      numField: 3.15,
-      stringField: r'$!/#bob%%20',
-      uriField: Uri.parse('https://dart.dev'),
-    ).location,
-
-    // redirect to the login page if the user is not logged in
-    redirect: (GoRouterState state) {
-      if (state.location == '/') {
-        final String location = AllTypesRoute(
-          requiredBigIntField: BigInt.two,
-          requiredBoolField: true,
-          requiredDateTimeField: DateTime.now(),
-          requiredDoubleField: 3.14,
-          requiredEnumField: PersonDetails.favoriteSport,
-          requiredEnhancedEnumField: SportDetails.football,
-          requiredIntField: -42,
-          requiredNumField: 3.15,
-          requiredStringField: r'$!/#bob%%20',
-          requiredUriField: Uri.parse('https://dart.dev'),
-          bigIntField: BigInt.zero,
-          boolField: false,
-          dateTimeField: DateTime(0),
-          doubleField: 3.14,
-          enumField: PersonDetails.favoriteSport,
-          enhancedEnumField: SportDetails.volleyball,
-          intField: -42,
-          numField: 3.15,
-          stringField: r'$!/#bob%%20',
-          uriField: Uri.parse('https://dart.dev'),
-        ).location;
-
-        return location;
-      }
-
-      // no need to redirect at all
-      return null;
-    },
+    initialLocation: AllTypesBaseRoute().location,
   );
 }
--- packages/go_router_builder/example/lib/all_types.g.dart
+++ packages/go_router_builder/example/lib/all_types.g.dart
@@ -9,64 +9,260 @@ part of 'all_types.dart';
 // **************************************************************************
 
 List<GoRoute> get $appRoutes => [
-      $allTypesRoute,
+      $allTypesBaseRoute,
     ];
 
-GoRoute get $allTypesRoute => GoRouteData.$route(
-      path:
-          '/:requiredBigIntField/:requiredBoolField/:requiredDateTimeField/:requiredDoubleField/:requiredEnumField/:requiredEnhancedEnumField/:requiredIntField/:requiredNumField/:requiredStringField/:requiredUriField',
-      factory: $AllTypesRouteExtension._fromState,
+GoRoute get $allTypesBaseRoute => GoRouteData.$route(
+      path: '/',
+      factory: $AllTypesBaseRouteExtension._fromState,
+      routes: [
+        GoRouteData.$route(
+          path: 'big-int-route/:requiredBigIntField',
+          factory: $BigIntRouteExtension._fromState,
+        ),
+        GoRouteData.$route(
+          path: 'bool-route/:requiredBoolField',
+          factory: $BoolRouteExtension._fromState,
+        ),
+        GoRouteData.$route(
+          path: 'date-time-route/:requiredDateTimeField',
+          factory: $DateTimeRouteExtension._fromState,
+        ),
+        GoRouteData.$route(
+          path: 'double-route/:requiredDoubleField',
+          factory: $DoubleRouteExtension._fromState,
+        ),
+        GoRouteData.$route(
+          path: 'int-route/:requiredIntField',
+          factory: $IntRouteExtension._fromState,
+        ),
+        GoRouteData.$route(
+          path: 'num-route/:requiredNumField',
+          factory: $NumRouteExtension._fromState,
+        ),
+        GoRouteData.$route(
+          path: 'double-route/:requiredDoubleField',
+          factory: $DoubleRouteExtension._fromState,
+        ),
+        GoRouteData.$route(
+          path: 'enum-route/:requiredEnumFieldField',
+          factory: $EnumRouteExtension._fromState,
+        ),
+        GoRouteData.$route(
+          path: 'enhanced-enum-route/:requiredEnumFieldField',
+          factory: $EnhancedEnumRouteExtension._fromState,
+        ),
+        GoRouteData.$route(
+          path: 'string-route/:requiredStringFieldField',
+          factory: $StringRouteExtension._fromState,
+        ),
+        GoRouteData.$route(
+          path: 'uri-route/:requiredUriFieldField',
+          factory: $UriRouteExtension._fromState,
+        ),
+      ],
     );
 
-extension $AllTypesRouteExtension on AllTypesRoute {
-  static AllTypesRoute _fromState(GoRouterState state) => AllTypesRoute(
+extension $AllTypesBaseRouteExtension on AllTypesBaseRoute {
+  static AllTypesBaseRoute _fromState(GoRouterState state) =>
+      const AllTypesBaseRoute();
+
+  String get location => GoRouteData.$location(
+        '/',
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+}
+
+extension $BigIntRouteExtension on BigIntRoute {
+  static BigIntRoute _fromState(GoRouterState state) => BigIntRoute(
         requiredBigIntField: BigInt.parse(state.params['requiredBigIntField']!),
-        requiredBoolField: _$boolConverter(state.params['requiredBoolField']!),
-        requiredDateTimeField:
-            DateTime.parse(state.params['requiredDateTimeField']!),
-        requiredDoubleField: double.parse(state.params['requiredDoubleField']!),
-        requiredEnumField: _$PersonDetailsEnumMap
-            ._$fromName(state.params['requiredEnumField']!),
-        requiredIntField: int.parse(state.params['requiredIntField']!),
-        requiredNumField: num.parse(state.params['requiredNumField']!),
-        requiredEnhancedEnumField: _$SportDetailsEnumMap
-            ._$fromName(state.params['requiredEnhancedEnumField']!),
-        requiredStringField: state.params['requiredStringField']!,
-        requiredUriField: Uri.parse(state.params['requiredUriField']!),
         bigIntField:
             _$convertMapValue('big-int-field', state.queryParams, BigInt.parse),
-        boolField:
-            _$convertMapValue('bool-field', state.queryParams, _$boolConverter),
-        dateTimeField: _$convertMapValue(
-            'date-time-field', state.queryParams, DateTime.parse),
-        doubleField:
-            _$convertMapValue('double-field', state.queryParams, double.parse),
-        enumField: _$convertMapValue(
-            'enum-field', state.queryParams, _$PersonDetailsEnumMap._$fromName),
-        enhancedEnumField: _$convertMapValue('enhanced-enum-field',
-            state.queryParams, _$SportDetailsEnumMap._$fromName),
-        intField: _$convertMapValue('int-field', state.queryParams, int.parse),
-        numField: _$convertMapValue('num-field', state.queryParams, num.parse),
-        stringField: state.queryParams['string-field'],
-        uriField: _$convertMapValue('uri-field', state.queryParams, Uri.parse),
       );
 
   String get location => GoRouteData.$location(
-        '/${Uri.encodeComponent(requiredBigIntField.toString())}/${Uri.encodeComponent(requiredBoolField.toString())}/${Uri.encodeComponent(requiredDateTimeField.toString())}/${Uri.encodeComponent(requiredDoubleField.toString())}/${Uri.encodeComponent(_$PersonDetailsEnumMap[requiredEnumField]!)}/${Uri.encodeComponent(_$SportDetailsEnumMap[requiredEnhancedEnumField]!)}/${Uri.encodeComponent(requiredIntField.toString())}/${Uri.encodeComponent(requiredNumField.toString())}/${Uri.encodeComponent(requiredStringField)}/${Uri.encodeComponent(requiredUriField.toString())}',
+        '/big-int-route/${Uri.encodeComponent(requiredBigIntField.toString())}',
         queryParams: {
           if (bigIntField != null) 'big-int-field': bigIntField!.toString(),
+        },
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+}
+
+extension $BoolRouteExtension on BoolRoute {
+  static BoolRoute _fromState(GoRouterState state) => BoolRoute(
+        requiredBoolField: _$boolConverter(state.params['requiredBoolField']!),
+        boolField:
+            _$convertMapValue('bool-field', state.queryParams, _$boolConverter),
+      );
+
+  String get location => GoRouteData.$location(
+        '/bool-route/${Uri.encodeComponent(requiredBoolField.toString())}',
+        queryParams: {
           if (boolField != null) 'bool-field': boolField!.toString(),
+        },
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+}
+
+extension $DateTimeRouteExtension on DateTimeRoute {
+  static DateTimeRoute _fromState(GoRouterState state) => DateTimeRoute(
+        requiredDateTimeField:
+            DateTime.parse(state.params['requiredDateTimeField']!),
+        dateTimeField: _$convertMapValue(
+            'date-time-field', state.queryParams, DateTime.parse),
+      );
+
+  String get location => GoRouteData.$location(
+        '/date-time-route/${Uri.encodeComponent(requiredDateTimeField.toString())}',
+        queryParams: {
           if (dateTimeField != null)
             'date-time-field': dateTimeField!.toString(),
+        },
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+}
+
+extension $DoubleRouteExtension on DoubleRoute {
+  static DoubleRoute _fromState(GoRouterState state) => DoubleRoute(
+        requiredDoubleField: double.parse(state.params['requiredDoubleField']!),
+        doubleField:
+            _$convertMapValue('double-field', state.queryParams, double.parse),
+      );
+
+  String get location => GoRouteData.$location(
+        '/double-route/${Uri.encodeComponent(requiredDoubleField.toString())}',
+        queryParams: {
           if (doubleField != null) 'double-field': doubleField!.toString(),
-          if (enumField != null)
-            'enum-field': _$PersonDetailsEnumMap[enumField!]!,
-          if (enhancedEnumField != null)
-            'enhanced-enum-field': _$SportDetailsEnumMap[enhancedEnumField!]!,
+        },
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+}
+
+extension $IntRouteExtension on IntRoute {
+  static IntRoute _fromState(GoRouterState state) => IntRoute(
+        requiredIntField: int.parse(state.params['requiredIntField']!),
+        intField: _$convertMapValue('int-field', state.queryParams, int.parse),
+      );
+
+  String get location => GoRouteData.$location(
+        '/int-route/${Uri.encodeComponent(requiredIntField.toString())}',
+        queryParams: {
           if (intField != null) 'int-field': intField!.toString(),
+        },
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+}
+
+extension $NumRouteExtension on NumRoute {
+  static NumRoute _fromState(GoRouterState state) => NumRoute(
+        requiredNumField: num.parse(state.params['requiredNumField']!),
+        numField: _$convertMapValue('num-field', state.queryParams, num.parse),
+      );
+
+  String get location => GoRouteData.$location(
+        '/num-route/${Uri.encodeComponent(requiredNumField.toString())}',
+        queryParams: {
           if (numField != null) 'num-field': numField!.toString(),
-          if (stringField != null) 'string-field': stringField!,
-          if (uriField != null) 'uri-field': uriField!.toString(),
+        },
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+}
+
+extension $EnumRouteExtension on EnumRoute {
+  static EnumRoute _fromState(GoRouterState state) => EnumRoute(
+        requiredEnumFieldField: _$PersonDetailsEnumMap
+            ._$fromName(state.params['requiredEnumFieldField']!),
+        enumFieldField: _$convertMapValue('enum-field-field', state.queryParams,
+            _$PersonDetailsEnumMap._$fromName),
+      );
+
+  String get location => GoRouteData.$location(
+        '/enum-route/${Uri.encodeComponent(_$PersonDetailsEnumMap[requiredEnumFieldField]!)}',
+        queryParams: {
+          if (enumFieldField != null)
+            'enum-field-field': _$PersonDetailsEnumMap[enumFieldField!]!,
+        },
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+}
+
+extension $EnhancedEnumRouteExtension on EnhancedEnumRoute {
+  static EnhancedEnumRoute _fromState(GoRouterState state) => EnhancedEnumRoute(
+        requiredEnumFieldField: _$SportDetailsEnumMap
+            ._$fromName(state.params['requiredEnumFieldField']!),
+        enumFieldField: _$convertMapValue('enum-field-field', state.queryParams,
+            _$SportDetailsEnumMap._$fromName),
+      );
+
+  String get location => GoRouteData.$location(
+        '/enhanced-enum-route/${Uri.encodeComponent(_$SportDetailsEnumMap[requiredEnumFieldField]!)}',
+        queryParams: {
+          if (enumFieldField != null)
+            'enum-field-field': _$SportDetailsEnumMap[enumFieldField!]!,
+        },
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+}
+
+extension $StringRouteExtension on StringRoute {
+  static StringRoute _fromState(GoRouterState state) => StringRoute(
+        requiredStringFieldField: state.params['requiredStringFieldField']!,
+        stringFieldField: state.queryParams['string-field-field'],
+      );
+
+  String get location => GoRouteData.$location(
+        '/string-route/${Uri.encodeComponent(requiredStringFieldField)}',
+        queryParams: {
+          if (stringFieldField != null) 'string-field-field': stringFieldField!,
+        },
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+}
+
+extension $UriRouteExtension on UriRoute {
+  static UriRoute _fromState(GoRouterState state) => UriRoute(
+        requiredUriFieldField:
+            Uri.parse(state.params['requiredUriFieldField']!),
+        uriFieldField:
+            _$convertMapValue('uri-field-field', state.queryParams, Uri.parse),
+      );
+
+  String get location => GoRouteData.$location(
+        '/uri-route/${Uri.encodeComponent(requiredUriFieldField.toString())}',
+        queryParams: {
+          if (uriFieldField != null)
+            'uri-field-field': uriFieldField!.toString(),
         },
       );
 
