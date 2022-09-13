950a40f42f50e89740b6d2019c310c6bed052933
Greg Spencer <gspencergoog@users.noreply.github.com>
2022-09-12 20:15:39 -0700

    [adaptive_scaffold] Clean up `adaptive_scaffold` package (#2558)

--- packages/adaptive_scaffold/example/lib/adaptive_scaffold_demo.dart
+++ packages/adaptive_scaffold/example/lib/adaptive_scaffold_demo.dart
@@ -6,11 +6,13 @@ import 'package:adaptive_scaffold/adaptive_scaffold.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const _MyApp());
+  runApp(const MyApp());
 }
 
-class _MyApp extends StatelessWidget {
-  const _MyApp({Key? key}) : super(key: key);
+/// The main application widget for this example.
+class MyApp extends StatelessWidget {
+  /// Creates a const main application widget.
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +24,7 @@ class _MyApp extends StatelessWidget {
 /// [AdaptiveScaffold].
 class MyHomePage extends StatelessWidget {
   /// Creates a const [MyHomePage].
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   // #docregion Example
   @override
@@ -31,16 +33,20 @@ class MyHomePage extends StatelessWidget {
     final List<Widget> children = <Widget>[
       for (int i = 0; i < 10; i++)
         Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-                color: const Color.fromARGB(255, 255, 201, 197), height: 400))
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            color: const Color.fromARGB(255, 255, 201, 197),
+            height: 400,
+          ),
+        )
     ];
 
     return BottomNavigationBarTheme(
         data: const BottomNavigationBarThemeData(
-            unselectedItemColor: Colors.black,
-            selectedItemColor: Colors.black,
-            backgroundColor: Colors.white),
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.black,
+          backgroundColor: Colors.white,
+        ),
         child: AdaptiveScaffold(
             // An option to override the default breakpoints used for small, medium,
             // and large.
@@ -59,14 +65,15 @@ class MyHomePage extends StatelessWidget {
             ],
             body: (_) => GridView.count(crossAxisCount: 2, children: children),
             smallBody: (_) => ListView.builder(
-                itemCount: children.length,
-                itemBuilder: (_, int idx) => children[idx]),
+                  itemCount: children.length,
+                  itemBuilder: (_, int idx) => children[idx],
+                ),
             // Define a default secondaryBody.
             secondaryBody: (_) =>
                 Container(color: const Color.fromARGB(255, 234, 158, 192)),
             // Override the default secondaryBody during the smallBreakpoint to be
             // empty. Must use AdaptiveScaffold.emptyBuilder to ensure it is properly
-            // overriden.
+            // overridden.
             smallSecondaryBody: AdaptiveScaffold.emptyBuilder));
   }
   // #enddocregion
