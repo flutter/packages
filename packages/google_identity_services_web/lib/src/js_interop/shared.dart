0e16392fe7dcc6fdae916efdeca8aba13a1456e3
David Iglesias Teixeira <ditman@gmail.com>
2022-09-22 15:22:51 -0700

    Add oauth2 library to js-interop

--- packages/google_identity_services_web/lib/src/js_interop/shared.dart
+++ packages/google_identity_services_web/lib/src/js_interop/shared.dart
@@ -5,3 +5,27 @@
 /// The type of several functions from the library, that don't receive
 /// parameters nor return anything.
 typedef VoidFn = void Function();
+
+/*
+// Enum: UX Mode
+// https://developers.google.com/identity/gsi/web/reference/js-reference#ux_mode
+// Used both by `oauth2.initCodeClient` and `id.initialize`.
+*/
+
+/// Use this enum to set the UX flow used by the Sign In With Google button.
+/// The default value is [popup].
+///
+/// This attribute has no impact on the OneTap UX.
+enum UxMode {
+  /// Performs sign-in UX flow in a pop-up window.
+  popup('popup'),
+  /// Performs sign-in UX flow by a full page redirection.
+  redirect('redirect');
+
+  ///
+  const UxMode(String uxMode) : _uxMode = uxMode;
+  final String _uxMode;
+
+  @override
+  String toString() => _uxMode;
+}
