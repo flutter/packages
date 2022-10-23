313fb223e474fed576e7c249f7bf5208f29a7d96
a-wallen <stephenwallen@google.com>
2022-10-22 15:36:55 -1000

    Refactor  test infra

---
+++ packages/flutter_adaptive_scaffold/test/test_breakpoints.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_adaptive_scaffold/src/breakpoints.dart';
+
+class TestBreakpoint0 extends Breakpoint {
+  @override
+  bool isActive(BuildContext context) {
+    return MediaQuery.of(context).size.width >= 0 &&
+        MediaQuery.of(context).size.width < 800;
+  }
+}
+
+class TestBreakpoint400 extends Breakpoint {
+  @override
+  bool isActive(BuildContext context) {
+    return MediaQuery.of(context).size.width > 400;
+  }
+}
+
+class TestBreakpoint800 extends Breakpoint {
+  @override
+  bool isActive(BuildContext context) {
+    return MediaQuery.of(context).size.width >= 800 &&
+        MediaQuery.of(context).size.width < 1000;
+  }
+}
+
+class TestBreakpoint1000 extends Breakpoint {
+  @override
+  bool isActive(BuildContext context) {
+    return MediaQuery.of(context).size.width >= 1000;
+  }
+}
+
+class NeverOnBreakpoint extends Breakpoint {
+  @override
+  bool isActive(BuildContext context) {
+    return false;
+  }
+}
