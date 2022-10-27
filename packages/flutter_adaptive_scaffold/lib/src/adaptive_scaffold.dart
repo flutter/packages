d37dfc2989a2c0c2debda68938d208696239c613
Peter Keefe <16809252+percula@users.noreply.github.com>
2022-10-27 15:37:44 -0400

    fix: Make drawer items scrollable

--- packages/flutter_adaptive_scaffold/lib/src/adaptive_scaffold.dart
+++ packages/flutter_adaptive_scaffold/lib/src/adaptive_scaffold.dart
@@ -478,13 +478,24 @@ class _AdaptiveScaffoldState extends State<AdaptiveScaffold> {
             : null,
         drawer: widget.drawerBreakpoint.isActive(context) && widget.useDrawer
             ? Drawer(
-                child: NavigationRail(
-                  extended: true,
-                  selectedIndex: widget.selectedIndex,
-                  destinations: widget.destinations
-                      .map((_) => AdaptiveScaffold.toRailDestination(_))
-                      .toList(),
-                  onDestinationSelected: widget.onSelectedIndexChange,
+                child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    return SingleChildScrollView(
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                        child: IntrinsicHeight(
+                          child: NavigationRail(
+                            extended: true,
+                            selectedIndex: widget.selectedIndex,
+                            destinations: widget.destinations
+                                .map((_) => AdaptiveScaffold.toRailDestination(_))
+                                .toList(),
+                            onDestinationSelected: widget.onSelectedIndexChange,
+                          ),
+                        ),
+                      ),
+                    );
+                  }
                 ),
               )
             : null,
