859ad144d29155137ca22bbe2788d56b6e171cb4
Stuart Morgan <stuartmorgan@google.com>
2022-11-11 15:49:39 -0500

    Autoformat

--- packages/pigeon/platform_tests/alternate_language_test_plugin/example/android/app/src/main/java/com/example/alternate_language_test_plugin_example/MainActivity.java
+++ packages/pigeon/platform_tests/alternate_language_test_plugin/example/android/app/src/main/java/com/example/alternate_language_test_plugin_example/MainActivity.java
@@ -6,5 +6,4 @@ package com.example.alternate_language_test_plugin_example;
 
 import io.flutter.embedding.android.FlutterActivity;
 
-public class MainActivity extends FlutterActivity {
-}
+public class MainActivity extends FlutterActivity {}
