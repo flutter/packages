579af61b78faaeaf9f480d2b0515a93445f58292
Mirko Mucaria <skogsfrae@gmail.com>
2022-11-29 14:22:01 +0100

    Revert "temporary fix for analyzer ci, will be fixed in go_router_builder code generator"
    This reverts commit 6ae5026c0df976c6003b1e4aee10ed95c6601783.

--- packages/go_router_builder/example/lib/all_types.g.dart
+++ packages/go_router_builder/example/lib/all_types.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-// ignore_for_file: always_specify_types, public_member_api_docs, unnecessary_null_checks
+// ignore_for_file: always_specify_types, public_member_api_docs
 
 part of 'all_types.dart';
 
