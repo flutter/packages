1d6e5f8e57351cc54f7571f24826033b16bf494a
Stuart Morgan <stuartmorgan@google.com>
2022-12-01 10:11:01 -0500

    Remove legacy e2e tests

--- packages/pigeon/e2e_tests/test_objc/ios/RunnerTests/RunnerTests.m
+++
@@ -1,8 +0,0 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#import <XCTest/XCTest.h>
-#import <e2e/E2EIosTest.h>
-
-E2E_IOS_RUNNER(RunnerTests)
--- packages/pigeon/run_tests.sh
+++ packages/pigeon/run_tests.sh
@@ -224,32 +224,6 @@ run_ios_legacy_unittests() {
   popd
 }
 
-run_ios_e2e_legacy_tests() {
-  DARTLE_H="e2e_tests/test_objc/ios/Runner/dartle.h"
-  DARTLE_M="e2e_tests/test_objc/ios/Runner/dartle.m"
-  DARTLE_DART="e2e_tests/test_objc/lib/dartle.dart"
-  $run_pigeon \
-    --input pigeons/message.dart \
-    --dart_out $DARTLE_DART \
-    --objc_header_out $DARTLE_H \
-    --objc_source_out $DARTLE_M \
-  dart format $DARTLE_DART
-
-  pushd $PWD
-  cd e2e_tests/test_objc
-  flutter build ios -t test_driver/e2e_test.dart --simulator
-  # TODO(gaaclarke): Transition to integration_test. `e2e` has been deprecated
-  # and has stopped working.
-  # cd ios
-  # xcodebuild \
-  #   -workspace Runner.xcworkspace \
-  #   -scheme RunnerTests \
-  #   -sdk iphonesimulator \
-  #   -destination 'platform=iOS Simulator,name=iPhone 8' \
-  #   test
-  popd
-}
-
 run_ios_objc_e2e_tests() {
   dart run tool/run_tests.dart -t ios_objc_integration_tests --skip-generation
 }
@@ -276,7 +250,6 @@ should_run_android_unittests=true
 should_run_dart_compilation_tests=true
 should_run_dart_unittests=true
 should_run_flutter_unittests=true
-should_run_ios_e2e_legacy_tests=true
 # TODO(stuartmorgan): Enable by default once CI issues are solved; see
 # https://github.com/flutter/packages/pull/2816.
 should_run_ios_objc_e2e_tests=false
@@ -307,7 +280,6 @@ while getopts "t:l?h" opt; do
     should_run_flutter_unittests=false
     should_run_ios_objc_unittests=false
     should_run_ios_objc_e2e_tests=false
-    should_run_ios_e2e_legacy_tests=false
     should_run_ios_legacy_unittests=false
     should_run_ios_swift_unittests=false
     should_run_ios_swift_e2e_tests=false
@@ -324,7 +296,6 @@ while getopts "t:l?h" opt; do
     dart_compilation_tests) should_run_dart_compilation_tests=true ;;
     dart_unittests) should_run_dart_unittests=true ;;
     flutter_unittests) should_run_flutter_unittests=true ;;
-    ios_e2e_legacy_tests) should_run_ios_e2e_legacy_tests=true ;;
     ios_objc_e2e_tests) should_run_ios_objc_e2e_tests=true ;;
     ios_objc_unittests) should_run_ios_objc_unittests=true ;;
     ios_unittests) should_run_ios_legacy_unittests=true ;;
@@ -350,7 +321,6 @@ while getopts "t:l?h" opt; do
   dart_compilation_tests   - Compilation tests on generated Dart code.
   dart_unittests           - Unit tests on and analysis on Pigeon's implementation.
   flutter_unittests        - Unit tests on generated Dart code.
-  ios_e2e_legacy_tests     - Legacy end-to-end Obj-C tests; build-only.
   ios_objc_unittests       - Unit tests on generated Obj-C code.
   ios_unittests            - Legacy unit tests on generated Obj-C code. Use ios_objc_unittests instead.
   ios_objc_e2e_tests       - Integration tests on generated Obj-C code.
@@ -411,9 +381,6 @@ if [ "$should_run_ios_swift_unittests" = true ]; then
 fi
 if [ "$should_run_ios_swift_e2e_tests" = true ]; then
   run_ios_swift_e2e_tests
-fi
-if [ "$should_run_ios_e2e_legacy_tests" = true ]; then
-  run_ios_e2e_legacy_tests
 fi
 if [ "$should_run_ios_objc_e2e_tests" = true ]; then
   run_ios_objc_e2e_tests
