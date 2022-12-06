54dbcd72cc3697569808f7d7b716d8b03fb5e1a9
stuartmorgan <stuartmorgan@google.com>
2022-12-06 14:25:11 -0500

    [pigeon] Add initial FlutterApi integration tests (#2901)
    * Add trivial async test as a baseline for the scaffolding
    
    * Kotlin generator async void fix
    
    * Add initial round-trip tests
    
    * Windows fixes
    
    * Cleanup
    
    * Version bump for bug fix
    
    * Fix analysis
    
    * Version bump in generator

--- packages/pigeon/platform_tests/alternate_language_test_plugin/android/src/main/java/com/example/alternate_language_test_plugin/AlternateLanguageTestPlugin.java
+++ packages/pigeon/platform_tests/alternate_language_test_plugin/android/src/main/java/com/example/alternate_language_test_plugin/AlternateLanguageTestPlugin.java
@@ -8,14 +8,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.example.alternate_language_test_plugin.CoreTests.AllTypes;
 import com.example.alternate_language_test_plugin.CoreTests.AllTypesWrapper;
+import com.example.alternate_language_test_plugin.CoreTests.FlutterIntegrationCoreApi;
 import com.example.alternate_language_test_plugin.CoreTests.HostIntegrationCoreApi;
+import com.example.alternate_language_test_plugin.CoreTests.Result;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 
 /** This plugin handles the native side of the integration tests in example/integration_test/. */
 public class AlternateLanguageTestPlugin implements FlutterPlugin, HostIntegrationCoreApi {
+  @Nullable FlutterIntegrationCoreApi flutterApi = null;
+
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
     HostIntegrationCoreApi.setup(binding.getBinaryMessenger(), this);
+    flutterApi = new FlutterIntegrationCoreApi(binding.getBinaryMessenger());
   }
 
   @Override
@@ -45,5 +50,36 @@ public class AlternateLanguageTestPlugin implements FlutterPlugin, HostIntegrati
   public @NonNull AllTypesWrapper createNestedString(@NonNull String string) {
     AllTypes innerObject = new AllTypes.Builder().setAString(string).build();
     return new AllTypesWrapper.Builder().setValues(innerObject).build();
+  }
+
+  @Override
+  public void noopAsync(Result<Void> result) {
+    result.success(null);
+  }
+
+  @Override
+  public void echoAsyncString(@NonNull String aString, Result<String> result) {
+    result.success(aString);
+  }
+
+  @Override
+  public void callFlutterNoop(Result<Void> result) {
+    flutterApi.noop(
+        new FlutterIntegrationCoreApi.Reply<Void>() {
+          public void reply(Void value) {
+            result.success(value);
+          }
+        });
+  }
+
+  @Override
+  public void callFlutterEchoString(@NonNull String aString, Result<String> result) {
+    flutterApi.echoString(
+        aString,
+        new FlutterIntegrationCoreApi.Reply<String>() {
+          public void reply(String value) {
+            result.success(value);
+          }
+        });
   }
 }
