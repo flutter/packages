508c1822f7a33075d74f4980a8d8b4a7249f4e0c
tarrinneal <tarrinneal@gmail.com>
2022-12-19 18:00:13 -0800

    start

---
+++ packages/pigeon/lib/generator.dart
@@ -0,0 +1,39 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'pigeon_lib.dart';
+
+/// Generator class that handles all generation.
+class Generator {
+  /// Constructor for Generator.
+  const Generator(this.pigeonOptions);
+
+  /// Options for any generator being called.
+  final PigeonOptions pigeonOptions;
+
+  /// Method for generation of pigeon files.
+  void generate() {
+    //
+  }
+
+  /// Writes a class.
+  void writeClass() {
+    //
+  }
+
+  /// Writes the encode (or toList) method for a class.
+  void writeEncode() {
+    //
+  }
+
+  /// Writes the decode (or fromList) method for a class.
+  void writeDecode() {
+    //
+  }
+
+  /// Writes an api.
+  void writeApi() {
+    //
+  }
+}
