16df2079a618b4bbdc1076d741fe9ddcb3dbecbc
tarrinneal <tarrinneal@gmail.com>
2022-12-22 10:19:50 -0800

    header -> source

--- packages/pigeon/test/objc_generator_test.dart
+++ packages/pigeon/test/objc_generator_test.dart
@@ -107,7 +107,7 @@ void main() {
       ],
     );
     final StringBuffer sink = StringBuffer();
-    final ObjcHeaderGenerator generator = ObjcHeaderGenerator();
+    final ObjcSourceGenerator generator = ObjcSourceGenerator();
     generator.generate(const ObjcOptions(header: 'foo.h'), root, sink);
     final String code = sink.toString();
     expect(code, contains('#import "foo.h"'));
