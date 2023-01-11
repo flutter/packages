22878a2b37bfbe79d3af1e182e9791c7ecbe71e6
Stuart Morgan <stuartmorgan@google.com>
2023-01-11 13:31:21 -0500

    Version bump

--- packages/pigeon/lib/generator_tools.dart
+++ packages/pigeon/lib/generator_tools.dart
@@ -9,7 +9,7 @@ import 'dart:mirrors';
 import 'ast.dart';
 
 /// The current version of pigeon. This must match the version in pubspec.yaml.
-const String pigeonVersion = '5.0.0';
+const String pigeonVersion = '5.0.1';
 
 /// Read all the content from [stdin] to a String.
 String readStdin() {
