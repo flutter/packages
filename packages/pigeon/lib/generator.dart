7dc64d31c3956d2526e6ca61bfce63474774f112
tarrinneal <tarrinneal@gmail.com>
2023-01-13 08:30:56 -0800

    [indent]

--- packages/pigeon/lib/generator.dart
+++ packages/pigeon/lib/generator.dart
@@ -1,6 +1,6 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE [indent].
+// found in the LICENSE file.
 
 import 'ast.dart';
 import 'generator_tools.dart';
