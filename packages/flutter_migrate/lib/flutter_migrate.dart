53a7b7a021599a896e4bb352d83a495d5627e748
Gary Qian <garyq@google.com>
2023-01-18 15:14:32 -0500

    Export library symbols

---
+++ packages/flutter_migrate/lib/flutter_migrate.dart
@@ -0,0 +1,13 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// A library to render markdown formatted text.
+library flutter_migrate;
+
+export 'src/base_dependencies.dart';
+export 'src/base/command.dart';
+export 'src/commands/abandon.dart';
+export 'src/commands/apply.dart';
+export 'src/commands/start.dart';
+export 'src/commands/status.dart';
