932b2fc04dcec1e68e842f761f5157e7d056eb9c
Gary Qian <garyq@google.com>
2023-01-18 15:39:20 -0500

    Remove extra doc

--- packages/flutter_migrate/lib/flutter_migrate.dart
+++ packages/flutter_migrate/lib/flutter_migrate.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// A library to render markdown formatted text.
 library flutter_migrate;
 
 export 'src/base_dependencies.dart';
