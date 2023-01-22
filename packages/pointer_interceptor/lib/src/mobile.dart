47d85f7aacad991ab4c0c694c61e9802c7493228
David Iglesias <ditman@gmail.com>
2023-01-21 17:41:49 -0800

    Remove extra whitespace

--- packages/pointer_interceptor/lib/src/mobile.dart
+++ packages/pointer_interceptor/lib/src/mobile.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 import 'package:flutter/widgets.dart';
 
 /// A [Widget] that prevents clicks from being swallowed by [HtmlElementView]s.
