f000ec6b6c996ffa2a8dbb5dfa2e1b7c046c2275
tarrinneal <tarrinneal@gmail.com>
2023-02-02 18:17:29 -0800

    undo deleting all lines with comments

--- packages/pigeon/platform_tests/test_plugin/windows/test_plugin.h
+++ packages/pigeon/platform_tests/test_plugin/windows/test_plugin.h
@@ -1,3 +1,6 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef FLUTTER_PLUGIN_TEST_PLUGIN_H_
 #define FLUTTER_PLUGIN_TEST_PLUGIN_H_
@@ -13,6 +16,8 @@
 
 namespace test_plugin {
 
+// This plugin handles the native side of the integration tests in
+// example/integration_test/
 class TestPlugin : public flutter::Plugin,
                    public core_tests_pigeontest::HostIntegrationCoreApi {
  public:
@@ -22,9 +27,11 @@ class TestPlugin : public flutter::Plugin,
 
   virtual ~TestPlugin();
 
+  // Disallow copy and assign.
   TestPlugin(const TestPlugin&) = delete;
   TestPlugin& operator=(const TestPlugin&) = delete;
 
+  // HostIntegrationCoreApi.
   std::optional<core_tests_pigeontest::FlutterError> Noop() override;
   core_tests_pigeontest::ErrorOr<core_tests_pigeontest::AllTypes> EchoAllTypes(
       const core_tests_pigeontest::AllTypes& everything) override;
@@ -265,3 +272,7 @@ class TestPlugin : public flutter::Plugin,
   std::unique_ptr<core_tests_pigeontest::FlutterIntegrationCoreApi>
       flutter_api_;
 };
+
+}  // namespace test_plugin
+
+#endif  // FLUTTER_PLUGIN_TEST_PLUGIN_H_
