a660db0e3ef87dd1d40c2fff539ea9074476459e
Stuart Morgan <stuartmorgan@google.com>
2023-02-07 15:45:55 -0500

    try running everything, with process_runner

--- packages/pigeon/tool/shared/process_utils.dart
+++ packages/pigeon/tool/shared/process_utils.dart
@@ -2,34 +2,29 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-import 'dart:io' show Process, stderr, stdout;
+// ignore_for_file: avoid_print
 
-Future<Process> _streamOutput(Future<Process> processFuture) async {
-  final Process process = await processFuture;
-  await Future.wait(<Future<Object?>>[
-    stdout.addStream(process.stdout),
-    stderr.addStream(process.stderr),
-  ]);
-  return process;
-}
+import 'dart:io' show Directory;
+
+import 'package:process_runner/process_runner.dart';
 
 Future<int> runProcess(String command, List<String> arguments,
     {String? workingDirectory,
     bool streamOutput = true,
     bool logFailure = false}) async {
-  final Future<Process> future = Process.start(
-    command,
-    arguments,
-    workingDirectory: workingDirectory,
-  );
-  final Process process = await (streamOutput ? _streamOutput(future) : future);
-  final int exitCode = await process.exitCode;
-  if (exitCode != 0 && logFailure) {
-    // ignore: avoid_print
-    print('$command $arguments failed:');
-    process.stdout.pipe(stdout);
-    process.stderr.pipe(stderr);
+  final ProcessRunner runner = ProcessRunner();
+  final ProcessRunnerResult result = await runner.runProcess(
+      <String>[command, ...arguments],
+      workingDirectory:
+          workingDirectory == null ? null : Directory(workingDirectory),
+      failOk: true,
+      printOutput: streamOutput);
+  if (result.exitCode != 0 && logFailure) {
+    print('$command $arguments failed.');
+    print('stderr:');
+    print(result.stderr);
+    print('stdout:');
+    print(result.stdout);
   }
-  return exitCode;
+  return result.exitCode;
 }
