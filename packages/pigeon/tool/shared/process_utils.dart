2963d9ca221b8a43a36556927a2843b190f48f93
Stuart Morgan <stuartmorgan@google.com>
2023-02-07 10:16:44 -0500

    print logging

--- packages/pigeon/tool/shared/process_utils.dart
+++ packages/pigeon/tool/shared/process_utils.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: avoid_print
+
 import 'dart:async';
 import 'dart:io' show Process, stderr, stdout;
 
@@ -18,15 +20,18 @@ Future<int> runProcess(String command, List<String> arguments,
     {String? workingDirectory,
     bool streamOutput = true,
     bool logFailure = false}) async {
+  print('Starting process');
   final Future<Process> future = Process.start(
     command,
     arguments,
     workingDirectory: workingDirectory,
   );
+  print('Waiting for process');
   final Process process = await (streamOutput ? _streamOutput(future) : future);
+  print('Waiting for exit');
   final int exitCode = await process.exitCode;
+  print('Done waiting');
   if (exitCode != 0 && logFailure) {
-    // ignore: avoid_print
     print('$command $arguments failed:');
     process.stdout.pipe(stdout);
     process.stderr.pipe(stderr);
