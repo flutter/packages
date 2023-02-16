87efaa4aa55575d04f5807984600349d552e2f26
Stuart Morgan <stuartmorgan@google.com>
2023-02-16 10:34:59 -0500

    Remove some unnecesasry/duplicate unit tests

--- packages/pigeon/platform_tests/alternate_language_test_plugin/android/src/test/java/com/example/alternate_language_test_plugin/PigeonTest.java
+++ packages/pigeon/platform_tests/alternate_language_test_plugin/android/src/test/java/com/example/alternate_language_test_plugin/PigeonTest.java
@@ -17,30 +17,6 @@ import org.mockito.ArgumentCaptor;
 
 public class PigeonTest {
   @Test
-  public void toListAndBack() {
-    Pigeon.AndroidSetRequest request = new Pigeon.AndroidSetRequest();
-    request.setValue(1234l);
-    request.setState(Pigeon.AndroidLoadingState.COMPLETE);
-    ArrayList<Object> list = request.toList();
-    Pigeon.AndroidSetRequest readRequest = Pigeon.AndroidSetRequest.fromList(list);
-    assertEquals(request.getValue(), readRequest.getValue());
-    assertEquals(request.getState(), readRequest.getState());
-  }
-
-  @Test
-  public void toListAndBackNested() {
-    Pigeon.AndroidNestedRequest nested = new Pigeon.AndroidNestedRequest();
-    Pigeon.AndroidSetRequest request = new Pigeon.AndroidSetRequest();
-    request.setValue(1234l);
-    request.setState(Pigeon.AndroidLoadingState.COMPLETE);
-    nested.setRequest(request);
-    ArrayList<Object> list = nested.toList();
-    Pigeon.AndroidNestedRequest readNested = Pigeon.AndroidNestedRequest.fromList(list);
-    assertEquals(nested.getRequest().getValue(), readNested.getRequest().getValue());
-    assertEquals(nested.getRequest().getState(), readNested.getRequest().getState());
-  }
-
-  @Test
   public void clearsHandler() {
     Pigeon.AndroidApi mockApi = mock(Pigeon.AndroidApi.class);
     BinaryMessenger binaryMessenger = mock(BinaryMessenger.class);
@@ -76,45 +52,5 @@ public class PigeonTest {
               assertTrue(details.contains("Cause:"));
               assertTrue(details.contains("Stacktrace:"));
             });
-  }
-
-  @Test
-  public void callsVoidMethod() {
-    Pigeon.AndroidApi mockApi = mock(Pigeon.AndroidApi.class);
-    BinaryMessenger binaryMessenger = mock(BinaryMessenger.class);
-    Pigeon.AndroidApi.setup(binaryMessenger, mockApi);
-    ArgumentCaptor<BinaryMessenger.BinaryMessageHandler> handler =
-        ArgumentCaptor.forClass(BinaryMessenger.BinaryMessageHandler.class);
-    verify(binaryMessenger).setMessageHandler(anyString(), handler.capture());
-    Pigeon.AndroidSetRequest request = new Pigeon.AndroidSetRequest();
-    request.setValue(1234l);
-    request.setState(Pigeon.AndroidLoadingState.COMPLETE);
-    MessageCodec<Object> codec = Pigeon.AndroidApi.getCodec();
-    ByteBuffer message = codec.encodeMessage(new ArrayList<Object>(Arrays.asList(request)));
-    message.rewind();
-    handler
-        .getValue()
-        .onMessage(
-            message,
-            (bytes) -> {
-              bytes.rewind();
-              @SuppressWarnings("unchecked")
-              ArrayList wrapped = (ArrayList) codec.decodeMessage(bytes);
-              assertTrue(wrapped.size() == 1);
-              assertNull(wrapped.get(0));
-            });
-    ArgumentCaptor<Pigeon.AndroidSetRequest> receivedRequest =
-        ArgumentCaptor.forClass(Pigeon.AndroidSetRequest.class);
-    verify(mockApi).setValue(receivedRequest.capture());
-    assertEquals(request.getValue(), receivedRequest.getValue().getValue());
-  }
-
-  @Test
-  public void encodeWithNullField() {
-    Pigeon.AndroidNestedRequest request = new Pigeon.AndroidNestedRequest();
-    request.setContext("hello");
-    MessageCodec<Object> codec = Pigeon.AndroidNestedApi.getCodec();
-    ByteBuffer message = codec.encodeMessage(request);
-    assertNotNull(message);
   }
 }
