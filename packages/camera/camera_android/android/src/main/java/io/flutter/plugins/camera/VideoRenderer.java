ea0637956c660f7fc0ff1a4657c9ae6748d4ea53
BradenBagby <bradenjbagby@gmail.com>
2023-02-27 10:47:33 -0700

    logs warning instead of throwing a fatal error if eglswapbuffer fails in thread

--- packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/VideoRenderer.java
+++ packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/VideoRenderer.java
@@ -358,8 +358,7 @@ public class VideoRenderer {
 
     EGLExt.eglPresentationTimeANDROID(display, surface, uptimeMillis() * 1000000);
     if (!EGL14.eglSwapBuffers(display, surface)) {
-      throw new RuntimeException(
-          "eglSwapBuffers()" + GLUtils.getEGLErrorString(EGL14.eglGetError()));
+        Log.w(TAG, "eglSwapBuffers() " + GLUtils.getEGLErrorString(EGL14.eglGetError()));
     }
   }
 }
