93d10957610c1ac6d0951bcfaa6e9c422179e76f
camsim99 <camillesimon90@gmail.com>
2023-02-27 15:25:20 -0800

    Fix copy

--- packages/camera/camera_android_camerax/android/src/main/java/io/flutter/plugins/camerax/InstanceManager.java
+++ packages/camera/camera_android_camerax/android/src/main/java/io/flutter/plugins/camerax/InstanceManager.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package com.example.wrapper_example;
+package io.flutter.plugins.camerax;
 
 import android.os.Handler;
 import android.os.Looper;
--- packages/camera/camera_android_camerax/android/src/test/java/io/flutter/plugins/camerax/InstanceManagerTest.java
+++ packages/camera/camera_android_camerax/android/src/test/java/io/flutter/plugins/camerax/InstanceManagerTest.java
@@ -1,4 +1,8 @@
-package com.example.wrapper_example;
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.camerax;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
