fe6f1fcfd96a48108fe881198f98c5169e07e11a
camsim99 <camillesimon90@gmail.com>
2023-03-01 17:02:16 -0800

    Fix analyze

--- packages/camera/camera_android_camerax/lib/src/image_capture.dart
+++ packages/camera/camera_android_camerax/lib/src/image_capture.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:camera_platform_interface/camera_platform_interface.dart'
-    show CameraException;
 import 'package:flutter/services.dart' show BinaryMessenger;
 
 import 'camerax_library.g.dart';
