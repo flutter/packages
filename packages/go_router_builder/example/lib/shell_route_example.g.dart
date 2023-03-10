ce95e76d1720c4173c7eea5e9c5cd797c4d95d95
GP4cK <gautier.bayzelon@gmail.com>
2023-03-10 10:53:21 +0800

    Add example

---
+++ packages/go_router_builder/example/lib/shell_route_example.g.dart
@@ -0,0 +1,62 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: always_specify_types, public_member_api_docs
+
+part of 'shell_route_example.dart';
+
+// **************************************************************************
+// GoRouterShellGenerator
+// **************************************************************************
+
+List<RouteBase> get $appRoutes => [
+      $myShellRouteData,
+    ];
+
+RouteBase get $myShellRouteData => ShellRouteData.$route(
+      factory: $MyShellRouteDataExtension._fromState,
+      routes: [
+        GoRouteData.$route(
+          path: '/foo',
+          factory: $FooRouteDataExtension._fromState,
+        ),
+        GoRouteData.$route(
+          path: '/bar',
+          factory: $BarRouteDataExtension._fromState,
+        ),
+      ],
+    );
+
+extension $MyShellRouteDataExtension on MyShellRouteData {
+  static MyShellRouteData _fromState(GoRouterState state) =>
+      const MyShellRouteData();
+}
+
+extension $FooRouteDataExtension on FooRouteData {
+  static FooRouteData _fromState(GoRouterState state) => const FooRouteData();
+
+  String get location => GoRouteData.$location(
+        '/foo',
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location, extra: this);
+}
+
+extension $BarRouteDataExtension on BarRouteData {
+  static BarRouteData _fromState(GoRouterState state) => const BarRouteData();
+
+  String get location => GoRouteData.$location(
+        '/bar',
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location, extra: this);
+}
