ebced85029eebeeeb89c04d7503487a47f608227
Viktor Malyi <viktor.malyi@simpleclub.com>
2023-03-14 13:08:50 +0100

    Revert "Update video_player.dart"
    This reverts commit e107320ca8412bb86c5987297f5df19e4ee4b64f.

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -382,6 +382,7 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
           _applyLooping();
           _applyVolume();
           _applyPlayPause();
+          _applyPlaybackSpeed();
           break;
         case VideoEventType.completed:
           // In this case we need to stop _timer, set isPlaying=false, and
@@ -416,8 +417,6 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
         initializingCompleter.completeError(obj);
       }
     }
-
-    _applyPlaybackSpeed();
 
     _eventSubscription = _videoPlayerPlatform
         .videoEventsFor(_textureId)
