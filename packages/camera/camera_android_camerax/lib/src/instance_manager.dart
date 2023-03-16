ba4541702f6b2760e84a73f14d6d7b411d1d7aa1
camsim99 <camillesimon90@gmail.com>
2023-03-16 15:03:06 -0700

    Fix analyzer

--- packages/camera/camera_android_camerax/lib/src/instance_manager.dart
+++ packages/camera/camera_android_camerax/lib/src/instance_manager.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/foundation.dart';
-
 /// Maintains instances used to communicate with the native objects they
 /// represent.
 ///
