9b886f8ceabd2d426e9d9c4764faada94736375e
Reid Baker <reidbaker@google.com>
2023-03-17 15:01:25 -0400

    Remove tests that were not being run on main, Rename MainActivityIntegrationTest to MainActivityTest to match existing documentation

--- packages/espresso/example/android/app/src/androidTest/java/io/flutter/com/example/espresso_example/MainActivityTest.java
+++ packages/espresso/example/android/app/src/androidTest/java/io/flutter/com/example/espresso_example/MainActivityTest.java
@@ -4,73 +4,16 @@
 
 package com.example.espresso_example;
 
-import static androidx.test.espresso.flutter.EspressoFlutter.onFlutterWidget;
-import static androidx.test.espresso.flutter.action.FlutterActions.click;
-import static androidx.test.espresso.flutter.action.FlutterActions.syntheticClick;
-import static androidx.test.espresso.flutter.assertion.FlutterAssertions.matches;
-import static androidx.test.espresso.flutter.matcher.FlutterMatchers.withText;
-import static androidx.test.espresso.flutter.matcher.FlutterMatchers.withTooltip;
-import static androidx.test.espresso.flutter.matcher.FlutterMatchers.withValueKey;
-import static com.google.common.truth.Truth.assertThat;
-
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.flutter.EspressoFlutter.WidgetInteraction;
-import androidx.test.espresso.flutter.assertion.FlutterAssertions;
-import androidx.test.espresso.flutter.matcher.FlutterMatchers;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import org.junit.Before;
-import org.junit.Test;
+import androidx.test.rule.ActivityTestRule;
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
+import io.flutter.plugins.DartIntegrationTest;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 
-/** Unit tests for {@link EspressoFlutter}. */
-@RunWith(AndroidJUnit4.class)
+@DartIntegrationTest
+@RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
-
-  @Before
-  public void setUp() throws Exception {
-    ActivityScenario.launch(MainActivity.class);
-  }
-
-  @Test
-  public void performTripleClick() {
-    WidgetInteraction interaction =
-        onFlutterWidget(withTooltip("Increment")).perform(click(), click()).perform(click());
-    assertThat(interaction).isNotNull();
-    onFlutterWidget(withValueKey("CountText")).check(matches(withText("Button tapped 3 times.")));
-  }
-
-  @Test
-  public void performClick() {
-    WidgetInteraction interaction = onFlutterWidget(withTooltip("Increment")).perform(click());
-    assertThat(interaction).isNotNull();
-    onFlutterWidget(withValueKey("CountText")).check(matches(withText("Button tapped 1 time.")));
-  }
-
-  @Test
-  public void performSyntheticClick() {
-    WidgetInteraction interaction =
-        onFlutterWidget(withTooltip("Increment")).perform(syntheticClick());
-    assertThat(interaction).isNotNull();
-    onFlutterWidget(withValueKey("CountText")).check(matches(withText("Button tapped 1 time.")));
-  }
-
-  @Test
-  public void performTwiceSyntheticClicks() {
-    WidgetInteraction interaction =
-        onFlutterWidget(withTooltip("Increment")).perform(syntheticClick(), syntheticClick());
-    assertThat(interaction).isNotNull();
-    onFlutterWidget(withValueKey("CountText")).check(matches(withText("Button tapped 2 times.")));
-  }
-
-  @Test
-  public void isIncrementButtonExists() {
-    onFlutterWidget(FlutterMatchers.withTooltip("Increment"))
-        .check(FlutterAssertions.matches(FlutterMatchers.isExisting()));
-  }
-
-  @Test
-  public void isAppBarExists() {
-    onFlutterWidget(FlutterMatchers.withType("AppBar"))
-        .check(FlutterAssertions.matches(FlutterMatchers.isExisting()));
-  }
+  @Rule
+  public ActivityTestRule<MainActivity> rule =
+      new ActivityTestRule<>(MainActivity.class, true, false);
 }
