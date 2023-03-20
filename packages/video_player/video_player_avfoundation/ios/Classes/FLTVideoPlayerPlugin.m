910c797fe4aab2fa402e71ef5763305c4399457d
Viktor Malyi <v.stratus@gmail.com>
2023-03-20 13:13:52 +0100

    Add test statement

--- packages/video_player/video_player_avfoundation/ios/Classes/FLTVideoPlayerPlugin.m
+++ packages/video_player/video_player_avfoundation/ios/Classes/FLTVideoPlayerPlugin.m
@@ -662,4 +662,9 @@ NS_INLINE UIViewController *rootViewController() {
   }
 }
 
+- (void)viktorTest:(FLTTextureMessage *)input error:(FlutterError **)error {
+  FLTVideoPlayer *player = self.playersByTextureId[input.textureId];
+  [player pause];
+}
+
 @end
