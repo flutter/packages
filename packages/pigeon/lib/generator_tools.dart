1cac25da9d9d3903e3c96d9b2dbf8131ec9644b0
stuartmorgan <stuartmorgan@google.com>
2023-04-03 12:52:35 -0700

    [pigeon] Take C++ out of experimental (#3603)
    [pigeon] Take C++ out of experimental

--- packages/pigeon/lib/generator_tools.dart
+++ packages/pigeon/lib/generator_tools.dart
@@ -11,7 +11,7 @@ import 'ast.dart';
 /// The current version of pigeon.
 ///
 /// This must match the version in pubspec.yaml.
-const String pigeonVersion = '9.1.4';
+const String pigeonVersion = '9.2.0';
 
 /// Read all the content from [stdin] to a String.
 String readStdin() {
