a6901deae144be6ae5874a4ee89ab1235db16945
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2023-04-18 16:06:54 -0400

    make point values nullable

--- packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/features/Point.java
+++ packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/features/Point.java
@@ -4,14 +4,14 @@
 
 package io.flutter.plugins.camera.features;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 /** Represents a point on an x/y axis. */
 public class Point {
   public final Double x;
   public final Double y;
 
-  public Point(@NonNull Double x, @NonNull Double y) {
+  public Point(@Nullable Double x, @Nullable Double y) {
     this.x = x;
     this.y = y;
   }
