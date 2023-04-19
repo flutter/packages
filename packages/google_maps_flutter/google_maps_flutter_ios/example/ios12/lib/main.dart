59a8caa8f70abe019bba4fc8029c0e2c1a9d63f8
Stuart Morgan <stuartmorgan@google.com>
2023-04-19 16:23:09 -0400

    Share Dart code

--- packages/google_maps_flutter/google_maps_flutter_ios/example/ios12/lib/main.dart
+++ packages/google_maps_flutter/google_maps_flutter_ios/example/ios12/lib/main.dart
@@ -3,71 +3,41 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-
-import 'animate_camera.dart';
-import 'lite_mode.dart';
-import 'map_click.dart';
-import 'map_coordinates.dart';
-import 'map_ui.dart';
-import 'marker_icons.dart';
-import 'move_camera.dart';
-import 'padding.dart';
-import 'page.dart';
-import 'place_circle.dart';
-import 'place_marker.dart';
-import 'place_polygon.dart';
-import 'place_polyline.dart';
-import 'scrolling_map.dart';
-import 'snapshot.dart';
-import 'tile_overlay.dart';
-
-final List<GoogleMapExampleAppPage> _allPages = <GoogleMapExampleAppPage>[
-  const MapUiPage(),
-  const MapCoordinatesPage(),
-  const MapClickPage(),
-  const AnimateCameraPage(),
-  const MoveCameraPage(),
-  const PlaceMarkerPage(),
-  const MarkerIconsPage(),
-  const ScrollingMapPage(),
-  const PlacePolylinePage(),
-  const PlacePolygonPage(),
-  const PlaceCirclePage(),
-  const PaddingPage(),
-  const SnapshotPage(),
-  const LiteModePage(),
-  const TileOverlayPage(),
-];
-
-/// MapsDemo is the Main Application.
-class MapsDemo extends StatelessWidget {
-  /// Default Constructor
-  const MapsDemo({super.key});
-
-  void _pushPage(BuildContext context, GoogleMapExampleAppPage page) {
-    Navigator.of(context).push(MaterialPageRoute<void>(
-        builder: (_) => Scaffold(
-              appBar: AppBar(title: Text(page.title)),
-              body: page,
-            )));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('GoogleMaps examples')),
-      body: ListView.builder(
-        itemCount: _allPages.length,
-        itemBuilder: (_, int index) => ListTile(
-          leading: _allPages[index].leading,
-          title: Text(_allPages[index].title),
-          onTap: () => _pushPage(context, _allPages[index]),
-        ),
-      ),
-    );
-  }
-}
+import 'package:maps_example_dart/animate_camera.dart';
+import 'package:maps_example_dart/lite_mode.dart';
+import 'package:maps_example_dart/map_click.dart';
+import 'package:maps_example_dart/map_coordinates.dart';
+import 'package:maps_example_dart/map_ui.dart';
+import 'package:maps_example_dart/maps_demo.dart';
+import 'package:maps_example_dart/marker_icons.dart';
+import 'package:maps_example_dart/move_camera.dart';
+import 'package:maps_example_dart/padding.dart';
+import 'package:maps_example_dart/page.dart';
+import 'package:maps_example_dart/place_circle.dart';
+import 'package:maps_example_dart/place_marker.dart';
+import 'package:maps_example_dart/place_polygon.dart';
+import 'package:maps_example_dart/place_polyline.dart';
+import 'package:maps_example_dart/scrolling_map.dart';
+import 'package:maps_example_dart/snapshot.dart';
+import 'package:maps_example_dart/tile_overlay.dart';
 
 void main() {
-  runApp(const MaterialApp(home: MapsDemo()));
+  runApp(const MaterialApp(
+      home: MapsDemo(<GoogleMapExampleAppPage>[
+    MapUiPage(),
+    MapCoordinatesPage(),
+    MapClickPage(),
+    AnimateCameraPage(),
+    MoveCameraPage(),
+    PlaceMarkerPage(),
+    MarkerIconsPage(),
+    ScrollingMapPage(),
+    PlacePolylinePage(),
+    PlacePolygonPage(),
+    PlaceCirclePage(),
+    PaddingPage(),
+    SnapshotPage(),
+    LiteModePage(),
+    TileOverlayPage(),
+  ])));
 }
