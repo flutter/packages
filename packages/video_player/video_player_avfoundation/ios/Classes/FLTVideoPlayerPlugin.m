7d167e22b9d62efb88b4653e8f4df7b7bc7ad7a8
tarrinneal <tarrinneal@gmail.com>
2023-04-20 16:57:40 -0700

    tolerance only when seeking to duration

--- packages/video_player/video_player_avfoundation/ios/Classes/FLTVideoPlayerPlugin.m
+++ packages/video_player/video_player_avfoundation/ios/Classes/FLTVideoPlayerPlugin.m
@@ -420,9 +420,19 @@ NS_INLINE UIViewController *rootViewController() {
 }
 
 - (void)seekTo:(int)location completionHandler:(void (^)(BOOL))completionHandler {
-  [_player seekToTime:CMTimeMake(location, 1000)
-        toleranceBefore:CMTimeMake(1, 1000)
-         toleranceAfter:CMTimeMake(1, 1000)
+    CMTime locationCMT = CMTimeMake(location, 1000);
+    CMTimeValue duration = _player.currentItem.asset.duration.value;
+    CMTimeValue locationCM = locationCMT.value;
+    if (duration == locationCM) {
+        [_player seekToTime:_player.currentItem.currentTime
+            toleranceBefore:CMTimeMake(1, 1000)
+             toleranceAfter:CMTimeMake(1, 1000)
+            completionHandler:completionHandler];
+        return;
+    }
+  [_player seekToTime:locationCMT
+        toleranceBefore:kCMTimeZero
+         toleranceAfter:kCMTimeZero
       completionHandler:completionHandler];
 }
 
