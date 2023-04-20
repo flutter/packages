4b767e3acab5c899f7ecaaf91202710691bc2b53
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2023-04-20 10:03:00 -0400

    fix test methods

--- packages/webview_flutter/webview_flutter_android/android/src/test/java/io/flutter/plugins/webviewflutter/GeolocationPermissionsCallbackTest.java
+++ packages/webview_flutter/webview_flutter_android/android/src/test/java/io/flutter/plugins/webviewflutter/GeolocationPermissionsCallbackTest.java
@@ -34,12 +34,12 @@ public class GeolocationPermissionsCallbackTest {
 
   @Before
   public void setUp() {
-    instanceManager = InstanceManager.open(identifier -> {});
+    instanceManager = InstanceManager.create(identifier -> {});
   }
 
   @After
   public void tearDown() {
-    instanceManager.close();
+    instanceManager.stopFinalizationListener();
   }
 
   @Test
