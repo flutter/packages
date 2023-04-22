a719034cb458faa2a66fa9be94f8f53160454927
camsim99 <camillesimon90@gmail.com>
2023-04-22 10:31:45 -0700

    Fix analyze

--- packages/camera/camera_android_camerax/example/lib/main.dart
+++ packages/camera/camera_android_camerax/example/lib/main.dart
@@ -276,10 +276,7 @@ class _CameraExampleHomeState extends State<CameraExampleHome>
             IconButton(
               icon: const Icon(Icons.flash_on),
               color: Colors.blue,
-              // TODO(camsim99): Remove this test to land this PR.
-              onPressed: () {
-                controller!.startImageStream((image) => print(image));
-              }, // TODO(camsim99): Add functionality back here.
+              onPressed: () {}, // TODO(camsim99): Add functionality back here.
             ),
             // The exposure and focus mode are currently not supported on the web.
             ...!kIsWeb
