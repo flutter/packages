3e88d03a166069b0a2687d131b15c72dde126105
stuartmorgan <stuartmorgan@google.com>
2023-04-25 19:11:12 -0700

    [tool] Move Android lint checks (#3816)
    Moves the checks for Android warning configuration from `lint-android`, where it made sense to put them at the time, to the new `check-gradle`, which is a newer command specifically for validating that our Gradle files are following best practices.
    
    Makes minor changes to the Pigeon platform test projects to make them conform to the checks, since they are now included in the run. The changes shouldn't actually change the behavior of the Pigeon tests.

--- script/tool/test/gradle_check_command_test.dart
+++ script/tool/test/gradle_check_command_test.dart
@@ -6,6 +6,7 @@ import 'package:args/command_runner.dart';
 import 'package:file/file.dart';
 import 'package:file/memory.dart';
 import 'package:flutter_plugin_tools/src/common/core.dart';
+import 'package:flutter_plugin_tools/src/common/plugin_utils.dart';
 import 'package:flutter_plugin_tools/src/gradle_check_command.dart';
 import 'package:test/test.dart';
 
@@ -31,25 +32,28 @@ void main() {
     runner.addCommand(command);
   });
 
-  void writeFakeBuildGradle(
+  /// Writes a fake android/build.gradle file for plugin [package] with the
+  /// given options.
+  void writeFakePluginBuildGradle(
     RepositoryPackage package, {
-    bool isApp = false,
     bool includeLanguageVersion = false,
     bool includeSourceCompat = false,
     bool commentSourceLanguage = false,
     bool includeNamespace = true,
     bool commentNamespace = false,
+    bool warningsConfigured = true,
   }) {
-    final Directory androidDir =
-        package.platformDirectory(FlutterPlatform.android);
-    final Directory parentDir =
-        isApp ? androidDir.childDirectory('app') : androidDir;
-    final File buildGradle = parentDir.childFile('build.gradle');
+    final File buildGradle = package
+        .platformDirectory(FlutterPlatform.android)
+        .childFile('build.gradle');
     buildGradle.createSync(recursive: true);
 
-    final String compileOptionsSection = '''
-    compileOptions {
-        ${commentSourceLanguage ? '// ' : ''}sourceCompatibility JavaVersion.VERSION_1_8
+    const String warningConfig = '''
+    lintOptions {
+        checkAllWarnings true
+        warningsAsErrors true
+        disable 'AndroidGradlePluginVersion', 'InvalidPackage', 'GradleDependency'
+        baseline file("lint-baseline.xml")
     }
 ''';
     final String javaSection = '''
@@ -59,6 +63,11 @@ java {
     }
 }
 
+''';
+    final String compileOptionsSection = '''
+    compileOptions {
+        ${commentSourceLanguage ? '// ' : ''}sourceCompatibility JavaVersion.VERSION_1_8
+    }
 ''';
     final String namespace =
         "${commentNamespace ? '// ' : ''}namespace '$_defaultFakeNamespace'";
@@ -84,19 +93,135 @@ android {
     defaultConfig {
         minSdkVersion 30
     }
-    lintOptions {
-        checkAllWarnings true
-    }
+${warningsConfigured ? warningConfig : ''}
+${includeSourceCompat ? compileOptionsSection : ''}
     testOptions {
         unitTests.includeAndroidResources = true
     }
-${includeSourceCompat ? compileOptionsSection : ''}
 }
 
 dependencies {
     implementation 'fake.package:fake:1.0.0'
 }
 ''');
+  }
+
+  /// Writes a fake android/build.gradle file for an example [package] with the
+  /// given options.
+  void writeFakeExampleTopLevelBuildGradle(
+    RepositoryPackage package, {
+    required String pluginName,
+    required bool warningsConfigured,
+  }) {
+    final File buildGradle = package
+        .platformDirectory(FlutterPlatform.android)
+        .childFile('build.gradle');
+    buildGradle.createSync(recursive: true);
+
+    final String warningConfig = '''
+gradle.projectsEvaluated {
+    project(":$pluginName") {
+        tasks.withType(JavaCompile) {
+            options.compilerArgs << "-Xlint:all" << "-Werror"
+        }
+    }
+}
+''';
+    buildGradle.writeAsStringSync('''
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath 'fake.package:fake:1.0.0'
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.buildDir = '../build'
+subprojects {
+    project.buildDir = "\${rootProject.buildDir}/\${project.name}"
+}
+subprojects {
+    project.evaluationDependsOn(':app')
+}
+
+task clean(type: Delete) {
+    delete rootProject.buildDir
+}
+
+${warningsConfigured ? warningConfig : ''}
+''');
+  }
+
+  /// Writes a fake android/app/build.gradle file for an example [package] with
+  /// the given options.
+  void writeFakeExampleAppBuildGradle(
+    RepositoryPackage package, {
+    required bool includeNamespace,
+    required bool commentNamespace,
+  }) {
+    final File buildGradle = package
+        .platformDirectory(FlutterPlatform.android)
+        .childDirectory('app')
+        .childFile('build.gradle');
+    buildGradle.createSync(recursive: true);
+
+    final String namespace =
+        "${commentNamespace ? '// ' : ''}namespace '$_defaultFakeNamespace'";
+    buildGradle.writeAsStringSync('''
+def flutterRoot = localProperties.getProperty('flutter.sdk')
+if (flutterRoot == null) {
+    throw new GradleException("Flutter SDK not found. Define location with flutter.sdk in the local.properties file.")
+}
+
+apply plugin: 'com.android.application'
+apply from: "\$flutterRoot/packages/flutter_tools/gradle/flutter.gradle"
+
+android {
+    ${includeNamespace ? namespace : ''}
+    compileSdkVersion flutter.compileSdkVersion
+
+    lintOptions {
+        disable 'InvalidPackage'
+    }
+
+    defaultConfig {
+        applicationId "io.flutter.plugins.cameraexample"
+        minSdkVersion 21
+        targetSdkVersion 28
+    }
+}
+
+flutter {
+    source '../..'
+}
+
+dependencies {
+    testImplementation 'fake.package:fake:1.0.0'
+}
+''');
+  }
+
+  void writeFakeExampleBuildGradles(
+    RepositoryPackage package, {
+    required String pluginName,
+    bool includeNamespace = true,
+    bool commentNamespace = false,
+    bool warningsConfigured = true,
+  }) {
+    writeFakeExampleTopLevelBuildGradle(package,
+        pluginName: pluginName, warningsConfigured: warningsConfigured);
+    writeFakeExampleAppBuildGradle(package,
+        includeNamespace: includeNamespace, commentNamespace: commentNamespace);
   }
 
   void writeFakeManifest(
@@ -136,7 +261,7 @@ dependencies {
   test('fails when build.gradle has no java compatibility version', () async {
     final RepositoryPackage package =
         createFakePlugin('a_plugin', packagesDir, examples: <String>[]);
-    writeFakeBuildGradle(package);
+    writeFakePluginBuildGradle(package);
     writeFakeManifest(package);
 
     Error? commandError;
@@ -158,7 +283,7 @@ dependencies {
   test('passes when sourceCompatibility is specified', () async {
     final RepositoryPackage package =
         createFakePlugin('a_plugin', packagesDir, examples: <String>[]);
-    writeFakeBuildGradle(package, includeSourceCompat: true);
+    writeFakePluginBuildGradle(package, includeSourceCompat: true);
     writeFakeManifest(package);
 
     final List<String> output =
@@ -175,7 +300,7 @@ dependencies {
   test('passes when toolchain languageVersion is specified', () async {
     final RepositoryPackage package =
         createFakePlugin('a_plugin', packagesDir, examples: <String>[]);
-    writeFakeBuildGradle(package, includeLanguageVersion: true);
+    writeFakePluginBuildGradle(package, includeLanguageVersion: true);
     writeFakeManifest(package);
 
     final List<String> output =
@@ -190,11 +315,12 @@ dependencies {
   });
 
   test('does not require java version in examples', () async {
-    final RepositoryPackage package = createFakePlugin('a_plugin', packagesDir);
-    writeFakeBuildGradle(package, includeLanguageVersion: true);
+    const String pluginName = 'a_plugin';
+    final RepositoryPackage package = createFakePlugin(pluginName, packagesDir);
+    writeFakePluginBuildGradle(package, includeLanguageVersion: true);
     writeFakeManifest(package);
     final RepositoryPackage example = package.getExamples().first;
-    writeFakeBuildGradle(example, isApp: true);
+    writeFakeExampleBuildGradles(example, pluginName: pluginName);
     writeFakeManifest(example, isApp: true);
 
     final List<String> output =
@@ -212,7 +338,7 @@ dependencies {
   test('fails when java compatibility version is commented out', () async {
     final RepositoryPackage package =
         createFakePlugin('a_plugin', packagesDir, examples: <String>[]);
-    writeFakeBuildGradle(package,
+    writeFakePluginBuildGradle(package,
         includeSourceCompat: true, commentSourceLanguage: true);
     writeFakeManifest(package);
 
@@ -235,7 +361,7 @@ dependencies {
   test('fails when languageVersion is commented out', () async {
     final RepositoryPackage package =
         createFakePlugin('a_plugin', packagesDir, examples: <String>[]);
-    writeFakeBuildGradle(package,
+    writeFakePluginBuildGradle(package,
         includeLanguageVersion: true, commentSourceLanguage: true);
     writeFakeManifest(package);
 
@@ -259,7 +385,7 @@ dependencies {
       () async {
     final RepositoryPackage package =
         createFakePlugin('a_plugin', packagesDir, examples: <String>[]);
-    writeFakeBuildGradle(package, includeLanguageVersion: true);
+    writeFakePluginBuildGradle(package, includeLanguageVersion: true);
     writeFakeManifest(package, packageName: 'wrong.package.name');
 
     Error? commandError;
@@ -281,7 +407,7 @@ dependencies {
   test('fails when namespace is missing', () async {
     final RepositoryPackage package =
         createFakePlugin('a_plugin', packagesDir, examples: <String>[]);
-    writeFakeBuildGradle(package,
+    writeFakePluginBuildGradle(package,
         includeLanguageVersion: true, includeNamespace: false);
     writeFakeManifest(package);
 
@@ -301,12 +427,13 @@ dependencies {
   });
 
   test('fails when namespace is missing from example', () async {
-    final RepositoryPackage package = createFakePlugin('a_plugin', packagesDir);
-    writeFakeBuildGradle(package, includeLanguageVersion: true);
+    const String pluginName = 'a_plugin';
+    final RepositoryPackage package = createFakePlugin(pluginName, packagesDir);
+    writeFakePluginBuildGradle(package, includeLanguageVersion: true);
     writeFakeManifest(package);
     final RepositoryPackage example = package.getExamples().first;
-    writeFakeBuildGradle(example,
-        isApp: true, includeLanguageVersion: true, includeNamespace: false);
+    writeFakeExampleBuildGradles(example,
+        pluginName: pluginName, includeNamespace: false);
     writeFakeManifest(example, isApp: true);
 
     Error? commandError;
@@ -330,11 +457,12 @@ dependencies {
   // don't just accidentally diverge.
   test('fails when namespace in example does not match AndroidManifest.xml',
       () async {
-    final RepositoryPackage package = createFakePlugin('a_plugin', packagesDir);
-    writeFakeBuildGradle(package, includeLanguageVersion: true);
+    const String pluginName = 'a_plugin';
+    final RepositoryPackage package = createFakePlugin(pluginName, packagesDir);
+    writeFakePluginBuildGradle(package, includeLanguageVersion: true);
     writeFakeManifest(package);
     final RepositoryPackage example = package.getExamples().first;
-    writeFakeBuildGradle(example, isApp: true, includeLanguageVersion: true);
+    writeFakeExampleBuildGradles(example, pluginName: pluginName);
     writeFakeManifest(example, isApp: true, packageName: 'wrong.package.name');
 
     Error? commandError;
@@ -356,7 +484,7 @@ dependencies {
   test('fails when namespace is commented out', () async {
     final RepositoryPackage package =
         createFakePlugin('a_plugin', packagesDir, examples: <String>[]);
-    writeFakeBuildGradle(package,
+    writeFakePluginBuildGradle(package,
         includeLanguageVersion: true, commentNamespace: true);
     writeFakeManifest(package);
 
@@ -373,5 +501,86 @@ dependencies {
         contains('build.gradle must set a "namespace"'),
       ]),
     );
+  });
+
+  test('fails if gradle-driven lint-warnings-as-errors is missing', () async {
+    const String pluginName = 'a_plugin';
+    final RepositoryPackage plugin =
+        createFakePlugin(pluginName, packagesDir, examples: <String>[]);
+    writeFakePluginBuildGradle(plugin,
+        includeLanguageVersion: true, warningsConfigured: false);
+    writeFakeManifest(plugin);
+
+    Error? commandError;
+    final List<String> output = await runCapturingPrint(
+        runner, <String>['gradle-check'], errorHandler: (Error e) {
+      commandError = e;
+    });
+
+    expect(commandError, isA<ToolExit>());
+    expect(
+        output,
+        containsAllInOrder(
+          <Matcher>[
+            contains('This package is not configured to enable all '
+                'Gradle-driven lint warnings and treat them as errors.'),
+            contains('The following packages had errors:'),
+          ],
+        ));
+  });
+
+  test('fails if plugin example javac lint-warnings-as-errors is missing',
+      () async {
+    const String pluginName = 'a_plugin';
+    final RepositoryPackage plugin = createFakePlugin(pluginName, packagesDir,
+        platformSupport: <String, PlatformDetails>{
+          platformAndroid: const PlatformDetails(PlatformSupport.inline),
+        });
+    writeFakePluginBuildGradle(plugin, includeLanguageVersion: true);
+    writeFakeManifest(plugin);
+    final RepositoryPackage example = plugin.getExamples().first;
+    writeFakeExampleBuildGradles(example,
+        pluginName: pluginName, warningsConfigured: false);
+    writeFakeManifest(example, isApp: true);
+
+    Error? commandError;
+    final List<String> output = await runCapturingPrint(
+        runner, <String>['gradle-check'], errorHandler: (Error e) {
+      commandError = e;
+    });
+
+    expect(commandError, isA<ToolExit>());
+    expect(
+        output,
+        containsAllInOrder(
+          <Matcher>[
+            contains('The example "example" is not configured to treat javac '
+                'lints and warnings as errors.'),
+            contains('The following packages had errors:'),
+          ],
+        ));
+  });
+
+  test(
+      'passes if non-plugin package example javac lint-warnings-as-errors is missing',
+      () async {
+    const String packageName = 'a_package';
+    final RepositoryPackage plugin =
+        createFakePackage(packageName, packagesDir);
+    final RepositoryPackage example = plugin.getExamples().first;
+    writeFakeExampleBuildGradles(example,
+        pluginName: packageName, warningsConfigured: false);
+    writeFakeManifest(example, isApp: true);
+
+    final List<String> output =
+        await runCapturingPrint(runner, <String>['gradle-check']);
+
+    expect(
+        output,
+        containsAllInOrder(
+          <Matcher>[
+            contains('Validating android/build.gradle'),
+          ],
+        ));
   });
 }
