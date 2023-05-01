445462e53a09bb491db6534e20ce15461914822b
camsim99 <camillesimon90@gmail.com>
2023-05-01 16:34:43 -0700

    Merge remote-tracking branch 'upstream/main' into camx_occ

--- packages/camera/camera_android_camerax/test/camera_info_test.mocks.dart
+++ packages/camera/camera_android_camerax/test/camera_info_test.mocks.dart
@@ -54,6 +54,14 @@ class MockTestCameraInfoHostApi extends _i1.Mock
       ) as int);
 
   @override
+  int getLiveCameraState(int? identifier) => (super.noSuchMethod(
+        Invocation.method(
+          #getLiveCameraState,
+          [identifier],
+        ),
+        returnValue: 0,
+      ) as int);
+  @override
   int getExposureState(int? identifier) => (super.noSuchMethod(
         Invocation.method(
           #getExposureState,
