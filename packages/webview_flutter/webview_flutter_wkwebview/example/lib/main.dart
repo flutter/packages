af72995ab51f4530a31863604f254e3caa560364
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2023-05-02 13:25:11 -0400

    fix dev uri

--- packages/webview_flutter/webview_flutter_wkwebview/example/lib/main.dart
+++ packages/webview_flutter/webview_flutter_wkwebview/example/lib/main.dart
@@ -147,7 +147,7 @@ Page resource error:
         },
       )
       ..loadRequest(LoadRequestParams(
-        uri: Uri.parse('https://flutter..dev'),
+        uri: Uri.parse('https://flutter.dev'),
       ));
   }
 
