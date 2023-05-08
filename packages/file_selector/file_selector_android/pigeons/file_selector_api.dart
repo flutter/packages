017ed72155638610960f37037af6f704afe9c93d
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2023-05-08 17:47:54 -0400

    implement pigeon files

---
+++ packages/file_selector/file_selector_android/pigeons/file_selector_api.dart
@@ -0,0 +1,39 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(
+  PigeonOptions(
+    dartOut: 'lib/src/file_selector_api.g.dart',
+    dartTestOut: 'test/test_file_selector_api.g.dart',
+    dartOptions: DartOptions(copyrightHeader: <String>[
+      'Copyright 2013 The Flutter Authors. All rights reserved.',
+      'Use of this source code is governed by a BSD-style license that can be',
+      'found in the LICENSE file.',
+    ]),
+    javaOut:
+        'android/src/main/java/dev/flutter/packages/file_selector_android/GeneratedFileSelectorApi.java',
+    javaOptions: JavaOptions(
+      package: 'dev.flutter.packages.file_selector_android',
+      className: 'GeneratedFileSelectorApi',
+      // copyrightHeader: <String>[
+      //   'Copyright 2013 The Flutter Authors. All rights reserved.',
+      //   'Use of this source code is governed by a BSD-style license that can be',
+      //   'found in the LICENSE file.',
+      // ],
+    ),
+    copyrightHeader: 'pigeons/copyright.txt',
+  ),
+)
+@HostApi(dartHostTestHandler: 'TestFileSelectorApi')
+abstract class FileSelectorApi {
+  @async
+  String? openFile(String? initialDirectory, List<String?>? mimeTypes);
+
+  @async
+  List<String?> openFiles(String? initialDirectory, List<String?>? mimeTypes);
+
+  @async
+  Future<String?> getDirectoryPath(String? initialDirectory);
+
+  @async
+  Future<List<String>> getDirectoryPaths(String? initialDirectory);
+}
