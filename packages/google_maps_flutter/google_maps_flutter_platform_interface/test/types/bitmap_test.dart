20749f438895103ebff2a6107d3572d2e4f45154
David Iglesias Teixeira <ditman@gmail.com>
2023-05-22 18:36:06 -0700

    [maps_platform] Remove unnecessary imports.

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/test/types/bitmap_test.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/test/types/bitmap_test.dart
@@ -1,11 +1,6 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-// ignore:unnecessary_import
-import 'dart:typed_data';
-import 'dart:ui';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
