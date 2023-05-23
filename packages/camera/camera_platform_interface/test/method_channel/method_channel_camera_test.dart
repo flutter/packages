8e6981e318d7ac469f687459dc1da6ac7cfb1e16
Stuart Morgan <stuartmorgan@google.com>
2023-05-23 15:05:04 -0400

    Fix camera tests

--- packages/camera/camera_platform_interface/test/method_channel/method_channel_camera_test.dart
+++ packages/camera/camera_platform_interface/test/method_channel/method_channel_camera_test.dart
@@ -1085,7 +1085,7 @@ void main() {
           isMethodCall('startImageStream', arguments: null),
         ]);
 
-        subscription.cancel();
+        await subscription.cancel();
       });
 
       test('Should stop streaming', () async {
@@ -1102,7 +1102,7 @@ void main() {
         final StreamSubscription<CameraImageData> subscription = camera
             .onStreamedFrameAvailable(cameraId)
             .listen((CameraImageData imageData) {});
-        subscription.cancel();
+        await subscription.cancel();
 
         // Assert
         expect(channel.log, <Matcher>[
