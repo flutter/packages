35262a48f3fa5877dd9f66a53011e09434f01ba9
stuartmorgan <stuartmorgan@google.com>
2023-06-09 20:27:10 -0400

    [image_picker] Add desktop support (#3882)
    (This is the combination PR for overall review.)
    
    This adds initial desktop support across all three desktop platforms:
    - Adds the concept of a camera delegation as discussed in https://github.com/flutter/flutter/issues/102115, and adds a helper base class at the platform interface level for implementations that want to use that approach. (Sharing it simplifies both the platform implementations and the client usage.)
    - Adds a new app-facing and platform-interface API to check at runtime for support for a given source, to make writing code using `image_picker` in a world of dynamic camera support viable. (In particular this is critical for published packages that use `image_picker`, as they won't know in advance if a client app will have a delegate set up).
    - Updates the Windows implementation to:
      - use the new delegation base class
      - implement the newer `getImageFromSource` API as an opportunistic fix while changing the code (if not implemented, it would fall back to `getImage`)
      - use new APIs in its example.
    - Made macOS and Linux implementations that are clones of the Windows approach. They are both slightly simpler however, as MIME type (for Linux) and UTI (for macOS) allow creating generic "image" and "video" filters instead of having to hard-code a file extension list.
    
    Since in my opinion this level of support is sufficient to allow basic real-world use of the desktop implementations, due to having a structure in place for handling camera support, this includes endorsing the desktop implementations. It is still the case that desktop does not support image resizing, which isn't ideal, but I don't think we should block on that. It is clearly documented in the README.
    
    The desktop implementations are separate packages even though they are mostly the same right now because:
    - the amount of (non-example) code being duplicated is small, and
    - it makes it much easier for us to change platform implementations over time; e.g., recent versions of macOS support `PHPicker`, meaning we can later add native code for that to the macOS implementation, with just a fallback to the current `file_selector` wrapper. It's plausible that better native options for Windows and/or Linux may become available in the future as well.
    
    Fixes https://github.com/flutter/flutter/issues/102115
    Fixes https://github.com/flutter/flutter/issues/102320
    Fixes https://github.com/flutter/flutter/issues/85100

--- packages/image_picker/image_picker/example/lib/readme_excerpts.dart
+++ packages/image_picker/image_picker/example/lib/readme_excerpts.dart
@@ -4,6 +4,28 @@
 
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+// #docregion CameraDelegate
+import 'package:image_picker_platform_interface/image_picker_platform_interface.dart';
+// #enddocregion CameraDelegate
+
+/// Example of a camera delegate
+// #docregion CameraDelegate
+class MyCameraDelegate extends ImagePickerCameraDelegate {
+  @override
+  Future<XFile?> takePhoto(
+      {ImagePickerCameraDelegateOptions options =
+          const ImagePickerCameraDelegateOptions()}) async {
+    return _takeAPhoto(options.preferredCameraDevice);
+  }
+
+  @override
+  Future<XFile?> takeVideo(
+      {ImagePickerCameraDelegateOptions options =
+          const ImagePickerCameraDelegateOptions()}) async {
+    return _takeAVideo(options.preferredCameraDevice);
+  }
+}
+// #enddocregion CameraDelegate
 
 /// Example function for README demonstration of various pick* calls.
 Future<List<XFile?>> readmePickExample() async {
@@ -49,6 +71,20 @@ Future<void> getLostData() async {
 }
 // #enddocregion LostData
 
+/// Example of camera delegate setup.
+// #docregion CameraDelegate
+void setUpCameraDelegate() {
+  final ImagePickerPlatform instance = ImagePickerPlatform.instance;
+  if (instance is CameraDelegatingImagePickerPlatform) {
+    instance.cameraDelegate = MyCameraDelegate();
+  }
+}
+// #enddocregion CameraDelegate
+
 // Stubs for the getLostData function.
 void _handleLostFiles(List<XFile> file) {}
 void _handleError(PlatformException? exception) {}
+
+// Stubs for MyCameraDelegate.
+Future<XFile?> _takeAPhoto(CameraDevice device) async => null;
+Future<XFile?> _takeAVideo(CameraDevice device) async => null;
