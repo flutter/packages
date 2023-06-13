9827799b50e5e1afcf30c3c559094e9c11448446
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2023-06-13 15:24:22 -0400

    license

--- packages/file_selector/file_selector_android/example/android/app/src/main/java/dev/flutter/packages/file_selector_android_example/TestContentProvider.java
+++ packages/file_selector/file_selector_android/example/android/app/src/main/java/dev/flutter/packages/file_selector_android_example/TestContentProvider.java
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package dev.flutter.packages.file_selector_android_example;
 
 import android.content.ContentProvider;
