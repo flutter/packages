d5f664475a734c4ecbe7c086aca0d01a04b4985e
tim Hoogstrate <>
2023-06-21 14:51:10 +0200

    made some updates regarding the video player

--- packages/video_player/video_player/test/video_player_test.dart
+++ packages/video_player/video_player/test/video_player_test.dart
@@ -68,9 +68,6 @@ class FakeController extends ValueNotifier<VideoPlayerValue>
   Future<void> setLooping(bool looping) async {}
 
   @override
-  Future<void> clearCache(bool clear) async {}
-
-  @override
   VideoFormat? get formatHint => null;
 
   @override
