e4e6951b70d17063476f981567a3cf700b377f04
Stuart Morgan <stuartmorgan@google.com>
2023-06-21 11:00:42 -0400

    Adjust template-generated files, replace test plugin

--- packages/pigeon/platform_tests/alternate_language_test_plugin/example/macos/RunnerTests/RunnerTests.swift
+++ packages/pigeon/platform_tests/alternate_language_test_plugin/example/macos/RunnerTests/RunnerTests.swift
@@ -4,24 +4,8 @@ import XCTest
 
 @testable import alternate_language_test_plugin
 
-// This demonstrates a simple unit test of the Swift portion of this plugin's implementation.
-//
-// See https://developer.apple.com/documentation/xctest for more information about using XCTest.
-
 class RunnerTests: XCTestCase {
-
-  func testGetPlatformVersion() {
-    let plugin = AlternateLanguageTestPlugin()
-
-    let call = FlutterMethodCall(methodName: "getPlatformVersion", arguments: [])
-
-    let resultExpectation = expectation(description: "result block must be called.")
-    plugin.handle(call) { result in
-      XCTAssertEqual(result as! String,
-                     "macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
-      resultExpectation.fulfill()
-    }
-    waitForExpectations(timeout: 1)
-  }
-
+  // Currently there are no macOS Objective-C unit tests, since the expectation
+  // is that the iOS Objective-C unit tests provide sufficient native unit test
+  // coverage. If macOS-specific issue come up, they can be tested here.
 }
