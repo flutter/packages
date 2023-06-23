ffb5e27989e2b18548675ecd0608f49b9b05acd2
tim Hoogstrate <>
2023-06-23 14:11:00 +0200

    rebase

--- packages/video_player/video_player_avfoundation/example/lib/main.dart
+++ packages/video_player/video_player_avfoundation/example/lib/main.dart
@@ -114,8 +114,6 @@ class _BumbleBeeRemoteVideo extends StatefulWidget {
 
 class _BumbleBeeRemoteVideoState extends State<_BumbleBeeRemoteVideo> {
   late MiniController _controller;
-  String networkMediaUrl =
-      'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4';
 
   @override
   void initState() {
