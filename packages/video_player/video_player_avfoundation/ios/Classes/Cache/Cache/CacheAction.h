493184c68b794d908f2b4e77659e660b4e138648
tim Hoogstrate <>
2023-07-06 15:53:41 +0200

    Added caching to obj-c

---
+++ packages/video_player/video_player_avfoundation/ios/Classes/Cache/Cache/CacheAction.h
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, CacheActionType) {
+    CacheActionTypeLocal = 0,
+    CacheActionTypeRemote
+};
+
+@interface CacheAction : NSObject
+
+- (instancetype)initWithActionType:(CacheActionType)actionType range:(NSRange)range;
+
+@property (nonatomic) CacheActionType actionType;
+@property (nonatomic) NSRange range;
+
+@end
---
+++ packages/video_player/video_player_avfoundation/ios/Classes/Cache/ResourceLoader/ContentInfo.m
@@ -0,0 +1,30 @@
+
+#import "ContentInfo.h"
+
+static NSString *kContentLengthKey = @"kContentLengthKey";
+static NSString *kContentTypeKey = @"kContentTypeKey";
+static NSString *kByteRangeAccessSupported = @"kByteRangeAccessSupported";
+
+@implementation ContentInfo
+
+- (NSString *)debugDescription {
+    return [NSString stringWithFormat:@"%@\ncontentLength: %lld\ncontentType: %@\nbyteRangeAccessSupported:%@", NSStringFromClass([self class]), self.contentLength, self.contentType, @(self.byteRangeAccessSupported)];
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [aCoder encodeObject:@(self.contentLength) forKey:kContentLengthKey];
+    [aCoder encodeObject:self.contentType forKey:kContentTypeKey];
+    [aCoder encodeObject:@(self.byteRangeAccessSupported) forKey:kByteRangeAccessSupported];
+}
+
+- (nullable instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super init];
+    if (self) {
+        _contentLength = [[aDecoder decodeObjectForKey:kContentLengthKey] longLongValue];
+        _contentType = [aDecoder decodeObjectForKey:kContentTypeKey];
+        _byteRangeAccessSupported = [[aDecoder decodeObjectForKey:kByteRangeAccessSupported] boolValue];
+    }
+    return self;
+}
+
+@end
