e3589911e283866bbc810fe55e733971dd9fc909
Nitin <nitinpoojary00@gmail.com>
2023-07-08 13:37:43 +0530

    specified type for widgetTester in integration_test

--- packages/flutter_image/example/integration_test/main_test.dart
+++ packages/flutter_image/example/integration_test/main_test.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_image_example/main.dart' as example;
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets(
     'example works',
-    (widgetTester) async {
+    (WidgetTester widgetTester) async {
       await widgetTester.pumpWidget(const example.MyApp());
 
       await widgetTester.pumpAndSettle();
