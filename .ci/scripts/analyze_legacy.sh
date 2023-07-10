b4c3424fb021d251b01af6d1dcf44d66b1c5be12
stuartmorgan <stuartmorgan@google.com>
2023-07-10 18:23:25 -0400

    [ci] Add initial LUCI legacy analysis (#4428)
    Adds new legacy analysis targets to LUCI in bringup mode.
    
    Also updates the repo tooling to use a Flutter->Dart version mapping instead of having to separately maintain a Dart version, now that we already have such a map in the tool anyway.
    
    Part of https://github.com/flutter/flutter/issues/114373

--- .ci/scripts/analyze_legacy.sh
+++ .ci/scripts/analyze_legacy.sh
@@ -4,15 +4,20 @@
 # found in the LICENSE file.
 set -e
 
-# Pathify the dependencies on changed packages (excluding major version
-# changes, which won't affect clients).
-./script/tool_runner.sh make-deps-path-based --target-dependencies-with-non-breaking-updates
+# Allow analyzing packages that use a dev dependency with a higher minimum
+# Flutter/Dart version than the package itself. Non-client code doesn't need to
+# work in legacy versions.
+#
+# This requires the --lib-only flag below.
+./script/tool_runner.sh remove-dev-dependencies
+
 # This uses --run-on-dirty-packages rather than --packages-for-branch
 # since only the packages changed by 'make-deps-path-based' need to be
 # re-checked.
-dart ./script/tool/bin/flutter_plugin_tools.dart dart-test --run-on-dirty-packages \
-    --log-timing --exclude=script/configs/dart_unit_tests_exceptions.yaml \
-    $PACKAGE_SHARDING
+./script/tool_runner.sh analyze --lib-only \
+    --skip-if-not-supporting-flutter-version="$CHANNEL" \
+    --custom-analysis=script/configs/custom_analysis.yaml
+
 # Restore the tree to a clean state, to avoid accidental issues if
 # other script steps are added to the enclosing task.
 git checkout .
