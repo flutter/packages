529d44eaf3db0a88221149e4495c8a605ba5658b
stuartmorgan <stuartmorgan@google.com>
2023-07-14 21:51:32 -0400

    Fix indentation, add one more docs reference

--- packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/SdkCapabilityChecker.java
+++ packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/SdkCapabilityChecker.java
@@ -18,42 +18,43 @@ public class SdkCapabilityChecker {
 
   @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.P)
   public static boolean supportsDistortionCorrection() {
-  // See https://developer.android.com/reference/android/hardware/camera2/CameraCharacteristics#DISTORTION_CORRECTION_AVAILABLE_MODES
+    // See https://developer.android.com/reference/android/hardware/camera2/CameraCharacteristics#DISTORTION_CORRECTION_AVAILABLE_MODES
     return SDK_VERSION >= Build.VERSION_CODES.P;
   }
 
   @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.O)
   public static boolean supportsEglRecordableAndroid() {
+    // See https://developer.android.com/reference/android/opengl/EGLExt#EGL_RECORDABLE_ANDROID
     return SDK_VERSION >= Build.VERSION_CODES.O;
   }
 
   @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
   public static boolean supportsEncoderProfiles() {
-  // See https://developer.android.com/reference/android/media/EncoderProfiles
+    // See https://developer.android.com/reference/android/media/EncoderProfiles
     return SDK_VERSION >= Build.VERSION_CODES.S;
   }
 
   @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.M)
   public static boolean supportsMarshmallowNoiseReductionModes() {
-  // See https://developer.android.com/reference/android/hardware/camera2/CameraCharacteristics#NOISE_REDUCTION_AVAILABLE_NOISE_REDUCTION_MODES
+    // See https://developer.android.com/reference/android/hardware/camera2/CameraCharacteristics#NOISE_REDUCTION_AVAILABLE_NOISE_REDUCTION_MODES
     return SDK_VERSION >= Build.VERSION_CODES.M;
   }
 
   @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.P)
   public static boolean supportsSessionConfiguration() {
-  // See https://developer.android.com/reference/android/hardware/camera2/params/SessionConfiguration
+    // See https://developer.android.com/reference/android/hardware/camera2/params/SessionConfiguration
     return SDK_VERSION >= Build.VERSION_CODES.P;
   }
 
   @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.N)
   public static boolean supportsVideoPause() {
-  // See https://developer.android.com/reference/androidx/camera/video/VideoRecordEvent.Pause
+    // See https://developer.android.com/reference/androidx/camera/video/VideoRecordEvent.Pause
     return SDK_VERSION >= Build.VERSION_CODES.N;
   }
 
   @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.R)
   public static boolean supportsZoomRatio() {
-  // See https://developer.android.com/reference/android/hardware/camera2/CaptureRequest#CONTROL_ZOOM_RATIO
+    // See https://developer.android.com/reference/android/hardware/camera2/CaptureRequest#CONTROL_ZOOM_RATIO
     return SDK_VERSION >= Build.VERSION_CODES.R;
   }
 }
