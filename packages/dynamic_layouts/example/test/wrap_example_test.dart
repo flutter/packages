0c54586e2f4b03aaeff81f8864ac0e0008188de5
Stuart Morgan <stuartmorgan@google.com>
2023-07-18 14:32:58 -0400

    Make tests handle M3

--- packages/dynamic_layouts/example/test/wrap_example_test.dart
+++ packages/dynamic_layouts/example/test/wrap_example_test.dart
@@ -9,11 +9,10 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Check that the children are layed out.',
       (WidgetTester tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: WrapExample(),
-      ),
+    const MaterialApp app = MaterialApp(
+      home: WrapExample(),
     );
+    await tester.pumpWidget(app);
     await tester.pumpAndSettle();
 
     // See if there are children layed out.
@@ -23,10 +22,21 @@ void main() {
     expect(find.text('Index 3'), findsOneWidget);
     expect(find.text('Index 4'), findsOneWidget);
 
+    // Material 3 changes the expected layout positioning.
+    final bool usesMaterial3 = (app.theme ?? ThemeData.light()).useMaterial3;
+    final Offset offset0 =
+        usesMaterial3 ? const Offset(0.0, 91.0) : const Offset(0.0, 103.0);
+    final Offset offset1 =
+        usesMaterial3 ? const Offset(65.0, 121.0) : const Offset(66.0, 124.0);
+    final Offset offset3 =
+        usesMaterial3 ? const Offset(270.0, 171.0) : const Offset(271.0, 174.0);
+    final Offset offset4 =
+        usesMaterial3 ? const Offset(380.0, 221.0) : const Offset(381.0, 224.0);
+
     // See if they are in expected position.
-    expect(tester.getTopLeft(find.text('Index 0')), const Offset(0.0, 103.0));
-    expect(tester.getTopLeft(find.text('Index 1')), const Offset(66.0, 124.0));
-    expect(tester.getTopLeft(find.text('Index 3')), const Offset(271.0, 174.0));
-    expect(tester.getTopLeft(find.text('Index 4')), const Offset(381.0, 224.0));
+    expect(tester.getTopLeft(find.text('Index 0')), offset0);
+    expect(tester.getTopLeft(find.text('Index 1')), offset1);
+    expect(tester.getTopLeft(find.text('Index 3')), offset3);
+    expect(tester.getTopLeft(find.text('Index 4')), offset4);
   });
 }
