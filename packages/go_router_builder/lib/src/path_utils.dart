205da049bc308c5643ad6fdf62b19e26e8a0bff0
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-07-20 10:52:53 +0800

    refactor: Simplify path utils methods to only keep what is used

--- packages/go_router_builder/lib/src/path_utils.dart
+++ packages/go_router_builder/lib/src/path_utils.dart
@@ -4,70 +4,29 @@
 
 final RegExp _parameterRegExp = RegExp(r':(\w+)(\((?:\\.|[^\\()])+\))?');
 
-/// Converts a [pattern] such as `/user/:id` into [RegExp].
+/// Extracts the path parameters from a [pattern] such as `/user/:id`.
 ///
-/// The path parameters can be specified by prefixing them with `:`. The
-/// `parameters` are used for storing path parameter names.
-///
+/// The path parameters can be specified by prefixing them with `:`.
 ///
 /// For example:
 ///
-///  `pattern` = `/user/:id/book/:bookId`
-///
-///  The `parameters` would contain `['id', 'bookId']` as a result of calling
-///  this method.
-///
-/// To extract the path parameter values from a [RegExpMatch], pass the
-/// [RegExpMatch] into [extractPathParameters] with the `parameters` that are
-/// used for generating the [RegExp].
-RegExp patternToRegExp(String pattern, List<String> parameters) {
-  final StringBuffer buffer = StringBuffer('^');
-  int start = 0;
-  for (final RegExpMatch match in _parameterRegExp.allMatches(pattern)) {
-    if (match.start > start) {
-      buffer.write(RegExp.escape(pattern.substring(start, match.start)));
-    }
-    final String name = match[1]!;
-    final String? optionalPattern = match[2];
-    final String regex = optionalPattern != null
-        ? _escapeGroup(optionalPattern, name)
-        : '(?<$name>[^/]+)';
-    buffer.write(regex);
-    parameters.add(name);
-    start = match.end;
-  }
-
-  if (start < pattern.length) {
-    buffer.write(RegExp.escape(pattern.substring(start)));
-  }
-
-  if (!pattern.endsWith('/')) {
-    buffer.write(r'(?=/|$)');
-  }
-  return RegExp(buffer.toString(), caseSensitive: false);
-}
-
-String _escapeGroup(String group, [String? name]) {
-  final String escapedGroup = group.replaceFirstMapped(
-      RegExp(r'[:=!]'), (Match match) => '\\${match[0]}');
-  if (name != null) {
-    return '(?<$name>$escapedGroup)';
-  }
-  return escapedGroup;
-}
+/// ```dart
+/// final pattern = '/user/:id/book/:bookId';
+/// final pathParameters = pathParametersFromPattern(pattern); // {'id', 'bookId'}
+/// ```
+Set<String> pathParametersFromPattern(String pattern) => <String>{
+      for (final RegExpMatch match in _parameterRegExp.allMatches(pattern))
+        match[1]!,
+    };
 
 /// Reconstructs the full path from a [pattern] and path parameters.
 ///
-/// This is useful for restoring the original path from a [RegExpMatch].
+/// For example:
 ///
-/// For example, A path matched a [RegExp] returned from [patternToRegExp] and
-/// produced a [RegExpMatch]. To reconstruct the path from the match, one
-/// can follow these steps:
-///
-/// 1. Get the `pathParameters` by calling [extractPathParameters] with the
-///    [RegExpMatch] and the parameters used for generating the [RegExp].
-/// 2. Call [patternToPath] with the `pathParameters` from the first step and
-///    the original `pattern` used for generating the [RegExp].
+/// ```dart
+/// final pattern = '/family/:id';
+/// final path = patternToPath(pattern, {'id': 'family-id'}); // '/family/family-id'
+/// ```
 String patternToPath(String pattern, Map<String, String> pathParameters) {
   final StringBuffer buffer = StringBuffer();
   int start = 0;
