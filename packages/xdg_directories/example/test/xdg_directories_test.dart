6d49031b829fb8ff040d617510144afeb5573b64
eriko13 <erikgerman917@gmail.com>
2023-07-23 16:24:49 -0700

    added documentation

--- packages/xdg_directories/example/test/xdg_directories_test.dart
+++ packages/xdg_directories/example/test/xdg_directories_test.dart
@@ -7,6 +7,7 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('XDG Directories', (WidgetTester _) async {
+    // Check that the test is running on Linux.
     if (!Platform.isLinux) {
       throw Exception('This test is only valid on Linux');
     }
@@ -15,6 +16,7 @@ void main() {
     expect(getUserDirectoryNames(), Set<String>,
         reason: 'getUserDirectoryNames() should return a Set<String>');
 
+    // Check that the XDG directories are not empty.
     expect(
       getUserDirectoryNames().length,
       isNot(0),
@@ -23,12 +25,14 @@ void main() {
 
     final Set<String> userDirectoryNames = getUserDirectoryNames();
 
+    // Check that getUserDirectory() returns a Directory.
     expect(
       getUserDirectory(userDirectoryNames.first),
       Directory,
       reason: 'getUserDirectory() should return a Directory',
     );
 
+    // Check that getUserDirectory() returns null if the directory does not exist.
     expect(
       getUserDirectory('randomString'),
       null,
@@ -36,36 +40,42 @@ void main() {
           'getUserDirectory() should return null if the directory does not exist',
     );
 
+    // Check that dataHome returns a Directory.
     expect(
       dataHome,
       Directory,
       reason: 'dataHome should return a Directory',
     );
 
+    // Check that configHome returns a Directory.
     expect(
       configHome,
       Directory,
       reason: 'configHome should return a Directory',
     );
 
+    // Check that cacheHome returns a Directory.
     expect(
       cacheHome,
       Directory,
       reason: 'cacheHome should return a Directory',
     );
 
+    // Check that runtimeDir returns a Directory.
     expect(
       runtimeDir,
       Directory,
       reason: 'runtimeDir should return a Directory',
     );
 
+    // Check that dataDirs returns a List<Directory>.
     expect(
       dataDirs,
       List<Directory>,
       reason: 'dataDirs should return a List<Directory>',
     );
 
+    // Check that configDirs returns a List<Directory>.
     expect(
       configDirs,
       List<Directory>,
