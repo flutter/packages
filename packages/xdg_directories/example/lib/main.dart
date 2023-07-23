205817f51e438e4409958d4ee8a236f498f18e69
eriko13 <erikgerman917@gmail.com>
2023-07-23 07:57:03 +0000

    added demos for all functionalities

--- packages/xdg_directories/example/lib/main.dart
+++ packages/xdg_directories/example/lib/main.dart
@@ -44,8 +44,8 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView(
+          shrinkWrap: true,
           children: <Widget>[
             const Padding(
               padding: EdgeInsets.all(10.0),
@@ -80,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Padding(
               padding: EdgeInsets.only(left: 10, bottom: 5),
               child: Text(
-                'User directory:',
+                'Selected directory:',
                 style: TextStyle(
                   color: Colors.black87,
                   fontSize: 18,
@@ -88,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Container(
-              margin: const EdgeInsets.only(left: 10),
+              margin: const EdgeInsets.symmetric(horizontal: 10),
               padding: const EdgeInsets.symmetric(
                 horizontal: 20,
                 vertical: 5,
@@ -100,6 +100,179 @@ class _MyHomePageState extends State<MyHomePage> {
                   color: Colors.white,
                 ),
               ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10, bottom: 5),
+              child: Text(
+                'Runtime directory:',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 5,
+              ),
+              color: Colors.black38,
+              child: Text(
+                runtimeDir?.path ?? '',
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10, bottom: 5),
+              child: Text(
+                'Cache home:',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 5,
+              ),
+              color: Colors.black38,
+              child: Text(
+                cacheHome.path,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10, bottom: 5),
+              child: Text(
+                'Config directories:',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: configDirs.length,
+              itemBuilder: (BuildContext context, int index) => Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 5,
+                ),
+                color: Colors.black38,
+                child: Text(
+                  configDirs[index].path,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10, bottom: 5),
+              child: Text(
+                'Data directories:',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: dataDirs.length,
+              itemBuilder: (BuildContext context, int index) => Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 5,
+                ),
+                color: Colors.black38,
+                child: Text(
+                  dataDirs[index].path,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10, bottom: 5),
+              child: Text(
+                'Config home:',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 5,
+              ),
+              color: Colors.black38,
+              child: Text(
+                configHome.path,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10, bottom: 5),
+              child: Text(
+                'Data home:',
+                style: TextStyle(
+                  color: Colors.black87,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 5,
+              ),
+              color: Colors.black38,
+              child: Text(
+                dataHome.path,
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 100,
             ),
           ],
         ),
