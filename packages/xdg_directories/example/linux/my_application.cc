9ced453c031f4392eaab451170c4ccba863e7405
eriko13 <erikgerman917@gmail.com>
2023-07-23 23:44:56 -0700

    added license

--- packages/xdg_directories/example/linux/my_application.cc
+++ packages/xdg_directories/example/linux/my_application.cc
@@ -1,4 +1,6 @@
-#include "my_application.h"
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.#include "my_application.h"
 
 #include <flutter_linux/flutter_linux.h>
 #ifdef GDK_WINDOWING_X11
