dacf2beeacbdc3fbcaae629f2342dc7eaf9a2484
J-P Nurmi <jpnurmi@gmail.com>
2023-07-24 20:49:01 +0200

    Fix typo

--- packages/path_provider/path_provider_linux/example/lib/main.dart
+++ packages/path_provider/path_provider_linux/example/lib/main.dart
@@ -67,7 +67,7 @@ class _MyAppState extends State<MyApp> {
     try {
       appCacheDirectory = await _provider.getApplicationCachePath();
     } on PlatformException {
-      appCacheDirectory = 'Failed to get cacge directory.';
+      appCacheDirectory = 'Failed to get cache directory.';
     }
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
