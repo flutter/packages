e21cd64aea9697dcb393ccc4c915927d48c56636
Maurits van Beusekom <maurits@vnbskm.nl>
2023-07-27 13:05:47 +0200

    Adds missing license block

--- packages/webview_flutter/webview_flutter_platform_interface/lib/src/types/javascript_log_level.dart
+++ packages/webview_flutter/webview_flutter_platform_interface/lib/src/types/javascript_log_level.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /// Represents the severity of a JavaScript log message.
 enum JavaScriptLogLevel {
   /// Indicates an error message was logged via an "error" event of the
