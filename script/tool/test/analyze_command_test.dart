97e24a8ac00c6bc4cf5005b7009dd4527f726568
Stuart Morgan <stuartmorgan@google.com>
2023-08-04 11:38:49 -0400

    Add support for skipping

--- script/tool/test/analyze_command_test.dart
+++ script/tool/test/analyze_command_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:args/command_runner.dart';
 import 'package:file/file.dart';
 import 'package:file/memory.dart';
@@ -313,6 +315,39 @@ void main() {
               runner, <String>['analyze', '--custom-analysis', '']),
           throwsA(isA<ToolExit>()));
     });
+  });
+
+  test('skips if requseted if "pub get" fails in the resolver', () async {
+    final RepositoryPackage plugin = createFakePlugin('foo', packagesDir);
+
+    final FakeProcessInfo failingPubGet = FakeProcessInfo(
+        MockProcess(
+            exitCode: 1,
+            stderr: 'So, because foo depends on both thing_one ^1.0.0 and '
+                'thing_two from path, version solving failed.'),
+        <String>['pub', 'get']);
+    processRunner.mockProcessesForExecutable['flutter'] = <FakeProcessInfo>[
+      failingPubGet,
+      // The command re-runs failures when --skip-if-resolver-fails is passed
+      // to check the output, so provide the same failing outcome.
+      failingPubGet,
+    ];
+
+    final List<String> output = await runCapturingPrint(
+        runner, <String>['analyze', '--skip-if-resolving-fails']);
+
+    expect(
+      output,
+      containsAllInOrder(<Matcher>[
+        contains('Skipping package due to pub resolution failure.'),
+      ]),
+    );
+    expect(
+        processRunner.recordedCalls,
+        orderedEquals(<ProcessCall>[
+          ProcessCall('flutter', const <String>['pub', 'get'], plugin.path),
+          ProcessCall('flutter', const <String>['pub', 'get'], plugin.path),
+        ]));
   });
 
   test('fails if "pub get" fails', () async {
