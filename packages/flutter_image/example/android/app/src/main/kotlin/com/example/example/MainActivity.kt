7867132910e949f8a9d31b4aa46c8a1223ba6290
Jemish Vasoya <79353977+jemishavasoya@users.noreply.github.com>
2023-08-06 01:19:49 +0530

    fix linux issue

--- packages/flutter_image/example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ packages/flutter_image/example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,4 +1,6 @@
-package com.example.example
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.package com.example.example
 
 import io.flutter.embedding.android.FlutterActivity
 
