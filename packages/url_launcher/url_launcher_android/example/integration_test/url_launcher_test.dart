8d31e6f92d4b28729654efe2e93483f76b89efc9
Reid Baker <reidbaker@google.com>
2023-08-07 11:47:29 -0400

    Add integration test for android url launch and close

--- packages/url_launcher/url_launcher_android/example/integration_test/url_launcher_test.dart
+++ packages/url_launcher/url_launcher_android/example/integration_test/url_launcher_test.dart
@@ -17,9 +17,28 @@ void main() {
     // Generally all devices should have some default browser.
     expect(await launcher.canLaunch('http://flutter.dev'), true);
 
+    // sms:, tel:, and mailto: links may not be openable on every device, so
+    // aren't tested here.
+  });
+
+  testWidgets('launch and close', (WidgetTester _) async {
+    final UrlLauncherPlatform launcher = UrlLauncherPlatform.instance;
+
     // Launch a url then close.
-    expect(await launcher.launch('http://flutter.dev'), true);
+    expect(
+        await launcher.launch('https://flutter.dev',
+            useSafariVC: true,
+            useWebView: true,
+            enableJavaScript: false,
+            enableDomStorage: false,
+            universalLinksOnly: false,
+            headers: <String, String>{'my_header_key': 'my_header_value'}),
+        true);
+    // Allow time for page to load.
+    await Future.delayed(Duration(seconds: 3));
     await launcher.closeWebView();
+    // Delay required to catch android side crashes in onDestroy
+    await Future.delayed(Duration(seconds: 3));
 
     // sms:, tel:, and mailto: links may not be openable on every device, so
     // aren't tested here.
