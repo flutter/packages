416af814df77c84c4dbd26a9083714a60bdf8a85
Jemish Vasoya <79353977+jemishavasoya@users.noreply.github.com>
2023-08-12 02:03:41 +0530

    android issue fix

--- packages/flutter_image/example/lib/main.dart
+++ packages/flutter_image/example/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'SharedPreferences Demo',
+      title: 'NetworkImageWithRetry Demo',
       home: NetworkImageWithRetryDemo(),
     );
   }
