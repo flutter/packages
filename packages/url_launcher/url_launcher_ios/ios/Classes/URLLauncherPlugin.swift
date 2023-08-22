55a345ba78b615ec308ef8a1e397a7794b6d4297
Chris Langham <chrislangham@rightnow.org>
2023-08-21 21:52:50 -0500

    reverting unintentional local changes

--- packages/url_launcher/url_launcher_ios/ios/Classes/URLLauncherPlugin.swift
+++ packages/url_launcher/url_launcher_ios/ios/Classes/URLLauncherPlugin.swift
@@ -67,7 +67,6 @@ public final class URLLauncherPlugin: NSObject, FlutterPlugin, UrlLauncherApi {
     * @param url The invalid URL string
     * @return The error to return
     */
-    
     func invalidURLError(for url: String) -> Error {
         GeneralError(code: "argument_error", message: "Unable to parse URL", details: "Provided URL: \(url)")
     }
