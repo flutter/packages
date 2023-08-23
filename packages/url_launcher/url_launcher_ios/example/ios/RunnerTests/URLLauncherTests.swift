495de26facb37bad2857fca64cdc01d516ced7cd
Chris Langham <chrislangham@rightnow.org>
2023-08-22 22:53:29 -0500

    converting unit tests to swift

--- packages/url_launcher/url_launcher_ios/example/ios/RunnerTests/URLLauncherTests.swift
+++ packages/url_launcher/url_launcher_ios/example/ios/RunnerTests/URLLauncherTests.swift
@@ -8,135 +8,134 @@ import XCTest
 @testable import url_launcher_ios
 
 class URLLauncherTests: XCTestCase {
-    var plugin: URLLauncherPlugin!
-    var launcher: FakeLauncher!
+  var plugin: FLTURLLauncherPlugin!
+  var launcher: FakeLauncher!
 
-    override func setUp() {
-        launcher = FakeLauncher()
-        plugin = URLLauncherPlugin(launcher: launcher, binaryMessenger: FakeFlutterBinaryMessenger())
+  override func setUp() {
+    launcher = FakeLauncher()
+    plugin = FLTURLLauncherPlugin(launcher: launcher)
+  }
+
+  func testCanLaunchSuccess() {
+    var error: FlutterError?
+    let result = plugin.canLaunchURL("good://url", error: &error)
+
+    XCTAssertTrue(result!.boolValue)
+    XCTAssertNil(error)
+  }
+
+  func testCanLaunchFailure() {
+    var error: FlutterError?
+    let result = plugin.canLaunchURL("bad://url", error: &error)
+
+    XCTAssertFalse(result!.boolValue)
+  }
+
+  func testCanLaunchFailureWithInvalidURL() {
+    var error: FlutterError?
+    let result = plugin.canLaunchURL("urls can't have spaces", error: &error)
+
+    XCTAssertNil(result)
+    XCTAssertNotNil(error)
+    XCTAssertEqual(error!.code, "argument_error")
+    XCTAssertEqual(error!.message, "Unable to parse URL")
+    XCTAssertEqual(error!.details as! String, "Provided URL: urls can't have spaces")
+  }
+
+  func testLaunchSuccess() {
+    let expectation = XCTestExpectation(description: "completion called")
+    plugin.launchURL("good://url", universalLinksOnly: false) { result, error in
+      XCTAssertTrue(result!.boolValue)
+      XCTAssertNil(error)
+      expectation.fulfill()
     }
 
-    func testCanLaunchSuccess() {
-        let result = plugin.canLaunchUrl(url: "good://url")
+    wait(for: [expectation], timeout: 1)
+  }
 
-        XCTAssertTrue(result)
+  func testLaunchFailure() {
+    let expectation = XCTestExpectation(description: "completion called")
+
+    plugin.launchURL("bad://url", universalLinksOnly: false) { result, error in
+      XCTAssertFalse(result!.boolValue)
+      XCTAssertNil(error)
+      expectation.fulfill()
     }
 
-    func testCanLaunchFailure() {
-        let result = plugin.canLaunchUrl(url: "bad://url")
+    wait(for: [expectation], timeout: 1)
+  }
 
-        XCTAssertFalse(result)
+  func testLaunchFailureWithInvalidURL() {
+    let expectation = XCTestExpectation(description: "completion called")
+
+    plugin.launchURL("urls can't have spaces", universalLinksOnly: false) { result, error in
+      XCTAssertNil(result)
+      XCTAssertNotNil(error)
+      XCTAssertEqual(error!.code, "argument_error")
+      XCTAssertEqual(error!.message, "Unable to parse URL")
+      XCTAssertEqual(error!.details as! String, "Provided URL: urls can't have spaces")
+
+      expectation.fulfill()
     }
 
-    func testCanLaunchFailureWithInvalidURL() {
-        let result = plugin.canLaunchUrl(url: "urls can't have spaces")
+    wait(for: [expectation], timeout: 1)
+  }
 
-        XCTAssertFalse(result)
+  func testLaunchWithoutUniversalLinks() {
+    let expectation = XCTestExpectation(description: "completion called")
+    plugin.launchURL("good://url", universalLinksOnly: false) { result, error in
+      XCTAssertNil(error)
+      expectation.fulfill()
     }
 
-    func testLaunchSuccess() {
-        let expectation = XCTestExpectation(description: "completion called")
-        plugin.launchUrl(url: "good://url", universalLinksOnly: false) { result in
-            switch result {
-            case .success(let success):
-                XCTAssertTrue(success)
-            case .failure(let error):
-                XCTFail("Unexpected error: \(error)")
-            }
-            expectation.fulfill()
-        }
+    wait(for: [expectation], timeout: 1)
 
-        wait(for: [expectation], timeout: 1)
-    }
-    
-    func testLaunchFailure() {
-        let expectation = XCTestExpectation(description: "completion called")
-        plugin.launchUrl(url: "bad://url", universalLinksOnly: false) { result in
-            switch result {
-            case .success(let success):
-                XCTAssertFalse(success)
-            case .failure(let error):
-                XCTFail("Unexpected error: \(error)")
-            }
-            expectation.fulfill()
-        }
+    XCTAssertEqual(launcher.passedOptions?[.universalLinksOnly] as? Bool, false)
+  }
 
-        wait(for: [expectation], timeout: 1)
+  func testLaunchWithUniversalLinks() {
+    let expectation = XCTestExpectation(description: "completion called")
+
+    plugin.launchURL("good://url", universalLinksOnly: true) { result, error in
+      XCTAssertNil(error)
+      expectation.fulfill()
     }
 
-    func testLaunchFailureWithInvalidURL() {
-        let expectation = XCTestExpectation(description: "completion called")
-        plugin.launchUrl(url: "urls can't have spaces", universalLinksOnly: false) { result in
-            switch result {
-            case .success(_):
-                XCTFail("Expected an error")
-            case .failure(let error):
-                let generalError = error as! GeneralError
-                XCTAssertEqual(generalError.code, "argument_error")
-                XCTAssertEqual(generalError.message, "Unable to parse URL")
-                XCTAssertEqual(generalError.details, "Provided URL: urls can't have spaces")
-            }
-            expectation.fulfill()
-        }
+    wait(for: [expectation], timeout: 1)
 
-        wait(for: [expectation], timeout: 1)
-    }
+    XCTAssertEqual(launcher.passedOptions?[.universalLinksOnly] as? Bool, true)
+  }
 
-    func testLaunchWithoutUniversalLinks() {
-        let expectation = XCTestExpectation(description: "completion called")
-        plugin.launchUrl(url: "good://url", universalLinksOnly: false) { result in
-            switch result {
-            case .success(let success):
-                XCTAssertTrue(success)
-            case .failure(let error):
-                XCTFail("Unexpected error: \(error)")
-            }
-            expectation.fulfill()
-        }
-
-        wait(for: [expectation], timeout: 1)
-        XCTAssertEqual(launcher.passedOptions?[.universalLinksOnly] as? Bool, false)
-    }
-
-    func testLaunchWithUniversalLinks() {
-        let expectation = XCTestExpectation(description: "completion called")
-        plugin.launchUrl(url: "good://url", universalLinksOnly: true) { result in
-            switch result {
-            case .success(let success):
-                XCTAssertTrue(success)
-            case .failure(let error):
-                XCTFail("Unexpected error: \(error)")
-            }
-            expectation.fulfill()
-        }
-
-        wait(for: [expectation], timeout: 1)
-        XCTAssertEqual(launcher.passedOptions?[.universalLinksOnly] as? Bool, true)
-    }
-    
 }
 
-class FakeLauncher: Launcher {
+class FakeLauncher: NSObject, FULLauncher {
   var passedOptions: [UIApplication.OpenExternalURLOptionsKey: Any]?
-  
-  func canOpenURL(_ url: URL) -> Bool {
+
+  func canOpen(_ url: URL) -> Bool {
     return url.scheme == "good"
   }
-  
-  func openURL(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any] = [:], completionHandler: ((Bool) -> Void)? = nil) {
+
+  func open(
+    _ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any] = [:],
+    completionHandler: ((Bool) -> Void)? = nil
+  ) {
     self.passedOptions = options
     completionHandler?(url.scheme == "good")
   }
 }
 
 class FakeFlutterBinaryMessenger: NSObject, FlutterBinaryMessenger {
-    func send(onChannel channel: String, message: Data?) { }
+  func send(onChannel channel: String, message: Data?) {}
 
-    func send(onChannel channel: String, message: Data?, binaryReply callback: FlutterBinaryReply? = nil) { }
+  func send(
+    onChannel channel: String, message: Data?, binaryReply callback: FlutterBinaryReply? = nil
+  ) {}
 
-    func setMessageHandlerOnChannel(_ channel: String, binaryMessageHandler handler: FlutterBinaryMessageHandler? = nil) -> FlutterBinaryMessengerConnection {
-        123
-    }
+  func setMessageHandlerOnChannel(
+    _ channel: String, binaryMessageHandler handler: FlutterBinaryMessageHandler? = nil
+  ) -> FlutterBinaryMessengerConnection {
+    123
+  }
 
-    func cleanUpConnection(_ connection: FlutterBinaryMessengerConnection) { }
+  func cleanUpConnection(_ connection: FlutterBinaryMessengerConnection) {}
 }
--- packages/url_launcher/url_launcher_ios/example/ios/RunnerUITests/URLLauncherUITests.swift
+++ packages/url_launcher/url_launcher_ios/example/ios/RunnerUITests/URLLauncherUITests.swift
@@ -5,33 +5,33 @@
 import XCTest
 
 class URLLauncherUITests: XCTestCase {
-    var app: XCUIApplication!
+  var app: XCUIApplication!
 
-    override func setUp() {
-        continueAfterFailure = false
+  override func setUp() {
+    continueAfterFailure = false
 
-        app = XCUIApplication()
-        app.launch()
+    app = XCUIApplication()
+    app.launch()
+  }
+
+  func testLaunch() {
+    let app = self.app!
+
+    let buttonNames: [String] = [
+      "Launch in app", "Launch in app(JavaScript ON)", "Launch in app(DOM storage ON)",
+      "Launch a universal link in a native app, fallback to Safari.(Youtube)",
+    ]
+    for buttonName in buttonNames {
+      let button = app.buttons[buttonName]
+      XCTAssertTrue(button.waitForExistence(timeout: 30.0))
+      XCTAssertEqual(app.webViews.count, 0)
+      button.tap()
+      let webView = app.webViews.firstMatch
+      XCTAssertTrue(webView.waitForExistence(timeout: 30.0))
+      XCTAssertTrue(app.buttons["ForwardButton"].waitForExistence(timeout: 30.0))
+      XCTAssertTrue(app.buttons["Share"].exists)
+      XCTAssertTrue(app.buttons["OpenInSafariButton"].exists)
+      app.buttons["Done"].tap()
     }
-
-    func testLaunch() {
-        let app = self.app!
-
-        let buttonNames: [String] = [
-            "Launch in app", "Launch in app(JavaScript ON)", "Launch in app(DOM storage ON)",
-            "Launch a universal link in a native app, fallback to Safari.(Youtube)"
-        ]
-        for buttonName in buttonNames {
-            let button = app.buttons[buttonName]
-            XCTAssertTrue(button.waitForExistence(timeout: 30.0))
-            XCTAssertEqual(app.webViews.count, 0)
-            button.tap()
-            let webView = app.webViews.firstMatch
-            XCTAssertTrue(webView.waitForExistence(timeout: 30.0))
-            XCTAssertTrue(app.buttons["ForwardButton"].waitForExistence(timeout: 30.0))
-            XCTAssertTrue(app.buttons["Share"].exists)
-            XCTAssertTrue(app.buttons["OpenInSafariButton"].exists)
-            app.buttons["Done"].tap()
-        }
-    }
+  }
 }
