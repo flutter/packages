b0122cf4260854306f0edc03f8a3acf6a1aae64d
Mairramer <mairramer.dasilva28@hotmail.com>
2023-09-08 10:35:14 -0300

    fix format

--- packages/ios_platform_images/test/ios_platform_images_test.dart
+++ packages/ios_platform_images/test/ios_platform_images_test.dart
@@ -8,13 +8,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ios_platform_images/ios_platform_images.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('plugins.flutter.io/ios_platform_images');
+  const MethodChannel channel =
+      MethodChannel('plugins.flutter.io/ios_platform_images');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    _ambiguate(TestDefaultBinaryMessengerBinding.instance)!.defaultBinaryMessenger.setMockMethodCallHandler(channel,
-        (MethodCall methodCall) async {
+    _ambiguate(TestDefaultBinaryMessengerBinding.instance)!
+        .defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
       if (methodCall.method == 'loadImage') {
         return <String, Object>{
           'scale': 1.0,
@@ -41,7 +43,8 @@ void main() {
     expect(IosPlatformImages.load('foobar'), isNotNull);
 
     final ImageProvider<Object> image = IosPlatformImages.load('foobar');
-    expect(image.obtainCacheStatus(configuration: ImageConfiguration.empty), isA<Future<ImageCacheStatus?>>());
+    expect(image.obtainCacheStatus(configuration: ImageConfiguration.empty),
+        isA<Future<ImageCacheStatus?>>());
     expect(image.resolve(ImageConfiguration.empty), isA<ImageStream>());
   });
 }
