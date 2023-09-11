100202a2e582ea9473d302bba3a4138709e7748c
Mairramer <mairramer.dasilva28@hotmail.com>
2023-09-11 18:35:19 -0300

    rollback comment

--- packages/ios_platform_images/ios/Classes/UIImageIosPlatformImages.swift
+++ packages/ios_platform_images/ios/Classes/UIImageIosPlatformImages.swift
@@ -6,28 +6,29 @@ import Flutter
 import Foundation
 import UIKit
 
-extension UIImage {
-  static func flutterImage(withName name: String) -> UIImage? {
-    let imageName = (name as NSString).lastPathComponent
-    let path = (name as NSString).deletingLastPathComponent
-    let screenScale = UIScreen.main.scale
+/// Loads a UIImage from the embedded Flutter project's assets.
+///
+/// This method loads the Flutter asset that is appropriate for the current
+/// screen.  If you are on a 2x retina device where usually `UIImage` would be
+/// loading `@2x` assets, it will attempt to load the `2.0x` variant.  It will
+/// load the standard image if it can't find the `2.0x` variant.
+///
+/// For example, if your Flutter project's `pubspec.yaml` lists "assets/foo.png"
+/// and "assets/2.0x/foo.png", calling
+/// `[UIImage flutterImageWithName:@"assets/foo.png"]` will load
+/// "assets/2.0x/foo.png".
+///
+/// See also https://flutter.dev/docs/development/ui/assets-and-images
+///
+/// Note: We don't yet support images from package dependencies (ex.
+/// `AssetImage('icons/heart.png', package: 'my_icons')`).
 
-    var scaledImage: UIImage?
-
-    // Search for a scaled image in the bundle
-    // The image name may be suffixed with a scale factor, e.g. @2x
-    // If a scaled image is not found, search for standard image
-
-    for scale in stride(from: Int(screenScale), through: 1, by: -1) {
-      let scaledName = imageName.replacingOccurrences(of: ".", with: "@\(scale)x.")
-      let scaledPath = "\(path)/\(scaledName)"
-      if let image = UIImage(named: scaledPath, in: Bundle.main, compatibleWith: nil) {
-        scaledImage = image
-        break  // Exit the loop if a scaled image is found
-      }
+func flutterImage(withName imageName: String) -> UIImage? {
+    for scale in stride(from: Int(UIScreen.main.scale), through: 1, by: -1) {
+      let scaledImage = imageName.replacingOccurrences(of: ".", with: "@\(scale)x.")
+      return UIImage(named: scaledImage, in: Bundle.main, compatibleWith: nil)
     }
 
-    // If a scaled image is not found return the standard image if it exists
-    return scaledImage ?? UIImage(named: imageName, in: Bundle.main, compatibleWith: nil)
+    return UIImage(named: imageName, in: Bundle.main, compatibleWith: nil)
   }
-}
+
