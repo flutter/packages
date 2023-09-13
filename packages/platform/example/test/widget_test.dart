7e486d73a4392cee4712f243f6dda55166dda5d2
Chris Langham <chrislangham@rightnow.org>
2023-09-12 23:28:39 -0500

    updates test name

--- packages/platform/example/test/widget_test.dart
+++ packages/platform/example/test/widget_test.dart
@@ -7,7 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:plaform_example/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
