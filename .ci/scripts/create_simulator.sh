f47b35bb320e1ba3ff756ba58f62e6e92bf242e5
Victoria Ashworth <vashworth@google.com>
2023-09-14 11:07:14 -0500

    test xcode 14 on mac 13

--- .ci/scripts/create_simulator.sh
+++ .ci/scripts/create_simulator.sh
@@ -9,7 +9,7 @@ set -o pipefail
 # The name here must match remove_simulator.sh
 readonly DEVICE_NAME=Flutter-iPhone
 readonly DEVICE=com.apple.CoreSimulator.SimDeviceType.iPhone-14
-readonly OS=com.apple.CoreSimulator.SimRuntime.iOS-17-0
+readonly OS=com.apple.CoreSimulator.SimRuntime.iOS-16-4
 
 xcrun simctl list
 xcrun simctl create "$DEVICE_NAME" "$DEVICE" "$OS" | xargs xcrun simctl boot
