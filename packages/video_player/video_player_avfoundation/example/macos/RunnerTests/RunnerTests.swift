38e504720946a2630544846b64ff93caca4c39eb
Stuart Morgan <stuartmorgan@google.com>
2023-09-21 11:12:47 -0400

    flutter create output, unmodified

---
+++ packages/video_player/video_player_avfoundation/example/macos/RunnerTests/RunnerTests.swift
@@ -0,0 +1,27 @@
+import FlutterMacOS
+import Cocoa
+import XCTest
+
+@testable import video_player_avfoundation
+
+// This demonstrates a simple unit test of the Swift portion of this plugin's implementation.
+//
+// See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+
+class RunnerTests: XCTestCase {
+
+  func testGetPlatformVersion() {
+    let plugin = VideoPlayerAvfoundationPlugin()
+
+    let call = FlutterMethodCall(methodName: "getPlatformVersion", arguments: [])
+
+    let resultExpectation = expectation(description: "result block must be called.")
+    plugin.handle(call) { result in
+      XCTAssertEqual(result as! String,
+                     "macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
+      resultExpectation.fulfill()
+    }
+    waitForExpectations(timeout: 1)
+  }
+
+}
