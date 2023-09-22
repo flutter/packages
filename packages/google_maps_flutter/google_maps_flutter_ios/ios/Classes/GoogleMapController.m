1660e467db40e2ce87acfa7916ec64383413dbc2
MichaelA71 <michael.r.adriaens@gmail.com>
2023-09-22 15:08:04 +0200

    Update GoogleMapController.m

--- packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/GoogleMapController.m
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/GoogleMapController.m
@@ -201,7 +201,8 @@
     if (![animationDuration isKindOfClass:[NSNull class]]) {
       animationDuration = @([animationDuration floatValue] / 1000);
     }
-    [self CameraUpdate:ToCameraUpdate(call.arguments[@"cameraUpdate"])
+    [self animateWithCameraUpdate:[FLTGoogleMapJSONConversions
+                                   cameraUpdateFromChannelValue:call.arguments[@"cameraUpdate"]]
                 animationDuration:animationDuration];
     result(nil);
   } else if ([call.method isEqualToString:@"camera#move"]) {
