02394f122b91d2f351e9bf71970077f952bc3870
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2023-09-27 14:11:04 -0400

    extend to 8 min

--- script/tool/lib/src/firebase_test_lab_command.dart
+++ script/tool/lib/src/firebase_test_lab_command.dart
@@ -287,7 +287,7 @@ class FirebaseTestLabCommand extends PackageLoopingCommand {
       '--test',
       'build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk',
       '--timeout',
-      '7m',
+      '8m',
       '--results-bucket=gs://${getStringArg(_gCloudResultsBucketArg)}',
       '--results-dir=$resultsDir',
       for (final String device in getStringListArg('device')) ...<String>[
