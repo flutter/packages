59114c6acdd59f799cd736f4da874979a1c4c397
Gray Mackall <mackall@google.com>
2023-09-28 13:06:58 -0700

    re-enable and see what happens

--- packages/google_sign_in/google_sign_in_android/example/android/app/src/androidTest/java/io/flutter/plugins/googlesigninexample/GoogleSignInTest.java
+++ packages/google_sign_in/google_sign_in_android/example/android/app/src/androidTest/java/io/flutter/plugins/googlesigninexample/GoogleSignInTest.java
@@ -8,13 +8,10 @@ import static org.junit.Assert.assertTrue;
 
 import androidx.test.core.app.ActivityScenario;
 import io.flutter.plugins.googlesignin.GoogleSignInPlugin;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class GoogleSignInTest {
   @Test
-  @Ignore(
-      "This is failing on Firebase Test Lab. See https://github.com/flutter/flutter/issues/135683")
   public void googleSignInPluginIsAdded() {
     final ActivityScenario<GoogleSignInTestActivity> scenario =
         ActivityScenario.launch(GoogleSignInTestActivity.class);
