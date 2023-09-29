bfc03ee90e508cb0eb608902e7e5ba01c3882209
Victoria Ashworth <vashworth@google.com>
2023-09-29 17:20:06 -0500

    Delete preexisting simulators before create new one

--- .ci/scripts/create_simulator.sh
+++ .ci/scripts/create_simulator.sh
@@ -11,7 +11,19 @@ readonly DEVICE_NAME=Flutter-iPhone
 readonly DEVICE=com.apple.CoreSimulator.SimDeviceType.iPhone-14
 readonly OS=com.apple.CoreSimulator.SimRuntime.iOS-16-4
 
+# Delete any existing devices named Flutter-iPhone. Having more than one may
+# cause issues when builds target the device.
+echo -e "Deleting any existing devices names $DEVICE_NAME..."
+RESULT=0
+while [[ $RESULT == 0 ]]; do
+    xcrun simctl delete "$DEVICE_NAME" || RESULT=1
+    if [ $RESULT == 0 ]; then
+        echo -e "Deleted $DEVICE_NAME"
+    fi
+done
+echo -e "\n"
+
 xcrun simctl list
-echo "\n\nCreating $DEVICE_NAME $DEVICE $OS ...\n\n"
+echo -e "\nCreating $DEVICE_NAME $DEVICE $OS ...\n"
 xcrun simctl create "$DEVICE_NAME" "$DEVICE" "$OS" | xargs xcrun simctl boot
 xcrun simctl list
