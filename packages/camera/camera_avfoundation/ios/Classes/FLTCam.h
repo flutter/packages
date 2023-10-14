729b7a2982fc8c2daf40870ecce2c97dfe86767a
Mairramer <mairramer.dasilva28@hotmail.com>
2023-10-14 09:16:14 -0300

    add interface

--- packages/camera/camera_avfoundation/ios/Classes/FLTCam.h
+++ packages/camera/camera_avfoundation/ios/Classes/FLTCam.h
@@ -107,6 +107,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)getMinZoomLevelWithResult:(FLTThreadSafeFlutterResult *)result;
 - (void)setZoomLevel:(CGFloat)zoom Result:(FLTThreadSafeFlutterResult *)result;
 - (void)setUpCaptureSessionForAudio;
+- (void)setImageExtension:(ImageExtension)imageExtension;
 
 @end
 
