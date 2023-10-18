flutter/flutter

commit 293715a6a40fd1e479c64ea082a4263c825661d4

add support to customize Slider interacivity (#121483)
design doc: https://docs.flutter.dev/go/permissible-slider-interaction

closes #113370

open questions:
  - No, as `SliderInteraction.none` doesn't exist anymore.
  - Yes (done)
  - Yes.
    - SliderInteraction
    - SliderAction
    - Slider.allowedInteraction
    - Slider.permissibleInteraction
    - Slider.interaction
    - Slider.allowedAction
    - Slider.permittedAction

packages/flutter/lib/src/material/slider.dart
@@ -34,6 +34,36 @@ typedef PaintValueIndicator = void Function(PaintingContext context, Offset offs
 
 enum _SliderType { material, adaptive }
 
+/// Possible ways for a user to interact with a [Slider].
+enum SliderInteraction {
+  /// Allows the user to interact with a [Slider] by tapping or sliding anywhere
+  /// on the track.
+  ///
+  /// Essentially all possible interactions are allowed.
+  ///
+  /// This is different from [SliderInteraction.slideOnly] as when you try
+  /// to slide anywhere other than the thumb, the thumb will move to the first
+  /// point of contact.
+  tapAndSlide,
+
+  /// Allows the user to interact with a [Slider] by only tapping anywhere on
+  /// the track.
+  ///
+  /// Sliding interaction is ignored.
+  tapOnly,
+
+  /// Allows the user to interact with a [Slider] only by sliding anywhere on
+  /// the track.
+  ///
+  /// Tapping interaction is ignored.
+  slideOnly,
+
+  /// Allows the user to interact with a [Slider] only by sliding the thumb.
+  ///
+  /// Tapping and sliding interactions on the track are ignored.
+  slideThumb;
+}
+
 /// A Material Design slider.
 ///
 /// Used to select from a range of values.
@@ -158,6 +188,7 @@ class Slider extends StatefulWidget {
     this.semanticFormatterCallback,
     this.focusNode,
     this.autofocus = false,
+    this.allowedInteraction,
   }) : _sliderType = _SliderType.material,
        assert(min <= max),
        assert(value >= min && value <= max,
@@ -198,6 +229,7 @@ class Slider extends StatefulWidget {
     this.semanticFormatterCallback,
     this.focusNode,
     this.autofocus = false,
+    this.allowedInteraction,
   }) : _sliderType = _SliderType.adaptive,
        assert(min <= max),
        assert(value >= min && value <= max,
@@ -502,6 +534,15 @@ class Slider extends StatefulWidget {
   /// {@macro flutter.widgets.Focus.autofocus}
   final bool autofocus;
 
+  /// Allowed way for the user to interact with the [Slider].
+  ///
+  /// For example, if this is set to [SliderInteraction.tapOnly], the user can
+  /// interact with the slider only by tapping anywhere on the track. Sliding
+  /// will have no effect.
+  ///
+  /// Defaults to [SliderInteraction.tapAndSlide].
+  final SliderInteraction? allowedInteraction;
+
   final _SliderType _sliderType ;
 
   @override
@@ -753,6 +794,7 @@ class _SliderState extends State<Slider> with TickerProviderStateMixin {
     const SliderComponentShape defaultThumbShape = RoundSliderThumbShape();
     final SliderComponentShape defaultValueIndicatorShape = defaults.valueIndicatorShape!;
     const ShowValueIndicator defaultShowValueIndicator = ShowValueIndicator.onlyForDiscrete;
+    const SliderInteraction defaultAllowedInteraction = SliderInteraction.tapAndSlide;
 
     final Set<MaterialState> states = <MaterialState>{
       if (!_enabled) MaterialState.disabled,
@@ -807,6 +849,9 @@ class _SliderState extends State<Slider> with TickerProviderStateMixin {
     final MouseCursor effectiveMouseCursor = MaterialStateProperty.resolveAs<MouseCursor?>(widget.mouseCursor, states)
       ?? sliderTheme.mouseCursor?.resolve(states)
       ?? MaterialStateMouseCursor.clickable.resolve(states);
+    final SliderInteraction effectiveAllowedInteraction = widget.allowedInteraction
+      ?? sliderTheme.allowedInteraction
+      ?? defaultAllowedInteraction;
 
     // This size is used as the max bounds for the painting of the value
     // indicators It must be kept in sync with the function with the same name
@@ -877,6 +922,7 @@ class _SliderState extends State<Slider> with TickerProviderStateMixin {
             semanticFormatterCallback: widget.semanticFormatterCallback,
             hasFocus: _focused,
             hovering: _hovering,
+            allowedInteraction: effectiveAllowedInteraction,
           ),
         ),
       ),
@@ -940,6 +986,7 @@ class _SliderRenderObjectWidget extends LeafRenderObjectWidget {
     required this.semanticFormatterCallback,
     required this.hasFocus,
     required this.hovering,
+    required this.allowedInteraction,
   });
 
   final double value;
@@ -956,6 +1003,7 @@ class _SliderRenderObjectWidget extends LeafRenderObjectWidget {
   final _SliderState state;
   final bool hasFocus;
   final bool hovering;
+  final SliderInteraction allowedInteraction;
 
   @override
   _RenderSlider createRenderObject(BuildContext context) {
@@ -977,6 +1025,7 @@ class _SliderRenderObjectWidget extends LeafRenderObjectWidget {
       hasFocus: hasFocus,
       hovering: hovering,
       gestureSettings: MediaQuery.gestureSettingsOf(context),
+      allowedInteraction: allowedInteraction,
     );
   }
 
@@ -1000,7 +1049,8 @@ class _SliderRenderObjectWidget extends LeafRenderObjectWidget {
       ..platform = Theme.of(context).platform
       ..hasFocus = hasFocus
       ..hovering = hovering
-      ..gestureSettings = MediaQuery.gestureSettingsOf(context);
+      ..gestureSettings = MediaQuery.gestureSettingsOf(context)
+      ..allowedInteraction = allowedInteraction;
     // Ticker provider cannot change since there's a 1:1 relationship between
     // the _SliderRenderObjectWidget object and the _SliderState object.
   }
@@ -1025,6 +1075,7 @@ class _RenderSlider extends RenderBox with RelayoutWhenSystemFontsChangeMixin {
     required bool hasFocus,
     required bool hovering,
     required DeviceGestureSettings gestureSettings,
+    required SliderInteraction allowedInteraction,
   }) : assert(value >= 0.0 && value <= 1.0),
         assert(secondaryTrackValue == null || (secondaryTrackValue >= 0.0 && secondaryTrackValue <= 1.0)),
         _platform = platform,
@@ -1040,7 +1091,8 @@ class _RenderSlider extends RenderBox with RelayoutWhenSystemFontsChangeMixin {
         _state = state,
         _textDirection = textDirection,
         _hasFocus = hasFocus,
-        _hovering = hovering {
+        _hovering = hovering,
+        _allowedInteraction = allowedInteraction {
     _updateLabelPainter();
     final GestureArenaTeam team = GestureArenaTeam();
     _drag = HorizontalDragGestureRecognizer()
@@ -1294,6 +1346,16 @@ class _RenderSlider extends RenderBox with RelayoutWhenSystemFontsChangeMixin {
     _updateForHover(_hovering);
   }
 
+  SliderInteraction _allowedInteraction;
+  SliderInteraction get allowedInteraction => _allowedInteraction;
+  set allowedInteraction(SliderInteraction value) {
+    if (value == _allowedInteraction) {
+      return;
+    }
+    _allowedInteraction = value;
+    markNeedsSemanticsUpdate();
+  }
+
   void _updateForFocus(bool focused) {
     if (focused) {
       _state.overlayController.forward();
@@ -1423,24 +1485,38 @@ class _RenderSlider extends RenderBox with RelayoutWhenSystemFontsChangeMixin {
   void _startInteraction(Offset globalPosition) {
     _state.showValueIndicator();
     if (!_active && isInteractive) {
-      _active = true;
-      // We supply the *current* value as the start location, so that if we have
-      // a tap, it consists of a call to onChangeStart with the previous value and
-      // a call to onChangeEnd with the new value.
-      onChangeStart?.call(_discretize(value));
-      _currentDragValue = _getValueFromGlobalPosition(globalPosition);
-      onChanged!(_discretize(_currentDragValue));
-      _state.overlayController.forward();
-      if (showValueIndicator) {
-        _state.valueIndicatorController.forward();
-        _state.interactionTimer?.cancel();
-        _state.interactionTimer = Timer(_minimumInteractionTime * timeDilation, () {
-          _state.interactionTimer = null;
-          if (!_active && !hasFocus &&
-              _state.valueIndicatorController.status == AnimationStatus.completed) {
-            _state.valueIndicatorController.reverse();
+      switch (allowedInteraction) {
+        case SliderInteraction.tapAndSlide:
+        case SliderInteraction.tapOnly:
+          _active = true;
+          _currentDragValue = _getValueFromGlobalPosition(globalPosition);
+          onChanged!(_discretize(_currentDragValue));
+        case SliderInteraction.slideThumb:
+          if (_isPointerOnOverlay(globalPosition)) {
+            _active = true;
+            _currentDragValue = value;
           }
-        });
+        case SliderInteraction.slideOnly:
+          break;
+      }
+
+      if (_active) {
+        // We supply the *current* value as the start location, so that if we have
+        // a tap, it consists of a call to onChangeStart with the previous value and
+        // a call to onChangeEnd with the new value.
+        onChangeStart?.call(_discretize(value));
+        _state.overlayController.forward();
+        if (showValueIndicator) {
+          _state.valueIndicatorController.forward();
+          _state.interactionTimer?.cancel();
+          _state.interactionTimer = Timer(_minimumInteractionTime * timeDilation, () {
+            _state.interactionTimer = null;
+            if (!_active && !hasFocus &&
+                _state.valueIndicatorController.status == AnimationStatus.completed) {
+              _state.valueIndicatorController.reverse();
+            }
+          });
+        }
       }
     }
   }
@@ -1473,15 +1549,30 @@ class _RenderSlider extends RenderBox with RelayoutWhenSystemFontsChangeMixin {
       return;
     }
 
-    if (isInteractive) {
-      final double valueDelta = details.primaryDelta! / _trackRect.width;
-      switch (textDirection) {
-        case TextDirection.rtl:
-          _currentDragValue -= valueDelta;
-        case TextDirection.ltr:
-          _currentDragValue += valueDelta;
-      }
-      onChanged!(_discretize(_currentDragValue));
+    // for slide only, there is no start interaction trigger, so _active
+    // will be false and needs to be made true.
+    if (!_active && allowedInteraction == SliderInteraction.slideOnly) {
+      _active = true;
+      _currentDragValue = value;
+    }
+
+    switch (allowedInteraction) {
+      case SliderInteraction.tapAndSlide:
+      case SliderInteraction.slideOnly:
+      case SliderInteraction.slideThumb:
+        if (_active && isInteractive) {
+          final double valueDelta = details.primaryDelta! / _trackRect.width;
+          switch (textDirection) {
+            case TextDirection.rtl:
+              _currentDragValue -= valueDelta;
+            case TextDirection.ltr:
+              _currentDragValue += valueDelta;
+          }
+          onChanged!(_discretize(_currentDragValue));
+        }
+      case SliderInteraction.tapOnly:
+        // cannot slide (drag) as its tapOnly.
+        break;
     }
   }
 
@@ -1495,6 +1586,10 @@ class _RenderSlider extends RenderBox with RelayoutWhenSystemFontsChangeMixin {
 
   void _handleTapUp(TapUpDetails details) {
     _endInteraction();
+  }
+
+  bool _isPointerOnOverlay(Offset globalPosition) {
+    return overlayRect!.contains(globalToLocal(globalPosition));
   }
 
   @override

flutter/packages

commit 4e1f509f57631551f04bd0ac96d4d18d614b711d

Fix `Slider` `onChanged` callback order & never calls `onChangeStart` on  `SliderInteraction.slideOnly` allowed interaction (#136720)
fixes [Slider will call onChanged before onChangeStart when sliding.](https://github.com/flutter/flutter/issues/136707)

This fixes a regression from https://github.com/flutter/flutter/pull/121483

### Code sample

<details>
<summary>expand to view the code sample</summary> 

```dart
import 'package:flutter/material.dart';

void main() => runApp(const MyApp());

class MyApp extends StatelessWidget {
  const MyApp({super.key});

  @override
  Widget build(BuildContext context) {
    return const MaterialApp(
      debugShowCheckedModeBanner: false,
      home: Example(),
    );
  }
}

class Example extends StatefulWidget {
  const Example({super.key});

  @override
  State<Example> createState() => _ExampleState();
}

class _ExampleState extends State<Example> {
  double _sliderValue = 0.5;

  @override
  Widget build(BuildContext context) {
    return Scaffold(
      body: Center(
        child: Slider(
          // allowedInteraction: SliderInteraction.tapAndSlide,
          // allowedInteraction: SliderInteraction.tapOnly,
          // allowedInteraction: SliderInteraction.slideOnly
          // allowedInteraction: SliderInteraction.slideThumb,
          value: _sliderValue,
          onChangeStart: (newValue) {
            print("onChangeStart ......");
          },
          onChanged: (newValue) {
            print("onChanged ......");
            setState(() {
              _sliderValue = newValue;
            });
          },
          onChangeEnd: (newValue) {
            print("onChangeEnd ......");
          },
        ),
      ),
    );
  }
}
```

</details>

packages/flutter/lib/src/material/slider.dart
@@ -1494,14 +1494,13 @@ class _RenderSlider extends RenderBox with RelayoutWhenSystemFontsChangeMixin {
         case SliderInteraction.tapOnly:
           _active = true;
           _currentDragValue = _getValueFromGlobalPosition(globalPosition);
-          onChanged!(_discretize(_currentDragValue));
         case SliderInteraction.slideThumb:
           if (_isPointerOnOverlay(globalPosition)) {
             _active = true;
             _currentDragValue = value;
           }
         case SliderInteraction.slideOnly:
-          break;
+          onChangeStart?.call(_discretize(value));
       }
 
       if (_active) {
@@ -1509,6 +1508,7 @@ class _RenderSlider extends RenderBox with RelayoutWhenSystemFontsChangeMixin {
         // a tap, it consists of a call to onChangeStart with the previous value and
         // a call to onChangeEnd with the new value.
         onChangeStart?.call(_discretize(value));
+        onChanged!(_discretize(_currentDragValue));
         _state.overlayController.forward();
         if (showValueIndicator) {
           _state.valueIndicatorController.forward();

packages/flutter/test/material/slider_test.dart
@@ -3886,6 +3886,7 @@ void main() {
       // (slider's left padding (overlayRadius), windowHeight / 2)
       const Offset startOfTheSliderTrack = Offset(24, 300);
       const Offset centerOfTheSlideTrack = Offset(400, 300);
+      final List<String> logs = <String>[];
 
       Widget buildWidget() => MaterialApp(
         home: Material(
@@ -3895,10 +3896,17 @@ void main() {
                 value: value,
                 key: sliderKey,
                 allowedInteraction: SliderInteraction.tapOnly,
+                onChangeStart: (double newValue) {
+                  logs.add('onChangeStart');
+                },
                 onChanged: (double newValue) {
+                  logs.add('onChanged');
                   setState(() {
                     value = newValue;
                   });
+                },
+                onChangeEnd: (double newValue) {
+                  logs.add('onChangeEnd');
                 },
               );
             }),
@@ -3909,26 +3917,35 @@ void main() {
       // allow tap only
       await tester.pumpWidget(buildWidget());
 
+      expect(logs, isEmpty);
+
       // test tap
       final TestGesture gesture = await tester.startGesture(centerOfTheSlideTrack);
       await tester.pump();
       // changes from 1.0 -> 0.5
       expect(value, 0.5);
+      expect(logs, <String>['onChangeStart', 'onChanged']);
 
       // test slide
       await gesture.moveTo(startOfTheSliderTrack);
       await tester.pump();
       // has no effect, remains 0.5
       expect(value, 0.5);
+      expect(logs, <String>['onChangeStart', 'onChanged']);
+
+      await gesture.up();
+      await tester.pump();
+      expect(logs, <String>['onChangeStart', 'onChanged', 'onChangeEnd']);
     });
 
-    testWidgetsWithLeakTracking('SliderInteraction.tapAndSlide', (WidgetTester tester) async {
+    testWidgetsWithLeakTracking('SliderInteraction.tapAndSlide (default)', (WidgetTester tester) async {
       double value = 1.0;
       final Key sliderKey = UniqueKey();
       // (slider's left padding (overlayRadius), windowHeight / 2)
       const Offset startOfTheSliderTrack = Offset(24, 300);
       const Offset centerOfTheSlideTrack = Offset(400, 300);
       const Offset endOfTheSliderTrack = Offset(800 - 24, 300);
+      final List<String> logs = <String>[];
 
       Widget buildWidget() => MaterialApp(
         home: Material(
@@ -3937,11 +3954,17 @@ void main() {
               return Slider(
                 value: value,
                 key: sliderKey,
-                // allowedInteraction: SliderInteraction.tapAndSlide, // default
+                onChangeStart: (double newValue) {
+                  logs.add('onChangeStart');
+                },
                 onChanged: (double newValue) {
+                  logs.add('onChanged');
                   setState(() {
                     value = newValue;
                   });
+                },
+                onChangeEnd: (double newValue) {
+                  logs.add('onChangeEnd');
                 },
               );
             }),
@@ -3951,11 +3974,14 @@ void main() {
 
       await tester.pumpWidget(buildWidget());
 
+      expect(logs, isEmpty);
+
       // Test tap.
       final TestGesture gesture = await tester.startGesture(centerOfTheSlideTrack);
       await tester.pump();
       // changes from 1.0 -> 0.5
       expect(value, 0.5);
+      expect(logs, <String>['onChangeStart', 'onChanged']);
 
       // test slide
       await gesture.moveTo(startOfTheSliderTrack);
@@ -3966,6 +3992,12 @@ void main() {
       await tester.pump();
       // changes from 0.0 -> 1.0
       expect(value, 1.0);
+      expect(logs, <String>['onChangeStart', 'onChanged', 'onChanged', 'onChanged']);
+
+      await gesture.up();
+      await tester.pump();
+
+      expect(logs, <String>['onChangeStart', 'onChanged', 'onChanged', 'onChanged', 'onChangeEnd']);
     });
 
     testWidgetsWithLeakTracking('SliderInteraction.slideOnly', (WidgetTester tester) async {
@@ -3975,6 +4007,7 @@ void main() {
       const Offset startOfTheSliderTrack = Offset(24, 300);
       const Offset centerOfTheSlideTrack = Offset(400, 300);
       const Offset endOfTheSliderTrack = Offset(800 - 24, 300);
+      final List<String> logs = <String>[];
 
       Widget buildApp() {
         return MaterialApp(
@@ -3985,10 +4018,17 @@ void main() {
                   value: value,
                   key: sliderKey,
                   allowedInteraction: SliderInteraction.slideOnly,
+                  onChangeStart: (double newValue) {
+                    logs.add('onChangeStart');
+                  },
                   onChanged: (double newValue) {
+                    logs.add('onChanged');
                     setState(() {
                       value = newValue;
                     });
+                  },
+                  onChangeEnd: (double newValue) {
+                    logs.add('onChangeEnd');
                   },
                 );
               }),
@@ -3999,11 +4039,14 @@ void main() {
 
       await tester.pumpWidget(buildApp());
 
+      expect(logs, isEmpty);
+
       // test tap
       final TestGesture gesture = await tester.startGesture(centerOfTheSlideTrack);
       await tester.pump();
       // has no effect as tap is disabled, remains 1.0
       expect(value, 1.0);
+      expect(logs, <String>['onChangeStart']);
 
       // test slide
       await gesture.moveTo(startOfTheSliderTrack);
@@ -4014,6 +4057,12 @@ void main() {
       await tester.pump();
       // changes from 0.0 -> 1.0
       expect(value, 1.0);
+      expect(logs, <String>['onChangeStart', 'onChanged', 'onChanged']);
+
+      await gesture.up();
+      await tester.pump();
+
+      expect(logs, <String>['onChangeStart', 'onChanged', 'onChanged', 'onChangeEnd']);
     });
 
     testWidgetsWithLeakTracking('SliderInteraction.slideThumb', (WidgetTester tester) async {
@@ -4023,6 +4072,7 @@ void main() {
       const Offset startOfTheSliderTrack = Offset(24, 300);
       const Offset centerOfTheSliderTrack = Offset(400, 300);
       const Offset endOfTheSliderTrack = Offset(800 - 24, 300);
+      final List<String> logs = <String>[];
 
       Widget buildApp() {
         return MaterialApp(
@@ -4033,10 +4083,17 @@ void main() {
                   value: value,
                   key: sliderKey,
                   allowedInteraction: SliderInteraction.slideThumb,
+                  onChangeStart: (double newValue) {
+                    logs.add('onChangeStart');
+                  },
                   onChanged: (double newValue) {
+                    logs.add('onChanged');
                     setState(() {
                       value = newValue;
                     });
+                  },
+                  onChangeEnd: (double newValue) {
+                    logs.add('onChangeEnd');
                   },
                 );
               }),
@@ -4047,17 +4104,21 @@ void main() {
 
       await tester.pumpWidget(buildApp());
 
+      expect(logs, isEmpty);
+
       // test tap
       final TestGesture gesture = await tester.startGesture(centerOfTheSliderTrack);
       await tester.pump();
       // has no effect, remains 1.0
       expect(value, 1.0);
+      expect(logs, isEmpty);
 
       // test slide
       await gesture.moveTo(startOfTheSliderTrack);
       await tester.pump();
       // has no effect, remains 1.0
       expect(value, 1.0);
+      expect(logs, isEmpty);
 
       // test slide thumb
       await gesture.up();
@@ -4065,22 +4126,36 @@ void main() {
       await tester.pump();
       // has no effect, remains 1.0
       expect(value, 1.0);
+      expect(logs, <String>['onChangeStart']);
+
       await gesture.moveTo(centerOfTheSliderTrack);
       await tester.pump();
       // changes from 1.0 -> 0.5
       expect(value, 0.5);
+      expect(logs, <String>['onChangeStart', 'onChanged']);
 
       // test tap inside overlay but not on thumb, then slide
       await gesture.up();
       // default overlay radius is 12, so 10 is inside the overlay
       await gesture.down(centerOfTheSliderTrack.translate(-10, 0));
       await tester.pump();
-      // has no effect, remains 1.0
+      // changes from 1.0 -> 0.5
       expect(value, 0.5);
+      expect(logs, <String>['onChangeStart', 'onChanged', 'onChangeEnd', 'onChangeStart']);
+
       await gesture.moveTo(endOfTheSliderTrack.translate(-10, 0));
       await tester.pump();
       // changes from 0.5 -> 1.0
       expect(value, 1.0);
+      expect(logs, <String>['onChangeStart', 'onChanged', 'onChangeEnd', 'onChangeStart', 'onChanged']);
+
+      await gesture.up();
+      await tester.pump();
+
+      expect(
+        logs,
+        <String>['onChangeStart', 'onChanged', 'onChangeEnd', 'onChangeStart', 'onChanged', 'onChangeEnd'],
+      );
     });
   });
 }