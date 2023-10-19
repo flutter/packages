4208c529e42c14d81152db9c281ebc1c062a635b
Kate Lovett <katelovett@google.com>
2023-10-19 18:16:27 -0500

    Fix typo

--- packages/two_dimensional_scrollables/test/table_view/table_test.dart
+++ packages/two_dimensional_scrollables/test/table_view/table_test.dart
@@ -949,7 +949,7 @@ void main() {
     testWidgets('paints decorations in correct order',
         (WidgetTester tester) async {
       // TODO(Piinks): Rewrite this to remove golden files from this repo when
-      //  mock_canvas is public - https://github.com/flutter/flutter/pull/131631
+      //  mock_canvas is public - https://github.com/flutter/flutter/issues/136933
       // foreground, background, and precedence per mainAxis
       TableView tableView = TableView.builder(
         rowCount: 2,
@@ -1050,10 +1050,12 @@ void main() {
 
     testWidgets('paint rects are correct when reversed and pinned',
         (WidgetTester tester) async {
+      // Regression test for https://github.com/flutter/flutter/issues/135386
       // TODO(Piinks): Rewrite this to remove golden files from this repo when
-      //  mock_canvas is public - https://github.com/flutter/flutter/pull/131631
+      //  mock_canvas is public - https://github.com/flutter/flutter/issues/136933
       // foreground, background, and precedence per mainAxis
-      final TableView tableView = TableView.builder(
+      TableView tableView = TableView.builder(
+        // Both axes reversed
         verticalDetails: const ScrollableDetails.vertical(reverse: true),
         horizontalDetails: const ScrollableDetails.horizontal(reverse: true),
         rowCount: 2,
@@ -1101,6 +1103,57 @@ void main() {
       await expectLater(
         find.byType(TableView),
         matchesGoldenFile('goldens/reversed.pinned.painting.png'),
+        skip: !runGoldens,
+      );
+
+      tableView = TableView.builder(
+        // Only one axis reversed
+        horizontalDetails: const ScrollableDetails.horizontal(reverse: true),
+        rowCount: 2,
+        pinnedRowCount: 1,
+        columnCount: 2,
+        pinnedColumnCount: 1,
+        columnBuilder: (int index) => TableSpan(
+          extent: const FixedTableSpanExtent(200.0),
+          foregroundDecoration: const TableSpanDecoration(
+            border: TableSpanBorder(
+              trailing: BorderSide(
+                color: Colors.orange,
+                width: 3,
+              ),
+            ),
+          ),
+          backgroundDecoration: TableSpanDecoration(
+            color: index.isEven ? Colors.red : null,
+          ),
+        ),
+        rowBuilder: (int index) => TableSpan(
+          extent: const FixedTableSpanExtent(200.0),
+          foregroundDecoration: const TableSpanDecoration(
+            border: TableSpanBorder(
+              leading: BorderSide(
+                color: Colors.green,
+                width: 3,
+              ),
+            ),
+          ),
+          backgroundDecoration: TableSpanDecoration(
+            color: index.isOdd ? Colors.blue : null,
+          ),
+        ),
+        cellBuilder: (_, TableVicinity vicinity) {
+          return const SizedBox.square(
+            dimension: 200,
+            child: Center(child: FlutterLogo()),
+          );
+        },
+      );
+
+      await tester.pumpWidget(MaterialApp(home: tableView));
+      await tester.pumpAndSettle();
+      await expectLater(
+        find.byType(TableView),
+        matchesGoldenFile('goldens/single-reversed.pinned.painting.png'),
         skip: !runGoldens,
       );
     });
