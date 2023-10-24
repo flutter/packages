78bcf3e069ba4edbc73a44dadc06202e1736ffd1
Stuart Morgan <stuartmorgan@google.com>
2023-10-24 14:03:55 -0400

    Adjust example to use browser view for simple case

--- packages/url_launcher/url_launcher/example/lib/main.dart
+++ packages/url_launcher/url_launcher/example/lib/main.dart
@@ -62,7 +62,13 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  Future<void> _launchInWebViewOrVC(Uri url) async {
+  Future<void> _launchInBrowserView(Uri url) async {
+    if (!await launchUrl(url, mode: LaunchMode.inAppBrowserView)) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+  Future<void> _launchInWebView(Uri url) async {
     if (!await launchUrl(url, mode: LaunchMode.inAppWebView)) {
       throw Exception('Could not launch $url');
     }
@@ -173,7 +179,7 @@ class _MyHomePageState extends State<MyHomePage> {
               const Padding(padding: EdgeInsets.all(16.0)),
               ElevatedButton(
                 onPressed: () => setState(() {
-                  _launched = _launchInWebViewOrVC(toLaunch);
+                  _launched = _launchInBrowserView(toLaunch);
                 }),
                 child: const Text('Launch in app'),
               ),
@@ -206,7 +212,7 @@ class _MyHomePageState extends State<MyHomePage> {
               const Padding(padding: EdgeInsets.all(16.0)),
               ElevatedButton(
                 onPressed: () => setState(() {
-                  _launched = _launchInWebViewOrVC(toLaunch);
+                  _launched = _launchInWebView(toLaunch);
                   Timer(const Duration(seconds: 5), () {
                     closeInAppWebView();
                   });
