e1395df2fe097143387afcd31d16f5da432b9688
Sviatoslav Maniuk <sviatoslav.maniuk@lanars.com>
2023-10-24 12:47:35 +0300

    [image_picker_platform_interface] Add option to limit max number of photos that can be picked

--- packages/image_picker/image_picker_platform_interface/lib/src/types/multi_image_picker_options.dart
+++ packages/image_picker/image_picker_platform_interface/lib/src/types/multi_image_picker_options.dart
@@ -9,8 +9,32 @@ class MultiImagePickerOptions {
   /// Creates an instance with the given [imageOptions].
   const MultiImagePickerOptions({
     this.imageOptions = const ImageOptions(),
+    this.maxImages,
   });
+
+  /// Creates an instance with the given [imageOptions] and [maxImages].
+  /// Throws if options are not valid.
+  MultiImagePickerOptions.createAndValidate({
+    this.imageOptions = const ImageOptions(),
+    this.maxImages,
+  }) {
+    _validateOptions(
+      maxImages: maxImages,
+    );
+  }
 
   /// The image-specific options for picking.
   final ImageOptions imageOptions;
+
+  /// Maximum number of images that can be picked.
+  ///
+  /// If null, the platform limits the maximum number of images.
+  final int? maxImages;
+
+  /// Validates that all values are within required ranges. Throws if not.
+  static void _validateOptions({int? maxImages}) {
+    if (maxImages != null && maxImages < 1) {
+      throw ArgumentError.value(maxImages, 'maxImages', 'must be positive');
+    }
+  }
 }
