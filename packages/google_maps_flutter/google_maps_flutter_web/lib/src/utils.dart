a0637f183b0b3f930f29400fc1fe857ace0de377
Navaron Bracke <brackenavaron@gmail.com>
2023-10-28 17:08:38 +0200

    add utility function to create divs

---
+++ packages/google_maps_flutter/google_maps_flutter_web/lib/src/utils.dart
@@ -0,0 +1,6 @@
+import 'package:web/web.dart';
+
+/// Convenience method to create a new [HTMLDivElement] element.
+HTMLDivElement createDivElement() {
+  return document.createElement('div') as HTMLDivElement;
+}
