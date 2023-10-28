98987373a067ba6d3647f699e3a769bfe3c3b3d2
Navaron Bracke <brackenavaron@gmail.com>
2023-10-28 17:45:39 +0200

    migrate another markers test

--- packages/google_maps_flutter/google_maps_flutter_web/example/integration_test/markers_test.dart
+++ packages/google_maps_flutter/google_maps_flutter_web/example/integration_test/markers_test.dart
@@ -4,7 +4,6 @@
 
 import 'dart:async';
 import 'dart:convert';
-import 'dart:html' as html;
 import 'dart:typed_data';
 import 'dart:ui';
 
@@ -14,8 +13,10 @@ import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platf
 import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:http/http.dart' as http;
 import 'package:integration_test/integration_test.dart';
+import 'package:web/web.dart';
 
 import 'resources/icon_image_base64.dart';
+import 'test_utils.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -28,7 +29,7 @@ void main() {
     setUp(() {
       events = StreamController<MapEvent<Object?>>();
       controller = MarkersController(stream: events);
-      map = gmaps.GMap(html.DivElement());
+      map = gmaps.GMap(createDivElement());
       controller.bindToMap(123, map);
     });
 
@@ -274,11 +275,11 @@ void main() {
       controller.addMarkers(markers);
 
       expect(controller.markers.length, 1);
-      final html.HtmlElement? content = controller.markers[const MarkerId('1')]
-          ?.infoWindow?.content as html.HtmlElement?;
-      expect(content?.innerHtml, contains('title for test'));
+      final HTMLElement? content = controller.markers[const MarkerId('1')]
+          ?.infoWindow?.content as HTMLElement?;
+      expect(content?.innerHTML, contains('title for test'));
       expect(
-          content?.innerHtml,
+          content?.innerHTML,
           contains(
             '<a href="https://www.google.com">Go to Google &gt;&gt;&gt;</a>',
           ));
@@ -299,8 +300,8 @@ void main() {
       controller.addMarkers(markers);
 
       expect(controller.markers.length, 1);
-      final html.HtmlElement? content = controller.markers[const MarkerId('1')]
-          ?.infoWindow?.content as html.HtmlElement?;
+      final HTMLElement? content = controller.markers[const MarkerId('1')]
+          ?.infoWindow?.content as HTMLElement?;
 
       content?.click();
 
