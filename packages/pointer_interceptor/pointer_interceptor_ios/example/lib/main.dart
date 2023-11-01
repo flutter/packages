f968902468af6be8f7ae3c4d9c7f126e218b3f8f
Louise Hsu <louisehsu@google.com>
2023-10-31 23:18:28 -0700

    remove extra test files

--- packages/pointer_interceptor/pointer_interceptor_ios/example/lib/main.dart
+++ packages/pointer_interceptor/pointer_interceptor_ios/example/lib/main.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:pointer_interceptor_ios/pointer_interceptor_ios.dart';
-import 'package:pointer_interceptor/pointer_interceptor.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:pointer_interceptor_ios/pointer_interceptor_ios.dart';
 
 void main() {
-  // platformViewRegistry.registerViewFactory('plugins.flutter.dev/pointer_interceptor_ios', viewFactory)
   runApp(const MaterialApp(home: PointerInterceptorIOSExample()));
 }
 
-class DummyPlatformView extends StatelessWidget {
-  const DummyPlatformView({super.key});
+class _DummyPlatformView extends StatelessWidget {
+  const _DummyPlatformView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This is used in the platform side to register the view.
     const String viewType = 'dummy_platform_view';
-    // Pass parameters to the platform side.
     final Map<String, dynamic> creationParams = <String, dynamic>{};
 
     return UiKitView(
@@ -28,19 +23,21 @@ class DummyPlatformView extends StatelessWidget {
   }
 }
 
+/// Example flutter app with a button overlaying the native view
 class PointerInterceptorIOSExample extends StatefulWidget {
+  /// Constructor
   const PointerInterceptorIOSExample({super.key});
 
   @override
   State<StatefulWidget> createState() {
-    return PointerInterceptorIOSExampleState();
+    return _PointerInterceptorIOSExampleState();
   }
 }
 
-class PointerInterceptorIOSExampleState
+class _PointerInterceptorIOSExampleState
     extends State<PointerInterceptorIOSExample> {
 
-  var buttonTapped = false;
+  bool _buttonTapped = false;
 
   @override
   Widget build(BuildContext context) {
@@ -49,17 +46,17 @@ class PointerInterceptorIOSExampleState
       child: Stack(
         alignment: AlignmentDirectional.center,
         children: [
-          const DummyPlatformView(),
+          const _DummyPlatformView(),
           PointerInterceptorIOSPlugin().buildWidget(
               debug: true,
               child: TextButton(
                   style: TextButton.styleFrom(foregroundColor: Colors.red),
-                  child: buttonTapped
-                      ? const Text("Tapped")
-                      : const Text("Initial"),
+                  child: _buttonTapped
+                      ? const Text('Tapped')
+                      : const Text('Initial'),
                   onPressed: () {
                     setState(() {
-                      buttonTapped = !buttonTapped;
+                      _buttonTapped = !_buttonTapped;
                     });
                   })),
         ],
