2bcdde2303357ef0f440d350da6dd39e060dff3f
Louise Hsu <louisehsu@google.com>
2023-11-07 13:28:39 -0800

    update ios test

--- packages/pointer_interceptor/pointer_interceptor_ios/test/pointer_interceptor_ios_test.dart
+++ packages/pointer_interceptor/pointer_interceptor_ios/test/pointer_interceptor_ios_test.dart
@@ -1,30 +1,44 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:pointer_interceptor_ios/pointer_interceptor_ios.dart';
 
-final Completer<WebViewController> _controller = Completer<WebViewController>();
+class TestApp extends StatefulWidget {
+  const TestApp({super.key});
 
-Widget boilerplate() {
-  return MaterialApp(
-      home: Scaffold(
-        body: const DummyPlatformView(),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {  },
-            child: PointerInterceptorIOSPlugin().buildWidget(child: child)),
-      ));
+  @override
+  State<StatefulWidget> createState() {
+    return TestAppState();
+  }
 }
 
-/// Find the tests in the
+class TestAppState extends State<TestApp> {
+  String _buttonText = 'Test Button';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: const Text('Body'),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: PointerInterceptorIOSPlugin().buildWidget(
+              child: TextButton(
+            onPressed: () => setState(() {
+              _buttonText = 'Clicked';
+            }),
+            child: Text(_buttonText),
+          ))),
+    ));
+  }
+}
 
 void main() {
-  testWidgets('Button remains clickable and is added to '
-      'hierarchy after being wrapped in pointer interceptor', (WidgetTester tester) async {
-        await tester.pumpWidget(boilerplate());
-        await tester.pump();
-// Test by adding a new html element when clicked?
-      });
+  testWidgets(
+      'Button remains clickable and is added to '
+      'hierarchy after being wrapped in pointer interceptor',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const TestApp());
+    await tester.tap(find.text('Test Button'));
+    expect(find.text('Clicked'), findsOneWidget);
+  });
 }
