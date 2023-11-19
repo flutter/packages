a5a04ca28b78645c8f89cf76602a4f66da33e6fa
Nitin <nitinpoojary00@gmail.com>
2023-11-19 18:20:24 +0530

    passing image extension as assetkey

--- packages/flutter_image/example/lib/main.dart
+++ packages/flutter_image/example/lib/main.dart
@@ -14,13 +14,13 @@ class MyApp extends StatelessWidget {
 
   /// Returns the URL to load an asset from this example app as a network source.
   String getUrlForAssetAsNetworkSource(String assetKey) {
-    return 'https://github.com/flutter/packages/blob/b96a6dae0ca418cf1e91633f275866aa9cffe437/packages/flutter_image/example/$assetKey.png?raw=true';
+    return 'https://github.com/flutter/packages/blob/b96a6dae0ca418cf1e91633f275866aa9cffe437/packages/flutter_image/example/$assetKey?raw=true';
   }
 
   @override
   Widget build(BuildContext context) {
     final String imageUrl =
-        getUrlForAssetAsNetworkSource('assets/flutter-mark-square-64');
+        getUrlForAssetAsNetworkSource('assets/flutter-mark-square-64.png');
 
     return MaterialApp(
       title: 'flutter_image example app',
