1f256a747b37d660db6fcb06f2a18cd16a6d815a
Jemish Vasoya <jempatellb@gmail.com>
2023-11-30 18:38:54 +0530

    formatted correctly

--- packages/css_colors/example/windows/runner/main.cpp
+++ packages/css_colors/example/windows/runner/main.cpp
@@ -9,39 +9,55 @@
 #include "flutter_window.h"
 #include "utils.h"
 
-int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t *command_line, _In_ int show_command) {
-  // Attach to console when present (e.g., 'flutter run') or create a
-  // new console when running with a debugger.
-  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
-    CreateAndAttachConsole();
-  }
+int APIENTRY
+wWinMain(_In_
+HINSTANCE instance, _In_opt_
+HINSTANCE prev,
+        _In_
+wchar_t *command_line, _In_
+int show_command
+) {
+// Attach to console when present (e.g., 'flutter run') or create a
+// new console when running with a debugger.
+if (!
+::AttachConsole(ATTACH_PARENT_PROCESS)
+&& ::IsDebuggerPresent()) {
+CreateAndAttachConsole();
 
-  // Initialize COM, so that it is available for use in the library and/or
-  // plugins.
-  ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+}
 
-  flutter::DartProject project(L"data");
+// Initialize COM, so that it is available for use in the library and/or
+// plugins.
+::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED
+);
 
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
+flutter::DartProject project(L"data");
 
-  project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
+std::vector <std::string> command_line_arguments =
+        GetCommandLineArguments();
 
-  FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.Create(L"example", origin, size)) {
-    return EXIT_FAILURE;
-  }
-  window.SetQuitOnClose(true);
+project.
+set_dart_entrypoint_arguments(std::move(command_line_arguments)
+);
 
-  ::MSG msg;
-  while (::GetMessage(&msg, nullptr, 0, 0)) {
-    ::TranslateMessage(&msg);
-    ::DispatchMessage(&msg);
-  }
+FlutterWindow window(project);
+Win32Window::Point origin(10, 10);
+Win32Window::Size size(1280, 720);
+if (!window.Create(L"example", origin, size)) {
+return
+EXIT_FAILURE;
+}
+window.SetQuitOnClose(true);
 
-  ::CoUninitialize();
-  return EXIT_SUCCESS;
+::MSG msg;
+while (
+::GetMessage(&msg, nullptr,
+0, 0)) {
+::TranslateMessage(&msg);
+::DispatchMessage(&msg);
+}
+
+::CoUninitialize();
+return
+EXIT_SUCCESS;
 }
--- packages/css_colors/example/windows/runner/win32_window.h
+++ packages/css_colors/example/windows/runner/win32_window.h
@@ -15,92 +15,104 @@
 // inherited from by classes that wish to specialize with custom
 // rendering and input handling
 class Win32Window {
- public:
-  struct Point {
-    unsigned int x;
-    unsigned int y;
-    Point(unsigned int x, unsigned int y) : x(x), y(y) {}
-  };
+public:
+    struct Point {
+        unsigned int x;
+        unsigned int y;
 
-  struct Size {
-    unsigned int width;
-    unsigned int height;
-    Size(unsigned int width, unsigned int height)
-        : width(width), height(height) {}
-  };
+        Point(unsigned int x, unsigned int y) : x(x), y(y) {}
+    };
 
-  Win32Window();
-  virtual ~Win32Window();
+    struct Size {
+        unsigned int width;
+        unsigned int height;
 
-  // Creates a win32 window with |title| that is positioned and sized using
-  // |origin| and |size|. New windows are created on the default monitor. Window
-  // sizes are specified to the OS in physical pixels, hence to ensure a
-  // consistent size this function will scale the inputted width and height as
-  // as appropriate for the default monitor. The window is invisible until
-  // |Show| is called. Returns true if the window was created successfully.
-  bool Create(const std::wstring& title, const Point& origin, const Size& size);
+        Size(unsigned int width, unsigned int height)
+                : width(width), height(height) {}
+    };
 
-  // Show the current window. Returns true if the window was successfully shown.
-  bool Show();
+    Win32Window();
 
-  // Release OS resources associated with window.
-  void Destroy();
+    virtual ~Win32Window();
 
-  // Inserts |content| into the window tree.
-  void SetChildContent(HWND content);
+    // Creates a win32 window with |title| that is positioned and sized using
+    // |origin| and |size|. New windows are created on the default monitor. Window
+    // sizes are specified to the OS in physical pixels, hence to ensure a
+    // consistent size this function will scale the inputted width and height as
+    // as appropriate for the default monitor. The window is invisible until
+    // |Show| is called. Returns true if the window was created successfully.
+    bool Create(const std::wstring &title, const Point &origin, const Size &size);
 
-  // Returns the backing Window handle to enable clients to set icon and other
-  // window properties. Returns nullptr if the window has been destroyed.
-  HWND GetHandle();
+    // Show the current window. Returns true if the window was successfully shown.
+    bool Show();
 
-  // If true, closing this window will quit the application.
-  void SetQuitOnClose(bool quit_on_close);
+    // Release OS resources associated with window.
+    void Destroy();
 
-  // Return a RECT representing the bounds of the current client area.
-  RECT GetClientArea();
+    // Inserts |content| into the window tree.
+    void SetChildContent(HWND content);
 
- protected:
-  // Processes and route salient window messages for mouse handling,
-  // size change and DPI. Delegates handling of these to member overloads that
-  // inheriting classes can handle.
-  virtual LRESULT MessageHandler(HWND window,
-                                 UINT const message,
-                                 WPARAM const wparam,
-                                 LPARAM const lparam) noexcept;
+    // Returns the backing Window handle to enable clients to set icon and other
+    // window properties. Returns nullptr if the window has been destroyed.
+    HWND GetHandle();
 
-  // Called when CreateAndShow is called, allowing subclass window-related
-  // setup. Subclasses should return false if setup fails.
-  virtual bool OnCreate();
+    // If true, closing this window will quit the application.
+    void SetQuitOnClose(bool quit_on_close);
 
-  // Called when Destroy is called.
-  virtual void OnDestroy();
+    // Return a RECT representing the bounds of the current client area.
+    RECT GetClientArea();
 
- private:
-  friend class WindowClassRegistrar;
+protected:
+    // Processes and route salient window messages for mouse handling,
+    // size change and DPI. Delegates handling of these to member overloads that
+    // inheriting classes can handle.
+    virtual LRESULT MessageHandler(HWND window,
+                                   UINT const message,
+                                   WPARAM const wparam,
+                                   LPARAM const lparam)
 
-  // OS callback called by message pump. Handles the WM_NCCREATE message which
-  // is passed when the non-client area is being created and enables automatic
-  // non-client DPI scaling so that the non-client area automatically
-  // responds to changes in DPI. All other messages are handled by
-  // MessageHandler.
-  static LRESULT CALLBACK WndProc(HWND const window,
-                                  UINT const message,
-                                  WPARAM const wparam,
-                                  LPARAM const lparam) noexcept;
+    noexcept;
 
-  // Retrieves a class instance pointer for |window|
-  static Win32Window* GetThisFromHandle(HWND const window) noexcept;
+    // Called when CreateAndShow is called, allowing subclass window-related
+    // setup. Subclasses should return false if setup fails.
+    virtual bool OnCreate();
 
-  // Update the window frame's theme to match the system theme.
-  static void UpdateTheme(HWND const window);
+    // Called when Destroy is called.
+    virtual void OnDestroy();
 
-  bool quit_on_close_ = false;
+private:
+    friend class WindowClassRegistrar;
 
-  // window handle for top level window.
-  HWND window_handle_ = nullptr;
+    // OS callback called by message pump. Handles the WM_NCCREATE message which
+    // is passed when the non-client area is being created and enables automatic
+    // non-client DPI scaling so that the non-client area automatically
+    // responds to changes in DPI. All other messages are handled by
+    // MessageHandler.
+    static LRESULT CALLBACK
+    WndProc(HWND
+    const window,
+    UINT const message,
+            WPARAM
+    const wparam,
+    LPARAM const lparam
+    )
+    noexcept;
 
-  // window handle for hosted content.
-  HWND child_content_ = nullptr;
+    // Retrieves a class instance pointer for |window|
+    static Win32Window *GetThisFromHandle(HWND const window)
+
+    noexcept;
+
+    // Update the window frame's theme to match the system theme.
+    static void UpdateTheme(HWND const window);
+
+    bool quit_on_close_ = false;
+
+    // window handle for top level window.
+    HWND window_handle_ = nullptr;
+
+    // window handle for hosted content.
+    HWND child_content_ = nullptr;
 };
 
 #endif  // RUNNER_WIN32_WINDOW_H_
