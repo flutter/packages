2dc9effc338f44a0e6a0c67fb755ceb38748fa6f
Jemish Vasoya <jempatellb@gmail.com>
2023-11-30 18:52:35 +0530

    Update widget_test.dart

--- packages/css_colors/example/test/widget_test.dart
+++ packages/css_colors/example/test/widget_test.dart
@@ -2,4 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-void main() {}
+import 'package:example/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+  });
+}
