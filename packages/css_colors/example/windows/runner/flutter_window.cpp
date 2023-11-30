ea19846bdf8bd0925b3d818e4b4c00f40135e085
Jemish Vasoya <jempatellb@gmail.com>
2023-11-30 22:13:05 +0530

    issue fix

--- packages/css_colors/example/windows/runner/flutter_window.cpp
+++ packages/css_colors/example/windows/runner/flutter_window.cpp
@@ -9,60 +9,60 @@
 #include "flutter/generated_plugin_registrant.h"
 
 FlutterWindow::FlutterWindow(const flutter::DartProject& project)
-        : project_(project) {}
+    : project_(project) {}
 
 FlutterWindow::~FlutterWindow() {}
 
 bool FlutterWindow::OnCreate() {
-    if (!Win32Window::OnCreate()) {
-        return false;
-    }
+  if (!Win32Window::OnCreate()) {
+    return false;
+  }
 
-    RECT frame = GetClientArea();
+  RECT frame = GetClientArea();
 
-    // The size here must match the window dimensions to avoid unnecessary surface
-    // creation / destruction in the startup path.
-    flutter_controller_ = std::make_unique<flutter::FlutterViewController>(
-            frame.right - frame.left, frame.bottom - frame.top, project_);
-    // Ensure that basic setup of the controller was successful.
-    if (!flutter_controller_->engine() || !flutter_controller_->view()) {
-        return false;
-    }
-    RegisterPlugins(flutter_controller_->engine());
-    SetChildContent(flutter_controller_->view()->GetNativeWindow());
+  // The size here must match the window dimensions to avoid unnecessary surface
+  // creation / destruction in the startup path.
+  flutter_controller_ = std::make_unique<flutter::FlutterViewController>(
+      frame.right - frame.left, frame.bottom - frame.top, project_);
+  // Ensure that basic setup of the controller was successful.
+  if (!flutter_controller_->engine() || !flutter_controller_->view()) {
+    return false;
+  }
+  RegisterPlugins(flutter_controller_->engine());
+  SetChildContent(flutter_controller_->view()->GetNativeWindow());
 
-    flutter_controller_->engine()->SetNextFrameCallback([&]() { this->Show(); });
+  flutter_controller_->engine()->SetNextFrameCallback([&]() { this->Show(); });
 
-    return true;
+  return true;
 }
 
 void FlutterWindow::OnDestroy() {
-    if (flutter_controller_) {
-        flutter_controller_ = nullptr;
-    }
+  if (flutter_controller_) {
+    flutter_controller_ = nullptr;
+  }
 
-    Win32Window::OnDestroy();
+  Win32Window::OnDestroy();
 }
 
 LRESULT
 FlutterWindow::MessageHandler(HWND hwnd, UINT const message,
                               WPARAM const wparam,
                               LPARAM const lparam) noexcept {
-// Give Flutter, including plugins, an opportunity to handle window messages.
-if (flutter_controller_) {
-std::optional<LRESULT> result =
+  // Give Flutter, including plugins, an opportunity to handle window messages.
+  if (flutter_controller_) {
+    std::optional<LRESULT> result =
         flutter_controller_->HandleTopLevelWindowProc(hwnd, message, wparam,
                                                       lparam);
-if (result) {
-return *result;
-}
-}
+    if (result) {
+      return *result;
+    }
+  }
 
-switch (message) {
-case WM_FONTCHANGE:
-flutter_controller_->engine()->ReloadSystemFonts();
-break;
-}
+  switch (message) {
+    case WM_FONTCHANGE:
+      flutter_controller_->engine()->ReloadSystemFonts();
+      break;
+  }
 
-return Win32Window::MessageHandler(hwnd, message, wparam, lparam);
+  return Win32Window::MessageHandler(hwnd, message, wparam, lparam);
 }
--- packages/css_colors/example/windows/runner/main.cpp
+++ packages/css_colors/example/windows/runner/main.cpp
@@ -10,37 +10,37 @@
 #include "utils.h"
 
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-        _In_ wchar_t* command_line, _In_ int show_command) {
-// Attach to console when present (e.g., 'flutter run') or create a
-// new console when running with a debugger.
-if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
-CreateAndAttachConsole();
-}
+                      _In_ wchar_t* command_line, _In_ int show_command) {
+  // Attach to console when present (e.g., 'flutter run') or create a
+  // new console when running with a debugger.
+  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+    CreateAndAttachConsole();
+  }
 
-// Initialize COM, so that it is available for use in the library and/or
-// plugins.
-::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+  // Initialize COM, so that it is available for use in the library and/or
+  // plugins.
+  ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
-flutter::DartProject project(L"data");
+  flutter::DartProject project(L"data");
 
-std::vector<std::string> command_line_arguments = GetCommandLineArguments();
+  std::vector<std::string> command_line_arguments = GetCommandLineArguments();
 
-project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
+  project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
-FlutterWindow window(project);
-Win32Window::Point origin(10, 10);
-Win32Window::Size size(1280, 720);
-if (!window.Create(L"example", origin, size)) {
-return EXIT_FAILURE;
-}
-window.SetQuitOnClose(true);
+  FlutterWindow window(project);
+  Win32Window::Point origin(10, 10);
+  Win32Window::Size size(1280, 720);
+  if (!window.Create(L"example", origin, size)) {
+    return EXIT_FAILURE;
+  }
+  window.SetQuitOnClose(true);
 
-::MSG msg;
-while (::GetMessage(&msg, nullptr, 0, 0)) {
-::TranslateMessage(&msg);
-::DispatchMessage(&msg);
-}
+  ::MSG msg;
+  while (::GetMessage(&msg, nullptr, 0, 0)) {
+    ::TranslateMessage(&msg);
+    ::DispatchMessage(&msg);
+  }
 
-::CoUninitialize();
-return EXIT_SUCCESS;
+  ::CoUninitialize();
+  return EXIT_SUCCESS;
 }
