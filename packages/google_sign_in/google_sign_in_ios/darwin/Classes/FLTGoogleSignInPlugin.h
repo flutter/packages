afd451711a77473c76d35a4286f105b3674c85e4
stuartmorgan <stuartmorgan@google.com>
2023-12-06 10:42:06 -0800

    [google_sign_in] Add macOS support (#4962)
    Adds macOS support to `google_sign_in_ios` by moving the existing source to `darwin/`, adjusting it somewhat to be macOS-compatible, and enabling `sharedDarwinSource` in the podspec.
    
    Unlike previous cases of code sharing, this doesn't rename the package to be something more generic, since when we do that the name is based on the underlying SDK, and the SDK is still mostly called "Google Sign-In for iOS", so I don't think there's a better name to give it at the moment.
    
    Most of https://github.com/flutter/flutter/issues/46157
    (Once this lands, there will be a follow-up PR to update the app-facing package to endorse it and list it in the README.)

--- packages/google_sign_in/google_sign_in_ios/darwin/Classes/FLTGoogleSignInPlugin.h
+++ packages/google_sign_in/google_sign_in_ios/darwin/Classes/FLTGoogleSignInPlugin.h
@@ -2,10 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#if TARGET_OS_OSX
+#import <FlutterMacOS/FlutterMacOS.h>
+#else
 #import <Flutter/Flutter.h>
+#endif
 
 #import "messages.g.h"
 
 @interface FLTGoogleSignInPlugin : NSObject <FlutterPlugin, FSIGoogleSignInApi>
-
+- (instancetype)init NS_UNAVAILABLE;
 @end
