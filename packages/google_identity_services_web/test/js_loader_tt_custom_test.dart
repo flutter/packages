6aaf828781be232c20d0eb1687880a5b2683d8d8
David Iglesias <ditman@gmail.com>
2023-12-05 16:45:48 -0800

    Don't lie on some comments.
    Co-authored-by: Kevin Moore <kevmoo@users.noreply.github.com>

--- packages/google_identity_services_web/test/js_loader_tt_custom_test.dart
+++ packages/google_identity_services_web/test/js_loader_tt_custom_test.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-@TestOn('browser') // Uses package:js
+@TestOn('browser') // Uses package:web
 
 import 'package:google_identity_services_web/loader.dart';
 import 'package:test/test.dart';
