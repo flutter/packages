1a8b46477aed6d02334cbc285689fd4a8ff76cc6
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2023-12-26 16:52:05 -0500

    make all test callback methods nullable

--- packages/pigeon/pigeons/core_tests.dart
+++ packages/pigeon/pigeons/core_tests.dart
@@ -818,7 +818,7 @@ abstract class ProxyIntegrationCoreApi extends ProxyApiSuperClass
   late AnEnum Function(AnEnum anEnum)? flutterEchoEnum;
 
   /// Returns the passed ProxyApi to test serialization and deserialization.
-  late ProxyApiSuperClass Function(ProxyApiSuperClass aProxyApi)
+  late ProxyApiSuperClass Function(ProxyApiSuperClass aProxyApi)?
       flutterEchoProxyApi;
 
   // ========== Nullable argument/return type tests ==========
@@ -849,7 +849,7 @@ abstract class ProxyIntegrationCoreApi extends ProxyApiSuperClass
   late AnEnum? Function(AnEnum? anEnum)? flutterEchoNullableEnum;
 
   /// Returns the passed ProxyApi to test serialization and deserialization.
-  late ProxyApiSuperClass? Function(ProxyApiSuperClass? aProxyApi)
+  late ProxyApiSuperClass? Function(ProxyApiSuperClass? aProxyApi)?
       flutterEchoNullableProxyApi;
 
   // ========== Async tests ==========
