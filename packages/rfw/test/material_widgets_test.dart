06b3677a5674ed04306c011404ac21aba4f65f85
Peixin Li <peixinli@google.com>
2023-12-27 14:26:47 -0800

    test value type

--- packages/rfw/test/material_widgets_test.dart
+++ packages/rfw/test/material_widgets_test.dart
@@ -126,33 +126,50 @@ void main() {
             Divider(),
             Padding(
               padding: [20.0],
-              child: Center(
-                child: DropdownButton(
-                  value: 'foo',
-                  elevation: 14,
-                  dropdownColor: 0xFF9E9E9E,
-                  underline: Container(
-                    height: 2,
-                    color: 0xFF7C4DFF,
+              child: Row(
+                mainAxisAlignment: 'spaceEvenly',
+                children: [
+                  DropdownButton(
+                    value: 'foo',
+                    elevation: 14,
+                    dropdownColor: 0xFF9E9E9E,
+                    underline: Container(
+                      height: 2,
+                      color: 0xFF7C4DFF,
+                    ),
+                    style: {
+                      color:0xFF7C4DFF,
+                    },
+                    items: [
+                      {
+                        value: 'foo',
+                        child: Text(text: 'foo'),
+                      },
+                      {
+                        value: 'bar',
+                        child: Text(text: 'bar'),
+                        onTap: event 'menu_item' { args: 'bar' },
+                      },
+                    ],
+                    borderRadius:[{x: 8.0, y: 8.0}, {x: 8.0, y: 8.0}, {x: 8.0, y: 8.0}, {x: 8.0, y: 8.0}],
+                    onChanged: event 'dropdown' {},
                   ),
-                  style: {
-                    color:0xFF7C4DFF,
-                  },
-                  items: [
-                    {
-                      value: 'foo',
-                      child: Text(text: 'foo'),
-                      onTap: event 'menu_item' {},
-                    },
-                    {
-                      value: 'bar',
-                      child: Text(text: 'bar'),
-                      onTap: event 'menu_item' {},
-                    },
-                  ],
-                  borderRadius:[{x: 8.0, y: 8.0}, {x: 8.0, y: 8.0}, {x: 8.0, y: 8.0}, {x: 8.0, y: 8.0}],
-                  onChanged: event 'dropdown' {},
-                ),
+                  DropdownButton(
+                    value: 1.0,
+                    items: [
+                      {
+                        value: 1.0,
+                        child: Text(text: 'first'),
+                      },
+                      {
+                        value: 2.0,
+                        child: Text(text: 'second'),
+                        onTap: event 'menu_item' { args: 'second' },
+                      },
+                    ],
+                    onChanged: event 'dropdown' {},
+                  ),
+                ],
               ),
             ),
           ],
@@ -170,7 +187,7 @@ void main() {
       skip: !runGoldens,
     );
 
-    await tester.tap(find.byType(DropdownButton<Object>));
+    await tester.tap(find.byType(DropdownButton<Object>).first);
     await tester.pumpAndSettle();
     await expectLater(
       find.byType(MaterialApp),
@@ -180,8 +197,15 @@ void main() {
     // Tap on the second item.
     await tester.tap(find.text('bar'));
     await tester.pumpAndSettle();
-    expect(eventLog, contains('menu_item {}'));
+    expect(eventLog, contains('menu_item {args: bar}'));
     expect(eventLog, contains('dropdown {value: bar}'));
+
+    await tester.tap(find.byType(DropdownButton<Object>).last);
+    await tester.pumpAndSettle();
+    await tester.tap(find.text('second'));
+    await tester.pumpAndSettle();
+    expect(eventLog, contains('menu_item {args: second}'));
+    expect(eventLog, contains('dropdown {value: 2.0}'));
 
     await tester.tapAt(const Offset(20.0, 20.0));
     await tester.pump();
