942a623c680aadb3c8e4220d566191bd2908bdd1
Louise Hsu <louisehsu@google.com>
2024-01-03 14:18:28 -0800

    wip

--- packages/in_app_purchase/in_app_purchase_storekit/pigeons/messages.dart
+++ packages/in_app_purchase/in_app_purchase_storekit/pigeons/messages.dart
@@ -11,47 +11,138 @@ import 'package:pigeon/pigeon.dart';
   copyrightHeader: 'pigeons/copyright.txt',
 ))
 
-/// Possible outcomes of launching a URL.
-enum LaunchResult {
-  /// The URL was successfully launched (or could be, for `canLaunchUrl`).
-  success,
+class PaymentTransactionWrapper {
+  PaymentTransactionWrapper({
+    required this.payment,
+    required this.transactionState,
+    this.originalTransaction,
+    this.transactionTimeStamp,
+    this.transactionIdentifier,
+    this.error,
+  });
 
-  /// There was no handler available for the URL.
-  failure,
+  final PaymentWrapper payment;
 
-  /// The URL could not be launched because it is invalid.
-  invalidUrl,
+  final PaymentTransactionStateWrapper transactionState;
+
+  final PaymentTransactionWrapper? originalTransaction;
+
+  final double? transactionTimeStamp;
+
+  final String? transactionIdentifier;
+
+  final ErrorWrapper? error;
 }
 
-/// Possible outcomes of handling a URL within the application.
-enum InAppLoadResult {
-  /// The URL was successfully loaded.
-  success,
+enum PaymentTransactionStateWrapper {
+  /// Indicates the transaction is being processed in App Store.
+  ///
+  /// You should update your UI to indicate that you are waiting for the
+  /// transaction to update to another state. Never complete a transaction that
+  /// is still in a purchasing state.
+  purchasing,
 
-  /// The URL did not load successfully.
-  failedToLoad,
+  /// The user's payment has been succesfully processed.
+  ///
+  /// You should provide the user the content that they purchased.
+  purchased,
 
-  /// The URL could not be launched because it is invalid.
-  invalidUrl,
+  /// The transaction failed.
+  ///
+  /// Check the [PaymentTransactionWrapper.error] property from
+  /// [PaymentTransactionWrapper] for details.
+  failed,
+
+  /// This transaction is restoring content previously purchased by the user.
+  ///
+  /// The previous transaction information can be obtained in
+  /// [PaymentTransactionWrapper.originalTransaction] from
+  /// [PaymentTransactionWrapper].
+  restored,
+
+  /// The transaction is in the queue but pending external action. Wait for
+  /// another callback to get the final state.
+  ///
+  /// You should update your UI to indicate that you are waiting for the
+  /// transaction to update to another state.
+  deferred,
+
+  /// Indicates the transaction is in an unspecified state.
+  unspecified,
+}
+
+class PaymentWrapper {
+  /// Creates a new [SKPaymentWrapper] with the provided information.
+  const PaymentWrapper({
+    required this.productIdentifier,
+    this.applicationUsername,
+    this.requestData,
+    this.quantity = 1,
+    this.simulatesAskToBuyInSandbox = false,
+    this.paymentDiscount,
+  });
+
+  final String productIdentifier;
+
+  final String? applicationUsername;
+
+  final String? requestData;
+
+  // default value is 0?
+  final int quantity;
+
+  final bool simulatesAskToBuyInSandbox;
+
+  final PaymentDiscountWrapper? paymentDiscount;
+}
+
+class ErrorWrapper {
+  // a lot of comparison operators are overriden in this class - do i add them here?
+  const ErrorWrapper(
+      {required this.code, required this.domain, required this.userInfo});
+
+  final int code;
+  final String domain;
+  final Map<String?, Object?> userInfo;
+}
+
+class PaymentDiscountWrapper {
+  const PaymentDiscountWrapper({
+    required this.identifier,
+    required this.keyIdentifier,
+    required this.nonce,
+    required this.signature,
+    required this.timestamp,
+  });
+
+  final String identifier;
+  final String keyIdentifier;
+  final String nonce;
+  final String signature;
+  final int timestamp;
+}
+
+class StorefrontWrapper {
+  StorefrontWrapper({
+    required this.countryCode,
+    required this.identifier,
+  });
+
+  final String countryCode;
+  final String identifier;
 }
 
 @HostApi()
-abstract class UrlLauncherApi {
-  /// Checks whether a URL can be loaded.
-  @ObjCSelector('canLaunchURL:')
-  LaunchResult canLaunchUrl(String url);
+abstract class InAppPurchaseAPI {
+  /// Returns if the current device is able to make payments
+  @ObjCSelector('canMakePayments')
+  bool canMakePayments();
 
-  /// Opens the URL externally, returning the status of launching it.
-  @async
-  @ObjCSelector('launchURL:universalLinksOnly:')
-  LaunchResult launchUrl(String url, bool universalLinksOnly);
+  @ObjCSelector('transactions')
+  List<PaymentTransactionWrapper> transactions();
 
-  /// Opens the URL in an in-app SFSafariViewController, returning the results
-  /// of loading it.
-  @async
-  @ObjCSelector('openSafariViewControllerWithURL:')
-  InAppLoadResult openUrlInSafariViewController(String url);
-
-  /// Closes the view controller opened by [openUrlInSafariViewController].
-  void closeSafariViewController();
+  @ObjCSelector('storefront')
+  List<StorefrontWrapper> storefront();
 }
+
+
