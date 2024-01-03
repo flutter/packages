97e4dad6596694a4392cd39603da610b5326eb0d
Michael Goderbauer <goderbauer@google.com>
2024-01-03 09:46:00 -0800

    review

--- packages/plugin_platform_interface/test/plugin_platform_interface_test.dart
+++ packages/plugin_platform_interface/test/plugin_platform_interface_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Unreachable code is used in docregion.
 // ignore_for_file: unreachable_from_main
 
 import 'package:mockito/mockito.dart';
