ba981851d893c2d287c2ed683ecf0ca9496f21ff
GavrielRosendorn74 <rosendorngavrielofficial@gmail.com>
2024-01-07 19:37:21 +0100

    fix issue 139733 - takeSnapShot for IOS17

--- packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/GoogleMapController.m
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/GoogleMapController.m
@@ -250,15 +250,13 @@
     result(nil);
   } else if ([call.method isEqualToString:@"map#takeSnapshot"]) {
     if (self.mapView != nil) {
-      UIGraphicsImageRendererFormat *format = [UIGraphicsImageRendererFormat defaultFormat];
-      format.scale = [[UIScreen mainScreen] scale];
-      UIGraphicsImageRenderer *renderer =
-          [[UIGraphicsImageRenderer alloc] initWithSize:self.mapView.frame.size format:format];
-
+      UIGraphicsImageRenderer *renderer = [[UIGraphicsImageRenderer alloc] initWithSize:self.mapView.bounds.size];
       UIImage *image = [renderer imageWithActions:^(UIGraphicsImageRendererContext *context) {
-        [self.mapView.layer renderInContext:context.CGContext];
+          [self.mapView drawViewHierarchyInRect:self.mapView.bounds afterScreenUpdates:YES];
       }];
-      result([FlutterStandardTypedData typedDataWithBytes:UIImagePNGRepresentation(image)]);
+      NSData *imageData = UIImagePNGRepresentation(image);
+      FlutterStandardTypedData *typedData = [FlutterStandardTypedData typedDataWithBytes:imageData];
+      result(typedData);
     } else {
       result([FlutterError errorWithCode:@"GoogleMap uninitialized"
                                  message:@"takeSnapshot called prior to map initialization"
