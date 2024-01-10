e3790227dbcb74eb3bc5879124c51daea89d23ed
Justin McCandless <jmccandless@google.com>
2024-01-10 15:00:56 -0800

    Created linked_text package

---
+++ packages/linked_text/example/linked_text_example.dart
@@ -0,0 +1,6 @@
+import 'package:linked_text/linked_text.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/linked_text/test/linked_text_test.dart
@@ -0,0 +1,16 @@
+import 'package:linked_text/linked_text.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
