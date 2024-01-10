7238aea1a486aba507e4b0f6c410fd110b610bbc
Justin McCandless <jmccandless@google.com>
2024-01-10 15:30:26 -0800

    Working with basic directly imported code

--- packages/linked_text/lib/linked_text.dart
+++ packages/linked_text/lib/linked_text.dart
@@ -1,8 +1,7 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 library;
 
-export 'src/linked_text_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/linked_text.dart';
+export 'src/text_linker.dart';
