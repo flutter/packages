281f8fa9d278dc693fe0166d66f194b9109bb5c2
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-01-15 14:43:57 -0500

    rename test file

--- packages/webview_flutter/webview_flutter_platform_interface/test/webview_flutter_platform_interface_export_test.dart
+++ packages/webview_flutter/webview_flutter_platform_interface/test/webview_flutter_platform_interface_export_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: unnecessary_statements
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart'
     as main_file;
@@ -9,53 +11,30 @@ import 'package:webview_flutter_platform_interface/webview_flutter_platform_inte
 void main() {
   group('webview_flutter_platform_interface', () {
     test(
-      'ensures webview_flutter_platform_interface.dart exports classes in types directory',
+      'ensures webview_flutter_platform_interface_export_test.dart exports classes in types directory',
       () {
-        // ignore: unnecessary_statements
         main_file.JavaScriptConsoleMessage;
-        // ignore: unnecessary_statements
         main_file.JavaScriptLogLevel;
-        // ignore: unnecessary_statements
         main_file.JavaScriptMessage;
-        // ignore: unnecessary_statements
         main_file.JavaScriptMode;
-        // ignore: unnecessary_statements
         main_file.LoadRequestMethod;
-        // ignore: unnecessary_statements
         main_file.NavigationDecision;
-        // ignore: unnecessary_statements
         main_file.NavigationRequest;
-        // ignore: unnecessary_statements
         main_file.NavigationRequestCallback;
-        // ignore: unnecessary_statements
         main_file.PageEventCallback;
-        // ignore: unnecessary_statements
         main_file.PlatformNavigationDelegateCreationParams;
-        // ignore: unnecessary_statements
         main_file.PlatformWebViewControllerCreationParams;
-        // ignore: unnecessary_statements
         main_file.PlatformWebViewCookieManagerCreationParams;
-        // ignore: unnecessary_statements
         main_file.PlatformWebViewPermissionRequest;
-        // ignore: unnecessary_statements
         main_file.PlatformWebViewWidgetCreationParams;
-        // ignore: unnecessary_statements
         main_file.ProgressCallback;
-        // ignore: unnecessary_statements
         main_file.WebViewPermissionResourceType;
-        // ignore: unnecessary_statements
         main_file.WebResourceRequest;
-        // ignore: unnecessary_statements
         main_file.WebResourceResponse;
-        // ignore: unnecessary_statements
         main_file.WebResourceError;
-        // ignore: unnecessary_statements
         main_file.WebResourceErrorCallback;
-        // ignore: unnecessary_statements
         main_file.WebViewCookie;
-        // ignore: unnecessary_statements
         main_file.WebResourceErrorType;
-        // ignore: unnecessary_statements
         main_file.UrlChange;
       },
     );
