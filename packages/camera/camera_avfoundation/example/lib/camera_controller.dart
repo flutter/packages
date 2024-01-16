1d0fca7a37739592fbe4f12a7b7364c0e4289ac4
Stuart Morgan <stuartmorgan@google.com>
2024-01-16 16:34:33 -0500

    More auto-promotion in camera

--- packages/camera/camera_avfoundation/example/lib/camera_controller.dart
+++ packages/camera/camera_avfoundation/example/lib/camera_controller.dart
@@ -538,7 +538,7 @@ class Optional<T> extends IterableBase<T> {
   Optional<S> transform<S>(S Function(T value) transformer) {
     return _value == null
         ? Optional<S>.absent()
-        : Optional<S>.of(transformer(_value as T));
+        : Optional<S>.of(transformer(_value));
   }
 
   /// Transforms the Optional value.
@@ -549,7 +549,7 @@ class Optional<T> extends IterableBase<T> {
   Optional<S> transformNullable<S>(S? Function(T value) transformer) {
     return _value == null
         ? Optional<S>.absent()
-        : Optional<S>.fromNullable(transformer(_value as T));
+        : Optional<S>.fromNullable(transformer(_value));
   }
 
   @override
