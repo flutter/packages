129e08c1dff159b1b49484e1f838f0a471273cd7
Jenn Magder <magder@google.com>
2024-01-18 22:54:24 -0800

    [pigeon] Run swift-format on ungenerated example app Swift files (#5934)
    Run `swift-format` linter/formatter on Swift files in example apps.  This only touches Swift files in example/test code.  
    
    This also doesn't format any autogenerated Swift files, which would require changed in the pigeon generator: https://github.com/flutter/flutter/issues/141799
    
    Part of turning on swift-format CI formatting https://github.com/flutter/packages/pull/5928

--- packages/pigeon/platform_tests/test_plugin/example/ios/RunnerTests/ListTests.swift
+++ packages/pigeon/platform_tests/test_plugin/example/ios/RunnerTests/ListTests.swift
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import XCTest
+
 @testable import test_plugin
 
 class ListTests: XCTestCase {
@@ -16,13 +17,13 @@ class ListTests: XCTestCase {
     let expectation = XCTestExpectation(description: "callback")
     api.echo(top) { result in
       switch result {
-        case .success(let res) :
-          XCTAssertEqual(1, res.testList?.count)
-          XCTAssertTrue(res.testList?[0] is TestMessage)
-          XCTAssert(equalsList(inside.testList, (res.testList?[0] as! TestMessage).testList))
-          expectation.fulfill()
-        case .failure(_) :
-          return
+      case .success(let res):
+        XCTAssertEqual(1, res.testList?.count)
+        XCTAssertTrue(res.testList?[0] is TestMessage)
+        XCTAssert(equalsList(inside.testList, (res.testList?[0] as! TestMessage).testList))
+        expectation.fulfill()
+      case .failure(_):
+        return
       }
     }
     wait(for: [expectation], timeout: 1.0)
