1f1db467572681ddd4696952c3f519961b657572
peixinli <pageli328@gmail.com>
2024-01-24 07:31:35 +0000

    fix

--- packages/rfw/test/material_widgets_test.dart
+++ packages/rfw/test/material_widgets_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rfw/formats.dart' show parseLibraryFile;
 import 'package:rfw/rfw.dart';
