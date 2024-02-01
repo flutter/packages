7a0fcb1d66eb58f5f21649f9c5e9aa9018890c19
camsim99 <camillesimon90@gmail.com>
2024-02-01 12:47:31 -0800

    Address comment

--- packages/camera/camera_platform_interface/lib/src/types/resolution_preset.dart
+++ packages/camera/camera_platform_interface/lib/src/types/resolution_preset.dart
@@ -4,28 +4,23 @@
 
 /// Affect the quality of video recording and image capture:
 ///
-/// If a preset is not available on the camera being used, a preset of lower
-/// quality will be selected automatically.
-///
-/// For the `camera_android_camerax` platform implementation of the plugin,
-/// these are treated as target resolutions and are not guaranteed. If
-/// unavailable, a fallback resolution of the next highest quality will be
-/// targeted.
-/// See https://developer.android.com/media/camera/camerax/configuration#specify-resolution.
+/// A preset is treated as a target resolution, and exact values are not
+/// guaranteed. Platform implementations may fall back to a higher or lower
+/// resolution if the specific preset is not available.
 enum ResolutionPreset {
-  /// 352x288 on iOS, 240p (320x240) on Android and Web
+  /// 352x288 on iOS, ~240p (320x240) on Android and Web
   low,
 
-  /// 480p (640x480 on iOS, 720x480 on Android and Web)
+  /// ~480p (640x480 on iOS, 720x480 on Android and Web)
   medium,
 
-  /// 720p (1280x720)
+  /// ~720p (1280x720)
   high,
 
-  /// 1080p (1920x1080)
+  /// ~1080p (1920x1080)
   veryHigh,
 
-  /// 2160p (3840x2160 on Android and iOS, 4096x2160 on Web)
+  /// ~2160p (3840x2160 on Android and iOS, 4096x2160 on Web)
   ultraHigh,
 
   /// The highest resolution available.
