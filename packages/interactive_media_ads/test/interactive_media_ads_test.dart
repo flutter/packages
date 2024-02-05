0801f4c6439ae7de7066c52a541a21940e269087
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-02-05 16:24:05 -0500

    fix analyze warning

--- packages/interactive_media_ads/test/interactive_media_ads_test.dart
+++ packages/interactive_media_ads/test/interactive_media_ads_test.dart
@@ -4,8 +4,8 @@
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:interactive_media_ads/interactive_media_ads.dart';
-import 'package:interactive_media_ads/interactive_media_ads_platform_interface.dart';
 import 'package:interactive_media_ads/interactive_media_ads_method_channel.dart';
+import 'package:interactive_media_ads/interactive_media_ads_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockInteractiveMediaAdsPlatform
