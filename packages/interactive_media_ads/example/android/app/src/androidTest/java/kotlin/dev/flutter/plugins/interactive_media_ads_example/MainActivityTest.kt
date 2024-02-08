1aa04557944d6aca50175e9ccc0d83d5e662a58b
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-02-08 11:59:31 -0500

    try getting integration tests running

---
+++ packages/interactive_media_ads/example/android/app/src/androidTest/java/kotlin/dev/flutter/plugins/interactive_media_ads_example/MainActivityTest.kt
@@ -0,0 +1,21 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package kotlin.dev.flutter.plugins.interactive_media_ads_example
+
+import androidx.test.rule.ActivityTestRule
+import dev.flutter.plugins.integration_test.FlutterTestRunner
+import io.flutter.embedding.android.FlutterActivity
+import org.junit.Rule
+import org.junit.runner.RunWith
+import kotlin.dev.flutter.plugins.DartIntegrationTest
+
+
+@DartIntegrationTest
+@RunWith(FlutterTestRunner::class)
+class MainActivityTest {
+  @JvmField
+  @Rule
+  var rule = ActivityTestRule(FlutterActivity::class.java)
+}
