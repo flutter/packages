d5e5171295e1214494c62a8f01d0d7bfa1aa7589
Aaron Clarke <aaclarke@google.com>
2024-02-07 16:58:32 -0800

    format

--- packages/google_maps_flutter/google_maps_flutter_ios/example/ios12/ios/RunnerTests/FLTTileProviderControllerTests.m
+++ packages/google_maps_flutter/google_maps_flutter_ios/example/ios12/ios/RunnerTests/FLTTileProviderControllerTests.m
@@ -22,17 +22,17 @@
 
 - (void)testFoo {
   id channel = OCMClassMock(FlutterMethodChannel.class);
-  FLTTileProviderController* controller =
-    [[FLTTileProviderController alloc] init:channel withTileOverlayIdentifier:@"foo"];
+  FLTTileProviderController *controller = [[FLTTileProviderController alloc] init:channel
+                                                        withTileOverlayIdentifier:@"foo"];
   XCTAssertNotNil(controller);
-  XCTestExpectation* expectation = [self expectationWithDescription:@"invokeMethod"];
+  XCTestExpectation *expectation = [self expectationWithDescription:@"invokeMethod"];
   OCMStub([channel invokeMethod:[OCMArg any] arguments:[OCMArg any] result:[OCMArg any]])
-    .andDo(^(NSInvocation *invocation) {
-      XCTAssertTrue([[NSThread currentThread] isMainThread]);
-      [expectation fulfill];
-    });
+      .andDo(^(NSInvocation *invocation) {
+        XCTAssertTrue([[NSThread currentThread] isMainThread]);
+        [expectation fulfill];
+      });
   [controller requestTileForX:0 y:0 zoom:0 receiver:nil];
-  [self waitForExpectations:@[expectation] timeout:10.0];
+  [self waitForExpectations:@[ expectation ] timeout:10.0];
 }
 
 @end
