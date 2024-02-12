e5575bde9c8e57460a4e560519e4eb5185e99201
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-02-12 16:58:49 -0500

    fix package name

--- packages/interactive_media_ads/android/src/test/kotlin/dev/flutter/packages/interactive_media_ads/InteractiveMediaAdsPluginTest.kt
+++ packages/interactive_media_ads/android/src/test/kotlin/dev/flutter/packages/interactive_media_ads/InteractiveMediaAdsPluginTest.kt
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package dev.flutter.plugins.interactive_media_ads
+package dev.flutter.packages.interactive_media_ads
 
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
