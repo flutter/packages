68c854eedf6ffee4d1064f363821cd063def6237
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-02-13 12:55:19 -0500

    add to int test too i guess

--- packages/interactive_media_ads/example/integration_test/interactive_media_ads_test.dart
+++ packages/interactive_media_ads/example/integration_test/interactive_media_ads_test.dart
@@ -2,8 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter_driver/driver_extension.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:interactive_media_ads_example/main.dart' as app;
+
+/// Entry point for integration tests that require espresso.
+// ignore: unreachable_from_main
+void integrationTestMain() {
+  enableFlutterDriverExtension();
+  app.main();
+}
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
