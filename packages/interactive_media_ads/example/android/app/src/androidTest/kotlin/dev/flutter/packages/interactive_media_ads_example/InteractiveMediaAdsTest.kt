a8785ef1458d4c35cf3edd3d73ccc6f15a3ec8f2
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-02-13 00:15:44 -0500

    fix integration test

--- packages/interactive_media_ads/example/android/app/src/androidTest/kotlin/dev/flutter/packages/interactive_media_ads_example/InteractiveMediaAdsTest.kt
+++ packages/interactive_media_ads/example/android/app/src/androidTest/kotlin/dev/flutter/packages/interactive_media_ads_example/InteractiveMediaAdsTest.kt
@@ -2,14 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package dev.flutter.packages.file_selector_android_example
+package dev.flutter.packages.interactive_media_ads_example
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import dev.flutter.plugins.interactive_media_ads_example.DriverExtensionActivity
+import org.junit.Rule
 
 class InteractiveMediaAdsTest {
-  @org.junit.Rule
+  @JvmField
+  @Rule
   var myActivityTestRule: ActivityScenarioRule<DriverExtensionActivity> =
-      ActivityScenarioRule<Any?>(DriverExtensionActivity::class.java)
+      ActivityScenarioRule(DriverExtensionActivity::class.java)
 
-  @Rule var intentsRule: IntentsRule = IntentsRule()
-
-  @org.junit.Test fun placeholerTest() {}
+  @org.junit.Test fun placeholderIntegrationTest() {}
 }
