350c2166653056e1d50d3bcb4dd777a35048a8f6
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-02-13 08:47:17 -0500

    try close system dialog

--- packages/interactive_media_ads/example/android/app/src/androidTest/kotlin/dev/flutter/packages/interactive_media_ads_example/InteractiveMediaAdsTest.kt
+++ packages/interactive_media_ads/example/android/app/src/androidTest/kotlin/dev/flutter/packages/interactive_media_ads_example/InteractiveMediaAdsTest.kt
@@ -4,6 +4,7 @@
 
 package dev.flutter.packages.interactive_media_ads_example
 
+import android.content.Intent
 import androidx.test.espresso.intent.rule.IntentsRule
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import org.junit.Rule
@@ -16,5 +17,17 @@ class InteractiveMediaAdsTest {
 
   @JvmField @Rule var intentsRule = IntentsRule()
 
-  @org.junit.Test fun placeholderIntegrationTest() {}
+  @org.junit.Test
+  fun placeholderIntegrationTest() {
+    clearAnySystemDialog()
+
+    myActivityTestRule.scenario.close()
+  }
+
+  private fun clearAnySystemDialog() {
+    myActivityTestRule.scenario.onActivity { activity ->
+      val closeDialog = Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
+      activity?.sendBroadcast(closeDialog)
+    }
+  }
 }
