acbe410a79ffc04a77d46f05de51266f3245fb75
Alex Usmanov <40719830+Alex-Usmanov@users.noreply.github.com>
2024-02-14 00:17:56 +0500

    [url_launcher] Add `InAppBrowserConfiguration` parameter in implementations (#5759)
    Platform implementation portion of #5166. Implements `InAppBrowserConfiguration` support on Android, as well as support for `InAppBrowserConfiguration.showTitle` parameter for hiding/showing webpage title in Android Custom Tabs.

--- packages/url_launcher/url_launcher_android/lib/url_launcher_android.dart
+++ packages/url_launcher/url_launcher_android/lib/url_launcher_android.dart
@@ -99,17 +99,23 @@ class UrlLauncherAndroid extends UrlLauncherPlatform {
     final bool succeeded;
     if (inApp) {
       succeeded = await _hostApi.openUrlInApp(
-          url,
-          // Prefer custom tabs unless a webview was specifically requested.
-          options.mode != PreferredLaunchMode.inAppWebView,
-          WebViewOptions(
-              enableJavaScript: options.webViewConfiguration.enableJavaScript,
-              enableDomStorage: options.webViewConfiguration.enableDomStorage,
-              headers: options.webViewConfiguration.headers));
+        url,
+        // Prefer custom tabs unless a webview was specifically requested.
+        options.mode != PreferredLaunchMode.inAppWebView,
+        WebViewOptions(
+          enableJavaScript: options.webViewConfiguration.enableJavaScript,
+          enableDomStorage: options.webViewConfiguration.enableDomStorage,
+          headers: options.webViewConfiguration.headers,
+        ),
+        BrowserOptions(
+          showTitle: options.browserConfiguration.showTitle,
+        ),
+      );
     } else {
       succeeded =
           await _hostApi.launchUrl(url, options.webViewConfiguration.headers);
     }
+
     // TODO(stuartmorgan): Remove this special handling as part of a
     // breaking change to rework failure handling across all platform. The
     // current behavior is backwards compatible with the previous Java error.
@@ -118,6 +124,7 @@ class UrlLauncherAndroid extends UrlLauncherPlatform {
           code: 'ACTIVITY_NOT_FOUND',
           message: 'No Activity found to handle intent { $url }');
     }
+
     return succeeded;
   }
 
