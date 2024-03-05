d173bc4995b09537fd6b158cef5daa309e366acc
Hyeokeun Mun <>
2024-03-05 09:00:58 +0900

    add: return without any processing if extension gif

--- packages/image_picker/image_picker_android/android/src/main/java/io/flutter/plugins/imagepicker/ImageResizer.java
+++ packages/image_picker/image_picker_android/android/src/main/java/io/flutter/plugins/imagepicker/ImageResizer.java
@@ -43,6 +43,11 @@ class ImageResizer {
     if (!shouldScale) {
       return imagePath;
     }
+    String[] parts = imagePath.split("\\.");
+    String extension = parts[parts.length - 1];
+    if (extension.toLowerCase().equals("gif")) {
+      return imagePath;
+    }
     try {
       String[] pathParts = imagePath.split("/");
       String imageName = pathParts[pathParts.length - 1];
