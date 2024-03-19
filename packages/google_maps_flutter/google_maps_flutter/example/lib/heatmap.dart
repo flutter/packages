609efce4af532a473c7da518bab51881f4ed7b03
Rexios <rexios@rexios.dev>
2024-03-19 14:40:29 -0400

    Export HeatmapRadius

--- packages/google_maps_flutter/google_maps_flutter/example/lib/heatmap.dart
+++ packages/google_maps_flutter/google_maps_flutter/example/lib/heatmap.dart
@@ -7,7 +7,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 import 'page.dart';
 
 class HeatmapPage extends GoogleMapExampleAppPage {
--- packages/google_maps_flutter/google_maps_flutter/lib/google_maps_flutter.dart
+++ packages/google_maps_flutter/google_maps_flutter/lib/google_maps_flutter.dart
@@ -29,6 +29,7 @@ export 'package:google_maps_flutter_platform_interface/google_maps_flutter_platf
         HeatmapGradient,
         HeatmapGradientColor,
         HeatmapId,
+        HeatmapRadius,
         InfoWindow,
         JointType,
         LatLng,
