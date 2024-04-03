483eadd3576338d37c331ff772cd9fd3ddc849d8
stuartmorgan <stuartmorgan@google.com>
2024-04-03 10:49:09 -0400

    [in_app_purchase] Mostly convert to Android Pigeon (#6262)
    Replaces manual method channels with Pigeon.
    
    This replaces all of the method calls in both direction with Pigeon calls, and converts all the top-level objects used for params and returns to Pigeon objects. However, because of the significant object graph, in order to somewhat limit the scope of this PR I made a cut at that layer in the object graph, with nested objects still using the existing JSON serialization. In a follow-up PR, those will be converted to typed Pigeon objects as well, completing the transition.
    
    Unfortunately a significant amount of JSON code can't be removed yet even though it's now unused by the plugin, because it's part of the API of the public wrappers, so clients may be using it. Once all the JSON code is unused, we could `@Deprecated` all of it, and then could do a follow-up breaking change later to remove it.
    
    Most of https://github.com/flutter/flutter/issues/117910

--- packages/in_app_purchase/in_app_purchase_android/android/src/main/java/io/flutter/plugins/inapppurchase/BillingClientFactoryImpl.java
+++ packages/in_app_purchase/in_app_purchase_android/android/src/main/java/io/flutter/plugins/inapppurchase/BillingClientFactoryImpl.java
@@ -10,8 +10,7 @@ import androidx.annotation.Nullable;
 import com.android.billingclient.api.BillingClient;
 import com.android.billingclient.api.UserChoiceBillingListener;
 import io.flutter.Log;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugins.inapppurchase.MethodCallHandlerImpl.BillingChoiceMode;
+import io.flutter.plugins.inapppurchase.Messages.PlatformBillingChoiceMode;
 
 /** The implementation for {@link BillingClientFactory} for the plugin. */
 final class BillingClientFactoryImpl implements BillingClientFactory {
@@ -19,16 +18,16 @@ final class BillingClientFactoryImpl implements BillingClientFactory {
   @Override
   public BillingClient createBillingClient(
       @NonNull Context context,
-      @NonNull MethodChannel channel,
-      int billingChoiceMode,
+      @NonNull Messages.InAppPurchaseCallbackApi callbackApi,
+      PlatformBillingChoiceMode billingChoiceMode,
       @Nullable UserChoiceBillingListener userChoiceBillingListener) {
     BillingClient.Builder builder = BillingClient.newBuilder(context).enablePendingPurchases();
     switch (billingChoiceMode) {
-      case BillingChoiceMode.ALTERNATIVE_BILLING_ONLY:
+      case ALTERNATIVE_BILLING_ONLY:
         // https://developer.android.com/google/play/billing/alternative/alternative-billing-without-user-choice-in-app
         builder.enableAlternativeBillingOnly();
         break;
-      case BillingChoiceMode.USER_CHOICE_BILLING:
+      case USER_CHOICE_BILLING:
         if (userChoiceBillingListener != null) {
           // https://developer.android.com/google/play/billing/alternative/alternative-billing-with-user-choice-in-app
           builder.enableUserChoiceBilling(userChoiceBillingListener);
@@ -38,7 +37,7 @@ final class BillingClientFactoryImpl implements BillingClientFactory {
               "userChoiceBillingListener null when USER_CHOICE_BILLING set. Defaulting to PLAY_BILLING_ONLY");
         }
         break;
-      case BillingChoiceMode.PLAY_BILLING_ONLY:
+      case PLAY_BILLING_ONLY:
         // Do nothing.
         break;
       default:
@@ -47,6 +46,6 @@ final class BillingClientFactoryImpl implements BillingClientFactory {
             "Unknown BillingChoiceMode " + billingChoiceMode + ", Defaulting to PLAY_BILLING_ONLY");
         break;
     }
-    return builder.setListener(new PluginPurchaseListener(channel)).build();
+    return builder.setListener(new PluginPurchaseListener(callbackApi)).build();
   }
 }
