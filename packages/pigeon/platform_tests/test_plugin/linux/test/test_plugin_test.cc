a4958b52ade43892a814eba2b3e660bdabf3c939
Stuart Morgan <stuartmorgan@google.com>
2024-04-05 11:17:39 -0400

    Unchanged boilerplate

---
+++ packages/pigeon/platform_tests/test_plugin/linux/test/test_plugin_test.cc
@@ -0,0 +1,32 @@
+#include "include/test_plugin/test_plugin.h"
+
+#include <flutter_linux/flutter_linux.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "test_plugin_private.h"
+
+// This demonstrates a simple unit test of the C portion of this plugin's
+// implementation.
+//
+// Once you have built the plugin's example app, you can run these tests
+// from the command line. For instance, for a plugin called my_plugin
+// built for x64 debug, run:
+// $ build/linux/x64/debug/plugins/my_plugin/my_plugin_test
+
+namespace test_plugin {
+namespace test {
+
+TEST(TestPlugin, GetPlatformVersion) {
+  g_autoptr(FlMethodResponse) response = get_platform_version();
+  ASSERT_NE(response, nullptr);
+  ASSERT_TRUE(FL_IS_METHOD_SUCCESS_RESPONSE(response));
+  FlValue* result = fl_method_success_response_get_result(
+      FL_METHOD_SUCCESS_RESPONSE(response));
+  ASSERT_EQ(fl_value_get_type(result), FL_VALUE_TYPE_STRING);
+  // The full string varies, so just validate that it has the right format.
+  EXPECT_THAT(fl_value_get_string(result), testing::StartsWith("Linux "));
+}
+
+}  // namespace test
+}  // namespace test_plugin
