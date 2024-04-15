30295800e9298b1a1ff9b5d8589164b63dca417a
camsim99 <camillesimon90@gmail.com>
2024-04-15 12:27:33 -0700

    Add todos, test file

--- packages/file_selector/file_selector_android/android/src/main/java/dev/flutter/packages/file_selector_android/FileUtils.java
+++ packages/file_selector/file_selector_android/android/src/main/java/dev/flutter/packages/file_selector_android/FileUtils.java
@@ -54,118 +54,6 @@ public class FileUtils {
     /** URI authority that represents a media document. */
     public static String MEDIA_DOCUMENT_AUTHORITY = "com.android.providers.media.documents";
 
-  /**
-   * Copies the file from the given content URI to a temporary directory, retaining the original
-   * file name if possible.
-   *
-   * <p>Each file is placed in its own directory to avoid conflicts according to the following
-   * scheme: {cacheDir}/{randomUuid}/{fileName}
-   *
-   * <p>File extension is changed to match MIME type of the file, if known. Otherwise, the extension
-   * is left unchanged.
-   *
-   * <p>If the original file name is unknown, a predefined "file_selector" filename is used and the
-   * file extension is deduced from the mime type.
-   */
-  public static String getPathFromCopyOfFileFromUri(final Context context, final Uri uri) {
-    try (InputStream inputStream = context.getContentResolver().openInputStream(uri)) {
-      String uuid = UUID.randomUUID().toString();
-      File targetDirectory = new File(context.getCacheDir(), uuid);
-      targetDirectory.mkdir();
-      // TODO(camsim99): according to the docs, `deleteOnExit` does not work reliably on Android; we should preferably
-      // just clear the picked files after the app startup.
-      targetDirectory.deleteOnExit();
-      String fileName = getFileName(context, uri);
-      String extension = getFileExtension(context, uri);
-
-      if (fileName == null) {
-        if (extension == null) {
-            throw new IllegalArgumentException("CAMILLE: NO EXTENSION FOUND");
-        } else {
-            fileName = "file_selector" + extension;
-        }
-      } else if (extension != null) {
-        fileName = getBaseName(fileName) + extension;
-      } else {
-        throw new IllegalArgumentException("CAMILLE: NO EXTENSiON FOUND 2");
-      }
-
-      File file = new File(targetDirectory, fileName);
-      try (OutputStream outputStream = new FileOutputStream(file)) {
-        copy(inputStream, outputStream);
-        return file.getPath();
-      }
-    } catch (IOException e) {
-      // If closing the output stream fails, we cannot be sure that the
-      // target file was written in full. Flushing the stream merely moves
-      // the bytes into the OS, not necessarily to the file.
-      return null;
-    } catch (SecurityException e) {
-      // Calling `ContentResolver#openInputStream()` has been reported to throw a
-      // `SecurityException` on some devices in certain circumstances. Instead of crashing, we
-      // return `null`.
-      //
-      // See https://github.com/flutter/flutter/issues/100025 for more details.
-      return null;
-    }
-  }
-
-  /** Returns the extension of file with dot, or null if it's empty. */
-  private static String getFileExtension(Context context, Uri uriFile) {
-    String extension;
-
-    try {
-      if (uriFile.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
-        final MimeTypeMap mime = MimeTypeMap.getSingleton();
-        extension = mime.getExtensionFromMimeType(context.getContentResolver().getType(uriFile));
-      } else {
-        extension =
-            MimeTypeMap.getFileExtensionFromUrl(
-                Uri.fromFile(new File(uriFile.getPath())).toString());
-      }
-    } catch (Exception e) {
-      return null;
-    }
-
-    if (extension == null || extension.isEmpty()) {
-      return null;
-    }
-
-    return "." + extension;
-  }
-
-  /** Returns the name of the file provided by ContentResolver; this may be null. */
-  private static String getFileName(Context context, Uri uriFile) {
-    try (Cursor cursor = queryFileName(context, uriFile)) {
-      if (cursor == null || !cursor.moveToFirst() || cursor.getColumnCount() < 1) return null;
-      return cursor.getString(0);
-    }
-  }
-
-  private static Cursor queryFileName(Context context, Uri uriFile) {
-    return context
-        .getContentResolver()
-        .query(uriFile, new String[] {MediaStore.MediaColumns.DISPLAY_NAME}, null, null, null);
-  }
-
-  private static void copy(InputStream in, OutputStream out) throws IOException {
-    final byte[] buffer = new byte[4 * 1024];
-    int bytesRead;
-    while ((bytesRead = in.read(buffer)) != -1) {
-      out.write(buffer, 0, bytesRead);
-    }
-    out.flush();
-  }
-
-  private static String getBaseName(String fileName) {
-    int lastDotIndex = fileName.lastIndexOf('.');
-    if (lastDotIndex < 0) {
-      return fileName;
-    }
-    // Basename is everything before the last '.'.
-    return fileName.substring(0, lastDotIndex);
-  }
-
     /**
      * Retrieves path of directory represented by the specified {@code Uri}.
      * 
@@ -209,5 +97,117 @@ public class FileUtils {
         } else {
             throw new UnsupportedOperationException("Retrieving the path from URIs with authority " + uriAuthority.toString() + " are unsupported by this plugin.");
         }
+    }
+
+    /**
+     * Copies the file from the given content URI to a temporary directory, retaining the original
+     * file name if possible.
+     *
+     * <p>Each file is placed in its own directory to avoid conflicts according to the following
+     * scheme: {cacheDir}/{randomUuid}/{fileName}
+     *
+     * <p>File extension is changed to match MIME type of the file, if known. Otherwise, the extension
+     * is left unchanged.
+     *
+     * <p>If the original file name is unknown, a predefined "file_selector" filename is used and the
+     * file extension is deduced from the mime type.
+     */
+    public static String getPathFromCopyOfFileFromUri(final Context context, final Uri uri) {
+        try (InputStream inputStream = context.getContentResolver().openInputStream(uri)) {
+        String uuid = UUID.randomUUID().toString();
+        File targetDirectory = new File(context.getCacheDir(), uuid);
+        targetDirectory.mkdir();
+        // TODO(camsim99): according to the docs, `deleteOnExit` does not work reliably on Android; we should preferably
+        // just clear the picked files after the app startup.
+        targetDirectory.deleteOnExit();
+        String fileName = getFileName(context, uri);
+        String extension = getFileExtension(context, uri);
+
+        if (fileName == null) {
+            if (extension == null) {
+                throw new IllegalArgumentException("CAMILLE: NO EXTENSION FOUND");
+            } else {
+                fileName = "file_selector" + extension;
+            }
+        } else if (extension != null) {
+            fileName = getBaseName(fileName) + extension;
+        } else {
+            throw new IllegalArgumentException("CAMILLE: NO EXTENSiON FOUND 2");
+        }
+
+        File file = new File(targetDirectory, fileName);
+        try (OutputStream outputStream = new FileOutputStream(file)) {
+            copy(inputStream, outputStream);
+            return file.getPath();
+        }
+        } catch (IOException e) {
+        // If closing the output stream fails, we cannot be sure that the
+        // target file was written in full. Flushing the stream merely moves
+        // the bytes into the OS, not necessarily to the file.
+        return null;
+        } catch (SecurityException e) {
+        // Calling `ContentResolver#openInputStream()` has been reported to throw a
+        // `SecurityException` on some devices in certain circumstances. Instead of crashing, we
+        // return `null`.
+        //
+        // See https://github.com/flutter/flutter/issues/100025 for more details.
+        return null;
+        }
+    }
+
+    /** Returns the extension of file with dot, or null if it's empty. */
+    private static String getFileExtension(Context context, Uri uriFile) {
+        String extension;
+
+        try {
+        if (uriFile.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
+            final MimeTypeMap mime = MimeTypeMap.getSingleton();
+            extension = mime.getExtensionFromMimeType(context.getContentResolver().getType(uriFile));
+        } else {
+            extension =
+                MimeTypeMap.getFileExtensionFromUrl(
+                    Uri.fromFile(new File(uriFile.getPath())).toString());
+        }
+        } catch (Exception e) {
+        return null;
+        }
+
+        if (extension == null || extension.isEmpty()) {
+        return null;
+        }
+
+        return "." + extension;
+    }
+
+    /** Returns the name of the file provided by ContentResolver; this may be null. */
+    private static String getFileName(Context context, Uri uriFile) {
+        try (Cursor cursor = queryFileName(context, uriFile)) {
+        if (cursor == null || !cursor.moveToFirst() || cursor.getColumnCount() < 1) return null;
+        return cursor.getString(0);
+        }
+    }
+
+    private static Cursor queryFileName(Context context, Uri uriFile) {
+        return context
+            .getContentResolver()
+            .query(uriFile, new String[] {MediaStore.MediaColumns.DISPLAY_NAME}, null, null, null);
+    }
+
+    private static void copy(InputStream in, OutputStream out) throws IOException {
+        final byte[] buffer = new byte[4 * 1024];
+        int bytesRead;
+        while ((bytesRead = in.read(buffer)) != -1) {
+        out.write(buffer, 0, bytesRead);
+        }
+        out.flush();
+    }
+
+    private static String getBaseName(String fileName) {
+        int lastDotIndex = fileName.lastIndexOf('.');
+        if (lastDotIndex < 0) {
+        return fileName;
+        }
+        // Basename is everything before the last '.'.
+        return fileName.substring(0, lastDotIndex);
     }
 }
