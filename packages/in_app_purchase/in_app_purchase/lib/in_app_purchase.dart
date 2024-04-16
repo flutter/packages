111daebd5e2e55b56185a970053a4f606a91b487
Reid Baker <reidbaker@google.com>
2024-04-16 14:10:35 -0400

    Update packages/in_app_purchase/in_app_purchase/lib/in_app_purchase.dart
    Co-authored-by: Gray Mackall <34871572+gmackall@users.noreply.github.com>

--- packages/in_app_purchase/in_app_purchase/lib/in_app_purchase.dart
+++ packages/in_app_purchase/in_app_purchase/lib/in_app_purchase.dart
@@ -210,7 +210,7 @@ class InAppPurchase implements InAppPurchasePlatformAdditionProvider {
         applicationUserName: applicationUserName,
       );
 
-  /// Returns the users country.
+  /// Returns the user's country.
   ///
   /// Android:
   /// Returns Play billing country code based on ISO-3166-1 alpha2 format.
