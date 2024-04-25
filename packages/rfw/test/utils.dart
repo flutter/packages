e36ff8fbac46a7a6451351950807e27cdc9d7446
Nemanja Mladenović <uberchilly@gmail.com>
2024-04-25 15:50:08 +0200

    Fixed formatting and end of line in utils

--- packages/rfw/test/utils.dart
+++ packages/rfw/test/utils.dart
@@ -27,11 +27,10 @@ final bool runGoldens = !kIsWeb && Platform.isLinux && isMainChannel;
 extension SlideTo on WidgetTester {
   Future<void> slideToValue(Finder slider, double value,
       {double paddingOffset = 24.0}) async {
-    final Offset zeroPoint = getTopLeft(slider) +
-        Offset(paddingOffset, getSize(slider).height / 2);
+    final Offset zeroPoint =
+        getTopLeft(slider) + Offset(paddingOffset, getSize(slider).height / 2);
     final double totalWidth = getSize(slider).width - (2 * paddingOffset);
     final double calculateOffset = value * (totalWidth / 100);
     await dragFrom(zeroPoint, Offset(calculateOffset, 0));
   }
 }
-
