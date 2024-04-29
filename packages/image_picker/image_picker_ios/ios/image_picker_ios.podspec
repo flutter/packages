ecc78e4ce09017b3afc6d5749d4097fe3932cefc
Victoria Ashworth <vashworth@google.com>
2024-04-29 16:38:55 -0500

    fix formatting and header file locations

--- packages/image_picker/image_picker_ios/ios/image_picker_ios.podspec
+++ packages/image_picker/image_picker_ios/ios/image_picker_ios.podspec
@@ -15,11 +15,10 @@ Downloaded by pub (not CocoaPods).
   s.source           = { :http => 'https://github.com/flutter/packages/tree/main/packages/image_picker_ios' }
   s.documentation_url = 'https://pub.dev/packages/image_picker_ios'
   s.source_files = 'image_picker_ios/Sources/image_picker_ios/**/*.{h,m}'
-  s.public_header_files = 'image_picker_ios/Sources/image_picker_ios/include/image_picker_ios/**/*.h'
+  s.public_header_files = 'image_picker_ios/Sources/image_picker_ios/**/*.h'
   s.module_map = 'image_picker_ios/Sources/image_picker_ios/include/ImagePickerPlugin.modulemap'
   s.dependency 'Flutter'
   s.platform = :ios, '12.0'
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES' }
   s.resource_bundles = {'image_picker_ios_privacy' => ['image_picker_ios/Sources/image_picker_ios/Resources/PrivacyInfo.xcprivacy']}
-  s.exclude_files = ['image_picker_ios/Package.swift', 'image_picker_ios/Sources/image_picker_ios/include/module.modulemap']
 end
--- packages/image_picker/image_picker_ios/ios/image_picker_ios/Package.swift
+++ packages/image_picker/image_picker_ios/ios/image_picker_ios/Package.swift
@@ -4,25 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "image_picker_ios",
-    platforms: [
-        .iOS("12.0"),
-        .macOS("10.14")
-    ],
-    products: [
-        .library(name: "image-picker-ios", targets: ["image_picker_ios"])
-    ],
-    dependencies: [],
-    targets: [
-        .target(
-            name: "image_picker_ios",
-            dependencies: [],
-            resources: [
-                .process("Resources")
-            ],
-            cSettings: [
-                .headerSearchPath("include/image_picker_ios")
-            ]
-        )
-    ]
+  name: "image_picker_ios",
+  platforms: [
+    .iOS("12.0"),
+    .macOS("10.14"),
+  ],
+  products: [
+    .library(name: "image-picker-ios", targets: ["image_picker_ios"])
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "image_picker_ios",
+      dependencies: [],
+      exclude: ["include/ImagePickerPlugin.modulemap"],
+      resources: [
+        .process("Resources")
+      ],
+      cSettings: [
+        .headerSearchPath("include/image_picker_ios")
+      ]
+    )
+  ]
 )
