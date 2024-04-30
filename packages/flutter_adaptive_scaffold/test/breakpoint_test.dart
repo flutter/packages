1a3ddaa53b4255f9c40dae8882dfc455f1385103
waleedf112 <waleedf112@gmail.com>
2024-04-30 14:40:14 +0300

    refactor: removed unnecessary imports

--- packages/flutter_adaptive_scaffold/test/breakpoint_test.dart
+++ packages/flutter_adaptive_scaffold/test/breakpoint_test.dart
@@ -2,9 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_adaptive_scaffold/src/breakpoints.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'simulated_layout.dart';
