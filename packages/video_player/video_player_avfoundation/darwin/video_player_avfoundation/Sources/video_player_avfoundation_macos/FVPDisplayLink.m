e9a4fe1bf7e4a4c6b108b31db70639df2fb36915
Victoria Ashworth <vashworth@google.com>
2024-05-01 15:21:29 -0500

    Convert video_player_avfoundation into a Swift Package

--- packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation_macos/FVPDisplayLink.m
+++ packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation_macos/FVPDisplayLink.m
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "../FVPDisplayLink.h"
+#import "video_player_avfoundation/FVPDisplayLink.h"
 
 #import <CoreVideo/CoreVideo.h>
 #import <Foundation/Foundation.h>
