69a22762b897848f2acfc87870c11bc54860e1ae
stuartmorgan <stuartmorgan@google.com>
2024-05-06 16:06:00 -0400

    [file_selector] Add support for SPM (#6672)
    Adjusts the structure of the iOS and macOS implementations to match SPM layout conventions, adds the SPM package specifications, and updates the podspecs to reflect the new paths to keep compatibility with Cocoapods.
    
    Does not include any Xcode project updates, in order to preserve compatibility with stable.
    
    Fixes https://github.com/flutter/flutter/issues/146903

--- packages/file_selector/file_selector_ios/pigeons/messages.dart
+++ packages/file_selector/file_selector_ios/pigeons/messages.dart
@@ -7,10 +7,12 @@ import 'package:pigeon/pigeon.dart';
 @ConfigurePigeon(PigeonOptions(
   dartOut: 'lib/src/messages.g.dart',
   dartTestOut: 'test/test_api.g.dart',
-  objcHeaderOut: 'ios/Classes/messages.g.h',
-  objcSourceOut: 'ios/Classes/messages.g.m',
+  objcHeaderOut:
+      'ios/file_selector_ios/Sources/file_selector_ios/include/file_selector_ios/messages.g.h',
+  objcSourceOut: 'ios/file_selector_ios/Sources/file_selector_ios/messages.g.m',
   objcOptions: ObjcOptions(
     prefix: 'FFS',
+    headerIncludePath: './include/file_selector_ios/messages.g.h',
   ),
   copyrightHeader: 'pigeons/copyright.txt',
 ))
