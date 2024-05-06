2da5dc72724ecf7611cfecddbd939dcb504e8c74
Tarrin Neal <tarrinneal@gmail.com>
2024-05-06 04:55:21 -0700

    Fix unnecessary toList/fromList calls during encode/decode process (#6426)
    Fix unnecessary toList/fromList calls during encode/decode process.
    
    also makes some kotlin code more idiomatic.
    
    removes the ability to have collisions with the name `list`.
    
    fixes https://github.com/flutter/flutter/issues/119351

--- packages/pigeon/platform_tests/test_plugin/macos/Classes/TestPlugin.swift
+++ packages/pigeon/platform_tests/test_plugin/macos/Classes/TestPlugin.swift
@@ -84,8 +84,8 @@ public class TestPlugin: NSObject, FlutterPlugin, HostIntegrationCoreApi {
     return anObject
   }
 
-  func echo(_ aList: [Any?]) throws -> [Any?] {
-    return aList
+  func echo(_ list: [Any?]) throws -> [Any?] {
+    return list
   }
 
   func echo(_ aMap: [String?: Any?]) throws -> [String?: Any?] {
@@ -241,8 +241,8 @@ public class TestPlugin: NSObject, FlutterPlugin, HostIntegrationCoreApi {
     completion(.success(anObject))
   }
 
-  func echoAsync(_ aList: [Any?], completion: @escaping (Result<[Any?], Error>) -> Void) {
-    completion(.success(aList))
+  func echoAsync(_ list: [Any?], completion: @escaping (Result<[Any?], Error>) -> Void) {
+    completion(.success(list))
   }
 
   func echoAsync(
@@ -284,8 +284,8 @@ public class TestPlugin: NSObject, FlutterPlugin, HostIntegrationCoreApi {
     completion(.success(anObject))
   }
 
-  func echoAsyncNullable(_ aList: [Any?]?, completion: @escaping (Result<[Any?]?, Error>) -> Void) {
-    completion(.success(aList))
+  func echoAsyncNullable(_ list: [Any?]?, completion: @escaping (Result<[Any?]?, Error>) -> Void) {
+    completion(.success(list))
   }
 
   func echoAsyncNullable(
@@ -456,10 +456,10 @@ public class TestPlugin: NSObject, FlutterPlugin, HostIntegrationCoreApi {
   }
 
   func callFlutterEcho(
-    _ aList: FlutterStandardTypedData,
+    _ list: FlutterStandardTypedData,
     completion: @escaping (Result<FlutterStandardTypedData, Error>) -> Void
   ) {
-    flutterAPI.echo(aList) { response in
+    flutterAPI.echo(list) { response in
       switch response {
       case .success(let res):
         completion(.success(res))
@@ -469,8 +469,8 @@ public class TestPlugin: NSObject, FlutterPlugin, HostIntegrationCoreApi {
     }
   }
 
-  func callFlutterEcho(_ aList: [Any?], completion: @escaping (Result<[Any?], Error>) -> Void) {
-    flutterAPI.echo(aList) { response in
+  func callFlutterEcho(_ list: [Any?], completion: @escaping (Result<[Any?], Error>) -> Void) {
+    flutterAPI.echo(list) { response in
       switch response {
       case .success(let res):
         completion(.success(res))
@@ -556,10 +556,10 @@ public class TestPlugin: NSObject, FlutterPlugin, HostIntegrationCoreApi {
   }
 
   func callFlutterEchoNullable(
-    _ aList: FlutterStandardTypedData?,
+    _ list: FlutterStandardTypedData?,
     completion: @escaping (Result<FlutterStandardTypedData?, Error>) -> Void
   ) {
-    flutterAPI.echoNullable(aList) { response in
+    flutterAPI.echoNullable(list) { response in
       switch response {
       case .success(let res):
         completion(.success(res))
@@ -570,9 +570,9 @@ public class TestPlugin: NSObject, FlutterPlugin, HostIntegrationCoreApi {
   }
 
   func callFlutterEchoNullable(
-    _ aList: [Any?]?, completion: @escaping (Result<[Any?]?, Error>) -> Void
+    _ list: [Any?]?, completion: @escaping (Result<[Any?]?, Error>) -> Void
   ) {
-    flutterAPI.echoNullable(aList) { response in
+    flutterAPI.echoNullable(list) { response in
       switch response {
       case .success(let res):
         completion(.success(res))
