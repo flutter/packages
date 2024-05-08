8d3233084dfd03c53cb1338d8afc58de420ec2eb
Rexios <rexios@rexios.dev>
2024-05-08 12:21:53 -0400

    Add comments to FLTGoogleMapHeatmapController.h

--- packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/FLTGoogleMapHeatmapController.h
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/FLTGoogleMapHeatmapController.h
@@ -10,19 +10,46 @@ NS_ASSUME_NONNULL_BEGIN
 
 // Defines heatmap controllable by Flutter.
 @interface FLTGoogleMapHeatmapController : NSObject
+
+/**
+ Initializes an instance of this class with a heatmap tile layer, a map view, and additional configuration options.
+
+ @param heatmapTileLayer The heatmap tile layer (of type GMUHeatmapTileLayer) that will be used to display heatmap data on the map.
+ @param mapView The map view (of type GMSMapView) where the heatmap layer will be overlaid.
+ @param options A dictionary (NSDictionary) containing any additional options or configuration settings for customizing the heatmap layer.
+
+ @return An initialized instance of this class, configured with the specified heatmap tile layer, map view, and additional options.
+ */
 - (instancetype)initWithHeatmapTileLayer:(GMUHeatmapTileLayer *)heatmapTileLayer
                                  mapView:(GMSMapView *)mapView
                                  options:(NSDictionary *)options;
+
+// Removes this heatmap from the map.
 - (void)removeHeatmap;
+
+// Clears the tile cache in order to visually udpate this heatmap
 - (void)clearTileCache;
 @end
 
+// Defines an interface for controlling heatmaps from Flutter.
 @interface FLTHeatmapsController : NSObject
+
+// Initializes the controller with a GMSMapView.
 - (instancetype)init:(GMSMapView *)mapView;
+
+// Adds heatmaps to the map.
 - (void)addHeatmaps:(NSArray *)heatmapsToAdd;
+
+// Updates heatmaps on the map.
 - (void)changeHeatmaps:(NSArray *)heatmapsToChange;
+
+// Removes heatmaps from the map.
 - (void)removeHeatmapsWithIdentifiers:(NSArray *)identifiers;
+
+// Returns true if a heatmap with the given identifier exists on the map.
 - (bool)hasHeatmapWithIdentifier:(NSString *)identifier;
+
+// Returns the data of the heatmap with the given identifier.
 - (nullable NSDictionary *)heatmapInfoWithIdentifier:(NSString *)identifier;
 @end
 
