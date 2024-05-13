0bd0307e2187bfe873b6a3fe80219eb0799406c1
Farhan Ali <71764231+alifarhanaf@users.noreply.github.com>
2024-05-13 16:23:27 +0500

    Updated route

--- web-side-nav-example-test.dart
+++ web-side-nav-example-test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
-import 'package:go_router_examples/web-side-nav-example.dart.dart';
+import 'package:go_router_examples/web-side-nav-example.dart';
 
 void main() {
   group('Navigation tests', () {
