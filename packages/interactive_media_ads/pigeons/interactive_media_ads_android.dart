90965aa1feff682dbf1c69b969a5281883e4d53d
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-05-13 21:37:15 -0400

    copyright header

--- packages/interactive_media_ads/pigeons/interactive_media_ads_android.dart
+++ packages/interactive_media_ads/pigeons/interactive_media_ads_android.dart
@@ -8,12 +8,8 @@ import 'package:pigeon/pigeon.dart';
 
 @ConfigurePigeon(
   PigeonOptions(
+    copyrightHeader: 'pigeons/copyright.txt',
     dartOut: 'lib/src/android/interactive_media_ads.g.dart',
-    dartOptions: DartOptions(copyrightHeader: <String>[
-      'Copyright 2013 The Flutter Authors. All rights reserved.',
-      'Use of this source code is governed by a BSD-style license that can be',
-      'found in the LICENSE file.',
-    ]),
     kotlinOut:
         'android/src/main/kotlin/dev/flutter/packages/interactive_media_ads/GeneratedInteractiveMediaAdsLibrary.kt',
     kotlinOptions: KotlinOptions(
