4c1cbf8caad59094a896e125b81d1dbdf5561f30
Srujan Gaddam <srujzs@google.com>
2024-05-24 14:20:33 -0700

    Remove unused import

--- packages/video_player/video_player_web/lib/src/pkg_web_tweaks.dart
+++ packages/video_player/video_player_web/lib/src/pkg_web_tweaks.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:js_interop';
 import 'package:web/web.dart' as web;
 
 /// Adds a "disablePictureInPicture" setter to [web.HTMLVideoElement]s.
