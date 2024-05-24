b77d3fb6a9fe306edfcab7f63879d794e27e7312
Thomas Aunvik <contact@thaun.dev>
2024-05-25 00:41:09 +0200

    Pubspec Flutter Version, Getters and Setters for in par with implemented type

--- packages/webview_flutter/webview_flutter_web/test/legacy/mock_fake_iframe_element.dart
+++ packages/webview_flutter/webview_flutter_web/test/legacy/mock_fake_iframe_element.dart
@@ -1,7 +1,6 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 import 'dart:js_interop';
 import 'package:web/web.dart' as html;
 
@@ -13,5 +12,6 @@ class FakeIFrameElement {
 
 extension type MockHTMLIFrameElement(JSObject _)
     implements html.HTMLIFrameElement, JSObject {
-  external String? src;
+  external set src(String? value);
+  external String? get src;
 }
