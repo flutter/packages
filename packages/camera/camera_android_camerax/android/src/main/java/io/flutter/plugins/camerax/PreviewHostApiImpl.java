8fe3230089bfa46347c63cbad22bf27a550955c3
camsim99 <camillesimon90@gmail.com>
2024-06-03 11:35:33 -0700

    Check transformation info

--- packages/camera/camera_android_camerax/android/src/main/java/io/flutter/plugins/camerax/PreviewHostApiImpl.java
+++ packages/camera/camera_android_camerax/android/src/main/java/io/flutter/plugins/camerax/PreviewHostApiImpl.java
@@ -18,6 +18,8 @@ import io.flutter.view.TextureRegistry;
 import java.util.Objects;
 import java.util.concurrent.Executors;
 
+import android.util.Log;
+
 public class PreviewHostApiImpl implements PreviewHostApi {
   final BinaryMessenger binaryMessenger;
   private final InstanceManager instanceManager;
@@ -78,6 +80,8 @@ public class PreviewHostApiImpl implements PreviewHostApi {
     return new Preview.SurfaceProvider() {
       @Override
       public void onSurfaceRequested(@NonNull SurfaceRequest request) {
+        request.setTransformationInfoListener(Executors.newSingleThreadExecutor(), getTransformationInfoListener());
+
         surfaceProducer.setSize(
             request.getResolution().getWidth(), request.getResolution().getHeight());
         Surface flutterSurface = surfaceProducer.getSurface();
@@ -108,6 +112,16 @@ public class PreviewHostApiImpl implements PreviewHostApi {
                   break;
               }
             });
+      }
+    };
+  }
+
+  private SurfaceRequest.TransformationInfoListener getTransformationInfoListener() {
+    return new SurfaceRequest.TransformationInfoListener() {
+      @Override
+      public void onTransformationInfoUpdate(SurfaceRequest.TransformationInfo transformationInfo) {
+        Log.e("CAMILLE DEBUG", "getRotationDegrees(): " + transformationInfo.getRotationDegrees());
+        Log.e("CAMILLE DEBUG", "hasCameraTransform():" + transformationInfo.hasCameraTransform());
       }
     };
   }
