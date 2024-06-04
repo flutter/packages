681b3edf3c711205f509527508943622bd58148e
Victoria Ashworth <vashworth@google.com>
2024-06-04 14:55:44 -0500

    Disable sandboxing for macOS tests in CI

--- packages/pigeon/tool/shared/native_project_runners.dart
+++ packages/pigeon/tool/shared/native_project_runners.dart
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
+import 'package:path/path.dart' as path;
+
 import 'flutter_utils.dart';
 import 'process_utils.dart';
 
@@ -38,11 +42,23 @@ Future<int> runFlutterBuild(
 }
 
 Future<int> runXcodeBuild(
-  String nativeProjectDirectory, {
+  String nativeProjectDirectory,
+  String platform, {
   String? sdk,
   String? destination,
+  String? configuration,
   List<String> extraArguments = const <String>[],
 }) {
+  File? disabledSandboxEntitlementFile;
+  if (extraArguments.contains('test') &&
+      platform.toLowerCase() == 'macos' &&
+      Platform.environment['LUCI_CI'] == 'True') {
+    disabledSandboxEntitlementFile = _createDisabledSandboxEntitlementFile(
+      nativeProjectDirectory,
+      configuration ?? 'Debug',
+    );
+  }
+
   return runProcess(
     'xcodebuild',
     <String>[
@@ -52,10 +68,65 @@ Future<int> runXcodeBuild(
       'Runner',
       if (sdk != null) ...<String>['-sdk', sdk],
       if (destination != null) ...<String>['-destination', destination],
+      if (configuration != null) ...<String>['-configuration', configuration],
       ...extraArguments,
+      if (disabledSandboxEntitlementFile != null)
+        'CODE_SIGN_ENTITLEMENTS=${disabledSandboxEntitlementFile.path}',
     ],
     workingDirectory: nativeProjectDirectory,
   );
+}
+
+/// Finds and copies macOS entitlements file. In the copy, disables sandboxing.
+/// If entitlements file is not found, returns null.
+///
+/// As of macOS 14, testing a macOS sandbox app may prompt the user to grant
+/// access to the app. To workaround this in CI, we create and use a entitlements
+/// file with sandboxing disabled. See
+/// https://developer.apple.com/documentation/security/app_sandbox/accessing_files_from_the_macos_app_sandbox.
+File? _createDisabledSandboxEntitlementFile(
+  String platformDirectory,
+  String configuration,
+) {
+  String entitlementDefaultFileName;
+  if (configuration == 'Release') {
+    entitlementDefaultFileName = 'Release';
+  } else {
+    entitlementDefaultFileName = 'DebugProfile';
+  }
+
+  final String entitlementFilePath = path.join(
+    platformDirectory,
+    'Runner',
+    '$entitlementDefaultFileName.entitlements',
+  );
+  final File entitlementFile = File(entitlementFilePath);
+
+  if (!entitlementFile.existsSync()) {
+    print('Unable to find entitlements file at ${entitlementFile.path}');
+    return null;
+  }
+
+  final String originalEntitlementFileContents =
+      entitlementFile.readAsStringSync();
+  final String tempEntitlementPath = Directory.systemTemp
+      .createTempSync('flutter_disable_sandbox_entitlement.')
+      .path;
+  final File disabledSandboxEntitlementFile = File(path.join(
+    tempEntitlementPath,
+    '${entitlementDefaultFileName}WithDisabledSandboxing.entitlements',
+  ));
+  disabledSandboxEntitlementFile.createSync(recursive: true);
+  disabledSandboxEntitlementFile.writeAsStringSync(
+    originalEntitlementFileContents.replaceAll(
+      RegExp(r'<key>com\.apple\.security\.app-sandbox<\/key>[\S\s]*?<true\/>'),
+      '''
+<key>com.apple.security.app-sandbox</key>
+	<false/>''',
+    ),
+  );
+
+  return disabledSandboxEntitlementFile;
 }
 
 Future<int> runGradleBuild(String nativeProjectDirectory, [String? command]) {
