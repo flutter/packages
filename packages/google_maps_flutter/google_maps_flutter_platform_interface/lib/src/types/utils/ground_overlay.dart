07ae965c59d4466fce8d2cf24918204843a63ec4
pdenert <pdenert09@gmail.com>
2024-06-13 10:01:29 +0200

    Update licenses

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/utils/ground_overlay.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/utils/ground_overlay.dart
@@ -1,3 +1,7 @@
+// Copyright 2024 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import '../types.dart';
 import 'maps_object.dart';
 
