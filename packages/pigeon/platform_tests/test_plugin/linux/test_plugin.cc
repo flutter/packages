32b464548786419cdeebb75fda4b21778f4889a4
Robert Ancell <robert.ancell@canonical.com>
2024-06-18 23:17:13 +1200

    Add test that require a fake messenger

--- packages/pigeon/platform_tests/test_plugin/linux/test_plugin.cc
+++ packages/pigeon/platform_tests/test_plugin/linux/test_plugin.cc
@@ -1815,30 +1815,6 @@ static void void_void(CoreTestsPigeonTestHostSmallApi* api,
 static CoreTestsPigeonTestHostSmallApiVTable host_small_api_vtable = {
     .echo = echo, .void_void = void_void};
 
-// Called when a method call is received from Flutter.
-static void test_plugin_handle_method_call(TestPlugin* self,
-                                           FlMethodCall* method_call) {
-  g_autoptr(FlMethodResponse) response = nullptr;
-
-  const gchar* method = fl_method_call_get_name(method_call);
-
-  if (strcmp(method, "getPlatformVersion") == 0) {
-    response = get_platform_version();
-  } else {
-    response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
-  }
-
-  fl_method_call_respond(method_call, response, nullptr);
-}
-
-FlMethodResponse* get_platform_version() {
-  struct utsname uname_data = {};
-  uname(&uname_data);
-  g_autofree gchar* version = g_strdup_printf("Linux %s", uname_data.version);
-  g_autoptr(FlValue) result = fl_value_new_string(version);
-  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
-}
-
 static void test_plugin_dispose(GObject* object) {
   TestPlugin* self = TEST_PLUGIN(object);
 
@@ -1886,12 +1862,6 @@ static TestPlugin* test_plugin_new(FlBinaryMessenger* messenger) {
   return self;
 }
 
-static void method_call_cb(FlMethodChannel* channel, FlMethodCall* method_call,
-                           gpointer user_data) {
-  TestPlugin* plugin = TEST_PLUGIN(user_data);
-  test_plugin_handle_method_call(plugin, method_call);
-}
-
 void test_plugin_register_with_registrar(FlPluginRegistrar* registrar) {
   g_autoptr(TestPlugin) plugin =
       test_plugin_new(fl_plugin_registrar_get_messenger(registrar));
@@ -1900,6 +1870,4 @@ void test_plugin_register_with_registrar(FlPluginRegistrar* registrar) {
   g_autoptr(FlMethodChannel) channel =
       fl_method_channel_new(fl_plugin_registrar_get_messenger(registrar),
                             "test_plugin", FL_METHOD_CODEC(codec));
-  fl_method_channel_set_method_call_handler(
-      channel, method_call_cb, g_object_ref(plugin), g_object_unref);
 }
