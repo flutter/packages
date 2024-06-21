f116dd244c55907bef9c262c1df41d34a409dd70
stuartmorgan <stuartmorgan@google.com>
2024-06-21 13:05:08 -0400

    [google_maps_flutter] Partial Android host API Pigeon conversion (#6967)
    Converts the host API methods other than the various map-object-update methods to Pigeon, as a starting point for a Pigeon conversion. This introduces the Pigeon structure for the main APIs, without getting into complex object serialization. Future work includes:
    - Converting the update methods.
    - Converting the calls from native to Dart (Flutter API in Pigeon terms).
    
    Also replaces the very minimal Dart unit tests with full tests of all the new methods.
    
    Part of https://github.com/flutter/flutter/issues/117907

--- packages/google_maps_flutter/google_maps_flutter_android/pigeons/messages.dart
+++ packages/google_maps_flutter/google_maps_flutter_android/pigeons/messages.dart
@@ -13,10 +13,10 @@ import 'package:pigeon/pigeon.dart';
 
 /// Pigeon equivalent of LatLng.
 class PlatformLatLng {
-  PlatformLatLng({required this.lat, required this.lng});
+  PlatformLatLng({required this.latitude, required this.longitude});
 
-  final double lat;
-  final double lng;
+  final double latitude;
+  final double longitude;
 }
 
 /// Pigeon equivalent of LatLngBounds.
@@ -45,6 +45,14 @@ class PlatformCluster {
   final List<String?> markerIds;
 }
 
+/// Pigeon representation of an x,y coordinate.
+class PlatformPoint {
+  PlatformPoint({required this.x, required this.y});
+
+  final int x;
+  final int y;
+}
+
 /// Pigeon equivalent of native TileOverlay properties.
 class PlatformTileLayer {
   PlatformTileLayer({
@@ -66,6 +74,59 @@ class PlatformZoomRange {
 
   final double min;
   final double max;
+}
+
+/// Interface for non-test interactions with the native SDK.
+///
+/// For test-only state queries, see [MapsInspectorApi].
+@HostApi()
+abstract class MapsApi {
+  /// Returns once the map instance is available.
+  @async
+  void waitForMap();
+
+  /// Gets the screen coordinate for the given map location.
+  PlatformPoint getScreenCoordinate(PlatformLatLng latLng);
+
+  /// Gets the map location for the given screen coordinate.
+  PlatformLatLng getLatLng(PlatformPoint screenCoordinate);
+
+  /// Gets the map region currently displayed on the map.
+  PlatformLatLngBounds getVisibleRegion();
+
+  /// Gets the current map zoom level.
+  double getZoomLevel();
+
+  /// Show the info window for the marker with the given ID.
+  void showInfoWindow(String markerId);
+
+  /// Hide the info window for the marker with the given ID.
+  void hideInfoWindow(String markerId);
+
+  /// Returns true if the marker with the given ID is currently displaying its
+  /// info window.
+  bool isInfoWindowShown(String markerId);
+
+  /// Sets the style to the given map style string, where an empty string
+  /// indicates that the style should be cleared.
+  ///
+  /// Returns false if there was an error setting the style, such as an invalid
+  /// style string.
+  bool setStyle(String style);
+
+  /// Returns true if the last attempt to set a style, either via initial map
+  /// style or setMapStyle, succeeded.
+  ///
+  /// This allows checking asynchronously for initial style failures, as there
+  /// is no way to return failures from map initialization.
+  bool didLastStyleSucceed();
+
+  /// Clears the cache of tiles previously requseted from the tile provider.
+  void clearTileCache(String tileOverlayId);
+
+  /// Takes a snapshot of the map and returns its image data.
+  @async
+  Uint8List takeSnapshot();
 }
 
 /// Inspector API only intended for use in integration tests.
