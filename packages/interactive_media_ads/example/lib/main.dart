0a7bebd684294c84d31420ea47afd0acb2bc48ef
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-06-25 13:55:35 -0400

    update readme and some bad docs

--- packages/interactive_media_ads/example/lib/main.dart
+++ packages/interactive_media_ads/example/lib/main.dart
@@ -7,8 +7,10 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
+// #docregion imports
 import 'package:interactive_media_ads/interactive_media_ads.dart';
 import 'package:video_player/video_player.dart';
+// #enddocregion imports
 
 /// Entry point for integration tests that require espresso.
 @pragma('vm:entry-point')
@@ -16,11 +18,6 @@ void integrationTestMain() {
   enableFlutterDriverExtension();
   main();
 }
-
-// IMA sample tag for a single skippable inline video ad. See more IMA sample
-// tags at https://developers.google.com/interactive-media-ads/docs/sdks/html5/client-side/tags
-const String _adTagUrl =
-    'https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_preroll_skippable&sz=640x480&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=';
 
 void main() {
   runApp(
@@ -34,7 +31,8 @@ void main() {
   );
 }
 
-/// Example widget displaying an Ad during a video.
+// #docregion example_widget
+/// Example widget displaying an Ad before a video.
 class AdExampleWidget extends StatefulWidget {
   /// Constructs an [AdExampleWidget].
   const AdExampleWidget({super.key});
@@ -44,14 +42,30 @@ class AdExampleWidget extends StatefulWidget {
 }
 
 class _AdExampleWidgetState extends State<AdExampleWidget> {
+  // IMA sample tag for a single skippable inline video ad. See more IMA sample
+  // tags at https://developers.google.com/interactive-media-ads/docs/sdks/html5/client-side/tags
+  static const String _adTagUrl =
+      'https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_preroll_skippable&sz=640x480&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=';
+
+  // The AdsLoader instance exposes the request ads method.
   late final AdsLoader _adsLoader;
+
+  // AdsManager exposes methods to control ad playback and listen to ad events.
   AdsManager? _adsManager;
+
+  // Whether the widget should be displaying the content video. The content
+  // player is hidden while Ads are playing.
   bool _shouldShowContentVideo = true;
 
+  // Controls the content video player.
   late final VideoPlayerController _contentVideoController;
+  // #enddocregion example_widget
 
+  // #docregion ad_and_content_players
   late final AdDisplayContainer _adDisplayContainer = AdDisplayContainer(
     onContainerAdded: (AdDisplayContainer container) {
+      // Ads can't be requested until the `AdDisplayContainer` has been added to
+      // the native View hierarchy.
       _requestAds(container);
     },
   );
@@ -75,21 +89,9 @@ class _AdExampleWidgetState extends State<AdExampleWidget> {
         setState(() {});
       });
   }
+  // #enddocregion ad_and_content_players
 
-  Future<void> _resumeContent() {
-    setState(() {
-      _shouldShowContentVideo = true;
-    });
-    return _contentVideoController.play();
-  }
-
-  Future<void> _pauseContent() {
-    setState(() {
-      _shouldShowContentVideo = false;
-    });
-    return _contentVideoController.pause();
-  }
-
+  // #docregion request_ads
   Future<void> _requestAds(AdDisplayContainer container) {
     _adsLoader = AdsLoader(
       container: container,
@@ -131,15 +133,35 @@ class _AdExampleWidgetState extends State<AdExampleWidget> {
     return _adsLoader.requestAds(AdsRequest(adTagUrl: _adTagUrl));
   }
 
+  Future<void> _resumeContent() {
+    setState(() {
+      _shouldShowContentVideo = true;
+    });
+    return _contentVideoController.play();
+  }
+
+  Future<void> _pauseContent() {
+    setState(() {
+      _shouldShowContentVideo = false;
+    });
+    return _contentVideoController.pause();
+  }
+  // #enddocregion request_ads
+
+  // #docregion dispose
   @override
   void dispose() {
     super.dispose();
     _contentVideoController.dispose();
     _adsManager?.destroy();
   }
+  // #enddocregion dispose
 
+  // #docregion example_widget
+  // #docregion widget_build
   @override
   Widget build(BuildContext context) {
+    // #enddocregion example_widget
     return Scaffold(
       body: Center(
         child: SizedBox(
@@ -179,5 +201,8 @@ class _AdExampleWidgetState extends State<AdExampleWidget> {
                 )
               : null,
     );
+    // #docregion example_widget
   }
+  // #enddocregion widget_build
 }
+// #enddocregion example_widget
