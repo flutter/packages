9994b83f405c0b8ede639d7ded50fc65214d6c2a
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-06-26 13:45:31 -0400

    some tests added

---
+++ packages/interactive_media_ads/example/ios/RunnerTests/AdEventTests.swift
@@ -0,0 +1,48 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import Flutter
+import XCTest
+
+@testable import interactive_media_ads
+import GoogleInteractiveMediaAds
+
+final class AdEventTests: XCTestCase {
+  func testType() {
+    let registrar = TestProxyApiRegistrar()
+    let api = registrar.apiDelegate.pigeonApiIMAAdEvent(registrar)
+    
+    let instance = TestAdEvent.customInit()
+    
+    let value = try? api.pigeonDelegate.type(pigeonApi: api, pigeonInstance: instance)
+    
+    XCTAssertEqual(value, .adBreakEnded)
+  }
+  
+  func testMessage() {
+    let registrar = TestProxyApiRegistrar()
+    let api = registrar.apiDelegate.pigeonApiIMAAdEvent(registrar)
+    
+    let instance = TestAdEvent.customInit()
+    
+    let value = try? api.pigeonDelegate.typeString(pigeonApi: api, pigeonInstance: instance)
+    
+    XCTAssertEqual(value, "message")
+  }
+}
+
+class TestAdEvent: IMAAdEvent {
+  static func customInit() -> TestAdEvent {
+      let instance = TestAdEvent.perform(NSSelectorFromString("new")).takeRetainedValue() as! TestAdEvent
+      return instance
+  }
+  
+  override var type: IMAAdEventType {
+    return .AD_BREAK_ENDED
+  }
+  
+  override var typeString: String {
+    return "message"
+  }
+}
---
+++ packages/interactive_media_ads/example/ios/RunnerTests/TestProxyApiRegistrar.swift
@@ -0,0 +1,16 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import Flutter
+import XCTest
+
+@testable import interactive_media_ads
+
+class TestProxyApiRegistrar: PigeonProxyApiRegistrar {
+  init() {
+    let mockBinaryMessenger = MockBinaryMessenger<String>(
+          codec: FlutterStandardMessageCodec.sharedInstance())
+    super.init(binaryMessenger: mockBinaryMessenger, apiDelegate: ProxyApiDelegate())
+  }
+}
