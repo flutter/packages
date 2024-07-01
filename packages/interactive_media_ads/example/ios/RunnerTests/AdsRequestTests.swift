bff367cc8e0ec38458e56fe8cf6dd7954c984160
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-07-01 15:05:55 -0400

    more more tests

---
+++ packages/interactive_media_ads/example/ios/RunnerTests/AdsRequestTests.swift
@@ -0,0 +1,24 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import Flutter
+import GoogleInteractiveMediaAds
+import XCTest
+
+@testable import interactive_media_ads
+
+final class AdsRequestTests: XCTestCase {
+  func testPigeonDefaultConstructor() {
+    let registrar = TestProxyApiRegistrar()
+    let api = registrar.apiDelegate.pigeonApiIMAAdsRequest(registrar)
+
+    let container = IMAAdDisplayContainer(adContainer: UIView(), viewController: nil)
+    let contentPlayhead = ContentPlayheadImpl()
+    let instance = try? api.pigeonDelegate.pigeonDefaultConstructor(pigeonApi: api, adTagUrl: "adTag", adDisplayContainer: container, contentPlayhead: contentPlayhead)
+
+    XCTAssertTrue(instance != nil)
+    XCTAssertEqual(instance?.adTagUrl, "adTag")
+    XCTAssertIdentical(instance?.adDisplayContainer, container)
+  }
+}
