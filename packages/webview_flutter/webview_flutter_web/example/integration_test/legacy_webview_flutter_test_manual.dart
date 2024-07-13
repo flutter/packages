40d9310b98c6e3f1f169e5bfe3dcf2fec975ba91
David Iglesias Teixeira <ditman@gmail.com>
2024-07-12 17:02:50 -0700

    Rename legacy integration test to prevent it from running in CI.

--- packages/webview_flutter/webview_flutter_web/example/integration_test/legacy_webview_flutter_test_manual.dart
+++ packages/webview_flutter/webview_flutter_web/example/integration_test/legacy_webview_flutter_test_manual.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// This test flakes badly in headless mode!
+
 import 'dart:async';
 
 import 'package:flutter_test/flutter_test.dart';
@@ -14,21 +16,21 @@ import 'wrapped_webview.dart';
 void main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  const String someUrl = 'http://example.com/';
-  const String fakeUrl = 'https://example.com/';
+  const String someUrl = 'about:blank';
+  const String fakeUrl = 'https://www.flutter.dev/';
 
   testWidgets('initialUrl', (WidgetTester tester) async {
     final Completer<WebViewController> controllerCompleter =
         Completer<WebViewController>();
     await tester.pumpWidget(
       wrappedLegacyWebView(fakeUrl, (WebViewController controller) {
-            controllerCompleter.complete(controller);
-          })
+        controllerCompleter.complete(controller);
+      }),
     );
     await controllerCompleter.future;
     // Pump 2 frames so the framework injects the platform view into the DOM.
     await tester.pump();
-    await tester.pump();
+    await tester.pump(const Duration(seconds: 5));
 
     // Assert an iframe has been rendered to the DOM with the correct src attribute.
     final web.HTMLIFrameElement? element =
@@ -42,15 +44,15 @@ void main() async {
         Completer<WebViewController>();
     await tester.pumpWidget(
       wrappedLegacyWebView(someUrl, (WebViewController controller) {
-            controllerCompleter.complete(controller);
-          })
+        controllerCompleter.complete(controller);
+      }),
     );
 
     final WebViewController controller = await controllerCompleter.future;
     await controller.loadUrl(fakeUrl);
     // Pump 2 frames so the framework injects the platform view into the DOM.
     await tester.pump();
-    await tester.pump();
+    await tester.pump(const Duration(seconds: 5));
 
     // Assert an iframe has been rendered to the DOM with the correct src attribute.
     final web.HTMLIFrameElement? element =
