1d6c30c0871513f732f871a8d7383e57606abe6e
Kevin Moore <kevmoo@google.com>
2024-07-22 15:45:48 -0700

    Listenable, ValueListenable, VoidCallback

---
+++ packages/flutter_core/lib/flutter_core.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/callbacks.dart';
+export 'src/listenable.dart';
+export 'src/value_listenable.dart';
