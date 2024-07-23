d618f9fe5f0f247db9ac5913c7b32fa2607d4033
André Sousa <andrelvsousa@gmail.com>
2024-07-23 18:44:14 -0300

    Supress linter warning about the use of unstable APIs

--- packages/video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/RtspVideoAsset.java
+++ packages/video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/RtspVideoAsset.java
@@ -21,6 +21,8 @@ final class RtspVideoAsset extends VideoAsset {
     return new MediaItem.Builder().setUri(assetUrl).build();
   }
 
+  // TODO: Migrate to stable API, see https://github.com/flutter/flutter/issues/147039.
+  @OptIn(markerClass = UnstableApi.class)
   @Override
   MediaSource.Factory getMediaSourceFactory(Context context) {
     return new RtspMediaSource.Factory();
