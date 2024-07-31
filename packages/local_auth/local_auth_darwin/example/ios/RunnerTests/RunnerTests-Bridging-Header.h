371124c372c89ea01014ebc7c317c1f9303a6b38
Stuart Morgan <stuartmorgan@google.com>
2024-07-31 14:22:12 -0400

    fixup: remove bridging header too

--- packages/local_auth/local_auth_darwin/example/ios/RunnerTests/RunnerTests-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-//
-//  Use this file to import your target's public headers that you would like to
-//  expose to Swift.
-//
-
-#import "OCSwiftMock.h"
