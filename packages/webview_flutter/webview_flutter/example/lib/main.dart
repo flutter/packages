ae5787aa492453f2cdc76bb06d0f04db0984c72d
Stuart Morgan <stuartmorgan@google.com>
2024-08-20 13:50:50 -0400

    Update comment in source

--- packages/webview_flutter/webview_flutter/example/lib/main.dart
+++ packages/webview_flutter/webview_flutter/example/lib/main.dart
@@ -15,7 +15,7 @@ import 'package:webview_flutter/webview_flutter.dart';
 // #docregion platform_imports
 // Import for Android features.
 import 'package:webview_flutter_android/webview_flutter_android.dart';
-// Import for iOS features.
+// Import for iOS/macOS features.
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 // #enddocregion platform_imports
 
