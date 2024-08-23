25795a4a18ad6d23e84eb8eb5ae603bca79eda39
chunhtai <47866232+chunhtai@users.noreply.github.com>
2024-08-23 10:29:15 -0700

    [go_router] Fixes issue so that the parseRouteInformationWithContext … (#7337)
    â¦can handle non-http Uris.
    
    fixes https://github.com/flutter/flutter/issues/148389

--- packages/go_router/test/parser_test.dart
+++ packages/go_router/test/parser_test.dart
@@ -81,6 +81,68 @@ void main() {
     expect(matches[1].route, routes[0].routes[0]);
   });
 
+  testWidgets('GoRouteInformationParser can handle empty path for non http uri',
+      (WidgetTester tester) async {
+    final List<GoRoute> routes = <GoRoute>[
+      GoRoute(
+        path: '/',
+        builder: (_, __) => const Placeholder(),
+        routes: <GoRoute>[
+          GoRoute(
+            path: 'abc',
+            builder: (_, __) => const Placeholder(),
+          ),
+        ],
+      ),
+    ];
+    final GoRouteInformationParser parser = await createParser(
+      tester,
+      routes: routes,
+      redirectLimit: 100,
+      redirect: (_, __) => null,
+    );
+
+    final BuildContext context = tester.element(find.byType(Router<Object>));
+
+    final RouteMatchList matchesObj =
+        await parser.parseRouteInformationWithDependencies(
+            createRouteInformation('elbaapp://domain'), context);
+    final List<RouteMatchBase> matches = matchesObj.matches;
+    expect(matches.length, 1);
+    expect(matchesObj.uri.toString(), 'elbaapp://domain/');
+  });
+
+  testWidgets('GoRouteInformationParser cleans up uri',
+      (WidgetTester tester) async {
+    final List<GoRoute> routes = <GoRoute>[
+      GoRoute(
+        path: '/',
+        builder: (_, __) => const Placeholder(),
+        routes: <GoRoute>[
+          GoRoute(
+            path: 'abc',
+            builder: (_, __) => const Placeholder(),
+          ),
+        ],
+      ),
+    ];
+    final GoRouteInformationParser parser = await createParser(
+      tester,
+      routes: routes,
+      redirectLimit: 100,
+      redirect: (_, __) => null,
+    );
+
+    final BuildContext context = tester.element(find.byType(Router<Object>));
+
+    final RouteMatchList matchesObj =
+        await parser.parseRouteInformationWithDependencies(
+            createRouteInformation('http://domain/abc/?query=bde'), context);
+    final List<RouteMatchBase> matches = matchesObj.matches;
+    expect(matches.length, 2);
+    expect(matchesObj.uri.toString(), 'http://domain/abc?query=bde');
+  });
+
   testWidgets(
       "GoRouteInformationParser can parse deeplink root route and maintain uri's scheme, host, query and fragment",
       (WidgetTester tester) async {
