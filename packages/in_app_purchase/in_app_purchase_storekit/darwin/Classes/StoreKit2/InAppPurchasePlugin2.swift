7e853a09bada964a02968a6ee177cb411a3f5bf7
louisehsu <louiseh0313@gmail.com>
2024-08-27 11:56:44 -0700

    oopsies update pigeon by like 8 versions

--- packages/in_app_purchase/in_app_purchase_storekit/darwin/Classes/StoreKit2/InAppPurchasePlugin2.swift
+++ packages/in_app_purchase/in_app_purchase_storekit/darwin/Classes/StoreKit2/InAppPurchasePlugin2.swift
@@ -4,31 +4,33 @@
 
 @available(iOS 15.0, macOS 12.0, *)
 extension InAppPurchasePlugin: InAppPurchase2API {
+  // MARK: - Pigeon Functions
+
+  // Wrapper method around StoreKit2's canMakePayments() method
   // https://developer.apple.com/documentation/storekit/appstore/3822277-canmakepayments
   func canMakePayments() throws -> Bool {
     return AppStore.canMakePayments
   }
 
-  // Pigeon method
+  // Wrapper method around StoreKit2's products() method
+  // https://developer.apple.com/documentation/storekit/product/3851116-products
   func products(
     identifiers: [String], completion: @escaping (Result<[SK2ProductMessage], any Error>) -> Void
   ) {
     Task {
       do {
-        let products = try await rawProducts(identifiers: identifiers)
+        let products = try await Product.products(for: identifiers)
         let productMessages = products.map { product in
           product.convertToPigeon()
         }
         completion(.success(productMessages))
       } catch {
-        completion(.failure(error))
+        throw PigeonError(
+          code: "storekit2_products_error",
+          message: error.localizedDescription,
+          details: error.localizedDescription
+        )
       }
     }
-  }
-
-  // Raw storekit calls
-
-  func rawProducts(identifiers: [String]) async throws -> [Product] {
-    return try await Product.products(for: identifiers)
   }
 }
