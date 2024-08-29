d472256ed9113baf8c8bde6e2d863d73a5e4cc78
LouiseHsu <louisehsu@google.com>
2024-08-29 14:32:24 -0700

    [in_app_purchase_storekit] Add storekit 2 support for canMakePayments and products (#7473)
    This PR contains support for StoreKit2's [canMakePayments](https://developer.apple.com/documentation/storekit/appstore/3822277-canmakepayments) and [products](https://developer.apple.com/documentation/storekit/product/3851116-products).
    
    This also contains basic scaffolding for SK2 support, such as pigeon translators.
    
    Part of https://github.com/flutter/flutter/issues/116383

---
+++ packages/in_app_purchase/in_app_purchase_storekit/lib/src/store_kit_2_wrappers/sk2_appstore_wrapper.dart
@@ -0,0 +1,18 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '../../store_kit_2_wrappers.dart';
+
+InAppPurchase2API _hostApi = InAppPurchase2API();
+
+/// Wrapper for StoreKit2's AppStore
+/// (https://developer.apple.com/documentation/storekit/appstore)
+final class AppStore {
+  /// Dart wrapper for StoreKit2's canMakePayments()
+  /// Returns a bool that indicates whether the person can make purchases.
+  /// (https://developer.apple.com/documentation/storekit/appstore/3822277-canmakepayments)
+  Future<bool> canMakePayments() {
+    return _hostApi.canMakePayments();
+  }
+}
