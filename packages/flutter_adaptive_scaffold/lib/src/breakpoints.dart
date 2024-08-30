a685295d40fffc65a45a367cec62c390271a6be1
Martijn van Dijk <mhvdijk@gmail.com>
2024-08-30 16:34:59 +0200

    Fix breakpoint not being active in certain cases like foldables

--- packages/flutter_adaptive_scaffold/lib/src/breakpoints.dart
+++ packages/flutter_adaptive_scaffold/lib/src/breakpoints.dart
@@ -284,6 +284,7 @@ class Breakpoint {
     final double width = MediaQuery.sizeOf(context).width;
     final double height = MediaQuery.sizeOf(context).height;
     final Orientation orientation = MediaQuery.orientationOf(context);
+    final bool isPortrait = orientation == Orientation.portrait;
 
     final double lowerBoundWidth = beginWidth ?? double.negativeInfinity;
     final double upperBoundWidth = endWidth ?? double.infinity;
@@ -295,11 +296,10 @@ class Breakpoint {
         ? width >= lowerBoundWidth
         : width >= lowerBoundWidth && width < upperBoundWidth;
 
-    final bool isHeightActive = isDesktop(context) ||
-        orientation == Orientation.portrait ||
-        (orientation == Orientation.landscape && andUp
+    final bool isHeightActive =
+        isPortrait || !isPortrait && isWidthActive || andUp
             ? isWidthActive || height >= lowerBoundHeight
-            : height >= lowerBoundHeight && height < upperBoundHeight);
+            : height >= lowerBoundHeight && height < upperBoundHeight;
 
     return isWidthActive && isHeightActive && isRightPlatform;
   }
