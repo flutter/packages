fb4a414abe09bdccd26200ca156f66623d3313a4
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-08-30 22:29:28 +0800

    test: Don't ignore leaks created by test helpers

--- packages/go_router/test/flutter_test_config.dart
+++ packages/go_router/test/flutter_test_config.dart
@@ -8,7 +8,5 @@ import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 
 FutureOr<void> testExecutable(FutureOr<void> Function() testMain) {
   LeakTesting.enable();
-  LeakTesting.settings =
-      LeakTesting.settings.withIgnored(createdByTestHelpers: true);
   return testMain();
 }
