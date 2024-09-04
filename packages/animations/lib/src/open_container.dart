4de134fdb33379d041247a5ccbe83f219d1ea58a
Justin McCandless <jmccandless@google.com>
2024-09-04 14:22:54 -0700

    Proper separation of buildPage and buildTransitions

--- packages/animations/lib/src/open_container.dart
+++ packages/animations/lib/src/open_container.dart
@@ -822,12 +822,7 @@ class _OpenContainerRoute<T> extends ModalRoute<T> {
                               height: _rectTween.end!.height,
                               child: FadeTransition(
                                 opacity: openOpacityTween!.animate(animation),
-                                child: Builder(
-                                  key: _openBuilderKey,
-                                  builder: (BuildContext context) {
-                                    return openBuilder(context, closeContainer);
-                                  },
-                                ),
+                                child: child,
                               ),
                             ),
                           ),
@@ -848,26 +843,9 @@ class _OpenContainerRoute<T> extends ModalRoute<T> {
   @override
   Widget buildTransitions(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
     // TODO(justinmc): This does the normal open container transition.
-    /*
-    return _defaultTransition(
-      context,
-      animation,
-      secondaryAnimation,
-      child,
-    );
-    */
     return _PredictiveBackGestureDetector(
       route: this,
       builder: (BuildContext context) {
-        // TODO(justinmc): Name? And should be in buildPage or no?
-        final Widget backlessChild = _defaultTransition(
-          context,
-          animation,
-          secondaryAnimation,
-          child,
-        );
-
-        print('justin buildTransitions popInProgress? $popGestureInProgress. isAnimating? ${animation.status} ${secondaryAnimation.status}');
         if (popGestureInProgress && animation.status == AnimationStatus.forward) {
           return _PredictiveBackOpenContainerPageTransition(
             animation: animation,
@@ -877,7 +855,12 @@ class _OpenContainerRoute<T> extends ModalRoute<T> {
           );
         }
 
-        return backlessChild;
+        return _defaultTransition(
+          context,
+          animation,
+          secondaryAnimation,
+          child,
+        );
       },
     );
   }
@@ -901,139 +884,6 @@ class _OpenContainerRoute<T> extends ModalRoute<T> {
         ),
       ),
     );
-    /*
-    return Align(
-      alignment: Alignment.topLeft,
-      child: AnimatedBuilder(
-        animation: animation,
-        builder: (BuildContext context, Widget? child) {
-          if (animation.isCompleted) {
-            return SizedBox.expand(
-              child: Material(
-                color: openColor,
-                elevation: openElevation,
-                shape: openShape,
-                child: Builder(
-                  key: _openBuilderKey,
-                  builder: (BuildContext context) {
-                    return openBuilder(context, closeContainer);
-                  },
-                ),
-              ),
-            );
-          }
-
-          final Animation<double> curvedAnimation = CurvedAnimation(
-            parent: animation,
-            curve: Curves.fastOutSlowIn,
-            reverseCurve:
-                _transitionWasInterrupted ? null : Curves.fastOutSlowIn.flipped,
-          );
-          TweenSequence<Color?>? colorTween;
-          TweenSequence<double>? closedOpacityTween, openOpacityTween;
-          Animatable<Color?>? scrimTween;
-          switch (animation.status) {
-            case AnimationStatus.dismissed:
-            case AnimationStatus.forward:
-              closedOpacityTween = _closedOpacityTween;
-              openOpacityTween = _openOpacityTween;
-              colorTween = _colorTween;
-              scrimTween = _scrimFadeInTween;
-            case AnimationStatus.reverse:
-              if (_transitionWasInterrupted) {
-                closedOpacityTween = _closedOpacityTween;
-                openOpacityTween = _openOpacityTween;
-                colorTween = _colorTween;
-                scrimTween = _scrimFadeInTween;
-                break;
-              }
-              closedOpacityTween = _closedOpacityTween.flipped;
-              openOpacityTween = _openOpacityTween.flipped;
-              colorTween = _colorTween.flipped;
-              scrimTween = _scrimFadeOutTween;
-            case AnimationStatus.completed:
-              assert(false); // Unreachable.
-          }
-          assert(colorTween != null);
-          assert(closedOpacityTween != null);
-          assert(openOpacityTween != null);
-          assert(scrimTween != null);
-
-          final Rect rect = _rectTween.evaluate(curvedAnimation)!;
-          return SizedBox.expand(
-            child: Container(
-              color: scrimTween!.evaluate(curvedAnimation),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Transform.translate(
-                  offset: Offset(rect.left, rect.top),
-                  child: SizedBox(
-                    width: rect.width,
-                    height: rect.height,
-                    child: Material(
-                      clipBehavior: Clip.antiAlias,
-                      animationDuration: Duration.zero,
-                      color: colorTween!.evaluate(animation),
-                      shape: _shapeTween.evaluate(curvedAnimation),
-                      elevation: _elevationTween.evaluate(curvedAnimation),
-                      child: Stack(
-                        fit: StackFit.passthrough,
-                        children: <Widget>[
-                          // Closed child fading out.
-                          FittedBox(
-                            fit: BoxFit.fitWidth,
-                            alignment: Alignment.topLeft,
-                            child: SizedBox(
-                              width: _rectTween.begin!.width,
-                              height: _rectTween.begin!.height,
-                              child: (hideableKey.currentState?.isInTree ??
-                                      false)
-                                  ? null
-                                  : FadeTransition(
-                                      opacity: closedOpacityTween!
-                                          .animate(animation),
-                                      child: Builder(
-                                        key: closedBuilderKey,
-                                        builder: (BuildContext context) {
-                                          // Use dummy "open container" callback
-                                          // since we are in the process of opening.
-                                          return closedBuilder(context, () {});
-                                        },
-                                      ),
-                                    ),
-                            ),
-                          ),
-
-                          // Open child fading in.
-                          FittedBox(
-                            fit: BoxFit.fitWidth,
-                            alignment: Alignment.topLeft,
-                            child: SizedBox(
-                              width: _rectTween.end!.width,
-                              height: _rectTween.end!.height,
-                              child: FadeTransition(
-                                opacity: openOpacityTween!.animate(animation),
-                                child: Builder(
-                                  key: _openBuilderKey,
-                                  builder: (BuildContext context) {
-                                    return openBuilder(context, closeContainer);
-                                  },
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  */
   }
 
   @override
