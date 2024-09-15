458cb4ee755ef6d1276df8f7a49cbaa3cfda7412
Vitaly Sokolov <sokoloff06@gmail.com>
2024-09-15 20:35:16 +0300

    Add integration test

---
+++ packages/google_adsense/example/integration_test/ad_widget_rendering_test.dart
@@ -0,0 +1,50 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Run with: flutter drive -d web-server --web-port 7357 --browser-name chrome --driver test_driver/integration_test.dart --target integration_test/ad_widget_rendering_test.dart
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:google_adsense/google_adsense.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:web/web.dart' as web;
+
+const String testClient = 'test_client';
+const String testSlot = 'test_slot';
+
+
+void main() async {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() async {
+    Adsense.resetForTesting();
+  });
+
+  group('adWidget', () {
+    testWidgets('AdUnitWidget is created and rendered',
+        (WidgetTester tester) async {
+      // When
+      Adsense().initialize(testClient);
+      final Widget adUnitWidget = Adsense().adUnit(adSlot: testSlot);
+      await tester.pumpWidget(adUnitWidget);
+      await tester.pumpWidget(adUnitWidget); // TODO(sokoloff06): remove hardcoded time
+      // Then
+      // Widget level
+      expect(find.byWidget(adUnitWidget), findsOneWidget);
+      expect(adUnitWidget, isA<AdUnitWidget>());
+      print('Widget found');
+
+      // DOM level
+      final web.HTMLElement? platformView =
+          web.document.querySelector('flt-platform-view') as web.HTMLElement?;
+      expect(platformView, isNotNull);
+      print('flt-platform-view element found');
+      final web.HTMLElement ins = platformView!.getElementsByTagName('ins').item(0)! as web.HTMLElement;
+      expect(ins.style.display, 'block');
+
+      // TODO(sokoloff06): Add server response
+
+    });
+  });
+}
