4062db47cd2cc67226194a38dfa37b2ae499d862
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-09-18 14:14:34 -0400

    impl done but with some broken code

---
+++ packages/interactive_media_ads/example/ios/RunnerTests/FriendlyObstructionTests.swift
@@ -0,0 +1,54 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import Flutter
+import GoogleInteractiveMediaAds
+import UIKit
+import XCTest
+
+@testable import interactive_media_ads
+
+class FriendlyObstructionProxyApiTest {
+  func testPigeonDefaultConstructor() {
+    let registrar = TestProxyApiRegistrar()
+    let api = registrar.apiDelegate.pigeonApiIMAFriendlyObstruction(registrar)
+
+    let instance = try? api.pigeonDelegate.pigeonDefaultConstructor(
+      pigeonApi: api, view: UIView(), purpose: .mediaControls, detailedReason: "myString")
+    XCTAssertNotNil(instance)
+  }
+
+  func testView() {
+    let registrar = TestProxyApiRegistrar()
+    let api = registrar.apiDelegate.pigeonApiIMAFriendlyObstruction(registrar)
+
+    let instance = IMAFriendlyObstruction(
+      view: UIView(), purpose: IMAFriendlyObstructionPurpose.closeAd, detailedReason: "reason")
+    let value = try? api.pigeonDelegate.view(pigeonApi: api, pigeonInstance: instance)
+
+    XCTAssertEqual(value, instance.view)
+  }
+
+  func testPurpose() {
+    let registrar = TestProxyApiRegistrar()
+    let api = registrar.apiDelegate.pigeonApiIMAFriendlyObstruction(registrar)
+
+    let instance = IMAFriendlyObstruction(
+      view: UIView(), purpose: IMAFriendlyObstructionPurpose.closeAd, detailedReason: "reason")
+    let value = try? api.pigeonDelegate.purpose(pigeonApi: api, pigeonInstance: instance)
+
+    XCTAssertEqual(value, FriendlyObstructionPurpose.closeAd)
+  }
+
+  func testDetailedReason() {
+    let registrar = TestProxyApiRegistrar()
+    let api = registrar.apiDelegate.pigeonApiIMAFriendlyObstruction(registrar)
+
+    let instance = IMAFriendlyObstruction(
+      view: UIView(), purpose: IMAFriendlyObstructionPurpose.closeAd, detailedReason: "reason")
+    let value = try? api.pigeonDelegate.detailedReason(pigeonApi: api, pigeonInstance: instance)
+
+    XCTAssertEqual(value, instance.detailedReason)
+  }
+}
