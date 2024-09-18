973e442cf2fdcd7df42024ece1dcac6f7fb83a65
NatanGameron <40638736+NatanGameron@users.noreply.github.com>
2024-09-18 19:32:35 -0300

    Support Specific Non Countable Markers Ids
    Create clusters, and specify markers to do not add to count.

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/cluster.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/cluster.dart
@@ -10,12 +10,13 @@ import 'types.dart';
 @immutable
 class Cluster {
   /// Creates a cluster with its location [LatLng], bounds [LatLngBounds],
-  /// and list of [MarkerId]s in the cluster.
+  /// list of [MarkerId]s in the cluster, and optional non-countable markers.
   const Cluster(
     this.clusterManagerId,
     this.markerIds, {
     required this.position,
     required this.bounds,
+    this.nonCountableMarkerIds = const [],
   }) : assert(markerIds.length > 0);
 
   /// ID of the [ClusterManager] of the cluster.
@@ -30,12 +31,18 @@ class Cluster {
   /// List of [MarkerId]s in the cluster.
   final List<MarkerId> markerIds;
 
-  /// Returns the number of markers in the cluster.
-  int get count => markerIds.length;
+  /// List of [MarkerId]s that should not be counted.
+  final List<MarkerId> nonCountableMarkerIds;
+
+  /// Returns the number of markers in the cluster, excluding non-countable markers.
+  int get count => markerIds.length - nonCountableMarkerIds.length;
+
+  /// Returns the total number of markers (including non-countable markers).
+  int get totalMarkers => markerIds.length;
 
   @override
   String toString() =>
-      '${objectRuntimeType(this, 'Cluster')}($clusterManagerId, $position, $bounds, $markerIds)';
+      '${objectRuntimeType(this, 'Cluster')}($clusterManagerId, $position, $bounds, $markerIds, $nonCountableMarkerIds)';
 
   @override
   bool operator ==(Object other) {
@@ -43,10 +50,11 @@ class Cluster {
         other.clusterManagerId == clusterManagerId &&
         other.position == position &&
         other.bounds == bounds &&
-        listEquals(other.markerIds, markerIds);
+        listEquals(other.markerIds, markerIds) &&
+        listEquals(other.nonCountableMarkerIds, nonCountableMarkerIds);
   }
 
   @override
   int get hashCode =>
-      Object.hash(clusterManagerId, position, bounds, markerIds);
+      Object.hash(clusterManagerId, position, bounds, markerIds, nonCountableMarkerIds);
 }
