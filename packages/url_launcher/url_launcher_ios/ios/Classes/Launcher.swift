94291007f356996a788786bc40de8f29afe9afca
ant-bry <46670110+ant-bry@users.noreply.github.com>
2024-09-23 17:09:46 +0800

    Update Launcher.swift

--- packages/url_launcher/url_launcher_ios/ios/Classes/Launcher.swift
+++ packages/url_launcher/url_launcher_ios/ios/Classes/Launcher.swift
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import UIKit
+
 /// Protocol for UIApplication methods relating to launching URLs.
 ///
 /// This protocol exists to allow injecting an alternate implementation for testing.
@@ -16,5 +18,21 @@ protocol Launcher {
     completionHandler completion: ((Bool) -> Void)?)
 }
 
-/// Launcher is intentionally a direct passthroguh to UIApplication.
-extension UIApplication: Launcher {}
+// TODO(hellohuanlin): This wrapper is a workaround for iOS 18 Beta 3 where completionHandler is annotated with @MainActor @Sendable, resulting in compile error when conforming UIApplication to Launcher. We should try again in newer betas.
+/// A default URL launcher.
+final class DefaultLauncher: Launcher {
+  func canOpenURL(_ url: URL) -> Bool {
+    return UIApplication.shared.canOpenURL(url)
+  }
+
+  func open(
+    _ url: URL,
+    options: [UIApplication.OpenExternalURLOptionsKey: Any],
+    completionHandler completion: ((Bool) -> Void)?
+  ) {
+    UIApplication.shared.open(
+      url,
+      options: options,
+      completionHandler: completion)
+  }
+}
