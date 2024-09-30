a45bc1c0c85dd59d4b9c6c1ec5509edac408976e
adsonpleal <adsonpleal@gmail.com>
2024-09-30 11:37:46 -0300

    Add license header to devtools_eval_test

--- packages/shared_preferences/shared_preferences/test/devtools_eval_test.dart
+++ packages/shared_preferences/shared_preferences/test/devtools_eval_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
