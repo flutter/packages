93e5cc08b1e8a1a806cb27b38db55795414e45f2
Yaakov Schectman <schectman@google.com>
2024-09-30 16:23:36 -0400

    Define initial pigeon

---
+++ packages/camera/camera_android/pigeons/messages.dart
@@ -0,0 +1,25 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(PigeonOptions(
+  dartOut: 'lib/src/messages.g.dart',
+  javaOptions: JavaOptions(package: 'io.flutter.plugins.camera'),
+  javaOut: 'android/src/main/java/io/flutter/plugins/camera/Messages.java',
+  copyrightHeader: 'pigeons/copyright.txt',
+))
+
+enum PlatformCameraLensDirection {
+  front,
+  back,
+  external,
+}
+
+class PlatformCameraDescription {
+  PlatformCameraDescription({required this.name, required this.lensDirection});
+  final String name;
+  final PlatformCameraLensDirection lensDirection;
+}
+
+@HostApi()
+abstract class CameraApi {
+  List<PlatformCameraDescription> getAvailableCameras();
+}
