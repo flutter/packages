cdd1a14a4513bd707041c5b48235a2d02f886795
Kenzie Davisson <kenzieschmoll@google.com>
2024-10-01 15:27:03 -0700

    dart doc

--- packages/web_benchmarks/lib/src/metrics.dart
+++ packages/web_benchmarks/lib/src/metrics.dart
@@ -2,37 +2,39 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// The metrics measured for each benchmark.
+/// The names for the metrics collected by the benchmark recorder.
 enum BenchmarkMetric {
-  /// A benchmark metric that includes frame-related computations prior to
-  /// submitting layer and picture operations to the underlying renderer, such as
-  /// HTML and CanvasKit. During this phase we compute transforms, clips, and
-  /// other information needed for rendering.
+  /// The name for the benchmark metric that includes frame-related computations
+  /// prior to submitting layer and picture operations to the underlying
+  /// renderer, such as HTML and CanvasKit.
+  ///
+  /// During this phase we compute transforms, clips, and other information
+  /// needed for rendering.
   prerollFrame('preroll_frame'),
 
-  /// A benchmark metric that includes submitting layer and picture information
-  /// to the renderer.
+  /// The name for the benchmark metric that includes submitting layer and
+  /// picture information to the renderer.
   applyFrame('apply_frame'),
 
-  /// A benchmark metric that measures the time spent in [PlatformDispatcher]'s
-  /// onDrawFrame callback.
+  /// The name for the benchmark metric that measures the time spent in
+  /// [PlatformDispatcher]'s onDrawFrame callback.
   drawFrame('draw_frame'),
 
-  /// A benchmark metric that tracks the timespan between vsync start and raster
-  /// finish for a Flutter frame.
+  /// The name for the benchmark metric that tracks the timespan between vsync
+  /// start and raster finish for a Flutter frame.
   ///
   /// This value corresponds to [FrameTiming.totalSpan] from the Flutter Engine.
   flutterFrameTotalTime('flutter_frame.total_time'),
 
-  /// A benchmark metric that tracks the duration to build the Flutter frame on
-  /// the Dart UI thread.
+  /// The name for the benchmark metric that tracks the duration to build the
+  /// Flutter frame on the Dart UI thread.
   ///
   /// This value corresponds to [FrameTiming.buildDuration] from the Flutter
   /// Engine.
   flutterFrameBuildTime('flutter_frame.build_time'),
 
-  /// A benchmark metric that tracks the duration to rasterize the Flutter frame
-  /// on the Dart raster thread.
+  /// The name for the benchmark metric that tracks the duration to rasterize
+  /// the Flutter frame on the Dart raster thread.
   ///
   /// This value corresponds to [FrameTiming.rasterDuration] from the Flutter
   /// Engine.
