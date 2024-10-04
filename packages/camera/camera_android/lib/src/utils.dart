b7f814af70671a8a5bad5251cdb14164c469654b
Yaakov Schectman <schectman@google.com>
2024-10-04 08:46:36 -0400

    Dart PR Feedback

--- packages/camera/camera_android/lib/src/utils.dart
+++ packages/camera/camera_android/lib/src/utils.dart
@@ -7,19 +7,6 @@ import 'package:flutter/services.dart';
 
 import 'messages.g.dart';
 
-/// Parses a string into a corresponding CameraLensDirection.
-CameraLensDirection parseCameraLensDirection(String string) {
-  switch (string) {
-    case 'front':
-      return CameraLensDirection.front;
-    case 'back':
-      return CameraLensDirection.back;
-    case 'external':
-      return CameraLensDirection.external;
-  }
-  throw ArgumentError('Unknown CameraLensDirection value');
-}
-
 /// Converts a [PlatformCameraLensDirection] to [CameraLensDirection].
 CameraLensDirection cameraLensDirectionFromPlatform(
     PlatformCameraLensDirection direction) {
