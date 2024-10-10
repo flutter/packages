55118bcb1d4cb14c028d6be706941ba02ef80d58
Ahmed Bilal <127771428+ahmedbilal008@users.noreply.github.com>
2024-10-11 02:33:10 +0500

    Updated the test for video player

--- packages/video_player/video_player_web/example/integration_test/video_player_test.dart
+++ packages/video_player/video_player_web/example/integration_test/video_player_test.dart
@@ -40,14 +40,19 @@ void main() {
       expect(video.playsInline, true, reason: 'Needed by safari iOS');
     });
 
-    testWidgets('setVolume', (WidgetTester tester) async {
+     testWidgets('setVolume', (WidgetTester tester) async {
       final VideoPlayer player = VideoPlayer(videoElement: video)..initialize();
 
       player.setVolume(0);
-
-      expect(video.volume, isZero, reason: 'Volume should be zero');
+      
       expect(video.muted, isTrue, reason: 'muted attribute should be true');
+      //If the volume is set to zero, tapping the unmute button may not restore the audio as expected.
+      expect(video.volume, greaterThan(0), reason: 'Volume should not be set to zero when muted');
 
+      player.setVolume(0.5);
+      expect(video.volume, 0.5, reason: 'Volume should be set to 0.5');
+      expect(video.muted, isFalse, reason: 'Muted attribute should be false');
+      
       expect(() {
         player.setVolume(-0.0001);
       }, throwsAssertionError, reason: 'Volume cannot be < 0');
