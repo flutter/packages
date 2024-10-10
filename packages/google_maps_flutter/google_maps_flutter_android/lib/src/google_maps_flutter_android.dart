604a1de6b481b49df036cc83c2614bae57ec1245
Yaakov Schectman <schectman@google.com>
2024-10-10 13:09:26 -0400

    Linter ignore directive

--- packages/google_maps_flutter/google_maps_flutter_android/lib/src/google_maps_flutter_android.dart
+++ packages/google_maps_flutter/google_maps_flutter_android/lib/src/google_maps_flutter_android.dart
@@ -868,7 +868,12 @@ class GoogleMapsFlutterAndroid extends GoogleMapsFlutterPlatform {
         return PlatformMapBitmapScaling.none;
     }
 
-    /// MapBitmapScaling is defined in the platform interface.
+    // The enum comes from a different package, which could get a new value at
+    // any time, so provide a fallback that ensures this won't break when used
+    // with a version that contains new values. This is deliberately outside
+    // the switch rather than a `default` so that the linter will flag the
+    // switch as needing an update.
+    // ignore: dead_code
     return PlatformMapBitmapScaling.auto;
   }
 
@@ -1325,8 +1330,12 @@ PlatformPatternItem platformPatternItemFromPatternItem(PatternItem item) {
           type: PlatformPatternItemType.gap, length: length);
   }
 
-  /// [PatternItemType] is defined in the platform interface package. In case
-  /// a new value is added, a fallback default value is returned.
+  // The enum comes from a different package, which could get a new value at
+  // any time, so provide a fallback that ensures this won't break when used
+  // with a version that contains new values. This is deliberately outside
+  // the switch rather than a `default` so that the linter will flag the
+  // switch as needing an update.
+  // ignore: dead_code
   return PlatformPatternItem(type: PlatformPatternItemType.dot);
 }
 
