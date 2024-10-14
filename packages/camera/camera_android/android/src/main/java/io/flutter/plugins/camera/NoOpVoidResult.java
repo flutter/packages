360a82b756f4bc6c2588c2430c761037464fe267
yaakovschectman <109111084+yaakovschectman@users.noreply.github.com>
2024-10-14 11:59:22 -0400

    Update NoOpVoidResult.java

--- packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/NoOpVoidResult.java
+++ packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/NoOpVoidResult.java
@@ -6,7 +6,10 @@ package io.flutter.plugins.camera;
 
 import androidx.annotation.NonNull;
 
-/** A convenience class for results of a Pigeon Flutter API method call that perform no action. */
+/** A convenience class for results of a Pigeon Flutter API method call that perform no action.
+  *
+  * <p>Longer term, any call using this is likely a good candidate to migrate to event channels.
+  */
 public class NoOpVoidResult implements Messages.VoidResult {
   @Override
   public void success() {}
