32d3c1781e2d286dcb31701b501c1d8325a2a6c7
Kallen Tu <kallentu@google.com>
2024-10-24 19:27:43 +0000

    Revert indented code block change

--- packages/webview_flutter/webview_flutter_web/lib/src/http_request_factory.dart
+++ packages/webview_flutter/webview_flutter_web/lib/src/http_request_factory.dart
@@ -38,24 +38,22 @@ class HttpRequestFactory {
   /// /// If `Access-Control-Expose-Headers` has not been set to true, only a subset of all the response headers will be returned when calling [getAllResponseHeaders].
   ///
   /// The following is equivalent to the [getString] sample above:
-  /// ```dart
-  /// var name = Uri.encodeQueryComponent('John');
-  /// var id = Uri.encodeQueryComponent('42');
-  /// HttpRequest.request('users.json?name=$name&id=$id')
-  ///   .then((HttpRequest resp) {
-  ///     // Do something with the response.
-  /// });
-  /// ```
+  ///
+  ///     var name = Uri.encodeQueryComponent('John');
+  ///     var id = Uri.encodeQueryComponent('42');
+  ///     HttpRequest.request('users.json?name=$name&id=$id')
+  ///       .then((HttpRequest resp) {
+  ///         // Do something with the response.
+  ///     });
   ///
   /// Here's an example of submitting an entire form with [FormData].
-  /// ```dart
-  /// var myForm = querySelector('form#myForm');
-  /// var data = new FormData(myForm);
-  /// HttpRequest.request('/submit', method: 'POST', sendData: data)
-  ///   .then((HttpRequest resp) {
-  ///     // Do something with the response.
-  /// });
-  /// ```
+  ///
+  ///     var myForm = querySelector('form#myForm');
+  ///     var data = new FormData(myForm);
+  ///     HttpRequest.request('/submit', method: 'POST', sendData: data)
+  ///       .then((HttpRequest resp) {
+  ///         // Do something with the response.
+  ///     });
   ///
   /// Requests for `file://` URIs are only supported by Chrome extensions
   /// with appropriate permissions in their manifest. Requests to file:// URIs
