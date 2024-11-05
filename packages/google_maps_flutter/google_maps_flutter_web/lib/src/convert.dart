49f3cb4b3e901bfea90225d6b19684bfaaa621df
Hari07 <22373191+Hari-07@users.noreply.github.com>
2024-11-05 09:08:10 +0400

    Use zIndexInt on web

--- packages/google_maps_flutter/google_maps_flutter_web/lib/src/convert.dart
+++ packages/google_maps_flutter/google_maps_flutter_web/lib/src/convert.dart
@@ -266,7 +266,7 @@ gmaps.InfoWindowOptions? _infoWindowOptionsFromMarker(Marker marker) {
 
   return gmaps.InfoWindowOptions()
     ..content = container
-    ..zIndex = marker.zIndex;
+    ..zIndex = marker.effectiveZIndex;
   // TODO(ditman): Compute the pixelOffset of the infoWindow, from the size of the Marker,
   // and the marker.infoWindow.anchor property.
 }
@@ -456,7 +456,7 @@ Future<gmaps.MarkerOptions> _markerOptionsFromMarker(
       marker.position.longitude,
     )
     ..title = sanitizeHtml(marker.infoWindow.title ?? '')
-    ..zIndex = marker.zIndex
+    ..zIndex = marker.effectiveZIndex
     ..visible = marker.visible
     ..opacity = marker.alpha
     ..draggable = marker.draggable
@@ -704,4 +704,14 @@ gmaps.LatLng _pixelToLatLng(gmaps.Map map, int x, int y) {
       gmaps.Point((x / scale) + bottomLeft.x, (y / scale) + topRight.y);
 
   return projection.fromPointToLatLng(point)!;
+}
+
+extension on Marker {
+  // We compare with zero because zIndexInt if non zero should get higher priority than zIndex
+  num? get effectiveZIndex {
+    if (zIndexInt != 0) {
+      return zIndexInt;
+    }
+    return zIndex;
+  }
 }
