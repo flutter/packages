7b75557f704e83553603eb9f1183041c3dd09891
Camille Simon <camillesimon@google.com>
2024-11-06 16:54:06 -0800

    Format and add back removed block

--- packages/animations/example/android/app/src/main/kotlin/dev/flutter/packages/animations/example/MainActivity.kt
+++ packages/animations/example/android/app/src/main/kotlin/dev/flutter/packages/animations/example/MainActivity.kt
@@ -6,4 +6,4 @@ package dev.flutter.packages.animations.example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
