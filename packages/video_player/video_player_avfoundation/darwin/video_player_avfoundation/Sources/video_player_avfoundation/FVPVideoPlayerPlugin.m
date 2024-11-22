83cec0c802706ab2036d7e227a514afcf13fc267
Paweł Jakubowski <pawel.jakubowski@leancode.pl>
2024-11-22 11:47:14 +0100

    Add FVPVideoPlayerPlugin_Test import to plugin file

--- packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation/FVPVideoPlayerPlugin.m
+++ packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation/FVPVideoPlayerPlugin.m
@@ -9,6 +9,7 @@
 #import "./include/video_player_avfoundation/FVPDisplayLink.h"
 #import "./include/video_player_avfoundation/FVPFrameUpdater.h"
 #import "./include/video_player_avfoundation/FVPVideoPlayer.h"
+#import "./include/video_player_avfoundation/FVPVideoPlayerPlugin_Test.h"
 #import "./include/video_player_avfoundation/messages.g.h"
 
 #if !__has_feature(objc_arc)
