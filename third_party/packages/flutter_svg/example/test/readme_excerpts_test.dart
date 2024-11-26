4f250c6333cf73538b4ee35a7dbbff2134c8ed4e
Stuart Morgan <stuartmorgan@google.com>
2024-11-26 15:00:49 -0500

    Switch to Dart unit tests

--- third_party/packages/flutter_svg/example/test/readme_excerpts_test.dart
+++ third_party/packages/flutter_svg/example/test/readme_excerpts_test.dart
@@ -5,30 +5,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg_example/readme_excerpts.dart' as excerpts;
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  testWidgets('example simple loadAsset works', (WidgetTester tester) async {
+  test('example simple loadAsset works', () async {
     final Widget svg = excerpts.loadAsset();
     expect(svg, isNotNull);
   });
 
-  testWidgets('example loadAsset with color filter works',
-      (WidgetTester tester) async {
+  test('example loadAsset with color filter works', () async {
     final Widget svg = excerpts.loadAsset();
     expect(svg, isNotNull);
   });
 
-  testWidgets('example loadAsset with a non-existent asset works',
-      (WidgetTester tester) async {
+  test('example loadAsset with a non-existent asset works', () async {
     final Widget svg = excerpts.loadMissingAsset();
     expect(svg, isNotNull);
   });
 
-  testWidgets('example loadAsset with a precompiled asset works',
-      (WidgetTester tester) async {
+  test('example loadAsset with a precompiled asset works', () async {
     final Widget svg = excerpts.loadPrecompiledAsset();
     expect(svg, isNotNull);
   });
