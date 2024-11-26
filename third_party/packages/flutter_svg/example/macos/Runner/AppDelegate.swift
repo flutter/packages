891424d5b785943acca125cacc97e35cbc44635b
Stuart Morgan <stuartmorgan@google.com>
2024-11-26 12:30:04 -0500

    Revert project automigrations

--- third_party/packages/flutter_svg/example/macos/Runner/AppDelegate.swift
+++ third_party/packages/flutter_svg/example/macos/Runner/AppDelegate.swift
@@ -1,13 +1,9 @@
 import Cocoa
 import FlutterMacOS
 
-@main
+@NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
-  }
-
-  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
 }
