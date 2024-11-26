5dc241eaf66707bc7995dc1bac7a7fca9b5a146f
Paweł Jakubowski <pawel.jakubowski@leancode.pl>
2024-11-26 17:21:36 +0100

    Start non-texture player ids from a high number to avoid collisions

--- packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation/FVPVideoPlayerPlugin.m
+++ packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation/FVPVideoPlayerPlugin.m
@@ -40,6 +40,10 @@
 
 #pragma mark -
 
+/// The next non-texture player ID, initialized to a high number to avoid collisions with
+/// texture IDs (which are generated separately).
+static int64_t nextNonTexturePlayerId = 1000000;
+
 @interface FVPVideoPlayerPlugin ()
 @property(readonly, weak, nonatomic) NSObject<FlutterTextureRegistry> *registry;
 @property(readonly, weak, nonatomic) NSObject<FlutterBinaryMessenger> *messenger;
@@ -96,9 +100,9 @@
     playerId = [self.registry registerTexture:(FVPVideoPlayerTextureApproach *)player];
     frameUpdater.textureId = playerId;
   } else {
-    // FIXME Possibly start with a predefined prefix and then increment it to avoid
-    //  collisions withtextureId.
-    playerId = arc4random();
+    @synchronized(self) {
+      playerId = nextNonTexturePlayerId++;
+    }
   }
 
   FlutterEventChannel *eventChannel = [FlutterEventChannel
