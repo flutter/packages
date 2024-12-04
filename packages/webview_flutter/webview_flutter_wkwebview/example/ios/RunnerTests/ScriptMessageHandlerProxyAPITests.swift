edabb717f13de99d15f108e3d04d7d65b83fe12d
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-12-04 15:16:42 -0500

    script message handler tests

---
+++ packages/webview_flutter/webview_flutter_wkwebview/example/ios/RunnerTests/ScriptMessageHandlerProxyAPITests.swift
@@ -0,0 +1,39 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import WebKit
+import Flutter
+import XCTest
+
+@testable import webview_flutter_wkwebview
+
+class ScriptMessageHandlerProxyAPITests: XCTestCase {
+  func testPigeonDefaultConstructor() {
+    let registrar = TestProxyApiRegistrar()
+    let api = registrar.apiDelegate.pigeonApiWKScriptMessageHandler(registrar)
+
+    let instance = try? api.pigeonDelegate.pigeonDefaultConstructor(pigeonApi: api )
+    XCTAssertNotNil(instance)
+  }
+
+  @MainActor func testDidReceiveScriptMessage() {
+    let api = TestScriptMessageHandlerApi()
+    let instance = ScriptMessageHandlerImpl(api: api)
+    let controller = WKUserContentController()
+    let message = WKScriptMessage()
+    
+    instance.userContentController(controller, didReceive: message)
+
+    XCTAssertEqual(api.didReceiveScriptMessageArgs, [controller, message])
+  }
+
+}
+
+class TestScriptMessageHandlerApi: PigeonApiProtocolWKScriptMessageHandler {
+  var didReceiveScriptMessageArgs: [AnyHashable?]? = nil
+  
+  func didReceiveScriptMessage(pigeonInstance pigeonInstanceArg: any WKScriptMessageHandler, controller controllerArg: WKUserContentController, message messageArg: WKScriptMessage, completion: @escaping (Result<Void, PigeonError>) -> Void) {
+    didReceiveScriptMessageArgs = [controllerArg, messageArg]
+  }
+}
