6f269a298a7204d979aae14469cc3947eda55fb6
Olli Helenius <olli.helenius@codemate.com>
2024-12-05 11:57:48 +0200

    Fix license blocks in new files

--- packages/camera/camera_windows/windows/task_runner.h
+++ packages/camera/camera_windows/windows/task_runner.h
@@ -1,6 +1,7 @@
-// Copyright 2024 The Flutter Authors. All rights reserved.
+// Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #ifndef PACKAGES_CAMERA_CAMERA_WINDOWS_WINDOWS_TASK_RUNNER_H_
 #define PACKAGES_CAMERA_CAMERA_WINDOWS_WINDOWS_TASK_RUNNER_H_
 
