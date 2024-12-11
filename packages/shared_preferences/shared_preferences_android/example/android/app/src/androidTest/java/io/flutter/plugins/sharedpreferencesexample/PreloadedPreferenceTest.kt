1df19cc7a03c8e0bac637140a214e8e85ed4391b
tarrinneal <tarrinneal@gmail.com>
2024-12-10 23:44:18 -0800

    test

---
+++ packages/shared_preferences/shared_preferences_android/example/android/app/src/androidTest/java/io/flutter/plugins/sharedpreferencesexample/PreloadedPreferenceTest.kt
@@ -0,0 +1,15 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package io.flutter.plugins.shared_preferences_example
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class PreloadedPreferenceTest {
+  // This test will be useful once the pr this proceeds is merged.
+  @Test
+  fun preloadedPreferencesIsAdded() {
+    assertTrue(true)
+  }
+}
