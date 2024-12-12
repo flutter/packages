ca97a7334947a9c951912ac94c3fc411942a804d
Joonas Kerttula <joonas.kerttula@codemate.com>
2024-12-12 19:57:26 +0200

    fix: import GoogleMapsUtilsObjC instead of import GoogleMapsUtils

--- packages/google_maps_flutter/google_maps_flutter_ios/ios/google_maps_flutter_ios/Sources/google_maps_flutter_ios/GoogleMapController.m
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/google_maps_flutter_ios/Sources/google_maps_flutter_ios/GoogleMapController.m
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-@import GoogleMapsUtils;
+@import GoogleMapsUtilsObjC;
 
 #import "GoogleMapController.h"
 
