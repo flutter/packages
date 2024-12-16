6b9f199659d5631726f859172292654c6292ea68
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-12-16 05:40:16 -0500

    start docs for platform webview

--- packages/webview_flutter/webview_flutter_wkwebview/lib/src/common/platform_webview.dart
+++ packages/webview_flutter/webview_flutter_wkwebview/lib/src/common/platform_webview.dart
@@ -6,7 +6,9 @@ import 'package:flutter/foundation.dart';
 
 import 'web_kit.g.dart';
 
+/// Platform agnostic native WebView.
 class PlatformWebView {
+  /// Creates a [PlatformWebView].
   PlatformWebView({
     required WKWebViewConfiguration initialConfiguration,
     void Function(
@@ -53,6 +55,8 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// A Boolean value that indicates whether there is a valid back item in the
+  /// back-forward list.
   Future<bool> canGoBack() {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -65,6 +69,8 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// A Boolean value that indicates whether there is a valid forward item in
+  /// the back-forward list.
   Future<bool> canGoForward() {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -77,6 +83,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// The object that contains the configuration details for the web view.
   WKWebViewConfiguration get configuration {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -89,6 +96,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// Evaluates the specified JavaScript string.
   Future<Object?> evaluateJavaScript(String javaScriptString) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -101,6 +109,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// The custom user agent string.
   Future<String?> getCustomUserAgent() {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -113,6 +122,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// An estimate of what fraction of the current navigation has been loaded.
   Future<double> getEstimatedProgress() {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -125,6 +135,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// The page title.
   Future<String?> getTitle() {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -137,6 +148,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// The URL being requested.
   Future<String?> getUrl() {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -149,6 +161,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// Navigates to the back item in the back-forward list.
   Future<void> goBack() {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -161,6 +174,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// Navigates to the forward item in the back-forward list.
   Future<void> goForward() {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -173,6 +187,8 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// Loads the web content that the specified URL request object references and
+  /// navigates to that content.
   Future<void> load(URLRequest request) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -185,6 +201,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// Loads the web content from the specified file and navigates to it.
   Future<void> loadFileUrl(String url, String readAccessUrl) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -197,6 +214,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// Convenience method to load a Flutter asset.
   Future<void> loadFlutterAsset(String key) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -209,6 +227,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// Loads the contents of the specified HTML string and navigates to it.
   Future<void> loadHtmlString(String string, String? baseUrl) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -221,7 +240,9 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
-  void Function(NSObject pigeon_instance, String? keyPath, NSObject? object,
+  /// Informs the observing object when the value at the specified key path
+  /// relative to the observed object has changed.
+  void Function(NSObject pigeonInstance, String? keyPath, NSObject? object,
       Map<KeyValueChangeKey, Object>? change)? get observeValue {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -234,6 +255,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// Reloads the current webpage.
   Future<void> reload() {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -246,6 +268,9 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// Stops the observer object from receiving change notifications for the
+  /// property specified by the key path relative to the object receiving this
+  /// message.
   Future<void> removeObserver(NSObject object, String keyPath) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -258,6 +283,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// The scroll view associated with the web view.
   UIScrollView get scrollView {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -270,6 +296,8 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// A Boolean value that indicates whether horizontal swipe gestures trigger
+  /// backward and forward page navigation.
   Future<void> setAllowsBackForwardNavigationGestures(bool allow) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -282,6 +310,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// The view’s background color.
   Future<void> setBackgroundColor(int? value) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -295,6 +324,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// The custom user agent string.
   Future<void> setCustomUserAgent(String? userAgent) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -307,6 +337,8 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// A Boolean value that indicates whether you can inspect the view with
+  /// Safari Web Inspector.
   Future<void> setInspectable(bool inspectable) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -319,6 +351,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// The object you use to manage navigation behavior for the web view.
   Future<void> setNavigationDelegate(WKNavigationDelegate delegate) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -331,6 +364,7 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// A Boolean value that determines whether the view is opaque.
   Future<void> setOpaque(bool opaque) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
@@ -343,6 +377,8 @@ class PlatformWebView {
     throw UnimplementedError('${webView.runtimeType} is not supported.');
   }
 
+  /// The object you use to integrate custom user interface elements, such as
+  /// contextual menus or panels, into web view interactions.
   Future<void> setUIDelegate(WKUIDelegate delegate) {
     final WKWebView webView = nativeWebView;
     switch (webView) {
