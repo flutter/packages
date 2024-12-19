4c388014ce7bfe45261097d03c0ad1cc526e371a
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2024-12-19 03:01:08 -0500

    user dispatch on main thread again

--- packages/webview_flutter/webview_flutter_wkwebview/darwin/webview_flutter_wkwebview/Sources/webview_flutter_wkwebview/NavigationDelegateProxyAPIDelegate.swift
+++ packages/webview_flutter/webview_flutter_wkwebview/darwin/webview_flutter_wkwebview/Sources/webview_flutter_wkwebview/NavigationDelegateProxyAPIDelegate.swift
@@ -84,11 +84,11 @@ extension NavigationDelegateImpl {
     _ webView: WKWebView, decidePolicyFor navigationAction: WebKit.WKNavigationAction,
     decisionHandler: @escaping @MainActor (WebKit.WKNavigationActionPolicy) -> Void
   ) {
-    registrar.dispatchOnMainThread { onFailure in
+    registrar.dispatchOnMainThread { _ in
       self.api.decidePolicyForNavigationAction(
         pigeonInstance: self, webView: webView, navigationAction: navigationAction
       ) { result in
-        DispatchQueue.main.async {
+        self.registrar.dispatchOnMainThread { onFailure in
           switch result {
           case .success(let policy):
             switch policy {
