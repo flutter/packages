6b01a26ee70c82bc3c2baabe1d23c0b3ef436820
David Iglesias Teixeira <ditman@gmail.com>
2024-12-19 19:48:32 -0800

    Remove unnecessary import.

--- packages/pointer_interceptor/pointer_interceptor_web/example/integration_test/widget_test.dart
+++ packages/pointer_interceptor/pointer_interceptor_web/example/integration_test/widget_test.dart
@@ -5,7 +5,6 @@
 // ignore_for_file: avoid_print
 
 // Imports the Flutter Driver API.
-import 'package:flutter/foundation.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
