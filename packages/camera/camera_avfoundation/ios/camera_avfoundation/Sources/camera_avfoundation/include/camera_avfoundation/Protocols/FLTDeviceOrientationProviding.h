715583871faf22bf1f33d895bc31a68e72987994
Marcin Chudy <marcin.chudy@leancode.pl>
2024-12-30 14:10:51 +0100

    Fix imports

--- packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/include/camera_avfoundation/Protocols/FLTDeviceOrientationProviding.h
+++ packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/include/camera_avfoundation/Protocols/FLTDeviceOrientationProviding.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-@import AVFoundation;
 @import Foundation;
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
