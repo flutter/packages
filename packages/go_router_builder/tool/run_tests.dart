14886ba8bf91e20abe3225459cb484b980d86f91
hangyu <jhy03261997@gmail.com>
2025-01-02 14:55:23 -0800

    Revert logging

--- packages/go_router_builder/tool/run_tests.dart
+++ packages/go_router_builder/tool/run_tests.dart
@@ -28,9 +28,6 @@ Future<void> main() async {
       .where((File f) => f.path.endsWith('.dart'))
       .toList();
   for (final File file in testFiles) {
-    if(!file.path.contains('set.dart')) {
-      continue;
-    }
     final String fileName = file.path.split('/').last;
     final File expectFile = File(p.join('${file.path}.expect'));
     if (!expectFile.existsSync()) {
@@ -62,8 +59,6 @@ Future<void> main() async {
           .format(results.join('\n\n'))
           .trim()
           .replaceAll('\r\n', '\n');
-
-      print(generated);
       expect(generated, equals(expectResult.replaceAll('\r\n', '\n')));
     }, timeout: const Timeout(Duration(seconds: 100)));
   }
