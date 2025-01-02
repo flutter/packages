d4c4f3a299c9b8b67184b76cbd89fc45c6822a39
Marcin Chudy <marcin.chudy@leancode.pl>
2025-01-02 10:04:21 +0100

    Flatten the structure

--- packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/FLTCamMediaSettingsAVWrapper.m
+++ packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/FLTCamMediaSettingsAVWrapper.m
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 #import "./include/camera_avfoundation/FLTCamMediaSettingsAVWrapper.h"
-#import "./include/camera_avfoundation/Protocols/FLTCaptureDeviceControlling.h"
+#import "./include/camera_avfoundation/FLTCaptureDeviceControlling.h"
 
 @implementation FLTCamMediaSettingsAVWrapper
 
--- packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/FLTDeviceOrientationProviding.m
+++ packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/FLTDeviceOrientationProviding.m
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "../include/camera_avfoundation/Protocols/FLTDeviceOrientationProviding.h"
+#import "./include/camera_avfoundation/FLTDeviceOrientationProviding.h"
 
 @implementation FLTDefaultDeviceOrientationProvider
 
