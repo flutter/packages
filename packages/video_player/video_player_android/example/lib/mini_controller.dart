881e80eac587ee6a68405c65c4534fb1ddeda7a9
Nils-Börge Margotti <nilsmargotti@gmail.com>
2025-01-03 15:46:33 +0100

    [video_player_android] add overwrite to fix analysis error

--- packages/video_player/video_player_android/example/lib/mini_controller.dart
+++ packages/video_player/video_player_android/example/lib/mini_controller.dart
@@ -266,6 +266,10 @@ class MiniController extends ValueNotifier<VideoPlayerValue> {
           value = value.copyWith(isBuffering: false);
         case VideoEventType.isPlayingStateUpdate:
           value = value.copyWith(isPlaying: event.isPlaying);
+        case VideoEventType.startedPictureInPicture:
+          throw UnimplementedError();
+        case VideoEventType.stoppedPictureInPicture:
+          throw UnimplementedError();
         case VideoEventType.unknown:
           break;
       }
