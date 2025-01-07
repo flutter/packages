cacfd26b0fa293860ecf9b5c36412704831e6f54
Paweł Jakubowski <pawel.jakubowski@leancode.pl>
2025-01-07 15:19:53 +0100

    Add back license that was removed by mistake

--- packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation_ios/FVPNativeVideoViewFactory.m
+++ packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation_ios/FVPNativeVideoViewFactory.m
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #import "../video_player_avfoundation/include/video_player_avfoundation/FVPNativeVideoViewFactory.h"
 
 #import "../video_player_avfoundation/include/video_player_avfoundation/FVPNativeVideoView.h"
