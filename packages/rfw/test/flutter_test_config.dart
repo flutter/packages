d3934197b654102808de26bd74c907b708f06020
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-01-18 17:07:04 +0800

    Add creation stack trace to debug on ci

--- packages/rfw/test/flutter_test_config.dart
+++ packages/rfw/test/flutter_test_config.dart
@@ -9,5 +9,7 @@ import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   LeakTesting.enable();
   LeakTracking.warnForUnsupportedPlatforms = false;
+  LeakTesting.settings = LeakTesting.settings
+      .withCreationStackTrace(); // To debug failing test on CI.
   await testMain();
 }
