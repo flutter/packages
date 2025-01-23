45e91160dcf8fd9d1a9098be699588b662ea62b9
Joonas Kerttula <joonas.kerttula@codemate.com>
2025-01-23 22:25:51 +0200

    run swift-format

--- packages/google_maps_flutter/google_maps_flutter_ios/ios/google_maps_flutter_ios/Package.swift
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/google_maps_flutter_ios/Package.swift
@@ -8,37 +8,39 @@
 import PackageDescription
 
 let package = Package(
-    name: "google_maps_flutter_ios",
-    platforms: [
-        .iOS(.v15),
-    ],
-    products: [
-        .library(name: "google-maps-flutter-ios", type: .static, targets: ["google_maps_flutter_ios"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/googlemaps/ios-maps-sdk", "9.0.0"..<"10.0.0"),
-        .package(url: "https://github.com/googlemaps/google-maps-ios-utils", "6.1.0"..<"7.0.0"),
-    ],
-    targets: [
-        .target(
-          name: "google_maps_flutter_ios",
-          dependencies: [
-            .product(
-              name: "GoogleMapsUtils",
-              package: "google-maps-ios-utils"
-            ),
-            .product(
-              name: "GoogleMaps",
-              package: "ios-maps-sdk"
-            ),
-          ],
-          exclude: ["include/google_maps_flutter_ios-umbrella.h", "include/google_maps_flutter_ios.modulemap"],
-          resources: [
-            .process("Resources")
-          ],
-          cSettings: [
-            .headerSearchPath("include/google_maps_flutter_ios")
-          ]
-        )
-    ]
+  name: "google_maps_flutter_ios",
+  platforms: [
+    .iOS(.v15)
+  ],
+  products: [
+    .library(name: "google-maps-flutter-ios", type: .static, targets: ["google_maps_flutter_ios"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/googlemaps/ios-maps-sdk", "9.0.0"..<"10.0.0"),
+    .package(url: "https://github.com/googlemaps/google-maps-ios-utils", "6.1.0"..<"7.0.0"),
+  ],
+  targets: [
+    .target(
+      name: "google_maps_flutter_ios",
+      dependencies: [
+        .product(
+          name: "GoogleMapsUtils",
+          package: "google-maps-ios-utils"
+        ),
+        .product(
+          name: "GoogleMaps",
+          package: "ios-maps-sdk"
+        ),
+      ],
+      exclude: [
+        "include/google_maps_flutter_ios-umbrella.h", "include/google_maps_flutter_ios.modulemap",
+      ],
+      resources: [
+        .process("Resources")
+      ],
+      cSettings: [
+        .headerSearchPath("include/google_maps_flutter_ios")
+      ]
+    )
+  ]
 )
