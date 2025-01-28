5632db9c95457773e22e1e67efe8d4a023c45354
Justin McCandless <jmccandless@google.com>
2025-01-27 16:28:01 -0800

    WIP adding examples from framework PR.
    TODO: Make sure they take advantage of being able to launch a URL
    themselves (not needing to use url_launcher in the example). Maybe
    TextLinker or that file need to do so too?

---
+++ packages/linked_text/example/linked_text.0.dart
@@ -0,0 +1,65 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+import 'package:linked_text/linked_text.dart';
+
+// This example demonstrates using LinkedText to make URLs open on tap.
+
+void main() {
+  runApp(const LinkedTextApp());
+}
+
+class LinkedTextApp extends StatelessWidget {
+  const LinkedTextApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Link Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+  static const String _text = 'Check out https://www.flutter.dev, or maybe just flutter.dev or www.flutter.dev.';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Builder(
+          builder: (BuildContext context) {
+            return SelectionArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  LinkedText(
+                    text: _text,
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
