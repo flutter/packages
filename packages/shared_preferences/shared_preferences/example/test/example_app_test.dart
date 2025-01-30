e7267ada8d7900078b01901d14dd09502e79f0d1
Kenzie Davisson <kenzieschmoll@google.com>
2025-01-30 14:26:14 -0800

    add test

---
+++ packages/shared_preferences/shared_preferences/example/test/example_app_test.dart
@@ -0,0 +1,25 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences_example/main.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
+
+import '../..//test/shared_preferences_async_test.dart';
+
+void main() {
+  group('SharedPreferences example app', () {
+    setUp(() {
+      SharedPreferencesAsyncPlatform.instance = FakeSharedPreferencesAsync();
+    });
+
+    tearDown(() {
+      SharedPreferencesAsyncPlatform.instance = null;
+    });
+
+    testWidgets('builds successfully', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+    });
+  });
+}
