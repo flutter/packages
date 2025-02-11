aab1d5ef5d818e7719d85323a567008739b4b667
Paweł Jakubowski <pawel.jakubowski@leancode.pl>
2025-02-11 11:07:19 +0100

    Format file

--- packages/camera/camera_avfoundation/example/ios/RunnerTests/CameraTestUtils.m
+++ packages/camera/camera_avfoundation/example/ios/RunnerTests/CameraTestUtils.m
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "CameraTestUtils.h"	
+#import "CameraTestUtils.h"
 
 #import <OCMock/OCMock.h>
 @import AVFoundation;
