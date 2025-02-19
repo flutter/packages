4f02de01b0d05fe13119c6ba9c876208d57b4944
bixat4software <bixat4software@gmail.com>
2025-02-19 15:16:31 +0000

    removed license comment

--- packages/flutter_markdown/lib/src/builder.dart
+++ packages/flutter_markdown/lib/src/builder.dart
@@ -1,5 +1,3 @@
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:1053232416.
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:410293059.
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
