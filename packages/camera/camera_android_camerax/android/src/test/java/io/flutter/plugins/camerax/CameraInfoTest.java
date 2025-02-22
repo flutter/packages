703f17672ed235db8ad107ab46623b6e822fdfac
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2025-02-22 15:47:58 -0500

    range tests

--- packages/camera/camera_android_camerax/android/src/test/java/io/flutter/plugins/camerax/CameraInfoTest.java
+++ packages/camera/camera_android_camerax/android/src/test/java/io/flutter/plugins/camerax/CameraInfoTest.java
@@ -1,136 +1,69 @@
-//// Copyright 2013 The Flutter Authors. All rights reserved.
-//// Use of this source code is governed by a BSD-style license that can be
-//// found in the LICENSE file.
-//
-//package io.flutter.plugins.camerax;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.ArgumentMatchers.eq;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.spy;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//import androidx.camera.core.CameraInfo;
-//import androidx.camera.core.CameraState;
-//import androidx.camera.core.ExposureState;
-//import androidx.camera.core.ZoomState;
-//import androidx.lifecycle.LiveData;
-//import io.flutter.plugin.common.BinaryMessenger;
-//import io.flutter.plugins.camerax.GeneratedCameraXLibrary.LiveDataSupportedType;
-//import java.util.Objects;
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnit;
-//import org.mockito.junit.MockitoRule;
-//
-//public class CameraInfoTest {
-//  @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
-//
-//  @Mock public CameraInfo mockCameraInfo;
-//  @Mock public BinaryMessenger mockBinaryMessenger;
-//
-//  InstanceManager testInstanceManager;
-//
-//  @Before
-//  public void setUp() {
-//    testInstanceManager = InstanceManager.create(identifier -> {});
-//  }
-//
-//  @After
-//  public void tearDown() {
-//    testInstanceManager.stopFinalizationListener();
-//  }
-//
-//  @Test
-//  public void getSensorRotationDegrees_makesCallToRetrieveSensorRotationDegrees() {
-//    final CameraInfoHostApiImpl cameraInfoHostApi =
-//        new CameraInfoHostApiImpl(mockBinaryMessenger, testInstanceManager);
-//
-//    testInstanceManager.addDartCreatedInstance(mockCameraInfo, 1);
-//
-//    when(mockCameraInfo.getSensorRotationDegrees()).thenReturn(90);
-//
-//    assertEquals((long) cameraInfoHostApi.getSensorRotationDegrees(1L), 90L);
-//    verify(mockCameraInfo).getSensorRotationDegrees();
-//  }
-//
-//  @Test
-//  public void getCameraState_makesCallToRetrieveLiveCameraState() {
-//    final CameraInfoHostApiImpl cameraInfoHostApiImpl =
-//        new CameraInfoHostApiImpl(mockBinaryMessenger, testInstanceManager);
-//    final LiveDataFlutterApiWrapper mockLiveDataFlutterApiWrapper =
-//        mock(LiveDataFlutterApiWrapper.class);
-//    final Long mockCameraInfoIdentifier = 27L;
-//    @SuppressWarnings("unchecked")
-//    final LiveData<CameraState> mockLiveCameraState = (LiveData<CameraState>) mock(LiveData.class);
-//
-//    testInstanceManager.addDartCreatedInstance(mockCameraInfo, mockCameraInfoIdentifier);
-//    cameraInfoHostApiImpl.liveDataFlutterApiWrapper = mockLiveDataFlutterApiWrapper;
-//    when(mockCameraInfo.getCameraState()).thenReturn(mockLiveCameraState);
-//
-//    final Long liveCameraStateIdentifier =
-//        cameraInfoHostApiImpl.getCameraState(mockCameraInfoIdentifier);
-//
-//    verify(mockLiveDataFlutterApiWrapper)
-//        .create(eq(mockLiveCameraState), eq(LiveDataSupportedType.CAMERA_STATE), any());
-//    assertEquals(
-//        liveCameraStateIdentifier,
-//        testInstanceManager.getIdentifierForStrongReference(mockLiveCameraState));
-//  }
-//
-//  @Test
-//  public void getExposureState_retrievesExpectedExposureState() {
-//    final CameraInfoHostApiImpl cameraInfoHostApiImpl =
-//        new CameraInfoHostApiImpl(mockBinaryMessenger, testInstanceManager);
-//    final ExposureState mockExposureState = mock(ExposureState.class);
-//    final Long mockCameraInfoIdentifier = 27L;
-//    final Long mockExposureStateIdentifier = 47L;
-//
-//    testInstanceManager.addDartCreatedInstance(mockCameraInfo, mockCameraInfoIdentifier);
-//    testInstanceManager.addDartCreatedInstance(mockExposureState, mockExposureStateIdentifier);
-//
-//    when(mockCameraInfo.getExposureState()).thenReturn(mockExposureState);
-//
-//    assertEquals(
-//        cameraInfoHostApiImpl.getExposureState(mockCameraInfoIdentifier),
-//        mockExposureStateIdentifier);
-//    verify(mockCameraInfo).getExposureState();
-//  }
-//
-//  @Test
-//  @SuppressWarnings("unchecked")
-//  public void getZoomState_retrievesExpectedZoomState() {
-//    final CameraInfoHostApiImpl cameraInfoHostApiImpl =
-//        new CameraInfoHostApiImpl(mockBinaryMessenger, testInstanceManager);
-//    final LiveData<ZoomState> mockLiveZoomState = (LiveData<ZoomState>) mock(LiveData.class);
-//    final ZoomState mockZoomState = mock(ZoomState.class);
-//    final Long mockCameraInfoIdentifier = 20L;
-//    final Long mockLiveZoomStateIdentifier = 74L;
-//
-//    testInstanceManager.addDartCreatedInstance(mockCameraInfo, mockCameraInfoIdentifier);
-//    testInstanceManager.addDartCreatedInstance(mockLiveZoomState, mockLiveZoomStateIdentifier);
-//
-//    when(mockCameraInfo.getZoomState()).thenReturn(mockLiveZoomState);
-//
-//    assertEquals(
-//        cameraInfoHostApiImpl.getZoomState(mockCameraInfoIdentifier), mockLiveZoomStateIdentifier);
-//    verify(mockCameraInfo).getZoomState();
-//  }
-//
-//  @Test
-//  public void flutterApiCreate_makesCallToCreateInstanceOnDartSide() {
-//    final CameraInfoFlutterApiImpl spyFlutterApi =
-//        spy(new CameraInfoFlutterApiImpl(mockBinaryMessenger, testInstanceManager));
-//
-//    spyFlutterApi.create(mockCameraInfo, reply -> {});
-//
-//    final long identifier =
-//        Objects.requireNonNull(testInstanceManager.getIdentifierForStrongReference(mockCameraInfo));
-//    verify(spyFlutterApi).create(eq(identifier), any());
-//  }
-//}
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.camerax;
+
+import androidx.camera.core.CameraInfo;
+import androidx.camera.core.CameraState;
+import androidx.camera.core.ExposureState;
+import androidx.camera.core.ZoomState;
+import androidx.lifecycle.LiveData;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class CameraInfoTest {
+  @Test
+  public void getSensorRotationDegrees_makesCallToRetrieveSensorRotationDegrees() {
+    final PigeonApiCameraInfo api = new TestProxyApiRegistrar().getPigeonApiCameraInfo();
+
+    final CameraInfo instance = mock(CameraInfo.class);
+    final long value = 0;
+    when(instance.getSensorRotationDegrees()).thenReturn((int) value);
+
+    assertEquals(value, api.sensorRotationDegrees(instance));
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void getCameraState_makesCallToRetrieveLiveCameraState() {
+    final PigeonApiCameraInfo api = new TestProxyApiRegistrar().getPigeonApiCameraInfo();
+
+    final CameraInfo instance = mock(CameraInfo.class);
+    final LiveData<CameraState> value = mock(LiveData.class);
+    when(instance.getCameraState()).thenReturn(value);
+
+    assertEquals(value, api.getCameraState(instance).getLiveData());
+  }
+
+  @Test
+  public void getExposureState_retrievesExpectedExposureState() {
+    final PigeonApiCameraInfo api = new TestProxyApiRegistrar().getPigeonApiCameraInfo();
+
+    final CameraInfo instance = mock(CameraInfo.class);
+    final androidx.camera.core.ExposureState value = mock(ExposureState.class);
+    when(instance.getExposureState()).thenReturn(value);
+
+    assertEquals(value, api.exposureState(instance));
+  }
+
+    @SuppressWarnings("unchecked")
+  @Test
+  public void getZoomState_retrievesExpectedZoomState() {
+    final PigeonApiCameraInfo api = new TestProxyApiRegistrar().getPigeonApiCameraInfo();
+
+    final CameraInfo instance = mock(CameraInfo.class);
+      final LiveData<ZoomState> value = mock(LiveData.class);
+    when(instance.getZoomState()).thenReturn(value);
+
+    assertEquals(value, api.getZoomState(instance ).getLiveData());
+  }
+
+}
--- packages/camera/camera_android_camerax/android/src/test/java/io/flutter/plugins/camerax/CameraIntegerRangeProxyApiTest.java
+++ packages/camera/camera_android_camerax/android/src/test/java/io/flutter/plugins/camerax/CameraIntegerRangeProxyApiTest.java
@@ -1,49 +1,52 @@
-//// Copyright 2013 The Flutter Authors. All rights reserved.
-//// Use of this source code is governed by a BSD-style license that can be
-//// found in the LICENSE file.
-//
-//package io.flutter.plugins.camerax
-//
-//import android.util.Range<*>
-//import org.junit.Test;
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertTrue;
-//import org.mockito.Mockito;
-//import org.mockito.Mockito.any;
-//import java.util.HashMap;
-//import static org.mockito.Mockito.eq;
-//import static org.mockito.Mockito.mock;
-//import org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//public class CameraIntegerRangeProxyApiTest {
-//  @Test
-//  public void pigeon_defaultConstructor() {
-//    final PigeonApiCameraIntegerRange api = new TestProxyApiRegistrar().getPigeonApiCameraIntegerRange();
-//
-//    assertTrue(api.pigeon_defaultConstructor() instanceof CameraIntegerRangeProxyApi.CameraIntegerRange);
-//  }
-//
-//  @Test
-//  public void lower() {
-//    final PigeonApiCameraIntegerRange api = new TestProxyApiRegistrar().getPigeonApiCameraIntegerRange();
-//
-//    final CameraIntegerRange instance = mock(CameraIntegerRange.class);
-//    final Long value = 0;
-//    when(instance.getLower()).thenReturn(value);
-//
-//    assertEquals(value, api.lower(instance));
-//  }
-//
-//  @Test
-//  public void upper() {
-//    final PigeonApiCameraIntegerRange api = new TestProxyApiRegistrar().getPigeonApiCameraIntegerRange();
-//
-//    final CameraIntegerRange instance = mock(CameraIntegerRange.class);
-//    final Long value = 0;
-//    when(instance.getUpper()).thenReturn(value);
-//
-//    assertEquals(value, api.upper(instance));
-//  }
-//
-//}
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.camerax;
+
+import android.util.Range;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.any;
+import java.util.HashMap;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class CameraIntegerRangeProxyApiTest {
+  @SuppressWarnings("unchecked")
+  @Test
+  public void pigeon_defaultConstructor() {
+    final PigeonApiCameraIntegerRange api = new TestProxyApiRegistrar().getPigeonApiCameraIntegerRange();
+
+    final long lower = 2;
+    final long upper = 1;
+
+    final Range<Integer> instance = (Range<Integer>) api.pigeon_defaultConstructor(lower, upper);
+    assertEquals((int) instance.getLower(), (int) lower);
+    assertEquals((int) instance.getUpper(), (int) upper);
+  }
+
+  @Test
+  public void lower() {
+    final PigeonApiCameraIntegerRange api = new TestProxyApiRegistrar().getPigeonApiCameraIntegerRange();
+
+    final int lower = 3;
+    final Range<Integer> instance = new Range<>(lower, 4);
+
+    assertEquals(lower, api.lower(instance));
+  }
+
+  @Test
+  public void upper() {
+    final PigeonApiCameraIntegerRange api = new TestProxyApiRegistrar().getPigeonApiCameraIntegerRange();
+
+    final int upper = 4;
+    final Range<Integer> instance = new Range<>(0, upper);
+
+    assertEquals(upper, api.upper(instance));
+  }
+}
