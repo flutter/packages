542cd06a336a94399698a27f0e7f7da318fcc954
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2025-02-23 14:45:13 -0500

    live data

--- packages/camera/camera_android_camerax/android/src/test/java/io/flutter/plugins/camerax/LiveDataProxyApiTest.java
+++ packages/camera/camera_android_camerax/android/src/test/java/io/flutter/plugins/camerax/LiveDataProxyApiTest.java
@@ -1,64 +1,86 @@
-//// Copyright 2013 The Flutter Authors. All rights reserved.
-//// Use of this source code is governed by a BSD-style license that can be
-//// found in the LICENSE file.
-//
-//package io.flutter.plugins.camerax
-//
-//import io.flutter.plugins.camerax.LiveDataProxyApi.LiveDataWrapper
-//import androidx.lifecycle.Observer<*>
-//import org.junit.Test;
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertTrue;
-//import org.mockito.Mockito;
-//import org.mockito.Mockito.any;
-//import java.util.HashMap;
-//import static org.mockito.Mockito.eq;
-//import static org.mockito.Mockito.mock;
-//import org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//public class LiveDataProxyApiTest {
-//  @Test
-//  public void type() {
-//    final PigeonApiLiveData api = new TestProxyApiRegistrar().getPigeonApiLiveData();
-//
-//    final LiveData instance = mock(LiveData.class);
-//    final LiveDataSupportedType value = io.flutter.plugins.camerax.LiveDataSupportedType.CAMERA_STATE;
-//    when(instance.getType()).thenReturn(value);
-//
-//    assertEquals(value, api.type(instance));
-//  }
-//
-//  @Test
-//  public void observe() {
-//    final PigeonApiLiveData api = new TestProxyApiRegistrar().getPigeonApiLiveData();
-//
-//    final LiveData instance = mock(LiveData.class);
-//    final androidx.lifecycle.Observer<*> observer = mock(Observer.class);
-//    api.observe(instance, observer);
-//
-//    verify(instance).observe(observer);
-//  }
-//
-//  @Test
-//  public void removeObservers() {
-//    final PigeonApiLiveData api = new TestProxyApiRegistrar().getPigeonApiLiveData();
-//
-//    final LiveData instance = mock(LiveData.class);
-//    api.removeObservers(instance );
-//
-//    verify(instance).removeObservers();
-//  }
-//
-//  @Test
-//  public void getValue() {
-//    final PigeonApiLiveData api = new TestProxyApiRegistrar().getPigeonApiLiveData();
-//
-//    final LiveData instance = mock(LiveData.class);
-//    final Any value = -1;
-//    when(instance.getValue()).thenReturn(value);
-//
-//    assertEquals(value, api.getValue(instance ));
-//  }
-//
-//}
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.camerax;
+
+import io.flutter.plugins.camerax.LiveDataProxyApi.LiveDataWrapper;
+
+import androidx.annotation.Nullable;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.any;
+import java.util.HashMap;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class LiveDataProxyApiTest {
+  @Test
+  public void type() {
+    final PigeonApiLiveData api = new TestProxyApiRegistrar().getPigeonApiLiveData();
+
+      final LiveDataSupportedType value = io.flutter.plugins.camerax.LiveDataSupportedType.CAMERA_STATE;
+    final LiveDataWrapper instance = new LiveDataWrapper(mock(LiveData.class), value);
+
+    assertEquals(value, api.type(instance));
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void observe() {
+    final PigeonApiLiveData api = new TestProxyApiRegistrar() {
+      @Nullable
+      @Override
+      public LifecycleOwner getLifecycleOwner() {
+        return mock(LifecycleOwner.class);
+      }
+    }.getPigeonApiLiveData();
+
+    final LiveData<String> liveData = mock(LiveData.class);
+    final LiveDataWrapper instance = new LiveDataWrapper(liveData, LiveDataSupportedType.CAMERA_STATE);
+
+    final Observer<String> observer = mock(Observer.class);
+    api.observe(instance, observer);
+
+    verify(liveData).observe(any(LifecycleOwner.class), eq(observer));
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void removeObservers() {
+    final PigeonApiLiveData api = new TestProxyApiRegistrar() {
+      @Nullable
+      @Override
+      public LifecycleOwner getLifecycleOwner() {
+        return mock(LifecycleOwner.class);
+      }
+    }.getPigeonApiLiveData();
+
+    final LiveData<String> liveData = mock(LiveData.class);
+    final LiveDataWrapper instance = new LiveDataWrapper(liveData, LiveDataSupportedType.CAMERA_STATE);
+
+    api.removeObservers(instance);
+
+    verify(liveData).removeObservers(any(LifecycleOwner.class));
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void getValue() {
+    final PigeonApiLiveData api = new TestProxyApiRegistrar().getPigeonApiLiveData();
+
+    final LiveData<String> liveData = mock(LiveData.class);
+    final String result = "result";
+    when(liveData.getValue()).thenReturn(result);
+    final LiveDataWrapper instance = new LiveDataWrapper(liveData, LiveDataSupportedType.CAMERA_STATE);
+
+    assertEquals(result, api.getValue(instance));
+  }
+}
