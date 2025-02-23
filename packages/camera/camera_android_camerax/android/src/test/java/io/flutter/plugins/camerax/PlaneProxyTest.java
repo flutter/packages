6e21dbafeb7fcc25982b4897e389bc9018382cd5
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2025-02-23 17:10:23 -0500

    start of preview tests

--- packages/camera/camera_android_camerax/android/src/test/java/io/flutter/plugins/camerax/PlaneProxyTest.java
+++ packages/camera/camera_android_camerax/android/src/test/java/io/flutter/plugins/camerax/PlaneProxyTest.java
@@ -1,58 +1,41 @@
-//// Copyright 2013 The Flutter Authors. All rights reserved.
-//// Use of this source code is governed by a BSD-style license that can be
-//// found in the LICENSE file.
-//
-//package io.flutter.plugins.camerax;
-//
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.ArgumentMatchers.eq;
-//import static org.mockito.Mockito.verify;
-//
-//import androidx.camera.core.ImageProxy;
-//import io.flutter.plugin.common.BinaryMessenger;
-//import io.flutter.plugins.camerax.GeneratedCameraXLibrary.PlaneProxyFlutterApi;
-//import java.util.Objects;
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnit;
-//import org.mockito.junit.MockitoRule;
-//
-//public class PlaneProxyTest {
-//  @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
-//  @Mock public ImageProxy.PlaneProxy mockPlaneProxy;
-//  @Mock public BinaryMessenger mockBinaryMessenger;
-//  @Mock public PlaneProxyFlutterApi mockFlutterApi;
-//
-//  InstanceManager instanceManager;
-//
-//  @Before
-//  public void setUp() {
-//    instanceManager = InstanceManager.create(identifier -> {});
-//  }
-//
-//  @After
-//  public void tearDown() {
-//    instanceManager.stopFinalizationListener();
-//  }
-//
-//  @Test
-//  public void flutterApiCreate_makesCallToCreateInstanceWithExpectedIdentifier() {
-//    final PlaneProxyFlutterApiImpl flutterApi =
-//        new PlaneProxyFlutterApiImpl(mockBinaryMessenger, instanceManager);
-//    final byte[] buffer = new byte[23];
-//    final long pixelStride = 20;
-//    final long rowStride = 2;
-//
-//    flutterApi.setApi(mockFlutterApi);
-//
-//    flutterApi.create(mockPlaneProxy, buffer, pixelStride, rowStride, reply -> {});
-//    final long instanceIdentifier =
-//        Objects.requireNonNull(instanceManager.getIdentifierForStrongReference(mockPlaneProxy));
-//
-//    verify(mockFlutterApi)
-//        .create(eq(instanceIdentifier), eq(buffer), eq(pixelStride), eq(rowStride), any());
-//  }
-//}
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.camerax;
+
+import androidx.camera.core.ImageProxy.PlaneProxy;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import static org.mockito.Mockito.any;
+
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class PlaneProxyTest {
+  @Test
+  public void pixelStride() {
+    final PigeonApiPlaneProxy api = new TestProxyApiRegistrar().getPigeonApiPlaneProxy();
+
+    final PlaneProxy instance = mock(PlaneProxy.class);
+    final long value = 0;
+    when(instance.getPixelStride()).thenReturn((int) value);
+
+    assertEquals(value, api.pixelStride(instance));
+  }
+
+  @Test
+  public void rowStride() {
+    final PigeonApiPlaneProxy api = new TestProxyApiRegistrar().getPigeonApiPlaneProxy();
+
+    final PlaneProxy instance = mock(PlaneProxy.class);
+    final long value = 0;
+    when(instance.getRowStride()).thenReturn((int) value);
+
+    assertEquals(value, api.rowStride(instance));
+  }
+}
--- packages/camera/camera_android_camerax/android/src/test/java/io/flutter/plugins/camerax/PreviewProxyApiTest.java
+++ packages/camera/camera_android_camerax/android/src/test/java/io/flutter/plugins/camerax/PreviewProxyApiTest.java
@@ -1,84 +1,122 @@
-//// Copyright 2013 The Flutter Authors. All rights reserved.
-//// Use of this source code is governed by a BSD-style license that can be
-//// found in the LICENSE file.
-//
-//package io.flutter.plugins.camerax
-//
-//import androidx.camera.core.Preview
-//import androidx.camera.core.resolutionselector.ResolutionSelector
-//import androidx.camera.core.ResolutionInfo
-//import org.junit.Test;
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertTrue;
-//import org.mockito.Mockito;
-//import org.mockito.Mockito.any;
-//import java.util.HashMap;
-//import static org.mockito.Mockito.eq;
-//import static org.mockito.Mockito.mock;
-//import org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//public class PreviewProxyApiTest {
-//  @Test
-//  public void pigeon_defaultConstructor() {
-//    final PigeonApiPreview api = new TestProxyApiRegistrar().getPigeonApiPreview();
-//
-//    assertTrue(api.pigeon_defaultConstructor(0) instanceof PreviewProxyApi.Preview);
-//  }
-//
-//  @Test
-//  public void resolutionSelector() {
-//    final PigeonApiPreview api = new TestProxyApiRegistrar().getPigeonApiPreview();
-//
-//    final Preview instance = mock(Preview.class);
-//    final androidx.camera.core.resolutionselector.ResolutionSelector value = mock(ResolutionSelector.class);
-//    when(instance.getResolutionSelector()).thenReturn(value);
-//
-//    assertEquals(value, api.resolutionSelector(instance));
-//  }
-//
-//  @Test
-//  public void setSurfaceProvider() {
-//    final PigeonApiPreview api = new TestProxyApiRegistrar().getPigeonApiPreview();
-//
-//    final Preview instance = mock(Preview.class);
-//    final SystemServicesManager systemServicesManager = mock(SystemServicesManager.class);
-//    final Long value = 0;
-//    when(instance.setSurfaceProvider(systemServicesManager)).thenReturn(value);
-//
-//    assertEquals(value, api.setSurfaceProvider(instance, systemServicesManager));
-//  }
-//
-//  @Test
-//  public void releaseSurfaceProvider() {
-//    final PigeonApiPreview api = new TestProxyApiRegistrar().getPigeonApiPreview();
-//
-//    final Preview instance = mock(Preview.class);
-//    api.releaseSurfaceProvider(instance );
-//
-//    verify(instance).releaseSurfaceProvider();
-//  }
-//
-//  @Test
-//  public void getResolutionInfo() {
-//    final PigeonApiPreview api = new TestProxyApiRegistrar().getPigeonApiPreview();
-//
-//    final Preview instance = mock(Preview.class);
-//    final androidx.camera.core.ResolutionInfo value = mock(ResolutionInfo.class);
-//    when(instance.getResolutionInfo()).thenReturn(value);
-//
-//    assertEquals(value, api.getResolutionInfo(instance ));
-//  }
-//
-//  @Test
-//  public void setTargetRotation() {
-//    final PigeonApiPreview api = new TestProxyApiRegistrar().getPigeonApiPreview();
-//
-//    final Preview instance = mock(Preview.class);
-//    final Long rotation = 0;
-//    api.setTargetRotation(instance, rotation);
-//
-//    verify(instance).setTargetRotation(rotation);
-//  }
-//
-//}
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.plugins.camerax;
+
+import androidx.annotation.NonNull;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.Preview;
+import androidx.camera.core.resolutionselector.ResolutionSelector;
+import androidx.camera.core.ResolutionInfo;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.mockito.Mockito.any;
+import java.util.HashMap;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.view.Surface;
+
+import io.flutter.view.TextureRegistry;
+
+@RunWith(RobolectricTestRunner.class)
+public class PreviewProxyApiTest {
+  @Test
+  public void pigeon_defaultConstructor() {
+      final PigeonApiPreview api = new TestProxyApiRegistrar().getPigeonApiPreview();
+
+      final ResolutionSelector mockResolutionSelector = new ResolutionSelector.Builder().build();
+      final long targetResolution = Surface.ROTATION_0;
+      final Preview instance =
+              api.pigeon_defaultConstructor(mockResolutionSelector, targetResolution);
+
+      assertEquals(instance.getResolutionSelector(), mockResolutionSelector);
+      assertEquals(instance.getTargetRotation(), Surface.ROTATION_0);
+  }
+
+  @Test
+  public void resolutionSelector() {
+    final PigeonApiPreview api = new TestProxyApiRegistrar().getPigeonApiPreview();
+
+    final Preview instance = mock(Preview.class);
+    final androidx.camera.core.resolutionselector.ResolutionSelector value = mock(ResolutionSelector.class);
+    when(instance.getResolutionSelector()).thenReturn(value);
+
+    assertEquals(value, api.resolutionSelector(instance));
+  }
+
+  @Test
+  public void setSurfaceProvider() {
+    final TextureRegistry mockTextureRegistry = mock(TextureRegistry.class);
+    final TextureRegistry.SurfaceProducer mockSurfaceProducer = mock(TextureRegistry.SurfaceProducer.class);
+    final long textureId = 0;
+    when(mockSurfaceProducer.id()).thenReturn(textureId);
+    when(mockTextureRegistry.createSurfaceProducer()).thenReturn(mockSurfaceProducer);
+    final PigeonApiPreview api = new TestProxyApiRegistrar() {
+      @NonNull
+      @Override
+      TextureRegistry getTextureRegistry() {
+        return mockTextureRegistry;
+      }
+    }.getPigeonApiPreview();
+
+    final Preview instance = mock(Preview.class);
+    final SystemServicesManager systemServicesManager = mock(SystemServicesManager.class);
+
+    assertEquals(textureId, api.setSurfaceProvider(instance, systemServicesManager));
+    verify(instance).setSurfaceProvider(any(Preview.SurfaceProvider.class));
+  }
+
+  @Test
+  public void releaseSurfaceProvider_makesCallToReleaseFlutterSurfaceTexture() {
+    final TextureRegistry mockTextureRegistry = mock(TextureRegistry.class);
+    final TextureRegistry.SurfaceProducer mockSurfaceProducer = mock(TextureRegistry.SurfaceProducer.class);
+    when(mockSurfaceProducer.id()).thenReturn(0L);
+    when(mockTextureRegistry.createSurfaceProducer()).thenReturn(mockSurfaceProducer);
+    final PigeonApiPreview api = new TestProxyApiRegistrar() {
+      @NonNull
+      @Override
+      TextureRegistry getTextureRegistry() {
+        return mockTextureRegistry;
+      }
+    }.getPigeonApiPreview();
+
+    final Preview instance = mock(Preview.class);
+    final SystemServicesManager systemServicesManager = mock(SystemServicesManager.class);
+    api.setSurfaceProvider(instance, systemServicesManager);
+    api.releaseSurfaceProvider(instance);
+
+    verify(mockSurfaceProducer).release();
+  }
+
+  @Test
+  public void getResolutionInfo() {
+    final PigeonApiPreview api = new TestProxyApiRegistrar().getPigeonApiPreview();
+
+    final Preview instance = mock(Preview.class);
+    final androidx.camera.core.ResolutionInfo value = mock(ResolutionInfo.class);
+    when(instance.getResolutionInfo()).thenReturn(value);
+
+    assertEquals(value, api.getResolutionInfo(instance ));
+  }
+
+  @Test
+  public void setTargetRotation() {
+    final PigeonApiPreview api = new TestProxyApiRegistrar().getPigeonApiPreview();
+
+    final Preview instance = mock(Preview.class);
+    final long rotation = 0;
+    api.setTargetRotation(instance, rotation);
+
+    verify(instance).setTargetRotation((int) rotation);
+  }
+}
