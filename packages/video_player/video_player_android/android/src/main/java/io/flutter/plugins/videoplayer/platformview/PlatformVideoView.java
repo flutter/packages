ce63f148b973e4fa3eb6bcab503ea7ec5baef440
Paweł Jakubowski <pawel.jakubowski@leancode.pl>
2025-02-25 21:46:33 +0100

    Handle SurfaceView on Android 9 correctly, document workarounds

--- packages/video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/platformview/PlatformVideoView.java
+++ packages/video_player/video_player_android/android/src/main/java/io/flutter/plugins/videoplayer/platformview/PlatformVideoView.java
@@ -5,9 +5,13 @@
 package io.flutter.plugins.videoplayer.platformview;
 
 import android.content.Context;
+import android.os.Build;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import androidx.annotation.NonNull;
+import androidx.annotation.OptIn;
+import androidx.media3.common.util.UnstableApi;
 import androidx.media3.exoplayer.ExoPlayer;
 import io.flutter.plugin.platform.PlatformView;
 
@@ -24,12 +28,51 @@ public final class PlatformVideoView implements PlatformView {
    * @param context The context in which the view is running.
    * @param exoPlayer The ExoPlayer instance used to play the video.
    */
+  @OptIn(markerClass = UnstableApi.class)
   public PlatformVideoView(@NonNull Context context, @NonNull ExoPlayer exoPlayer) {
     surfaceView = new SurfaceView(context);
-    // The line below is needed to display the video correctly on older Android versions (blank
-    // space instead of a video).
-    surfaceView.setZOrderMediaOverlay(true);
-    exoPlayer.setVideoSurfaceView(surfaceView);
+
+    if (android.os.Build.VERSION.SDK_INT == Build.VERSION_CODES.P) {
+      // Workaround for rendering issues on Android 9 (API 28).
+      // On Android 9, using setVideoSurfaceView seems to lead to issues where the first frame is
+      // not displayed if the video is paused initially.
+      // To ensure the first frame is visible, the surface is directly set using holder.getSurface()
+      // when the surface is created, and ExoPlayer seeks to a position to force rendering of the
+      // first frame.
+      setupSurfaceWithCallback(exoPlayer);
+    } else {
+      if (android.os.Build.VERSION.SDK_INT <= Build.VERSION_CODES.N_MR1) {
+        // Avoid blank space instead of a video on Android versions below 8 by adjusting video's
+        // z-layer within the Android view hierarchy:
+        surfaceView.setZOrderMediaOverlay(true);
+      }
+      exoPlayer.setVideoSurfaceView(surfaceView);
+    }
+  }
+
+  private void setupSurfaceWithCallback(@NonNull ExoPlayer exoPlayer) {
+    surfaceView
+        .getHolder()
+        .addCallback(
+            new SurfaceHolder.Callback() {
+              @Override
+              public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                exoPlayer.setVideoSurface(holder.getSurface());
+                // Force first frame rendering:
+                exoPlayer.seekTo(1);
+              }
+
+              @Override
+              public void surfaceChanged(
+                  @NonNull SurfaceHolder holder, int format, int width, int height) {
+                // No implementation needed.
+              }
+
+              @Override
+              public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+                exoPlayer.setVideoSurface(null);
+              }
+            });
   }
 
   /**
