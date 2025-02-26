e9586099e3693fa3e2afa0169c6225050c61231b
Ernesto Ramirez <neartox@outlook.com>
2025-02-25 19:03:43 -0600

    [go_router_builder] add example

--- packages/go_router_builder/example/lib/shared/data.dart
+++ packages/go_router_builder/example/lib/shared/data.dart
@@ -191,3 +191,46 @@ class Repository {
     return FamilyPerson(family: family, person: family.person(pid));
   }
 }
+
+class JsonExample {
+  const JsonExample({required this.id, required this.name});
+
+  factory JsonExample.fromJson(Map<String, dynamic> json) {
+    return JsonExample(
+      id: json['id'] as String,
+      name: json['name'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+    };
+  }
+
+  final String id;
+  final String name;
+}
+
+const List<JsonExample> jsonData = <JsonExample>[
+  JsonExample(id: '1', name: 'people'),
+  JsonExample(id: '2', name: 'animals'),
+];
+
+class JsonExampleNested<T> {
+  const JsonExampleNested({required this.child});
+
+  factory JsonExampleNested.fromJson(
+    Map<String, dynamic> json,
+    T Function(Object? json) fromJsonT,
+  ) {
+    return JsonExampleNested<T>(child: fromJsonT(json['child']));
+  }
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{'child': child};
+  }
+
+  final T child;
+}
