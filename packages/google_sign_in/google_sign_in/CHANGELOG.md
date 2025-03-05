6a8f83d7978b2fd6e543ea0f77241158ce96be50
Neil Self <neilself@gmail.com>
2025-03-05 13:56:27 -0800

    Revert changes to app-facing package for this branch.

--- packages/google_sign_in/google_sign_in/CHANGELOG.md
+++ packages/google_sign_in/google_sign_in/CHANGELOG.md
@@ -1,7 +1,5 @@
-## 6.3.0
+## NEXT
 
-* Adds a sign-in field to allow Android clients to explicitly specify an account name. This
-  capability is only available within Android for the underlying libraries.
 * Updates minimum supported SDK version to Flutter 3.22/Dart 3.4.
 
 ## 6.2.2
--- packages/google_sign_in/google_sign_in/lib/google_sign_in.dart
+++ packages/google_sign_in/google_sign_in/lib/google_sign_in.dart
@@ -187,7 +187,6 @@ class GoogleSignIn {
     this.clientId,
     this.serverClientId,
     this.forceCodeForRefreshToken = false,
-    this.forceAccountName,
   }) {
     // Start initializing.
     if (kIsWeb) {
@@ -264,9 +263,6 @@ class GoogleSignIn {
   /// Force the authorization code to be valid for a refresh token every time. Only needed on Android.
   final bool forceCodeForRefreshToken;
 
-  /// Explicitly specifies the account name to be used in sign-in. Must only be set on Android.
-  final String? forceAccountName;
-
   final StreamController<GoogleSignInAccount?> _currentUserController =
       StreamController<GoogleSignInAccount?>.broadcast();
 
@@ -321,7 +317,6 @@ class GoogleSignIn {
       clientId: clientId,
       serverClientId: serverClientId,
       forceCodeForRefreshToken: forceCodeForRefreshToken,
-      forceAccountName: forceAccountName,
     ));
 
     unawaited(GoogleSignInPlatform.instance.userDataEvents
