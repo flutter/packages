0c85de34d1ed084f8bbf1b720a07e54d9fb341e2
David Chopin <david.chopin@wwt.com>
2025-03-28 20:02:43 -0500

    Added google_maps_flutter_ios test - DC

--- packages/google_maps_flutter/google_maps_flutter_ios/test/google_maps_flutter_ios_test.dart
+++ packages/google_maps_flutter/google_maps_flutter_ios/test/google_maps_flutter_ios_test.dart
@@ -940,6 +940,33 @@ void main() {
     expect((await stream.next).value.value, equals(objectId));
   });
 
+  test('active level change events to correct stream', () async {
+    const int mapId = 1;
+    const String expectedName = 'some-name';
+    const String expectedShortName = 'some-short-name';
+    final PlatformIndoorLevel platformIndoorLevel = PlatformIndoorLevel(
+      name: expectedName,
+      shortName: expectedShortName,
+    );
+    const IndoorLevel indoorLevel =
+        IndoorLevel(name: expectedName, shortName: expectedShortName);
+
+    final GoogleMapsFlutterIOS maps = GoogleMapsFlutterIOS();
+    final HostMapMessageHandler callbackHandler =
+        maps.ensureHandlerInitialized(mapId);
+
+    final StreamQueue<MapActiveLevelChangedEvent> stream =
+        StreamQueue<MapActiveLevelChangedEvent>(
+            maps.onActiveLevelChanged(mapId: mapId));
+
+    // Simulate message from the native side.
+    callbackHandler.onActiveLevelChanged(platformIndoorLevel);
+
+    final IndoorLevel? value = (await stream.next).value;
+    expect(value?.name, equals(indoorLevel.name));
+    expect(value?.shortName, equals(indoorLevel.shortName));
+  });
+
   test('moveCamera calls through with expected newCameraPosition', () async {
     const int mapId = 1;
     final (GoogleMapsFlutterIOS maps, MockMapsApi api) =
