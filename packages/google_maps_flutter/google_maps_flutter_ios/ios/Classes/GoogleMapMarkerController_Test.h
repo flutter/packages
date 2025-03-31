e336c0c095bbf5877898006f0ad0b59f65811ff4
Stuart Morgan <stuartmorgan@google.com>
2025-03-30 20:36:11 -0400

    [google_maps_flutter] Fix iOS info window regression
    Fixes a regression introduced during the migration to structured Pigeon
    data, where the info window data was not passed through to the
    underlying SDK marker object. This wasn't caught because there was
    apparently no native unit testing of marker properties, and this level
    of info window detail isn't inspected in integration tests.
    
    Adds missing tests, and backfills tests for other marker properties as
    well.
    
    Fixes https://github.com/flutter/flutter/issues/159471

--- packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/GoogleMapMarkerController_Test.h
+++ packages/google_maps_flutter/google_maps_flutter_ios/ios/Classes/GoogleMapMarkerController_Test.h
@@ -4,26 +4,18 @@
 
 #import "GoogleMapMarkerController.h"
 
+/// Methods exposed for unit testing.
 @interface FLTGoogleMapMarkerController (Test)
 
-/// Extracts an icon image from the iconData array.
-///
-/// @param platformBitmap The Pigeon representation of the icon image.
-/// @param registrar A Flutter plugin registrar.
-/// @param screenScale Screen scale factor for scaling bitmaps. Must be greater than 0.
-/// @return A UIImage object created from the icon data.
-/// @note Assert unless screenScale is greater than 0.
-- (UIImage *)iconFromBitmap:(FGMPlatformBitmap *)platformBitmap
-                  registrar:(NSObject<FlutterPluginRegistrar> *)registrar
-                screenScale:(CGFloat)screenScale;
+/// The underlying controlled GMSMarker.
+@property(strong, nonatomic, readonly) GMSMarker *marker;
 
-/// Checks if an image can be scaled from an original size to a target size using a scale factor
-/// while maintaining the aspect ratio.
-///
-/// @param originalSize The original size of the image.
-/// @param targetSize The desired target size to scale the image to.
-/// @return A BOOL indicating whether the image can be scaled to the target size with scale
-/// factor.
-+ (BOOL)isScalableWithScaleFactorFromSize:(CGSize)originalSize toSize:(CGSize)targetSize;
+@end
+
+/// Methods exposed for unit testing.
+@interface FLTMarkersController (Test)
+
+/// A mapping from marker identifiers to corresponding marker controllers.
+@property(strong, nonatomic, readonly) NSMutableDictionary *markerIdentifierToController;
 
 @end
