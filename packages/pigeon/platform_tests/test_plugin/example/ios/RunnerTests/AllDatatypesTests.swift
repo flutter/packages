fdcb8f11f0e41462cd749958d540491e993ea005
tarrinneal <tarrinneal@gmail.com>
2025-03-31 22:40:31 -0700

    tests

--- packages/pigeon/platform_tests/test_plugin/example/ios/RunnerTests/AllDatatypesTests.swift
+++ packages/pigeon/platform_tests/test_plugin/example/ios/RunnerTests/AllDatatypesTests.swift
@@ -73,4 +73,108 @@ class AllDatatypesTests: XCTestCase {
     }
     wait(for: [expectation], timeout: 1.0)
   }
+
+  private let correctList: [Any?] = ["a", 2, "three"]
+  private let matchingList: [Any?] = ["a", 2, "three"]
+  private let differentList: [Any?] = ["a", 2, "three", 4.0]
+
+  private let correctMap: [AnyHashable: Any?] = ["a": 1, "b": 2, "c": "three"]
+  private let matchingMap: [AnyHashable: Any?] = ["a": 1, "b": 2, "c": "three"]
+  private let differentKeyMap: [AnyHashable: Any?] = ["a": 1, "b": 2, "d": "three"]
+  private let differentValueMap: [AnyHashable: Any?] = ["a": 1, "b": 2, "c": "five"]
+
+  private lazy var correctListInMap: [AnyHashable: Any?] = ["a": 1, "b": 2, "c": correctList]
+  private lazy var matchingListInMap: [AnyHashable: Any?] = ["a": 1, "b": 2, "c": matchingList]
+  private lazy var differentListInMap: [AnyHashable: Any?] = ["a": 1, "b": 2, "c": differentList]
+
+  private lazy var correctMapInList: [Any?] = ["a", 2, correctMap]
+  private lazy var matchingMapInList: [Any?] = ["a", 2, matchingMap]
+  private lazy var differentKeyMapInList: [Any?] = ["a", 2, differentKeyMap]
+  private lazy var differentValueMapInList: [Any?] = ["a", 2, differentValueMap]
+
+  func testEqualityMethodCorrectlyChecksDeepEquality() {
+    let generic = AllNullableTypes(list: correctList, map: correctMap)
+    let identical = generic
+    XCTAssertEqual(generic, identical, "Identical copies should be equal")
+  }
+
+  func testEqualityMethodCorrectlyIdentifiesNonMatchingClasses() {
+    let generic = AllNullableTypes(list: correctList, map: correctMap)
+    let allNull = AllNullableTypes()
+    XCTAssertNotEqual(
+      allNull, generic, "Instance with nil properties should not equal instance with values")
+  }
+
+  func testEqualityMethodCorrectlyIdentifiesNonMatchingListsInClasses() {
+    let withList = AllNullableTypes(list: correctList)
+    let withDifferentList = AllNullableTypes(list: differentList)
+    XCTAssertNotEqual(
+      withList, withDifferentList, "Instances with different lists should not be equal")
+  }
+
+  func testEqualityMethodCorrectlyIdentifiesMatchingButUniqueListsInClasses() {
+    let withList = AllNullableTypes(list: correctList)
+    let withMatchingList = AllNullableTypes(list: matchingList)
+    XCTAssertEqual(withList, withMatchingList, "Instances with equivalent lists should be equal")
+  }
+
+  func testEqualityMethodCorrectlyIdentifiesNonMatchingKeysInMapsInClasses() {
+    let withMap = AllNullableTypes(map: correctMap)
+    let withDifferentMap = AllNullableTypes(map: differentKeyMap)
+    XCTAssertNotEqual(
+      withMap, withDifferentMap, "Instances with different map keys should not be equal")
+  }
+
+  func testEqualityMethodCorrectlyIdentifiesNonMatchingValuesInMapsInClasses() {
+    let withMap = AllNullableTypes(map: correctMap)
+    let withDifferentMap = AllNullableTypes(map: differentValueMap)
+    XCTAssertNotEqual(
+      withMap, withDifferentMap, "Instances with different map values should not be equal")
+  }
+
+  func testEqualityMethodCorrectlyIdentifiesMatchingButUniqueMapsInClasses() {
+    let withMap = AllNullableTypes(map: correctMap)
+    let withMatchingMap = AllNullableTypes(map: matchingMap)
+    XCTAssertEqual(withMap, withMatchingMap, "Instances with equivalent maps should be equal")
+  }
+
+  func testEqualityMethodCorrectlyIdentifiesNonMatchingListsNestedInMapsInClasses() {
+    let withListInMap = AllNullableTypes(map: correctListInMap)
+    let withDifferentListInMap = AllNullableTypes(map: differentListInMap)
+    XCTAssertNotEqual(
+      withListInMap, withDifferentListInMap,
+      "Instances with different nested lists in maps should not be equal")
+  }
+
+  func testEqualityMethodCorrectlyIdentifiesMatchingButUniqueListsNestedInMapsInClasses() {
+    let withListInMap = AllNullableTypes(map: correctListInMap)
+    let withMatchingListInMap = AllNullableTypes(map: matchingListInMap)
+    XCTAssertEqual(
+      withListInMap, withMatchingListInMap,
+      "Instances with equivalent nested lists in maps should be equal")
+  }
+
+  func testEqualityMethodCorrectlyIdentifiesNonMatchingKeysInMapsNestedInListsInClasses() {
+    let withMapInList = AllNullableTypes(list: correctMapInList)
+    let withDifferentMapInList = AllNullableTypes(list: differentKeyMapInList)
+    XCTAssertNotEqual(
+      withMapInList, withDifferentMapInList,
+      "Instances with different nested map keys in lists should not be equal")
+  }
+
+  func testEqualityMethodCorrectlyIdentifiesNonMatchingValuesInMapsNestedInListsInClasses() {
+    let withMapInList = AllNullableTypes(list: correctMapInList)
+    let withDifferentMapInList = AllNullableTypes(list: differentValueMapInList)
+    XCTAssertNotEqual(
+      withMapInList, withDifferentMapInList,
+      "Instances with different nested map values in lists should not be equal")
+  }
+
+  func testEqualityMethodCorrectlyIdentifiesMatchingButUniqueMapsNestedInListsInClasses() {
+    let withMapInList = AllNullableTypes(list: correctMapInList)
+    let withMatchingMapInList = AllNullableTypes(list: matchingMapInList)
+    XCTAssertEqual(
+      withMapInList, withMatchingMapInList,
+      "Instances with equivalent nested maps in lists should be equal")
+  }
 }
