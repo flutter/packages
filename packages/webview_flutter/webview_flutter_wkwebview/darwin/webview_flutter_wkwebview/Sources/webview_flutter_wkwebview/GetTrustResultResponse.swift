52c530b99a014edfdb419c80b92c5e54260c2aa8
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2025-04-06 16:03:00 -0400

    add security imports

--- packages/webview_flutter/webview_flutter_wkwebview/darwin/webview_flutter_wkwebview/Sources/webview_flutter_wkwebview/GetTrustResultResponse.swift
+++ packages/webview_flutter/webview_flutter_wkwebview/darwin/webview_flutter_wkwebview/Sources/webview_flutter_wkwebview/GetTrustResultResponse.swift
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import Darwin
+import Security
 
 /// Data class used to respond to `SecTrustGetTrustResult`.
 ///
--- packages/webview_flutter/webview_flutter_wkwebview/darwin/webview_flutter_wkwebview/Sources/webview_flutter_wkwebview/SecWrappers.swift
+++ packages/webview_flutter/webview_flutter_wkwebview/darwin/webview_flutter_wkwebview/Sources/webview_flutter_wkwebview/SecWrappers.swift
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import Security
+
 /// Wrapper for `SecTrust`.
 ///
 /// Corefoundation types don't support being casted in Swift and will always succeed
