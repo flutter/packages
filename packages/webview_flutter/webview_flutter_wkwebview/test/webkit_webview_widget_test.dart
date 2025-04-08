45abbc125990bb344668721be50d9cc9d120eb60
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2025-04-07 22:17:39 -0400

    reset wkwebview

--- packages/webview_flutter/webview_flutter_wkwebview/test/webkit_webview_widget_test.dart
+++ packages/webview_flutter/webview_flutter_wkwebview/test/webkit_webview_widget_test.dart
@@ -2,31 +2,29 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io';
-
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:webview_flutter_wkwebview/src/common/instance_manager.dart';
-import 'package:webview_flutter_wkwebview/src/foundation/foundation.dart';
-import 'package:webview_flutter_wkwebview/src/ui_kit/ui_kit.dart';
-import 'package:webview_flutter_wkwebview/src/web_kit/web_kit.dart';
+import 'package:webview_flutter_wkwebview/src/common/platform_webview.dart';
+import 'package:webview_flutter_wkwebview/src/common/web_kit.g.dart';
 import 'package:webview_flutter_wkwebview/src/webkit_proxy.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
-import 'webkit_webview_controller_test.mocks.dart'
-    show MockUIScrollViewDelegate;
+
 import 'webkit_webview_widget_test.mocks.dart';
 
-@GenerateMocks(<Type>[WKUIDelegate, WKWebViewConfiguration])
+@GenerateMocks(<Type>[
+  WKUIDelegate,
+  WKWebViewConfiguration,
+  UIScrollViewDelegate,
+])
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   group('WebKitWebViewWidget', () {
     testWidgets('build', (WidgetTester tester) async {
-      final InstanceManager testInstanceManager = InstanceManager(
-        onWeakReferenceRemoved: (_) {},
-      );
+      final PigeonInstanceManager testInstanceManager = TestInstanceManager();
 
       final WebKitWebViewController controller =
           createTestWebViewController(testInstanceManager);
@@ -43,16 +41,20 @@ void main() {
         Builder(builder: (BuildContext context) => widget.build(context)),
       );
 
-      expect(find.byType(Platform.isMacOS ? AppKitView : UiKitView),
-          findsOneWidget);
+      expect(
+        find.byType(
+          defaultTargetPlatform == TargetPlatform.macOS
+              ? AppKitView
+              : UiKitView,
+        ),
+        findsOneWidget,
+      );
       expect(find.byKey(const Key('keyValue')), findsOneWidget);
     });
 
     testWidgets('Key of the PlatformView changes when the controller changes',
         (WidgetTester tester) async {
-      final InstanceManager testInstanceManager = InstanceManager(
-        onWeakReferenceRemoved: (_) {},
-      );
+      final PigeonInstanceManager testInstanceManager = TestInstanceManager();
 
       // Pump WebViewWidget with first controller.
       final WebKitWebViewController controller1 =
@@ -119,9 +121,7 @@ void main() {
     testWidgets(
         'Key of the PlatformView is the same when the creation params are equal',
         (WidgetTester tester) async {
-      final InstanceManager testInstanceManager = InstanceManager(
-        onWeakReferenceRemoved: (_) {},
-      );
+      final PigeonInstanceManager testInstanceManager = TestInstanceManager();
 
       final WebKitWebViewController controller =
           createTestWebViewController(testInstanceManager);
@@ -177,50 +177,59 @@ void main() {
 }
 
 WebKitWebViewController createTestWebViewController(
-  InstanceManager testInstanceManager,
+  PigeonInstanceManager testInstanceManager,
 ) {
   return WebKitWebViewController(
     WebKitWebViewControllerCreationParams(
-      webKitProxy: WebKitProxy(createWebView: (
-        WKWebViewConfiguration configuration, {
-        void Function(
-          String keyPath,
-          NSObject object,
-          Map<NSKeyValueChangeKey, Object?> change,
-        )? observeValue,
-        InstanceManager? instanceManager,
-      }) {
-        final WKWebView webView = WKWebViewIOS.detached(
-          instanceManager: testInstanceManager,
-        );
-        testInstanceManager.addDartCreatedInstance(webView);
-        return webView;
-      }, createWebViewConfiguration: ({InstanceManager? instanceManager}) {
-        return MockWKWebViewConfiguration();
-      }, createUIDelegate: ({
-        dynamic onCreateWebView,
-        dynamic requestMediaCapturePermission,
-        dynamic runJavaScriptAlertDialog,
-        dynamic runJavaScriptConfirmDialog,
-        dynamic runJavaScriptTextInputDialog,
-        InstanceManager? instanceManager,
-      }) {
-        final MockWKUIDelegate mockWKUIDelegate = MockWKUIDelegate();
-        when(mockWKUIDelegate.copy()).thenReturn(MockWKUIDelegate());
+      webKitProxy: WebKitProxy(
+        newPlatformWebView: ({
+          required WKWebViewConfiguration initialConfiguration,
+          void Function(
+            NSObject,
+            String?,
+            NSObject?,
+            Map<KeyValueChangeKey, Object>?,
+          )? observeValue,
+        }) {
+          final UIViewWKWebView webView = UIViewWKWebView.pigeon_detached(
+            pigeon_instanceManager: testInstanceManager,
+          );
+          testInstanceManager.addDartCreatedInstance(webView);
+          return PlatformWebView.fromNativeWebView(webView);
+        },
+        newWKWebViewConfiguration: () {
+          return MockWKWebViewConfiguration();
+        },
+        newWKUIDelegate: ({
+          dynamic onCreateWebView,
+          dynamic requestMediaCapturePermission,
+          dynamic runJavaScriptAlertPanel,
+          dynamic runJavaScriptConfirmPanel,
+          dynamic runJavaScriptTextInputPanel,
+        }) {
+          final MockWKUIDelegate mockWKUIDelegate = MockWKUIDelegate();
+          when(mockWKUIDelegate.pigeon_copy()).thenReturn(MockWKUIDelegate());
 
-        testInstanceManager.addDartCreatedInstance(mockWKUIDelegate);
-        return mockWKUIDelegate;
-      }, createUIScrollViewDelegate: ({
-        void Function(UIScrollView, double, double)? scrollViewDidScroll,
-      }) {
-        final MockUIScrollViewDelegate mockScrollViewDelegate =
-            MockUIScrollViewDelegate();
-        when(mockScrollViewDelegate.copy())
-            .thenReturn(MockUIScrollViewDelegate());
+          testInstanceManager.addDartCreatedInstance(mockWKUIDelegate);
+          return mockWKUIDelegate;
+        },
+        newUIScrollViewDelegate: ({
+          dynamic scrollViewDidScroll,
+        }) {
+          final MockUIScrollViewDelegate mockScrollViewDelegate =
+              MockUIScrollViewDelegate();
+          when(mockScrollViewDelegate.pigeon_copy())
+              .thenReturn(MockUIScrollViewDelegate());
 
-        testInstanceManager.addDartCreatedInstance(mockScrollViewDelegate);
-        return mockScrollViewDelegate;
-      }),
+          testInstanceManager.addDartCreatedInstance(mockScrollViewDelegate);
+          return mockScrollViewDelegate;
+        },
+      ),
     ),
   );
+}
+
+// Test InstanceManager that sets `onWeakReferenceRemoved` as a noop.
+class TestInstanceManager extends PigeonInstanceManager {
+  TestInstanceManager() : super(onWeakReferenceRemoved: (_) {});
 }
