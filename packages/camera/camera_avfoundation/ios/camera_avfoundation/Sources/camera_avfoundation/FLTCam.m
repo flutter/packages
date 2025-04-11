2e78c19ebafc7df52a18f4b4b54f716881d22713
Jorge S. <jorgesarpe@gmail.com>
2025-04-11 13:22:07 +0200

    call completion in all paths

--- packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/FLTCam.m
+++ packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/FLTCam.m
@@ -846,43 +846,50 @@ static void selectBestFormatForRequestedFrameRate(
 - (void)startVideoRecordingWithCompletion:(void (^)(FlutterError *_Nullable))completion
                     messengerForStreaming:(nullable NSObject<FlutterBinaryMessenger> *)messenger {
   if (!_isRecording) {
+    
+    // Main logic to setup the video recording in a block to reuse in the completion block.
+    void (^videoRecordingSetupWithCompletion)(void) = ^void(void) {
+      NSError *error;
+      self->_videoRecordingPath = [self getTemporaryFilePathWithExtension:@"mp4"
+                                                                subfolder:@"videos"
+                                                                   prefix:@"REC_"
+                                                                    error:error];
+      if (error) {
+        completion(FlutterErrorFromNSError(error));
+        return;
+      }
+      if (![self setupWriterForPath:self->_videoRecordingPath]) {
+        completion([FlutterError errorWithCode:@"IOError"
+                                       message:@"Setup Writer Failed"
+                                       details:nil]);
+        return;
+      }
+      // startWriting should not be called in didOutputSampleBuffer where it can cause state
+      // in which _isRecording is YES but _videoWriter.status is AVAssetWriterStatusUnknown
+      // in stopVideoRecording if it is called after startVideoRecording but before
+      // didOutputSampleBuffer had chance to call startWriting and lag at start of video
+      // https://github.com/flutter/flutter/issues/132016
+      // https://github.com/flutter/flutter/issues/151319
+      [self->_videoWriter startWriting];
+      self->_isFirstVideoSample = YES;
+      self->_isRecording = YES;
+      self->_isRecordingPaused = NO;
+      self->_videoTimeOffset = CMTimeMake(0, 1);
+      self->_audioTimeOffset = CMTimeMake(0, 1);
+      self->_videoIsDisconnected = NO;
+      self->_audioIsDisconnected = NO;
+      completion(nil);
+    };
+
     if (messenger != nil) {
-      // Start image stream without waiting for result.
       [self startImageStreamWithMessenger:messenger
-                               completion:^(FlutterError *_Nullable error){
+                               completion:^(FlutterError *_Nullable error) {
+                                 videoRecordingSetupWithCompletion();
                                }];
+      return;
     }
 
-    NSError *error;
-    _videoRecordingPath = [self getTemporaryFilePathWithExtension:@"mp4"
-                                                        subfolder:@"videos"
-                                                           prefix:@"REC_"
-                                                            error:error];
-    if (error) {
-      completion(FlutterErrorFromNSError(error));
-      return;
-    }
-    if (![self setupWriterForPath:_videoRecordingPath]) {
-      completion([FlutterError errorWithCode:@"IOError"
-                                     message:@"Setup Writer Failed"
-                                     details:nil]);
-      return;
-    }
-    // startWriting should not be called in didOutputSampleBuffer where it can cause state
-    // in which _isRecording is YES but _videoWriter.status is AVAssetWriterStatusUnknown
-    // in stopVideoRecording if it is called after startVideoRecording but before
-    // didOutputSampleBuffer had chance to call startWriting and lag at start of video
-    // https://github.com/flutter/flutter/issues/132016
-    // https://github.com/flutter/flutter/issues/151319
-    [_videoWriter startWriting];
-    _isFirstVideoSample = YES;
-    _isRecording = YES;
-    _isRecordingPaused = NO;
-    _videoTimeOffset = CMTimeMake(0, 1);
-    _audioTimeOffset = CMTimeMake(0, 1);
-    _videoIsDisconnected = NO;
-    _audioIsDisconnected = NO;
-    completion(nil);
+    videoRecordingSetupWithCompletion();
   } else {
     completion([FlutterError errorWithCode:@"Error"
                                    message:@"Video is already recording"
