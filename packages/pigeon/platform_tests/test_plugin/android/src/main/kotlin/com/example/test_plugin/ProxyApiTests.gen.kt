45619f73539fd3c2c4c9d84a6aa8fe509937d255
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2025-04-13 18:19:46 -0400

    test it on ci

--- packages/pigeon/platform_tests/test_plugin/android/src/main/kotlin/com/example/test_plugin/ProxyApiTests.gen.kt
+++ packages/pigeon/platform_tests/test_plugin/android/src/main/kotlin/com/example/test_plugin/ProxyApiTests.gen.kt
@@ -88,7 +88,7 @@ class ProxyApiTestsPigeonInstanceManager(
    */
   var clearFinalizedWeakReferencesInterval: Long = 3000
     set(value) {
-      handler.removeCallbacks(releaseAllFinalizedInstancesRunnable)
+      handler.removeCallbacks(::releaseAllFinalizedInstances)
       field = value
       releaseAllFinalizedInstances()
     }
