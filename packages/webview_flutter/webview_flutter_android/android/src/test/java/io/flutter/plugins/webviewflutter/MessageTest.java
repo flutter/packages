253819c5092a8766a568ded1ee793ef5216efefd
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2025-04-13 19:26:19 -0400

    formatting

--- packages/webview_flutter/webview_flutter_android/android/src/test/java/io/flutter/plugins/webviewflutter/MessageTest.java
+++ packages/webview_flutter/webview_flutter_android/android/src/test/java/io/flutter/plugins/webviewflutter/MessageTest.java
@@ -4,22 +4,16 @@
 
 package io.flutter.plugins.webviewflutter;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import android.os.Message;
-import android.webkit.PermissionRequest;
-import java.util.Collections;
-import java.util.List;
 import org.junit.Test;
 
 public class MessageTest {
   @Test
   public void sendToTarget() {
-    final PigeonApiAndroidMessage api =
-        new TestProxyApiRegistrar().getPigeonApiAndroidMessage();
+    final PigeonApiAndroidMessage api = new TestProxyApiRegistrar().getPigeonApiAndroidMessage();
 
     final Message instance = mock(Message.class);
     api.sendToTarget(instance);
