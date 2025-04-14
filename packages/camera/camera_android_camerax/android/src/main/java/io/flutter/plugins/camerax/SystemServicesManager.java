d63078b9c7de0d9885bdb25f8f6ceea0f1ad28f7
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2025-04-14 16:54:10 -0400

    add docs for systemservices

--- packages/camera/camera_android_camerax/android/src/main/java/io/flutter/plugins/camerax/SystemServicesManager.java
+++ packages/camera/camera_android_camerax/android/src/main/java/io/flutter/plugins/camerax/SystemServicesManager.java
@@ -11,9 +11,11 @@ import androidx.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
 
+/** Utility class used to access system services not provided by the camerax library. */
 public abstract class SystemServicesManager {
   @NonNull private final CameraPermissionsManager cameraPermissionsManager;
 
+  /** Handles result of a permissions request. */
   public interface PermissionsResultListener {
     void onResult(boolean isSuccessful, @Nullable CameraPermissionsError error);
   }
@@ -30,6 +32,12 @@ public abstract class SystemServicesManager {
   @Nullable
   abstract CameraPermissionsManager.PermissionsRegistry getPermissionsRegistry();
 
+  /**
+   * Requests camera permissions using an instance of a {@link CameraPermissionsManager}.
+   *
+   * <p>Will result with {@code null} if permissions were approved or there were no errors;
+   * otherwise, it will result with the error data explaining what went wrong.
+   */
   public void requestCameraPermissions(
       @NonNull Boolean enableAudio, @NonNull PermissionsResultListener listener) {
     if (!(getContext() instanceof Activity)) {
