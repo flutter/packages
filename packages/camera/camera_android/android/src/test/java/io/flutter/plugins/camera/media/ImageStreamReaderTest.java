cc61ef89e5a4240201c0f9dfc312e57539c76b94
kwikwag <392199+kwikwag@users.noreply.github.com>
2025-04-15 12:16:29 +0300

    fix: move getImage to ImageStreamReaderTestUtils

--- packages/camera/camera_android/android/src/test/java/io/flutter/plugins/camera/media/ImageStreamReaderTest.java
+++ packages/camera/camera_android/android/src/test/java/io/flutter/plugins/camera/media/ImageStreamReaderTest.java
@@ -31,48 +31,6 @@ import org.robolectric.RobolectricTestRunner;
 
 @RunWith(RobolectricTestRunner.class)
 public class ImageStreamReaderTest {
-  public static Image getImage(int imageWidth, int imageHeight, int padding, int imageFormat) {
-    int rowStride = imageWidth + padding;
-
-    int ySize = (rowStride * imageHeight) - padding;
-    int uSize = (ySize / 2) - (padding / 2);
-    int vSize = uSize;
-
-    // Mock YUV image
-    Image mockImage = mock(Image.class);
-    when(mockImage.getWidth()).thenReturn(imageWidth);
-    when(mockImage.getHeight()).thenReturn(imageHeight);
-    when(mockImage.getFormat()).thenReturn(imageFormat);
-
-    // Mock planes. YUV images have 3 planes (Y, U, V).
-    Image.Plane planeY = mock(Image.Plane.class);
-    Image.Plane planeU = mock(Image.Plane.class);
-    Image.Plane planeV = mock(Image.Plane.class);
-
-    // Y plane is width*height
-    // Row stride is generally == width but when there is padding it will
-    // be larger.
-    // Here we are adding 256 padding.
-    when(planeY.getBuffer()).thenReturn(ByteBuffer.allocate(ySize));
-    when(planeY.getRowStride()).thenReturn(rowStride);
-    when(planeY.getPixelStride()).thenReturn(1);
-
-    // U and V planes are always the same sizes/values.
-    // https://developer.android.com/reference/android/graphics/ImageFormat#YUV_420_888
-    when(planeU.getBuffer()).thenReturn(ByteBuffer.allocate(uSize));
-    when(planeV.getBuffer()).thenReturn(ByteBuffer.allocate(vSize));
-    when(planeU.getRowStride()).thenReturn(rowStride);
-    when(planeV.getRowStride()).thenReturn(rowStride);
-    when(planeU.getPixelStride()).thenReturn(2);
-    when(planeV.getPixelStride()).thenReturn(2);
-
-    // Add planes to image
-    Image.Plane[] planes = {planeY, planeU, planeV};
-    when(mockImage.getPlanes()).thenReturn(planes);
-
-    return mockImage;
-  }
-
   /** If we request YUV42 we should stream in YUV420. */
   @Test
   public void computeStreamImageFormat_computesCorrectStreamFormatYuv() {
@@ -112,7 +70,7 @@ public class ImageStreamReaderTest {
 
     // Note: the code for getImage() was previously inlined, with uSize set to one less than
     // getImage() calculates (see function implementation)
-    Image mockImage = ImageStreamReaderTest.getImage(1280, 720, 256, ImageFormat.YUV_420_888);
+    Image mockImage = ImageStreamReaderTestUtils.getImage(1280, 720, 256, ImageFormat.YUV_420_888);
 
     CameraCaptureProperties mockCaptureProps = mock(CameraCaptureProperties.class);
     EventChannel.EventSink mockEventSink = mock(EventChannel.EventSink.class);
@@ -141,7 +99,7 @@ public class ImageStreamReaderTest {
 
     // Note: the code for getImage() was previously inlined, with uSize set to one less than
     // getImage() calculates (see function implementation)
-    Image mockImage = ImageStreamReaderTest.getImage(1280, 720, 256, ImageFormat.YUV_420_888);
+    Image mockImage = ImageStreamReaderTestUtils.getImage(1280, 720, 256, ImageFormat.YUV_420_888);
 
     CameraCaptureProperties mockCaptureProps = mock(CameraCaptureProperties.class);
     EventChannel.EventSink mockEventSink = mock(EventChannel.EventSink.class);
@@ -181,7 +139,7 @@ public class ImageStreamReaderTest {
       CameraCaptureProperties mockCaptureProps = mock(CameraCaptureProperties.class);
       EventChannel.EventSink mockEventSink = mock(EventChannel.EventSink.class);
 
-      Image mockImage = ImageStreamReaderTest.getImage(1280, 720, 256, ImageFormat.YUV_420_888);
+      Image mockImage = ImageStreamReaderTestUtils.getImage(1280, 720, 256, ImageFormat.YUV_420_888);
       imageStreamReader.onImageAvailable(mockImage, mockCaptureProps, mockEventSink);
 
       // make sure the image was closed, even when skipping frames
