98fb6e959c7909fb9646e2561669a3d658c86746
10909 <zhihua@addcn.com>
2025-04-15 15:33:51 +0800

    [google_maps_flutter]format: format unit test code

--- packages/google_maps_flutter/google_maps_flutter/test/controller_test.dart
+++ packages/google_maps_flutter/google_maps_flutter/test/controller_test.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -18,7 +17,7 @@ void main() {
       (WidgetTester tester) async {
     final FakeGoogleMapsFlutterPlatform platform =
         FakeGoogleMapsFlutterPlatform();
-        
+
     GoogleMapsFlutterPlatform.instance = platform;
 
     final FakePlatformViewsController fakePlatformViewsController =
