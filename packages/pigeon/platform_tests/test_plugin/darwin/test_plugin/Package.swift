400dcb1d74e5afd44a32287943591d5cd81ef785
Stuart Morgan <stuartmorgan@google.com>
2025-04-17 10:55:15 -0400

    Check in all the automatic project changes

--- packages/pigeon/platform_tests/test_plugin/darwin/test_plugin/Package.swift
+++ packages/pigeon/platform_tests/test_plugin/darwin/test_plugin/Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "test_plugin",
-    platforms: [
-        .iOS("12.0"),
-        .macOS("10.14")
-    ],
-    products: [
-        .library(name: "test-plugin", targets: ["test_plugin"])
-    ],
-    dependencies: [],
-    targets: [
-        .target(
-            name: "test_plugin",
-            dependencies: [],
-            resources: []
-        )
-    ]
+  name: "test_plugin",
+  platforms: [
+    .iOS("12.0"),
+    .macOS("10.14"),
+  ],
+  products: [
+    .library(name: "test-plugin", targets: ["test_plugin"])
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "test_plugin",
+      dependencies: [],
+      resources: []
+    )
+  ]
 )
