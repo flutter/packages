bb591c3dfa94f0a1715eac1ec8ffdd1ccc8d9556
wangyongqi <wyq@addcn.com>
2025-05-06 18:03:11 +0800

    Ensure to perform validity checks before using images, provide clear error messages, and enhance the robustness and maintainability of the code.

--- packages/vector_graphics/lib/src/listener.dart
+++ packages/vector_graphics/lib/src/listener.dart
@@ -773,7 +773,12 @@ class FlutterVectorGraphicsListener extends VectorGraphicsCodecListener {
   @override
   void onDrawImage(int imageId, double x, double y, double width, double height,
       Float64List? transform) {
-    final Image image = _images[imageId]!;
+    final Image? image = _images[imageId];
+    if (image == null) {
+      throw ArgumentError(
+          'Invalid imageId: $imageId. Image not found in _images.');
+    }
+
     if (transform != null) {
       _canvas.save();
       _canvas.transform(transform);
