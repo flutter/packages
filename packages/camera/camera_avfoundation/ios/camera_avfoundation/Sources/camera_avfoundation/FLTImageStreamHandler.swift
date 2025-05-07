5bfa7361ae7b4af7fcceaa64ab9792cc3eaf2a14
Robert Odrowaz <robert.odrowaz@leancode.pl>
2025-05-07 19:13:27 +0200

    Fix imports for SwiftPM

--- packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/FLTImageStreamHandler.swift
+++ packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/FLTImageStreamHandler.swift
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import Flutter
+import ObjectiveC
+
 class FLTImageStreamHandler: NSObject, FlutterStreamHandler {
 
   /// The queue on which `eventSink` property should be accessed.
--- packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/QueueUtils.swift
+++ packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/QueueUtils.swift
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import Dispatch
+
 /// Queue-specific context data to be associated with the capture session queue.
 let fltCaptureSessionQueueSpecificKey = DispatchSpecificKey<String>()
 let fltCaptureSessionQueueSpecificValue = "capture_session_queue"
