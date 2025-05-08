8622d3fcd8abfa21b7d4e1b5c1e7d13c3d700b2c
Robert Odrowaz <robert.odrowaz@leancode.pl>
2025-05-08 15:11:35 +0200

    Extract format util methods from DefaultCamera

--- packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/DefaultCamera.swift
+++ packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/DefaultCamera.swift
@@ -127,66 +127,6 @@ final class DefaultCamera: NSObject, Camera {
       details: error.domain)
   }
 
-  // Returns frame rate supported by format closest to targetFrameRate.
-  private static func bestFrameRate(for format: FLTCaptureDeviceFormat, targetFrameRate: Double)
-    -> Double
-  {
-    var bestFrameRate = 0.0
-    var minDistance = Double.greatestFiniteMagnitude
-    for range in format.videoSupportedFrameRateRanges {
-      let frameRate = min(
-        max(targetFrameRate, Double(range.minFrameRate)), Double(range.maxFrameRate))
-      let distance = abs(frameRate - targetFrameRate)
-      if distance < minDistance {
-        bestFrameRate = frameRate
-        minDistance = distance
-      }
-    }
-    return bestFrameRate
-  }
-
-  // Finds format with same resolution as current activeFormat in captureDevice for which
-  // bestFrameRateForFormat returned frame rate closest to mediaSettings.framesPerSecond.
-  // Preferred are formats with the same subtype as current activeFormat. Sets this format
-  // as activeFormat and also updates mediaSettings.framesPerSecond to value which
-  // bestFrameRateForFormat returned for that format.
-  private static func selectBestFormatForRequestedFrameRate(
-    captureDevice: FLTCaptureDevice,
-    mediaSettings: FCPPlatformMediaSettings,
-    targetFrameRate: Double,
-    videoDimensionsForFormat: (FLTCaptureDeviceFormat) -> CMVideoDimensions
-  ) {
-    let targetResolution = videoDimensionsForFormat(captureDevice.activeFormat)
-    let preferredSubType = CMFormatDescriptionGetMediaSubType(
-      captureDevice.activeFormat.formatDescription)
-    var bestFormat = captureDevice.activeFormat
-    var _bestFrameRate = bestFrameRate(for: bestFormat, targetFrameRate: targetFrameRate)
-    var minDistance = abs(_bestFrameRate - targetFrameRate)
-    var isBestSubTypePreferred = true
-
-    for format in captureDevice.formats {
-      let resolution = videoDimensionsForFormat(format)
-      if resolution.width != targetResolution.width || resolution.height != targetResolution.height
-      {
-        continue
-      }
-      let frameRate = bestFrameRate(for: format, targetFrameRate: targetFrameRate)
-      let distance = abs(frameRate - targetFrameRate)
-      let subType = CMFormatDescriptionGetMediaSubType(format.formatDescription)
-      let isSubTypePreferred = subType == preferredSubType
-      if distance < minDistance
-        || (distance == minDistance && isSubTypePreferred && !isBestSubTypePreferred)
-      {
-        bestFormat = format
-        _bestFrameRate = frameRate
-        minDistance = distance
-        isBestSubTypePreferred = isSubTypePreferred
-      }
-    }
-    captureDevice.activeFormat = bestFormat
-    mediaSettings.framesPerSecond = NSNumber(value: _bestFrameRate)
-  }
-
   private static func createConnection(
     captureDevice: FLTCaptureDevice,
     videoFormat: FourCharCode,
@@ -276,7 +216,7 @@ final class DefaultCamera: NSObject, Camera {
         throw error
       }
 
-      DefaultCamera.selectBestFormatForRequestedFrameRate(
+      FormatUtils.selectBestFormatForRequestedFrameRate(
         captureDevice: captureDevice,
         mediaSettings: mediaSettings,
         targetFrameRate: targetFrameRate.doubleValue,
