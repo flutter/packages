d6bd8bc955a1cbeb67de27f95da0cd0d18eb021a
Stuart Morgan <stuartmorgan@google.com>
2025-05-29 11:24:42 -0400

    Handle emulators without sign-in support

--- packages/google_sign_in/google_sign_in_android/example/integration_test/google_sign_in_test.dart
+++ packages/google_sign_in/google_sign_in_android/example/integration_test/google_sign_in_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_sign_in_platform_interface/google_sign_in_platform_interface.dart';
 import 'package:integration_test/integration_test.dart';
@@ -17,6 +18,16 @@ void main() {
   testWidgets('Method channel handler is present', (WidgetTester tester) async {
     // Validate that the native method handler is present and configured.
     final GoogleSignInPlatform signIn = GoogleSignInPlatform.instance;
-    await expectLater(signIn.signOut(const SignOutParams()), completes);
+    try {
+      await signIn.signOut(const SignOutParams());
+    } on PlatformException catch (e) {
+      if (e.code == 'Clear Failed') {
+        // This is thrown when running on a device or emulator without Google
+        // Sign in support. It comes from a call into the SDK, so is fine for
+        // the purposes of this test since it means the native handler ran.
+      } else {
+        fail('Unexpected exception: e');
+      }
+    }
   });
 }
