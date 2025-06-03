flutter/flutter

commit 3df4a3c6a5f3ca9ff1605add0ba78b1e73c85bc3

Baseline-align CupertinoTextField placeholder (#166952)
| Before | After | 
| --- | --- |
| <img width="348" alt="non baseline aligned placeholder"
src="https://github.com/user-attachments/assets/62f779cc-1d16-41f3-92f8-a2de16a04895"
/> | <img width="348" alt="baseline aligned placeholder"
src="https://github.com/user-attachments/assets/e43d200f-ef47-4d10-a74a-a2c6998b44f5"
/> |


Fixes [Can't align placeholder of CupertinoTextField with min lines to
top](https://github.com/flutter/flutter/issues/138794)

packages/flutter/lib/src/cupertino/text_field.dart
@@ -5,6 +5,7 @@
 /// @docImport 'package:flutter/material.dart';
 library;
 
+import 'dart:math' as math;
 import 'dart:ui' as ui show BoxHeightStyle, BoxWidthStyle;
 
 import 'package:flutter/foundation.dart' show defaultTargetPlatform;
@@ -1387,14 +1388,14 @@ class _CupertinoTextFieldState extends State<CupertinoTextField>
             // In the middle part, stack the placeholder on top of the main EditableText
             // if needed.
             Expanded(
-              child: Stack(
-                // Ideally this should be baseline aligned. However that comes at
-                // the cost of the ability to compute the intrinsic dimensions of
-                // this widget.
-                // See also https://github.com/flutter/flutter/issues/13715.
-                alignment: AlignmentDirectional.center,
-                textDirection: widget.textDirection,
-                children: <Widget>[if (placeholder != null) placeholder, editableText],
+              child: Directionality(
+                textDirection: widget.textDirection ?? Directionality.of(context),
+                child: _BaselineAlignedStack(
+                  placeholder: placeholder,
+                  editableText: editableText,
+                  editableTextBaseline: textStyle.textBaseline ?? TextBaseline.alphabetic,
+                  placeholderBaseline: placeholderStyle.textBaseline ?? TextBaseline.alphabetic,
+                ),
               ),
             ),
             if (suffixWidget != null) suffixWidget,
@@ -1692,5 +1693,192 @@ class _CupertinoTextFieldState extends State<CupertinoTextField>
         ),
       ),
     );
+  }
+}
+
+enum _BaselineAlignedStackSlot { placeholder, editableText }
+
+class _BaselineAlignedStack
+    extends SlottedMultiChildRenderObjectWidget<_BaselineAlignedStackSlot, RenderBox> {
+  const _BaselineAlignedStack({
+    required this.editableTextBaseline,
+    required this.placeholderBaseline,
+    required this.editableText,
+    this.placeholder,
+  });
+
+  final TextBaseline editableTextBaseline;
+  final TextBaseline placeholderBaseline;
+  final Widget? placeholder;
+  final Widget editableText;
+
+  @override
+  Iterable<_BaselineAlignedStackSlot> get slots => _BaselineAlignedStackSlot.values;
+
+  @override
+  Widget? childForSlot(_BaselineAlignedStackSlot slot) {
+    return switch (slot) {
+      _BaselineAlignedStackSlot.placeholder => placeholder,
+      _BaselineAlignedStackSlot.editableText => editableText,
+    };
+  }
+
+  @override
+  _RenderBaselineAlignedStack createRenderObject(BuildContext context) {
+    return _RenderBaselineAlignedStack(
+      editableTextBaseline: editableTextBaseline,
+      placeholderBaseline: placeholderBaseline,
+    );
+  }
+
+  @override
+  void updateRenderObject(BuildContext context, _RenderBaselineAlignedStack renderObject) {
+    renderObject
+      ..editableTextBaseline = editableTextBaseline
+      ..placeholderBaseline = placeholderBaseline;
+  }
+}
+
+class _BaselineAlignedStackParentData extends ContainerBoxParentData<RenderBox> {}
+
+class _RenderBaselineAlignedStack extends RenderBox
+    with SlottedContainerRenderObjectMixin<_BaselineAlignedStackSlot, RenderBox> {
+  _RenderBaselineAlignedStack({
+    required TextBaseline editableTextBaseline,
+    required TextBaseline placeholderBaseline,
+  }) : _editableTextBaseline = editableTextBaseline,
+       _placeholderBaseline = placeholderBaseline;
+
+  TextBaseline get editableTextBaseline => _editableTextBaseline;
+  TextBaseline _editableTextBaseline;
+  set editableTextBaseline(TextBaseline value) {
+    if (_editableTextBaseline == value) {
+      return;
+    }
+    _editableTextBaseline = value;
+    markNeedsLayout();
+  }
+
+  TextBaseline get placeholderBaseline => _placeholderBaseline;
+  TextBaseline _placeholderBaseline;
+  set placeholderBaseline(TextBaseline value) {
+    if (_placeholderBaseline == value) {
+      return;
+    }
+    _placeholderBaseline = value;
+    markNeedsLayout();
+  }
+
+  @override
+  void setupParentData(RenderBox child) {
+    if (child.parentData is! _BaselineAlignedStackParentData) {
+      child.parentData = _BaselineAlignedStackParentData();
+    }
+  }
+
+  RenderBox? get _placeholderChild {
+    return childForSlot(_BaselineAlignedStackSlot.placeholder);
+  }
+
+  RenderBox get _editableTextChild {
+    final RenderBox? child = childForSlot(_BaselineAlignedStackSlot.editableText);
+    assert(child != null);
+    return child!;
+  }
+
+  @override
+  void performLayout() {
+    assert(constraints.hasTightWidth);
+    final RenderBox? placeholder = _placeholderChild;
+    final RenderBox editableText = _editableTextChild;
+
+    final _BaselineAlignedStackParentData editableTextParentData =
+        editableText.parentData! as _BaselineAlignedStackParentData;
+    final _BaselineAlignedStackParentData? placeholderParentData =
+        placeholder?.parentData as _BaselineAlignedStackParentData?;
+
+    size = _computeSize(
+      constraints: constraints,
+      layoutChild: ChildLayoutHelper.layoutChild,
+      getBaseline: ChildLayoutHelper.getBaseline,
+    );
+
+    final double editableTextBaselineValue =
+        editableText.getDistanceToBaseline(editableTextBaseline)!;
+    final double? placeholderBaselineValue = placeholder?.getDistanceToBaseline(
+      placeholderBaseline,
+    );
+
+    assert(placeholder != null || placeholderBaselineValue == null);
+    final double placeholderY =
+        placeholderBaselineValue != null
+            ? editableTextBaselineValue - placeholderBaselineValue
+            : 0.0;
+
+    final double offsetYAdjustment = math.max(0, placeholderY);
+    editableTextParentData.offset = Offset(0, offsetYAdjustment);
+    placeholderParentData?.offset = Offset(0, placeholderY + offsetYAdjustment);
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    final RenderBox? placeholder = _placeholderChild;
+    final RenderBox editableText = _editableTextChild;
+
+    if (placeholder != null) {
+      final _BaselineAlignedStackParentData placeholderParentData =
+          placeholder.parentData! as _BaselineAlignedStackParentData;
+      context.paintChild(placeholder, offset + placeholderParentData.offset);
+    }
+
+    final _BaselineAlignedStackParentData editableTextParentData =
+        editableText.parentData! as _BaselineAlignedStackParentData;
+    context.paintChild(editableText, offset + editableTextParentData.offset);
+  }
+
+  @override
+  Size computeDryLayout(covariant BoxConstraints constraints) {
+    return _computeSize(
+      constraints: constraints,
+      layoutChild: ChildLayoutHelper.dryLayoutChild,
+      getBaseline: ChildLayoutHelper.getDryBaseline,
+    );
+  }
+
+  Size _computeSize({
+    required BoxConstraints constraints,
+    required ChildLayouter layoutChild,
+    required ChildBaselineGetter getBaseline,
+  }) {
+    double width = constraints.minWidth;
+    double height = constraints.minHeight;
+
+    final RenderBox editableText = _editableTextChild;
+    final Size editableTextSize = layoutChild(editableText, constraints);
+    final double editableTextBaselineValue =
+        getBaseline(editableText, constraints, editableTextBaseline)!;
+    final double editableTextDescent = editableTextSize.height - editableTextBaselineValue;
+
+    Size? placeholderSize;
+    double? placeholderBaselineValue;
+    final RenderBox? placeholder = _placeholderChild;
+    if (placeholder != null) {
+      placeholderSize = layoutChild(placeholder, constraints);
+      width = math.max(width, placeholderSize.width);
+      placeholderBaselineValue = getBaseline(placeholder, constraints, placeholderBaseline);
+      final double placeholderDescent = placeholderSize.height - placeholderBaselineValue!;
+      // The size is the sum of the placeholder's max ascent and descent and the
+      // editable text's max ascent and descent.
+      final double maxExtentBaseline =
+          math.max(editableTextBaselineValue, placeholderBaselineValue) +
+          math.max(editableTextDescent, placeholderDescent);
+      height = math.max(height, maxExtentBaseline);
+    }
+
+    height = math.max(height, editableTextSize.height);
+    width = math.max(width, editableTextSize.width);
+    final Size size = Size(width, height);
+    assert(size.isFinite);
+    return size;
   }
 }

packages/flutter/test/cupertino/text_field_test.dart
@@ -10286,6 +10286,69 @@ void main() {
     expect(rectWithText.height, greaterThan(100));
   });
 
+  testWidgets('Placeholder is baseline aligned with text', (WidgetTester tester) async {
+    const String placeholderTextContent = 'hint text';
+    const String actualTextContent = 'text';
+    double currentPlaceholderFontSize = 0;
+    double currentTextFontSize = 0;
+    late StateSetter setState;
+
+    await tester.pumpWidget(
+      CupertinoApp(
+        home: Center(
+          child: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setter) {
+              setState = setter;
+              return CupertinoTextField(
+                minLines: 4,
+                maxLines: 6,
+                placeholder: placeholderTextContent,
+                placeholderStyle: TextStyle(fontSize: currentPlaceholderFontSize),
+                style: TextStyle(fontSize: currentTextFontSize),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+
+    Future<void> performBaselineAlignmentCheck(double placeholderSize, double textSize) async {
+      setState(() {
+        currentPlaceholderFontSize = placeholderSize;
+        currentTextFontSize = textSize;
+      });
+      await tester.pump();
+      await tester.enterText(find.byType(CupertinoTextField), actualTextContent);
+      await tester.pump();
+
+      expect(find.text(placeholderTextContent), findsOneWidget);
+      expect(find.text(actualTextContent), findsOneWidget);
+
+      // The placeholder and text are baseline aligned, so some portion of them
+      // extends both above and below the baseline.
+      const double ahemBaselineRatio =
+          0.8; // https://web-platform-tests.org/writing-tests/ahem.html
+      final double placeholderHeightAboveBaseline = placeholderSize * ahemBaselineRatio;
+      final double textHeightAboveBaseline = textSize * ahemBaselineRatio;
+      final double placeholderTopDy = tester.getTopLeft(find.text(placeholderTextContent)).dy;
+      final double textTopDy = tester.getTopLeft(find.text(actualTextContent)).dy;
+
+      expect(
+        textTopDy,
+        closeTo(placeholderTopDy + placeholderHeightAboveBaseline - textHeightAboveBaseline, 1.0),
+      );
+    }
+
+    // Placeholder and text are baseline aligned when the placeholder is larger.
+    await performBaselineAlignmentCheck(40.0, 20.0);
+
+    await tester.enterText(find.byType(CupertinoTextField), '');
+    await tester.pump();
+
+    // Placeholder and text are baseline aligned when the text is larger.
+    await performBaselineAlignmentCheck(20.0, 40.0);
+  });
+
   testWidgets('Start the floating cursor on long tap', (WidgetTester tester) async {
     EditableText.debugDeterministicCursor = true;
     final TextEditingController controller = TextEditingController(text: 'abcd');

flutter/packages

commit 6c71f76c0881f546d85ae4c925ffe4c995449e96

CupertinoSearchTextField and CupertinoSliverNavigationBar.search more fidelity updates (#169708)
This PR does the following:

## Show large title mid-transition if automaticallyImplyLeading is false

(see
https://github.com/flutter/flutter/pull/169708#issuecomment-2922792587)

## Correct color for the CupertinoSearchTextField placeholder 

(see
https://github.com/flutter/flutter/issues/163020#issuecomment-2660522169)

| Dark mode | Light mode | 
| --- | --- |
| <img width="381" alt="placeholder color dark mode"
src="https://github.com/user-attachments/assets/e37e23fa-9f4f-495e-8f02-b9c38a4faffb"
/> | <img width="381" alt="placeholder color light mode"
src="https://github.com/user-attachments/assets/16e24a61-2528-44e0-9afa-8431487cf5ff"
/> |

And also:

- Removes flaky mid-transition goldens
- Fixes a CupertinoTextField crash caused by
https://github.com/flutter/flutter/pull/166952 where size > constraints

Fixes [Improve fidelity of CupertinoSliverNavigationBar.search and
CupertinoSearchTextField](https://github.com/flutter/flutter/issues/163020)

packages/flutter/lib/src/cupertino/nav_bar.dart
@@ -883,12 +883,19 @@ class _CupertinoNavigationBarState extends State<CupertinoNavigationBar> {
 /// It should be placed at top of the screen and automatically accounts for
 /// the iOS status bar.
 ///
+/// This navigation bar is expanded only in portrait orientation. In landscape
+/// mode, the navigation bar remains permanently collapsed. The navigation bar
+/// also collapses when scrolling in portrait mode.
+///
 /// Minimally, a [largeTitle] widget will appear in the middle of the app bar
 /// when the sliver is collapsed and transfer to the area below in larger font
-/// when the sliver is expanded.
+/// when the sliver is expanded. This expanded view will only trigger in
+/// portrait orientation, while in landscape mode the bar will stay in its
+/// collapsed view.
 ///
-/// For advanced uses, an optional [middle] widget can be supplied to show a
-/// different widget in the middle of the navigation bar when the sliver is collapsed.
+/// For advanced uses, an optional [middle] widget
+/// can be supplied to show a different widget in the middle of the navigation
+/// bar when the sliver is collapsed.
 ///
 /// Like [CupertinoNavigationBar], it also supports a [leading] and [trailing]
 /// widget on the static section on top that remains while scrolling.
@@ -1085,10 +1092,12 @@ class CupertinoSliverNavigationBar extends StatefulWidget {
   /// A widget to place in the middle of the static navigation bar instead of
   /// the [largeTitle].
   ///
-  /// This widget is visible in both collapsed and expanded states if
-  /// [alwaysShowMiddle] is true, otherwise just in collapsed state. The text
-  /// supplied in [largeTitle] will no longer appear in collapsed state if a
-  /// [middle] widget is provided.
+  /// If [alwaysShowMiddle] is true, this widget is visible in both the
+  /// collapsed and expanded states of the navigation bar. Else, it is visible
+  /// only in the collapsed state.
+  ///
+  /// If null, [largeTitle] will be displayed in the navigation bar's collapsed
+  /// state.
   final Widget? middle;
 
   /// {@macro flutter.cupertino.CupertinoNavigationBar.trailing}
@@ -3060,7 +3069,6 @@ class _NavigationBarComponentsTransition {
     final KeyedSubtree? bottomLargeTitle =
         bottomComponents.largeTitleKey.currentWidget as KeyedSubtree?;
     final KeyedSubtree? topBackLabel = topComponents.backLabelKey.currentWidget as KeyedSubtree?;
-    final KeyedSubtree? topLeading = topComponents.leadingKey.currentWidget as KeyedSubtree?;
 
     if (bottomLargeTitle == null || !bottomLargeExpanded) {
       return null;
@@ -3093,32 +3101,28 @@ class _NavigationBarComponentsTransition {
       );
     }
 
-    if (topLeading != null) {
-      // Unlike bottom middle, the bottom large title moves when it can't
-      // transition to the top back label position.
-      final RelativeRect from = positionInTransitionBox(
-        bottomComponents.largeTitleKey,
-        from: bottomNavBarBox,
-      );
+    // Unlike bottom middle, the bottom large title moves when it can't
+    // transition to the top back label position.
+    final RelativeRect from = positionInTransitionBox(
+      bottomComponents.largeTitleKey,
+      from: bottomNavBarBox,
+    );
 
-      final RelativeRectTween positionTween = RelativeRectTween(
-        begin: from,
-        end: from.shift(Offset(forwardDirection * bottomNavBarBox.size.width / 4.0, 0.0)),
-      );
+    final RelativeRectTween positionTween = RelativeRectTween(
+      begin: from,
+      end: from.shift(Offset(forwardDirection * bottomNavBarBox.size.width / 4.0, 0.0)),
+    );
 
-      // Just shift slightly towards the trailing edge instead of moving to the
-      // back label position.
-      return PositionedTransition(
-        rect: animation.drive(positionTween),
-        child: FadeTransition(
-          opacity: fadeOutBy(0.4),
-          // Keep the font when transitioning into a non-back-label leading.
-          child: DefaultTextStyle(style: bottomLargeTitleTextStyle!, child: bottomLargeTitle.child),
-        ),
-      );
-    }
-
-    return null;
+    // Just shift slightly towards the trailing edge instead of moving to the
+    // back label position.
+    return PositionedTransition(
+      rect: animation.drive(positionTween),
+      child: FadeTransition(
+        opacity: fadeOutBy(0.4),
+        // Keep the font when transitioning into a non-back-label leading.
+        child: DefaultTextStyle(style: bottomLargeTitleTextStyle!, child: bottomLargeTitle.child),
+      ),
+    );
   }
 
   Widget? get bottomTrailing {

packages/flutter/lib/src/cupertino/search_field.dart
@@ -483,10 +483,17 @@ class _CupertinoSearchTextFieldState extends State<CupertinoSearchTextField> wit
   Widget build(BuildContext context) {
     final String placeholder =
         widget.placeholder ?? CupertinoLocalizations.of(context).searchTextFieldPlaceholderLabel;
-
+    final Color defaultPlaceholderColor = CupertinoDynamicColor.resolve(
+      CupertinoColors.secondaryLabel,
+      context,
+    );
     final TextStyle placeholderStyle =
         widget.placeholderStyle ??
-        TextStyle(color: CupertinoColors.systemGrey.withOpacity(1.0 - _fadeExtent));
+        TextStyle(
+          color: defaultPlaceholderColor.withAlpha(
+            (255 * (defaultPlaceholderColor.a * (1 - _fadeExtent))).round(),
+          ),
+        );
 
     // The icon size will be scaled by a factor of the accessibility text scale,
     // to follow the behavior of `UISearchTextField`.

packages/flutter/lib/src/cupertino/text_field.dart
@@ -1879,6 +1879,6 @@ class _RenderBaselineAlignedStack extends RenderBox
     width = math.max(width, editableTextSize.width);
     final Size size = Size(width, height);
     assert(size.isFinite);
-    return size;
+    return constraints.constrain(size);
   }
 }

packages/flutter/test/cupertino/nav_bar_test.dart
@@ -2846,14 +2846,6 @@ void main() {
     // Tap the search field.
     await tester.tap(find.byType(CupertinoSearchTextField), warnIfMissed: false);
     await tester.pump();
-    // Pump halfway through the animation.
-    await tester.pump(const Duration(milliseconds: 150));
-
-    await expectLater(
-      find.byType(CupertinoSliverNavigationBar),
-      matchesGoldenFile('nav_bar.search.transition_forward.png'),
-    );
-
     // Pump to the end of the animation.
     await tester.pump(const Duration(milliseconds: 300));
 
@@ -2865,14 +2857,6 @@ void main() {
     // Tap the 'Cancel' button to exit the search view.
     await tester.tap(find.widgetWithText(CupertinoButton, 'Cancel'));
     await tester.pump();
-    // Pump halfway through the animation.
-    await tester.pump(const Duration(milliseconds: 150));
-
-    await expectLater(
-      find.byType(CupertinoSliverNavigationBar),
-      matchesGoldenFile('nav_bar.search.transition_backward.png'),
-    );
-
     // Pump for the duration of the search field animation.
     await tester.pump(const Duration(milliseconds: 300));
 

packages/flutter/test/cupertino/nav_bar_transition_test.dart
@@ -1850,6 +1850,33 @@ void main() {
     expect(find.text('Page 2'), findsOneWidget);
   });
 
+  testWidgets('Bottom large title is shown mid-transition when top has no leading', (
+    WidgetTester tester,
+  ) async {
+    setWindowToPortrait(tester);
+    await startTransitionBetween(
+      tester,
+      from: const CupertinoSliverNavigationBar(largeTitle: Text('Page 1')),
+      to: const CupertinoSliverNavigationBar(
+        largeTitle: Text('Page 2'),
+        automaticallyImplyLeading: false,
+      ),
+    );
+
+    // Go to the next page.
+    await tester.pump(const Duration(milliseconds: 600));
+
+    // Start the gesture at the edge of the screen.
+    final TestGesture gesture = await tester.startGesture(const Offset(5.0, 200.0));
+    // Trigger the swipe.
+    await gesture.moveBy(const Offset(200.0, 0.0));
+
+    // Back gestures should trigger and draw the hero transition in the very same
+    // frame (since the "from" route has already moved to reveal the "to" route).
+    await tester.pump();
+    expect(flying(tester, find.text('Page 1')), findsOneWidget);
+  });
+
   testWidgets('Back label is not clipped mid-transition', (WidgetTester tester) async {
     const String label = 'backbackback';
     await startTransitionBetween(

packages/flutter/test/cupertino/search_field_test.dart
@@ -129,7 +129,7 @@ void main() {
     );
 
     Text placeholder = tester.widget(find.text('Search'));
-    expect(placeholder.style!.color!.value, CupertinoColors.systemGrey.darkColor.value);
+    expect(placeholder.style!.color!.value, CupertinoColors.secondaryLabel.darkColor.value);
 
     await tester.pumpAndSettle();
 
@@ -141,7 +141,7 @@ void main() {
     );
 
     placeholder = tester.widget(find.text('Search'));
-    expect(placeholder.style!.color!.value, CupertinoColors.systemGrey.color.value);
+    expect(placeholder.style!.color!.value, CupertinoColors.secondaryLabel.color.value);
   });
 
   testWidgets("placeholderStyle modifies placeholder's style and doesn't affect text's style", (
@@ -623,7 +623,7 @@ void main() {
     expect(suffixIconFinder, findsOneWidget);
     expect(placeholderFinder, findsOneWidget);
 
-    // Initially, the icons and placeholder text are fully opaque.
+    // Initially, the icons are fully opaque.
     expect(
       tester
           .widget<Opacity>(find.ancestor(of: prefixIconFinder, matching: find.byType(Opacity)))
@@ -636,7 +636,8 @@ void main() {
           .opacity,
       equals(1.0),
     );
-    expect(tester.widget<Text>(placeholderFinder).style?.color?.a, equals(1.0));
+    // The default placeholder color is semi-transparent.
+    expect(tester.widget<Text>(placeholderFinder).style?.color?.a, equals(0.6));
 
     final double searchTextFieldHeight = tester.getSize(searchTextFieldFinder).height;
 