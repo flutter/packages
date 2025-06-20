5af21003570c376be861ea9bf6b921cb1d051019
Camille Simon <camillesimon@google.com>
2025-06-20 09:24:43 -0700

    update pr based on engine changes + add tests

--- packages/camera/camera_android_camerax/android/src/main/java/io/flutter/plugins/camerax/PreviewProxyApi.java
+++ packages/camera/camera_android_camerax/android/src/main/java/io/flutter/plugins/camerax/PreviewProxyApi.java
@@ -126,11 +126,6 @@ class PreviewProxyApi extends PigeonApiPreview {
           flutterSurface,
           Executors.newSingleThreadExecutor(),
           (result) -> {
-            // RACE CONDITION EXPLANATION for https://github.com/flutter/flutter/pull/169899:
-            // Sometimes, this callback is called after the next SurfaceRequest comes in,
-            // making a call on line 122 to get a `Surface`, which is the same `Surface` that was (potentially)
-            // successfully used and should not be re-used.
-            //
             // See
             // https://developer.android.com/reference/androidx/camera/core/SurfaceRequest.Result
             // for documentation.
