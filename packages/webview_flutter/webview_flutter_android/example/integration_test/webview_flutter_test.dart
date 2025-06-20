f4703af220092499d0d85733d752b4dc9b680fc6
jesswrd <jesswon@google.com>
2025-06-20 14:21:08 -0400

    should fix firebase test

--- packages/webview_flutter/webview_flutter_android/example/integration_test/webview_flutter_test.dart
+++ packages/webview_flutter/webview_flutter_android/example/integration_test/webview_flutter_test.dart
@@ -535,9 +535,8 @@ Future<void> main() async {
       // Makes sure we get the correct event that indicates the video is actually playing.
       await videoPlaying.future;
 
-      final String fullScreenString = await controller
-          .runJavaScriptReturningResult('isFullScreen();') as String;
-      final bool fullScreen = fullScreenString == 'true';
+      final bool fullScreen = await controller
+          .runJavaScriptReturningResult('isFullScreen();') as bool;
 
       expect(fullScreen, false);
     });
