10503f583789e0156c7d7318d0d705578e2a262b
Ortes <malo.allee@gmail.com>
2025-07-07 17:04:08 +0200

    Format code in video_player.dart

--- packages/video_player/video_player_web/lib/src/video_player.dart
+++ packages/video_player/video_player_web/lib/src/video_player.dart
@@ -33,7 +33,8 @@ const Map<int, String> _kErrorValueToErrorDescription = <int, String>{
 
 // The default error message, when the error is an empty string
 // See: https://developer.mozilla.org/en-US/docs/Web/API/MediaError/message
-const String _kDefaultErrorMessage = 'No further diagnostic information can be determined or provided.';
+const String _kDefaultErrorMessage =
+    'No further diagnostic information can be determined or provided.';
 
 /// Wraps a [web.HTMLVideoElement] so its API complies with what is expected by the plugin.
 class VideoPlayer {
@@ -305,7 +306,8 @@ class VideoPlayer {
 
   // Sends an [VideoEventType.initialized] [VideoEvent] with info about the wrapped video.
   void _sendInitialized() {
-    final Duration? duration = convertNumVideoDurationToPluginDuration(_videoElement.duration);
+    final Duration? duration =
+        convertNumVideoDurationToPluginDuration(_videoElement.duration);
 
     final Size? size = _videoElement.videoHeight.isFinite
         ? Size(
@@ -332,7 +334,9 @@ class VideoPlayer {
     if (_isBuffering != buffering) {
       _isBuffering = buffering;
       _eventController.add(VideoEvent(
-        eventType: _isBuffering ? VideoEventType.bufferingStart : VideoEventType.bufferingEnd,
+        eventType: _isBuffering
+            ? VideoEventType.bufferingStart
+            : VideoEventType.bufferingEnd,
       ));
     }
   }
