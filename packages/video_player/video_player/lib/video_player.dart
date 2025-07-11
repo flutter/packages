9ece2949b1b98ace01825b459bd035f93999cb8f
Konoha-orz <517136675@qq.com>
2025-07-11 17:24:27 +0800

    [video_player]Fix video_player Windows platform loop seekTo issue on video complete
    video_player Windows platform loop seekTo issue on video complete
    
    - Prevent unintended seekTo loop when video ends on Windows platform
    - Add platform-specific check to handle video completion correctly
    - Ensure smooth playback termination without redundant seek operations

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -512,7 +512,12 @@ class VideoPlayerController extends ValueNotifier<VideoPlayerValue> {
           // position=value.duration. Instead of setting the values directly,
           // we use pause() and seekTo() to ensure the platform stops playing
           // and seeks to the last frame of the video.
-          pause().then((void pauseResult) => seekTo(value.duration));
+          if(Platform.isWindows){
+            // windows implents seekTo End will loop buffer, so we need to pause and seek to the end
+            pause();
+          }else{
+            pause().then((void pauseResult) => seekTo(value.duration));
+          } 
           value = value.copyWith(isCompleted: true);
         case VideoEventType.bufferingUpdate:
           value = value.copyWith(buffered: event.buffered);
