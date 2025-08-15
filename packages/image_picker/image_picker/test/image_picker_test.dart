69b83d1ea64b8ae224160160ec839ddf8bb5fc13
Stuart Morgan <stuartmorgan@google.com>
2025-08-15 15:12:34 -0400

    Fix the test bug caught by Gemini

--- packages/image_picker/image_picker/test/image_picker_test.dart
+++ packages/image_picker/image_picker/test/image_picker_test.dart
@@ -607,9 +607,9 @@ void main() {
                     )
                     .having(
                       (MultiImagePickerOptions options) =>
-                          options.imageOptions.maxWidth,
+                          options.imageOptions.maxHeight,
                       'maxHeight',
-                      equals(10.0),
+                      equals(20.0),
                     )
                     .having(
                       (MultiImagePickerOptions options) =>
@@ -631,9 +631,9 @@ void main() {
                     )
                     .having(
                       (MultiImagePickerOptions options) =>
-                          options.imageOptions.maxWidth,
+                          options.imageOptions.maxHeight,
                       'maxHeight',
-                      equals(10.0),
+                      equals(20.0),
                     )
                     .having(
                       (MultiImagePickerOptions options) =>
