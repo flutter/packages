eb0173082587bd55514e4fe4321a1dd8706f1bf2
Jenn Magder <magder@google.com>
2025-08-15 15:30:42 -0700

    Change plugin to see this working

--- packages/camera/camera/lib/camera.dart
+++ packages/camera/camera/lib/camera.dart
@@ -15,6 +15,8 @@ export 'package:camera_platform_interface/camera_platform_interface.dart'
         ResolutionPreset,
         XFile;
 
+
+
 export 'src/camera_controller.dart';
 export 'src/camera_image.dart';
 export 'src/camera_preview.dart';
