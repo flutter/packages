1f977e74d20e0d37f7f1908949ff6a67498216e0
Jenn Magder <magder@google.com>
2025-08-15 15:58:13 -0700

    Revert "Change plugin to see this working"
    This reverts commit eb0173082587bd55514e4fe4321a1dd8706f1bf2.

--- packages/camera/camera/lib/camera.dart
+++ packages/camera/camera/lib/camera.dart
@@ -15,8 +15,6 @@ export 'package:camera_platform_interface/camera_platform_interface.dart'
         ResolutionPreset,
         XFile;
 
-
-
 export 'src/camera_controller.dart';
 export 'src/camera_image.dart';
 export 'src/camera_preview.dart';
