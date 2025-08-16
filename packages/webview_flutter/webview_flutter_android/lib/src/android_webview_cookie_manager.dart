eb33afdda226129890de5b9ddbb30157a1eed758
Stuart Morgan <stuartmorgan@google.com>
2025-08-16 13:51:41 -0400

    Re-add camera and webview: autoformat

--- packages/webview_flutter/webview_flutter_android/lib/src/android_webview_cookie_manager.dart
+++ packages/webview_flutter/webview_flutter_android/lib/src/android_webview_cookie_manager.dart
@@ -25,7 +25,8 @@ class AndroidWebViewCookieManagerCreationParams
 
   /// Creates a [AndroidWebViewCookieManagerCreationParams] instance based on [PlatformWebViewCookieManagerCreationParams].
   factory AndroidWebViewCookieManagerCreationParams.fromPlatformWebViewCookieManagerCreationParams(
-      PlatformWebViewCookieManagerCreationParams params) {
+    PlatformWebViewCookieManagerCreationParams params,
+  ) {
     return AndroidWebViewCookieManagerCreationParams._(params);
   }
 }
@@ -36,13 +37,14 @@ class AndroidWebViewCookieManager extends PlatformWebViewCookieManager {
   AndroidWebViewCookieManager(
     PlatformWebViewCookieManagerCreationParams params, {
     CookieManager? cookieManager,
-  })  : _cookieManager = cookieManager ?? CookieManager.instance,
-        super.implementation(
-          params is AndroidWebViewCookieManagerCreationParams
-              ? params
-              : AndroidWebViewCookieManagerCreationParams
-                  .fromPlatformWebViewCookieManagerCreationParams(params),
-        );
+  }) : _cookieManager = cookieManager ?? CookieManager.instance,
+       super.implementation(
+         params is AndroidWebViewCookieManagerCreationParams
+             ? params
+             : AndroidWebViewCookieManagerCreationParams.fromPlatformWebViewCookieManagerCreationParams(
+               params,
+             ),
+       );
 
   final CookieManager _cookieManager;
 
@@ -55,7 +57,8 @@ class AndroidWebViewCookieManager extends PlatformWebViewCookieManager {
   Future<void> setCookie(WebViewCookie cookie) {
     if (!_isValidPath(cookie.path)) {
       throw ArgumentError(
-          'The path property for the provided cookie was not given a legal value.');
+        'The path property for the provided cookie was not given a legal value.',
+      );
     }
     return _cookieManager.setCookie(
       cookie.domain,
@@ -81,8 +84,10 @@ class AndroidWebViewCookieManager extends PlatformWebViewCookieManager {
     bool accept,
   ) {
     // ignore: invalid_use_of_protected_member
-    final WebView webView = _cookieManager.pigeon_instanceManager
-        .getInstanceWithWeakReference(controller.webViewIdentifier)!;
+    final WebView webView =
+        _cookieManager.pigeon_instanceManager.getInstanceWithWeakReference(
+          controller.webViewIdentifier,
+        )!;
     return _cookieManager.setAcceptThirdPartyCookies(webView, accept);
   }
 }
