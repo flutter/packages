99ab3c3c259122607fe796586847287ee9b10bca
Omar Hanafy <omar_hanafy@icloud.com>
2025-08-21 00:46:34 +0300

    [go_router] Fix license headers to match repository standards

--- packages/go_router/example/lib/top_level_on_enter.dart
+++ packages/go_router/example/lib/top_level_on_enter.dart
@@ -1,5 +1,7 @@
-// Copyright 2013 The Flutter Authors.
-// Use of this source code is governed by a BSD-style license.
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
