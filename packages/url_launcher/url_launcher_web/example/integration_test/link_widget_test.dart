4ac086ddf101f6b60a4f12e0c7544ce9f8d606b2
pedromassango <pedromassango.developer@gmail.com>
2025-08-26 23:17:38 +0200

    nit

--- packages/url_launcher/url_launcher_web/example/integration_test/link_widget_test.dart
+++ packages/url_launcher/url_launcher_web/example/integration_test/link_widget_test.dart
@@ -8,7 +8,6 @@ import 'dart:typed_data';
 import 'dart:ui_web' as ui_web;
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:url_launcher_platform_interface/link.dart';
