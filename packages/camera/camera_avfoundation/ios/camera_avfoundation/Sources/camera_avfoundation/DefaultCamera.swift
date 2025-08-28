0635db4bd429d41e74e4df759dcbb53c7f484c7e
Julien Delarbre <julien@delarbre.biz>
2025-08-28 22:18:49 +0200

    Fix bug when enableAudio is False and video recording is requested

--- packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/DefaultCamera.swift
+++ packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/DefaultCamera.swift
@@ -94,7 +94,7 @@ final class DefaultCamera: FLTCam, Camera {
 
   func setUpCaptureSessionForAudioIfNeeded() {
     // Don't setup audio twice or we will lose the audio.
-    guard !mediaSettings.enableAudio || !isAudioSetup else { return }
+    guard mediaSettings.enableAudio && !isAudioSetup else { return }
 
     let audioDevice = audioCaptureDeviceFactory()
     do {
