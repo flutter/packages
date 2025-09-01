3f92df89ddeb9c0265ba8915799af6f5be09d10a
Moritz <mosum@google.com>
2025-09-01 16:35:04 +0200

    reset

--- packages/example_svg_transformer/lib/main.dart
+++ packages/example_svg_transformer/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: VectorGraphic(
             loader: AssetBytesLoader(
-              'assets/example',
+              'example',
               packageName: 'example_svg_transformer',
             ),
           ),
