0a2bcf0192084973702cb218f4f779c2eed6b6ff
Moritz <mosum@google.com>
2025-09-01 13:33:12 +0200

    fix

--- packages/vector_graphics_compiler/lib/src/util/isolate_processor.dart
+++ packages/vector_graphics_compiler/lib/src/util/isolate_processor.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// ignore_for_file: avoid_print
+// ignore_for_file: avoid_print, public_member_api_docs
 
 import 'dart:async';
 import 'dart:io';
