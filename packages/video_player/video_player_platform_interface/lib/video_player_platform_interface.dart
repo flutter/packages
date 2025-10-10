de7d679870f7885646b2ac37633afb497d67dd2c
Arvid Nyström <apps@arvidnystrom.se>
2025-10-10 18:48:06 +0200

    [video_player_platform_interface] Adds setter for allowing background playback  This PR is a subset of #9212 - it only adds platform interface changes.  Partially resolves #62739. (#10054)
    Adds setter for allowing background playback
    
    This PR is a subset of #9212 - it only adds platform interface changes.
    
    Partially resolves #62739.
    
    ## Pre-Review Checklist
    
    **Note**: The Flutter team is currently trialing the use of [Gemini Code Assist for GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code). Comments from the `gemini-code-assist` bot should not be taken as authoritative feedback from the Flutter team. If you find its comments useful you can update your code accordingly, but if you are unsure or disagree with the feedback, please feel free to wait for a Flutter team member's review for guidance on which automated comments should be addressed.
    
    [^1]: Regular contributors who have demonstrated familiarity with the repository guidelines only need to comment if the PR is not auto-exempted by repo tooling.

--- packages/video_player/video_player_platform_interface/lib/video_player_platform_interface.dart
+++ packages/video_player/video_player_platform_interface/lib/video_player_platform_interface.dart
@@ -117,6 +117,13 @@ abstract class VideoPlayerPlatform extends PlatformInterface {
     throw UnimplementedError('setMixWithOthers() has not been implemented.');
   }
 
+  /// Sets whether the video should continue to play in the background.
+  Future<void> setAllowBackgroundPlayback(bool allowBackgroundPlayback) {
+    throw UnimplementedError(
+      'setAllowBackgroundPlayback() has not been implemented.',
+    );
+  }
+
   /// Sets additional options on web.
   Future<void> setWebOptions(int playerId, VideoPlayerWebOptions options) {
     throw UnimplementedError('setWebOptions() has not been implemented.');
