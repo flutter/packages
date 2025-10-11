cce216caa8399da68790a39d3904438046609c73
Flop <kukuzuo@gmail.com>
2025-10-11 21:14:07 +0800

    [image_picker] Improve test matcher

--- packages/image_picker/image_picker_android/test/image_picker_android_test.dart
+++ packages/image_picker/image_picker_android/test/image_picker_android_test.dart
@@ -778,49 +778,35 @@ void main() {
     });
 
     test('does not accept an invalid limit argument', () {
+      final Matcher throwsLimitArgumentError = throwsA(
+        isA<ArgumentError>()
+            .having((ArgumentError error) => error.name, 'name', 'limit')
+            .having(
+              (ArgumentError error) => error.message,
+              'message',
+              'cannot be lower than 2',
+            ),
+      );
+
       expect(
         () => picker.getMedia(
           options: const MediaOptions(allowMultiple: true, limit: -1),
         ),
-        throwsA(
-          isA<ArgumentError>()
-              .having((ArgumentError error) => error.name, 'name', 'limit')
-              .having(
-                (ArgumentError error) => error.message,
-                'message',
-                'cannot be lower than 2',
-              ),
-        ),
+        throwsLimitArgumentError,
       );
 
       expect(
         () => picker.getMedia(
           options: const MediaOptions(allowMultiple: true, limit: 0),
         ),
-        throwsA(
-          isA<ArgumentError>()
-              .having((ArgumentError error) => error.name, 'name', 'limit')
-              .having(
-                (ArgumentError error) => error.message,
-                'message',
-                'cannot be lower than 2',
-              ),
-        ),
+        throwsLimitArgumentError,
       );
 
       expect(
         () => picker.getMedia(
           options: const MediaOptions(allowMultiple: true, limit: 1),
         ),
-        throwsA(
-          isA<ArgumentError>()
-              .having((ArgumentError error) => error.name, 'name', 'limit')
-              .having(
-                (ArgumentError error) => error.message,
-                'message',
-                'cannot be lower than 2',
-              ),
-        ),
+        throwsLimitArgumentError,
       );
     });
 
