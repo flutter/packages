bfdb21c7dd57ba6e10ea7a364e07f1553a753fed
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2025-11-04 10:46:21 -0500

    start of interface updateF

--- packages/cross_file/cross_file_platform_interface/lib/cross_file_platform_interface.dart
+++ packages/cross_file/cross_file_platform_interface/lib/cross_file_platform_interface.dart
@@ -1,5 +1,37 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+// Platform Interface
+import 'package:flutter/foundation.dart';
+
+abstract base class CrossFilePlatform {
+  static CrossFilePlatform? instance;
+
+  PlatformXFile createPlatformXFile(
+    PlatformBatteryManagerCreationParams params,
+  );
+}
+
+@immutable
+base class PlatformBatteryManagerCreationParams {
+  const PlatformBatteryManagerCreationParams();
+}
+
+base class PlatformXFile {
+  factory PlatformXFile(PlatformBatteryManagerCreationParams params) {
+    assert(CrossFilePlatform.instance != null);
+    final PlatformXFile implementation = CrossFilePlatform.instance!
+        .createPlatformBatteryManager(params);
+    return implementation;
+  }
+
+  @protected
+  PlatformBatteryManager.implementation(this.params);
+
+  final PlatformBatteryManagerCreationParams params;
+
+  PlatformBatteryManagerExtension? get extension => null;
+
+  Future<int> getLevel() {
+    throw UnimplementedError(
+      'getLevel is not implemented on the current platform',
+    );
+  }
 }
