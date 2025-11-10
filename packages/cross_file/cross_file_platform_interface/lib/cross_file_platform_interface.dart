7342a96e737def8c249374d56249b6bf1cccb84c
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2025-11-10 13:19:23 -0500

    base xfile returns path

--- packages/cross_file/cross_file_platform_interface/lib/cross_file_platform_interface.dart
+++ packages/cross_file/cross_file_platform_interface/lib/cross_file_platform_interface.dart
@@ -1,37 +1,5 @@
 // Platform Interface
-import 'package:flutter/foundation.dart';
 
-abstract base class CrossFilePlatform {
-  static CrossFilePlatform? instance;
-
-  PlatformXFile createPlatformXFile(
-    PlatformBatteryManagerCreationParams params,
-  );
-}
-
-@immutable
-base class PlatformBatteryManagerCreationParams {
-  const PlatformBatteryManagerCreationParams();
-}
-
-base class PlatformXFile {
-  factory PlatformXFile(PlatformBatteryManagerCreationParams params) {
-    assert(CrossFilePlatform.instance != null);
-    final PlatformXFile implementation = CrossFilePlatform.instance!
-        .createPlatformBatteryManager(params);
-    return implementation;
-  }
-
-  @protected
-  PlatformBatteryManager.implementation(this.params);
-
-  final PlatformBatteryManagerCreationParams params;
-
-  PlatformBatteryManagerExtension? get extension => null;
-
-  Future<int> getLevel() {
-    throw UnimplementedError(
-      'getLevel is not implemented on the current platform',
-    );
-  }
+abstract base class XFile {
+  String get path;
 }
