acb77d1735bd6bbe887744ba8c69adc4890ddda4
tarrinneal <tarrinneal@gmail.com>
2025-11-11 12:33:30 -0800

    format

--- packages/pigeon/platform_tests/shared_test_plugin_code/lib/src/generated/ni_tests.gen.m
+++ packages/pigeon/platform_tests/shared_test_plugin_code/lib/src/generated/ni_tests.gen.m
@@ -1,6 +1,6 @@
+#include <stdint.h>
 #import <Foundation/Foundation.h>
 #import <objc/message.h>
-#include <stdint.h>
 #import "../../../../test_plugin/example/temp/test_plugin.h"
 
 #if !__has_feature(objc_arc)
@@ -12,10 +12,10 @@
 
 typedef struct {
   int64_t version;
-  void *(*newWaiter)(void);
-  void (*awaitWaiter)(void *);
-  void *(*currentIsolate)(void);
-  void (*enterIsolate)(void *);
+  void* (*newWaiter)(void);
+  void (*awaitWaiter)(void*);
+  void* (*currentIsolate)(void);
+  void (*enterIsolate)(void*);
   void (*exitIsolate)(void);
   int64_t (*getMainPortId)(void);
   bool (*getCurrentThreadOwnsIsolate)(int64_t);
@@ -23,34 +23,36 @@ typedef struct {
 
 id objc_retainBlock(id);
 
-#define BLOCKING_BLOCK_IMPL(ctx, BLOCK_SIG, INVOKE_DIRECT, INVOKE_LISTENER)                      \
-  assert(ctx->version >= 1);                                                                     \
-  void *targetIsolate = ctx->currentIsolate();                                                   \
-  int64_t targetPort = ctx->getMainPortId == NULL ? 0 : ctx->getMainPortId();                    \
-  return BLOCK_SIG {                                                                             \
-    void *currentIsolate = ctx->currentIsolate();                                                \
-    bool mayEnterIsolate = currentIsolate == NULL && ctx->getCurrentThreadOwnsIsolate != NULL && \
-                           ctx->getCurrentThreadOwnsIsolate(targetPort);                         \
-    if (currentIsolate == targetIsolate || mayEnterIsolate) {                                    \
-      if (mayEnterIsolate) {                                                                     \
-        ctx->enterIsolate(targetIsolate);                                                        \
-      }                                                                                          \
-      INVOKE_DIRECT;                                                                             \
-      if (mayEnterIsolate) {                                                                     \
-        ctx->exitIsolate();                                                                      \
-      }                                                                                          \
-    } else {                                                                                     \
-      void *waiter = ctx->newWaiter();                                                           \
-      INVOKE_LISTENER;                                                                           \
-      ctx->awaitWaiter(waiter);                                                                  \
-    }                                                                                            \
+#define BLOCKING_BLOCK_IMPL(ctx, BLOCK_SIG, INVOKE_DIRECT, INVOKE_LISTENER)    \
+  assert(ctx->version >= 1);                                                   \
+  void* targetIsolate = ctx->currentIsolate();                                 \
+  int64_t targetPort = ctx->getMainPortId == NULL ? 0 : ctx->getMainPortId();  \
+  return BLOCK_SIG {                                                           \
+    void* currentIsolate = ctx->currentIsolate();                              \
+    bool mayEnterIsolate =                                                     \
+        currentIsolate == NULL &&                                              \
+        ctx->getCurrentThreadOwnsIsolate != NULL &&                            \
+        ctx->getCurrentThreadOwnsIsolate(targetPort);                          \
+    if (currentIsolate == targetIsolate || mayEnterIsolate) {                  \
+      if (mayEnterIsolate) {                                                   \
+        ctx->enterIsolate(targetIsolate);                                      \
+      }                                                                        \
+      INVOKE_DIRECT;                                                           \
+      if (mayEnterIsolate) {                                                   \
+        ctx->exitIsolate();                                                    \
+      }                                                                        \
+    } else {                                                                   \
+      void* waiter = ctx->newWaiter();                                         \
+      INVOKE_LISTENER;                                                         \
+      ctx->awaitWaiter(waiter);                                                \
+    }                                                                          \
   };
 
-typedef id (^ProtocolTrampoline)(void *sel);
-__attribute__((visibility("default"))) __attribute__((used)) id
-_test_plugin_protocolTrampoline_1mbt9g9(id target, void *sel) {
-  return ((ProtocolTrampoline)((id(*)(id, SEL, SEL))objc_msgSend)(
-      target, @selector(getDOBJCDartProtocolMethodForSelector:), sel))(sel);
+
+typedef id  (^ProtocolTrampoline)(void * sel);
+__attribute__((visibility("default"))) __attribute__((used))
+id  _test_plugin_protocolTrampoline_1mbt9g9(id target, void * sel) {
+  return ((ProtocolTrampoline)((id (*)(id, SEL, SEL))objc_msgSend)(target, @selector(getDOBJCDartProtocolMethodForSelector:), sel))(sel);
 }
 #undef BLOCKING_BLOCK_IMPL
 
