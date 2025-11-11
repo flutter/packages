326f932579ce78efcdc890ef815ad8b1bd02e808
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2025-11-10 19:20:03 -0500

    create platform interface

--- packages/cross_file/cross_file_platform_interface/lib/cross_file_platform_interface.dart
+++ packages/cross_file/cross_file_platform_interface/lib/cross_file_platform_interface.dart
@@ -1,5 +1,29 @@
 // Platform Interface
 
-abstract base class XFile {
-  String get path;
+import 'dart:convert';
+import 'dart:typed_data';
+
+abstract base class XFilePlatform {
+  static XFilePlatform? instance;
+
+  PlatformXFile createPlatformXFile(String path);
+}
+
+abstract base class PlatformXFile {
+  PlatformXFile(this.path);
+
+  final String path;
+
+  Future<DateTime> lastModified();
+
+  /// Reads the entire file contents as a list of bytes.
+  Future<Uint8List> readAsBytes();
+
+  /// Reads the entire file contents as a string using the given Encoding.
+  Future<String> readAsString({Encoding encoding = utf8});
+
+  /// The length of the file.
+  Future<int> length();
+
+  Stream<Uint8List> openRead([int? start, int? end]);
 }
