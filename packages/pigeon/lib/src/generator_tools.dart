d07dabced85cd9736ae08b829b4746e61a9f607e
stuartmorgan-g <stuartmorgan@google.com>
2025-11-11 23:36:44 -0500

    [pigeon] Deprecate dartHostTestHandler (#10403)
    Deprecates `dartHostTestHandler`, in preparation for removing sometime in 2026 if no significant new information comes to light.
    
    Part of https://github.com/flutter/flutter/issues/178322
    
    ## Pre-Review Checklist
    
    [^1]: Regular contributors who have demonstrated familiarity with the repository guidelines only need to comment if the PR is not auto-exempted by repo tooling.

--- packages/pigeon/lib/src/generator_tools.dart
+++ packages/pigeon/lib/src/generator_tools.dart
@@ -15,7 +15,7 @@ import 'generator.dart';
 /// The current version of pigeon.
 ///
 /// This must match the version in pubspec.yaml.
-const String pigeonVersion = '26.0.5';
+const String pigeonVersion = '26.1.0';
 
 /// Read all the content from [stdin] to a String.
 String readStdin() {
