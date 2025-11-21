6a84a5421e84466b01ca71db20ab31cfc088e65d
Daniel Ferreira <danferreiradev@gmail.com>
2025-11-21 15:35:31 -0300

    [file_selector] Add parameter to control whether the new folders button is visible in the file dialog (#9965)
    Adds a new parameter `canCreateDirectories` to `FileDialogOptions` to control the visibility of the New Folder button in file dialogs on supported platforms.
    
    Currently only configurable on Linux and macOS
    
    This is the "Combined PR"
    Let me know if we are okay with this approach so I can open a new PR covering the interface changes
    
    Fixes: flutter/flutter#141339
    
    ## Pre-Review Checklist
    
    **Note**: The Flutter team is currently trialing the use of [Gemini Code Assist for GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code). Comments from the `gemini-code-assist` bot should not be taken as authoritative feedback from the Flutter team. If you find its comments useful you can update your code accordingly, but if you are unsure or disagree with the feedback, please feel free to wait for a Flutter team member's review for guidance on which automated comments should be addressed.
    
    [^1]: Regular contributors who have demonstrated familiarity with the repository guidelines only need to comment if the PR is not auto-exempted by repo tooling.

--- packages/file_selector/file_selector/test/file_selector_test.dart
+++ packages/file_selector/file_selector/test/file_selector_test.dart
@@ -12,6 +12,7 @@ void main() {
   const String initialDirectory = '/home/flutteruser';
   const String confirmButtonText = 'Use this profile picture';
   const String suggestedName = 'suggested_name';
+
   const List<XTypeGroup> acceptedTypeGroups = <XTypeGroup>[
     XTypeGroup(
       label: 'documents',
@@ -227,6 +228,18 @@ void main() {
       );
       expect(location?.path, expectedSavePath);
     });
+
+    test('sets the directory creation control flag', () async {
+      const bool canCreateDirectories = false;
+      fakePlatformImplementation
+        ..setExpectations(canCreateDirectories: canCreateDirectories)
+        ..setPathsResponse(<String>[expectedSavePath]);
+
+      final FileSaveLocation? location = await getSaveLocation(
+        canCreateDirectories: canCreateDirectories,
+      );
+      expect(location?.path, expectedSavePath);
+    });
   });
 
   group('getDirectoryPath', () {
@@ -275,6 +288,18 @@ void main() {
 
       final String? directoryPath = await getDirectoryPath(
         confirmButtonText: confirmButtonText,
+      );
+      expect(directoryPath, expectedDirectoryPath);
+    });
+
+    test('sets the directory creation control flag', () async {
+      const bool canCreateDirectories = true;
+      fakePlatformImplementation
+        ..setExpectations(canCreateDirectories: canCreateDirectories)
+        ..setPathsResponse(<String>[expectedDirectoryPath]);
+
+      final String? directoryPath = await getDirectoryPath(
+        canCreateDirectories: canCreateDirectories,
       );
       expect(directoryPath, expectedDirectoryPath);
     });
@@ -330,6 +355,17 @@ void main() {
       );
       expect(directoryPaths, expectedDirectoryPaths);
     });
+    test('sets the directory creation control flag', () async {
+      const bool canCreateDirectories = true;
+      fakePlatformImplementation
+        ..setExpectations(canCreateDirectories: canCreateDirectories)
+        ..setPathsResponse(expectedDirectoryPaths);
+
+      final List<String?> directoryPaths = await getDirectoryPaths(
+        canCreateDirectories: canCreateDirectories,
+      );
+      expect(directoryPaths, expectedDirectoryPaths);
+    });
   });
 }
 
@@ -341,6 +377,7 @@ class FakeFileSelector extends Fake
   String? initialDirectory;
   String? confirmButtonText;
   String? suggestedName;
+  bool? canCreateDirectories;
   // Return values.
   List<XFile>? files;
   List<String>? paths;
@@ -351,11 +388,13 @@ class FakeFileSelector extends Fake
     String? initialDirectory,
     String? suggestedName,
     String? confirmButtonText,
+    bool? canCreateDirectories,
   }) {
     this.acceptedTypeGroups = acceptedTypeGroups;
     this.initialDirectory = initialDirectory;
     this.suggestedName = suggestedName;
     this.confirmButtonText = confirmButtonText;
+    this.canCreateDirectories = canCreateDirectories;
   }
 
   // ignore: use_setters_to_change_properties
@@ -435,9 +474,19 @@ class FakeFileSelector extends Fake
   Future<String?> getDirectoryPath({
     String? initialDirectory,
     String? confirmButtonText,
+    bool canCreateDirectories = true,
   }) async {
     expect(initialDirectory, this.initialDirectory);
     expect(confirmButtonText, this.confirmButtonText);
+    expect(canCreateDirectories, this.canCreateDirectories);
+    return paths?[0];
+  }
+
+  @override
+  Future<String?> getDirectoryPathWithOptions(FileDialogOptions options) async {
+    expect(options.initialDirectory, initialDirectory);
+    expect(options.confirmButtonText, confirmButtonText);
+    expect(options.canCreateDirectories, canCreateDirectories);
     return paths?[0];
   }
 
@@ -445,9 +494,21 @@ class FakeFileSelector extends Fake
   Future<List<String>> getDirectoryPaths({
     String? initialDirectory,
     String? confirmButtonText,
+    bool canCreateDirectories = true,
   }) async {
     expect(initialDirectory, this.initialDirectory);
     expect(confirmButtonText, this.confirmButtonText);
+    expect(canCreateDirectories, this.canCreateDirectories);
+    return paths!;
+  }
+
+  @override
+  Future<List<String>> getDirectoryPathsWithOptions(
+    FileDialogOptions options,
+  ) async {
+    expect(options.initialDirectory, initialDirectory);
+    expect(options.confirmButtonText, confirmButtonText);
+    expect(options.canCreateDirectories, canCreateDirectories);
     return paths!;
   }
 }
