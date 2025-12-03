89e5820b23ba61a318e0854b5ba43d98ae3865e6
Marty Fuhry <martyfuhry@gmail.com>
2025-12-03 15:40:21 -0500

    Fixes comments and adds unit tests.

--- packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/map_color_scheme.dart
+++ packages/google_maps_flutter/google_maps_flutter_platform_interface/lib/src/types/map_color_scheme.dart
@@ -2,14 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// The color scheme of the map to be used with cloud styles
+/// The color scheme of the map to be used with cloud styles.
 enum MapColorScheme {
-  /// The light color scheme for the map
+  /// The light color scheme for the map.
   light,
 
-  /// The dark color scheme for the map
+  /// The dark color scheme for the map.
   dark,
 
-  /// The system default color scheme for the map
+  /// The system default color scheme for the map.
   followSystem,
 }
