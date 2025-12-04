a70c189dc3d5e449f5d26bb1ccc03889a29bc4e8
Kate Lovett <katelovett@google.com>
2025-12-04 14:51:25 -0600

    Fix web

--- packages/two_dimensional_scrollables/test/goldens_web.dart
+++ packages/two_dimensional_scrollables/test/goldens_web.dart
@@ -5,5 +5,5 @@
 import 'dart:async';
 
 // package:flutter_goldens is not used as part of the test process for web.
-Future<void> testExecutable(FutureOr<void> Function() testMain) async =>
+Future<void> testExecutable(FutureOr<void> Function() testMain, {String? namePrefix}) async =>
     testMain();
