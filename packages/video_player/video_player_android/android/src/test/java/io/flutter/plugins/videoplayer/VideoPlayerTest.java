cfbdb9b84b75ef3f3c9970d76c3a92ec0490910c
nateshmbhat <nateshmbhat1@gmail.com>
2025-12-04 22:00:59 +0530

    fix(video_player_android): fix Java formatting issues

--- packages/video_player/video_player_android/android/src/test/java/io/flutter/plugins/videoplayer/VideoPlayerTest.java
+++ packages/video_player/video_player_android/android/src/test/java/io/flutter/plugins/videoplayer/VideoPlayerTest.java
@@ -4,12 +4,12 @@
 
 package io.flutter.plugins.videoplayer;
 
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
