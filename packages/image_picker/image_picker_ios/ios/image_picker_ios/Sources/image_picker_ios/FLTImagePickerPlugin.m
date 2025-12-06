7f52402ea533340f5714be8afadfce6baca67894
Calvin Ren <j.calvin.ren@gmail.com>
2025-12-06 19:57:56 +1000

    [image_picker] Transparent pressing on iOS 26 (#173453)

--- packages/image_picker/image_picker_ios/ios/image_picker_ios/Sources/image_picker_ios/FLTImagePickerPlugin.m
+++ packages/image_picker/image_picker_ios/ios/image_picker_ios/Sources/image_picker_ios/FLTImagePickerPlugin.m
@@ -37,6 +37,8 @@
 /// the array.
 @property(strong, nonatomic)
     NSMutableArray<UIImagePickerController *> *imagePickerControllerOverrides;
+@property(strong, nonatomic) UIWindow *interactionBlockerWindow;
+@property(weak, nonatomic) UIWindow *previousKeyWindow;
 
 @end
 
@@ -323,6 +325,7 @@ typedef NS_ENUM(NSInteger, ImagePickerClassType) { UIImagePickerClassType, PHPic
       [UIImagePickerController isCameraDeviceAvailable:device]) {
     imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
     imagePickerController.cameraDevice = device;
+    [self addInteractionBlocker];
     [[self viewControllerWithWindow:nil] presentViewController:imagePickerController
                                                       animated:YES
                                                     completion:nil];
@@ -532,7 +535,10 @@ typedef NS_ENUM(NSInteger, ImagePickerClassType) { UIImagePickerClassType, PHPic
 - (void)imagePickerController:(UIImagePickerController *)picker
     didFinishPickingMediaWithInfo:(NSDictionary<NSString *, id> *)info {
   NSURL *videoURL = info[UIImagePickerControllerMediaURL];
-  [picker dismissViewControllerAnimated:YES completion:nil];
+  [picker dismissViewControllerAnimated:YES
+                             completion:^{
+                               [self removeInteractionBlocker];
+                             }];
   // The method dismissViewControllerAnimated does not immediately prevent
   // further didFinishPickingMediaWithInfo invocations. A nil check is necessary
   // to prevent below code to be unwantly executed multiple times and cause a
@@ -618,7 +624,10 @@ typedef NS_ENUM(NSInteger, ImagePickerClassType) { UIImagePickerClassType, PHPic
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
-  [picker dismissViewControllerAnimated:YES completion:nil];
+  [picker dismissViewControllerAnimated:YES
+                             completion:^{
+                               [self removeInteractionBlocker];
+                             }];
   [self sendCallResultWithSavedPathList:nil];
 }
 
@@ -672,6 +681,42 @@ typedef NS_ENUM(NSInteger, ImagePickerClassType) { UIImagePickerClassType, PHPic
   }
   self.callContext.result(nil, error);
   self.callContext = nil;
+}
+
+- (void)addInteractionBlocker {
+  if (self.interactionBlockerWindow != nil) {
+    return;
+  }
+  UIViewController *topController = [self viewControllerWithWindow:nil];
+  UIWindow *presentingWindow = topController.view.window;
+  if (!presentingWindow) {
+    return;
+  }
+  self.previousKeyWindow = presentingWindow;
+  UIWindow *blockerWindow;
+  if (@available(iOS 13.0, *)) {
+    blockerWindow = [[UIWindow alloc] initWithWindowScene:presentingWindow.windowScene];
+  } else {
+    blockerWindow = [[UIWindow alloc] initWithFrame:presentingWindow.bounds];
+  }
+  blockerWindow.frame = presentingWindow.bounds;
+  blockerWindow.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+  blockerWindow.windowLevel = presentingWindow.windowLevel + 1;
+  UIViewController *vc = [[UIViewController alloc] init];
+  vc.view.backgroundColor = [UIColor clearColor];
+  vc.view.userInteractionEnabled = YES;
+  blockerWindow.rootViewController = vc;
+  [blockerWindow makeKeyAndVisible];
+  self.interactionBlockerWindow = blockerWindow;
+}
+
+- (void)removeInteractionBlocker {
+  self.interactionBlockerWindow.hidden = YES;
+  if (self.previousKeyWindow) {
+    [self.previousKeyWindow makeKeyWindow];
+  }
+  self.interactionBlockerWindow = nil;
+  self.previousKeyWindow = nil;
 }
 
 @end
