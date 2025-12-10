72f24a73057520a4490787a2d9ffe633818bdb22
stuartmorgan-g <stuartmorgan@google.com>
2025-12-10 16:47:08 -0500

    [file_selector] Update Linux example for deprecations (#10542)
    The `file_selector_linux` example was using some methods that have been deprecated in favor of param-object versions. This updates to use those new versions.

--- packages/file_selector/file_selector_linux/example/lib/get_directory_page.dart
+++ packages/file_selector/file_selector_linux/example/lib/get_directory_page.dart
@@ -14,7 +14,9 @@ class GetDirectoryPage extends StatelessWidget {
   Future<void> _getDirectoryPath(BuildContext context) async {
     const confirmButtonText = 'Choose';
     final String? directoryPath = await FileSelectorPlatform.instance
-        .getDirectoryPath(confirmButtonText: confirmButtonText);
+        .getDirectoryPathWithOptions(
+          const FileDialogOptions(confirmButtonText: confirmButtonText),
+        );
     if (directoryPath == null) {
       // Operation was canceled by the user.
       return;
