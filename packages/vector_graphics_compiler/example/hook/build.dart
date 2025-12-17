a25d2084f46662de18fdfaa18c09ba59f59a80eb
Moritz <mosum@google.com>
2025-12-17 16:27:07 +0100

    Single commit

---
+++ packages/vector_graphics_compiler/example/hook/build.dart
@@ -0,0 +1,18 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:hooks/hooks.dart';
+import 'package:vector_graphics_compiler/build.dart';
+
+void main(List<String> args) {
+  build(args, (BuildInput input, BuildOutputBuilder output) async {
+    await compileSvg(
+      input,
+      output,
+      name: 'example_file',
+      file: input.packageRoot.resolve('assets/example.svg'),
+      options: const Options(dumpDebug: true, concurrency: 2),
+    );
+  });
+}
