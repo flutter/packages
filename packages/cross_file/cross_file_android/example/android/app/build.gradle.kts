5b4b0fea32d781cb3818b23f445efb2301f3c712
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2025-12-29 14:04:05 -0500

    update gradle to standard

--- packages/cross_file/cross_file_android/example/android/app/build.gradle.kts
+++ packages/cross_file/cross_file_android/example/android/app/build.gradle.kts
@@ -11,19 +11,22 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/kotlin")
+        }
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "dev.flutter.packages.cross_file_android_example"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +35,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- packages/cross_file/cross_file_android/example/android/build.gradle.kts
+++ packages/cross_file/cross_file_android/example/android/build.gradle.kts
@@ -22,3 +22,27 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+allprojects {
+  repositories {
+    // See https://github.com/flutter/flutter/blob/master/docs/ecosystem/Plugins-and-Packages-repository-structure.md#gradle-structure for more info.
+    val artifactRepoKey = "ARTIFACT_HUB_REPOSITORY"
+    val artifactRepoUrl = System.getenv(artifactRepoKey)
+
+    if (artifactRepoUrl != null) {
+      println("Using artifact hub")
+      maven { url = uri(artifactRepoUrl) }
+    }
+
+    google()
+    mavenCentral()
+  }
+}
+
+gradle.projectsEvaluated {
+  project(":cross_file_android") {
+    tasks.withType<JavaCompile> {
+      options.compilerArgs.addAll(listOf("-Xlint:all", "-Werror"))
+    }
+  }
+}
