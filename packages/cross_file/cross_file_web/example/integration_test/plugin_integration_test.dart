317702cb6ce93e611b38c8edefcaef8ffd8287f3
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2026-01-07 15:21:43 -0500

    fix integration test

--- packages/cross_file/cross_file_web/example/integration_test/plugin_integration_test.dart
+++ packages/cross_file/cross_file_web/example/integration_test/plugin_integration_test.dart
@@ -1,24 +1,14 @@
-// This is a basic Flutter integration test.
-//
-// Since integration tests run in a full Flutter application, they can interact
-// with the host side of a plugin implementation, unlike Dart unit tests.
-//
-// For more information about Flutter integration tests, please see
-// https://flutter.dev/to/integration-testing
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
-import 'package:cross_file_web/cross_file_web.dart';
-
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('getPlatformVersion test', (WidgetTester tester) async {
-    final CrossFileWeb plugin = CrossFileWeb();
-    final String? version = await plugin.getPlatformVersion();
-    // The version string depends on the host platform running the test, so
-    // just assert that some non-empty string is returned.
-    expect(version?.isNotEmpty, true);
-  });
+  // Since this test is lacking integration tests, this test ensures the example
+  // app can be launched on an emulator/device.
+  testWidgets('Launch Test', (WidgetTester tester) async {});
 }
