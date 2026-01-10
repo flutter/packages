ab8fdd0fbe63fbb8e562f26308f7c49d76e8b616
Jenn Magder <magder@google.com>
2026-01-09 16:56:25 -0800

    Refactor

--- packages/path_provider/path_provider_foundation/darwin/RunnerTests/RunnerTests.swift
+++ packages/path_provider/path_provider_foundation/darwin/RunnerTests/RunnerTests.swift
@@ -13,73 +13,48 @@ import Testing
 #endif
 
 struct RunnerTests {
-  @Test func getTemporaryDirectory() throws {
+  @Test(arguments: [
+    (DirectoryType.temp, FileManager.SearchPathDirectory.cachesDirectory),
+    (.applicationDocuments, .documentDirectory),
+    (.library, .libraryDirectory),
+    (.downloads, .downloadsDirectory),
+  ])
+  func directoryPath(
+    directoryType: DirectoryType,
+    searchPathDirectory: FileManager.SearchPathDirectory
+  ) throws {
     let plugin = PathProviderPlugin()
-    let path = plugin.getDirectoryPath(type: .temp)
-    #expect(
-      path == NSSearchPathForDirectoriesInDomains(
-        FileManager.SearchPathDirectory.cachesDirectory,
-        FileManager.SearchPathDomainMask.userDomainMask,
+    let path = try #require(plugin.getDirectoryPath(type: directoryType))
+    let expected = try #require(
+      NSSearchPathForDirectoriesInDomains(
+        searchPathDirectory,
+        .userDomainMask,
         true
-      ).first)
-  }
-
-  @Test func getApplicationDocumentsDirectory() throws {
-    let plugin = PathProviderPlugin()
-    let path = plugin.getDirectoryPath(type: .applicationDocuments)
-    #expect(
-      path == NSSearchPathForDirectoriesInDomains(
-        FileManager.SearchPathDirectory.documentDirectory,
-        FileManager.SearchPathDomainMask.userDomainMask,
-        true
-      ).first)
+      ).first
+    )
+    #expect(path == expected)
   }
 
   @Test func getApplicationSupportDirectory() throws {
     let plugin = PathProviderPlugin()
-    let path = plugin.getDirectoryPath(type: .applicationSupport)
+    let path = try #require(plugin.getDirectoryPath(type: .applicationSupport))
+    let base = try #require(
+      NSSearchPathForDirectoriesInDomains(
+        .applicationSupportDirectory,
+        .userDomainMask,
+        true
+      ).first
+    )
+
     #if os(iOS)
       // On iOS, the application support directory path should be just the system application
       // support path.
-      #expect(
-        path == NSSearchPathForDirectoriesInDomains(
-          FileManager.SearchPathDirectory.applicationSupportDirectory,
-          FileManager.SearchPathDomainMask.userDomainMask,
-          true
-        ).first)
+      #expect(path == base)
     #else
       // On macOS, the application support directory path should be the system application
       // support path with an added subdirectory based on the app name.
-      #expect(
-        path!.hasPrefix(
-          NSSearchPathForDirectoriesInDomains(
-            FileManager.SearchPathDirectory.applicationSupportDirectory,
-            FileManager.SearchPathDomainMask.userDomainMask,
-            true
-          ).first!))
-      #expect(path!.hasSuffix("Example"))
+      #expect(path.hasPrefix(base))
+      #expect(path.hasSuffix("Example"))
     #endif
-  }
-
-  @Test func getLibraryDirectory() throws {
-    let plugin = PathProviderPlugin()
-    let path = plugin.getDirectoryPath(type: .library)
-    #expect(
-      path == NSSearchPathForDirectoriesInDomains(
-        FileManager.SearchPathDirectory.libraryDirectory,
-        FileManager.SearchPathDomainMask.userDomainMask,
-        true
-      ).first)
-  }
-
-  @Test func getDownloadsDirectory() throws {
-    let plugin = PathProviderPlugin()
-    let path = plugin.getDirectoryPath(type: .downloads)
-    #expect(
-      path == NSSearchPathForDirectoriesInDomains(
-        FileManager.SearchPathDirectory.downloadsDirectory,
-        FileManager.SearchPathDomainMask.userDomainMask,
-        true
-      ).first)
   }
 }
