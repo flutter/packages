fded3862cf64b627d3b8cb2b370081c2f51f7fdd
ValentinVignal <valentin.vignal.dev@outlook.fr>
2026-01-15 00:58:15 +0800

    Update example

---
+++ packages/go_router_builder/example/lib/typed_go_route_parameter_example.dart
@@ -0,0 +1,92 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+part 'typed_go_route_parameter_example.g.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  App({super.key});
+
+  @override
+  Widget build(BuildContext context) =>
+      MaterialApp.router(routerConfig: _router);
+
+  final GoRouter _router = GoRouter(
+    initialLocation: '/int-route',
+    routes: $appRoutes,
+  );
+}
+
+@TypedGoRoute<IntRoute>(path: '/int-route')
+class IntRoute extends GoRouteData with $IntRoute {
+  IntRoute({
+    @TypedGoRouteParameter(name: 'intField') this.intField,
+    @TypedGoRouteParameter(name: 'int_field_with_default_value')
+    this.intFieldWithDefaultValue = 1,
+  });
+
+  final int? intField;
+  final int intFieldWithDefaultValue;
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) => Screen(
+    intField: intField,
+    intFieldWithDefaultValue: intFieldWithDefaultValue,
+  );
+}
+
+class Screen extends StatelessWidget {
+  const Screen({
+    required this.intField,
+    required this.intFieldWithDefaultValue,
+    super.key,
+  });
+
+  final int? intField;
+  final int intFieldWithDefaultValue;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: const Text('Go router with custom URI parameter names'),
+    ),
+    body: Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            title: const Text('intField:'),
+            subtitle: Text('$intField'),
+            trailing: const Icon(Icons.add),
+            onTap: () {
+              final int newValue = (intField ?? 0) + 1;
+              IntRoute(
+                intField: newValue,
+                intFieldWithDefaultValue: intFieldWithDefaultValue,
+              ).go(context);
+            },
+          ),
+          ListTile(
+            title: const Text('intFieldWithDefaultValue:'),
+            subtitle: Text('$intFieldWithDefaultValue'),
+            trailing: const Icon(Icons.add),
+            onTap: () {
+              final int newValue = intFieldWithDefaultValue + 1;
+              IntRoute(
+                intField: intField,
+                intFieldWithDefaultValue: newValue,
+              ).go(context);
+            },
+          ),
+        ],
+      ),
+    ),
+  );
+}
---
+++ packages/go_router_builder/example/lib/typed_go_route_parameter_example.g.dart
@@ -0,0 +1,66 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: always_specify_types, public_member_api_docs
+
+part of 'typed_go_route_parameter_example.dart';
+
+// **************************************************************************
+// GoRouterGenerator
+// **************************************************************************
+
+List<RouteBase> get $appRoutes => [$intRoute];
+
+RouteBase get $intRoute =>
+    GoRouteData.$route(path: '/int-route', factory: $IntRoute._fromState);
+
+mixin $IntRoute on GoRouteData {
+  static IntRoute _fromState(GoRouterState state) => IntRoute(
+    intField: _$convertMapValue(
+      'intField',
+      state.uri.queryParameters,
+      int.tryParse,
+    ),
+    intFieldWithDefaultValue:
+        _$convertMapValue(
+          'int_field_with_default_value',
+          state.uri.queryParameters,
+          int.parse,
+        ) ??
+        1,
+  );
+
+  IntRoute get _self => this as IntRoute;
+
+  @override
+  String get location => GoRouteData.$location(
+    '/int-route',
+    queryParams: {
+      if (_self.intField != null) 'intField': _self.intField!.toString(),
+      if (_self.intFieldWithDefaultValue != 1)
+        'int_field_with_default_value': _self.intFieldWithDefaultValue
+            .toString(),
+    },
+  );
+
+  @override
+  void go(BuildContext context) => context.go(location);
+
+  @override
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  @override
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  @override
+  void replace(BuildContext context) => context.replace(location);
+}
+
+T? _$convertMapValue<T>(
+  String key,
+  Map<String, String> map,
+  T? Function(String) converter,
+) {
+  final value = map[key];
+  return value == null ? null : converter(value);
+}
