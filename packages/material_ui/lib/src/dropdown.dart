e8e808d5314548f7dff066bd284ab7a9162c60f2
Mairramer <mairramer.dasilva28@hotmail.com>
2026-01-15 14:57:15 -0300

    Add expands property to DropdownButton

--- packages/material_ui/lib/src/dropdown.dart
+++ packages/material_ui/lib/src/dropdown.dart
@@ -1772,6 +1772,7 @@ class _DropdownButtonState<T> extends State<DropdownButton<T>> with WidgetsBindi
               isEmpty: widget._isEmpty,
               isFocused: _hasPrimaryFocus,
               isHovering: _isHovering,
+              expands: widget.isExpanded,
               child: widget.padding == null
                   ? result
                   : Padding(padding: widget.padding!, child: result),
