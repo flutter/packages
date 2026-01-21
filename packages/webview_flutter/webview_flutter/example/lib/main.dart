903cb6c4819229330a783854666bb24bd02a8ad2
Khaled <spkhalad@gmail.com>
2026-01-21 23:32:24 +0600

    [webview_flutter] Use CookieManager.getCookies instead of document.cookie

--- packages/webview_flutter/webview_flutter/example/lib/main.dart
+++ packages/webview_flutter/webview_flutter/example/lib/main.dart
@@ -428,9 +428,12 @@ class SampleMenu extends StatelessWidget {
   }
 
   Future<void> _onListCookies(BuildContext context) async {
-    final cookies =
-        await webViewController.runJavaScriptReturningResult('document.cookie')
-            as String;
+    final Uri? domain = Uri.tryParse(
+      (await webViewController.currentUrl()) ?? '',
+    );
+    final List<WebViewCookie> cookies = await cookieManager.getCookies(
+      domain: domain,
+    );
     if (context.mounted) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
@@ -536,13 +539,12 @@ class SampleMenu extends StatelessWidget {
     return webViewController.loadHtmlString(kTransparentBackgroundPage);
   }
 
-  Widget _getCookieList(String cookies) {
-    if (cookies == '""') {
+  Widget _getCookieList(List<WebViewCookie> cookies) {
+    if (cookies.isEmpty) {
       return Container();
     }
-    final List<String> cookieList = cookies.split(';');
-    final Iterable<Text> cookieWidgets = cookieList.map(
-      (String cookie) => Text(cookie),
+    final Iterable<Text> cookieWidgets = cookies.map(
+      (WebViewCookie cookie) => Text(cookie.toString()),
     );
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
