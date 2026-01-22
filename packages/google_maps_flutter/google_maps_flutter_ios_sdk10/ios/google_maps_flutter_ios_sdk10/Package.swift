0fa16a07dd7658f6fc337736b711e745dcc88b13
Stuart Morgan <stuartmorgan@google.com>
2026-01-22 11:41:53 -0500

    Fix license block

--- packages/google_maps_flutter/google_maps_flutter_ios_sdk10/ios/google_maps_flutter_ios_sdk10/Package.swift
+++ packages/google_maps_flutter/google_maps_flutter_ios_sdk10/ios/google_maps_flutter_ios_sdk10/Package.swift
@@ -1,4 +1,5 @@
 // swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 // Copyright 2013 The Flutter Authors
 // Use of this source code is governed by a BSD-style license that can be
