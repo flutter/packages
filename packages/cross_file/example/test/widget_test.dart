5a45953904ea584fa57eb79ae74016cdc003d52c
crackedhandle <divyansh.shah.ece23@itbhu.ac.in>
2026-01-22 23:16:29 +0530

    example(test): wait for app to settle before asserting UI in widget_test

--- packages/cross_file/example/test/widget_test.dart
+++ packages/cross_file/example/test/widget_test.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:cross_file_example/main.dart';
 
 void main() {
   testWidgets('Displays created file text', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Build our app and wait for any async build work to complete.
     await tester.pumpWidget(const MyApp());
+    await tester.pumpAndSettle();
 
     // Verify that the text is shown.
     expect(find.text('Created file: demo.txt'), findsOneWidget);
