f7cfe24cfc41794bc552c5d4c17a65cb3ee3f0c9
Rick Hohler <rick.hohler@gmail.com>
2026-01-29 11:26:22 -0600

    Move console message logic to Dart

--- packages/webview_flutter/webview_flutter_wkwebview/lib/src/webkit_webview_controller.dart
+++ packages/webview_flutter/webview_flutter_wkwebview/lib/src/webkit_webview_controller.dart
@@ -700,7 +700,7 @@ class WebKitWebViewController extends PlatformWebViewController {
   @override
   Future<void> setOnConsoleMessage(
     void Function(JavaScriptConsoleMessage consoleMessage) onConsoleMessage,
-  ) {
+  ) async {
     _onConsoleMessageCallback = onConsoleMessage;
 
     final JavaScriptChannelParams channelParams = WebKitJavaScriptChannelParams(
@@ -736,7 +736,11 @@ class WebKitWebViewController extends PlatformWebViewController {
       },
     );
 
-    addJavaScriptChannel(channelParams);
+    if (_javaScriptChannelParams.containsKey('fltConsoleMessage')) {
+      await removeJavaScriptChannel('fltConsoleMessage');
+    }
+
+    await addJavaScriptChannel(channelParams);
     return _injectConsoleOverride();
   }
 
