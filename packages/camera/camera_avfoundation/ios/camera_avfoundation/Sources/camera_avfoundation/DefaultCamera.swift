5c957e58c5d4cfa8a094643883dc516789510a57
Rick Hohler <rick.hohler@gmail.com>
2026-01-29 16:57:27 -0600

    Implement missing startWriting error check and update isRecording visibility

--- packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/DefaultCamera.swift
+++ packages/camera/camera_avfoundation/ios/camera_avfoundation/Sources/camera_avfoundation/DefaultCamera.swift
@@ -543,6 +543,7 @@ final class DefaultCamera: NSObject, Camera {
     // didOutputSampleBuffer had chance to call startWriting and lag at start of video
     // https://github.com/flutter/flutter/issues/132016
     // https://github.com/flutter/flutter/issues/151319
+<<<<<<< Updated upstream
     guard let videoWriter = videoWriter, videoWriter.startWriting() else {
       completion(
         FlutterError(
@@ -550,6 +551,16 @@ final class DefaultCamera: NSObject, Camera {
           message: "Unable to start writing",
           details: videoWriter?.error?.localizedDescription))
       return
+=======
+    if videoWriter?.startWriting() == false {
+       let error = videoWriter?.error
+       completion(
+         FlutterError(
+           code: "IOError",
+           message: "Unable to start writing: \(error?.localizedDescription ?? "unknown error")",
+           details: nil))
+       return
+>>>>>>> Stashed changes
     }
     isFirstVideoSample = true
     isRecording = true
