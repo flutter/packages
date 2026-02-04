dcc447482dc4ae77a49d7eff582ce2357e8ca60e
louisehsu <louiseh0313@gmail.com>
2026-02-03 16:55:35 -0800

    remove old changes

--- packages/local_auth/local_auth_darwin/darwin/local_auth_darwin/Sources/local_auth_darwin/LocalAuthPlugin.swift
+++ packages/local_auth/local_auth_darwin/darwin/local_auth_darwin/Sources/local_auth_darwin/LocalAuthPlugin.swift
@@ -33,7 +33,7 @@ struct StickyAuthState {
 /// A flutter plugin for local authentication.
 // TODO(stuartmorgan): Remove the @unchecked Sendable, and convert to strict thread enforcement.
 // This was added to minimize the changes while converting from Swift to Objective-C.
-public final class LocalAuthPlugin: NSObject, FlutterPlugin, LocalAuthApi, FlutterSceneLifeCycleDelegate, @unchecked Sendable {
+public final class LocalAuthPlugin: NSObject, FlutterPlugin, LocalAuthApi, @unchecked Sendable {
 
   /// The factory to create LAContexts.
   private let authContextFactory: AuthContextFactory
@@ -43,9 +43,13 @@ public final class LocalAuthPlugin: NSObject, FlutterPlugin, LocalAuthApi, Flutt
   public static func register(with registrar: FlutterPluginRegistrar) {
     let instance = LocalAuthPlugin(
       contextFactory: DefaultAuthContextFactory())
+    // Register for both application and scene delegates for backward compatibility.
+    // Apps using UIScene lifecycle will receive sceneDidBecomeActive,
+    // while apps not yet migrated will receive applicationDidBecomeActive.
     registrar.addApplicationDelegate(instance)
-    registrar.addSceneDelegate(instance)
-
+    #if os(iOS)
+      registrar.addSceneDelegate(instance)
+    #endif
     // Workaround for https://github.com/flutter/flutter/issues/118103.
     #if os(iOS)
       let messenger = registrar.messenger()
@@ -251,28 +255,36 @@ public final class LocalAuthPlugin: NSObject, FlutterPlugin, LocalAuthApi, Flutt
       ))
   }
 
+  private func retryStickyAuth() {
+    if let lastCallState = self.lastCallState {
+      authenticate(
+        options: lastCallState.options,
+        strings: lastCallState.strings,
+        completion: lastCallState.resultHandler)
+    }
+  }
+
   // MARK: App delegate
 
-  // This method is called when the app is resumed from the background only on iOS
+  // These methods are called when the app is resumed from the background only on iOS.
+  // Both are kept for backward compatibility: sceneDidBecomeActive for apps using UIScene lifecycle,
+  // and applicationDidBecomeActive for apps that haven't migrated yet.
   #if os(iOS)
-    public func applicationDidBecomeActive(_ application: UIApplication) {
-      if let lastCallState = self.lastCallState {
-        authenticate(
-          options: lastCallState.options,
-          strings: lastCallState.strings,
-          completion: lastCallState.resultHandler)
-      }
-    }
-
+    @MainActor
     public func sceneDidBecomeActive(_ scene: UIScene) {
-      if let lastCallState = self.lastCallState {
-        authenticate(
-          options: lastCallState.options,
-          strings: lastCallState.strings,
-          completion: lastCallState.resultHandler)
-      }
+      retryStickyAuth()
     }
 
+    @MainActor
+    public func applicationDidBecomeActive(_ application: UIApplication) {
+      retryStickyAuth()
+    }
   #endif  // os(iOS)
 
 }
+
+// MARK: - FlutterSceneLifeCycleDelegate
+
+#if os(iOS)
+  extension LocalAuthPlugin: FlutterSceneLifeCycleDelegate {}
+#endif
