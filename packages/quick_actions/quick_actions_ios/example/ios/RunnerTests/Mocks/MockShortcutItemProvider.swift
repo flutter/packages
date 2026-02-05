2320352a34a008bba8c12376401d6fcc3fadbdee
Stuart Morgan <stuartmorgan@google.com>
2026-02-05 15:00:44 -0500

    Restore native tests

---
+++ packages/quick_actions/quick_actions_ios/example/ios/RunnerTests/Mocks/MockShortcutItemProvider.swift
@@ -0,0 +1,11 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import UIKit
+
+@testable import quick_actions_ios
+
+final class MockShortcutItemProvider: ShortcutItemProviding {
+  var shortcutItems: [UIApplicationShortcutItem]? = nil
+}
