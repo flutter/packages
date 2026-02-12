d97486715dc506b89f7b3f928bad390bd34c2f05
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2026-02-11 22:31:01 -0500

    create some semblance of unit tests

--- packages/cross_file/cross_file_darwin/darwin/Tests/CrossFileDarwinApiTests.swift
+++ packages/cross_file/cross_file_darwin/darwin/Tests/CrossFileDarwinApiTests.swift
@@ -1,10 +1,48 @@
-//
-// Created by Maurice Parrish on 1/21/26.
-//
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-import Foundation
+#if os(iOS)
+  import Flutter
+#elseif os(macOS)
+  import FlutterMacOS
+#else
+  #error("Unsupported platform.")
+#endif
 
+import Testing
+import cross_file_darwin
+import UIKit
 
-struct A: URL {
+@testable import cross_file_darwin
+
+struct CrossFileDarwinApiTests {
+  func createTempTestFile() throws -> URL {
+    let fileManager = FileManager.default
+    let tempDirectory = fileManager.temporaryDirectory
+    let fileName = UUID().uuidString + ".txt"
+    let fileURL = tempDirectory.appendingPathComponent(fileName)
+    
+    try "Hello, World!".write(to: fileURL, atomically: true, encoding: .utf8)
+    
+    return fileURL
+  }
   
+  @Test func startAccessingSecurityScopedResource() throws {
+    let testFileURL = try createTempTestFile()
+    
+    let api = CrossFileDarwinApiImpl()
+    let canAccess = try api.startAccessingSecurityScopedResource(url: testFileURL.path)
+    
+    #expect(canAccess)
+  }
+  
+  @Test func tryCreateBookmarkedUrl() throws {
+    let testFileURL = try createTempTestFile()
+    
+    let api = CrossFileDarwinApiImpl()
+    let bookmarkedURLString = try! api.tryCreateBookmarkedUrl(url: testFileURL.path)!
+    
+    #expect(URL(fileURLWithPath: bookmarkedURLString) == testFileURL)
+  }
 }
