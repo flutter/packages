23f83c252470984be3f5f78b27105da04f66418d
louisehsu <louiseh0313@gmail.com>
2026-02-13 13:08:42 -0800

    swift 5

--- packages/google_maps_flutter/google_maps_flutter_ios_shared_code/example/ios/Runner/AppDelegate.swift
+++ packages/google_maps_flutter/google_maps_flutter_ios_shared_code/example/ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-@preconcurrency import Flutter
+import Flutter
 import GoogleMaps
 import UIKit
 
