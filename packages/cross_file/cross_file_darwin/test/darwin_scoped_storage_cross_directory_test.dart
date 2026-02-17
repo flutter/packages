307589699dd1bfe8d0648f5b1a5deaa31dcf078a
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2026-02-16 20:10:48 -0500

    add darwin tests

--- packages/cross_file/cross_file_darwin/test/darwin_scoped_storage_cross_directory_test.dart
+++ packages/cross_file/cross_file_darwin/test/darwin_scoped_storage_cross_directory_test.dart
@@ -2,17 +2,53 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
+import 'package:cross_file_darwin/cross_file_darwin.dart';
 import 'package:cross_file_darwin/src/cross_file_darwin_apis.g.dart';
 import 'package:cross_file_darwin/src/darwin_scoped_storage_cross_directory.dart';
+import 'package:cross_file_platform_interface/cross_file_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:path/path.dart' as path;
 
 import 'darwin_scoped_storage_cross_directory_test.mocks.dart';
+
+final Directory testDirectory = Directory(
+  path.join(Directory.current.path, 'test'),
+);
 
 @GenerateMocks(<Type>[CrossFileDarwinApi])
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    CrossFilePlatform.instance = CrossFileDarwin();
+  });
+
+  test('exists', () async {
+    final directory = PlatformXDirectory(
+      PlatformXDirectoryCreationParams(uri: testDirectory.path),
+    );
+
+    expect(await directory.exists(), testDirectory.existsSync());
+  });
+
+  test('list', () async {
+    final directory = PlatformXDirectory(
+      PlatformXDirectoryCreationParams(uri: testDirectory.path),
+    );
+
+    expect(
+      (await directory.list(ListParams()).toList()).map(
+        (PlatformXFileEntity entity) => entity.params.uri,
+      ),
+      (await testDirectory.list().toList()).map(
+        (FileSystemEntity entity) => entity.uri.toString(),
+      ),
+    );
+  });
 
   test('startAccessingSecurityScopedResource', () async {
     final mockApi = MockCrossFileDarwinApi();
--- packages/cross_file/cross_file_darwin/test/darwin_scoped_storage_cross_file_test.dart
+++ packages/cross_file/cross_file_darwin/test/darwin_scoped_storage_cross_file_test.dart
@@ -2,17 +2,97 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
+import 'package:cross_file_darwin/cross_file_darwin.dart';
 import 'package:cross_file_darwin/src/cross_file_darwin_apis.g.dart';
 import 'package:cross_file_darwin/src/darwin_scoped_storage_cross_file.dart';
+import 'package:cross_file_platform_interface/cross_file_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:path/path.dart' as path;
 
 import 'darwin_scoped_storage_cross_file_test.mocks.dart';
+
+final File testFile = File(
+  path.join(Directory.current.path, 'test', 'test_file.txt'),
+);
 
 @GenerateMocks(<Type>[CrossFileDarwinApi])
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    CrossFilePlatform.instance = CrossFileDarwin();
+  });
+
+  test('lastModified', () async {
+    final file = PlatformXFile(
+      PlatformXFileCreationParams(uri: testFile.path),
+    );
+
+    expect(await file.lastModified(), testFile.lastModifiedSync());
+  });
+
+  test('length', () async {
+    final file = PlatformXFile(
+      PlatformXFileCreationParams(uri: testFile.path),
+    );
+
+    expect(await file.length(), await testFile.length());
+  });
+
+  test('openRead', () async {
+    final file = PlatformXFile(
+      PlatformXFileCreationParams(uri: testFile.path),
+    );
+
+    expect(
+      await file.openRead().toList(),
+      await testFile.openRead().toList(),
+    );
+  });
+
+  test('readAsBytes', () async {
+    final file = PlatformXFile(
+      PlatformXFileCreationParams(uri: testFile.path),
+    );
+
+    expect(await file.readAsBytes(), await testFile.readAsBytes());
+  });
+
+  test('readAsString', () async {
+    final file = PlatformXFile(
+      PlatformXFileCreationParams(uri: testFile.path),
+    );
+
+    expect(await file.readAsString(), await testFile.readAsString());
+  });
+
+  test('canRead', () async {
+    final file = PlatformXFile(
+      PlatformXFileCreationParams(uri: testFile.path),
+    );
+
+    expect(await file.canRead(), testFile.existsSync());
+  });
+
+  test('exists', () async {
+    final file = PlatformXFile(
+      PlatformXFileCreationParams(uri: testFile.path),
+    );
+
+    expect(await file.exists(), testFile.existsSync());
+  });
+
+  test('name', () async {
+    final file = PlatformXFile(
+      PlatformXFileCreationParams(uri: testFile.path),
+    );
+
+    expect(await file.name(), 'test_file.txt');
+  });
 
   test('startAccessingSecurityScopedResource', () async {
     final mockApi = MockCrossFileDarwinApi();
