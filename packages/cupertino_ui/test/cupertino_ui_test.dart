a32711df3ebf9fa7632b52549520f82fd7a2e2b4
Kate Lovett <katelovett@google.com>
2026-02-17 12:52:39 -0600

    Analysis

--- packages/cupertino_ui/test/cupertino_ui_test.dart
+++ packages/cupertino_ui/test/cupertino_ui_test.dart
@@ -2,12 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:cupertino_ui/cupertino_ui.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Cupertino library is exported', (WidgetTester tester) async {
-    await tester.pumpWidget(const CupertinoApp());
+    await tester.pumpWidget(const CupertinoApp(home: SizedBox.shrink()));
   });
 }
