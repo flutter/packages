bc3b9533f39cb62a8e701ac22e82d2f45e25853d
Kate Lovett <katelovett@google.com>
2026-02-17 12:30:13 -0600

    Further set up

--- packages/cupertino_ui/lib/cupertino_ui.dart
+++ packages/cupertino_ui/lib/cupertino_ui.dart
@@ -1,5 +1,10 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// The Flutter Cupertino Design library.
+///
+/// To use, import `package:cupertino_ui/cupertino_ui.dart`.
+library cupertino_ui;
+
+export 'package:flutter/cupertino.dart';
