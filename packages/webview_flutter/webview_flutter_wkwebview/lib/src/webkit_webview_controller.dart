8d91d7cc99a1d9ea27fd0ea98cade8c5dcf36e02
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2026-02-20 17:56:17 -0500

    make channel name a const and move if statement

--- packages/webview_flutter/webview_flutter_wkwebview/lib/src/webkit_webview_controller.dart
+++ packages/webview_flutter/webview_flutter_wkwebview/lib/src/webkit_webview_controller.dart
@@ -294,6 +294,8 @@ class WebKitWebViewController extends PlatformWebViewController {
     _webView.setUIDelegate(_uiDelegate);
   }
 
+  static const String _onConsoleMessageChannelName = 'fltConsoleMessage';
+
   /// The WebKit WebView being controlled.
   late final PlatformWebView _webView = PlatformWebView(
     initialConfiguration: _webKitParams._configuration,
@@ -703,8 +705,13 @@ class WebKitWebViewController extends PlatformWebViewController {
   ) async {
     _onConsoleMessageCallback = onConsoleMessage;
 
+    // If channel name is already present, the callback is already registered.
+    if (_javaScriptChannelParams.containsKey(_onConsoleMessageChannelName)) {
+      return;
+    }
+
     final JavaScriptChannelParams channelParams = WebKitJavaScriptChannelParams(
-      name: 'fltConsoleMessage',
+      name: _onConsoleMessageChannelName,
       onMessageReceived: (JavaScriptMessage message) {
         if (_onConsoleMessageCallback == null) {
           return;
@@ -735,11 +742,6 @@ class WebKitWebViewController extends PlatformWebViewController {
         );
       },
     );
-
-    // If fltConsoleMessage is already present, the callback is already registered.
-    if (_javaScriptChannelParams.containsKey('fltConsoleMessage')) {
-      return;
-    }
 
     await addJavaScriptChannel(channelParams);
     return _injectConsoleOverride();
@@ -908,7 +910,8 @@ class WebKitWebViewController extends PlatformWebViewController {
     // Therefore, the replacer parameter of JSON.stringify() is used and the
     // removeCyclicObject method is passed in to solve the error.
     final overrideScript = WKUserScript(
-      source: '''
+      source:
+          '''
 var _flutter_webview_plugin_overrides = _flutter_webview_plugin_overrides || {
   removeCyclicObject: function() {
     const traversalStack = [];
@@ -937,7 +940,7 @@ var _flutter_webview_plugin_overrides = _flutter_webview_plugin_overrides || {
       message: message
     };
 
-    window.webkit.messageHandlers.fltConsoleMessage.postMessage(JSON.stringify(log));
+    window.webkit.messageHandlers.$_onConsoleMessageChannelName.postMessage(JSON.stringify(log));
   }
 };
 
