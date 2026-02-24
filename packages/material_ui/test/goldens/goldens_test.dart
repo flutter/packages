d13e4556d55f9e8698f852b010e09254c274cb5d
Kate Lovett <katelovett@google.com>
2026-02-24 15:04:06 -0600

    Reshuffle

--- packages/material_ui/test/goldens/goldens_test.dart
+++ packages/material_ui/test/goldens/goldens_test.dart
@@ -2,9 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:material_ui/material_ui.dart';
 
 void main() {
   testWidgets('Inconsequential golden test', (WidgetTester tester) async {
