16fa126030f10d7a064e12ba28bafdf6d7695aea
kienhantrung <kienhantrung@gmail.com>
2026-03-02 22:57:23 +0700

    Update test for overrideOnExit

---
+++ packages/go_router_builder/example/lib/not_override_on_exit_example.g.dart
@@ -0,0 +1,80 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: always_specify_types, public_member_api_docs
+
+part of 'not_override_on_exit_example.dart';
+
+// **************************************************************************
+// GoRouterGenerator
+// **************************************************************************
+
+List<RouteBase> get $appRoutes => [$homeRoute, $sub1Route, $sub2Route];
+
+RouteBase get $homeRoute =>
+    GoRouteData.$route(path: '/', factory: $HomeRoute._fromState);
+
+mixin $HomeRoute on GoRouteData {
+  static HomeRoute _fromState(GoRouterState state) => const HomeRoute();
+
+  @override
+  String get location => GoRouteData.$location('/');
+
+  @override
+  void go(BuildContext context) => context.go(location);
+
+  @override
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  @override
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  @override
+  void replace(BuildContext context) => context.replace(location);
+}
+
+RouteBase get $sub1Route =>
+    GoRouteData.$route(path: '/sub-1-route', factory: $Sub1Route._fromState);
+
+mixin $Sub1Route on GoRouteData {
+  static Sub1Route _fromState(GoRouterState state) => const Sub1Route();
+
+  @override
+  String get location => GoRouteData.$location('/sub-1-route');
+
+  @override
+  void go(BuildContext context) => context.go(location);
+
+  @override
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  @override
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  @override
+  void replace(BuildContext context) => context.replace(location);
+}
+
+RouteBase get $sub2Route =>
+    GoRouteData.$route(path: '/sub-2-route', factory: $Sub2Route._fromState);
+
+mixin $Sub2Route on GoRouteData {
+  static Sub2Route _fromState(GoRouterState state) => const Sub2Route();
+
+  @override
+  String get location => GoRouteData.$location('/sub-2-route');
+
+  @override
+  void go(BuildContext context) => context.go(location);
+
+  @override
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  @override
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  @override
+  void replace(BuildContext context) => context.replace(location);
+}
---
+++ packages/go_router_builder/example/test/not_override_on_exit_example_test.dart
@@ -0,0 +1,36 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:go_router_builder_example/not_override_on_exit_example.dart';
+
+void main() {
+  testWidgets('HomeScreen should return result from Sub2Screen', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(App());
+    expect(find.byType(HomeScreen), findsOne);
+
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Go to sub 1 screen'));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(Sub1Screen), findsOne);
+
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Go to sub 2 screen'));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(Sub2Screen), findsOne);
+
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Go back to sub 1 screen'));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(HomeScreen), findsOne);
+
+    await tester.tap(find.widgetWithText(ElevatedButton, 'Sub2Screen'));
+
+    expect(find.byType(Sub1Screen), findsNothing);
+    expect(find.byType(Sub2Screen), findsNothing);
+  });
+}
