c28b92fe1c72b20379659693a69ec3a715278bda
Stuart Morgan <stuartmorgan@google.com>
2026-03-06 19:42:06 -0500

    [google_maps_flutter] Add missing exports
    Like much of the `google_maps_flutter` APIs currently, advanced markers
    rely on clients using types that are defined in the platform interface
    package. This re-exports them, and fixes the examples to not import the
    platform interface.
    
    Ideally we would have more separation between the API layers, but this
    is deeply ingrained in the API design of the plugin at this point, so
    changing it would require a comprehensive overhaul.

--- packages/google_maps_flutter/google_maps_flutter/example/lib/advanced_markers_clustering.dart
+++ packages/google_maps_flutter/google_maps_flutter/example/lib/advanced_markers_clustering.dart
@@ -6,7 +6,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
 import 'clustering.dart';
 import 'page.dart';
