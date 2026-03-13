2465e4a735f4ef697d1e0c92f47eac310f8dae19
Christoph Deil <deil.christoph@gmail.com>
2026-03-13 08:21:19 +0100

    [camera_avfoundation] Remove unused Objective-C testing files

--- packages/camera/camera_avfoundation/example/ios/RunnerTests/Mocks/MockWritableData.swift
+++ packages/camera/camera_avfoundation/example/ios/RunnerTests/Mocks/MockWritableData.swift
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import Foundation
+
 @testable import camera_avfoundation
 
 /// A mock implementation of `WritableData` that allows injecting a custom implementation
