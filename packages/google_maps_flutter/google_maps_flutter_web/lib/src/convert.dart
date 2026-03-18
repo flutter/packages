5b5556196f6e7f1301f71b97ef486131f0cdc37e
Stuart Morgan <stuartmorgan@google.com>
2026-03-18 09:45:02 -0400

    Address my review comment

--- packages/google_maps_flutter/google_maps_flutter_web/lib/src/convert.dart
+++ packages/google_maps_flutter/google_maps_flutter_web/lib/src/convert.dart
@@ -176,8 +176,13 @@ gmaps.ColorScheme? _gmapTypeColorSchemeForPluginColor(MapColorScheme? scheme) {
     case MapColorScheme.followSystem:
       return gmaps.ColorScheme.FOLLOW_SYSTEM;
   }
+  // The enum comes from a different package, which could get a new value at
+  // any time, so provide a fallback that ensures this won't break when used
+  // with a version that contains new values. This is deliberately outside
+  // the switch rather than a `default` so that the linter will flag the
+  // switch as needing an update.
   // ignore: dead_code
-  return gmaps.ColorScheme.FOLLOW_SYSTEM;
+  return null;
 }
 
 gmaps.MapOptions _applyInitialPosition(
