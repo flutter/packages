148f3205771b65a58a9d0386a0b4333fd4df1050
Stuart Morgan <stuartmorgan@google.com>
2026-03-18 12:37:36 -0400

    Restore the unit test behavior that was in main.m

--- packages/camera/camera_avfoundation/example/ios/Runner/AppDelegate.swift
+++ packages/camera/camera_avfoundation/example/ios/Runner/AppDelegate.swift
@@ -15,6 +15,13 @@ import UIKit
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    // Plugin registration eventually sends camera operations on the background queue, which
+    // would run concurrently with the test cases during unit tests, making the debugging
+    // process confusing. This setup is actually not necessary for the unit tests, so
+    // skip it when running unit tests.
+    if NSClassFromString("XCTestCase") != nil {
+      return
+    }
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
