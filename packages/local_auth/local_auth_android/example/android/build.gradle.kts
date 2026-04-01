3c399909698476cf4c763ace118977bda756200a
Stuart Morgan <stuartmorgan@google.com>
2026-04-01 15:28:45 -0400

    Add missing comment

--- packages/local_auth/local_auth_android/example/android/build.gradle.kts
+++ packages/local_auth/local_auth_android/example/android/build.gradle.kts
@@ -32,6 +32,10 @@ tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
 
+// Build the plugin project with warnings enabled. This is here rather than
+// in the plugin itself to avoid breaking clients that have different
+// warnings (e.g., deprecation warnings from a newer SDK than this project
+// builds with).
 gradle.projectsEvaluated {
     project(":local_auth_android") {
         tasks.withType<JavaCompile> {
