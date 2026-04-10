5459a1d51e179e973b583d148fe700bf60bf371f
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2026-04-10 11:50:07 -0400

    formatting

--- packages/webview_flutter/webview_flutter_wkwebview/darwin/Tests/WebViewFlutterPluginTests.swift
+++ packages/webview_flutter/webview_flutter_wkwebview/darwin/Tests/WebViewFlutterPluginTests.swift
@@ -13,30 +13,32 @@ import XCTest
 
 class WebViewFlutterPluginTests: XCTestCase {
   #if os(iOS)
-  func testInstanceManagerIsDeallocatedInApplicationWillTerminate() {
-    let plugin = WebViewFlutterPlugin(binaryMessenger: TestBinaryMessenger())
-    plugin.proxyApiRegistrar!.setUp()
+    func testInstanceManagerIsDeallocatedInApplicationWillTerminate() {
+      let plugin = WebViewFlutterPlugin(binaryMessenger: TestBinaryMessenger())
+      plugin.proxyApiRegistrar!.setUp()
 
-    let view = UIView()
-    _ = plugin.proxyApiRegistrar!.instanceManager.addHostCreatedInstance(view)
+      let view = UIView()
+      _ = plugin.proxyApiRegistrar!.instanceManager.addHostCreatedInstance(view)
 
-    // Attaches an associated object to the InstanceManager to listen for when it is deallocated.
-    var finalizer: TestFinalizer? = TestFinalizer()
-    objc_setAssociatedObject(plugin.proxyApiRegistrar!.instanceManager, malloc(1), finalizer, .OBJC_ASSOCIATION_RETAIN)
+      // Attaches an associated object to the InstanceManager to listen for when it is deallocated.
+      var finalizer: TestFinalizer? = TestFinalizer()
+      objc_setAssociatedObject(
+        plugin.proxyApiRegistrar!.instanceManager, malloc(1), finalizer, .OBJC_ASSOCIATION_RETAIN)
 
-    let expectation = self.expectation(description: "Wait for InstanceManager to be deallocated.")
-    TestFinalizer.onDeinit = {
-      expectation.fulfill()
+      let expectation = self.expectation(description: "Wait for InstanceManager to be deallocated.")
+      TestFinalizer.onDeinit = {
+        expectation.fulfill()
+      }
+
+      // Ensure method is from `FlutterApplicationLifeCycleDelegate`.
+      (plugin as FlutterApplicationLifeCycleDelegate).applicationWillTerminate!(
+        UIApplication.shared)
+      XCTAssertNil(plugin.proxyApiRegistrar)
+
+      finalizer = nil
+      waitForExpectations(timeout: 5.0)
     }
-
-    // Ensure method is from `FlutterApplicationLifeCycleDelegate`.
-    (plugin as FlutterApplicationLifeCycleDelegate).applicationWillTerminate!(UIApplication.shared)
-    XCTAssertNil(plugin.proxyApiRegistrar)
-
-    finalizer = nil
-    waitForExpectations(timeout: 5.0)
-  }
-#endif
+  #endif
 }
 
 class TestFinalizer {
@@ -46,4 +48,3 @@ class TestFinalizer {
     Self.onDeinit?()
   }
 }
-
