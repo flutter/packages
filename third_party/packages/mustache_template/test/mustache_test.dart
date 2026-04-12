3b96a7b6678ed8b0d03fa136b8275acec3f572e5
Jay Slater <slater.jay@gmail.com>
2026-04-12 12:03:17 -0400

    Lower camel case for unsupported specs constant

--- third_party/packages/mustache_template/test/mustache_test.dart
+++ third_party/packages/mustache_template/test/mustache_test.dart
@@ -1,10 +1,9 @@
 import 'mustache_specs.dart' as specs;
 
-/// Optional specifications that are not currently supported by this library.
-///
-/// Values correspond to one of the keys in the SPECS map in `specs/specs.dart`.
-const List<String> UNSUPPORTED_SPECS = ['inheritance', 'dynamic_names'];
+/// Optional specifications that are not currently supported
+/// by this library, in the format of the keys in the SPECS map in specs/specs.dart
+const List<String> unsupportedSpecs = ['inheritance', 'dynamic_names'];
 
 void main() {
-  specs.defineTests(UNSUPPORTED_SPECS);
+  specs.defineTests(unsupportedSpecs);
 }
