cb1e5bc8309a565cce25543a4db3fa9155374ed9
Kate Lovett <katelovett@google.com>
2026-04-16 15:08:39 -0500

    Bump goldctl, format, fix headers

--- packages/cupertino_ui/test/goldens/goldens_test.dart
+++ packages/cupertino_ui/test/goldens/goldens_test.dart
@@ -1,4 +1,4 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
+// Copyright 2013 The Flutter Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,11 +11,7 @@ void main() {
     // The test validates the Flutter Gold integration. Any changes to the
     // golden file can be approved at any time.
     await tester.pumpWidget(
-      const CupertinoApp(
-        home: Center(
-          child: Text('Cupertino Goldens'),
-        ),
-      ),
+      const CupertinoApp(home: Center(child: Text('Cupertino Goldens'))),
     );
 
     await tester.pumpAndSettle();
