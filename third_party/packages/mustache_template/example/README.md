4cadd06ce62825f246a5eb3f1937172d5097350a
Jah-yee <jydu_seven@outlook.com>
2026-04-17 04:33:22 +0800

    Add example app for mustache_template package
    Fixes https://github.com/flutter/flutter/issues/183936
    
    - Added example/ directory with a minimal Flutter app demonstrating basic usage
    - Removed mustache_template from temp_exclude_excerpt.yaml since it now has an example app
    - The example app uses code excerpts for documentation generation

---
+++ third_party/packages/mustache_template/example/README.md
@@ -0,0 +1,13 @@
+# mustache_template_example
+
+Example app for the `mustache_template` package.
+
+This example demonstrates basic usage of the Mustache template library,
+including rendering templates with lists and conditional sections.
+
+## Running the example
+
+```sh
+cd example
+flutter run
+```
---
+++ third_party/packages/mustache_template/example/lib/main.dart
@@ -0,0 +1,51 @@
+// Copyright 2024, the Flutter project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:mustache_template/mustache_template.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mustache Template Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Mustache Template Demo'),
+        ),
+        body: Center(
+          child: Text(_renderTemplate()),
+        ),
+      ),
+    );
+  }
+
+  String _renderTemplate() {
+    // #docregion basic-usage
+    const source = '''
+    {{# names }}
+      <div>{{ lastname }}, {{ firstname }}</div>
+    {{/ names }}
+    {{^ names }}
+      <div>No names.</div>
+    {{/ names }}
+    ''';
+
+    final template = Template(source, name: 'example-template.html');
+
+    return template.renderString({
+      'names': [
+        {'firstname': 'Greg', 'lastname': 'Lowe'},
+        {'firstname': 'Bob', 'lastname': 'Johnson'},
+      ]
+    });
+    // #enddocregion basic-usage
+  }
+}
