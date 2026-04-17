ba7cd48c9b7f191822b409bd1d1b762be5316d65
Stuart Morgan <stuartmorgan@google.com>
2026-04-17 11:24:53 -0400

    Autoformat

--- packages/local_auth/local_auth_android/android/src/main/kotlin/io/flutter/plugins/localauth/AuthenticationHelper.kt
+++ packages/local_auth/local_auth_android/android/src/main/kotlin/io/flutter/plugins/localauth/AuthenticationHelper.kt
@@ -21,7 +21,6 @@ import java.util.concurrent.Executor
 /**
  * Authenticates the user with biometrics and sends corresponding response back to Flutter.
  *
- *
  * One instance per call is generated to ensure readable separation of executable paths across
  * method calls.
  */
@@ -33,166 +32,164 @@ internal class AuthenticationHelper(
     private val completionHandler: AuthCompletionHandler,
     allowCredentials: Boolean
 ) : BiometricPrompt.AuthenticationCallback(), ActivityLifecycleCallbacks, DefaultLifecycleObserver {
-    /** The callback that handles the result of this authentication process.  */
-    internal interface AuthCompletionHandler {
-        /** Called when authentication attempt is complete.  */
-        fun complete(authResult: AuthResult?)
+  /** The callback that handles the result of this authentication process. */
+  internal interface AuthCompletionHandler {
+    /** Called when authentication attempt is complete. */
+    fun complete(authResult: AuthResult?)
+  }
+
+  private val promptInfo: PromptInfo
+  private val isAuthSticky: Boolean
+  private val uiThreadExecutor: UiThreadExecutor
+  private var activityPaused = false
+  private var biometricPrompt: BiometricPrompt? = null
+
+  init {
+    this.isAuthSticky = options.sticky
+    this.uiThreadExecutor = UiThreadExecutor()
+
+    val promptBuilder =
+        PromptInfo.Builder()
+            .setDescription(strings.reason)
+            .setTitle(strings.signInTitle)
+            .setSubtitle(strings.signInHint)
+            .setConfirmationRequired(options.sensitiveTransaction)
+
+    var allowedAuthenticators =
+        (BiometricManager.Authenticators.BIOMETRIC_WEAK or
+            BiometricManager.Authenticators.BIOMETRIC_STRONG)
+
+    if (allowCredentials) {
+      allowedAuthenticators =
+          allowedAuthenticators or BiometricManager.Authenticators.DEVICE_CREDENTIAL
+    } else {
+      promptBuilder.setNegativeButtonText(strings.cancelButton)
     }
 
-    private val promptInfo: PromptInfo
-    private val isAuthSticky: Boolean
-    private val uiThreadExecutor: UiThreadExecutor
-    private var activityPaused = false
-    private var biometricPrompt: BiometricPrompt? = null
+    promptBuilder.setAllowedAuthenticators(allowedAuthenticators)
+    this.promptInfo = promptBuilder.build()
+  }
 
-    init {
-        this.isAuthSticky = options.sticky
-        this.uiThreadExecutor = UiThreadExecutor()
+  /** Start the biometric listener. */
+  fun authenticate() {
+    if (lifecycle != null) {
+      lifecycle.addObserver(this)
+    } else {
+      activity.getApplication().registerActivityLifecycleCallbacks(this)
+    }
+    biometricPrompt = BiometricPrompt(activity, uiThreadExecutor, this)
+    biometricPrompt!!.authenticate(promptInfo)
+  }
 
-        val promptBuilder =
-            PromptInfo.Builder()
-                .setDescription(strings.reason)
-                .setTitle(strings.signInTitle)
-                .setSubtitle(strings.signInHint)
-                .setConfirmationRequired(options.sensitiveTransaction)
+  /** Cancels the biometric authentication. */
+  fun stopAuthentication() {
+    if (biometricPrompt != null) {
+      biometricPrompt!!.cancelAuthentication()
+      biometricPrompt = null
+    }
+  }
 
-        var allowedAuthenticators =
-            (BiometricManager.Authenticators.BIOMETRIC_WEAK
-                    or BiometricManager.Authenticators.BIOMETRIC_STRONG)
+  /** Stops the biometric listener. */
+  private fun stop() {
+    if (lifecycle != null) {
+      lifecycle.removeObserver(this)
+      return
+    }
+    activity.getApplication().unregisterActivityLifecycleCallbacks(this)
+  }
 
-        if (allowCredentials) {
-            allowedAuthenticators =
-                allowedAuthenticators or BiometricManager.Authenticators.DEVICE_CREDENTIAL
-        } else {
-            promptBuilder.setNegativeButtonText(strings.cancelButton)
+  @SuppressLint("SwitchIntDef")
+  override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+    val code: AuthResultCode?
+    when (errorCode) {
+      BiometricPrompt.ERROR_USER_CANCELED -> code = AuthResultCode.USER_CANCELED
+      BiometricPrompt.ERROR_NEGATIVE_BUTTON -> code = AuthResultCode.NEGATIVE_BUTTON
+      BiometricPrompt.ERROR_NO_DEVICE_CREDENTIAL -> code = AuthResultCode.NO_CREDENTIALS
+      BiometricPrompt.ERROR_NO_BIOMETRICS -> code = AuthResultCode.NOT_ENROLLED
+      BiometricPrompt.ERROR_HW_UNAVAILABLE -> code = AuthResultCode.HARDWARE_UNAVAILABLE
+      BiometricPrompt.ERROR_HW_NOT_PRESENT -> code = AuthResultCode.NO_HARDWARE
+      BiometricPrompt.ERROR_LOCKOUT -> code = AuthResultCode.LOCKED_OUT_TEMPORARILY
+      BiometricPrompt.ERROR_LOCKOUT_PERMANENT -> code = AuthResultCode.LOCKED_OUT_PERMANENTLY
+      BiometricPrompt.ERROR_CANCELED -> {
+        // If we are doing sticky auth and the activity has been paused,
+        // ignore this error. We will start listening again when resumed.
+        if (activityPaused && isAuthSticky) {
+          return
         }
-
-        promptBuilder.setAllowedAuthenticators(allowedAuthenticators)
-        this.promptInfo = promptBuilder.build()
+        code = AuthResultCode.SYSTEM_CANCELED
+      }
+      BiometricPrompt.ERROR_TIMEOUT -> code = AuthResultCode.TIMEOUT
+      BiometricPrompt.ERROR_NO_SPACE -> code = AuthResultCode.NO_SPACE
+      BiometricPrompt.ERROR_SECURITY_UPDATE_REQUIRED ->
+          code = AuthResultCode.SECURITY_UPDATE_REQUIRED
+      else -> code = AuthResultCode.UNKNOWN_ERROR
     }
+    completionHandler.complete(AuthResult(code, errString.toString()))
+    stop()
+  }
 
-    /** Start the biometric listener.  */
-    fun authenticate() {
-        if (lifecycle != null) {
-            lifecycle.addObserver(this)
-        } else {
-            activity.getApplication().registerActivityLifecycleCallbacks(this)
-        }
-        biometricPrompt = BiometricPrompt(activity, uiThreadExecutor, this)
-        biometricPrompt!!.authenticate(promptInfo)
+  override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+    completionHandler.complete(AuthResult(AuthResultCode.SUCCESS, null))
+    stop()
+  }
+
+  override fun onAuthenticationFailed() {
+    // No-op; this is called for incremental failures. Wait for a final
+    // resolution via the success or error callbacks.
+  }
+
+  /**
+   * If the activity is paused, we keep track because biometric dialog simply returns "User
+   * cancelled" when the activity is paused.
+   */
+  override fun onActivityPaused(ignored: Activity?) {
+    if (isAuthSticky) {
+      activityPaused = true
     }
+  }
 
-    /** Cancels the biometric authentication.  */
-    fun stopAuthentication() {
-        if (biometricPrompt != null) {
-            biometricPrompt!!.cancelAuthentication()
-            biometricPrompt = null
-        }
+  override fun onActivityResumed(ignored: Activity?) {
+    if (isAuthSticky) {
+      activityPaused = false
+      val prompt = BiometricPrompt(activity, uiThreadExecutor, this)
+      // When activity is resuming, we cannot show the prompt right away. We need to post it to the
+      // UI queue.
+      uiThreadExecutor.handler.post(Runnable { prompt.authenticate(promptInfo) })
     }
+  }
 
-    /** Stops the biometric listener.  */
-    private fun stop() {
-        if (lifecycle != null) {
-            lifecycle.removeObserver(this)
-            return
-        }
-        activity.getApplication().unregisterActivityLifecycleCallbacks(this)
+  override fun onPause(owner: LifecycleOwner) {
+    onActivityPaused(null)
+  }
+
+  override fun onResume(owner: LifecycleOwner) {
+    onActivityResumed(null)
+  }
+
+  // Unused methods for activity lifecycle.
+  override fun onActivityCreated(activity: Activity?, bundle: Bundle?) {}
+
+  override fun onActivityStarted(activity: Activity?) {}
+
+  override fun onActivityStopped(activity: Activity?) {}
+
+  override fun onActivitySaveInstanceState(activity: Activity?, bundle: Bundle?) {}
+
+  override fun onActivityDestroyed(activity: Activity?) {}
+
+  override fun onDestroy(owner: LifecycleOwner) {}
+
+  override fun onStop(owner: LifecycleOwner) {}
+
+  override fun onStart(owner: LifecycleOwner) {}
+
+  override fun onCreate(owner: LifecycleOwner) {}
+
+  internal class UiThreadExecutor : Executor {
+    val handler: Handler = Handler(Looper.getMainLooper())
+
+    override fun execute(command: Runnable) {
+      handler.post(command)
     }
-
-    @SuppressLint("SwitchIntDef")
-    override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-        val code: AuthResultCode?
-        when (errorCode) {
-            BiometricPrompt.ERROR_USER_CANCELED -> code = AuthResultCode.USER_CANCELED
-            BiometricPrompt.ERROR_NEGATIVE_BUTTON -> code = AuthResultCode.NEGATIVE_BUTTON
-            BiometricPrompt.ERROR_NO_DEVICE_CREDENTIAL -> code = AuthResultCode.NO_CREDENTIALS
-            BiometricPrompt.ERROR_NO_BIOMETRICS -> code = AuthResultCode.NOT_ENROLLED
-            BiometricPrompt.ERROR_HW_UNAVAILABLE -> code = AuthResultCode.HARDWARE_UNAVAILABLE
-            BiometricPrompt.ERROR_HW_NOT_PRESENT -> code = AuthResultCode.NO_HARDWARE
-            BiometricPrompt.ERROR_LOCKOUT -> code = AuthResultCode.LOCKED_OUT_TEMPORARILY
-            BiometricPrompt.ERROR_LOCKOUT_PERMANENT -> code = AuthResultCode.LOCKED_OUT_PERMANENTLY
-            BiometricPrompt.ERROR_CANCELED -> {
-                // If we are doing sticky auth and the activity has been paused,
-                // ignore this error. We will start listening again when resumed.
-                if (activityPaused && isAuthSticky) {
-                    return
-                }
-                code = AuthResultCode.SYSTEM_CANCELED
-            }
-
-            BiometricPrompt.ERROR_TIMEOUT -> code = AuthResultCode.TIMEOUT
-            BiometricPrompt.ERROR_NO_SPACE -> code = AuthResultCode.NO_SPACE
-            BiometricPrompt.ERROR_SECURITY_UPDATE_REQUIRED -> code =
-                AuthResultCode.SECURITY_UPDATE_REQUIRED
-
-            else -> code = AuthResultCode.UNKNOWN_ERROR
-        }
-        completionHandler.complete(AuthResult(code, errString.toString()))
-        stop()
-    }
-
-    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-        completionHandler.complete(AuthResult(AuthResultCode.SUCCESS, null))
-        stop()
-    }
-
-    override fun onAuthenticationFailed() {
-        // No-op; this is called for incremental failures. Wait for a final
-        // resolution via the success or error callbacks.
-    }
-
-    /**
-     * If the activity is paused, we keep track because biometric dialog simply returns "User
-     * cancelled" when the activity is paused.
-     */
-    override fun onActivityPaused(ignored: Activity?) {
-        if (isAuthSticky) {
-            activityPaused = true
-        }
-    }
-
-    override fun onActivityResumed(ignored: Activity?) {
-        if (isAuthSticky) {
-            activityPaused = false
-            val prompt = BiometricPrompt(activity, uiThreadExecutor, this)
-            // When activity is resuming, we cannot show the prompt right away. We need to post it to the
-            // UI queue.
-            uiThreadExecutor.handler.post(Runnable { prompt.authenticate(promptInfo) })
-        }
-    }
-
-    override fun onPause(owner: LifecycleOwner) {
-        onActivityPaused(null)
-    }
-
-    override fun onResume(owner: LifecycleOwner) {
-        onActivityResumed(null)
-    }
-
-    // Unused methods for activity lifecycle.
-    override fun onActivityCreated(activity: Activity?, bundle: Bundle?) {}
-
-    override fun onActivityStarted(activity: Activity?) {}
-
-    override fun onActivityStopped(activity: Activity?) {}
-
-    override fun onActivitySaveInstanceState(activity: Activity?, bundle: Bundle?) {}
-
-    override fun onActivityDestroyed(activity: Activity?) {}
-
-    override fun onDestroy(owner: LifecycleOwner) {}
-
-    override fun onStop(owner: LifecycleOwner) {}
-
-    override fun onStart(owner: LifecycleOwner) {}
-
-    override fun onCreate(owner: LifecycleOwner) {}
-
-    internal class UiThreadExecutor : Executor {
-        val handler: Handler = Handler(Looper.getMainLooper())
-
-        override fun execute(command: Runnable) {
-            handler.post(command)
-        }
-    }
+  }
 }
--- packages/local_auth/local_auth_android/android/src/main/kotlin/io/flutter/plugins/localauth/LocalAuthPlugin.kt
+++ packages/local_auth/local_auth_android/android/src/main/kotlin/io/flutter/plugins/localauth/LocalAuthPlugin.kt
@@ -24,225 +24,211 @@ import kotlin.jvm.functions.Function1
 /**
  * Flutter plugin providing access to local authentication.
  *
- *
  * Instantiate this in an add to app scenario to gracefully handle activity and context changes.
  */
 class LocalAuthPlugin
 /**
  * Default constructor for LocalAuthPlugin.
  *
- *
  * Use this constructor when adding this plugin to an app with v2 embedding.
  */
-    : FlutterPlugin, ActivityAware, LocalAuthApi {
-    @get:VisibleForTesting
-    var activity: Activity? = null
-        private set
-    private var authHelper: AuthenticationHelper? = null
+: FlutterPlugin, ActivityAware, LocalAuthApi {
+  @get:VisibleForTesting
+  var activity: Activity? = null
+    private set
 
-    @JvmField
-    @VisibleForTesting
-    val authInProgress: AtomicBoolean = AtomicBoolean(false)
+  private var authHelper: AuthenticationHelper? = null
 
-    private var lifecycle: Lifecycle? = null
-    private var biometricManager: BiometricManager? = null
-    private var keyguardManager: KeyguardManager? = null
+  @JvmField @VisibleForTesting val authInProgress: AtomicBoolean = AtomicBoolean(false)
 
-    override fun isDeviceSupported(): Boolean {
-        return this.isDeviceSecure || canAuthenticateWithBiometrics()
+  private var lifecycle: Lifecycle? = null
+  private var biometricManager: BiometricManager? = null
+  private var keyguardManager: KeyguardManager? = null
+
+  override fun isDeviceSupported(): Boolean {
+    return this.isDeviceSecure || canAuthenticateWithBiometrics()
+  }
+
+  override fun deviceCanSupportBiometrics(): Boolean {
+    return hasBiometricHardware()
+  }
+
+  override fun getEnrolledBiometrics(): MutableList<AuthClassification?>? {
+    if (biometricManager == null) {
+      return null
+    }
+    val biometrics = ArrayList<AuthClassification?>()
+    if (biometricManager!!.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK) ==
+        BiometricManager.BIOMETRIC_SUCCESS) {
+      biometrics.add(AuthClassification.WEAK)
+    }
+    if (biometricManager!!.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG) ==
+        BiometricManager.BIOMETRIC_SUCCESS) {
+      biometrics.add(AuthClassification.STRONG)
+    }
+    return biometrics
+  }
+
+  override fun stopAuthentication(): Boolean {
+    try {
+      if (authHelper != null && authInProgress.get()) {
+        authHelper!!.stopAuthentication()
+        authHelper = null
+      }
+      authInProgress.set(false)
+      return true
+    } catch (e: Exception) {
+      return false
+    }
+  }
+
+  override fun authenticate(
+      options: AuthOptions,
+      strings: AuthStrings,
+      callback: Function1<in Result<AuthResult>, Unit>
+  ) {
+    if (authInProgress.get()) {
+      completeWithValue<AuthResult>(callback, AuthResult(AuthResultCode.ALREADY_IN_PROGRESS, null))
+      return
     }
 
-    override fun deviceCanSupportBiometrics(): Boolean {
-        return hasBiometricHardware()
+    if (activity == null || activity!!.isFinishing()) {
+      completeWithValue<AuthResult>(callback, AuthResult(AuthResultCode.NO_ACTIVITY, null))
+      return
     }
 
-    override fun getEnrolledBiometrics(): MutableList<AuthClassification?>? {
-        if (biometricManager == null) {
-            return null
-        }
-        val biometrics = ArrayList<AuthClassification?>()
-        if (biometricManager!!.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK)
-            == BiometricManager.BIOMETRIC_SUCCESS
-        ) {
-            biometrics.add(AuthClassification.WEAK)
-        }
-        if (biometricManager!!.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)
-            == BiometricManager.BIOMETRIC_SUCCESS
-        ) {
-            biometrics.add(AuthClassification.STRONG)
-        }
-        return biometrics
+    if (activity !is FragmentActivity) {
+      completeWithValue<AuthResult>(
+          callback, AuthResult(AuthResultCode.NOT_FRAGMENT_ACTIVITY, null))
+      return
     }
 
-    override fun stopAuthentication(): Boolean {
-        try {
-            if (authHelper != null && authInProgress.get()) {
-                authHelper!!.stopAuthentication()
-                authHelper = null
-            }
-            authInProgress.set(false)
-            return true
-        } catch (e: Exception) {
-            return false
-        }
+    if (!isDeviceSupported()) {
+      completeWithValue<AuthResult>(callback, AuthResult(AuthResultCode.NO_CREDENTIALS, null))
+      return
     }
 
-    override fun authenticate(
-        options: AuthOptions,
-        strings: AuthStrings,
-        callback: Function1<in Result<AuthResult>, Unit>
-    ) {
-        if (authInProgress.get()) {
-            completeWithValue<AuthResult>(
-                callback, AuthResult(AuthResultCode.ALREADY_IN_PROGRESS, null)
-            )
-            return
-        }
+    authInProgress.set(true)
+    val completionHandler = createAuthCompletionHandler(callback)
 
-        if (activity == null || activity!!.isFinishing()) {
-            completeWithValue<AuthResult>(callback, AuthResult(AuthResultCode.NO_ACTIVITY, null))
-            return
-        }
+    val allowCredentials = !options.biometricOnly && canAuthenticateWithDeviceCredential()
 
-        if (activity !is FragmentActivity) {
-            completeWithValue<AuthResult>(
-                callback, AuthResult(AuthResultCode.NOT_FRAGMENT_ACTIVITY, null)
-            )
-            return
-        }
+    sendAuthenticationRequest(options, strings, allowCredentials, completionHandler)
+  }
 
-        if (!isDeviceSupported()) {
-            completeWithValue<AuthResult>(
-                callback, AuthResult(AuthResultCode.NO_CREDENTIALS, null)
-            )
-            return
-        }
+  @VisibleForTesting
+  fun createAuthCompletionHandler(
+      callback: Function1<in Result<AuthResult>, Unit>
+  ): AuthCompletionHandler {
+    return AuthCompletionHandler { authResult: AuthResult? ->
+      onAuthenticationCompleted(callback, authResult)
+    }
+  }
 
-        authInProgress.set(true)
-        val completionHandler = createAuthCompletionHandler(callback)
+  @VisibleForTesting
+  fun sendAuthenticationRequest(
+      options: AuthOptions,
+      strings: AuthStrings,
+      allowCredentials: Boolean,
+      completionHandler: AuthCompletionHandler
+  ) {
+    authHelper =
+        AuthenticationHelper(
+            lifecycle,
+            activity as FragmentActivity?,
+            options,
+            strings,
+            completionHandler,
+            allowCredentials)
 
-        val allowCredentials = !options.biometricOnly && canAuthenticateWithDeviceCredential()
+    authHelper!!.authenticate()
+  }
 
-        sendAuthenticationRequest(options, strings, allowCredentials, completionHandler)
+  fun onAuthenticationCompleted(
+      callback: Function1<in Result<AuthResult>, Unit>,
+      value: AuthResult?
+  ) {
+    if (authInProgress.compareAndSet(true, false)) {
+      completeWithValue<AuthResult>(callback, value!!)
+    }
+  }
+
+  @get:VisibleForTesting
+  val isDeviceSecure: Boolean
+    get() {
+      if (keyguardManager == null) return false
+      return keyguardManager!!.isDeviceSecure()
     }
 
-    @VisibleForTesting
-    fun createAuthCompletionHandler(
-        callback: Function1<in Result<AuthResult>, Unit>
-    ): AuthCompletionHandler {
-        return AuthCompletionHandler { authResult: AuthResult? ->
-            onAuthenticationCompleted(
-                callback,
-                authResult
-            )
-        }
+  private fun canAuthenticateWithBiometrics(): Boolean {
+    if (biometricManager == null) return false
+    return (biometricManager!!.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK) ==
+        BiometricManager.BIOMETRIC_SUCCESS)
+  }
+
+  private fun hasBiometricHardware(): Boolean {
+    if (biometricManager == null) return false
+    return (biometricManager!!.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK) !=
+        BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE)
+  }
+
+  @VisibleForTesting
+  fun canAuthenticateWithDeviceCredential(): Boolean {
+    if (Build.VERSION.SDK_INT < 30) {
+      // Checking for device credential only authentication via the BiometricManager
+      // is not allowed before API level 30, so we check for presence of PIN, pattern,
+      // or password instead.
+      return this.isDeviceSecure
     }
 
-    @VisibleForTesting
-    fun sendAuthenticationRequest(
-        options: AuthOptions,
-        strings: AuthStrings,
-        allowCredentials: Boolean,
-        completionHandler: AuthCompletionHandler
-    ) {
-        authHelper =
-            AuthenticationHelper(
-                lifecycle,
-                activity as FragmentActivity?,
-                options,
-                strings,
-                completionHandler,
-                allowCredentials
-            )
+    if (biometricManager == null) return false
+    return (biometricManager!!.canAuthenticate(BiometricManager.Authenticators.DEVICE_CREDENTIAL) ==
+        BiometricManager.BIOMETRIC_SUCCESS)
+  }
 
-        authHelper!!.authenticate()
-    }
+  override fun onAttachedToEngine(binding: FlutterPluginBinding) {
+    setUp(binding.getBinaryMessenger(), this)
+  }
 
-    fun onAuthenticationCompleted(
-        callback: Function1<in Result<AuthResult>, Unit>,
-        value: AuthResult?
-    ) {
-        if (authInProgress.compareAndSet(true, false)) {
-            completeWithValue<AuthResult>(callback, value!!)
-        }
-    }
+  override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
+    setUp(binding.getBinaryMessenger(), null)
+  }
 
-    @get:VisibleForTesting
-    val isDeviceSecure: Boolean
-        get() {
-            if (keyguardManager == null) return false
-            return keyguardManager!!.isDeviceSecure()
-        }
+  private fun setServicesFromActivity(activity: Activity?) {
+    if (activity == null) return
+    this.activity = activity
+    val context = activity.getBaseContext()
+    biometricManager = BiometricManager.from(activity)
+    keyguardManager = context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager?
+  }
 
-    private fun canAuthenticateWithBiometrics(): Boolean {
-        if (biometricManager == null) return false
-        return (biometricManager!!.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK)
-                == BiometricManager.BIOMETRIC_SUCCESS)
-    }
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    setServicesFromActivity(binding.getActivity())
+    lifecycle = FlutterLifecycleAdapter.getActivityLifecycle(binding)
+  }
 
-    private fun hasBiometricHardware(): Boolean {
-        if (biometricManager == null) return false
-        return (biometricManager!!.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK)
-                != BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE)
-    }
+  override fun onDetachedFromActivityForConfigChanges() {
+    lifecycle = null
+    activity = null
+  }
 
-    @VisibleForTesting
-    fun canAuthenticateWithDeviceCredential(): Boolean {
-        if (Build.VERSION.SDK_INT < 30) {
-            // Checking for device credential only authentication via the BiometricManager
-            // is not allowed before API level 30, so we check for presence of PIN, pattern,
-            // or password instead.
-            return this.isDeviceSecure
-        }
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    setServicesFromActivity(binding.getActivity())
+    lifecycle = FlutterLifecycleAdapter.getActivityLifecycle(binding)
+  }
 
-        if (biometricManager == null) return false
-        return (biometricManager!!.canAuthenticate(BiometricManager.Authenticators.DEVICE_CREDENTIAL)
-                == BiometricManager.BIOMETRIC_SUCCESS)
-    }
+  override fun onDetachedFromActivity() {
+    lifecycle = null
+    activity = null
+  }
 
-    override fun onAttachedToEngine(binding: FlutterPluginBinding) {
-        setUp(binding.getBinaryMessenger(), this)
-    }
+  @VisibleForTesting
+  fun setBiometricManager(biometricManager: BiometricManager?) {
+    this.biometricManager = biometricManager
+  }
 
-    override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
-        setUp(binding.getBinaryMessenger(), null)
-    }
-
-    private fun setServicesFromActivity(activity: Activity?) {
-        if (activity == null) return
-        this.activity = activity
-        val context = activity.getBaseContext()
-        biometricManager = BiometricManager.from(activity)
-        keyguardManager = context.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager?
-    }
-
-    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        setServicesFromActivity(binding.getActivity())
-        lifecycle = FlutterLifecycleAdapter.getActivityLifecycle(binding)
-    }
-
-    override fun onDetachedFromActivityForConfigChanges() {
-        lifecycle = null
-        activity = null
-    }
-
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-        setServicesFromActivity(binding.getActivity())
-        lifecycle = FlutterLifecycleAdapter.getActivityLifecycle(binding)
-    }
-
-    override fun onDetachedFromActivity() {
-        lifecycle = null
-        activity = null
-    }
-
-    @VisibleForTesting
-    fun setBiometricManager(biometricManager: BiometricManager?) {
-        this.biometricManager = biometricManager
-    }
-
-    @VisibleForTesting
-    fun setKeyguardManager(keyguardManager: KeyguardManager?) {
-        this.keyguardManager = keyguardManager
-    }
+  @VisibleForTesting
+  fun setKeyguardManager(keyguardManager: KeyguardManager?) {
+    this.keyguardManager = keyguardManager
+  }
 }
