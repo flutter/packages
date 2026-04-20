61911c2c3a86855b9733392ebc39af19d2bde018
Camille Simon <camillesimon@google.com>
2026-04-20 10:44:14 -0700

    restore android

--- packages/camera/camera_android_camerax/android/src/main/java/io/flutter/plugins/camerax/ProxyApiRegistrar.java
+++ packages/camera/camera_android_camerax/android/src/main/java/io/flutter/plugins/camerax/ProxyApiRegistrar.java
@@ -17,10 +17,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.LifecycleOwner;
 import io.flutter.plugin.common.BinaryMessenger;
-import io.flutter.plugin.platform.PlatformViewRegistry;
 import io.flutter.view.TextureRegistry;
-import androidx.camera.view.PreviewView;
-
 
 public class ProxyApiRegistrar extends CameraXLibraryPigeonProxyApiRegistrar {
   @NonNull
@@ -29,8 +26,6 @@ public class ProxyApiRegistrar extends CameraXLibraryPigeonProxyApiRegistrar {
   @NonNull private final TextureRegistry textureRegistry;
 
   @NonNull private Context context;
-
-  @NonNull private PlatformViewRegistry platformViewRegistry;
 
   @Nullable private CameraPermissionsManager.PermissionsRegistry permissionsRegistry;
 
@@ -70,11 +65,9 @@ public class ProxyApiRegistrar extends CameraXLibraryPigeonProxyApiRegistrar {
   public ProxyApiRegistrar(
       @NonNull BinaryMessenger binaryMessenger,
       @NonNull Context context,
-      @NonNull PlatformViewRegistry platformViewRegistry,
       @NonNull TextureRegistry textureRegistry) {
     super(binaryMessenger);
     this.context = context;
-    this.platformViewRegistry = platformViewRegistry;
     this.textureRegistry = textureRegistry;
   }
 
@@ -110,13 +103,6 @@ public class ProxyApiRegistrar extends CameraXLibraryPigeonProxyApiRegistrar {
 
   public void setContext(@NonNull Context context) {
     this.context = context;
-  }
-
-  public void registerPlatformView(@NonNull PreviewView previewPlatformView) {
-    platformViewRegistry
-    .registerViewFactory(
-                "plugins.flutter.dev/camera_android_camerax",
-                new CameraPreviewFactory(previewPlatformView));
   }
 
   @Nullable
@@ -449,11 +435,5 @@ public class ProxyApiRegistrar extends CameraXLibraryPigeonProxyApiRegistrar {
   @Override
   public PigeonApiImageProxyUtils getPigeonApiImageProxyUtils() {
     return new ImageProxyUtilsProxyApi(this);
-  }
-
-  @NonNull
-  @Override
-  public PigeonApiPreviewView getPigeonApiPreviewView() {
-    return new PreviewViewProxyApi(this);
   }
 }
