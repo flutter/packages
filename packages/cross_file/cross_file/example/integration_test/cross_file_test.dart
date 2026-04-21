a0169fb6ce9e1de42e68fde9d74beff64c9e1573
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2026-04-21 11:31:31 -0400

    add a test

--- packages/cross_file/cross_file/example/integration_test/cross_file_test.dart
+++ packages/cross_file/cross_file/example/integration_test/cross_file_test.dart
@@ -2,13 +2,32 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
+import 'package:cross_file/cross_file.dart';
+import 'package:cross_file_io/cross_file_io.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  // Since this test is lacking integration tests, this test ensures the example
-  // app can be launched on an emulator/device.
-  testWidgets('Launch Test', (WidgetTester tester) async {});
+  testWidgets('Can read from file using dart:io implementation', (
+    WidgetTester tester,
+  ) async {
+    final Directory dir = await getTemporaryDirectory();
+
+    final xDir = XDirectory.fromPath(dir.path);
+    expect(await xDir.exists(), isTrue);
+
+    final file = XFile.fromPath(path.join(dir.path, 'hello.txt'));
+    file.getExtension<IOXFileExtension>().file
+      ..createSync()
+      ..writeAsStringSync('Hello, World!');
+
+    expect(await file.readAsString(), 'Hello, World!');
+  }, skip: kIsWeb);
 }
