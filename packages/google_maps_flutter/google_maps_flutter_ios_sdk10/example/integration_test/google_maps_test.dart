9d1322f89e7afa8a1ff492f6c2837c3ccf035629
Stuart Morgan <stuartmorgan@google.com>
2026-04-23 11:35:44 -0400

    [google_maps_flutter] Replace use of zIndex in examples and tests
    Replaces uses of the deprecated `zIndex` in examples and tests with the
    preferred `zIndexInt`.
    
    This doesn't change the production use of `zIndex` in
    google_maps_flutter_web, as that could change behavior, so should stay
    for now.

--- packages/google_maps_flutter/google_maps_flutter_ios_sdk10/example/integration_test/google_maps_test.dart
+++ packages/google_maps_flutter/google_maps_flutter_ios_sdk10/example/integration_test/google_maps_test.dart
@@ -2274,7 +2274,7 @@ AdvancedMarker _copyAdvancedMarkerWithClusterManagerId(
     position: marker.position,
     rotation: marker.rotation,
     visible: marker.visible,
-    zIndex: marker.zIndex.toInt(),
+    zIndex: marker.zIndexInt,
     onTap: marker.onTap,
     onDragStart: marker.onDragStart,
     onDrag: marker.onDrag,
