flutter/packages

commit 27b12509e5203fea3aa2ed3cc4a4c38cf3349634

[google_maps_flutter] Add Advanced Markers support (#7882)
This PR adds Advanced Markers support to `google_maps_flutter` as discussed in [#155526](https://github.com/flutter/flutter/issues/155526). The document from the issue offers several options to implement Advanced Markers support, this PR uses option 1 (Advanced Marker Dart class is a subclass Marker class).

Summary of changes:
* Add map capability check
* Add `AdvancedMarker` class
* Add `MarkerCollisionBehavior` enum to control Advanced Marker's behavior when it collides with another marker
* Add `PinConfig` bitmap descriptor for customizing Advanced Marker's pin and icon
* Add `markerType` parameter to indicate that Advanced Markers should be used
* Rename `cloudMapId` to `mapId`

Notes:
* native implementations need to know what kind of marker should be used, this is needed to use correct marker options, cluster manager and marker controller. Indicating marker type is done by using a `markerType` option when creating a `GoogleMap` (could be `marker` or `advancedMarker`). Default option is `marker`
* `cloudMapId` is deprecated in favor of `mapId`. New name follows SDKs, documentation and Cloud Console naming.
* web implementation uses generics because `gmaps.Marker` and `gmaps.AdvancedMarkerElement` are not related to each other and should be handled differently
* legacy markers are deprecated but still supported in Maps Javascript API. `google_maps_flutter` still uses them by default in this PR because of backward-compatibility. [#130472](https://github.com/flutter/flutter/issues/130472) is related, package users will be able to use Advanced Markers to fix the deprecation warning
* Advanced Markers example on iOS  doesn't show advanced markers, seems to be [a known issue](https://issuetracker.google.com/issues/370536110)

packages/google_maps_flutter/google_maps_flutter/example/pubspec.yaml
@@ -3,8 +3,8 @@ description: Demonstrates how to use the google_maps_flutter plugin.
 publish_to: none
 
 environment:
-  sdk: ^3.9.0
-  flutter: ">=3.35.0"
+  sdk: ^3.10.0
+  flutter: ">=3.38.0"
 
 dependencies:
   cupertino_icons: ^1.0.5
@@ -18,8 +18,8 @@ dependencies:
     # The example app is bundled with the plugin so we use a path dependency on
     # the parent directory to use the current plugin's version.
     path: ../
-  google_maps_flutter_android: ^2.16.1
-  google_maps_flutter_platform_interface: ^2.14.0
+  google_maps_flutter_android: ^2.19.1
+  google_maps_flutter_platform_interface: ^2.14.2
 
 dev_dependencies:
   build_runner: ^2.1.10

packages/google_maps_flutter/google_maps_flutter/pubspec.yaml
@@ -2,11 +2,11 @@ name: google_maps_flutter
 description: A Flutter plugin for integrating Google Maps in iOS and Android applications.
 repository: https://github.com/flutter/packages/tree/main/packages/google_maps_flutter/google_maps_flutter
 issue_tracker: https://github.com/flutter/flutter/issues?q=is%3Aissue+is%3Aopen+label%3A%22p%3A+maps%22
-version: 2.14.2
+version: 2.15.0
 
 environment:
-  sdk: ^3.9.0
-  flutter: ">=3.35.0"
+  sdk: ^3.10.0
+  flutter: ">=3.38.0"
 
 flutter:
   plugin:
@@ -21,10 +21,10 @@ flutter:
 dependencies:
   flutter:
     sdk: flutter
-  google_maps_flutter_android: ^2.16.1
-  google_maps_flutter_ios: ^2.15.4
-  google_maps_flutter_platform_interface: ^2.14.0
-  google_maps_flutter_web: ^0.5.14
+  google_maps_flutter_android: ^2.19.1
+  google_maps_flutter_ios: ^2.18.0
+  google_maps_flutter_platform_interface: ^2.14.2
+  google_maps_flutter_web: ^0.6.1
 
 dev_dependencies:
   flutter_test:

packages/google_maps_flutter/google_maps_flutter/example/integration_test/src/maps_controller.dart
@@ -567,7 +567,7 @@ void runTests() {
     skip: isAndroid || isWeb || isIOS,
   );
 
-  testWidgets('testCloudMapId', (WidgetTester tester) async {
+  testWidgets('testMapId', (WidgetTester tester) async {
     final mapIdCompleter = Completer<int>();
     final Key key = GlobalKey();
 
@@ -579,7 +579,7 @@ void runTests() {
         onMapCreated: (GoogleMapController controller) {
           mapIdCompleter.complete(controller.mapId);
         },
-        cloudMapId: kCloudMapId,
+        mapId: kMapId,
       ),
     );
     await tester.pumpAndSettle();

packages/google_maps_flutter/google_maps_flutter/example/integration_test/src/shared.dart
@@ -22,7 +22,7 @@ const CameraPosition kInitialCameraPosition = CameraPosition(
 );
 
 // Dummy map ID
-const String kCloudMapId = '000000000000000'; // Dummy map ID.
+const String kMapId = '000000000000000'; // Dummy map ID.
 
 /// True if the test is running in an iOS device
 final bool isIOS = defaultTargetPlatform == TargetPlatform.iOS;

packages/google_maps_flutter/google_maps_flutter/example/lib/advanced_marker_icons.dart
@@ -1,0 +1,156 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+
+import 'page.dart';
+import 'place_advanced_marker.dart';
+
+/// Page that demonstrates how to use custom [AdvanceMarker] icons.
+class AdvancedMarkerIconsPage extends GoogleMapExampleAppPage {
+  /// Default constructor.
+  const AdvancedMarkerIconsPage({Key? key, required this.mapId})
+    : super(
+        key: key,
+        const Icon(Icons.image_outlined),
+        'Advanced marker icons',
+      );
+
+  /// Map ID to use for the GoogleMap.
+  final String? mapId;
+
+  @override
+  Widget build(BuildContext context) {
+    return _AdvancedMarkerIconsBody(mapId: mapId);
+  }
+}
+
+const LatLng _kMapCenter = LatLng(52.4478, -3.5402);
+
+class _AdvancedMarkerIconsBody extends StatefulWidget {
+  const _AdvancedMarkerIconsBody({required this.mapId});
+
+  /// Map ID to use for the GoogleMap.
+  final String? mapId;
+
+  @override
+  State<_AdvancedMarkerIconsBody> createState() =>
+      _AdvancedMarkerIconsBodyState();
+}
+
+class _AdvancedMarkerIconsBodyState extends State<_AdvancedMarkerIconsBody> {
+  final Set<AdvancedMarker> _markers = <AdvancedMarker>{};
+
+  GoogleMapController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        AdvancedMarkersCapabilityStatus(controller: controller),
+        Expanded(
+          child: GoogleMap(
+            mapId: widget.mapId,
+            markerType: GoogleMapMarkerType.advancedMarker,
+            initialCameraPosition: const CameraPosition(
+              target: _kMapCenter,
+              zoom: 7.0,
+            ),
+            markers: _markers,
+            onMapCreated: (GoogleMapController controller) {
+              setState(() {
+                this.controller = controller;
+              });
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: TextButton(
+            onPressed: _markers.isNotEmpty
+                ? null
+                : () async {
+                    final AssetMapBitmap asset = await BitmapDescriptor.asset(
+                      const ImageConfiguration(size: Size(12, 12)),
+                      'assets/red_square.png',
+                    );
+                    final AssetMapBitmap largeAsset =
+                        await BitmapDescriptor.asset(
+                          const ImageConfiguration(size: Size(36, 36)),
+                          'assets/red_square.png',
+                        );
+
+                    setState(() {
+                      _markers.addAll(<AdvancedMarker>[
+                        // Default icon
+                        AdvancedMarker(
+                          markerId: const MarkerId('1'),
+                          position: LatLng(
+                            _kMapCenter.latitude + 1,
+                            _kMapCenter.longitude + 1,
+                          ),
+                        ),
+                        // Custom pin colors
+                        AdvancedMarker(
+                          markerId: const MarkerId('2'),
+                          position: LatLng(
+                            _kMapCenter.latitude - 1,
+                            _kMapCenter.longitude - 1,
+                          ),
+                          icon: BitmapDescriptor.pinConfig(
+                            borderColor: Colors.red,
+                            backgroundColor: Colors.black,
+                            glyph: const CircleGlyph(color: Colors.red),
+                          ),
+                        ),
+                        // Pin with text
+                        AdvancedMarker(
+                          markerId: const MarkerId('3'),
+                          position: LatLng(
+                            _kMapCenter.latitude - 1,
+                            _kMapCenter.longitude + 1,
+                          ),
+                          icon: BitmapDescriptor.pinConfig(
+                            borderColor: Colors.blue,
+                            backgroundColor: Colors.white,
+                            glyph: const TextGlyph(
+                              text: 'Hi!',
+                              textColor: Colors.blue,
+                            ),
+                          ),
+                        ),
+                        // Pin with bitmap
+                        AdvancedMarker(
+                          markerId: const MarkerId('4'),
+                          position: LatLng(
+                            _kMapCenter.latitude + 1,
+                            _kMapCenter.longitude - 1,
+                          ),
+                          icon: BitmapDescriptor.pinConfig(
+                            borderColor: Colors.red,
+                            backgroundColor: Colors.white,
+                            glyph: BitmapGlyph(bitmap: asset),
+                          ),
+                        ),
+                        // Custom marker icon
+                        AdvancedMarker(
+                          markerId: const MarkerId('5'),
+                          position: LatLng(
+                            _kMapCenter.latitude,
+                            _kMapCenter.longitude,
+                          ),
+                          icon: largeAsset,
+                        ),
+                      ]);
+                    });
+                  },
+            child: const Text('Add advanced markers'),
+          ),
+        ),
+      ],
+    );
+  }
+}

packages/google_maps_flutter/google_maps_flutter/example/lib/advanced_markers_clustering.dart
@@ -6,25 +6,39 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
+import 'clustering.dart';
 import 'page.dart';
+import 'place_advanced_marker.dart';
 
-/// Page for demonstrating marker clustering support.
-class ClusteringPage extends GoogleMapExampleAppPage {
-  /// Default Constructor.
-  const ClusteringPage({Key? key})
-    : super(const Icon(Icons.place), 'Manage clustering', key: key);
+/// Page for demonstrating advanced marker clustering support.
+/// Same as [ClusteringPage] but works with [AdvancedMarker].
+class AdvancedMarkersClustering extends GoogleMapExampleAppPage {
+  /// Default constructor.
+  const AdvancedMarkersClustering({Key? key, required this.mapId})
+    : super(
+        key: key,
+        const Icon(Icons.place_outlined),
+        'Manage clusters of advanced markers',
+      );
+
+  /// Map ID to use for the GoogleMap.
+  final String? mapId;
 
   @override
   Widget build(BuildContext context) {
-    return const _ClusteringBody();
+    return _ClusteringBody(mapId: mapId);
   }
 }
 
 /// Body of the clustering page.
 class _ClusteringBody extends StatefulWidget {
   /// Default Constructor.
-  const _ClusteringBody();
+  const _ClusteringBody({required this.mapId});
+
+  /// Map ID to use for the GoogleMap.
+  final String? mapId;
 
   @override
   State<StatefulWidget> createState() => _ClusteringBodyState();
@@ -64,7 +78,7 @@ class _ClusteringBodyState extends State<_ClusteringBody> {
       <ClusterManagerId, ClusterManager>{};
 
   /// Map of markers with identifier as the key.
-  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  Map<MarkerId, AdvancedMarker> markers = <MarkerId, AdvancedMarker>{};
 
   /// Id of the currently selected marker.
   MarkerId? selectedMarker;
@@ -84,34 +98,40 @@ class _ClusteringBodyState extends State<_ClusteringBody> {
     });
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
   /// Returns selected or unselected state of the given [marker].
-  Marker copyWithSelectedState(Marker marker, bool isSelected) {
+  AdvancedMarker copyWithSelectedState(AdvancedMarker marker, bool isSelected) {
     return marker.copyWith(
       iconParam: isSelected
-          ? BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen)
-          : BitmapDescriptor.defaultMarker,
+          ? BitmapDescriptor.pinConfig(
+              backgroundColor: Colors.blue,
+              borderColor: Colors.white,
+              glyph: const CircleGlyph(color: Colors.white),
+            )
+          : BitmapDescriptor.pinConfig(
+              backgroundColor: Colors.white,
+              borderColor: Colors.blue,
+              glyph: const CircleGlyph(color: Colors.blue),
+            ),
     );
   }
 
   void _onMarkerTapped(MarkerId markerId) {
-    final Marker? tappedMarker = markers[markerId];
+    final AdvancedMarker? tappedMarker = markers[markerId];
     if (tappedMarker != null) {
       setState(() {
         final MarkerId? previousMarkerId = selectedMarker;
         if (previousMarkerId != null && markers.containsKey(previousMarkerId)) {
-          final Marker resetOld = copyWithSelectedState(
+          final AdvancedMarker resetOld = copyWithSelectedState(
             markers[previousMarkerId]!,
             false,
           );
           markers[previousMarkerId] = resetOld;
         }
         selectedMarker = markerId;
-        final Marker newMarker = copyWithSelectedState(tappedMarker, true);
+        final AdvancedMarker newMarker = copyWithSelectedState(
+          tappedMarker,
+          true,
+        );
         markers[markerId] = newMarker;
       });
     }
@@ -167,7 +187,7 @@ class _ClusteringBodyState extends State<_ClusteringBody> {
       final double clusterManagerLongitudeOffset =
           clusterManagerIndex * _clusterManagerLongitudeOffset;
 
-      final marker = Marker(
+      final marker = AdvancedMarker(
         markerId: markerId,
         clusterManagerId: clusterManager.clusterManagerId,
         position: LatLng(
@@ -176,6 +196,11 @@ class _ClusteringBodyState extends State<_ClusteringBody> {
         ),
         infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
         onTap: () => _onMarkerTapped(markerId),
+        icon: BitmapDescriptor.pinConfig(
+          backgroundColor: Colors.white,
+          borderColor: Colors.blue,
+          glyph: const CircleGlyph(color: Colors.blue),
+        ),
       );
       markers[markerId] = marker;
     }
@@ -196,7 +221,7 @@ class _ClusteringBodyState extends State<_ClusteringBody> {
 
   void _changeMarkersAlpha() {
     for (final MarkerId markerId in markers.keys) {
-      final Marker marker = markers[markerId]!;
+      final AdvancedMarker marker = markers[markerId]!;
       final double current = marker.alpha;
       markers[markerId] = marker.copyWith(
         alphaParam: current == _fullyVisibleAlpha
@@ -210,18 +235,23 @@ class _ClusteringBodyState extends State<_ClusteringBody> {
   @override
   Widget build(BuildContext context) {
     final MarkerId? selectedId = selectedMarker;
+    final Cluster? lastCluster = this.lastCluster;
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
+        AdvancedMarkersCapabilityStatus(controller: controller),
         SizedBox(
           height: 300.0,
           child: GoogleMap(
+            mapId: widget.mapId,
+            markerType: GoogleMapMarkerType.advancedMarker,
             onMapCreated: _onMapCreated,
             initialCameraPosition: const CameraPosition(
               target: LatLng(-33.852, 151.25),
               zoom: 11.0,
             ),
-            markers: Set<Marker>.of(markers.values),
+            markers: Set<AdvancedMarker>.of(markers.values),
             clusterManagers: Set<ClusterManager>.of(clusterManagers.values),
           ),
         ),
@@ -283,7 +313,7 @@ class _ClusteringBodyState extends State<_ClusteringBody> {
               Padding(
                 padding: const EdgeInsets.all(10),
                 child: Text(
-                  'Cluster with ${lastCluster!.count} markers clicked at ${lastCluster!.position}',
+                  'Cluster with ${lastCluster.count} markers clicked at ${lastCluster.position}',
                 ),
               ),
           ],

packages/google_maps_flutter/google_maps_flutter/example/lib/collision_behavior.dart
@@ -1,0 +1,154 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+
+import 'page.dart';
+import 'place_advanced_marker.dart';
+
+/// Page demonstrating how to use AdvancedMarker's collision behavior.
+class AdvancedMarkerCollisionBehaviorPage extends GoogleMapExampleAppPage {
+  /// Default constructor.
+  const AdvancedMarkerCollisionBehaviorPage({Key? key, required this.mapId})
+    : super(
+        const Icon(Icons.not_listed_location),
+        'Advanced marker collision behavior',
+        key: key,
+      );
+
+  /// Map ID to use for the GoogleMap.
+  final String? mapId;
+
+  @override
+  Widget build(BuildContext context) {
+    return _CollisionBehaviorPageBody(mapId: mapId);
+  }
+}
+
+class _CollisionBehaviorPageBody extends StatefulWidget {
+  const _CollisionBehaviorPageBody({required this.mapId});
+
+  final String? mapId;
+
+  @override
+  State<_CollisionBehaviorPageBody> createState() =>
+      _CollisionBehaviorPageBodyState();
+}
+
+class _CollisionBehaviorPageBodyState
+    extends State<_CollisionBehaviorPageBody> {
+  static const LatLng center = LatLng(-33.86711, 151.1947171);
+  static const double zoomOutLevel = 9;
+  static const double zoomInLevel = 12;
+
+  MarkerCollisionBehavior markerCollisionBehavior =
+      MarkerCollisionBehavior.optionalAndHidesLowerPriority;
+
+  GoogleMapController? controller;
+  final List<AdvancedMarker> markers = <AdvancedMarker>[];
+
+  void _addMarkers() {
+    final newMarkers = <AdvancedMarker>[
+      for (int i = 0; i < 12; i++)
+        AdvancedMarker(
+          markerId: MarkerId('marker_${i}_$markerCollisionBehavior'),
+          position: LatLng(
+            center.latitude + sin(i * pi / 6.0) / 20.0,
+            center.longitude + cos(i * pi / 6.0) / 20.0,
+          ),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+          collisionBehavior: markerCollisionBehavior,
+        ),
+    ];
+
+    markers.clear();
+    markers.addAll(newMarkers);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        AdvancedMarkersCapabilityStatus(controller: controller),
+        Expanded(
+          child: GoogleMap(
+            mapId: widget.mapId,
+            markerType: GoogleMapMarkerType.advancedMarker,
+            initialCameraPosition: const CameraPosition(
+              target: center,
+              zoom: zoomInLevel,
+            ),
+            markers: Set<AdvancedMarker>.of(markers),
+            tiltGesturesEnabled: false,
+            zoomGesturesEnabled: false,
+            rotateGesturesEnabled: false,
+            scrollGesturesEnabled: false,
+            onMapCreated: (GoogleMapController controller) {
+              setState(() {
+                this.controller = controller;
+              });
+            },
+          ),
+        ),
+        const SizedBox(height: 12),
+        Text(
+          'Current collision behavior: ${markerCollisionBehavior.name}',
+          style: Theme.of(context).textTheme.labelLarge,
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 8),
+        Wrap(
+          alignment: WrapAlignment.spaceEvenly,
+          children: <Widget>[
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _addMarkers();
+                });
+              },
+              child: const Text('Add markers'),
+            ),
+            TextButton(
+              onPressed: () {
+                controller?.animateCamera(
+                  CameraUpdate.newCameraPosition(
+                    const CameraPosition(target: center, zoom: zoomOutLevel),
+                  ),
+                );
+              },
+              child: const Text('Zoom out'),
+            ),
+            TextButton(
+              onPressed: () {
+                controller?.animateCamera(
+                  CameraUpdate.newCameraPosition(
+                    const CameraPosition(target: center, zoom: zoomInLevel),
+                  ),
+                );
+              },
+              child: const Text('Zoom in'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  markerCollisionBehavior =
+                      markerCollisionBehavior ==
+                          MarkerCollisionBehavior.optionalAndHidesLowerPriority
+                      ? MarkerCollisionBehavior.requiredDisplay
+                      : MarkerCollisionBehavior.optionalAndHidesLowerPriority;
+                  _addMarkers();
+                });
+              },
+              child: const Text('Toggle collision behavior'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}

packages/google_maps_flutter/google_maps_flutter/example/lib/main.dart
@@ -8,8 +8,11 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
+import 'advanced_marker_icons.dart';
+import 'advanced_markers_clustering.dart';
 import 'animate_camera.dart';
 import 'clustering.dart';
+import 'collision_behavior.dart';
 import 'ground_overlay.dart';
 import 'heatmap.dart';
 import 'lite_mode.dart';
@@ -21,6 +24,7 @@ import 'marker_icons.dart';
 import 'move_camera.dart';
 import 'padding.dart';
 import 'page.dart';
+import 'place_advanced_marker.dart';
 import 'place_circle.dart';
 import 'place_marker.dart';
 import 'place_polygon.dart';
@@ -29,6 +33,10 @@ import 'scrolling_map.dart';
 import 'snapshot.dart';
 import 'tile_overlay.dart';
 
+/// Place your map ID here. Map ID is required for pages that use advanced
+/// markers.
+const String? _mapId = null;
+
 final List<GoogleMapExampleAppPage> _allPages = <GoogleMapExampleAppPage>[
   const MapUiPage(),
   const MapCoordinatesPage(),
@@ -36,7 +44,9 @@ final List<GoogleMapExampleAppPage> _allPages = <GoogleMapExampleAppPage>[
   const AnimateCameraPage(),
   const MoveCameraPage(),
   const PlaceMarkerPage(),
+  const PlaceAdvancedMarkerPage(mapId: _mapId),
   const MarkerIconsPage(),
+  const AdvancedMarkerIconsPage(mapId: _mapId),
   const ScrollingMapPage(),
   const PlacePolylinePage(),
   const PlacePolygonPage(),
@@ -47,8 +57,10 @@ final List<GoogleMapExampleAppPage> _allPages = <GoogleMapExampleAppPage>[
   const TileOverlayPage(),
   const GroundOverlayPage(),
   const ClusteringPage(),
+  const AdvancedMarkersClustering(mapId: _mapId),
   const MapIdPage(),
   const HeatmapPage(),
+  const AdvancedMarkerCollisionBehaviorPage(mapId: _mapId),
 ];
 
 /// MapsDemo is the Main Application.

commit 24e03181f2115cd1089f6c50784054e0ab212bc4

[google_maps_flutter] Fix styles on web
Fixes a regression from https://github.com/flutter/packages/pull/7882
that caused non-cloud map styling not be applied on web, since the
app-facing package changed to sending '' rather than null when a cloud
mapId wasn't provided, and the web implementation didn't handle that
case.

Fixes https://github.com/flutter/flutter/issues/185171

packages/google_maps_flutter/google_maps_flutter_web/lib/src/convert.dart
@@ -124,12 +124,17 @@ gmaps.MapOptions _configurationAndStyleToGmapsOptions(
   options.fullscreenControl = false;
   options.streetViewControl = false;
 
+  // Treat an empty mapId as null, as the app-facing package may pass it either
+  // way.
+  final String? mapId = configuration.mapId?.isEmpty ?? true
+      ? null
+      : configuration.mapId;
   // If using cloud map, do not set options.styles
-  if (configuration.mapId == null) {
+  if (mapId == null) {
     options.styles = styles;
   }
 
-  options.mapId = configuration.mapId;
+  options.mapId = mapId;
 
   final gmaps.ColorScheme? jsColorScheme = _gmapTypeColorSchemeForPluginColor(
     configuration.colorScheme,

packages/google_maps_flutter/google_maps_flutter_web/pubspec.yaml
@@ -2,7 +2,7 @@ name: google_maps_flutter_web
 description: Web platform implementation of google_maps_flutter
 repository: https://github.com/flutter/packages/tree/main/packages/google_maps_flutter/google_maps_flutter_web
 issue_tracker: https://github.com/flutter/flutter/issues?q=is%3Aissue+is%3Aopen+label%3A%22p%3A+maps%22
-version: 0.6.2
+version: 0.6.2+1
 
 environment:
   sdk: ^3.9.0

packages/google_maps_flutter/google_maps_flutter_web/example/integration_test/cloud_map_styles_test.dart
@@ -26,7 +26,7 @@ void main() {
     textDirection: TextDirection.ltr,
   );
 
-  testWidgets('cloudMapId present => mapId set & styles omitted', (
+  testWidgets('cloud mapId present => mapId set & styles omitted', (
     WidgetTester tester,
   ) async {
     const testMapConfig = MapConfiguration(mapId: 'test-cloud-map-id');
@@ -46,7 +46,7 @@ void main() {
       mapId: 1, // Internal controller ID
       streamController: stream,
       widgetConfiguration: cfg(),
-      mapConfiguration: testMapConfig, // cloudMapId is set here
+      mapConfiguration: testMapConfig, // Cloud mapId is set here
     );
 
     controller.debugSetOverrides(
@@ -70,13 +70,13 @@ void main() {
     expect(
       captured!.styles == null || captured!.styles!.isEmpty,
       isTrue,
-      reason: 'When cloudMapId is set, styles must not be applied.',
+      reason: 'When cloud mapId is set, styles must not be applied.',
     );
 
     controller.dispose();
   });
 
-  testWidgets('no cloudMapId => styles applied', (WidgetTester tester) async {
+  testWidgets('no cloud mapId => styles applied', (WidgetTester tester) async {
     await tester.pumpWidget(
       const Directionality(textDirection: TextDirection.ltr, child: SizedBox()),
     );
@@ -113,13 +113,65 @@ void main() {
     expect(
       captured!.mapId,
       anyOf(isNull, isEmpty),
-      reason: 'mapId should be empty/null when no Cloud Map is used.',
+      reason: 'mapId should be empty/null when no cloud mapId is used.',
     );
     expect(captured!.styles, isNotNull);
     expect(
       captured!.styles,
       isNotEmpty,
-      reason: 'When cloudMapId is null, styles should be applied.',
+      reason: 'When cloud mapId is null, styles should be applied.',
+    );
+
+    controller.dispose();
+  });
+
+  testWidgets('empty cloud mapId is treated as no mapId', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const Directionality(textDirection: TextDirection.ltr, child: SizedBox()),
+    );
+
+    final stream = StreamController<MapEvent<Object?>>();
+    addTearDown(() {
+      // Stream is closed by controller.dispose()
+    });
+
+    gmaps.MapOptions? captured;
+    final controller = GoogleMapController(
+      mapId: 2, // Internal controller ID
+      streamController: stream,
+      widgetConfiguration: cfg(),
+      mapConfiguration: const MapConfiguration(mapId: ''),
+    );
+
+    controller.debugSetOverrides(
+      setOptions: (gmaps.MapOptions options) {
+        captured = options;
+      },
+    );
+
+    final styles = <gmaps.MapTypeStyle>[
+      gmaps.MapTypeStyle()
+        ..featureType = 'poi'
+        ..elementType = 'labels',
+    ];
+
+    controller.updateStyles(styles);
+
+    await tester.pump();
+
+    expect(captured, isNotNull);
+    expect(
+      captured!.mapId,
+      anyOf(isNull, isEmpty),
+      reason: 'mapId should be empty/null when no cloud mapId is used.',
+    );
+    expect(captured!.styles, isNotNull);
+    expect(
+      captured!.styles,
+      isNotEmpty,
+      reason: 'When cloud mapId is empty, styles should be applied.',
     );
 
     controller.dispose();