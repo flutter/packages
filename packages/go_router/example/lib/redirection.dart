757d561fe8c117c705bef8e8c9549e4eda080931
Chun-Heng Tai <chtai@google.com>
2026-05-04 11:55:02 -0700

    [go_router] remove provider from examples

--- packages/go_router/example/lib/redirection.dart
+++ packages/go_router/example/lib/redirection.dart
@@ -4,7 +4,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
 // This scenario demonstrates how to use redirect to handle a sign-in flow.
 //
@@ -34,6 +33,25 @@ class LoginInfo extends ChangeNotifier {
   }
 }
 
+/// Provides login information to its descendants.
+class LoginInfoProvider extends InheritedNotifier<LoginInfo> {
+  /// Creates a [LoginInfoProvider].
+  const LoginInfoProvider({
+    super.key,
+    required super.notifier,
+    required super.child,
+  });
+
+  /// Returns the [LoginInfo] from the closest [LoginInfoProvider] ancestor.
+  static LoginInfo of(BuildContext context, {bool listen = true}) {
+    if (listen) {
+      return context.dependOnInheritedWidgetOfExactType<LoginInfoProvider>()!.notifier!;
+    } else {
+      return (context.getElementForInheritedWidgetOfExactType<LoginInfoProvider>()!.widget as LoginInfoProvider).notifier!;
+    }
+  }
+}
+
 void main() => runApp(App());
 
 /// The main app.
@@ -48,8 +66,8 @@ class App extends StatelessWidget {
 
   // add the login info into the tree as app state that can change over time
   @override
-  Widget build(BuildContext context) => ChangeNotifierProvider<LoginInfo>.value(
-    value: _loginInfo,
+  Widget build(BuildContext context) => LoginInfoProvider(
+    notifier: _loginInfo,
     child: MaterialApp.router(
       routerConfig: _router,
       title: title,
@@ -107,7 +125,7 @@ class LoginScreen extends StatelessWidget {
       child: ElevatedButton(
         onPressed: () {
           // log a user in, letting all the listeners know
-          context.read<LoginInfo>().login('test-user');
+          LoginInfoProvider.of(context, listen: false).login('test-user');
 
           // router will automatically redirect from /login to / using
           // refreshListenable
@@ -125,7 +143,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final LoginInfo info = context.read<LoginInfo>();
+    final LoginInfo info = LoginInfoProvider.of(context, listen: false);
 
     return Scaffold(
       appBar: AppBar(
