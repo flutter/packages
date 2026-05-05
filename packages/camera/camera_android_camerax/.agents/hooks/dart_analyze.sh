9f77fbefce299ee706128e4b3720a128d01dc5ff
Reid Baker <reidbaker@google.com>
2026-05-05 16:48:26 -0400

    add licence to hooks

--- packages/camera/camera_android_camerax/.agents/hooks/dart_analyze.sh
+++ packages/camera/camera_android_camerax/.agents/hooks/dart_analyze.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright 2013 The Flutter Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 # .agents/hooks/dart_analyze.sh
 
 LOGFILE="/tmp/jetski_hooks.log"
