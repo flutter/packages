7ffe1f1bec020e285397345894872d4c8b4153bb
Justin McCandless <jmccandless@google.com>
2026-05-07 11:24:45 -0700

    [cupertino_ui,material_ui] Post-import adjustments for new package-based structure.
    A single squashed commit of all work after copying the Material and
    Cupertino libraries, and related code, into material_ui and
    cupertino_ui. Puts the code in the right place and sets it all up
    properly in the new repo so that it passes CI.
    
     * Copied code was moved to material_ui and cupertino_ui.
     * Imports of flutter/material.dart and flutter/cupertino.dart were
       changed to material_ui/material_ui.dart and
       cupertino_ui/cupertino_ui.dart, respectively.
     * Relative imports were updated to new paths or packages to work in
       their new location.
     * The flutter_goldens package was referenced from
       script/flutter_goldens instead of the flutter/flutter sdk version.
     * Dependencies referenced with `any` version were updated to the
       caret version in flutter/flutter's lock file.
     * Examples were set up as their own packages in the `example` directory.
     * READMEs were updated to be consistent with the new setup.
     * Pubspecs were updated to work with the new setup. Set to Flutter
       3.44.0, which is the stable version first containing the code freeze.
     * Dart data-driven fixes were set up in the fix_data directories along
       with their tests in the test_fixes directories.
     * Doc imports were updated to work in the new setup.
     * One dart fix test was removed since it was already tested in
       flutter/flutter's widgets library.
     * The drag_target dart fix was deleted and kept in flutter/flutter.
     * Other widgets-only dart fixes were deleted in cupertino_ui for the
       same reason.
     * Tests with cross imports were skipped and moved to the
       temporarily_skipped_tests directories, to be fixed before 1.0.0.
     * Formatting was adjusted to pass analysis here.
     * A usage of flutter_test's byTooltip finder was removed since it
       contains a cross import of material.
     * License headers were updated to match this repository.
     * Batch release was disabled for now (until we're ready to release
       1.0.0).
     * Exceptions were added for some pinned dependencies that came from
       flutter/flutter.
     * Some fixes and skips were added to the example tests in order to be
       able to run them on Chrome. It seems that these tests were never run
       on web in flutter/flutter.

---
+++ packages/cupertino_ui/example/test/list_section/list_section_inset.0_test.dart
@@ -0,0 +1,34 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:cupertino_ui/cupertino_ui.dart';
+import 'package:cupertino_ui_examples/list_section/list_section_inset.0.dart'
+    as example;
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Has exactly 1 CupertinoListSection inset grouped widget', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const example.CupertinoListSectionInsetApp());
+
+    final Finder listSectionFinder = find.byType(CupertinoListSection);
+    expect(listSectionFinder, findsOneWidget);
+
+    final CupertinoListSection listSectionWidget = tester
+        .widget<CupertinoListSection>(listSectionFinder);
+    expect(
+      listSectionWidget.type,
+      equals(CupertinoListSectionType.insetGrouped),
+    );
+  });
+
+  testWidgets('CupertinoListSection has 3 CupertinoListTile children', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const example.CupertinoListSectionInsetApp());
+
+    expect(find.byType(CupertinoListTile), findsNWidgets(3));
+  });
+}
---
+++ packages/material_ui/example/test/popup_menu/popup_menu.1_test.dart
@@ -0,0 +1,27 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:material_ui/material_ui.dart';
+import 'package:material_ui_examples/popup_menu/popup_menu.1.dart' as example;
+
+void main() {
+  testWidgets('Can open popup menu', (WidgetTester tester) async {
+    const String menuItem = 'Item 1';
+
+    await tester.pumpWidget(const example.PopupMenuApp());
+
+    expect(find.text(menuItem), findsNothing);
+
+    // Open popup menu.
+    await tester.tap(find.byIcon(Icons.adaptive.more));
+    await tester.pumpAndSettle();
+    expect(find.text(menuItem), findsOneWidget);
+
+    // Close popup menu.
+    await tester.tapAt(const Offset(1, 1));
+    await tester.pumpAndSettle();
+    expect(find.text(menuItem), findsNothing);
+  });
+}
