5c7f674c97714dd21964183296a90bd5d6863dce
Justin McCandless <jmccandless@google.com>
2026-05-08 15:44:42 -0700

    Skip cupertino_ui test files that fail due to cross imports.

--- packages/cupertino_ui/test/cupertino/menu_anchor_test.dart
+++ packages/cupertino_ui/test/cupertino/menu_anchor_test.dart
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+@Skip(
+  'This file is skipped due to a cross-import that needs to be fixed. Tracked in https://github.com/flutter/flutter/issues/177028.',
+)
+
 import 'dart:async';
 import 'dart:math' as math;
 import 'dart:ui' as ui;
