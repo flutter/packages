066dcbf7598c2d4f69e5964940cbec5d551de843
Qun Cheng <36861262+QuncCccccc@users.noreply.github.com>
2026-05-22 10:39:16 -0700

    [material_ui]Project import generated by Copybara. (#11756)
    part of https://github.com/flutter/flutter/issues/184950
    
    ## Pre-Review Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [AI contribution guidelines] and understand my
    responsibilities, or I am not using AI tools.
    - [x] I read the [Tree Hygiene] page, which explains my
    responsibilities.
    - [x] I read and followed the [relevant style guides] and ran [the
    auto-formatter].
    - [x] I signed the [CLA].
    - [x] The title of the PR starts with the name of the package surrounded
    by square brackets, e.g. `[shared_preferences]`
    - [x] I [linked to at least one issue that this PR fixes] in the
    description above.
    - [ ] I followed [the version and CHANGELOG instructions], using
    [semantic versioning] and the [repository CHANGELOG style], or I have
    commented below to indicate which documented exception this PR falls
    under[^1].
    - [x] I updated/added any relevant documentation (doc comments with
    `///`).
    - [ ] I added new tests to check the change I am making, or I have
    commented below to indicate which [test exemption] this PR falls
    under[^1].
    - [ ] All existing and new tests are passing.
    
    If you need help, consider asking for advice on the #hackers-new channel
    on [Discord].
    
    **Note**: The Flutter team is currently trialing the use of [Gemini Code
    Assist for
    GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code).
    Comments from the `gemini-code-assist` bot should not be taken as
    authoritative feedback from the Flutter team. If you find its comments
    useful you can update your code accordingly, but if you are unsure or
    disagree with the feedback, please feel free to wait for a Flutter team
    member's review for guidance on which automated comments should be
    addressed.
    
    [^1]: Regular contributors who have demonstrated familiarity with the
    repository guidelines only need to comment if the PR is not
    auto-exempted by repo tooling.
    
    <!-- Links -->
    [Contributor Guide]:
    https://github.com/flutter/packages/blob/main/CONTRIBUTING.md
    [AI contribution guidelines]:
    https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#ai-contribution-guidelines
    [Tree Hygiene]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Tree-hygiene.md
    [relevant style guides]:
    https://github.com/flutter/packages/blob/main/CONTRIBUTING.md#style
    [the auto-formatter]:
    https://github.com/flutter/packages/blob/main/script/tool/README.md#format-code
    [CLA]: https://cla.developers.google.com/
    [Discord]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Chat.md
    [linked to at least one issue that this PR fixes]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Tree-hygiene.md#overview
    [the version and CHANGELOG instructions]:
    https://github.com/flutter/flutter/blob/master/docs/ecosystem/contributing/README.md#version-and-changelog-updates
    [semantic versioning]:
    https://dart.dev/tools/pub/versioning#semantic-versions
    [repository CHANGELOG style]:
    https://github.com/flutter/flutter/blob/master/docs/ecosystem/contributing/README.md#changelog-style
    [test exemption]:
    https://github.com/flutter/flutter/blob/master/docs/contributing/Tree-hygiene.md#tests
    
    Co-authored-by: Flutter Material Tokens Copybara Bot <flutter-material-tokens-copybara@google.com>

---
+++ packages/material_ui/tool/gen_defaults/data/app_bar_medium_flexible.dart
@@ -0,0 +1,43 @@
+// Copyright 2013 The Flutter Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Version: 36.0.6
+
+class TokenAppBarMediumFlexible {
+  /// md.comp.app-bar.medium-flexible.container.height
+  static const double containerHeight = 112.00;
+
+  /// md.comp.app-bar.medium-flexible.subtitle.font
+  static const double subtitleFontFontSize = 14.00;
+
+  /// md.comp.app-bar.medium-flexible.subtitle.font
+  static const double subtitleFontFontWeight = 500;
+
+  /// md.comp.app-bar.medium-flexible.subtitle.font
+  static const double subtitleFontLineHeight = 20.00;
+
+  /// md.comp.app-bar.medium-flexible.subtitle.font
+  static const double subtitleFontLetterSpacing = 0.10;
+
+  /// md.comp.app-bar.medium-flexible.subtitle.font
+  static const String subtitleFontFontFamily = 'Roboto';
+
+  /// md.comp.app-bar.medium-flexible.title.font
+  static const double titleFontFontSize = 28.00;
+
+  /// md.comp.app-bar.medium-flexible.title.font
+  static const double titleFontFontWeight = 400;
+
+  /// md.comp.app-bar.medium-flexible.title.font
+  static const double titleFontLineHeight = 36.00;
+
+  /// md.comp.app-bar.medium-flexible.title.font
+  static const double titleFontLetterSpacing = 0.00;
+
+  /// md.comp.app-bar.medium-flexible.title.font
+  static const String titleFontFontFamily = 'Roboto';
+
+  /// md.comp.app-bar.medium-flexible.with-subtitle.container.height
+  static const double withSubtitleContainerHeight = 136.00;
+}
---
+++ packages/material_ui/tool/gen_defaults/data/color_role.dart
@@ -0,0 +1,151 @@
+// Copyright 2013 The Flutter Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Version: 36.0.6
+
+enum TokenColorRole {
+  /// md.sys.color.background
+  background,
+
+  /// md.sys.color.error
+  error,
+
+  /// md.sys.color.error-container
+  errorContainer,
+
+  /// md.sys.color.inverse-on-surface
+  inverseOnSurface,
+
+  /// md.sys.color.inverse-primary
+  inversePrimary,
+
+  /// md.sys.color.inverse-surface
+  inverseSurface,
+
+  /// md.sys.color.on-background
+  onBackground,
+
+  /// md.sys.color.on-error
+  onError,
+
+  /// md.sys.color.on-error-container
+  onErrorContainer,
+
+  /// md.sys.color.on-primary
+  onPrimary,
+
+  /// md.sys.color.on-primary-container
+  onPrimaryContainer,
+
+  /// md.sys.color.on-primary-fixed
+  onPrimaryFixed,
+
+  /// md.sys.color.on-primary-fixed-variant
+  onPrimaryFixedVariant,
+
+  /// md.sys.color.on-secondary
+  onSecondary,
+
+  /// md.sys.color.on-secondary-container
+  onSecondaryContainer,
+
+  /// md.sys.color.on-secondary-fixed
+  onSecondaryFixed,
+
+  /// md.sys.color.on-secondary-fixed-variant
+  onSecondaryFixedVariant,
+
+  /// md.sys.color.on-surface
+  onSurface,
+
+  /// md.sys.color.on-surface-variant
+  onSurfaceVariant,
+
+  /// md.sys.color.on-tertiary
+  onTertiary,
+
+  /// md.sys.color.on-tertiary-container
+  onTertiaryContainer,
+
+  /// md.sys.color.on-tertiary-fixed
+  onTertiaryFixed,
+
+  /// md.sys.color.on-tertiary-fixed-variant
+  onTertiaryFixedVariant,
+
+  /// md.sys.color.outline
+  outline,
+
+  /// md.sys.color.outline-variant
+  outlineVariant,
+
+  /// md.sys.color.primary
+  primary,
+
+  /// md.sys.color.primary-container
+  primaryContainer,
+
+  /// md.sys.color.primary-fixed
+  primaryFixed,
+
+  /// md.sys.color.primary-fixed-dim
+  primaryFixedDim,
+
+  /// md.sys.color.scrim
+  scrim,
+
+  /// md.sys.color.secondary
+  secondary,
+
+  /// md.sys.color.secondary-container
+  secondaryContainer,
+
+  /// md.sys.color.secondary-fixed
+  secondaryFixed,
+
+  /// md.sys.color.secondary-fixed-dim
+  secondaryFixedDim,
+
+  /// md.sys.color.shadow
+  shadow,
+
+  /// md.sys.color.surface
+  surface,
+
+  /// md.sys.color.surface-bright
+  surfaceBright,
+
+  /// md.sys.color.surface-container
+  surfaceContainer,
+
+  /// md.sys.color.surface-container-high
+  surfaceContainerHigh,
+
+  /// md.sys.color.surface-container-highest
+  surfaceContainerHighest,
+
+  /// md.sys.color.surface-container-low
+  surfaceContainerLow,
+
+  /// md.sys.color.surface-container-lowest
+  surfaceContainerLowest,
+
+  /// md.sys.color.surface-dim
+  surfaceDim,
+
+  /// md.sys.color.surface-variant
+  surfaceVariant,
+
+  /// md.sys.color.tertiary
+  tertiary,
+
+  /// md.sys.color.tertiary-container
+  tertiaryContainer,
+
+  /// md.sys.color.tertiary-fixed
+  tertiaryFixed,
+
+  /// md.sys.color.tertiary-fixed-dim
+  tertiaryFixedDim,
+}
