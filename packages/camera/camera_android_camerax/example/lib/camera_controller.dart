0a7d03db6f8138efb943f63cf12f088bac435c6d
Mairramer <mairramer.dasilva28@hotmail.com>
2026-05-24 11:54:48 -0300

    fix parameter

--- packages/camera/camera_android_camerax/example/lib/camera_controller.dart
+++ packages/camera/camera_android_camerax/example/lib/camera_controller.dart
@@ -502,6 +502,7 @@ class CameraController extends ValueNotifier<CameraValue> {
   Future<void> startVideoRecording({
     onLatestImageAvailable? onAvailable,
     bool enablePersistentRecording = true,
+    String? videoOutputPath,
   }) async {
     _throwIfNotInitialized('startVideoRecording');
     if (value.isRecordingVideo) {
@@ -524,6 +525,7 @@ class CameraController extends ValueNotifier<CameraValue> {
           _cameraId,
           streamCallback: streamCallback,
           enablePersistentRecording: enablePersistentRecording,
+          videoOutputPath: videoOutputPath,
         ),
       );
       value = value.copyWith(
