81482e789b345834fd3c1db88b7eefef6c67e36a
Qun Cheng <quncheng@google.com>
2026-05-28 17:24:05 -0700

    Add M3E IconButton

--- packages/material_ui/tool/gen_defaults/bin/gen_defaults.dart
+++ packages/material_ui/tool/gen_defaults/bin/gen_defaults.dart
@@ -30,10 +30,8 @@ import '../templates/chip_template.dart';
 // import '../templates/expansion_tile_template.dart';
 // import '../templates/fab_template.dart';
 import '../templates/filter_chip_template.dart';
-
-// import '../templates/icon_button_template.dart';
+import '../templates/icon_button_template.dart';
 import '../templates/input_chip_template.dart';
-
 // import '../templates/input_decorator_template.dart';
 // import '../templates/list_tile_template.dart';
 // import '../templates/menu_template.dart';
@@ -89,7 +87,7 @@ Future<void> main(List<String> args) async {
   // const ExpansionTileTemplateM3().generateFile(verbose: verbose);
   // const FabTemplateM3().generateFile(verbose: verbose);
   const FilterChipTemplateM3().generateFile(verbose: verbose);
-  // const IconButtonTemplateM3().generateFile(verbose: verbose);
+  const IconButtonTemplate().generateFile(verbose: verbose);
   const InputChipTemplateM3().generateFile(verbose: verbose);
   // const InputDecoratorTemplateM3().generateFile(verbose: verbose);
   // const ListTileTemplateM3().generateFile(verbose: verbose);
