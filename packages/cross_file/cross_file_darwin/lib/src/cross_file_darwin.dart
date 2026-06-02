eab82fc37cdf39bb3aeb456320ae9fc1dcb1c2e1
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2026-06-02 13:53:46 -0400

    add dynamic library

--- packages/cross_file/cross_file_darwin/lib/src/cross_file_darwin.dart
+++ packages/cross_file/cross_file_darwin/lib/src/cross_file_darwin.dart
@@ -2,18 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:ffi' as ffi;
+
 import 'package:cross_file_io/cross_file_io.dart';
 import 'package:cross_file_platform_interface/cross_file_platform_interface.dart';
 
 import 'darwin_scoped_storage_cross_directory.dart';
 import 'darwin_scoped_storage_cross_file.dart';
-import 'ffi_bindings.g.dart';
 
 /// Implementation of [CrossFilePlatform] for iOS and macOS.
 base class CrossFileDarwin extends CrossFileIO {
   /// Registers this class as the default instance of [CrossFilePlatform].
   static void registerWith() {
     CrossFilePlatform.instance = CrossFileDarwin();
+    ffi.DynamicLibrary.open(
+      '/System/Library/Frameworks/Photos.framework/Versions/Current/Photos',
+    );
   }
 
   /// Whether the current implementation of `cross_file` is [CrossFileDarwin].
@@ -34,14 +38,3 @@ base class CrossFileDarwin extends CrossFileIO {
     return DarwinScopedStorageXDirectory(params);
   }
 }
-
-// final ffi.DynamicLibrary _dylib = () {
-//   return ffi.DynamicLibrary.open(
-//     '/System/Library/Frameworks/Foundation.framework/Foundation',
-//   );
-// }();
-
-// /// The bindings to the native functions in [_dylib].
-// final FoundationFFI _lib = () {
-//   return FoundationFFI(_dylib);
-// }();
