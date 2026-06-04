4cbcef1f3b38a3aebc66a7b5567ea3cd934d8545
Stuart Morgan <stuartmorgan@google.com>
2026-06-03 21:31:27 -0400

    Tool analyzer fix

--- script/tool/test/license_check_command_test.dart
+++ script/tool/test/license_check_command_test.dart
@@ -424,12 +424,13 @@ void main() {
         comment: '',
         copyright: 'Copyright (c) 2018 Dan Field\n',
         license: <String>[
-          'Permission is hereby granted, free of charge, to any person obtaining a copy\n'
-              'of this software and associated documentation files (the "Software"), to deal\n'
-              'in the Software without restriction, including without limitation the rights\n'
-              'to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n'
-              'copies of the Software, and to permit persons to whom the Software is\n'
-              'furnished to do so, subject to the following conditions:',
+          '''
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:'''
         ],
       );
       // Create an empty code file; this package should redirect to the LICENSE
