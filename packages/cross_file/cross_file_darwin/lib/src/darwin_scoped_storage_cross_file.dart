83ae9342234aff46e4142edd3ad0a17d85b11d8a
Maurice Parrish <10687576+bparrishMines@users.noreply.github.com>
2026-06-04 14:35:43 -0400

    use nsnumber

--- packages/cross_file/cross_file_darwin/lib/src/darwin_scoped_storage_cross_file.dart
+++ packages/cross_file/cross_file_darwin/lib/src/darwin_scoped_storage_cross_file.dart
@@ -192,17 +192,10 @@ base class PhotoKitDarwinScopedStorageXFile extends DarwinScopedStorageXFile
       if (firstObject != null) {
         final resource = PHAssetResource.as(firstObject);
         final ObjCObject? fileSize = resource.valueForKey('fileSize'.toNSString());
-        print('here');
+
         if (fileSize != null) {
-          final b = NSNumber.as(fileSize);
-          return b.intValue;
-          // print('inside');
-          // print(fileSize.ref.pointer.address);
-          // final pointer = Pointer<Long>.fromAddress(fileSize.ref.pointer.address);
-          // print('pointer made');
-          // return pointer.value;
+          return NSNumber.as(fileSize).intValue;
         }
-        //return figleSize?.ref.pointer.cast<UnsignedLong>().value;
       }
     }
 
