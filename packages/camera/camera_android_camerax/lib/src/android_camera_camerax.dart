a487e2209c8d6fd5c32d904dda834aac81d672aa
Camille Simon <camillesimon@google.com>
2026-06-18 12:31:29 -0700

    fake commit

--- packages/camera/camera_android_camerax/lib/src/android_camera_camerax.dart
+++ packages/camera/camera_android_camerax/lib/src/android_camera_camerax.dart
@@ -76,7 +76,7 @@ class AndroidCameraCameraX extends CameraPlatform {
     },
   );
 
-  /// Handles retrieving media orientation for a device.f
+  /// Handles retrieving media orientation for a device.
   late final DeviceOrientationManager deviceOrientationManager = DeviceOrientationManager(
     onDeviceOrientationChanged: (_, String orientation) {
       final DeviceOrientation deviceOrientation = _deserializeDeviceOrientation(orientation);
