8a7f3a22793c8b2e115838fd8551272a4798f22f
Mairramer <mairramer.dasilva28@hotmail.com>
2026-06-23 10:57:17 -0300

    Fix SliverGeometry maxPaintExtent assertion in CarouselView.weighted

--- packages/material_ui/lib/src/carousel.dart
+++ packages/material_ui/lib/src/carousel.dart
@@ -1438,6 +1438,7 @@ class _RenderSliverWeightedCarousel extends RenderSliverFixedExtentBoxAdaptor {
         _buildItemExtent(lastIndex, layoutDimensions),
       );
       trailingScrollOffset += extraLayoutOffset;
+      estimatedMaxScrollOffset = trailingScrollOffset;
     } else {
       trailingScrollOffset = indexToLayoutOffset(deprecatedExtraItemExtent, lastIndex + 1);
     }
