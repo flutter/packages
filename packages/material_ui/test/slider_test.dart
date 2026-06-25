a76907c8c39a7dd3bbb6d6bb8a9fef849453595a
Qun Cheng <36861262+QuncCccccc@users.noreply.github.com>
2026-06-25 08:57:09 -0700

    [material_ui] Remove widgets import in `slider_test.dart` (#11977)
    Part of https://github.com/flutter/flutter/issues/182636 and https://github.com/flutter/flutter/issues/188395
    
    This PR:
    * Removed the cross-import of `widgets/semantics_tester.dart`.
    * Removed @Skip tag, all tests in this file has passed. `semantics_tester.dart` has existed in material_ui, so we can directly import `semantics_tester.dart`;
    * Moved the file to `test/` folder.
    
    ## Pre-Review Checklist

--- packages/material_ui/test/slider_test.dart
+++ packages/material_ui/test/slider_test.dart
@@ -2,22 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-@Skip(
-  'This file is skipped due to a cross-import that needs to be fixed. Tracked in https://github.com/flutter/flutter/issues/177028.',
-)
 import 'dart:ui';
 
 import 'package:cupertino_ui/cupertino_ui.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
-import 'package:material_ui/material_ui.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/src/physics/utils.dart' show nearEqual;
 import 'package:flutter_test/flutter_test.dart';
+import 'package:material_ui/material_ui.dart';
 
-import '../widgets/semantics_tester.dart';
+import 'semantics_tester.dart';
 
 /// A [RoundedRectSliderTrackShape] that logs its paint.
 class LoggingRoundedRectSliderTrackShape extends RoundedRectSliderTrackShape {
