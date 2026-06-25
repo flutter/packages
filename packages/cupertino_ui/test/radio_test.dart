ccdef4990a671332a2a283c0e3362b7e19eeee40
Renzo Olivares <roliv@google.com>
2026-06-25 01:35:36 +0000

    updates remove unnecessary SemanticsHandle

--- packages/cupertino_ui/test/radio_test.dart
+++ packages/cupertino_ui/test/radio_test.dart
@@ -172,8 +172,6 @@ void main() {
   }, variant: TargetPlatformVariant.all());
 
   testWidgets('Radio semantics', (WidgetTester tester) async {
-    final SemanticsHandle handle = tester.ensureSemantics();
-
     await tester.pumpWidget(
       CupertinoApp(
         home: Center(child: CupertinoRadio<int>(value: 1, groupValue: 2, onChanged: (int? i) {})),
@@ -253,13 +251,9 @@ void main() {
         isInMutuallyExclusiveGroup: true,
       ),
     );
-
-    handle.dispose();
   });
 
   testWidgets('has semantic events', (WidgetTester tester) async {
-    final SemanticsHandle handle = tester.ensureSemantics();
-
     final Key key = UniqueKey();
     dynamic semanticEvent;
     int? radioValue = 2;
@@ -296,7 +290,6 @@ void main() {
     });
     expect(object.debugSemantics!.getSemanticsData().hasAction(SemanticsAction.tap), true);
 
-    handle.dispose();
     tester.binding.defaultBinaryMessenger.setMockDecodedMessageHandler<dynamic>(
       SystemChannels.accessibility,
       null,
