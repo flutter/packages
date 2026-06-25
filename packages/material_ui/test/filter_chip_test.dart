a6f0e90167cd61b03f87494e6425b56afa830cc8
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2026-06-25 14:48:19 -0700

    Enable filter_chip_test

--- packages/material_ui/test/filter_chip_test.dart
+++ packages/material_ui/test/filter_chip_test.dart
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-@Skip(
-  'This file is skipped due to a cross-import that needs to be fixed. Tracked in https://github.com/flutter/flutter/issues/177028.',
-)
 // This file is run as part of a reduced test set in CI on Mac and Windows
 // machines.
 @Tags(<String>['reduced-test-set'])
@@ -12,12 +9,12 @@ library;
 
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/gestures.dart';
-import 'package:material_ui/material_ui.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:material_ui/material_ui.dart';
 
-import '../widgets/feedback_tester.dart';
+import 'feedback_tester.dart';
 
 /// Adds the basic requirements for a Chip.
 Widget wrapForChip({
