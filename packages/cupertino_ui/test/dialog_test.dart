ab8a7ceba58b5c771f0c852de0e221338f31ae81
Justin McCandless <jmccandless@google.com>
2026-06-29 15:57:56 -0700

    SemanticsHandle

--- packages/cupertino_ui/test/dialog_test.dart
+++ packages/cupertino_ui/test/dialog_test.dart
@@ -683,6 +683,9 @@ void main() {
   });
 
   testWidgets('Has semantic annotations', (WidgetTester tester) async {
+    final SemanticsHandle semanticsHandle = tester.ensureSemantics();
+    addTearDown(semanticsHandle.dispose);
+
     await tester.pumpWidget(
       const CupertinoApp(
         home: CupertinoAlertDialog(
@@ -1661,6 +1664,9 @@ void main() {
   });
 
   testWidgets('showCupertinoDialog - custom barrierLabel', (WidgetTester tester) async {
+    final SemanticsHandle semanticsHandle = tester.ensureSemantics();
+    addTearDown(semanticsHandle.dispose);
+
     await tester.pumpWidget(
       CupertinoApp(
         home: Builder(
