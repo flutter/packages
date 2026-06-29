f17020066919681223726a7cd13e5cecef7de0da
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2026-06-29 08:15:22 -0700

    [material_ui] Enable `switch_list_tile_test` (#12020)
    Port over `switch_list_tile_test` from https://github.com/flutter/flutter/pull/184279
    
    Work towards https://github.com/flutter/flutter/issues/182636 and https://github.com/flutter/flutter/issues/188395
    
    ## Pre-Review Checklist
    
    **Note**: The Flutter team is currently trialing the use of [Gemini Code Assist for GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code). Comments from the `gemini-code-assist` bot should not be taken as authoritative feedback from the Flutter team. If you find its comments useful you can update your code accordingly, but if you are unsure or disagree with the feedback, please feel free to wait for a Flutter team member's review for guidance on which automated comments should be addressed.
    
    [^1]: Regular contributors who have demonstrated familiarity with the repository guidelines only need to comment if the PR is not auto-exempted by repo tooling.

--- packages/material_ui/test/switch_list_tile_test.dart
+++ packages/material_ui/test/switch_list_tile_test.dart
@@ -2,17 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-@Skip(
-  'This file is skipped due to a cross-import that needs to be fixed. Tracked in https://github.com/flutter/flutter/issues/177028.',
-)
 import 'package:cupertino_ui/cupertino_ui.dart';
 import 'package:flutter/gestures.dart';
-import 'package:material_ui/material_ui.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:material_ui/material_ui.dart';
 
-import '../widgets/feedback_tester.dart';
-import '../widgets/semantics_tester.dart';
+import 'feedback_tester.dart';
+import 'semantics_tester.dart';
 
 Widget wrap({required Widget child}) {
   return MediaQuery(
