c24c3d54d3a1da0b715cd262d0351648400ab358
Rohit Sangwan <rohitsangwan647@gmail.com>
2026-07-01 10:18:31 +0530

    Split core_tests into parts

--- packages/pigeon/pigeons/core_tests.dart
+++ packages/pigeon/pigeons/core_tests.dart
@@ -4,6 +4,8 @@
 
 import 'package:pigeon/pigeon.dart';
 
+part 'core_tests_apis.dart';
+
 enum AnEnum { one, two, three, fortyTwo, fourHundredTwentyTwo }
 
 // Enums require special logic, having multiple ensures that the logic can be
@@ -1475,56 +1477,4 @@ abstract class FlutterIntegrationCoreApi {
   @ObjCSelector('echoAsyncString:')
   @SwiftFunction('echoAsync(_:)')
   String echoAsyncString(String aString);
-}
-
-/// An API that can be implemented for minimal, compile-only tests.
-//
-// This is also here to test that multiple host APIs can be generated
-// successfully in all languages (e.g., in Java where it requires having a
-// wrapper class).
-@HostApi()
-abstract class HostTrivialApi {
-  void noop();
-}
-
-/// A simple API implemented in some unit tests.
-//
-// This is separate from HostIntegrationCoreApi to avoid having to update a
-// lot of unit tests every time we add something to the integration test API.
-// TODO(stuartmorgan): Restructure the unit tests to reduce the number of
-// different APIs we define.
-@HostApi()
-abstract class HostSmallApi {
-  @async
-  @ObjCSelector('echoString:')
-  String echo(String aString);
-
-  @async
-  void voidVoid();
-}
-
-/// A simple API called in some unit tests.
-//
-// This is separate from FlutterIntegrationCoreApi to allow for incrementally
-// moving from the previous fragmented unit test structure to something more
-// unified.
-// TODO(stuartmorgan): Restructure the unit tests to reduce the number of
-// different APIs we define.
-@FlutterApi()
-abstract class FlutterSmallApi {
-  @ObjCSelector('echoWrappedList:')
-  @SwiftFunction('echo(_:)')
-  TestMessage echoWrappedList(TestMessage msg);
-
-  @ObjCSelector('echoString:')
-  @SwiftFunction('echo(string:)')
-  String echoString(String aString);
-}
-
-/// A data class containing a List, used in unit tests.
-// TODO(stuartmorgan): Evaluate whether these unit tests are still useful; see
-// TODOs above about restructuring.
-class TestMessage {
-  // ignore: always_specify_types, strict_raw_type
-  List? testList;
 }
