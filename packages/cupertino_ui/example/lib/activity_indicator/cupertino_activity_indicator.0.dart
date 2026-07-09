da87c0e941f22f2fd3a5eb6ca74dd35ca4034d55
Tong Mu <dkwingsmt@gmail.com>
2026-07-09 16:26:03 -0700

    Add starting region

--- packages/cupertino_ui/example/lib/activity_indicator/cupertino_activity_indicator.0.dart
+++ packages/cupertino_ui/example/lib/activity_indicator/cupertino_activity_indicator.0.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// #region body
 import 'package:cupertino_ui/cupertino_ui.dart';
 
 /// Flutter code sample for [CupertinoActivityIndicator].
