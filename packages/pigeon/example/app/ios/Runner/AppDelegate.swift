c2330a8df2f2f264fe06420c0db69ba076739dd6
Zubii <ionutzubascu@yahoo.com>
2026-07-11 13:44:29 +0300

    feat(pigeon): migrate example apps to UIScene

--- packages/pigeon/example/app/ios/Runner/AppDelegate.swift
+++ packages/pigeon/example/app/ios/Runner/AppDelegate.swift
@@ -94,15 +94,19 @@ func sendEvents(_ eventListener: EventListener) {
 }
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
 }
+
 
 // TODO(stuartmorgan): Once 3.33+ reaches stable, remove this subclass and move the setup to
 // AppDelegate.register(...). This approach is only used because this example needs to support
