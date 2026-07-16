99cd38553c84dae91480d97a4fdcff9f3771f1bc
Tong Mu <dkwingsmt@users.noreply.github.com>
2026-07-16 12:51:49 -0700

    [material_ui, cupertino_ui] Rename l10n macros (#12211)
    This PR renames macros used by l10n.
    
    For why the rename is needed, see https://github.com/flutter/packages/pull/12198.
    
    ## Pre-Review Checklist
    
    **Note**: The Flutter team is currently trialing the use of [Gemini Code Assist for GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code). Comments from the `gemini-code-assist` bot should not be taken as authoritative feedback from the Flutter team. If you find its comments useful you can update your code accordingly, but if you are unsure or disagree with the feedback, please feel free to wait for a Flutter team member's review for guidance on which automated comments should be addressed.
    
    [^1]: Regular contributors who have demonstrated familiarity with the repository guidelines only need to comment if the PR is not auto-exempted by repo tooling.

--- script/l10n/lib/gen_material_localizations.dart
+++ script/l10n/lib/gen_material_localizations.dart
@@ -77,4 +77,4 @@ const String materialFactoryArguments =
 
 const String materialSupportedLanguagesConstant = 'kMaterialSupportedLanguages';
 
-const String materialSupportedLanguagesDocMacro = 'flutter.localizations.material.languages';
+const String materialSupportedLanguagesDocMacro = 'material_ui.localizations.languages';
