3e4077bcf7bf87484c54a4352c02e05f7ff18ddc
Navaron Bracke <brackenavaron@gmail.com>
2026-07-17 09:54:24 +0200

    fix material_ui cross imports in cupertino_ui samples

--- packages/cupertino_ui/example/test/list_tile/cupertino_list_tile.0_test.dart
+++ packages/cupertino_ui/example/test/list_tile/cupertino_list_tile.0_test.dart
@@ -35,7 +35,7 @@ void main() {
       tester
           .firstWidget<CupertinoListTile>(tileWithBackgroundFinder)
           .backgroundColor,
-      CupertinoColors.activeBlue,
+      const Color(0xFF0000FF),
     );
   });
 }
