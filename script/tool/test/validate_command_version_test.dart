825235b416ae368e8fa060f60bce245c60323778
stuartmorgan-g <stuartmorgan@google.com>
2026-07-21 15:46:07 -0400

    [tool] Rename batch change override label (#12263)
    (again)
    
    This was given a more descriptive lbale in
    https://github.com/flutter/packages/pull/11835, but it appears based on failures when attempting to generate the PR that there is an undocumented length limit of 50 characters for label names, and `override: skip-batch-release-repo-check-` uses 40 of them, which causes problems when adding the package name.
    
    This renames to `override: batch-`, which is less descriptive, but should work around the length limit.
    
    Longer term, we may need to revisit having the package name in the label to avoid edge case issues with long package names.

--- script/tool/test/validate_command_version_test.dart
+++ script/tool/test/validate_command_version_test.dart
@@ -1584,7 +1584,7 @@ release:
         );
       });
       test(
-        'ignores changelog and pubspec yaml version modifications check with override: skip-batch-release-repo-check label',
+        'ignores changelog and pubspec yaml version modifications check with override: batch-<package> label',
         () async {
           final RepositoryPackage package = createFakePackage(
             'package',
@@ -1615,7 +1615,7 @@ packages/package/pubspec.yaml
           final List<String> output = await runCapturingPrint(runner, <String>[
             'validate',
             '--base-sha=main',
-            '--pr-labels=override: skip-batch-release-repo-check-package',
+            '--pr-labels=override: batch-package',
           ]);
 
           expect(
