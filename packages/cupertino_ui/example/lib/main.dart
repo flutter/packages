5263ac3ec51a858fc419d00be8a45bf23ddc83bd
Justin McCandless <jmccandless@google.com>
2026-08-05 15:20:08 -0700

    All examples

--- packages/cupertino_ui/example/lib/main.dart
+++ packages/cupertino_ui/example/lib/main.dart
@@ -5,7 +5,68 @@
 import 'package:cupertino_ui/cupertino_ui.dart';
 
 import 'activity_indicator/cupertino_activity_indicator.0.dart'
-    as activity_indicator;
+    as cupertino_activity_indicator_0;
+import 'activity_indicator/cupertino_linear_activity_indicator.0.dart'
+    as cupertino_linear_activity_indicator_0;
+import 'bottom_tab_bar/cupertino_tab_bar.0.dart' as cupertino_tab_bar_0;
+import 'button/cupertino_button.0.dart' as cupertino_button_0;
+import 'checkbox/cupertino_checkbox.0.dart' as cupertino_checkbox_0;
+import 'context_menu/cupertino_context_menu.0.dart' as cupertino_context_menu_0;
+import 'context_menu/cupertino_context_menu.1.dart' as cupertino_context_menu_1;
+import 'date_picker/cupertino_date_picker.0.dart' as cupertino_date_picker_0;
+import 'date_picker/cupertino_timer_picker.0.dart' as cupertino_timer_picker_0;
+import 'dialog/cupertino_action_sheet.0.dart' as cupertino_action_sheet_0;
+import 'dialog/cupertino_alert_dialog.0.dart' as cupertino_alert_dialog_0;
+import 'dialog/cupertino_popup_surface.0.dart' as cupertino_popup_surface_0;
+import 'expansion_tile/cupertino_expansion_tile.0.dart'
+    as cupertino_expansion_tile_0;
+import 'form_row/cupertino_form_row.0.dart' as cupertino_form_row_0;
+import 'list_section/list_section_base.0.dart' as list_section_base_0;
+import 'list_section/list_section_inset.0.dart' as list_section_inset_0;
+import 'list_tile/cupertino_list_tile.0.dart' as cupertino_list_tile_0;
+import 'magnifier/cupertino_magnifier.0.dart' as cupertino_magnifier_0;
+import 'magnifier/cupertino_text_magnifier.0.dart'
+    as cupertino_text_magnifier_0;
+import 'magnifier/text_magnifier.0.dart' as text_magnifier_0;
+import 'menu_anchor/menu_anchor.0.dart' as menu_anchor_0;
+import 'menu_anchor/menu_anchor.1.dart' as menu_anchor_1;
+import 'nav_bar/cupertino_navigation_bar.0.dart' as cupertino_navigation_bar_0;
+import 'nav_bar/cupertino_navigation_bar.1.dart' as cupertino_navigation_bar_1;
+import 'nav_bar/cupertino_navigation_bar.2.dart' as cupertino_navigation_bar_2;
+import 'nav_bar/cupertino_sliver_nav_bar.0.dart' as cupertino_sliver_nav_bar_0;
+import 'nav_bar/cupertino_sliver_nav_bar.1.dart' as cupertino_sliver_nav_bar_1;
+import 'nav_bar/cupertino_sliver_nav_bar.2.dart' as cupertino_sliver_nav_bar_2;
+import 'page_scaffold/cupertino_page_scaffold.0.dart'
+    as cupertino_page_scaffold_0;
+import 'picker/cupertino_picker.0.dart' as cupertino_picker_0;
+import 'radio/cupertino_radio.0.dart' as cupertino_radio_0;
+import 'radio/cupertino_radio.toggleable.0.dart'
+    as cupertino_radio_toggleable_0;
+import 'refresh/cupertino_sliver_refresh_control.0.dart'
+    as cupertino_sliver_refresh_control_0;
+import 'route/show_cupertino_dialog.0.dart' as show_cupertino_dialog_0;
+import 'route/show_cupertino_modal_popup.0.dart'
+    as show_cupertino_modal_popup_0;
+import 'scrollbar/cupertino_scrollbar.0.dart' as cupertino_scrollbar_0;
+import 'scrollbar/cupertino_scrollbar.1.dart' as cupertino_scrollbar_1;
+import 'search_field/cupertino_search_field.0.dart' as cupertino_search_field_0;
+import 'search_field/cupertino_search_field.1.dart' as cupertino_search_field_1;
+import 'segmented_control/cupertino_segmented_control.0.dart'
+    as cupertino_segmented_control_0;
+import 'segmented_control/cupertino_sliding_segmented_control.0.dart'
+    as cupertino_sliding_segmented_control_0;
+import 'sheet/cupertino_sheet.0.dart' as cupertino_sheet_0;
+import 'sheet/cupertino_sheet.1.dart' as cupertino_sheet_1;
+import 'sheet/cupertino_sheet.2.dart' as cupertino_sheet_2;
+import 'sheet/cupertino_sheet.3.dart' as cupertino_sheet_3;
+import 'slider/cupertino_slider.0.dart' as cupertino_slider_0;
+import 'switch/cupertino_switch.0.dart' as cupertino_switch_0;
+import 'tab_scaffold/cupertino_tab_controller.0.dart'
+    as cupertino_tab_controller_0;
+import 'tab_scaffold/cupertino_tab_scaffold.0.dart' as cupertino_tab_scaffold_0;
+import 'text_field/cupertino_text_field.0.dart' as cupertino_text_field_0;
+import 'text_form_field_row/cupertino_text_form_field_row.1.dart'
+    as cupertino_text_form_field_row_1;
 
 void main() {
   runApp(ExampleApp());
@@ -20,7 +81,234 @@ class ExampleApp extends StatelessWidget {
     _Example(
       'activity_indicator/cupertino_activity_indicator.0.dart',
       (BuildContext context) =>
-          const activity_indicator.CupertinoIndicatorApp(),
+          const cupertino_activity_indicator_0.CupertinoIndicatorApp(),
+    ),
+    _Example(
+      'activity_indicator/cupertino_linear_activity_indicator.0.dart',
+      (BuildContext context) =>
+          const cupertino_linear_activity_indicator_0.CupertinoLinearActivityIndicatorApp(),
+    ),
+    _Example(
+      'bottom_tab_bar/cupertino_tab_bar.0.dart',
+      (BuildContext context) => const cupertino_tab_bar_0.CupertinoTabBarApp(),
+    ),
+    _Example(
+      'button/cupertino_button.0.dart',
+      (BuildContext context) => const cupertino_button_0.CupertinoButtonApp(),
+    ),
+    _Example(
+      'checkbox/cupertino_checkbox.0.dart',
+      (BuildContext context) =>
+          const cupertino_checkbox_0.CupertinoCheckboxApp(),
+    ),
+    _Example(
+      'context_menu/cupertino_context_menu.0.dart',
+      (BuildContext context) => const cupertino_context_menu_0.ContextMenuApp(),
+    ),
+    _Example(
+      'context_menu/cupertino_context_menu.1.dart',
+      (BuildContext context) => const cupertino_context_menu_1.ContextMenuApp(),
+    ),
+    _Example(
+      'date_picker/cupertino_date_picker.0.dart',
+      (BuildContext context) => const cupertino_date_picker_0.DatePickerApp(),
+    ),
+    _Example(
+      'date_picker/cupertino_timer_picker.0.dart',
+      (BuildContext context) => const cupertino_timer_picker_0.TimerPickerApp(),
+    ),
+    _Example(
+      'dialog/cupertino_action_sheet.0.dart',
+      (BuildContext context) => const cupertino_action_sheet_0.ActionSheetApp(),
+    ),
+    _Example(
+      'dialog/cupertino_alert_dialog.0.dart',
+      (BuildContext context) => const cupertino_alert_dialog_0.AlertDialogApp(),
+    ),
+    _Example(
+      'dialog/cupertino_popup_surface.0.dart',
+      (BuildContext context) =>
+          const cupertino_popup_surface_0.PopupSurfaceApp(),
+    ),
+    _Example(
+      'expansion_tile/cupertino_expansion_tile.0.dart',
+      (BuildContext context) =>
+          const cupertino_expansion_tile_0.CupertinoExpansionTileApp(),
+    ),
+    _Example(
+      'form_row/cupertino_form_row.0.dart',
+      (BuildContext context) =>
+          const cupertino_form_row_0.CupertinoFormRowApp(),
+    ),
+    _Example(
+      'list_section/list_section_base.0.dart',
+      (BuildContext context) =>
+          const list_section_base_0.CupertinoListSectionBaseApp(),
+    ),
+    _Example(
+      'list_section/list_section_inset.0.dart',
+      (BuildContext context) =>
+          const list_section_inset_0.CupertinoListSectionInsetApp(),
+    ),
+    _Example(
+      'list_tile/cupertino_list_tile.0.dart',
+      (BuildContext context) =>
+          const cupertino_list_tile_0.CupertinoListTileApp(),
+    ),
+    _Example(
+      'magnifier/cupertino_magnifier.0.dart',
+      (BuildContext context) =>
+          const cupertino_magnifier_0.CupertinoMagnifierApp(),
+    ),
+    _Example(
+      'magnifier/cupertino_text_magnifier.0.dart',
+      (BuildContext context) =>
+          const cupertino_text_magnifier_0.CupertinoTextMagnifierApp(),
+    ),
+    _Example(
+      'magnifier/text_magnifier.0.dart',
+      (BuildContext context) =>
+          const text_magnifier_0.TextMagnifierExampleApp(text: 'Hello world!'),
+    ),
+    _Example(
+      'menu_anchor/menu_anchor.0.dart',
+      (BuildContext context) => const menu_anchor_0.CupertinoMenuAnchorApp(),
+    ),
+    _Example(
+      'menu_anchor/menu_anchor.1.dart',
+      (BuildContext context) => const menu_anchor_1.CupertinoMenuAnchorApp(),
+    ),
+    _Example(
+      'nav_bar/cupertino_navigation_bar.0.dart',
+      (BuildContext context) => const cupertino_navigation_bar_0.NavBarApp(),
+    ),
+    _Example(
+      'nav_bar/cupertino_navigation_bar.1.dart',
+      (BuildContext context) => const cupertino_navigation_bar_1.NavBarApp(),
+    ),
+    _Example(
+      'nav_bar/cupertino_navigation_bar.2.dart',
+      (BuildContext context) => const cupertino_navigation_bar_2.NavBarApp(),
+    ),
+    _Example(
+      'nav_bar/cupertino_sliver_nav_bar.0.dart',
+      (BuildContext context) =>
+          const cupertino_sliver_nav_bar_0.SliverNavBarApp(),
+    ),
+    _Example(
+      'nav_bar/cupertino_sliver_nav_bar.1.dart',
+      (BuildContext context) =>
+          const cupertino_sliver_nav_bar_1.SliverNavBarApp(),
+    ),
+    _Example(
+      'nav_bar/cupertino_sliver_nav_bar.2.dart',
+      (BuildContext context) =>
+          const cupertino_sliver_nav_bar_2.SliverNavBarApp(),
+    ),
+    _Example(
+      'page_scaffold/cupertino_page_scaffold.0.dart',
+      (BuildContext context) =>
+          const cupertino_page_scaffold_0.PageScaffoldApp(),
+    ),
+    _Example(
+      'picker/cupertino_picker.0.dart',
+      (BuildContext context) => const cupertino_picker_0.CupertinoPickerApp(),
+    ),
+    _Example(
+      'radio/cupertino_radio.0.dart',
+      (BuildContext context) => const cupertino_radio_0.CupertinoRadioApp(),
+    ),
+    _Example(
+      'radio/cupertino_radio.toggleable.0.dart',
+      (BuildContext context) =>
+          const cupertino_radio_toggleable_0.CupertinoRadioApp(),
+    ),
+    _Example(
+      'refresh/cupertino_sliver_refresh_control.0.dart',
+      (BuildContext context) =>
+          const cupertino_sliver_refresh_control_0.RefreshControlApp(),
+    ),
+    _Example(
+      'route/show_cupertino_dialog.0.dart',
+      (BuildContext context) =>
+          const show_cupertino_dialog_0.CupertinoDialogApp(),
+    ),
+    _Example(
+      'route/show_cupertino_modal_popup.0.dart',
+      (BuildContext context) =>
+          const show_cupertino_modal_popup_0.ModalPopupApp(),
+    ),
+    _Example(
+      'scrollbar/cupertino_scrollbar.0.dart',
+      (BuildContext context) => const cupertino_scrollbar_0.ScrollbarApp(),
+    ),
+    _Example(
+      'scrollbar/cupertino_scrollbar.1.dart',
+      (BuildContext context) => const cupertino_scrollbar_1.ScrollbarApp(),
+    ),
+    _Example(
+      'search_field/cupertino_search_field.0.dart',
+      (BuildContext context) =>
+          const cupertino_search_field_0.SearchTextFieldApp(),
+    ),
+    _Example(
+      'search_field/cupertino_search_field.1.dart',
+      (BuildContext context) =>
+          const cupertino_search_field_1.SearchTextFieldApp(),
+    ),
+    _Example(
+      'segmented_control/cupertino_segmented_control.0.dart',
+      (BuildContext context) =>
+          const cupertino_segmented_control_0.SegmentedControlApp(),
+    ),
+    _Example(
+      'segmented_control/cupertino_sliding_segmented_control.0.dart',
+      (BuildContext context) =>
+          const cupertino_sliding_segmented_control_0.SegmentedControlApp(),
+    ),
+    _Example(
+      'sheet/cupertino_sheet.0.dart',
+      (BuildContext context) => const cupertino_sheet_0.CupertinoSheetApp(),
+    ),
+    _Example(
+      'sheet/cupertino_sheet.1.dart',
+      (BuildContext context) => const cupertino_sheet_1.CupertinoSheetApp(),
+    ),
+    _Example(
+      'sheet/cupertino_sheet.2.dart',
+      (BuildContext context) =>
+          const cupertino_sheet_2.RestorableSheetExampleApp(),
+    ),
+    _Example(
+      'sheet/cupertino_sheet.3.dart',
+      (BuildContext context) => const cupertino_sheet_3.CupertinoSheetApp(),
+    ),
+    _Example(
+      'slider/cupertino_slider.0.dart',
+      (BuildContext context) => const cupertino_slider_0.CupertinoSliderApp(),
+    ),
+    _Example(
+      'switch/cupertino_switch.0.dart',
+      (BuildContext context) => const cupertino_switch_0.CupertinoSwitchApp(),
+    ),
+    _Example(
+      'tab_scaffold/cupertino_tab_controller.0.dart',
+      (BuildContext context) =>
+          const cupertino_tab_controller_0.TabControllerApp(),
+    ),
+    _Example(
+      'tab_scaffold/cupertino_tab_scaffold.0.dart',
+      (BuildContext context) => const cupertino_tab_scaffold_0.TabScaffoldApp(),
+    ),
+    _Example(
+      'text_field/cupertino_text_field.0.dart',
+      (BuildContext context) =>
+          const cupertino_text_field_0.CupertinoTextFieldApp(),
+    ),
+    _Example(
+      'text_form_field_row/cupertino_text_form_field_row.1.dart',
+      (BuildContext context) =>
+          const cupertino_text_form_field_row_1.FormSectionApp(),
     ),
   ];
 
@@ -37,9 +325,9 @@ class ExampleApp extends StatelessWidget {
 }
 
 class _ExampleHome extends StatelessWidget {
-  const _ExampleHome({required this._examples});
+  const _ExampleHome({required this.examples});
 
-  final List<_Example> _examples;
+  final List<_Example> examples;
 
   @override
   Widget build(BuildContext context) {
@@ -47,9 +335,9 @@ class _ExampleHome extends StatelessWidget {
       navigationBar: CupertinoNavigationBar(
         middle: const Text(ExampleApp.title),
       ),
-      child: Center(
+      child: SafeArea(
         child: ListView(
-          children: _examples
+          children: examples
               .map((_Example example) => _ExampleListItem(example: example))
               .toList(),
         ),
@@ -60,17 +348,17 @@ class _ExampleHome extends StatelessWidget {
 
 /// One item that opens its example when tapped.
 class _ExampleListItem extends StatelessWidget {
-  const _ExampleListItem({required this._example});
+  const _ExampleListItem({required this.example});
 
-  final _Example _example;
+  final _Example example;
 
   @override
   Widget build(BuildContext context) {
     return CupertinoListTile(
       onTap: () {
-        Navigator.of(context).pushNamed(_example.url);
+        Navigator.of(context).pushNamed(example.url);
       },
-      title: Text(_example.filepath),
+      title: Text(example.filepath),
     );
   }
 }
@@ -89,8 +377,8 @@ class _Example {
     final filename = segments.last;
 
     final filenameSegments = filename.split('.');
-    assert(filenameSegments.length == 3);
-    final number = filenameSegments[1];
+    assert(filenameSegments.length >= 3);
+    final number = filenameSegments[filenameSegments.length - 2];
 
     return '/$directory/$filename/$number';
   }
--- packages/cupertino_ui/example/test/main_test.dart
+++ packages/cupertino_ui/example/test/main_test.dart
@@ -1,27 +1,217 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:cupertino_ui_examples/main.dart';
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:cupertino_ui_examples/activity_indicator/cupertino_activity_indicator.0.dart'
-    as activity_indicator;
+    as cupertino_activity_indicator_0;
+import 'package:cupertino_ui_examples/activity_indicator/cupertino_linear_activity_indicator.0.dart'
+    as cupertino_linear_activity_indicator_0;
+import 'package:cupertino_ui_examples/bottom_tab_bar/cupertino_tab_bar.0.dart'
+    as cupertino_tab_bar_0;
+import 'package:cupertino_ui_examples/button/cupertino_button.0.dart'
+    as cupertino_button_0;
+import 'package:cupertino_ui_examples/checkbox/cupertino_checkbox.0.dart'
+    as cupertino_checkbox_0;
+import 'package:cupertino_ui_examples/context_menu/cupertino_context_menu.0.dart'
+    as cupertino_context_menu_0;
+import 'package:cupertino_ui_examples/context_menu/cupertino_context_menu.1.dart'
+    as cupertino_context_menu_1;
+import 'package:cupertino_ui_examples/date_picker/cupertino_date_picker.0.dart'
+    as cupertino_date_picker_0;
+import 'package:cupertino_ui_examples/date_picker/cupertino_timer_picker.0.dart'
+    as cupertino_timer_picker_0;
+import 'package:cupertino_ui_examples/dialog/cupertino_action_sheet.0.dart'
+    as cupertino_action_sheet_0;
+import 'package:cupertino_ui_examples/dialog/cupertino_alert_dialog.0.dart'
+    as cupertino_alert_dialog_0;
+import 'package:cupertino_ui_examples/dialog/cupertino_popup_surface.0.dart'
+    as cupertino_popup_surface_0;
+import 'package:cupertino_ui_examples/expansion_tile/cupertino_expansion_tile.0.dart'
+    as cupertino_expansion_tile_0;
+import 'package:cupertino_ui_examples/form_row/cupertino_form_row.0.dart'
+    as cupertino_form_row_0;
+import 'package:cupertino_ui_examples/list_section/list_section_base.0.dart'
+    as list_section_base_0;
+import 'package:cupertino_ui_examples/list_section/list_section_inset.0.dart'
+    as list_section_inset_0;
+import 'package:cupertino_ui_examples/list_tile/cupertino_list_tile.0.dart'
+    as cupertino_list_tile_0;
+import 'package:cupertino_ui_examples/magnifier/cupertino_magnifier.0.dart'
+    as cupertino_magnifier_0;
+import 'package:cupertino_ui_examples/magnifier/cupertino_text_magnifier.0.dart'
+    as cupertino_text_magnifier_0;
+import 'package:cupertino_ui_examples/magnifier/text_magnifier.0.dart'
+    as text_magnifier_0;
+import 'package:cupertino_ui_examples/main.dart';
+import 'package:cupertino_ui_examples/menu_anchor/menu_anchor.0.dart'
+    as menu_anchor_0;
+import 'package:cupertino_ui_examples/menu_anchor/menu_anchor.1.dart'
+    as menu_anchor_1;
+import 'package:cupertino_ui_examples/nav_bar/cupertino_navigation_bar.0.dart'
+    as cupertino_navigation_bar_0;
+import 'package:cupertino_ui_examples/nav_bar/cupertino_navigation_bar.1.dart'
+    as cupertino_navigation_bar_1;
+import 'package:cupertino_ui_examples/nav_bar/cupertino_navigation_bar.2.dart'
+    as cupertino_navigation_bar_2;
+import 'package:cupertino_ui_examples/nav_bar/cupertino_sliver_nav_bar.0.dart'
+    as cupertino_sliver_nav_bar_0;
+import 'package:cupertino_ui_examples/nav_bar/cupertino_sliver_nav_bar.1.dart'
+    as cupertino_sliver_nav_bar_1;
+import 'package:cupertino_ui_examples/nav_bar/cupertino_sliver_nav_bar.2.dart'
+    as cupertino_sliver_nav_bar_2;
+import 'package:cupertino_ui_examples/page_scaffold/cupertino_page_scaffold.0.dart'
+    as cupertino_page_scaffold_0;
+import 'package:cupertino_ui_examples/picker/cupertino_picker.0.dart'
+    as cupertino_picker_0;
+import 'package:cupertino_ui_examples/radio/cupertino_radio.0.dart'
+    as cupertino_radio_0;
+import 'package:cupertino_ui_examples/radio/cupertino_radio.toggleable.0.dart'
+    as cupertino_radio_toggleable_0;
+import 'package:cupertino_ui_examples/refresh/cupertino_sliver_refresh_control.0.dart'
+    as cupertino_sliver_refresh_control_0;
+import 'package:cupertino_ui_examples/route/show_cupertino_dialog.0.dart'
+    as show_cupertino_dialog_0;
+import 'package:cupertino_ui_examples/route/show_cupertino_modal_popup.0.dart'
+    as show_cupertino_modal_popup_0;
+import 'package:cupertino_ui_examples/scrollbar/cupertino_scrollbar.0.dart'
+    as cupertino_scrollbar_0;
+import 'package:cupertino_ui_examples/scrollbar/cupertino_scrollbar.1.dart'
+    as cupertino_scrollbar_1;
+import 'package:cupertino_ui_examples/search_field/cupertino_search_field.0.dart'
+    as cupertino_search_field_0;
+import 'package:cupertino_ui_examples/search_field/cupertino_search_field.1.dart'
+    as cupertino_search_field_1;
+import 'package:cupertino_ui_examples/segmented_control/cupertino_segmented_control.0.dart'
+    as cupertino_segmented_control_0;
+import 'package:cupertino_ui_examples/segmented_control/cupertino_sliding_segmented_control.0.dart'
+    as cupertino_sliding_segmented_control_0;
+import 'package:cupertino_ui_examples/sheet/cupertino_sheet.0.dart'
+    as cupertino_sheet_0;
+import 'package:cupertino_ui_examples/sheet/cupertino_sheet.1.dart'
+    as cupertino_sheet_1;
+import 'package:cupertino_ui_examples/sheet/cupertino_sheet.2.dart'
+    as cupertino_sheet_2;
+import 'package:cupertino_ui_examples/sheet/cupertino_sheet.3.dart'
+    as cupertino_sheet_3;
+import 'package:cupertino_ui_examples/slider/cupertino_slider.0.dart'
+    as cupertino_slider_0;
+import 'package:cupertino_ui_examples/switch/cupertino_switch.0.dart'
+    as cupertino_switch_0;
+import 'package:cupertino_ui_examples/tab_scaffold/cupertino_tab_controller.0.dart'
+    as cupertino_tab_controller_0;
+import 'package:cupertino_ui_examples/tab_scaffold/cupertino_tab_scaffold.0.dart'
+    as cupertino_tab_scaffold_0;
+import 'package:cupertino_ui_examples/text_field/cupertino_text_field.0.dart'
+    as cupertino_text_field_0;
+import 'package:cupertino_ui_examples/text_form_field_row/cupertino_text_form_field_row.1.dart'
+    as cupertino_text_form_field_row_1;
+import 'package:flutter_test/flutter_test.dart';
+
+const Map<String, Type> examples = <String, Type>{
+  'activity_indicator/cupertino_activity_indicator.0.dart':
+      cupertino_activity_indicator_0.CupertinoIndicatorApp,
+  'activity_indicator/cupertino_linear_activity_indicator.0.dart':
+      cupertino_linear_activity_indicator_0.CupertinoLinearActivityIndicatorApp,
+  'bottom_tab_bar/cupertino_tab_bar.0.dart':
+      cupertino_tab_bar_0.CupertinoTabBarApp,
+  'button/cupertino_button.0.dart': cupertino_button_0.CupertinoButtonApp,
+  'checkbox/cupertino_checkbox.0.dart':
+      cupertino_checkbox_0.CupertinoCheckboxApp,
+  'context_menu/cupertino_context_menu.0.dart':
+      cupertino_context_menu_0.ContextMenuApp,
+  'context_menu/cupertino_context_menu.1.dart':
+      cupertino_context_menu_1.ContextMenuApp,
+  'date_picker/cupertino_date_picker.0.dart':
+      cupertino_date_picker_0.DatePickerApp,
+  'date_picker/cupertino_timer_picker.0.dart':
+      cupertino_timer_picker_0.TimerPickerApp,
+  'dialog/cupertino_action_sheet.0.dart':
+      cupertino_action_sheet_0.ActionSheetApp,
+  'dialog/cupertino_alert_dialog.0.dart':
+      cupertino_alert_dialog_0.AlertDialogApp,
+  'dialog/cupertino_popup_surface.0.dart':
+      cupertino_popup_surface_0.PopupSurfaceApp,
+  'expansion_tile/cupertino_expansion_tile.0.dart':
+      cupertino_expansion_tile_0.CupertinoExpansionTileApp,
+  'form_row/cupertino_form_row.0.dart':
+      cupertino_form_row_0.CupertinoFormRowApp,
+  'list_section/list_section_base.0.dart':
+      list_section_base_0.CupertinoListSectionBaseApp,
+  'list_section/list_section_inset.0.dart':
+      list_section_inset_0.CupertinoListSectionInsetApp,
+  'list_tile/cupertino_list_tile.0.dart':
+      cupertino_list_tile_0.CupertinoListTileApp,
+  'magnifier/cupertino_magnifier.0.dart':
+      cupertino_magnifier_0.CupertinoMagnifierApp,
+  'magnifier/cupertino_text_magnifier.0.dart':
+      cupertino_text_magnifier_0.CupertinoTextMagnifierApp,
+  'magnifier/text_magnifier.0.dart': text_magnifier_0.TextMagnifierExampleApp,
+  'menu_anchor/menu_anchor.0.dart': menu_anchor_0.CupertinoMenuAnchorApp,
+  'menu_anchor/menu_anchor.1.dart': menu_anchor_1.CupertinoMenuAnchorApp,
+  'nav_bar/cupertino_navigation_bar.0.dart':
+      cupertino_navigation_bar_0.NavBarApp,
+  'nav_bar/cupertino_navigation_bar.1.dart':
+      cupertino_navigation_bar_1.NavBarApp,
+  'nav_bar/cupertino_navigation_bar.2.dart':
+      cupertino_navigation_bar_2.NavBarApp,
+  'nav_bar/cupertino_sliver_nav_bar.0.dart':
+      cupertino_sliver_nav_bar_0.SliverNavBarApp,
+  'nav_bar/cupertino_sliver_nav_bar.1.dart':
+      cupertino_sliver_nav_bar_1.SliverNavBarApp,
+  'nav_bar/cupertino_sliver_nav_bar.2.dart':
+      cupertino_sliver_nav_bar_2.SliverNavBarApp,
+  'page_scaffold/cupertino_page_scaffold.0.dart':
+      cupertino_page_scaffold_0.PageScaffoldApp,
+  'picker/cupertino_picker.0.dart': cupertino_picker_0.CupertinoPickerApp,
+  'radio/cupertino_radio.0.dart': cupertino_radio_0.CupertinoRadioApp,
+  'radio/cupertino_radio.toggleable.0.dart':
+      cupertino_radio_toggleable_0.CupertinoRadioApp,
+  'refresh/cupertino_sliver_refresh_control.0.dart':
+      cupertino_sliver_refresh_control_0.RefreshControlApp,
+  'route/show_cupertino_dialog.0.dart':
+      show_cupertino_dialog_0.CupertinoDialogApp,
+  'route/show_cupertino_modal_popup.0.dart':
+      show_cupertino_modal_popup_0.ModalPopupApp,
+  'scrollbar/cupertino_scrollbar.0.dart': cupertino_scrollbar_0.ScrollbarApp,
+  'scrollbar/cupertino_scrollbar.1.dart': cupertino_scrollbar_1.ScrollbarApp,
+  'search_field/cupertino_search_field.0.dart':
+      cupertino_search_field_0.SearchTextFieldApp,
+  'search_field/cupertino_search_field.1.dart':
+      cupertino_search_field_1.SearchTextFieldApp,
+  'segmented_control/cupertino_segmented_control.0.dart':
+      cupertino_segmented_control_0.SegmentedControlApp,
+  'segmented_control/cupertino_sliding_segmented_control.0.dart':
+      cupertino_sliding_segmented_control_0.SegmentedControlApp,
+  'sheet/cupertino_sheet.0.dart': cupertino_sheet_0.CupertinoSheetApp,
+  'sheet/cupertino_sheet.1.dart': cupertino_sheet_1.CupertinoSheetApp,
+  'sheet/cupertino_sheet.2.dart': cupertino_sheet_2.RestorableSheetExampleApp,
+  'sheet/cupertino_sheet.3.dart': cupertino_sheet_3.CupertinoSheetApp,
+  'slider/cupertino_slider.0.dart': cupertino_slider_0.CupertinoSliderApp,
+  'switch/cupertino_switch.0.dart': cupertino_switch_0.CupertinoSwitchApp,
+  'tab_scaffold/cupertino_tab_controller.0.dart':
+      cupertino_tab_controller_0.TabControllerApp,
+  'tab_scaffold/cupertino_tab_scaffold.0.dart':
+      cupertino_tab_scaffold_0.TabScaffoldApp,
+  'text_field/cupertino_text_field.0.dart':
+      cupertino_text_field_0.CupertinoTextFieldApp,
+  'text_form_field_row/cupertino_text_form_field_row.1.dart':
+      cupertino_text_form_field_row_1.FormSectionApp,
+};
 
 void main() {
-  testWidgets('cupertino_activity_indicator.0', (WidgetTester tester) async {
-    await tester.pumpWidget(ExampleApp());
+  for (final MapEntry<String, Type> entry in examples.entries) {
+    testWidgets(entry.key, (WidgetTester tester) async {
+      await tester.pumpWidget(ExampleApp());
 
-    final Finder finder = find.text(
-      'activity_indicator/cupertino_activity_indicator.0.dart',
-    );
-    await tester.scrollUntilVisible(finder, 200.0);
-    await tester.ensureVisible(finder);
-    await tester.pumpAndSettle();
-    await tester.tap(finder);
-    await tester.pump();
-    await tester.pump(const Duration(seconds: 1));
+      final Finder finder = find.text(entry.key);
+      await tester.scrollUntilVisible(finder, 200.0);
+      await tester.ensureVisible(finder);
+      await tester.pumpAndSettle();
+      await tester.tap(finder);
+      await tester.pump();
+      await tester.pump(const Duration(seconds: 1));
 
-    expect(
-      find.byType(activity_indicator.CupertinoIndicatorApp),
-      findsOneWidget,
-    );
-  });
+      expect(find.byType(entry.value), findsOneWidget);
+    });
+  }
 }
