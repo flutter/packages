83f10c1e9aa6c80f936cd206c160a5c2caf51da8
Qun Cheng <quncheng@google.com>
2026-08-07 17:10:45 -0700

    Rename IconButton M3E template

--- packages/material_ui/tool/gen_defaults/bin/gen_defaults.dart
+++ packages/material_ui/tool/gen_defaults/bin/gen_defaults.dart
@@ -82,7 +82,7 @@ Future<void> main(List<String> args) async {
   // const ExpansionTileTemplateM3().generateFile(verbose: verbose);
   // const FabTemplateM3().generateFile(verbose: verbose);
   // const FilterChipTemplateM3().generateFile(verbose: verbose);
-  const IconButtonTemplate().generateFile(verbose: verbose);
+  const IconButtonTemplateM3E().generateFile(verbose: verbose);
   // const InputChipTemplateM3().generateFile(verbose: verbose);
   // const InputDecoratorTemplateM3().generateFile(verbose: verbose);
   // const ListTileTemplateM3().generateFile(verbose: verbose);
