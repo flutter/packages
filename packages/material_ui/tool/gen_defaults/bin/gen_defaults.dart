33832aa71e43fe71eb57853ca384a74145af67cc
Qun Cheng <quncheng@google.com>
2026-08-11 16:14:11 -0700

    Address Banner gen_defaults review comments

--- packages/material_ui/tool/gen_defaults/bin/gen_defaults.dart
+++ packages/material_ui/tool/gen_defaults/bin/gen_defaults.dart
@@ -16,7 +16,6 @@ import '../templates/action_chip_template.dart';
 import '../templates/app_bar_template.dart';
 // import '../templates/badge_template.dart';
 import '../templates/banner_template.dart';
-
 // import '../templates/bottom_app_bar_template.dart';
 import '../templates/bottom_sheet_template.dart';
 
