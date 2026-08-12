0308729f22baa7066bb90a3cbbcf1d1740e4af00
Qun Cheng <36861262+QuncCccccc@users.noreply.github.com>
2026-08-12 12:09:20 -0700

    [material_ui] Migrate M3 `Banner` template to use new `gen_defaults` (#12227)
    Work towards https://github.com/flutter/flutter/issues/187899
    Fixes https://github.com/flutter/flutter/issues/188402
    
    ## Pre-Review Checklist
    
    - [x] I read the [Contributor Guide] and followed the process outlined
    there for submitting PRs.
    - [x] I read the [AI contribution guidelines] and understand my
    responsibilities, or I am not using AI tools.
    - [x] I read the [Tree Hygiene] page, which explains my
    responsibilities.
    - [x] I read and followed the [relevant style guides] and ran [the
    auto-formatter].
    - [x] I signed the [CLA].
    - [x] The title of the PR starts with the name of the package surrounded
    by square brackets, e.g. `[shared_preferences]`
    - [x] I [linked to at least one issue that this PR fixes] in the
    description above.
    - [x] I followed [the version and CHANGELOG instructions], using
    [semantic versioning] and the [repository CHANGELOG style], or I have
    commented below to indicate which documented exception this PR falls
    under[^1].
    - [x] I updated/added any relevant documentation (doc comments with
    `///`).
    - [ ] I added new tests to check the change I am making, or I have
    commented below to indicate which [test exemption] this PR falls
    under[^1].
    - [x] All existing and new tests are passing.

--- packages/material_ui/tool/gen_defaults/test/gen_defaults_test.dart
+++ packages/material_ui/tool/gen_defaults/test/gen_defaults_test.dart
@@ -11,6 +11,7 @@ import '../data/typescale.dart';
 import '../data/typescale_emphasized.dart';
 import '../templates/action_chip_template.dart';
 import '../templates/app_bar_template.dart';
+import '../templates/banner_template.dart';
 import '../templates/badge_template.dart';
 import '../templates/bottom_sheet_template.dart';
 import '../templates/template.dart';
@@ -220,9 +221,14 @@ void main() {
       );
     });
 
-    test('BannerTemplateM3 emits M3 Banner defaults from tokens', () {
-      // Intentionally empty, will be implemented during migration. See:
-      // https://github.com/flutter/flutter/issues/187899
+    test('BannerTemplateM3 emits M3 Banner defaults from banner tokens', () {
+      final String contents = _generateContents(const BannerTemplateM3());
+      expect(contents, contains('class _BannerDefaultsM3 extends MaterialBannerThemeData'));
+      expect(contents, contains('elevation: 1.0'));
+      expect(contents, contains('Color? get backgroundColor => _colors.surfaceContainerLow'));
+      expect(contents, contains('Color? get surfaceTintColor => Colors.transparent'));
+      expect(contents, contains('Color? get dividerColor => _colors.outlineVariant'));
+      expect(contents, contains('TextStyle? get contentTextStyle => _textTheme.bodyMedium'));
     });
 
     test('BottomAppBarTemplateM3 emits M3 BottomAppBar defaults from tokens', () {
