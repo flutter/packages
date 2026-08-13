cf1b157cb107bad31fad5892b615ee0fb8d5ea87
Ryan Holanda <ryanholanda08@gmail.com>
2026-08-13 18:55:08 -0300

    Address background color review feedback

--- packages/google_maps_flutter/google_maps_flutter_android/lib/src/google_maps_flutter_android.dart
+++ packages/google_maps_flutter/google_maps_flutter_android/lib/src/google_maps_flutter_android.dart
@@ -1253,6 +1253,7 @@ PlatformMarkerType _platformMarkerTypeFromMarkerType(MarkerType markerType) {
 }
 
 PlatformMapConfiguration _platformMapConfigurationFromMapConfiguration(MapConfiguration config) {
+  final Color? backgroundColor = config.backgroundColor;
   return PlatformMapConfiguration(
     compassEnabled: config.compassEnabled,
     cameraTargetBounds: _platformCameraTargetBoundsFromCameraTargetBounds(
@@ -1277,9 +1278,9 @@ PlatformMapConfiguration _platformMapConfigurationFromMapConfiguration(MapConfig
     markerType: _platformMarkerTypeFromMarkerType(config.markerType ?? MarkerType.marker),
     mapId: config.mapId,
     style: config.style,
-    backgroundColor: config.backgroundColor == null
+    backgroundColor: backgroundColor == null
         ? null
-        : PlatformColor(argbValue: config.backgroundColor!.toARGB32()),
+        : PlatformColor(argbValue: backgroundColor.toARGB32()),
   );
 }
 
@@ -1290,6 +1291,7 @@ PlatformMapConfiguration _platformMapConfigurationFromOptionsJson(Map<String, Ob
   // to support this legacy API that relied on cross-package magic strings.
   final List<double>? padding = (options['padding'] as List<Object?>?)?.cast<double>();
   final mapType = options['mapType'] as int?;
+  final backgroundColor = options['backgroundColor'] as int?;
   return PlatformMapConfiguration(
     compassEnabled: options['compassEnabled'] as bool?,
     cameraTargetBounds: _platformCameraTargetBoundsFromCameraTargetBoundsJson(
@@ -1323,9 +1325,7 @@ PlatformMapConfiguration _platformMapConfigurationFromOptionsJson(Map<String, Ob
     markerType: PlatformMarkerType.marker,
     mapId: options['mapId'] as String?,
     style: options['style'] as String?,
-    backgroundColor: options['backgroundColor'] == null
-        ? null
-        : PlatformColor(argbValue: options['backgroundColor']! as int),
+    backgroundColor: backgroundColor == null ? null : PlatformColor(argbValue: backgroundColor),
   );
 }
 
