9ca63ca430bc00baa933a77e11fdf4ed398dadc4
Victor Gomez <victor.orozco@cloudsufi.com>
2026-08-14 12:21:09 -0600

    Add conditional import for google_sign_in_ios_objc in Placeholder.swift

--- packages/google_sign_in/google_sign_in_ios/darwin/google_sign_in_ios/Sources/google_sign_in_ios/Placeholder.swift
+++ packages/google_sign_in/google_sign_in_ios/darwin/google_sign_in_ios/Sources/google_sign_in_ios/Placeholder.swift
@@ -1,3 +1,7 @@
 // Copyright 2013 The Flutter Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+#if canImport(google_sign_in_ios_objc)
+  @_exported import google_sign_in_ios_objc
+#endif
