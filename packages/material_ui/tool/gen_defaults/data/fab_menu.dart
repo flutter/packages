37bc5d65fb5cfd7a431c71eae154a85dada45ae0
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2026-08-19 11:31:00 -0700

    [material_ui] Add Material tokens (version 38.2.50) (#12471)
    Export the Material tokens to `material_ui`. This PR simply exports the existing tokens from g3 to Github.
    
    Work towards https://github.com/flutter/flutter/issues/186906
    
    ## Pre-Review Checklist
    
    **Test exemption:** All generated code.
    
    **Note**: The Flutter team is currently trialing the use of [Gemini Code Assist for GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code). Comments from the `gemini-code-assist` bot should not be taken as authoritative feedback from the Flutter team. If you find its comments useful you can update your code accordingly, but if you are unsure or disagree with the feedback, please feel free to wait for a Flutter team member's review for guidance on which automated comments should be addressed.
    
    [^1]: Regular contributors who have demonstrated familiarity with the repository guidelines only need to comment if the PR is not auto-exempted by repo tooling.

---
+++ packages/material_ui/tool/gen_defaults/data/fab_menu.dart
@@ -0,0 +1,69 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Version: 38.2.31
+
+// dart format off
+import 'shape_struct.dart';
+import 'typescale.dart';
+import 'typescale_struct.dart';
+
+class TokenFabMenu {
+  /// md.comp.fab-menu.close-button.between-space
+  static const double closeButtonBetweenSpace = 8.00;
+
+  /// md.comp.fab-menu.close-button.container.elevation
+  static const double closeButtonContainerElevation = 6.00;
+
+  /// md.comp.fab-menu.close-button.container.height
+  static const double closeButtonContainerHeight = 56.00;
+
+  /// md.comp.fab-menu.close-button.container.shape
+  static const ShapeStruct closeButtonContainerShape = ShapeStruct(
+    family: 'SHAPE_FAMILY_CIRCULAR',
+    topLeft: 0.00,
+    topRight: 0.00,
+    bottomLeft: 0.00,
+    bottomRight: 0.00,
+  );
+
+  /// md.comp.fab-menu.close-button.container.width
+  static const double closeButtonContainerWidth = 56.00;
+
+  /// md.comp.fab-menu.close-button.icon.size
+  static const double closeButtonIconSize = 20.00;
+
+  /// md.comp.fab-menu.menu-item.between-space
+  static const double menuItemBetweenSpace = 4.00;
+
+  /// md.comp.fab-menu.menu-item.container.elevation
+  static const double menuItemContainerElevation = 0.00;
+
+  /// md.comp.fab-menu.menu-item.container.height
+  static const double menuItemContainerHeight = 56.00;
+
+  /// md.comp.fab-menu.menu-item.container.shape
+  static const ShapeStruct menuItemContainerShape = ShapeStruct(
+    family: 'SHAPE_FAMILY_CIRCULAR',
+    topLeft: 0.00,
+    topRight: 0.00,
+    bottomLeft: 0.00,
+    bottomRight: 0.00,
+  );
+
+  /// md.comp.fab-menu.menu-item.icon-label-space
+  static const double menuItemIconLabelSpace = 8.00;
+
+  /// md.comp.fab-menu.menu-item.icon.size
+  static const double menuItemIconSize = 24.00;
+
+  /// md.comp.fab-menu.menu-item.label-text
+  static const TypescaleStruct menuItemLabelText = TokenTypescale.titleMedium;
+
+  /// md.comp.fab-menu.menu-item.leading-space
+  static const double menuItemLeadingSpace = 24.00;
+
+  /// md.comp.fab-menu.menu-item.trailing-space
+  static const double menuItemTrailingSpace = 24.00;
+}
