f41920bf8e2d6b15c585a7841888f29dfafc8c12
Stuart Morgan <stuartmorgan@google.com>
2026-08-26 15:23:06 -0400

    Sync

---
+++ packages/google_maps_flutter/google_maps_flutter_ios_shared_code/ios/google_maps_flutter_ios/Sources/google_maps_flutter_ios/GroundOverlayController.swift
@@ -0,0 +1,220 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import GoogleMaps
+import UIKit
+import google_maps_flutter_ios_objc
+
+/// Controller of a single ground overlay on the map.
+class GroundOverlayController: NSObject {
+  let groundOverlay: GMSGroundOverlay
+  private weak var mapView: GMSMapView?
+  let createdWithBounds: Bool
+  var zoomLevel: NSNumber?
+
+  init(
+    groundOverlay: GMSGroundOverlay, identifier: String, mapView: GMSMapView,
+    isCreatedWithBounds: Bool
+  ) {
+    self.groundOverlay = groundOverlay
+    self.mapView = mapView
+    self.groundOverlay.userData = [identifier]
+    self.createdWithBounds = isCreatedWithBounds
+    super.init()
+  }
+
+  func removeGroundOverlay() {
+    groundOverlay.map = nil
+  }
+
+  /// Updates the controller's ground overlay with the properties from a FGMPlatformGroundOverlay.
+  ///
+  /// Setting the ground overlay to visible will set its map to the controller's mapView.
+  func update(
+    from platformGroundOverlay: FGMPlatformGroundOverlay, assetProvider: FGMAssetProvider,
+    screenScale: CGFloat
+  ) {
+    if let mapView = mapView {
+      GroundOverlayController.update(
+        groundOverlay,
+        from: platformGroundOverlay,
+        mapView: mapView,
+        assetProvider: assetProvider,
+        screenScale: screenScale,
+        usingBounds: createdWithBounds
+      )
+    }
+  }
+
+  /// Updates the given GMSGroundOverlay with the properties from a FGMPlatformGroundOverlay.
+  ///
+  /// Setting the ground overlay to visible will set its map to the given mapView.
+  static func update(
+    _ groundOverlay: GMSGroundOverlay,
+    from platformGroundOverlay: FGMPlatformGroundOverlay,
+    mapView: GMSMapView,
+    assetProvider: FGMAssetProvider,
+    screenScale: CGFloat,
+    usingBounds useBounds: Bool
+  ) {
+    groundOverlay.isTappable = platformGroundOverlay.clickable
+    groundOverlay.zIndex = Int32(platformGroundOverlay.zIndex)
+    if let anchor = platformGroundOverlay.anchor {
+      groundOverlay.anchor = CGPoint(x: anchor.x, y: anchor.y)
+    }
+    groundOverlay.icon = FGMIconFromBitmap(platformGroundOverlay.image, assetProvider, screenScale)
+    groundOverlay.bearing = platformGroundOverlay.bearing
+    groundOverlay.opacity = Float(1.0 - platformGroundOverlay.transparency)
+    if useBounds {
+      if let bounds = platformGroundOverlay.bounds {
+        groundOverlay.bounds = GMSCoordinateBounds(
+          coordinate: CLLocationCoordinate2D(
+            latitude: bounds.northeast.latitude,
+            longitude: bounds.northeast.longitude
+          ),
+          coordinate: CLLocationCoordinate2D(
+            latitude: bounds.southwest.latitude,
+            longitude: bounds.southwest.longitude
+          )
+        )
+      }
+    } else {
+      if let position = platformGroundOverlay.position {
+        groundOverlay.position = CLLocationCoordinate2D(
+          latitude: position.latitude,
+          longitude: position.longitude
+        )
+      }
+    }
+
+    // This must be done last, to avoid visual flickers of default property values.
+    groundOverlay.map = platformGroundOverlay.visible ? mapView : nil
+  }
+}
+
+/// Controller of multiple ground overlays on the map.
+class GroundOverlaysController: NSObject {
+  private var groundOverlayControllerByIdentifier: [String: GroundOverlayController] = [:]
+  private weak var eventDelegate: FGMMapEventDelegate?
+  private let assetProvider: FGMAssetProvider
+  private weak var mapView: GMSMapView?
+
+  init(mapView: GMSMapView, eventDelegate: FGMMapEventDelegate, assetProvider: FGMAssetProvider) {
+    self.mapView = mapView
+    self.eventDelegate = eventDelegate
+    self.assetProvider = assetProvider
+    super.init()
+  }
+
+  func add(_ groundOverlaysToAdd: [FGMPlatformGroundOverlay]) {
+    guard let mapView = mapView else { return }
+    let screenScale = getScreenScale()
+    for groundOverlay in groundOverlaysToAdd {
+      let identifier = groundOverlay.groundOverlayId
+      let gmsOverlay: GMSGroundOverlay
+      var isCreatedWithBounds = false
+      if let position = groundOverlay.position {
+        guard let zoomLevel = groundOverlay.zoomLevel else {
+          fatalError("If ground overlay is initialized with position, zoomLevel is required")
+        }
+        gmsOverlay = GMSGroundOverlay(
+          position: CLLocationCoordinate2D(
+            latitude: position.latitude,
+            longitude: position.longitude
+          ),
+          icon: FGMIconFromBitmap(groundOverlay.image, assetProvider, screenScale),
+          zoomLevel: CGFloat(zoomLevel.doubleValue)
+        )
+      } else {
+        isCreatedWithBounds = true
+        // TODO(stuartmorgan): Convert these to Pigeon error handling rather
+        // than being fatal. The original Obj-C code used NSAssert instead of
+        // passing errors back, which isn't how we should handle errors.
+        guard let bounds = groundOverlay.bounds else {
+          fatalError("If ground overlay is initialized without position, bounds are required")
+        }
+        let gmsBounds = GMSCoordinateBounds(
+          coordinate: CLLocationCoordinate2D(
+            latitude: bounds.northeast.latitude,
+            longitude: bounds.northeast.longitude
+          ),
+          coordinate: CLLocationCoordinate2D(
+            latitude: bounds.southwest.latitude,
+            longitude: bounds.southwest.longitude
+          )
+        )
+        gmsOverlay = GMSGroundOverlay(
+          bounds: gmsBounds,
+          icon: FGMIconFromBitmap(groundOverlay.image, assetProvider, screenScale)
+        )
+      }
+      let controller = GroundOverlayController(
+        groundOverlay: gmsOverlay,
+        identifier: identifier,
+        mapView: mapView,
+        isCreatedWithBounds: isCreatedWithBounds
+      )
+      controller.zoomLevel = groundOverlay.zoomLevel
+      controller.update(
+        from: groundOverlay,
+        assetProvider: assetProvider,
+        screenScale: screenScale
+      )
+      groundOverlayControllerByIdentifier[identifier] = controller
+    }
+  }
+
+  func change(_ groundOverlaysToChange: [FGMPlatformGroundOverlay]) {
+    let screenScale = getScreenScale()
+    for groundOverlay in groundOverlaysToChange {
+      let identifier = groundOverlay.groundOverlayId
+      groundOverlayControllerByIdentifier[identifier]?.update(
+        from: groundOverlay,
+        assetProvider: assetProvider,
+        screenScale: screenScale
+      )
+    }
+  }
+
+  func removeGroundOverlays(withIdentifiers identifiers: [String]) {
+    for identifier in identifiers {
+      if let controller = groundOverlayControllerByIdentifier[identifier] {
+        controller.removeGroundOverlay()
+        groundOverlayControllerByIdentifier.removeValue(forKey: identifier)
+      }
+    }
+  }
+
+  func didTapGroundOverlay(withIdentifier identifier: String) {
+    if hasGroundOverlays(withIdentifier: identifier) {
+      eventDelegate?.didTapGroundOverlay(withIdentifier: identifier)
+    }
+  }
+
+  func hasGroundOverlays(withIdentifier identifier: String) -> Bool {
+    return groundOverlayControllerByIdentifier[identifier] != nil
+  }
+
+  func getScreenScale() -> CGFloat {
+    // TODO(jokerttu): This method is called on marker creation, which, for initial markers, is done
+    // before the view is added to the view hierarchy. This means that the traitCollection values may
+    // not be matching the right display where the map is finally shown. The solution should be
+    // revisited after the proper way to fetch the display scale is resolved for platform views. This
+    // should be done under the context of the following issue:
+    // https://github.com/flutter/flutter/issues/125496.
+    return mapView?.traitCollection.displayScale ?? 1.0
+  }
+
+  func groundOverlay(withIdentifier identifier: String) -> FGMPlatformGroundOverlay? {
+    guard let controller = groundOverlayControllerByIdentifier[identifier] else {
+      return nil
+    }
+    return FGMGetPigeonGroundOverlay(
+      controller.groundOverlay,
+      identifier,
+      controller.createdWithBounds,
+      controller.zoomLevel
+    )
+  }
+}
---
+++ packages/google_maps_flutter/google_maps_flutter_ios_shared_code/ios/google_maps_flutter_ios/Sources/google_maps_flutter_ios/TileOverlayController.swift
@@ -0,0 +1,191 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import GoogleMaps
+import UIKit
+import google_maps_flutter_ios_objc
+
+/// Protocol for requesting tiles from the Dart side.
+// TODO(stuartmorgan): Adjust this to match the Swift API once the Pigeon
+// generation is switched to Swift.
+protocol TileProviderDelegate: AnyObject {
+  func tile(
+    withOverlayIdentifier tileOverlayId: String,
+    location: FGMPlatformPoint,
+    zoom: Int,
+    completion: @escaping (FGMPlatformTile?, FlutterError?) -> Void
+  )
+}
+
+/// Controller of a single tile overlay on the map.
+class TileOverlayController: NSObject {
+  let layer: GMSTileLayer
+  private weak var mapView: GMSMapView?
+
+  init(tileOverlay: FGMPlatformTileOverlay, tileLayer: GMSTileLayer, mapView: GMSMapView) {
+    self.layer = tileLayer
+    self.mapView = mapView
+    super.init()
+    TileOverlayController.update(tileLayer, from: tileOverlay, with: mapView)
+  }
+
+  func removeTileOverlay() {
+    layer.map = nil
+  }
+
+  func clearTileCache() {
+    layer.clearTileCache()
+  }
+
+  /// Updates the controller's tile overlay with the properties from a FGMPlatformTileOverlay.
+  ///
+  /// Setting the tile overlay to visible will set its map to the controller's mapView.
+  func update(from overlay: FGMPlatformTileOverlay) {
+    if let mapView = mapView {
+      TileOverlayController.update(layer, from: overlay, with: mapView)
+    }
+  }
+
+  /// Updates the given GMSTileLayer with the properties from a FGMPlatformTileOverlay.
+  ///
+  /// Setting the tile overlay to visible will set its map to the given mapView.
+  static func update(
+    _ tileLayer: GMSTileLayer,
+    from platformOverlay: FGMPlatformTileOverlay,
+    with mapView: GMSMapView
+  ) {
+    tileLayer.opacity = Float(1.0 - platformOverlay.transparency)
+    tileLayer.zIndex = Int32(platformOverlay.zIndex)
+    tileLayer.fadeIn = platformOverlay.fadeIn
+    tileLayer.tileSize = Int(platformOverlay.tileSize)
+
+    // This must be done last, to avoid visual flickers of default property values.
+    tileLayer.map = platformOverlay.visible ? mapView : nil
+  }
+}
+
+/// Custom tile layer that requests tiles through a TileProviderDelegate.
+class TileProviderController: GMSTileLayer {
+  let tileOverlayIdentifier: String
+  private weak var tileProviderDelegate: TileProviderDelegate?
+
+  init(
+    tileOverlayIdentifier: String,
+    tileProvider: TileProviderDelegate
+  ) {
+    self.tileOverlayIdentifier = tileOverlayIdentifier
+    self.tileProviderDelegate = tileProvider
+    super.init()
+  }
+
+  func handleResultTile(_ tile: UIImage?) -> UIImage? {
+    guard let tile = tile, let imageRef = tile.cgImage else {
+      return tile
+    }
+    let bitmapInfo = imageRef.bitmapInfo
+    let isFloat = bitmapInfo.contains(.floatComponents)
+    let bitsPerComponent = imageRef.bitsPerComponent
+
+    // Engine uses f16 pixel format for wide gamut images.
+    // If it is wide gamut, we want to downsample it.
+    if isFloat && bitsPerComponent == 16 {
+      let colorSpace = CGColorSpaceCreateDeviceRGB()
+      if let context = CGContext(
+        data: nil,
+        width: Int(tile.size.width),
+        height: Int(tile.size.height),
+        bitsPerComponent: 8,
+        bytesPerRow: 0,
+        space: colorSpace,
+        bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+      ) {
+        context.draw(imageRef, in: CGRect(origin: .zero, size: tile.size))
+        if let image = context.makeImage() {
+          return UIImage(cgImage: image)
+        }
+      }
+    }
+    return tile
+  }
+
+  override func requestTileFor(x: UInt, y: UInt, zoom: UInt, receiver: any GMSTileReceiver) {
+    DispatchQueue.main.async { [weak self] in
+      guard let self = self else { return }
+      self.tileProviderDelegate?.tile(
+        withOverlayIdentifier: self.tileOverlayIdentifier,
+        location: FGMPlatformPoint.makeWith(x: Double(x), y: Double(y)),
+        zoom: Int(zoom)
+      ) { [weak self] tile, error in
+        guard let self = self else { return }
+        let typedData = tile?.data
+        let tileImage: UIImage
+        if let data = typedData?.data {
+          tileImage = self.handleResultTile(UIImage(data: data)) ?? kGMSTileLayerNoTile
+        } else {
+          tileImage = kGMSTileLayerNoTile
+        }
+        if let error = error {
+          NSLog(
+            "Can't get tile: errorCode = \(error.code), errorMessage = \(error.message ?? "nil"), details = \(error.details ?? "nil")"
+          )
+        }
+        receiver.receiveTileWith(x: x, y: y, zoom: zoom, image: tileImage)
+      }
+    }
+  }
+}
+
+/// Controller of multiple tile overlays on the map.
+class TileOverlaysController: NSObject {
+  private var tileOverlayIdentifierToController: [String: TileOverlayController] = [:]
+  private weak var tileProviderDelegate: TileProviderDelegate?
+  private weak var mapView: GMSMapView?
+
+  init(mapView: GMSMapView, tileProvider: TileProviderDelegate) {
+    self.mapView = mapView
+    self.tileProviderDelegate = tileProvider
+    super.init()
+  }
+
+  func add(_ tileOverlaysToAdd: [FGMPlatformTileOverlay]) {
+    guard let mapView = mapView, let tileProviderDelegate = tileProviderDelegate else { return }
+    for tileOverlay in tileOverlaysToAdd {
+      let identifier = tileOverlay.tileOverlayId
+      let tileProvider = TileProviderController(
+        tileOverlayIdentifier: identifier,
+        tileProvider: tileProviderDelegate
+      )
+      let controller = TileOverlayController(
+        tileOverlay: tileOverlay,
+        tileLayer: tileProvider,
+        mapView: mapView
+      )
+      tileOverlayIdentifierToController[identifier] = controller
+    }
+  }
+
+  func change(_ tileOverlaysToChange: [FGMPlatformTileOverlay]) {
+    for tileOverlay in tileOverlaysToChange {
+      let identifier = tileOverlay.tileOverlayId
+      tileOverlayIdentifierToController[identifier]?.update(from: tileOverlay)
+    }
+  }
+
+  func removeTileOverlay(withIdentifiers identifiers: [String]) {
+    for identifier in identifiers {
+      if let controller = tileOverlayIdentifierToController[identifier] {
+        controller.removeTileOverlay()
+        tileOverlayIdentifierToController.removeValue(forKey: identifier)
+      }
+    }
+  }
+
+  func clearTileCache(withIdentifier identifier: String) {
+    tileOverlayIdentifierToController[identifier]?.clearTileCache()
+  }
+
+  func tileOverlay(withIdentifier identifier: String) -> TileOverlayController? {
+    return tileOverlayIdentifierToController[identifier]
+  }
+}
