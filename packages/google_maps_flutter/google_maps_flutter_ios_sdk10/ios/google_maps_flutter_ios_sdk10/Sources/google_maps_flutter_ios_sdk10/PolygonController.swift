4797ae24bad34888df7a6f0e96054bb86f0b635e
stuartmorgan-g <stuartmorgan@google.com>
2026-08-26 15:02:01 -0400

    [google_maps_flutter] Convert simple object controllers to Swift (#12618)
    This converts CircleController, PolygonController, and PolylineController to Swift in the `_sdk*` packages.
    
    The remaining Obj-C code will be migrated in a series of follow-up PRs to keep the scope of each PR reasonable for review.
    
    The conversion process was:
    - Initial conversion via Gemini, with explicit instruction to keep the structure the same.
    - Side-by-side manual review of the old and new versions of the files.
    - Manual fixes and improvements.
    
    This includes test changes beyond what we would normally expect for a language conversion PR (e.g., name changes from dropping the prefix) because the conversion to Swift exposed a latent issue in the tests. Several tests had a pattern where a helper method was creating a local `mapView` instance, using it to construct the subcontroller under test, and then returning that subcontroller—at which point the map view goes out of scope. The subcontrollers have weak references to the map view, so it can be deallocated before the test actually runs, and doing so causes the tests to fail (as many operations in the subcontroller no-op when the map view is nil). In cases where the helper is only used once, it can be inlined; in cases where it's used repeatedly we can instead return multiple values and keep the map view alive.
    
    Part of https://github.com/flutter/flutter/issues/119108
    
    ## Pre-Review Checklist
    
    [^1]: Regular contributors who have demonstrated familiarity with the repository guidelines only need to comment if the PR is not auto-exempted by repo tooling.

---
+++ packages/google_maps_flutter/google_maps_flutter_ios_sdk10/ios/google_maps_flutter_ios_sdk10/Sources/google_maps_flutter_ios_sdk10/PolygonController.swift
@@ -0,0 +1,105 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import GoogleMaps
+
+#if canImport(google_maps_flutter_ios_sdk10_objc)
+  import google_maps_flutter_ios_sdk10_objc
+#endif
+
+/// Defines polygon controllable by Flutter.
+class PolygonController: NSObject {
+  let polygon: GMSPolygon
+  private weak var mapView: GMSMapView?
+
+  init(identifier: String, mapView: GMSMapView) {
+    self.polygon = GMSPolygon()
+    self.mapView = mapView
+    self.polygon.userData = [identifier]
+    super.init()
+  }
+
+  func removePolygon() {
+    polygon.map = nil
+  }
+
+  /// Updates the controller's polygon with the properties from a FGMPlatformPolygon.
+  ///
+  /// Setting the polygon to visible will set its map to the controller's mapView.
+  func update(from platformPolygon: FGMPlatformPolygon) {
+    if let mapView = mapView {
+      PolygonController.update(polygon, from: platformPolygon, with: mapView)
+    }
+  }
+
+  /// Updates the given GMSPolygon with the properties from a FGMPlatformPolygon.
+  ///
+  /// Setting the polygon to visible will set its map to the given mapView.
+  static func update(
+    _ polygon: GMSPolygon, from platformPolygon: FGMPlatformPolygon, with mapView: GMSMapView
+  ) {
+    polygon.isTappable = platformPolygon.consumesTapEvents
+    polygon.zIndex = Int32(platformPolygon.zIndex)
+    polygon.path = FGMGetPathFromPoints(FGMGetPointsForPigeonLatLngs(platformPolygon.points))
+    polygon.holes = FGMGetHolesForPigeonLatLngArrays(platformPolygon.holes).map {
+      FGMGetPathFromPoints($0)
+    }
+    polygon.fillColor = FGMGetColorForPigeonColor(platformPolygon.fillColor)
+    polygon.strokeColor = FGMGetColorForPigeonColor(platformPolygon.strokeColor)
+    polygon.strokeWidth = CGFloat(platformPolygon.strokeWidth)
+
+    // This must be done last, to avoid visual flickers of default property values.
+    polygon.map = platformPolygon.visible ? mapView : nil
+  }
+}
+
+class PolygonsController: NSObject {
+  private var polygonIdentifierToController: [String: PolygonController] = [:]
+  private weak var eventDelegate: FGMMapEventDelegate?
+  private weak var mapView: GMSMapView?
+
+  init(mapView: GMSMapView, eventDelegate: FGMMapEventDelegate) {
+    self.mapView = mapView
+    self.eventDelegate = eventDelegate
+    super.init()
+  }
+
+  func add(_ polygons: [FGMPlatformPolygon]) {
+    guard let mapView = mapView else { return }
+    for polygon in polygons {
+      let identifier = polygon.polygonId
+      let controller = PolygonController(identifier: identifier, mapView: mapView)
+      // TODO(stuarmorgan): Consider updating the flow here to do the update from within
+      // the initialiazer, as in CircleController.
+      controller.update(from: polygon)
+      polygonIdentifierToController[identifier] = controller
+    }
+  }
+
+  func change(_ polygons: [FGMPlatformPolygon]) {
+    for polygon in polygons {
+      let identifier = polygon.polygonId
+      polygonIdentifierToController[identifier]?.update(from: polygon)
+    }
+  }
+
+  func removePolygon(withIdentifiers identifiers: [String]) {
+    for identifier in identifiers {
+      if let controller = polygonIdentifierToController[identifier] {
+        controller.removePolygon()
+        polygonIdentifierToController.removeValue(forKey: identifier)
+      }
+    }
+  }
+
+  func didTapPolygon(withIdentifier identifier: String) {
+    if hasPolygon(withIdentifier: identifier) {
+      eventDelegate?.didTapPolygon(withIdentifier: identifier)
+    }
+  }
+
+  func hasPolygon(withIdentifier identifier: String) -> Bool {
+    return polygonIdentifierToController[identifier] != nil
+  }
+}
---
+++ packages/google_maps_flutter/google_maps_flutter_ios_sdk9/ios/google_maps_flutter_ios_sdk9/Sources/google_maps_flutter_ios_sdk9/CircleController.swift
@@ -0,0 +1,98 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import GoogleMaps
+
+#if canImport(google_maps_flutter_ios_sdk9_objc)
+  import google_maps_flutter_ios_sdk9_objc
+#endif
+
+/// Defines circle controllable by Flutter.
+class CircleController: NSObject {
+  let circle: GMSCircle
+  private weak var mapView: GMSMapView?
+
+  init(circle: FGMPlatformCircle, mapView: GMSMapView) {
+    self.circle = GMSCircle()
+    self.mapView = mapView
+    self.circle.userData = [circle.circleId]
+    super.init()
+    CircleController.update(self.circle, from: circle, with: mapView)
+  }
+
+  func removeCircle() {
+    circle.map = nil
+  }
+
+  /// Updates the controller's circle with the properties from a FGMPlatformCircle.
+  ///
+  /// Setting the circle to visible will set its map to the given mapView.
+  func update(from platformCircle: FGMPlatformCircle) {
+    if let mapView = mapView {
+      CircleController.update(circle, from: platformCircle, with: mapView)
+    }
+  }
+
+  /// Updates the given GMSCircle with the properties from a FGMPlatformCircle.
+  ///
+  /// Setting the circle to visible will set its map to the given mapView.
+  static func update(
+    _ circle: GMSCircle, from platformCircle: FGMPlatformCircle, with mapView: GMSMapView
+  ) {
+    circle.isTappable = platformCircle.consumeTapEvents
+    circle.zIndex = Int32(platformCircle.zIndex)
+    circle.position = FGMGetCoordinateForPigeonLatLng(platformCircle.center)
+    circle.radius = platformCircle.radius
+    circle.strokeColor = FGMGetColorForPigeonColor(platformCircle.strokeColor)
+    circle.strokeWidth = CGFloat(platformCircle.strokeWidth)
+    circle.fillColor = FGMGetColorForPigeonColor(platformCircle.fillColor)
+
+    // This must be done last, to avoid visual flickers of default property values.
+    circle.map = platformCircle.visible ? mapView : nil
+  }
+}
+
+class CirclesController: NSObject {
+  private weak var eventDelegate: FGMMapEventDelegate?
+  private weak var mapView: GMSMapView?
+  private var circleIdToController: [String: CircleController] = [:]
+
+  init(mapView: GMSMapView, eventDelegate: FGMMapEventDelegate) {
+    self.mapView = mapView
+    self.eventDelegate = eventDelegate
+    super.init()
+  }
+
+  func add(_ circles: [FGMPlatformCircle]) {
+    guard let mapView = mapView else { return }
+    for circle in circles {
+      circleIdToController[circle.circleId] = CircleController(circle: circle, mapView: mapView)
+    }
+  }
+
+  func change(_ circles: [FGMPlatformCircle]) {
+    for circle in circles {
+      circleIdToController[circle.circleId]?.update(from: circle)
+    }
+  }
+
+  func removeCircles(withIdentifiers identifiers: [String]) {
+    for identifier in identifiers {
+      if let controller = circleIdToController[identifier] {
+        controller.removeCircle()
+        circleIdToController.removeValue(forKey: identifier)
+      }
+    }
+  }
+
+  func hasCircle(withIdentifier identifier: String) -> Bool {
+    return circleIdToController[identifier] != nil
+  }
+
+  func didTapCircle(withIdentifier identifier: String) {
+    if hasCircle(withIdentifier: identifier) {
+      eventDelegate?.didTapCircle(withIdentifier: identifier)
+    }
+  }
+}
