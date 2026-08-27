5c6c1931b81371f4ab30cc5571c88a4726c59a18
Ricardo Dalarme <ricardodalarme@outlook.com>
2026-08-27 09:41:36 -0300

    refactor: remove unnecessary material imports from camera

--- packages/camera/camera_android_camerax/example/lib/camera_preview.dart
+++ packages/camera/camera_android_camerax/example/lib/camera_preview.dart
@@ -3,8 +3,8 @@
 // found in the LICENSE file.
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 import 'camera_controller.dart';
 
