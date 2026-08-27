64fce9885635ace087b01b77f99d5ee44dc6ae1a
Ricardo Dalarme <ricardodalarme@outlook.com>
2026-08-27 09:28:55 -0300

    refactor: remove unnecessary material imports from pointer_interceptor

--- packages/pointer_interceptor/pointer_interceptor/example/lib/platforms/native_widget_ios.dart
+++ packages/pointer_interceptor/pointer_interceptor/example/lib/platforms/native_widget_ios.dart
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 /// A widget representing an underlying platform view.
 class NativeWidget extends StatelessWidget {
