f045e811a38529e6c618d8a201e6757196b45390
Stuart Morgan <stuartmorgan@google.com>
2026-08-27 10:43:39 -0400

    Sync

---
+++ packages/google_maps_flutter/google_maps_flutter_ios_sdk10/ios/google_maps_flutter_ios_sdk10/Sources/google_maps_flutter_ios_sdk10/ConversionUtils.swift
@@ -0,0 +1,25 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#if canImport(google_maps_flutter_ios_sdk10_objc)
+  import google_maps_flutter_ios_sdk10_objc
+#endif
+
+/// Converts a GMUCluster to its Pigeon representation.
+func pigeonCluster(for cluster: GMUCluster, clusterManagerIdentifier: String) -> FGMPlatformCluster
+{
+  var bounds = GMSCoordinateBounds()
+
+  let markerIds = cluster.items.filter { $0 is GMSMarker }.compactMap { marker in
+    bounds = bounds.includingCoordinate(marker.position)
+    return markerIdentifierFromMarker(marker as! GMSMarker)
+  }
+
+  return FGMPlatformCluster.make(
+    withClusterManagerId: clusterManagerIdentifier,
+    position: FGMGetPigeonLatLngForCoordinate(cluster.position),
+    bounds: FGMGetPigeonLatLngBoundsForCoordinateBounds(bounds),
+    markerIds: markerIds
+  )
+}
