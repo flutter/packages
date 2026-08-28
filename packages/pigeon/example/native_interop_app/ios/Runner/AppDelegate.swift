ba453be46b0b1e34ba368a694d6c6b6ac4136ca6
tarrinneal <tarrinneal@gmail.com>
2026-08-28 12:32:29 -0700

    remove unneeded sub class from new example

--- packages/pigeon/example/native_interop_app/ios/Runner/AppDelegate.swift
+++ packages/pigeon/example/native_interop_app/ios/Runner/AppDelegate.swift
@@ -14,26 +14,11 @@ private class PigeonApiImplementation: NativeInteropExampleApi {
 }
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-}
-
-// TODO(stuartmorgan): Once 3.33+ reaches stable, remove this subclass and move the setup to
-// AppDelegate.register(...). This approach is only used because this example needs to support
-// both stable and master, and 3.32 doesn't have FlutterPluginRegistrant, while 3.33+ can't use
-// the older application(didFinishLaunchingWithOptions) approach.
-@objc class ExampleViewController: FlutterViewController {
-  override func awakeFromNib() {
-    super.awakeFromNib()
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
 
     let api = PigeonApiImplementation()
     NativeInteropExampleApiSetup.register(api: api)
-
   }
 }
