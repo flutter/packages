5907f4958dd469b7088d560af040843df88cc37f
Kostia Sokolovskyi <sokolovskyi.konstantin@gmail.com>
2026-08-30 14:09:05 +0200

    Make the internal Point an alias for Offset.

--- packages/material_ui/lib/src/shapes/point.dart
+++ packages/material_ui/lib/src/shapes/point.dart
@@ -9,6 +9,7 @@
 library;
 
 import 'dart:math' as math;
+import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:vector_math/vector_math_64.dart' show Matrix4, Vector3;
@@ -19,21 +20,27 @@ import 'package:vector_math/vector_math_64.dart' show Matrix4, Vector3;
 /// [RoundedPolygon.transformed] to apply arbitrary transformations to a shape.
 typedef PointTransformer = (double, double) Function(double x, double y);
 
+/// A two dimensional coordinate pair used by the shape algorithms.
 @internal
-@immutable
-class Point {
-  const Point(this.x, this.y);
+typedef Point = Offset;
 
-  static const zero = Point(0, 0);
+/// The geometry [Offset] does not provide.
+@internal
+extension PointGeometry on Offset {
+  /// The horizontal coordinate of this point.
+  double get x => dx;
 
-  final double x;
+  /// The vertical coordinate of this point.
+  double get y => dy;
 
-  final double y;
+  /// The angle of this point in radians, measured clockwise from the positive
+  /// X axis.
+  double get angleRadians => direction;
 
-  Point copy() => Point(x, y);
-
+  /// Returns this point rotated a quarter turn counterclockwise around (0, 0).
   Point rotate90() => Point(-y, x);
 
+  /// Returns this point rotated by [degrees] around [center].
   Point rotate(double degrees, {Point center = Point.zero}) {
     final double radians = degrees * math.pi / 180;
     final Point off = this - center;
@@ -42,30 +49,24 @@ class Point {
     return Point(off.x * cos - off.y * sin, off.x * sin + off.y * cos) + center;
   }
 
-  Point translate(double dx, double dy) => Point(x + dx, y + dy);
-
-  Point scale(double sx, double sy) => Point(x * sx, y * sy);
-
-  double get angleDegrees => angleRadians * math.pi / 180;
-
-  double get angleRadians => math.atan2(y, x);
-
   /// The magnitude of the [Point], which is the distance of this point from
   /// (0, 0).
   ///
   /// If you need this value to compare it to another [Point]'s distance,
   /// consider using [getDistanceSquared] instead, since it is cheaper to
   /// compute.
-  double getDistance() => math.sqrt(x * x + y * y);
+  double getDistance() => distance;
 
   /// The square of the magnitude (which is the distance of this point from
   /// (0, 0)) of the [Point].
   ///
   /// This is cheaper than computing the [getDistance] itself.
-  double getDistanceSquared() => x * x + y * y;
+  double getDistanceSquared() => distanceSquared;
 
+  /// The dot product of this point and [other], both taken as vectors.
   double dotProduct(Point other) => x * other.x + y * other.y;
 
+  /// The dot product of this point and the vector ([otherX], [otherY]).
   double dotProductXY(double otherX, double otherY) => x * otherX + y * otherY;
 
   /// Compute the Z coordinate of the cross product of two vectors, to check
@@ -74,78 +75,19 @@ class Point {
   /// are co-linear.
   bool clockwise(Point other) => (x * other.y - y * other.x) > 0;
 
+  /// Returns the unit vector representing the direction to this point from
+  /// (0, 0).
   Point getDirection() {
     final double d = getDistance();
     assert(d > 0, "Can't get the direction of a 0-length vector");
     return this / d;
   }
 
-  /// Unary negation operator.
-  ///
-  /// Returns a [Point] with the coordinates negated.
-  ///
-  /// If the [Point] represents an arrow on a plane, this operator returns the
-  /// same arrow but pointing in the reverse direction.
-  Point operator -() => Point(-x, -y);
-
-  /// Binary subtraction operator.
-  ///
-  /// Returns a Point whose [x] value is the left-hand-side operand's [x]
-  /// minus the right-hand-side operand's [x] and whose [y] value is the
-  /// left-hand-side operand's [y] minus the right-hand-side operand's [y].
-  Point operator -(Point operand) => Point(x - operand.x, y - operand.y);
-
-  /// Binary addition operator.
-  ///
-  /// Returns a Point whose [x] value is the sum of the [x] values of the two
-  /// operands, and whose [y] value is the sum of the [y] values of the two
-  /// operands.
-  Point operator +(Point operand) => Point(x + operand.x, y + operand.y);
-
-  /// Multiplication operator.
-  ///
-  /// Returns a Point whose coordinates are the coordinates of the
-  /// left-hand-side operand (a [Point]) multiplied by the scalar
-  /// right-hand-side operand (a [double]).
-  Point operator *(double operand) => Point(x * operand, y * operand);
-
-  /// Division operator.
-  ///
-  /// Returns a Point whose coordinates are the coordinates of the
-  /// left-hand-side operand (a [Point]) divided by the scalar
-  /// right-hand-side operand (a [double]).
-  Point operator /(double operand) => Point(x / operand, y / operand);
-
-  /// Modulo (remainder) operator.
-  ///
-  /// Returns a Point whose coordinates are the remainder of dividing the
-  /// coordinates of the left-hand-side operand (a [Point]) by the scalar
-  /// right-hand-side operand (a [double]).
-  Point operator %(double operand) => Point(x % operand, y % operand);
-
+  /// Returns a copy of this point with [f] applied to it.
   Point transformed(PointTransformer f) {
     final (double, double) result = f(x, y);
     return Point(result.$1, result.$2);
   }
-
-  @override
-  String toString() => 'Point($x, $y)';
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(other, this)) {
-      return true;
-    }
-
-    if (other is! Point) {
-      return false;
-    }
-
-    return other.x == x && other.y == y;
-  }
-
-  @override
-  int get hashCode => Object.hashAll([x, y]);
 }
 
 /// Adapts a [Matrix4] into a [PointTransformer].
