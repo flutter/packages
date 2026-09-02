30bb8e8e9aa25e56c7471df4eae82e3ee1d7d324
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2026-09-02 15:10:16 -0700

    Format

--- packages/material_ui/tool/gen_defaults/bin/gen_defaults.dart
+++ packages/material_ui/tool/gen_defaults/bin/gen_defaults.dart
@@ -15,6 +15,7 @@ import 'package:args/args.dart';
 import '../templates/action_chip_template.dart';
 import '../templates/app_bar_template.dart';
 import '../templates/badge_template.dart';
+
 // import '../templates/banner_template.dart';
 // import '../templates/bottom_app_bar_template.dart';
 import '../templates/bottom_sheet_template.dart';
