9af9c6076d79680faee0cd7e63c2ed4afd7c6e00
chunhtai <47866232+chunhtai@users.noreply.github.com>
2026-09-03 11:41:10 -0700

    [material_ui] Copies a11y_assessments (#12686)
    as title.
    
    Move the app in material gives us benefit that we can both fix accessibility issue and verify the fix on the app faster without waiting for a release cycle
    
    ## Pre-Review Checklist
    
    **Note**: The Flutter team is currently trialing the use of [Gemini Code Assist for GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code). Comments from the `gemini-code-assist` bot should not be taken as authoritative feedback from the Flutter team. If you find its comments useful you can update your code accordingly, but if you are unsure or disagree with the feedback, please feel free to wait for a Flutter team member's review for guidance on which automated comments should be addressed.
    
    [^1]: Regular contributors who have demonstrated familiarity with the repository guidelines only need to comment if the PR is not auto-exempted by repo tooling.

---
+++ packages/material_ui/test_apps/a11y_assessments/test/date_picker_test.dart
@@ -0,0 +1,27 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:a11y_assessments/use_cases/date_picker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:material_ui/material_ui.dart';
+
+import 'test_utils.dart';
+
+void main() {
+  testWidgets('date picker can run', (WidgetTester tester) async {
+    await pumpsUseCase(tester, DatePickerUseCase());
+    expect(find.text('Show Date Picker'), findsOneWidget);
+
+    await tester.tap(find.text('Show Date Picker'));
+    await tester.pumpAndSettle();
+    expect(find.byType(DatePickerDialog), findsOneWidget);
+  });
+
+  testWidgets('datepicker has one h1 tag', (WidgetTester tester) async {
+    await pumpsUseCase(tester, DatePickerUseCase());
+    final Finder findHeadingLevelOnes = find.bySemanticsLabel('DatePicker Demo');
+    await tester.pumpAndSettle();
+    expect(findHeadingLevelOnes, findsOne);
+  });
+}
