ddbeab0b8286940c5f88b031504ffcecb70fd4c2
Gyeony95 <rlg1133@naver.com>
2026-09-07 17:50:41 +0900

    [material_ui] Remove unconditional dart:io import from about.dart
    about.dart imported dart:io only to read Platform.resolvedExecutable as the
    fallback application name. Because about.dart is reachable from the package
    barrel, pana marks material_ui as not supporting the web and drops the
    platform:web and is:wasm-ready tags. Move the lookup behind a conditional
    import so the web build never references dart:io.
    
    Fixes https://github.com/flutter/flutter/issues/191887

---
+++ packages/material_ui/lib/src/_about_web.dart
@@ -0,0 +1,8 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// The file name of the currently running executable.
+///
+/// There is no executable on the web, so this is always the empty string.
+String get executableName => '';
