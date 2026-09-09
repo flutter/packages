155ba9a20e05659b9f4f7a61ef6d2901ed673300
Ricardo Dalarme <ricardodalarme@outlook.com>
2026-09-09 11:04:04 -0300

    feat(web_benchmarks): migrate test_app to material_ui

--- packages/web_benchmarks/testing/test_app/lib/home_page.dart
+++ packages/web_benchmarks/testing/test_app/lib/home_page.dart
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:material_ui/material_ui.dart';
 
 const ValueKey<String> textKey = ValueKey<String>('textKey');
 const ValueKey<String> aboutPageKey = ValueKey<String>('aboutPageKey');
