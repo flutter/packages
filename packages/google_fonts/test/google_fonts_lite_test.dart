ff08f0d8a9b6c780112afd4e9eca77dcdfb71718
Ricardo Dalarme <ricardodalarme@outlook.com>
2026-09-09 10:57:01 -0300

    feat(google_fonts): migrate to material_ui

--- packages/google_fonts/test/google_fonts_lite_test.dart
+++ packages/google_fonts/test/google_fonts_lite_test.dart
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:material_ui/material_ui.dart';
 
 void main() {
   testWidgets('GoogleFontsLite getFont returns the correct font with the given parameters', (
