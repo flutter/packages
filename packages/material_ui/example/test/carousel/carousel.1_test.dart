4c53346ff8364849e45ac61158c8e8c33572eabe
Mairramer <mairramer.dasilva28@hotmail.com>
2026-09-09 10:54:17 -0300

    format

--- packages/material_ui/example/test/carousel/carousel.1_test.dart
+++ packages/material_ui/example/test/carousel/carousel.1_test.dart
@@ -32,31 +32,35 @@ void main() {
     expect(find.text('Item 6'), findsOneWidget);
   });
 
-  testWidgets('CarouselView.builder items are wrapped with default properties', (
-    WidgetTester tester,
-  ) async {
-    await tester.pumpWidget(const example.CarouselBuilderExampleApp());
+  testWidgets(
+    'CarouselView.builder items are wrapped with default properties',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(const example.CarouselBuilderExampleApp());
 
-    expect(find.byType(CarouselView), findsOneWidget);
+      expect(find.byType(CarouselView), findsOneWidget);
 
-    final Finder itemMaterialFinder = find.descendant(
-      of: find.byType(CarouselView),
-      matching: find.byType(Material),
-    ).first;
-    
-    final Material material = tester.widget<Material>(itemMaterialFinder);
-    expect(material.clipBehavior, Clip.antiAlias);
-    expect(
-      material.shape, 
-      const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(28.0))),
-    );
+      final Finder itemMaterialFinder = find
+          .descendant(
+            of: find.byType(CarouselView),
+            matching: find.byType(Material),
+          )
+          .first;
 
-    final Finder paddingFinder = find.ancestor(
-      of: itemMaterialFinder,
-      matching: find.byType(Padding),
-    ).first;
-    
-    final Padding padding = tester.widget<Padding>(paddingFinder);
-    expect(padding.padding, const EdgeInsets.all(4.0));
-  });
+      final Material material = tester.widget<Material>(itemMaterialFinder);
+      expect(material.clipBehavior, Clip.antiAlias);
+      expect(
+        material.shape,
+        const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(28.0)),
+        ),
+      );
+
+      final Finder paddingFinder = find
+          .ancestor(of: itemMaterialFinder, matching: find.byType(Padding))
+          .first;
+
+      final Padding padding = tester.widget<Padding>(paddingFinder);
+      expect(padding.padding, const EdgeInsets.all(4.0));
+    },
+  );
 }
