44a1f67ea0aeca934ec7567342bc773a5919f1da
Victor Sanni <victorsanniay@gmail.com>
2026-09-09 16:12:49 -0700

    [cupertino_ui]Add awaitNotRequired to Future-returning callsites without await (#12817)
    cupertino_ui split of [Add awaitNotRequired annotation to Material/Cupertino libraries](https://github.com/flutter/flutter/pull/185446)
    
    Part of [Use @awaitNotRequired in Flutter SDK](https://github.com/flutter/flutter/issues/168555)

--- packages/cupertino_ui/lib/src/route.dart
+++ packages/cupertino_ui/lib/src/route.dart
@@ -1344,6 +1344,7 @@ class CupertinoModalPopupRoute<T> extends PopupRoute<T> {
 ///  * [CupertinoActionSheet], which is the widget usually returned by the
 ///    `builder` argument to [showCupertinoModalPopup].
 ///  * <https://developer.apple.com/design/human-interface-guidelines/ios/views/action-sheets/>
+@awaitNotRequired
 Future<T?> showCupertinoModalPopup<T>({
   required BuildContext context,
   required WidgetBuilder builder,
@@ -1447,6 +1448,7 @@ Widget _buildCupertinoDialogTransitions(
 ///  * [DisplayFeatureSubScreen], which documents the specifics of how
 ///    [DisplayFeature]s can split the screen into sub-screens.
 ///  * <https://developer.apple.com/design/human-interface-guidelines/alerts/>
+@awaitNotRequired
 Future<T?> showCupertinoDialog<T>({
   required BuildContext context,
   required WidgetBuilder builder,
