11f84b547fad252bc584ad69190999c0a5ebbd2a
Ricardo Dalarme <ricardodalarme@outlook.com>
2026-09-09 10:59:28 -0300

    feat(rfw): migrate to material_ui

--- packages/rfw/test/material_widgets_test.dart
+++ packages/rfw/test/material_widgets_test.dart
@@ -6,8 +6,8 @@
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:rfw/formats.dart' show parseLibraryFile;
 import 'package:rfw/rfw.dart';
 
