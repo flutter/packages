9ea3f4ee086158ccb2fbe5ddf3e96072bbdda4e1
Navaron Bracke <brackenavaron@gmail.com>
2026-09-11 14:58:55 +0200

    add dropdown button tester

---
+++ packages/material_ui/test/dropdown_button_tester.dart
@@ -0,0 +1,136 @@
+// Copyright 2013 The Flutter Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:material_ui/material_ui.dart';
+
+/// The test app for the [DropdownButton] and [DropdownButtonFormField] widget tests.
+class TestApp extends StatefulWidget {
+  const TestApp({super.key, required this.textDirection, required this.child, this.mediaSize});
+
+  final TextDirection textDirection;
+  final Widget child;
+  final Size? mediaSize;
+
+  @override
+  State<TestApp> createState() => _TestAppState();
+}
+
+class _TestAppState extends State<TestApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Localizations(
+      locale: const Locale('en', 'US'),
+      delegates: const <LocalizationsDelegate<dynamic>>[
+        DefaultWidgetsLocalizations.delegate,
+        DefaultMaterialLocalizations.delegate,
+      ],
+      child: MediaQuery(
+        data: const MediaQueryData().copyWith(size: widget.mediaSize),
+        child: Directionality(
+          textDirection: widget.textDirection,
+          child: Navigator(
+            onGenerateRoute: (RouteSettings settings) {
+              assert(settings.name == '/');
+              return MaterialPageRoute<void>(
+                settings: settings,
+                builder: (BuildContext context) => widget.child,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// Build a [DropdownButton] for testing.
+Widget buildDropdownButton({
+  Key? buttonKey,
+  String? initialValue = 'two',
+  ValueChanged<String?>? onChanged,
+  VoidCallback? onTap,
+  Widget? icon,
+  Color? iconDisabledColor,
+  Color? iconEnabledColor,
+  double iconSize = 24.0,
+  bool isDense = false,
+  bool isExpanded = false,
+  Widget? hint,
+  Widget? disabledHint,
+  Widget? underline,
+  List<String>? items = const <String>['one', 'two', 'three', 'four'],
+  List<Widget> Function(BuildContext)? selectedItemBuilder,
+  double? itemHeight = kMinInteractiveDimension,
+  double? menuWidth,
+  AlignmentDirectional alignment = AlignmentDirectional.centerStart,
+  FocusNode? focusNode,
+  bool autofocus = false,
+  Color? focusColor,
+  Color? dropdownColor,
+  double? menuMaxHeight,
+  EdgeInsetsGeometry? padding,
+}) {
+  final List<DropdownMenuItem<String>>? listItems = items?.map<DropdownMenuItem<String>>((
+    String item,
+  ) {
+    return DropdownMenuItem<String>(
+      key: ValueKey<String>(item),
+      value: item,
+      child: Text(item, key: ValueKey<String>('${item}Text')),
+    );
+  }).toList();
+
+  return DropdownButton<String>(
+    key: buttonKey,
+    value: initialValue,
+    hint: hint,
+    disabledHint: disabledHint,
+    onChanged: onChanged,
+    onTap: onTap,
+    icon: icon,
+    iconSize: iconSize,
+    iconDisabledColor: iconDisabledColor,
+    iconEnabledColor: iconEnabledColor,
+    isDense: isDense,
+    isExpanded: isExpanded,
+    underline: underline,
+    focusNode: focusNode,
+    autofocus: autofocus,
+    focusColor: focusColor,
+    dropdownColor: dropdownColor,
+    items: listItems,
+    selectedItemBuilder: selectedItemBuilder,
+    itemHeight: itemHeight,
+    menuWidth: menuWidth,
+    alignment: alignment,
+    menuMaxHeight: menuMaxHeight,
+    padding: padding,
+  );
+}
+
+/// Build the [TestApp] frame for the dropdown test.
+Widget buildFrame({
+  required Widget child,
+  TextDirection textDirection = TextDirection.ltr,
+  Size? mediaSize,
+  Alignment dropdownAlignment = Alignment.center,
+  bool? useMaterial3,
+  InputDecorationThemeData? localInputDecorationTheme,
+}) {
+  return Theme(
+    data: ThemeData(useMaterial3: useMaterial3),
+    child: TestApp(
+      textDirection: textDirection,
+      mediaSize: mediaSize,
+      child: Material(
+        child: Align(
+          alignment: dropdownAlignment,
+          child: RepaintBoundary(
+            child: InputDecorationTheme(data: localInputDecorationTheme, child: child),
+          ),
+        ),
+      ),
+    ),
+  );
+}
