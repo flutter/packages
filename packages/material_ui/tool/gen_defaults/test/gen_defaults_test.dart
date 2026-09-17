b8264024e19eb5f1f331fa494f64da68795fd6ec
Qun Cheng <quncheng@google.com>
2026-09-17 15:00:38 -0700

    [material_ui] Migrate M3 Drawer template to use new gen_defaults

--- packages/material_ui/tool/gen_defaults/test/gen_defaults_test.dart
+++ packages/material_ui/tool/gen_defaults/test/gen_defaults_test.dart
@@ -23,7 +23,7 @@ import '../templates/card_template.dart';
 // import '../templates/date_picker_template.dart';
 // import '../templates/dialog_template.dart';
 // import '../templates/divider_template.dart';
-// import '../templates/drawer_template.dart';
+import '../templates/drawer_template.dart';
 // import '../templates/expansion_tile_template.dart';
 // import '../templates/fab_template.dart';
 // import '../templates/filter_chip_template.dart';
@@ -499,8 +499,14 @@ void main() {
     });
 
     test('DrawerTemplateM3 emits M3 Drawer defaults from tokens', () {
-      // Intentionally empty, will be implemented during migration. See:
-      // https://github.com/flutter/flutter/issues/187899
+      final String contents = _generateContents(const DrawerTemplateM3());
+      expect(contents, contains('class _DrawerDefaultsM3 extends DrawerThemeData'));
+      expect(contents, contains('elevation: 1.0'));
+      expect(
+        contents,
+        contains('Color? get backgroundColor => Theme.of(context).colorScheme.surfaceContainerLow'),
+      );
+      expect(contents, contains('ShapeBorder? get endShape => RoundedRectangleBorder('));
     });
 
     test('ExpansionTileTemplateM3 emits M3 ExpansionTile defaults from tokens', () {
