9171f24cc704e393714fc96ef7f92d83e7841b8a
Qun Cheng <quncheng@google.com>
2026-09-17 15:00:25 -0700

    [material_ui] Migrate M3 Divider template to use new gen_defaults

--- packages/material_ui/tool/gen_defaults/bin/gen_defaults.dart
+++ packages/material_ui/tool/gen_defaults/bin/gen_defaults.dart
@@ -26,7 +26,7 @@ import '../templates/card_template.dart';
 // import '../templates/color_scheme_template.dart';
 // import '../templates/date_picker_template.dart';
 // import '../templates/dialog_template.dart';
-// import '../templates/divider_template.dart';
+import '../templates/divider_template.dart';
 // import '../templates/drawer_template.dart';
 // import '../templates/expansion_tile_template.dart';
 // import '../templates/fab_template.dart';
@@ -83,7 +83,7 @@ Future<void> main(List<String> args) async {
   // const ColorSchemeTemplateM3().generateFile(verbose: verbose);
   // const DatePickerTemplateM3().generateFile(verbose: verbose);
   // const DialogTemplateM3().generateFile(verbose: verbose);
-  // const DividerTemplateM3().generateFile(verbose: verbose);
+  const DividerTemplateM3().generateFile(verbose: verbose);
   // const DrawerTemplateM3().generateFile(verbose: verbose);
   // const ExpansionTileTemplateM3().generateFile(verbose: verbose);
   // const FabTemplateM3().generateFile(verbose: verbose);
