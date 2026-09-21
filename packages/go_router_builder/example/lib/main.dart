ea750a4f24839cf083264916aa98c7556e80fb12
Kate Lovett <katelovett@google.com>
2026-09-21 17:29:44 -0500

    Revert "[go_router_builder] Migrate to material_ui (#12913)"
    This reverts commit f25a6d951c0000f95fca9141aa4035d854abfad1.

--- packages/go_router_builder/example/lib/main.dart
+++ packages/go_router_builder/example/lib/main.dart
@@ -6,7 +6,7 @@
 
 import 'dart:async';
 
-import 'package:material_ui/material_ui.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
