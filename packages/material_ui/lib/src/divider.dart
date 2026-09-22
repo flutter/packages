96628bb1e8a3144ee3406cea3967167b977ae574
Hameed Habeeblahi Gbolahan <ahabeeblahi1@gmail.com>
2026-09-22 01:45:11 +0100

    [material_ui] Document Material 3 fallback for Divider.color and VerticalDivider.color
    The Divider.color and VerticalDivider.color docstrings previously
    stated that when the color and DividerThemeData.color are both null,
    ThemeData.dividerColor is used. In Material 3 (the default) the
    effective fallback is actually ColorScheme.outlineVariant. Align the
    public field docs with the actual behavior.
    
    Port of https://github.com/flutter/flutter/pull/193136 (closed due to
    code freeze on packages/flutter/lib/src/material).
    
    Fixes: https://github.com/flutter/flutter/issues/190534

--- packages/material_ui/lib/src/divider.dart
+++ packages/material_ui/lib/src/divider.dart
@@ -131,7 +131,8 @@ class Divider extends StatelessWidget {
   /// The color to use when painting the line.
   ///
   /// If this is null, then the [DividerThemeData.color] is used. If that is
-  /// also null, then [ThemeData.dividerColor] is used.
+  /// also null, then if [ThemeData.useMaterial3] is true it defaults to
+  /// [ColorScheme.outlineVariant], otherwise [ThemeData.dividerColor] is used.
   /// {@endtemplate}
   ///
   /// <callout-box>
@@ -324,7 +325,8 @@ class VerticalDivider extends StatelessWidget {
   /// The color to use when painting the line.
   ///
   /// If this is null, then the [DividerThemeData.color] is used. If that is
-  /// also null, then [ThemeData.dividerColor] is used.
+  /// also null, then if [ThemeData.useMaterial3] is true it defaults to
+  /// [ColorScheme.outlineVariant], otherwise [ThemeData.dividerColor] is used.
   ///
   /// <callout-box>
   ///
