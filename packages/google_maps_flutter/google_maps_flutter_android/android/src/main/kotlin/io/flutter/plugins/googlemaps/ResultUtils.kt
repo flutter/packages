cf18d4f535faec73b554a226c729a62a9697c89b
Stuart Morgan <stuartmorgan@google.com>
2026-09-22 13:56:42 -0400

    Remove old compat utils

--- packages/google_maps_flutter/google_maps_flutter_android/android/src/main/kotlin/io/flutter/plugins/googlemaps/ResultUtils.kt
+++ packages/google_maps_flutter/google_maps_flutter_android/android/src/main/kotlin/io/flutter/plugins/googlemaps/ResultUtils.kt
@@ -35,37 +35,12 @@ private val noopContinuation =
 @Suppress("UNCHECKED_CAST")
 fun <T> emptyContinuation(): Continuation<T> = noopContinuation as Continuation<T>
 
-fun <T> completeWithError(callback: (Result<@JvmSuppressWildcards T>) -> Unit, failure: Throwable) {
-  callback(Result.failure(failure))
-}
-
-fun <T> completeWithValue(callback: (Result<@JvmSuppressWildcards T>) -> Unit, value: T) {
-  callback(Result.success(value))
-}
-
-fun completeWithUnitSuccess(callback: (Result<Unit>) -> Unit) {
-  callback(Result.success(Unit))
-}
-
 @Suppress("UNCHECKED_CAST")
 class ResultCompat<T>(private val result: Result<T>) {
   private val value: T? = result.getOrNull()
   private val exception = result.exceptionOrNull()
-  val isSuccess = result.isSuccess
-  val isFailure = result.isFailure
 
   companion object {
-    @JvmStatic
-    fun <T> success(value: T, callback: Any) {
-      val castedCallback: (Result<T>) -> Unit = callback as (Result<T>) -> Unit
-      castedCallback(Result.success(value))
-    }
-
-    @JvmStatic
-    fun <T> asCompatCallback(result: (ResultCompat<T>) -> Unit): (Result<T>) -> Unit {
-      return { result(ResultCompat(it)) }
-    }
-
     @JvmStatic
     fun <T> asContinuation(result: (ResultCompat<T>) -> Unit): Continuation<T> {
       return object : Continuation<T> {
